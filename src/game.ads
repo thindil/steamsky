5c2ec7bef77f2187718ffee26e5471f51951d277
Bartek thindil Jasicki <thindil@laeran.pl>
2016-07-20 22:33:03 +0200

    started work on messages system

--- src/game.ads
+++ src/game.ads
@@ -18,7 +18,7 @@
 package Game is
 
     type GameStates is (Quit, Main_Menu, Sky_Map_View, Control_Speed,
-        Ship_Info, Crew_Info, Giving_Orders); -- Game states
+        Ship_Info, Crew_Info, Giving_Orders, Messages_View); -- Game states
     type Date_Record is -- Data for game date/time
         record
             Year : Natural;
