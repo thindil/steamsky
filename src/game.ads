22c3e7d6ad116b9b1a9d918d3c5449010b036fbf
Bartek thindil Jasicki <thindil@laeran.pl>
2016-08-02 08:55:57 +0200

    started work on random events

--- src/game.ads
+++ src/game.ads
@@ -21,7 +21,7 @@ package Game is
 
     type GameStates is (Quit, Main_Menu, Sky_Map_View, Control_Speed,
         Ship_Info, Crew_Info, Giving_Orders, Messages_View, Trade_View,
-        Help_View, Quit_Confirm, New_Game); -- Game states
+        Help_View, Quit_Confirm, New_Game, Combat); -- Game states
     type Date_Record is -- Data for game date/time
         record
             Year : Natural;
