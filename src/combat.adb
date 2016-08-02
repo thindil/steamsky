e5c4e4591fb1d9408d15e3c09a0c239170233622
thindil@laeran.pl <thindil@laeran.pl>
2016-08-02 16:53:01 +0000

    continue work on combat ui
    FossilOrigin-Name: 0357a6744edbf866d358a83a8416994b8a823a6b4dfe7ccb142d699183b3669c

--- src/combat.adb
+++ src/combat.adb
@@ -26,9 +26,12 @@ package body Combat is
         record
             Name : Unbounded_String;
             Durability : Natural;
+            MaxDurability : Natural;
             Damage : Positive;
             DamageRange : Natural;
             Accuracy : Positive;
+            Distance : Unbounded_String;
+            Speed : ShipSpeed; 
         end record;
     Enemy : Enemy_Record;
 
@@ -38,15 +41,18 @@ package body Combat is
             when SmallPirateShip =>
                 Enemy := (Name => To_Unbounded_String("Small pirates ship"),
                     Durability => 100, Damage => 5, DamageRange => 1, Accuracy
-                    => 1);
+                    => 1, MaxDurability => 100, Distance =>
+                    To_Unbounded_String("Long"), Speed => FULL_SPEED);
             when SmallUndeadShip =>
                 Enemy := (Name => To_Unbounded_String("Small undead ship"),
                     Durability => 100, Damage => 10, DamageRange => 0, Accuracy
-                    => 1);
+                    => 1, MaxDurability => 100, Distance =>
+                    To_Unbounded_String("Long"), Speed => FULL_SPEED);
             when SmallDrone =>
                 Enemy := (Name => To_Unbounded_String("Small clockwork drone"),
                     Durability => 50, Damage => 5, DamageRange => 0, Accuracy
-                    => 1);
+                    => 1, MaxDurability => 50, Distance =>
+                    To_Unbounded_String("Long"), Speed => FULL_SPEED);
         end case;
     end StartCombat;
 
@@ -68,10 +74,54 @@ package body Combat is
         end loop;
         Move_Cursor(Line => 1, Column => 2);
         Add(Str => "Pilot: " & To_String(PilotName));
+        Change_Attributes(Line => 1, Column => 2,
+            Count => 1, Color => 1);
         Move_Cursor(Line => 2, Column => 2);
         Add(Str => "Engineer: " & To_String(EngineerName));
+        Change_Attributes(Line => 2, Column => 2,
+            Count => 1, Color => 1);
         Move_Cursor(Line => 3, Column => 2);
         Add(Str => "Gunner: " & To_String(GunnerName));
+        Change_Attributes(Line => 3, Column => 2,
+            Count => 1, Color => 1);
+        Move_Cursor(Line => 5, Column => 2);
+        Add(Str => "Ship status:");
+        for I in PlayerShip.Modules.First_Index..PlayerShip.Modules.Last_Index loop
+            Move_Cursor(Line => Line_Position(6 + I), Column => 2);
+            Add(Str => To_String(PlayerShip.Modules.Element(I).Name) & ": ");
+            if PlayerShip.Modules.Element(I).Durability < PlayerShip.Modules.Element(I).MaxDurability then
+                Add(Str => "Damaged");
+            else
+                Add(Str => "OK");
+            end if;
+        end loop;
+        Move_Cursor(Line => 5, Column => (Columns / 2));
+        Add(Str => "Enemy status:");
+        Move_Cursor(Line => 7, Column => (Columns / 2));
+        Add(Str => "Enemy: " & To_String(Enemy.Name));
+        Move_Cursor(Line => 8, Column => (Columns / 2));
+        Add(Str => "Distance: " & To_String(Enemy.Distance));
+        Move_Cursor(Line => 9, Column => (Columns / 2));
+        Add(Str => "Status: ");
+        if Enemy.Durability = Enemy.MaxDurability then
+            Add(Str => "Ok");
+        else
+            Add(Str => "Damaged");
+        end if;
+        Move_Cursor(Line => 10, Column => (Columns / 2));
+        Add(Str => "Speed: ");
+        case Enemy.Speed is
+            when FULL_STOP =>
+                Add(Str => "Stopped");
+            when QUARTER_SPEED =>
+                Add(Str => "Slow");
+            when HALF_SPEED =>
+                Add(Str => "Medium");
+            when FULL_SPEED =>
+                Add(Str => "Fast");
+            when others =>
+                null;
+        end case;
     end ShowCombat;
 
 end Combat;
