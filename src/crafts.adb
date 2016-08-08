4095a196c9e518f2e72f46d301f44fe0d6d9cec6
Bartek thindil Jasicki <thindil@laeran.pl>
2016-08-08 08:57:19 +0200

    continue work on crafting ui

--- src/crafts.adb
+++ src/crafts.adb
@@ -15,11 +15,117 @@
 --    You should have received a copy of the GNU General Public License
 --    along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with UserInterface; use UserInterface;
+with Messages; use Messages;
+
 package body Crafts is
 
-    procedure ShowCraft is
+    RecipeIndex : Natural;
+
+    procedure SetRecipe is
+        MaterialIndex : Natural := 0;
+        FreeCargo : Integer := 0;
     begin
-        null;
+        -- Check for materials
+        for I in PlayerShip.Cargo.First_Index..PlayerShip.Cargo.Last_Index loop
+            if Objects_Prototypes(PlayerShip.Cargo.Element(I).ProtoIndex).IType = Recipes(RecipeIndex).MaterialType and
+                PlayerShip.Cargo.Element(I).Amount >= Recipes(RecipeIndex).MaterialAmount then
+                MaterialIndex := I;
+                exit;
+            end if;
+        end loop;
+        if MaterialIndex = 0 then
+            ShowDialog("You don't have enough materials to start manufacturing this recipe.");
+            return;
+        end if;
+        for I in PlayerShip.Modules.First_Index..PlayerShip.Modules.Last_Index loop
+            if PlayerShip.Modules.Element(I).Mtype = CARGO then
+                FreeCargo := FreeCargo + PlayerShip.Modules.Element(I).Max_Value;
+            end if;
+        end loop;
+        for I in PlayerShip.Cargo.First_Index..PlayerShip.Cargo.Last_Index loop
+            FreeCargo := FreeCargo - (Objects_Prototypes(PlayerShip.Cargo.Element(I).ProtoIndex).Weight * 
+                PlayerShip.Cargo.Element(I).Amount);
+        end loop;
+        FreeCargo := FreeCargo - (Objects_Prototypes(Recipes(RecipeIndex).ResultIndex).Weight *
+            Recipes(RecipeIndex).ResultAmount);
+        FreeCargo := FreeCargo + (Objects_Prototypes(MaterialIndex).Weight *
+            Recipes(RecipeIndex).MaterialAmount);
+        if FreeCargo < 0 then
+            ShowDialog("You don't have that much free space in your ship cargo.");
+            return;
+        end if;
+        PlayerShip.Craft := RecipeIndex;
+        AddMessage(To_String(Objects_Prototypes(Recipes(RecipeIndex).ResultIndex).Name) & " was set as manufacturing order.");
+    end SetRecipe;
+
+    procedure ShowCraft(Key : Key_Code) is
+        MAmount : Natural := 0;
+    begin
+        if Key /= KEY_NONE then
+            Erase;
+            Refresh;
+            ShowGameMenu(Craft_View);
+        end if;
+        Move_Cursor(Line => 2, Column => 2);
+        Add(Str => "Recipes");
+        for I in Recipes'Range loop
+            Move_Cursor(Line => Line_Position(2 + I), Column => 2);
+            Add(Str => Character'Val(96 + I) & " " & To_String(Objects_Prototypes(Recipes(I).ResultIndex).Name));
+            Change_Attributes(Line => Line_Position(2 + I), Column => 2, Count => 1, Color => 1);
+        end loop;
+        if Key /= KEY_NONE then -- Show info about selected recipe
+            if (Key >= Key_Code(96 + Recipes'First)) and (Key <= Key_Code(96 + Recipes'Last)) then
+                RecipeIndex := Integer(Key) - 96;
+                Move_Cursor(Line => 3, Column => (Columns / 2));
+                Add(Str => "Name: " & To_String(Objects_Prototypes(Recipes(RecipeIndex).ResultIndex).Name));
+                Move_Cursor(Line => 4, Column => (Columns / 2));
+                Add(Str => "Amount:" & Integer'Image(Recipes(RecipeIndex).ResultAmount));
+                Move_Cursor(Line => 5, Column => (Columns / 2));
+                Add(Str => "Material needed: ");
+                for I in Objects_Prototypes'Range loop
+                    if Objects_Prototypes(I).IType = Recipes(RecipeIndex).MaterialType then
+                        if MAmount > 0 then
+                            Add(Str => " or ");
+                        end if;
+                        Add(Str => To_String(Objects_Prototypes(I).Name));
+                        MAmount := MAmount + 1;
+                    end if;
+                end loop;
+                Move_Cursor(Line => 6, Column => (Columns / 2));
+                Add(Str => "Material amount:" & Integer'Image(Recipes(RecipeIndex).MaterialAmount));
+                Move_Cursor(Line => 7, Column => (Columns / 2));
+                Add(Str => "Workplace: ");
+                case Recipes(RecipeIndex).Workplace is
+                    when ALCHEMY_LAB =>
+                        Add(Str => "Alchemy lab");
+                    when others =>
+                        null;
+                end case;
+                Move_Cursor(Line => 9, Column => (Columns / 2));
+                Add(Str => "SPACE for set manufacturing order");
+                Change_Attributes(Line => 9, Column => (Columns / 2), Count => 5, Color => 1);
+            end if;
+        end if;
     end ShowCraft;
+
+    function CraftKeys(Key : Key_Code) return GameStates is
+    begin
+        case Key is
+            when Character'Pos('q') | Character'Pos('Q') => -- Back to sky map
+                DrawGame(Sky_Map_View);
+                return Sky_Map_View;
+            when Character'Pos(' ') => -- Set selected manufacturing order
+                ShowCraft(Key_Code(RecipeIndex + 96));
+                if RecipeIndex > 0 then
+                    SetRecipe;
+                end if;
+                return Craft_View;
+            when others =>
+                ShowCraft(Key);
+                return Craft_View;
+        end case;
+    end CraftKeys;
 
 end Crafts;
