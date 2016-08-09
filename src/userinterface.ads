a84ac2a04bb56bb31a438319115109520ddbc47a
Bartek thindil Jasicki <thindil@laeran.pl>
2016-08-09 08:13:13 +0200

    moved help to separate files

--- src/userinterface.ads
+++ src/userinterface.ads
@@ -28,7 +28,6 @@ package UserInterface is
     function MainMenuKeys(Key : Key_Code) return GameStates; -- Handle keys on main menu
     function GameMenuKeys(CurrentState : GameStates; Key : Key_Code) return GameStates; -- Handle keys in game menu
     function SpeedMenuKeys(OldState : GameStates; Key : Key_Code) return GameStates; -- Handle keys in speed control menu
-    function HelpKeys(Key : Key_Code) return GameStates; -- Handle keys in help window
     function ConfirmKeys(OldState : GameStates; Key : Key_Code) return GameStates; -- Handle keys in confirm window
     function NewGameKeys(Key : Key_Code) return GameStates; -- Handle keys in new game window
     
