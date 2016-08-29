04e2a0fa120065ce5239b8f34cbfa0e1fd4e08e6
Bartek thindil Jasicki <thindil@laeran.pl>
2016-08-29 08:36:23 +0200

    code cleanup

--- src/bases.ads
+++ src/bases.ads
@@ -34,8 +34,7 @@ package Bases is
     procedure BuyItems(ItemIndex : Positive; Amount : String); -- Buy items from bases
     procedure SellItems(ItemIndex : Positive; Amount : String); -- Sell items from bases
     function GenerateBaseName return Unbounded_String; -- Generate random name for base
-    procedure ShowTrade(Key : Key_Code); -- Show trade window
-    procedure ShowTrade2; -- Show trade window
+    procedure ShowTrade; -- Show trade window
     function TradeKeys(Key : Key_Code) return GameStates; -- Handle keys in bases trades
 
 end Bases;
