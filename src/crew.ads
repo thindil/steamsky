0cc5fb8f4417e85b69f50dce184d1f63e0b86024
Bartek thindil Jasicki <thindil@laeran.pl>
2016-09-27 15:34:13 +0200

    finished work on upgrading ship modules

--- src/crew.ads
+++ src/crew.ads
@@ -21,7 +21,7 @@ with Items; use Items;
 package Crew is
 
     type Skills_Array is array(1..7, 1..2) of Natural;
-    type Crew_Orders is (Pilot, Engineer, Gunner, Rest, Repair, Craft);
+    type Crew_Orders is (Pilot, Engineer, Gunner, Rest, Repair, Craft, Upgrading);
     type Member_Data is -- Data structure for ship crew member
         record
             Name : Unbounded_String; -- Name of member
