0955012f0e76ee9a2c0ba3ae68a6a5b415d5f910
thindil@laeran.pl <thindil@laeran.pl>
2016-10-18 09:51:23 +0000

    replaced rename module window by form
    FossilOrigin-Name: c6e3b36d944453fd5f4dc68b64fd7c9d8932f8a69a66f88bf6998965e0451985

--- src/ships-ui.ads
+++ src/ships-ui.ads
@@ -24,5 +24,6 @@ package Ships.UI is
     function ShipInfoKeys(Key : Key_Code; OldState : GameStates) return GameStates; -- Handle keys in ship info menu
     function CargoInfoKeys(Key : Key_Code; OldState : GameStates) return GameStates; -- Handle keys in cargo info menu
     function ShipUpgradeKeys(Key : Key_Code) return GameStates; -- Handle keys in upgrade menu
+    function RenameModuleKeys(Key : Key_Code) return GameStates; -- Handle keys in upgrade menu
 
 end Ships.UI;
