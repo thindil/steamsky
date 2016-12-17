faa1d140d0e83eabf0cfe88fdee311945589e51c
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2016-12-17 06:34:58 +0000

    updated destroyed ship list to pad
    FossilOrigin-Name: dcf6e7d038c8df37151d37b9623ce409d6636525916a73793d2d8c2fbe734d83

--- src/statistics.adb
+++ src/statistics.adb
@@ -20,6 +20,9 @@ with Ships; use Ships;
 
 package body Statistics is
 
+    DestroyedShipsPad : Window;
+    StartIndex, EndIndex : Integer := 0;
+
     procedure UpdateDestroyedShips(ShipName : Unbounded_String) is
         ProtoIndex : Natural := 0;
         procedure UpdateData(Ship : in out DestroyedShips_Data) is
@@ -52,59 +55,92 @@ package body Statistics is
         GameStats.MapVisited := 1;
     end ClearGameStats;
 
-    procedure ShowGameStats is
+    procedure ShowGameStats(RefreshOnly : Boolean := False) is
         MinutesDiff : Natural;
         TimePassed : Date_Record := (Year => 0, Month => 0, Day => 0, Hour => 0, Minutes => 0);
         VisitedPercent : Natural;
     begin
-        MinutesDiff := (GameDate.Minutes + (GameDate.Hour * 60) + (GameDate.Day * 1440) + (GameDate.Month * 43200) + 
+        if not RefreshOnly then
+            MinutesDiff := (GameDate.Minutes + (GameDate.Hour * 60) + (GameDate.Day * 1440) + (GameDate.Month * 43200) + 
             (GameDate.Year * 518400)) - 829571520;
-        while MinutesDiff > 0 loop
-            if MinutesDiff >= 518400 then
-                TimePassed.Year := TimePassed.Year + 1;
-                MinutesDiff := MinutesDiff - 518400;
-            elsif MinutesDiff >= 43200 then
-                TimePassed.Month := TimePassed.Month + 1;
-                MinutesDiff := MinutesDiff - 43200;
-            elsif MinutesDiff >= 1440 then
-                TimePassed.Day := TimePassed.Day + 1;
-                MinutesDiff := MinutesDiff - 1440;
-            elsif MinutesDiff >= 60 then
-                TimePassed.Hour := TimePassed.Hour + 1;
-                MinutesDiff := MinutesDiff - 60;
-            else
-                TimePassed.Minutes := MinutesDiff;
-                MinutesDiff := 0;
-            end if;
-        end loop;
-        Move_Cursor(Line => 2, Column => 2);
-        Add(Str => "Time passed:" & Natural'Image(TimePassed.Year) & "y," & Natural'Image(TimePassed.Month) & "m," 
-            & Natural'Image(TimePassed.Day) & "d," & Natural'Image(TimePassed.Hour) & "h," & Natural'Image(TimePassed.Minutes) & "mins");
-        if GameStats.DestroyedShips.Length > 0 then
-            Move_Cursor(Line => 2, Column => (Columns / 2));
-            Add(Str => "Destroyed ships:");
-            for I in GameStats.DestroyedShips.First_Index..GameStats.DestroyedShips.Last_Index loop
-                Move_Cursor(Line => Line_Position(2 + I), Column => ((Columns / 2) + 1));
-                Add(Str => To_String(Enemies_List.Element(GameStats.DestroyedShips.Element(I).ProtoIndex).Name) & ":" & 
-                    Positive'Image(GameStats.DestroyedShips.Element(I).Amount));
+            while MinutesDiff > 0 loop
+                if MinutesDiff >= 518400 then
+                    TimePassed.Year := TimePassed.Year + 1;
+                    MinutesDiff := MinutesDiff - 518400;
+                elsif MinutesDiff >= 43200 then
+                    TimePassed.Month := TimePassed.Month + 1;
+                    MinutesDiff := MinutesDiff - 43200;
+                elsif MinutesDiff >= 1440 then
+                    TimePassed.Day := TimePassed.Day + 1;
+                    MinutesDiff := MinutesDiff - 1440;
+                elsif MinutesDiff >= 60 then
+                    TimePassed.Hour := TimePassed.Hour + 1;
+                    MinutesDiff := MinutesDiff - 60;
+                else
+                    TimePassed.Minutes := MinutesDiff;
+                    MinutesDiff := 0;
+                end if;
             end loop;
+            Move_Cursor(Line => 2, Column => 2);
+            Add(Str => "Time passed:" & Natural'Image(TimePassed.Year) & "y," & Natural'Image(TimePassed.Month) & "m," 
+            & Natural'Image(TimePassed.Day) & "d," & Natural'Image(TimePassed.Hour) & "h," & Natural'Image(TimePassed.Minutes) & "mins");
+            if GameStats.DestroyedShips.Length > 0 then
+                DestroyedShipsPad := New_Pad(Line_Position(GameStats.DestroyedShips.Length + 2), (Columns / 2));
+                Add(Win => DestroyedShipsPad, Str => "Destroyed ships:");
+                for I in GameStats.DestroyedShips.First_Index..GameStats.DestroyedShips.Last_Index loop
+                    Move_Cursor(Win => DestroyedShipsPad, Line => Line_Position(I), Column => 0);
+                    Add(Win => DestroyedShipsPad, Str => 
+                        To_String(Enemies_List.Element(GameStats.DestroyedShips.Element(I).ProtoIndex).Name) & ":" & 
+                        Positive'Image(GameStats.DestroyedShips.Element(I).Amount));
+                end loop;
+                EndIndex := Integer(GameStats.DestroyedShips.Length) - Integer(Lines - 2);
+            else
+                DestroyedShipsPad := New_Pad(2, (Columns / 2));
+                Add(Win => DestroyedShipsPad, Str => "Destroyed ships:");
+                Move_Cursor(Win => DestroyedShipsPad, Line => 1, Column => 0);
+                Add(Win => DestroyedShipsPad, Str => "none");
+            end if;
+            Move_Cursor(Line => 3, Column => 2);
+            VisitedPercent := Natural((Float(GameStats.BasesVisited) / 1024.0) * 100.0);
+            Add(Str => "Bases visited:" & Positive'Image(GameStats.BasesVisited) & " (" & Natural'Image(VisitedPercent) & "% )");
+            Move_Cursor(Line => 4, Column => 2);
+            VisitedPercent := Natural((Float(GameStats.MapVisited) / (1024.0 * 1024.0)) * 100.0);
+            Add(Str => "Map discovered:" & Positive'Image(VisitedPercent) & "%");
+            Refresh;
         end if;
-        Move_Cursor(Line => 3, Column => 2);
-        VisitedPercent := Natural((Float(GameStats.BasesVisited) / 1024.0) * 100.0);
-        Add(Str => "Bases visited:" & Positive'Image(GameStats.BasesVisited) & " (" & Natural'Image(VisitedPercent) & "% )");
-        Move_Cursor(Line => 4, Column => 2);
-        VisitedPercent := Natural((Float(GameStats.MapVisited) / (1024.0 * 1024.0)) * 100.0);
-        Add(Str => "Map discovered:" & Positive'Image(VisitedPercent) & "%");
+        Refresh(DestroyedShipsPad, Line_Position(StartIndex), 0, 2, (Columns / 2), (Lines - 1), Columns);
     end ShowGameStats;
 
     function ShowGameStatsKeys(Key : Key_Code) return GameStates is
     begin
         case Key is
             when Character'Pos('q') | Character'Pos('Q') => -- Back to sky map
+                StartIndex := 0;
                 DrawGame(Sky_Map_View);
                 return Sky_Map_View;
+            when 56 | KEY_UP => -- Scroll destroyed ships list one line up
+                StartIndex := StartIndex - 1;
+            when 50 | KEY_DOWN => -- Scroll destroyed ships list one line down
+                StartIndex := StartIndex + 1;
+            when 51 | KEY_NPAGE => -- Scroll destroyed ship list one screen down
+                StartIndex := StartIndex + Integer(Lines - 2);
+            when 57 | KEY_PPAGE => -- Scroll destroyed ship list one screen up
+                StartIndex := StartIndex - Integer(Lines - 2);
+            when 55 | KEY_HOME => -- Scroll destroyed ship list to start
+                StartIndex := 0;
+            when 49 | KEY_END => -- Scroll destroyed ship list to end
+                StartIndex := EndIndex;
             when others =>
-                return GameStats_View;
+                null;
         end case;
+        if StartIndex < 0 then
+            StartIndex := 0;
+        end if;
+        if StartIndex > EndIndex then
+            StartIndex := EndIndex;
+        end if;
+        ShowGameStats(True);
+        return GameStats_View;
     end ShowGameStatsKeys;
+
 end Statistics;
