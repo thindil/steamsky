91f413a292a5463995e810c973c8744e41f75e99
thindil@laeran.pl <thindil@laeran.pl>
2017-04-07 05:42:23 +0000

    updated game version
    FossilOrigin-Name: 0c3fbad0a06267cb9cf9fb957a570a8c407fb382885b9d92855058cb8ec483e5

--- src/game.ads
+++ src/game.ads
@@ -80,7 +80,7 @@ package Game is
       Minutes: Natural;
    end record;
    GameDate: Date_Record;
-   GameVersion: constant String := "Version: 1.0";
+   GameVersion: constant String := "Version: 1.0.1";
    package UnboundedString_Container is new Vectors
      (Positive,
       Unbounded_String);
