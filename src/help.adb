b3b404f52b825a4aaa67fe41c6c312d5f1b21466
Bartek thindil Jasicki <thindil@laeran.pl>
2017-05-18 07:28:01 +0200

    added support for many help files

--- src/help.adb
+++ src/help.adb
@@ -17,6 +17,7 @@
 
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Directories; use Ada.Directories;
+with Log; use Log;
 
 package body Help is
 
@@ -24,35 +25,50 @@ package body Help is
       HelpFile: File_Type;
       RawData: Unbounded_String;
       TmpHelp: Help_Data;
+      Files: Search_Type;
+      FoundFile: Directory_Entry_Type;
    begin
       if Help_List.Length > 0 then
          return;
       end if;
-      if not Exists("data/help.dat") then
-         raise Help_File_Not_Found;
+      if not Exists("data/help/") then
+         raise Help_Directory_Not_Found;
       end if;
-      TmpHelp :=
-        (Title => Null_Unbounded_String, Text => Null_Unbounded_String);
-      Open(HelpFile, In_File, "data/help.dat");
-      while not End_Of_File(HelpFile) loop
-         RawData := To_Unbounded_String(Get_Line(HelpFile));
-         if Element(RawData, 1) /= '[' then
-            Append(TmpHelp.Text, RawData);
-            Append(TmpHelp.Text, ASCII.LF);
-         else
-            if TmpHelp.Text /= Null_Unbounded_String then
-               Help_List.Append(New_Item => TmpHelp);
-               TmpHelp :=
-                 (Title => Null_Unbounded_String,
-                  Text => Null_Unbounded_String);
+      Start_Search(Files, "data/help/", "*.dat");
+      if not More_Entries(Files) then
+         raise Help_Files_Not_Found;
+      end if;
+      while More_Entries(Files) loop
+         Get_Next_Entry(Files, FoundFile);
+         TmpHelp :=
+           (Title => Null_Unbounded_String, Text => Null_Unbounded_String);
+         LogMessage("Loading help file: " & Full_Name(FoundFile), Everything);
+         Open(HelpFile, In_File, Full_Name(FoundFile));
+         while not End_Of_File(HelpFile) loop
+            RawData := To_Unbounded_String(Get_Line(HelpFile));
+            if Element(RawData, 1) /= '[' then
+               Append(TmpHelp.Text, RawData);
+               Append(TmpHelp.Text, ASCII.LF);
+            else
+               if TmpHelp.Text /= Null_Unbounded_String then
+                  Help_List.Append(New_Item => TmpHelp);
+                  LogMessage
+                    ("Help added: " & To_String(TmpHelp.Title),
+                     Everything);
+                  TmpHelp :=
+                    (Title => Null_Unbounded_String,
+                     Text => Null_Unbounded_String);
+               end if;
+               if Length(RawData) > 2 then
+                  TmpHelp.Title :=
+                    To_Unbounded_String
+                      (Slice(RawData, 2, Length(RawData) - 1));
+               end if;
             end if;
-            if Length(RawData) > 2 then
-               TmpHelp.Title :=
-                 To_Unbounded_String(Slice(RawData, 2, Length(RawData) - 1));
-            end if;
-         end if;
+         end loop;
+         Close(HelpFile);
       end loop;
-      Close(HelpFile);
+      End_Search(Files);
    end LoadHelp;
 
 end Help;
