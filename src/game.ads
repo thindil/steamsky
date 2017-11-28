5102925da9c21744c5960d0c835bc2da89c65565
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2017-11-28 06:03:03 +0000

    added option to set which skill is used for unarmed combat
    FossilOrigin-Name: 6b4868a7ac4e81eb80835530aa084d0298a5b8b88e249706f2a7f7f86a27f07e

--- src/game.ads
+++ src/game.ads
@@ -163,6 +163,7 @@ package Game is
    TalkingSkill: Positive; -- Index of skill used for talk in bases or with other ships
    PerceptionSkill: Positive; -- Index of skill used for spoting
    DodgeSkill: Positive; -- Index of skill used for dodge in character's combat
+   UnarmedSkill: Positive; -- Index of skill used for unarmed attacks in character's combat
    HeadArmor: Unbounded_String; -- Name of item type used as characters head armor
    ChestArmor: Unbounded_String; -- Name of item type used as characters torso armor
    ArmsArmor: Unbounded_String; -- Name of item type used as characters arms armor
