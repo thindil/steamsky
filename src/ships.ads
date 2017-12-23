561a3b171d3d4bd2693fa9d9ffad558adbe2f1ad
Bartek thindil Jasicki <thindil@laeran.pl>
2017-12-23 07:03:52 +0100

    moved save/load player ship code to separated files

--- src/ships.ads
+++ src/ships.ads
@@ -17,7 +17,6 @@
 
 with Ada.Containers.Vectors; use Ada.Containers;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
-with Ada.Text_IO; use Ada.Text_IO;
 with Crew; use Crew;
 with Game; use Game;
 with Missions; use Missions;
@@ -105,9 +104,5 @@ package Ships is
      return Unbounded_String; -- Generate random name for ship
    function CountCombatValue
      return Natural; -- Count combat value of player ship
-   procedure SavePlayerShip
-     (SaveGame: in out File_Type); -- Save player ship to file
-   procedure LoadPlayerShip
-     (SaveGame: File_Type); -- Load saved player ship from file
 
 end Ships;
