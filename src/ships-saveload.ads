8112563be9df5e8899794cd3cd5cbc4c35131834
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2017-12-23 06:03:51 +0000

    moved save/load player ship code to separated files
    FossilOrigin-Name: 7b69656db5d7f1b0186fb6a9a64ab9ae756707d0b1fb9c20e86fe7aa603f0179

---
+++ src/ships-saveload.ads
@@ -0,0 +1,27 @@
+--    Copyright 2017 Bartek thindil Jasicki
+--
+--    This file is part of Steam Sky.
+--
+--    Steam Sky is free software: you can redistribute it and/or modify
+--    it under the terms of the GNU General Public License as published by
+--    the Free Software Foundation, either version 3 of the License, or
+--    (at your option) any later version.
+--
+--    Steam Sky is distributed in the hope that it will be useful,
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--    GNU General Public License for more details.
+--
+--    You should have received a copy of the GNU General Public License
+--    along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
+
+with Ada.Text_IO; use Ada.Text_IO;
+
+package Ships.SaveLoad is
+
+   procedure SavePlayerShip
+     (SaveGame: in out File_Type); -- Save player ship to file
+   procedure LoadPlayerShip
+     (SaveGame: File_Type); -- Load saved player ship from file
+
+end Ships.SaveLoad;
