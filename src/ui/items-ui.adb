196c95ccf67aae9ee410e1597402bd5610d17bd5
thindil@laeran.pl <thindil@laeran.pl>
2017-12-26 06:02:28 +0000

    moved showing item info to separated function
    FossilOrigin-Name: cae8ac0e70028c9fd59ba1f2e23bb214d336beb317f62547a5ad2bf0d286eda2

--- src/ui/items-ui.adb
+++ src/ui/items-ui.adb
@@ -1,4 +1,4 @@
---    Copyright 2016-2017 Bartek thindil Jasicki
+--    Copyright 2017 Bartek thindil Jasicki
 --
 --    This file is part of Steam Sky.
 --
@@ -14,6 +14,8 @@
 --
 --    You should have received a copy of the GNU General Public License
 --    along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
+
+with Utils.UI; use Utils.UI;
 
 package body Items.UI is
 
@@ -52,5 +54,103 @@ package body Items.UI is
          Count => TextLength,
          Color => TextColor);
    end ShowItemStatus;
+
+   function ShowItemInfo
+     (Inventory: Inventory_Container.Vector;
+      ItemIndex: Positive) return Line_Position is
+      InfoWindow, BoxWindow: Window;
+      ProtoIndex: constant Positive := Inventory(ItemIndex).ProtoIndex;
+      ItemWeight: constant Positive :=
+        Inventory(ItemIndex).Amount * Items_List(ProtoIndex).Weight;
+      WindowHeight: Line_Position := 8;
+      WindowWidth: Column_Position;
+      CurrentLine: Line_Position := 1;
+   begin
+      if Inventory(ItemIndex).Durability < 100 then
+         WindowHeight := WindowHeight + 1;
+      end if;
+      if Items_List(ProtoIndex).IType = WeaponType then
+         WindowHeight := WindowHeight + 1;
+      end if;
+      WindowHeight :=
+        WindowHeight +
+        Line_Position
+          ((Length(Items_List(ProtoIndex).Description) /
+            (Natural(Columns / 2) - 4)));
+      WindowWidth :=
+        Column_Position(Length(Items_List(ProtoIndex).Description) + 5);
+      if WindowWidth <
+        Column_Position
+          (Positive'Image(Items_List(ProtoIndex).Weight)'Length + 5) then
+         WindowWidth :=
+           Column_Position
+             (Positive'Image(Items_List(ProtoIndex).Weight)'Length + 5);
+      end if;
+      if WindowWidth > (Columns / 2) then
+         WindowWidth := Columns / 2;
+      end if;
+      BoxWindow := Create(WindowHeight, WindowWidth, 3, (Columns / 2));
+      WindowFrame(BoxWindow, 2, "Item info");
+      InfoWindow :=
+        Create(WindowHeight - 2, WindowWidth - 4, 4, (Columns / 2) + 2);
+      Add(Win => InfoWindow, Str => "Type: ");
+      if Items_List(ProtoIndex).ShowType = Null_Unbounded_String then
+         Add
+           (Win => InfoWindow,
+            Str => To_String(Items_List(ProtoIndex).IType));
+      else
+         Add
+           (Win => InfoWindow,
+            Str => To_String(Items_List(ProtoIndex).ShowType));
+      end if;
+      Move_Cursor(Win => InfoWindow, Line => CurrentLine, Column => 0);
+      Add
+        (Win => InfoWindow,
+         Str => "Amount:" & Positive'Image(Inventory(ItemIndex).Amount));
+      CurrentLine := CurrentLine + 1;
+      Move_Cursor(Win => InfoWindow, Line => CurrentLine, Column => 0);
+      Add
+        (Win => InfoWindow,
+         Str =>
+           "Weight:" & Positive'Image(Items_List(ProtoIndex).Weight) & " kg");
+      CurrentLine := CurrentLine + 1;
+      Move_Cursor(Win => InfoWindow, Line => CurrentLine, Column => 0);
+      Add
+        (Win => InfoWindow,
+         Str => "Total weight:" & Positive'Image(ItemWeight) & " kg");
+      CurrentLine := CurrentLine + 1;
+      if Items_List(ProtoIndex).IType = WeaponType then
+         Move_Cursor(Win => InfoWindow, Line => CurrentLine, Column => 0);
+         Add
+           (Win => InfoWindow,
+            Str =>
+              "Skill: " &
+              To_String(Skills_List(Items_List(ProtoIndex).Value(3)).Name) &
+              "/" &
+              To_String
+                (Attributes_Names
+                   (Skills_List(Items_List(ProtoIndex).Value(3)).Attribute)));
+         CurrentLine := CurrentLine + 1;
+      end if;
+      if Inventory(ItemIndex).Durability < 100 then
+         Move_Cursor(Win => InfoWindow, Line => CurrentLine, Column => 0);
+         Add(Win => InfoWindow, Str => "Status: ");
+         ShowItemStatus(Inventory, ItemIndex, InfoWindow, CurrentLine);
+         CurrentLine := CurrentLine + 1;
+      end if;
+      if Items_List(ProtoIndex).Description /= Null_Unbounded_String then
+         CurrentLine := CurrentLine + 1;
+         Move_Cursor(Win => InfoWindow, Line => CurrentLine, Column => 0);
+         Add
+           (Win => InfoWindow,
+            Str => To_String(Items_List(ProtoIndex).Description));
+      end if;
+      Refresh_Without_Update;
+      Refresh_Without_Update(BoxWindow);
+      Delete(BoxWindow);
+      Refresh_Without_Update(InfoWindow);
+      Delete(InfoWindow);
+      return WindowHeight + 3;
+   end ShowItemInfo;
 
 end Items.UI;
