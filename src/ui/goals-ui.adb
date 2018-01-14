cf78cdcc04c507dd32f3b997adf21a4f44d193eb
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2018-01-14 06:01:35 +0000

    continue work on goals ui
    FossilOrigin-Name: a239c3eb1f32ce44744d11b719e4a6d2e2391b02d9e4db4e49501f4a060b4d32

--- src/ui/goals-ui.adb
+++ src/ui/goals-ui.adb
@@ -19,14 +19,17 @@ with Ada.Text_IO; use Ada.Text_IO;
 with GNAT.Directory_Operations; use GNAT.Directory_Operations;
 with Gtk.Widget; use Gtk.Widget;
 with Gtk.Tree_Model; use Gtk.Tree_Model;
+with Gtk.Tree_Store; use Gtk.Tree_Store;
 with Glib; use Glib;
 with Glib.Error; use Glib.Error;
 with Glib.Object; use Glib.Object;
 with Game; use Game;
+with Goals; use Goals;
 
 package body Goals.UI is
 
-   function HideGoals(User_Data: access Gtkada_Builder_Record'Class) return Boolean is
+   function HideGoals
+     (User_Data: access Gtkada_Builder_Record'Class) return Boolean is
    begin
       return Hide_On_Delete(Gtk_Widget(Get_Object(User_Data, "goalswindow")));
    end HideGoals;
@@ -38,6 +41,24 @@ package body Goals.UI is
 
    procedure CreateGoalsMenu(Builder: Gtkada_Builder) is
       Error: aliased GError;
+      GoalsList: Gtk_Tree_Store;
+      CategoryIter: Gtk_Tree_Iter;
+      procedure AddGoals(CategoryName: String; GType: GoalTypes) is
+         GoalsIter: Gtk_Tree_Iter;
+      begin
+         Append(GoalsList, CategoryIter, Null_Iter);
+         Set(GoalsList, CategoryIter, 0, CategoryName);
+         for I in Goals_List.Iterate loop
+            if Goals_List(I).GType = GType then
+               Append(GoalsList, GoalsIter, CategoryIter);
+               Set
+                 (GoalsList,
+                  GoalsIter,
+                  0,
+                  GoalText(Goals_Container.To_Index(I)));
+            end if;
+         end loop;
+      end AddGoals;
    begin
       if Add_From_File
           (Builder,
@@ -49,6 +70,16 @@ package body Goals.UI is
       end if;
       Register_Handler(Builder, "Show_Goals", ShowGoalsMenu'Access);
       Register_Handler(Builder, "Hide_Goals", HideGoals'Access);
+      LoadGoals;
+      GoalsList := Gtk_Tree_Store(Get_Object(Builder, "goalslist"));
+      Append(GoalsList, CategoryIter, Null_Iter);
+      Set(GoalsList, CategoryIter, 0, "Random");
+      AddGoals("Gain max reputation in bases", REPUTATION);
+      AddGoals("Destroy enemy ships", DESTROY);
+      AddGoals("Discover map", DISCOVER);
+      AddGoals("Visit bases", VISIT);
+      AddGoals("Craft items", CRAFT);
+      AddGoals("Finish missions", MISSION);
    end CreateGoalsMenu;
 
 end Goals.UI;
