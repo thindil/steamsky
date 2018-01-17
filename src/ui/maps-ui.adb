2331f3577d4b50c57d4a3bc41f934923fe6690d8
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2018-01-17 06:03:12 +0000

    continue work on sky map ui
    FossilOrigin-Name: f88271893c623d149847f8d62cb932c040db11537c188e14ba00b35d4997ef31

--- src/ui/maps-ui.adb
+++ src/ui/maps-ui.adb
@@ -23,11 +23,14 @@ with Gtk.Widget; use Gtk.Widget;
 with Gtk.Message_Dialog; use Gtk.Message_Dialog;
 with Gtk.Dialog; use Gtk.Dialog;
 with Gtk.Window; use Gtk.Window;
+with Gtk.Label; use Gtk.Label;
 with Glib; use Glib;
 with Glib.Error; use Glib.Error;
 with Game; use Game;
 with MainMenu; use MainMenu;
 with Utils.UI; use Utils.UI;
+with Ships; use Ships;
+with Messages; use Messages;
 
 package body Maps.UI is
 
@@ -56,41 +59,72 @@ package body Maps.UI is
    end QuitGame;
 
    procedure QuitGameMenu(Object: access Gtkada_Builder_Record'Class) is
-      Finished: constant Boolean := QuitGame(Object);
    begin
-      if not Finished then
+      if not QuitGame(Object) then
          ShowDialog
            ("Can't quit game.",
             Gtk_Window(Get_Object(Object, "skymapwindow")));
       end if;
    end QuitGameMenu;
 
+   procedure HideLastMessage(Object: access Gtkada_Builder_Record'Class) is
+   begin
+      if not HideWindow(Get_Object(Object, "infolastmessage")) then
+         ShowDialog
+           ("Can't hide last message.",
+            Gtk_Window(Get_Object(Object, "skymapwindow")));
+      end if;
+   end HideLastMessage;
+
    procedure CreateSkyMap is
       Error: aliased GError;
    begin
-      if Builder /= null then
-         Show_All(Gtk_Widget(Get_Object(Builder, "skymapwindow")));
-         Hide(Gtk_Widget(Get_Object(Builder, "lblnofuel")));
-         Hide(Gtk_Widget(Get_Object(Builder, "lblnofood")));
-         Hide(Gtk_Widget(Get_Object(Builder, "lblnodrink")));
-         return;
+      if Builder = null then
+         Gtk_New(Builder);
+         if Add_From_File
+             (Builder,
+              To_String(DataDirectory) & "ui" & Dir_Separator & "skymap.glade",
+              Error'Access) =
+           Guint(0) then
+            Put_Line("Error : " & Get_Message(Error));
+            return;
+         end if;
+         Register_Handler(Builder, "Quit_Game", QuitGame'Access);
+         Register_Handler(Builder, "Quit_Game_Menu", QuitGameMenu'Access);
+         Register_Handler
+           (Builder,
+            "Hide_Last_Message",
+            HideLastMessage'Access);
+         Do_Connect(Builder);
       end if;
-      Gtk_New(Builder);
-      if Add_From_File
-          (Builder,
-           To_String(DataDirectory) & "ui" & Dir_Separator & "skymap.glade",
-           Error'Access) =
-        Guint(0) then
-         Put_Line("Error : " & Get_Message(Error));
-         return;
-      end if;
-      Register_Handler(Builder, "Quit_Game", QuitGame'Access);
-      Register_Handler(Builder, "Quit_Game_Menu", QuitGameMenu'Access);
-      Do_Connect(Builder);
+      Set_Text(Gtk_Label(Get_Object(Builder, "lbltime")), FormatedTime);
       Show_All(Gtk_Widget(Get_Object(Builder, "skymapwindow")));
       Hide(Gtk_Widget(Get_Object(Builder, "lblnofuel")));
       Hide(Gtk_Widget(Get_Object(Builder, "lblnofood")));
       Hide(Gtk_Widget(Get_Object(Builder, "lblnodrink")));
+      if LastMessage = Null_Unbounded_String then
+         Hide(Gtk_Widget(Get_Object(Builder, "infolastmessage")));
+      else
+         Set_Text
+           (Gtk_Label(Get_Object(Builder, "lbllastmessage")),
+            To_String(LastMessage));
+      end if;
+      if PlayerShip.Speed = DOCKED then
+         Hide(Gtk_Widget(Get_Object(Builder, "cmbspeed")));
+         Hide(Gtk_Widget(Get_Object(Builder, "btnmoveto")));
+         Set_Sensitive(Gtk_Widget(Get_Object(Builder, "btnupleft")), False);
+         Set_Sensitive(Gtk_Widget(Get_Object(Builder, "btnup")), False);
+         Set_Sensitive(Gtk_Widget(Get_Object(Builder, "btnupright")), False);
+         Set_Sensitive(Gtk_Widget(Get_Object(Builder, "btnleft")), False);
+         Set_Sensitive(Gtk_Widget(Get_Object(Builder, "btnright")), False);
+         Set_Sensitive
+           (Gtk_Widget(Get_Object(Builder, "btnbottomleft")),
+            False);
+         Set_Sensitive(Gtk_Widget(Get_Object(Builder, "btnbottom")), False);
+         Set_Sensitive
+           (Gtk_Widget(Get_Object(Builder, "btnbottomright")),
+            False);
+      end if;
    end CreateSkyMap;
 
 end Maps.UI;
