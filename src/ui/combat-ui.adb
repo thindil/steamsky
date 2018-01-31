c26f338a0153600cccbbadf16b98f01417670bdc
Bartek thindil Jasicki <thindil@laeran.pl>
2018-01-31 07:01:52 +0100

    continue work on combat ui

--- src/ui/combat-ui.adb
+++ src/ui/combat-ui.adb
@@ -22,26 +22,103 @@ with Gtk.Widget; use Gtk.Widget;
 with Gtk.Text_Buffer; use Gtk.Text_Buffer;
 with Gtk.List_Store; use Gtk.List_Store;
 with Gtk.Tree_Model; use Gtk.Tree_Model;
+with Gtk.Label; use Gtk.Label;
 with Glib; use Glib;
 with Glib.Error; use Glib.Error;
 with Glib.Object; use Glib.Object;
+with Gdk.RGBA; use Gdk.RGBA;
 with Game; use Game;
 with Utils.UI; use Utils.UI;
+with Bases; use Bases;
+with ShipModules; use ShipModules;
+with Events; use Events;
+with Maps; use Maps;
+with Crew; use Crew;
+with Ships.Crew; use Ships.Crew;
 
 package body Combat.UI is
 
    Builder: Gtkada_Builder;
+   PilotOrders: constant array(1 .. 4) of Unbounded_String :=
+     (To_Unbounded_String("Go closer"),
+      To_Unbounded_String("Keep distance"),
+      To_Unbounded_String("Evade"),
+      To_Unbounded_String("Escape"));
+   EngineerOrders: constant array(1 .. 4) of Unbounded_String :=
+     (To_Unbounded_String("All stop"),
+      To_Unbounded_String("Quarter speed"),
+      To_Unbounded_String("Half speed"),
+      To_Unbounded_String("Full speed"));
+   GunnerOrders: constant array(1 .. 6) of Unbounded_String :=
+     (To_Unbounded_String("Don't shoot"),
+      To_Unbounded_String("Precise fire"),
+      To_Unbounded_String("Fire at will"),
+      To_Unbounded_String("Aim for engine"),
+      To_Unbounded_String("Aim in weapon"),
+      To_Unbounded_String("Aim in hull"));
 
-   procedure ShowCombatUI is
+   procedure RefreshCombatUI is
       Iter: Gtk_Tree_Iter;
       List: Gtk_List_Store;
       DamagePercent: Gint;
       IsDamaged: Boolean := False;
+      EnemyInfo: Unbounded_String;
+      MemberIndex: Natural;
    begin
-      Show_All(Gtk_Widget(Get_Object(Builder, "combatwindow")));
       Hide(Gtk_Widget(Get_Object(Builder, "btnboard")));
       Set_Text(Gtk_Text_Buffer(Get_Object(Builder, "txtmessages")), "");
-      Clear(Gtk_List_Store(Get_Object(Builder, "crewlist")));
+      List := Gtk_List_Store(Get_Object(Builder, "crewlist"));
+      Clear(List);
+      Append(List, Iter);
+      MemberIndex := FindMember(Pilot);
+      if MemberIndex = 0 then
+         Set(List, Iter, 0, "Pilot:");
+         Set(List, Iter, 1, 0);
+         Set(List, Iter, 2, "vacant");
+      else
+         Set(List, Iter, 0, "Pilot:");
+         Set(List, Iter, 1, Gint(MemberIndex));
+         Set(List, Iter, 2, To_String(PlayerShip.Crew(MemberIndex).Name));
+         Set(List, Iter, 3, To_String(PilotOrders(PilotOrder)));
+      end if;
+      Append(List, Iter);
+      MemberIndex := FindMember(Engineer);
+      if MemberIndex = 0 then
+         Set(List, Iter, 0, "Engineer:");
+         Set(List, Iter, 1, 0);
+         Set(List, Iter, 2, "vacant");
+      else
+         Set(List, Iter, 0, "Engineer:");
+         Set(List, Iter, 1, Gint(MemberIndex));
+         Set(List, Iter, 2, To_String(PlayerShip.Crew(MemberIndex).Name));
+         Set(List, Iter, 3, To_String(EngineerOrders(EngineerOrder)));
+      end if;
+      for I in Guns.First_Index .. Guns.Last_Index loop
+         Append(List, Iter);
+         Set
+           (List,
+            Iter,
+            0,
+            To_String(PlayerShip.Modules(Guns(I)(1)).Name) & ": ");
+         if PlayerShip.Modules(Guns(I)(1)).Owner /= 0 then
+            if PlayerShip.Crew(PlayerShip.Modules(Guns(I)(1)).Owner).Order =
+              Gunner then
+               Set
+                 (List,
+                  Iter,
+                  2,
+                  To_String
+                    (PlayerShip.Crew(PlayerShip.Modules(Guns(I)(1)).Owner)
+                       .Name));
+               Set(List, Iter, 3, To_String(GunnerOrders(Guns(I)(2))));
+            else
+               Set(List, Iter, 2, "Vacant");
+            end if;
+         else
+            Set(List, Iter, 2, "Vacant");
+         end if;
+         Set(List, Iter, 1, Gint(I));
+      end loop;
       List := Gtk_List_Store(Get_Object(Builder, "damagelist"));
       Clear(List);
       for Module of PlayerShip.Modules loop
@@ -60,7 +137,106 @@ package body Combat.UI is
       if not IsDamaged then
          Hide(Gtk_Widget(Get_Object(Builder, "scrolldamage")));
       end if;
-      Clear(Gtk_List_Store(Get_Object(Builder, "enemylist")));
+      Append(EnemyInfo, "Name: ");
+      Append(EnemyInfo, EnemyName);
+      Append(EnemyInfo, ASCII.LF);
+      Append(EnemyInfo, "Type: ");
+      Append(EnemyInfo, Enemy.Ship.Name);
+      Append(EnemyInfo, ASCII.LF);
+      Append(EnemyInfo, "Home: ");
+      Append(EnemyInfo, SkyBases(Enemy.Ship.HomeBase).Name);
+      Append(EnemyInfo, ASCII.LF);
+      Append(EnemyInfo, "Distance: ");
+      if Enemy.Distance >= 15000 then
+         Append(EnemyInfo, "Escaped");
+      elsif Enemy.Distance < 15000 and Enemy.Distance >= 10000 then
+         Append(EnemyInfo, "Long");
+      elsif Enemy.Distance < 10000 and Enemy.Distance >= 5000 then
+         Append(EnemyInfo, "Medium");
+      elsif Enemy.Distance < 5000 and Enemy.Distance >= 1000 then
+         Append(EnemyInfo, "Short");
+      else
+         Append(EnemyInfo, "Close");
+      end if;
+      Append(EnemyInfo, ASCII.LF);
+      Append(EnemyInfo, "Status: ");
+      if Enemy.Distance < 15000 then
+         if Enemy.Ship.Modules(1).Durability = 0 then
+            Append(EnemyInfo, "Destroyed");
+         else
+            declare
+               EnemyStatus: Unbounded_String := To_Unbounded_String("Ok");
+            begin
+               for Module of Enemy.Ship.Modules loop
+                  if Module.Durability < Module.MaxDurability then
+                     EnemyStatus := To_Unbounded_String("Damaged");
+                     exit;
+                  end if;
+               end loop;
+               Append(EnemyInfo, EnemyStatus);
+            end;
+            for Module of Enemy.Ship.Modules loop
+               if Module.Durability > 0 then
+                  case Modules_List(Module.ProtoIndex).MType is
+                     when ARMOR =>
+                        Append(EnemyInfo, " (armored)");
+                     when GUN =>
+                        Append(EnemyInfo, " (gun)");
+                     when BATTERING_RAM =>
+                        Append(EnemyInfo, " (battering ram)");
+                     when HARPOON_GUN =>
+                        Append(EnemyInfo, " (harpoon gun)");
+                     when others =>
+                        null;
+                  end case;
+               end if;
+            end loop;
+         end if;
+      else
+         Append(EnemyInfo, "Unknown");
+      end if;
+      Append(EnemyInfo, ASCII.LF);
+      Append(EnemyInfo, "Speed: ");
+      if Enemy.Distance < 15000 then
+         case Enemy.Ship.Speed is
+            when FULL_STOP =>
+               Append(EnemyInfo, "Stopped");
+            when QUARTER_SPEED =>
+               Append(EnemyInfo, "Slow");
+            when HALF_SPEED =>
+               Append(EnemyInfo, "Medium");
+            when FULL_SPEED =>
+               Append(EnemyInfo, "Fast");
+            when others =>
+               null;
+         end case;
+      else
+         Append(EnemyInfo, "Unknown");
+      end if;
+      Set_Text
+        (Gtk_Label(Get_Object(Builder, "lblenemyinfo")),
+         To_String(EnemyInfo));
+   end RefreshCombatUI;
+
+   procedure ShowCombatUI is
+      CombatStarted: Boolean;
+   begin
+      if EnemyName /=
+        ProtoShips_List
+          (Events_List(SkyMap(PlayerShip.SkyX, PlayerShip.SkyY).EventIndex)
+             .Data)
+          .Name then
+         CombatStarted :=
+           StartCombat
+             (Events_List(SkyMap(PlayerShip.SkyX, PlayerShip.SkyY).EventIndex)
+                .Data,
+              False);
+         if not CombatStarted then
+            return;
+         end if;
+      end if;
+      Show_All(Gtk_Widget(Get_Object(Builder, "combatwindow")));
+      RefreshCombatUI;
    end ShowCombatUI;
 
    procedure CreateCombatUI is
@@ -78,6 +254,14 @@ package body Combat.UI is
          Put_Line("Error : " & Get_Message(Error));
          return;
       end if;
+      Override_Background_Color
+        (Gtk_Widget(Get_Object(Builder, "messagesview")),
+         0,
+         Black_RGBA);
+      Override_Color
+        (Gtk_Widget(Get_Object(Builder, "messagesview")),
+         0,
+         White_RGBA);
       Register_Handler(Builder, "Hide_Window", HideWindow'Access);
       Register_Handler(Builder, "Quit_Game", QuitGame'Access);
       Do_Connect(Builder);
