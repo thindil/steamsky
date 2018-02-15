2abd88059111081666242bdb85e6752c7c8bcd7e
thindil@laeran.pl <thindil@laeran.pl>
2018-02-15 06:01:58 +0000

    added showing available in base missions
    FossilOrigin-Name: 12ed5b04740b2400dff253d9f13e9395363fc563fe6a6ea2ba9bbe8f7b0c504a

--- src/ui/missions-ui.adb
+++ src/ui/missions-ui.adb
@@ -17,24 +17,263 @@
 
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with Ada.Exceptions; use Ada.Exceptions;
 with GNAT.Directory_Operations; use GNAT.Directory_Operations;
 with Gtkada.Builder; use Gtkada.Builder;
 with Gtk.Widget; use Gtk.Widget;
+with Gtk.Tree_Model; use Gtk.Tree_Model;
+with Gtk.List_Store; use Gtk.List_Store;
+with Gtk.Label; use Gtk.Label;
+with Gtk.Tree_View; use Gtk.Tree_View;
+with Gtk.Tree_View_Column; use Gtk.Tree_View_Column;
+with Gtk.Tree_Selection; use Gtk.Tree_Selection;
+with Gtk.Window; use Gtk.Window;
 with Glib; use Glib;
 with Glib.Error; use Glib.Error;
+with Maps; use Maps;
 with Maps.UI; use Maps.UI;
 with Game; use Game;
+with Ships; use Ships;
+with Bases; use Bases;
+with Messages; use Messages;
+with Items; use Items;
+with ShipModules; use ShipModules;
+with Utils.UI; use Utils.UI;
 
 package body Missions.UI is
 
    Builder: Gtkada_Builder;
+   MissionIndex: Positive;
 
    function HideMissions
      (Object: access Gtkada_Builder_Record'Class) return Boolean is
    begin
       CreateSkyMap;
-      return Hide_on_Delete(Gtk_Widget(Get_Object(Object, "missionswindow")));
+      return Hide_On_Delete(Gtk_Widget(Get_Object(Object, "missionswindow")));
    end HideMissions;
+
+   procedure HideLastMessage(Object: access Gtkada_Builder_Record'Class) is
+   begin
+      Hide(Gtk_Widget(Get_Object(Object, "infolastmessage")));
+      LastMessage := Null_Unbounded_String;
+   end HideLastMessage;
+
+   procedure ShowLastMessage is
+   begin
+      if LastMessage = Null_Unbounded_String then
+         HideLastMessage(Builder);
+      else
+         Set_Text
+           (Gtk_Label(Get_Object(Builder, "lbllastmessage")),
+            To_String(LastMessage));
+         Show_All(Gtk_Widget(Get_Object(Builder, "infolastmessage")));
+         LastMessage := Null_Unbounded_String;
+      end if;
+   end ShowLastMessage;
+
+   procedure ShowMissionInfo(Object: access Gtkada_Builder_Record'Class) is
+      MissionsIter: Gtk_Tree_Iter;
+      MissionsModel: Gtk_Tree_Model;
+      MissionInfo: Unbounded_String;
+      Mission: Mission_Data;
+      MinutesDiff: Natural;
+      MissionTime: Date_Record :=
+        (Year => 0, Month => 0, Day => 0, Hour => 0, Minutes => 0);
+      HaveCabin, CabinTaken: Boolean := False;
+      CanAccept: Boolean := True;
+      MissionsLimit: Natural;
+   begin
+      Get_Selected
+        (Gtk.Tree_View.Get_Selection
+           (Gtk_Tree_View(Get_Object(Object, "treemissions"))),
+         MissionsModel,
+         MissionsIter);
+      if MissionsIter = Null_Iter then
+         return;
+      end if;
+      MissionIndex :=
+        Natural'Value(To_String(Get_Path(MissionsModel, MissionsIter))) + 1;
+      Mission :=
+        SkyBases(SkyMap(PlayerShip.SkyX, PlayerShip.SkyY).BaseIndex).Missions
+          (MissionIndex);
+      case Mission.MType is
+         when Deliver =>
+            MissionInfo :=
+              To_Unbounded_String("Item: ") & Items_List(Mission.Target).Name;
+            Append
+              (MissionInfo,
+               ASCII.LF &
+               "Weight:" &
+               Positive'Image(Items_List(Mission.Target).Weight) &
+               " kg");
+            Append
+              (MissionInfo,
+               ASCII.LF &
+               "To base: " &
+               To_String
+                 (SkyBases(SkyMap(Mission.TargetX, Mission.TargetY).BaseIndex)
+                    .Name));
+         when Patrol =>
+            MissionInfo := To_Unbounded_String("Patrol selected area");
+         when Destroy =>
+            MissionInfo :=
+              To_Unbounded_String
+                ("Target: " & To_String(ProtoShips_List(Mission.Target).Name));
+         when Explore =>
+            MissionInfo := To_Unbounded_String("Explore selected area");
+         when Passenger =>
+            CanAccept := False;
+            for Module of PlayerShip.Modules loop
+               if Module.ProtoIndex = Mission.Target then
+                  if Module.Owner = 0 then
+                     HaveCabin := True;
+                     CanAccept := True;
+                     exit;
+                  else
+                     CabinTaken := True;
+                  end if;
+               end if;
+            end loop;
+            MissionInfo := To_Unbounded_String("Needed cabin: ");
+            if HaveCabin then
+               Append(MissionInfo, Modules_List(Mission.Target).Name);
+            elsif CabinTaken then
+               Append
+                 (MissionInfo,
+                  To_Unbounded_String("<span foreground=""yellow"">") &
+                  Modules_List(Mission.Target).Name &
+                  To_Unbounded_String("</span>"));
+            else
+               Append
+                 (MissionInfo,
+                  To_Unbounded_String("<span foreground=""red"">") &
+                  Modules_List(Mission.Target).Name &
+                  To_Unbounded_String("</span>"));
+            end if;
+            Append
+              (MissionInfo,
+               ASCII.LF &
+               "To base: " &
+               To_String
+                 (SkyBases(SkyMap(Mission.TargetX, Mission.TargetY).BaseIndex)
+                    .Name));
+      end case;
+      Append
+        (MissionInfo,
+         ASCII.LF &
+         "Distance:" &
+         Integer'Image(CountDistance(Mission.TargetX, Mission.TargetY)));
+      MinutesDiff := Mission.Time;
+      while MinutesDiff > 0 loop
+         if MinutesDiff >= 518400 then
+            MissionTime.Year := MissionTime.Year + 1;
+            MinutesDiff := MinutesDiff - 518400;
+         elsif MinutesDiff >= 43200 then
+            MissionTime.Month := MissionTime.Month + 1;
+            MinutesDiff := MinutesDiff - 43200;
+         elsif MinutesDiff >= 1440 then
+            MissionTime.Day := MissionTime.Day + 1;
+            MinutesDiff := MinutesDiff - 1440;
+         elsif MinutesDiff >= 60 then
+            MissionTime.Hour := MissionTime.Hour + 1;
+            MinutesDiff := MinutesDiff - 60;
+         else
+            MissionTime.Minutes := MinutesDiff;
+            MinutesDiff := 0;
+         end if;
+      end loop;
+      Append(MissionInfo, ASCII.LF & "Time limit:");
+      if MissionTime.Year > 0 then
+         Append(MissionInfo, Positive'Image(MissionTime.Year) & "y");
+      end if;
+      if MissionTime.Month > 0 then
+         Append(MissionInfo, Positive'Image(MissionTime.Month) & "m");
+      end if;
+      if MissionTime.Day > 0 then
+         Append(MissionInfo, Positive'Image(MissionTime.Day) & "d");
+      end if;
+      if MissionTime.Hour > 0 then
+         Append(MissionInfo, Positive'Image(MissionTime.Hour) & "h");
+      end if;
+      if MissionTime.Minutes > 0 then
+         Append(MissionInfo, Positive'Image(MissionTime.Minutes) & "mins");
+      end if;
+      Append
+        (MissionInfo,
+         ASCII.LF &
+         "Reward:" &
+         Positive'Image(Mission.Reward) &
+         " " &
+         To_String(MoneyName));
+      Set_Markup
+        (Gtk_Label(Get_Object(Object, "lblinfo")),
+         To_String(MissionInfo));
+      case SkyBases(SkyMap(PlayerShip.SkyX, PlayerShip.SkyY).BaseIndex)
+        .Reputation
+        (1) is
+         when 0 .. 25 =>
+            MissionsLimit := 1;
+         when 26 .. 50 =>
+            MissionsLimit := 3;
+         when 51 .. 75 =>
+            MissionsLimit := 5;
+         when 76 .. 100 =>
+            MissionsLimit := 10;
+         when others =>
+            MissionsLimit := 0;
+      end case;
+      for Mission of PlayerShip.Missions loop
+         if Mission.StartBase =
+           SkyMap(PlayerShip.SkyX, PlayerShip.SkyY).BaseIndex then
+            MissionsLimit := MissionsLimit - 1;
+         end if;
+      end loop;
+      if MissionsLimit > 0 then
+         Set_Label
+           (Gtk_Label(Get_Object(Object, "lblavailable")),
+            "You can take" &
+            Natural'Image(MissionsLimit) &
+            " more missions in this base.");
+      else
+         Set_Label
+           (Gtk_Label(Get_Object(Object, "lblavailable")),
+            "You can't take any more missions in this base.");
+         CanAccept := False;
+      end if;
+      if not CanAccept then
+         Set_Sensitive(Gtk_Widget(Get_Object(Object, "btnaccept")), False);
+      else
+         Set_Sensitive(Gtk_Widget(Get_Object(Object, "btnaccept")), True);
+      end if;
+   end ShowMissionInfo;
+
+   procedure AcceptSelectedMission
+     (Object: access Gtkada_Builder_Record'Class) is
+      MissionsIter: Gtk_Tree_Iter;
+      MissionsModel: Gtk_Tree_Model;
+   begin
+      AcceptMission(MissionIndex);
+      Get_Selected
+        (Gtk.Tree_View.Get_Selection
+           (Gtk_Tree_View(Get_Object(Object, "treemissions"))),
+         MissionsModel,
+         MissionsIter);
+      if MissionsIter = Null_Iter then
+         return;
+      end if;
+      Remove(-(MissionsModel), MissionsIter);
+      Set_Cursor
+        (Gtk_Tree_View(Get_Object(Builder, "treemissions")),
+         Gtk_Tree_Path_New_From_String("0"),
+         Gtk_Tree_View_Column(Get_Object(Builder, "columnmission")),
+         False);
+      ShowLastMessage;
+   exception
+      when An_Exception : Missions_Accepting_Error =>
+         ShowDialog
+           (Exception_Message(An_Exception),
+            Gtk_Window(Get_Object(Object, "missionswindow")));
+   end AcceptSelectedMission;
 
    procedure CreateMissionsUI is
       Error: aliased GError;
@@ -52,12 +291,49 @@ package body Missions.UI is
          return;
       end if;
       Register_Handler(Builder, "Hide_Missions", HideMissions'Access);
+      Register_Handler(Builder, "Hide_Last_Message", HideLastMessage'Access);
+      Register_Handler(Builder, "Show_Mission_Info", ShowMissionInfo'Access);
+      Register_Handler
+        (Builder,
+         "Accept_Mission",
+         AcceptSelectedMission'Access);
       Do_Connect(Builder);
    end CreateMissionsUI;
 
    procedure ShowMissionsUI is
+      BaseIndex: constant Positive :=
+        SkyMap(PlayerShip.SkyX, PlayerShip.SkyY).BaseIndex;
+      MissionsIter: Gtk_Tree_Iter;
+      MissionsList: Gtk_List_Store;
    begin
+      MissionsList := Gtk_List_Store(Get_Object(Builder, "missionslist"));
+      Clear(MissionsList);
+      for Mission of SkyBases(BaseIndex).Missions loop
+         Append(MissionsList, MissionsIter);
+         case Mission.MType is
+            when Deliver =>
+               Set(MissionsList, MissionsIter, 0, "Deliver item to base");
+            when Patrol =>
+               Set(MissionsList, MissionsIter, 0, "Patrol area");
+            when Destroy =>
+               Set(MissionsList, MissionsIter, 0, "Destroy ship");
+            when Explore =>
+               Set(MissionsList, MissionsIter, 0, "Explore area");
+            when Passenger =>
+               Set
+                 (MissionsList,
+                  MissionsIter,
+                  0,
+                  "Transport passenger to base");
+         end case;
+      end loop;
       Show_All(Gtk_Widget(Get_Object(Builder, "missionswindow")));
+      Set_Cursor
+        (Gtk_Tree_View(Get_Object(Builder, "treemissions")),
+         Gtk_Tree_Path_New_From_String("0"),
+         Gtk_Tree_View_Column(Get_Object(Builder, "columnmission")),
+         False);
+      ShowLastMessage;
    end ShowMissionsUI;
 
 end Missions.UI;
