d80aa75c67491e634ff8c5fd867cfff832a79d20
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2018-02-21 10:13:35 +0000

    finished work on crafting ui
    FossilOrigin-Name: 9ceece9fe89f31bf5c84837bef95493eb4ef923242321d1b696c4204627642cb

--- src/ui/crafts-ui.adb
+++ src/ui/crafts-ui.adb
@@ -16,6 +16,7 @@
 --    along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
 with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Exceptions; use Ada.Exceptions;
 with GNAT.Directory_Operations; use GNAT.Directory_Operations;
 with Gtkada.Builder; use Gtkada.Builder;
 with Gtk.Widget; use Gtk.Widget;
@@ -24,6 +25,10 @@ with Gtk.Tree_Model; use Gtk.Tree_Model;
 with Gtk.List_Store; use Gtk.List_Store;
 with Gtk.Tree_View; use Gtk.Tree_View;
 with Gtk.Tree_View_Column; use Gtk.Tree_View_Column;
+with Gtk.Tree_Selection; use Gtk.Tree_Selection;
+with Gtk.Adjustment; use Gtk.Adjustment;
+with Gtk.Window; use Gtk.Window;
+with Gtk.Combo_Box; use Gtk.Combo_Box;
 with Glib; use Glib;
 with Glib.Error; use Glib.Error;
 with Gdk.RGBA; use Gdk.RGBA;
@@ -33,10 +38,13 @@ with Messages; use Messages;
 with Help.UI; use Help.UI;
 with Ships; use Ships;
 with Items; use Items;
+with Utils.UI; use Utils.UI;
+with Trades; use Trades;
 
 package body Crafts.UI is
 
    Builder: Gtkada_Builder;
+   RecipeIndex: Integer;
 
    function HideCrafts
      (Object: access Gtkada_Builder_Record'Class) return Boolean is
@@ -71,6 +79,238 @@ package body Crafts.UI is
       ShowHelpUI(5);
    end ShowHelp;
 
+   procedure ShowRecipeInfo(Object: access Gtkada_Builder_Record'Class) is
+      RecipesIter: Gtk_Tree_Iter;
+      RecipesModel: Gtk_Tree_Model;
+      RecipeInfo: Unbounded_String := Null_Unbounded_String;
+      Recipe: Craft_Data;
+      MAmount, CargoIndex: Natural := 0;
+      HaveWorkplace, IsMaterial, IsTool: Boolean := False;
+      WorkplaceName: Unbounded_String := Null_Unbounded_String;
+      TextLength: Positive;
+   begin
+      Get_Selected
+        (Gtk.Tree_View.Get_Selection
+           (Gtk_Tree_View(Get_Object(Object, "treerecipes"))),
+         RecipesModel,
+         RecipesIter);
+      if RecipesIter = Null_Iter then
+         return;
+      end if;
+      RecipeIndex := Integer(Get_Int(RecipesModel, RecipesIter, 1));
+      if RecipeIndex > 0 then
+         Recipe := Recipes_List(RecipeIndex);
+      else
+         Recipe.MaterialTypes.Append
+         (New_Item => Items_List(abs (RecipeIndex)).IType);
+         Recipe.MaterialAmounts.Append(New_Item => 1);
+         Recipe.ResultIndex := abs (RecipeIndex);
+         Recipe.ResultAmount := 0;
+         Recipe.Workplace := ALCHEMY_LAB;
+         for ProtoRecipe of Recipes_List loop
+            if ProtoRecipe.ResultIndex = Recipe.ResultIndex then
+               Recipe.Skill := ProtoRecipe.Skill;
+               Recipe.Time := ProtoRecipe.Difficulty * 15;
+               exit;
+            end if;
+         end loop;
+         Recipe.Difficulty := 1;
+         Recipe.BaseType := 0;
+         Recipe.Tool := AlchemyTools;
+      end if;
+      if RecipeIndex > 0 then
+         Append
+           (RecipeInfo,
+            "Amount:" & Integer'Image(Recipe.ResultAmount) & ASCII.LF);
+      end if;
+      Append(RecipeInfo, "Materials needed: ");
+      for I in
+        Recipe.MaterialTypes.First_Index ..
+            Recipe.MaterialTypes.Last_Index loop
+         Append(RecipeInfo, ASCII.LF & "-");
+         MAmount := 0;
+         for J in Items_List.Iterate loop
+            IsMaterial := False;
+            if RecipeIndex > 0 then
+               if Items_List(J).IType = Recipe.MaterialTypes(I) then
+                  IsMaterial := True;
+               end if;
+            else
+               if Items_List(J).Name = Items_List(Recipe.ResultIndex).Name then
+                  IsMaterial := True;
+               end if;
+            end if;
+            if IsMaterial then
+               if MAmount > 0 then
+                  Append(RecipeInfo, " or");
+               end if;
+               CargoIndex :=
+                 FindItem(PlayerShip.Cargo, Objects_Container.To_Index(J));
+               if CargoIndex = 0 then
+                  Append(RecipeInfo, "<span foreground=""red"">");
+               end if;
+               Append
+                 (RecipeInfo,
+                  Integer'Image(Recipe.MaterialAmounts(I)) &
+                  "x" &
+                  To_String(Items_List(J).Name));
+               if CargoIndex > 0 then
+                  TextLength :=
+                    Positive'Image(PlayerShip.Cargo(CargoIndex).Amount)'Length;
+                  Append
+                    (RecipeInfo,
+                     "(" &
+                     Positive'Image(PlayerShip.Cargo(CargoIndex).Amount)
+                       (2 .. TextLength) &
+                     ")");
+               else
+                  Append(RecipeInfo, "</span>");
+               end if;
+               MAmount := MAmount + 1;
+            end if;
+         end loop;
+      end loop;
+      if Recipe.Tool /= To_Unbounded_String("None") then
+         Append(RecipeInfo, ASCII.LF & "Tool: ");
+         MAmount := 0;
+         for I in Items_List.Iterate loop
+            IsTool := False;
+            if Items_List(I).IType = Recipe.Tool then
+               IsTool := True;
+            end if;
+            if IsTool then
+               if MAmount > 0 then
+                  Append(RecipeInfo, " or ");
+               end if;
+               CargoIndex :=
+                 FindItem(PlayerShip.Cargo, Objects_Container.To_Index(I));
+               if CargoIndex = 0 then
+                  Append(RecipeInfo, "<span foreground=""red"">");
+               end if;
+               Append(RecipeInfo, To_String(Items_List(I).Name));
+               if CargoIndex = 0 then
+                  Append(RecipeInfo, "</span>");
+               end if;
+               MAmount := MAmount + 1;
+            end if;
+         end loop;
+      end if;
+      Append(RecipeInfo, ASCII.LF & "Workplace: ");
+      for Module of PlayerShip.Modules loop
+         if Modules_List(Module.ProtoIndex).MType = Recipe.Workplace then
+            WorkplaceName := Module.Name;
+            if Module.Durability > 0 then
+               HaveWorkplace := True;
+               exit;
+            end if;
+         end if;
+      end loop;
+      if WorkplaceName = Null_Unbounded_String then
+         for Module of Modules_List loop
+            if Module.MType = Recipe.Workplace then
+               WorkplaceName := Module.Name;
+               exit;
+            end if;
+         end loop;
+      end if;
+      if not HaveWorkplace then
+         Append(RecipeInfo, "<span foreground=""red"">");
+      end if;
+      Append(RecipeInfo, WorkplaceName);
+      if not HaveWorkplace then
+         Append(RecipeInfo, "</span>");
+      end if;
+      Append
+        (RecipeInfo,
+         ASCII.LF &
+         "Skill: " &
+         To_String(Skills_List(Recipe.Skill).Name) &
+         "/" &
+         To_String(Attributes_Names(Skills_List(Recipe.Skill).Attribute)));
+      Append
+        (RecipeInfo,
+         ASCII.LF & "Time needed:" & Positive'Image(Recipe.Time) & " minutes");
+      Set_Markup
+        (Gtk_Label(Get_Object(Object, "lblinfo")),
+         To_String(RecipeInfo));
+   end ShowRecipeInfo;
+
+   procedure ShowSetRecipe(Object: access Gtkada_Builder_Record'Class) is
+      ModulesIter: Gtk_Tree_Iter;
+      ModulesList: Gtk_List_Store;
+      MaxAmount: Positive;
+      AmountAdj: constant Gtk_Adjustment :=
+        Gtk_Adjustment(Get_Object(Builder, "amountadj"));
+      MType: ModuleType;
+   begin
+      MaxAmount := CheckRecipe(RecipeIndex);
+      Set_Value(AmountAdj, 1.0);
+      Set_Upper(AmountAdj, Gdouble(MaxAmount));
+      Set_Label
+        (Gtk_Label(Get_Object(Object, "lbltimes")),
+         "Times (max" & Positive'Image(MaxAmount) & "):");
+      if RecipeIndex > 0 then
+         MType := Recipes_List(RecipeIndex).Workplace;
+      else
+         MType := ALCHEMY_LAB;
+      end if;
+      ModulesList := Gtk_List_Store(Get_Object(Object, "moduleslist"));
+      Clear(ModulesList);
+      for Module of PlayerShip.Modules loop
+         if Modules_List(Module.ProtoIndex).MType = MType then
+            Append(ModulesList, ModulesIter);
+            Set(ModulesList, ModulesIter, 0, To_String(Module.Name));
+         end if;
+      end loop;
+      Show_All(Gtk_Widget(Get_Object(Object, "recipewindow")));
+      if RecipeIndex < 1 then
+         Hide(Gtk_Widget(Get_Object(Object, "boxamount")));
+      end if;
+      Set_Active(Gtk_Combo_Box(Get_Object(Object, "cmbmodules")), 0);
+   exception
+      when An_Exception : Crafting_No_Materials =>
+         ShowDialog
+           ("You don't have enough materials to start manufacturing " &
+            Exception_Message(An_Exception) &
+            ".",
+            Gtk_Window(Get_Object(Object, "craftswindow")));
+      when An_Exception : Crafting_No_Tools =>
+         ShowDialog
+           ("You don't have proper tool to start manufacturing " &
+            Exception_Message(An_Exception) &
+            ".",
+            Gtk_Window(Get_Object(Object, "craftswindow")));
+      when Trade_No_Free_Cargo =>
+         ShowDialog
+           ("You don't have that much free space in your ship cargo.",
+            Gtk_Window(Get_Object(Object, "craftswindow")));
+      when An_Exception : Crafting_No_Workshop =>
+         ShowDialog
+           ("You don't have proper workplace to start manufacturing " &
+            Exception_Message(An_Exception) &
+            ".",
+            Gtk_Window(Get_Object(Object, "craftswindow")));
+   end ShowSetRecipe;
+
+   procedure SetCrafting(Object: access Gtkada_Builder_Record'Class) is
+      ModulesBox: constant Gtk_Combo_Box :=
+        Gtk_Combo_Box(Get_Object(Object, "cmbmodules"));
+      WorkshopName: constant Unbounded_String :=
+        To_Unbounded_String
+          (Get_String(Get_Model(ModulesBox), Get_Active_Iter(ModulesBox), 0));
+      Amount: constant Natural :=
+        Natural(Get_Value(Gtk_Adjustment(Get_Object(Builder, "amountadj"))));
+   begin
+      for I in PlayerShip.Modules.Iterate loop
+         if PlayerShip.Modules(I).Name = WorkshopName then
+            SetRecipe(Modules_Container.To_Index(I), Amount, RecipeIndex);
+            exit;
+         end if;
+      end loop;
+      Hide(Gtk_Widget(Get_Object(Object, "recipewindow")));
+      ShowLastMessage;
+   end SetCrafting;
+
    procedure CreateCraftsUI is
       Error: aliased GError;
    begin
@@ -97,6 +337,10 @@ package body Crafts.UI is
       Register_Handler(Builder, "Hide_Crafts", HideCrafts'Access);
       Register_Handler(Builder, "Hide_Last_Message", HideLastMessage'Access);
       Register_Handler(Builder, "Show_Help", ShowHelp'Access);
+      Register_Handler(Builder, "Show_Recipe_Info", ShowRecipeInfo'Access);
+      Register_Handler(Builder, "Show_Set_Recipe", ShowSetRecipe'Access);
+      Register_Handler(Builder, "Hide_Window", HideWindow'Access);
+      Register_Handler(Builder, "Set_Crafting", SetCrafting'Access);
       Do_Connect(Builder);
    end CreateCraftsUI;
 
