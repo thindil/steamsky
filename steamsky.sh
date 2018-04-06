b256418b6bdf2d5ce1db6805f1bae82d361eb193
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2018-04-06 05:02:23 +0000

    small code update
    FossilOrigin-Name: 7fc56cd79fe8f09cc7d9c1f577233537ba83b1a604ee3981246efb353c1ecba7

--- steamsky.sh
+++ steamsky.sh
@@ -12,13 +12,9 @@ if [ -d "$prefix/lib" ]; then
    GDK_PIXBUF_MODULE_FILE=\"$prefix/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache\";
    GDK_PIXBUF_MODULEDIR=\"$prefix/lib/gdk-pixbuf-2.0/2.10.0/loaders/\";
    FONTCONFIG_FILE=\"$prefix/etc/fonts/fonts.conf\";
-   XDG_DATA_DIRS=\"$prefix/share\";
-   XDG_CONFIG_DIRS=\"$prefix/etc\";
    GSETTINGS_BACKEND=memory;
-   GLADE_BASE_DIR=\"$prefix\";
    export LD_LIBRARY_PATH PKG_CONFIG_PATH GDK_PIXBUF_MODULEDIR;
-   export GDK_PIXBUF_MODULE_FILE FONTCONFIG_FILE XDG_DATA_DIRS;
-   export GLADE_BASE_DIR GSETTINGS_BACKEND"
+   export GDK_PIXBUF_MODULE_FILE FONTCONFIG_FILE GSETTINGS_BACKEND"
 else
    env=`gtkada-env.sh --print-only`
 fi
