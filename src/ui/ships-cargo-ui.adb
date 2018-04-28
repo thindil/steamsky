ac378b6be29274ee73905ca4d414899d84e42d32
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2018-04-28 05:03:57 +0000

    added showing info about damage type of weapon
    FossilOrigin-Name: d78166688a6df47c0ad20b790fbd574fa9b4c94fee95aefc19383babc6c546fe

--- src/ui/ships-cargo-ui.adb
+++ src/ui/ships-cargo-ui.adb
@@ -125,6 +125,17 @@ package body Ships.Cargo.UI is
               (ItemInfo,
                ASCII.LF & "Can't be used with shield (two-handed weapon).");
          end if;
+         Append(ItemInfo, ASCII.LF & "Damage type: ");
+         case Items_List(ProtoIndex).Value(5) is
+            when 1 =>
+               Append(ItemInfo, "cutting");
+            when 2 =>
+               Append(ItemInfo, "impaling");
+            when 3 =>
+               Append(ItemInfo, "blunt");
+            when others =>
+               null;
+         end case;
       end if;
       Set_Markup
         (Gtk_Label(Get_Object(Object, "lbliteminfo2")),
