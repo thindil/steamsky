c5106f7a5013cd67af3d4e6948e3e87c483f390d
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2018-07-07 05:31:17 +0000

    continue work on stories ui
    FossilOrigin-Name: ffcd45fd5a9ec721a95c5c620cf730ce7e345bdea48c157a2d271bb3c3b9919a

--- src/ui/stories-ui.adb
+++ src/ui/stories-ui.adb
@@ -15,37 +15,92 @@
 --    You should have received a copy of the GNU General Public License
 --    along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
+with GNAT.String_Split; use GNAT.String_Split;
 with Gtk.Window; use Gtk.Window;
 with Gtk.Stack; use Gtk.Stack;
 with Gtk.Widget; use Gtk.Widget;
 with Gtk.Tree_Model; use Gtk.Tree_Model;
 with Gtk.List_Store; use Gtk.List_Store;
 with Gtk.Combo_Box; use Gtk.Combo_Box;
+with Gtk.Text_Buffer; use Gtk.Text_Buffer;
+with Gtk.Text_Iter; use Gtk.Text_Iter;
+with Glib; use Glib;
 with Utils.UI; use Utils.UI;
 with Messages; use Messages;
 with Maps.UI; use Maps.UI;
+with Ships; use Ships;
+with Bases; use Bases;
 
 package body Stories.UI is
 
    Builder: Gtkada_Builder;
+   Setting: Boolean;
+
+   procedure GetStoryLocation(StoryX, StoryY: in out Positive) is
+      Tokens: Slice_Set;
+   begin
+      if CurrentStory.Data /= Null_Unbounded_String then
+         Create(Tokens, To_String(CurrentStory.Data), ";");
+         if Slice_Count(Tokens) < 3 then
+            for I in SkyBases'Range loop
+               if SkyBases(I).Name = CurrentStory.Data then
+                  StoryX := SkyBases(I).SkyX;
+                  StoryY := SkyBases(I).SkyY;
+                  exit;
+               end if;
+            end loop;
+         else
+            StoryX := Integer'Value(Slice(Tokens, 1));
+            StoryY := Integer'Value(Slice(Tokens, 2));
+         end if;
+      else
+         StoryX := PlayerShip.SkyX;
+         StoryY := PlayerShip.SkyY;
+      end if;
+   end GetStoryLocation;
 
    procedure ShowStoryInfo(Object: access Gtkada_Builder_Record'Class) is
+      StoryIndex: Positive;
+      StoryBuffer: constant Gtk_Text_Buffer :=
+        Gtk_Text_Buffer(Get_Object(Object, "txtstory"));
+      Iter: Gtk_Text_Iter;
    begin
-      null;
+      if Setting then
+         return;
+      end if;
+      StoryIndex :=
+        Natural(Get_Active(Gtk_Combo_Box(Get_Object(Object, "cmbstories")))) +
+        1;
+      Set_Text(StoryBuffer, "");
+      Get_Start_Iter(StoryBuffer, Iter);
+      Insert(StoryBuffer, Iter, "" & ASCII.LF);
+      for StepText of FinishedStories(StoryIndex).StepsTexts loop
+         Insert(StoryBuffer, Iter, To_String(StepText) & ASCII.LF & ASCII.LF);
+      end loop;
+      if Natural(FinishedStories(StoryIndex).StepsTexts.Length) <
+        FinishedStories(StoryIndex).StepsAmount then
+         Insert(StoryBuffer, Iter, To_String(GetCurrentStoryText) & ASCII.LF);
+         Show_All(Gtk_Widget(Get_Object(Builder, "btnstorycenter")));
+         Show_All(Gtk_Widget(Get_Object(Builder, "btnstorydestination")));
+      else
+         Hide(Gtk_Widget(Get_Object(Builder, "btnstorycenter")));
+         Hide(Gtk_Widget(Get_Object(Builder, "btnstorydestination")));
+      end if;
    end ShowStoryInfo;
 
    procedure SetStoryAsDestination
      (Object: access Gtkada_Builder_Record'Class) is
+      NewX, NewY: Positive := 1;
    begin
-  --    if Events_List(EventIndex).SkyX = PlayerShip.SkyX and
-  --      Events_List(EventIndex).SkyY = PlayerShip.SkyY then
-  --       ShowDialog
-  --         ("You are at this event now.",
-  --          Gtk_Window(Get_Object(Object, "eventswindow")));
-  --       return;
-  --    end if;
-  --    PlayerShip.DestinationX := Events_List(EventIndex).SkyX;
-  --    PlayerShip.DestinationY := Events_List(EventIndex).SkyY;
+      GetStoryLocation(NewX, NewY);
+      if NewX = PlayerShip.SkyX and NewY = PlayerShip.SkyY then
+         ShowDialog
+           ("You are at this story location now.",
+            Gtk_Window(Get_Object(Object, "skymapwindow")));
+         return;
+      end if;
+      PlayerShip.DestinationX := NewX;
+      PlayerShip.DestinationY := NewY;
       AddMessage
         ("You set the travel destination for your ship.",
          OrderMessage);
@@ -57,8 +112,10 @@ package body Stories.UI is
    end SetStoryAsDestination;
 
    procedure ShowStory(Object: access Gtkada_Builder_Record'Class) is
+      NewX, NewY: Positive := 1;
    begin
-      --ShowSkyMap(Events_List(EventIndex).SkyX, Events_List(EventIndex).SkyY);
+      GetStoryLocation(NewX, NewY);
+      ShowSkyMap(NewX, NewY);
       Set_Visible_Child_Name
         (Gtk_Stack(Get_Object(Object, "gamestack")),
          "skymap");
@@ -83,16 +140,26 @@ package body Stories.UI is
       if FinishedStories.Length = 0 then
          Show_All(Gtk_Widget(Get_Object(Builder, "btnmenu")));
          Hide(Gtk_Widget(Get_Object(Builder, "btnclose")));
-         ShowDialog("You didn't discovered any story yet.", Gtk_Window(Get_Object(Builder, "skymapwindow")));
+         ShowDialog
+           ("You didn't discovered any story yet.",
+            Gtk_Window(Get_Object(Builder, "skymapwindow")));
          return;
       end if;
+      Setting := True;
       StoriesList := Gtk_List_Store(Get_Object(Builder, "storieslist"));
       Clear(StoriesList);
       for FinishedStory of FinishedStories loop
          Append(StoriesList, StoriesIter);
-         Set(StoriesList, StoriesIter, 0, To_String(Stories_List(FinishedStory.Index).Name));
+         Set
+           (StoriesList,
+            StoriesIter,
+            0,
+            To_String(Stories_List(FinishedStory.Index).Name));
       end loop;
-      Set_Active(Gtk_Combo_Box(Get_Object(Builder, "cmbstories")), 0);
+      Setting := False;
+      Set_Active
+        (Gtk_Combo_Box(Get_Object(Builder, "cmbstories")),
+         Gint(FinishedStories.Length - 1));
       Set_Visible_Child_Name
         (Gtk_Stack(Get_Object(Builder, "gamestack")),
          "stories");
