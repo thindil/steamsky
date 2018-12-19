c60f7ef158799c1c7d4396ca788466771ae450b4
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2018-12-19 05:32:47 +0000

    small update to item status info
    FossilOrigin-Name: 9ef0b41d062666f1a099ccc6fbee605e7bd1c79add858d7281782608f5d2b82a

--- src/ui/ships-cargo-ui.adb
+++ src/ui/ships-cargo-ui.adb
@@ -72,16 +72,16 @@ package body Ships.Cargo.UI is
             when 100 =>
                Set(CargoList, CargoIter, 7, False);
             when 81 .. 99 =>
-               Set(CargoList, CargoIter, 5, "Slightly used");
+               Set(CargoList, CargoIter, 5, " Slightly used ");
                Visible := True;
             when 51 .. 80 =>
-               Set(CargoList, CargoIter, 5, "Damaged");
+               Set(CargoList, CargoIter, 5, " Damaged ");
                Visible := True;
             when 21 .. 50 =>
-               Set(CargoList, CargoIter, 5, "Heavily damaged");
+               Set(CargoList, CargoIter, 5, " Heavily damaged ");
                Visible := True;
             when others =>
-               Set(CargoList, CargoIter, 5, "Almost destroyed");
+               Set(CargoList, CargoIter, 5, " Almost destroyed ");
                Visible := True;
          end case;
          Set(CargoList, CargoIter, 6, Gint(PlayerShip.Cargo(I).Durability));
