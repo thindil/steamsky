e303982f44618a3b60ebe9c1a8d752dd5ec21b3d
thindil@laeran.pl <thindil@laeran.pl>
2019-01-01 05:39:22 +0000

    added some precondition checks
    FossilOrigin-Name: d6c9033fe5448259690aab0b55dc1f2f56c7a61f9531838afe01e05b21a990e7

--- src/crew-inventory.adb
+++ src/crew-inventory.adb
@@ -1,4 +1,4 @@
---    Copyright 2017-2018 Bartek thindil Jasicki
+--    Copyright 2017-2019 Bartek thindil Jasicki
 --
 --    This file is part of Steam Sky.
 --
@@ -15,7 +15,6 @@
 --    You should have received a copy of the GNU General Public License
 --    along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
-with Ships; use Ships;
 with Messages; use Messages;
 with Ships.Cargo; use Ships.Cargo;
 with Ships.Crew; use Ships.Crew;
--- src/crew-inventory.ads
+++ src/crew-inventory.ads
@@ -1,4 +1,4 @@
---    Copyright 2017 Bartek thindil Jasicki
+--    Copyright 2017-2019 Bartek thindil Jasicki
 --
 --    This file is part of Steam Sky.
 --
@@ -15,22 +15,40 @@
 --    You should have received a copy of the GNU General Public License
 --    along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
+with Ships; use Ships;
+
 package Crew.Inventory is
 
    procedure UpdateInventory(MemberIndex: Positive; Amount: Integer;
-      ProtoIndex, Durability, InventoryIndex, Price: Natural :=
-        0); -- Update member inventory
+      ProtoIndex, Durability, InventoryIndex, Price: Natural := 0) with
+      Pre =>
+      (MemberIndex <= PlayerShip.Crew.Last_Index and
+       ProtoIndex <= Items_List.Last_Index and
+       InventoryIndex <=
+         PlayerShip.Crew(MemberIndex).Inventory
+           .Last_Index); -- Update member inventory
    function FreeInventory(MemberIndex: Positive;
-      Amount: Integer)
-     return Integer; -- Return available space in crew member inventory after adding/extracting Amount
-   procedure TakeOffItem
-     (MemberIndex,
-      ItemIndex: Positive); -- Remove selected item from character equipment
-   function ItemIsUsed
-     (MemberIndex, ItemIndex: Positive)
-     return Boolean; -- Check if selected crew member use this item
+      Amount: Integer) return Integer with
+      Pre => MemberIndex <=
+      PlayerShip.Crew
+        .Last_Index; -- Return available space in crew member inventory after adding/extracting Amount
+   procedure TakeOffItem(MemberIndex, ItemIndex: Positive) with
+      Pre =>
+      (MemberIndex <= PlayerShip.Crew.Last_Index and
+       ItemIndex <=
+         PlayerShip.Crew(MemberIndex).Inventory
+           .Last_Index); -- Remove selected item from character equipment
+   function ItemIsUsed(MemberIndex, ItemIndex: Positive) return Boolean with
+      Pre =>
+      (MemberIndex <= PlayerShip.Crew.Last_Index and
+       ItemIndex <=
+         PlayerShip.Crew(MemberIndex).Inventory
+           .Last_Index); -- Check if selected crew member use this item
    function FindTools(MemberIndex: Positive; ItemType: Unbounded_String;
-      Order: Crew_Orders)
-     return Natural; -- Search for specified tools in character and ship cargo, return 0 if tools not found otherwise index of tool in character inventory
+      Order: Crew_Orders) return Natural with
+      Pre =>
+      (MemberIndex <= PlayerShip.Crew.Last_Index and
+       ItemType /=
+         Null_Unbounded_String); -- Search for specified tools in character and ship cargo, return 0 if tools not found otherwise index of tool in character inventory
 
 end Crew.Inventory;
