254562a28e656bfa14379997230d5dcc21a063cb
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2019-04-23 04:38:48 +0000

    fixed crash on trying to writting to the closed debug log
    FossilOrigin-Name: 70e475353b8e163e1bbc6505a03dbb41a112e4403708512960037a2ffeb23fec

--- src/log.ads
+++ src/log.ads
@@ -15,15 +15,12 @@
 --    You should have received a copy of the GNU General Public License
 --    along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
-with Ada.Text_IO; use Ada.Text_IO;
-
 package Log is
 
    type Debug_Types is
      (None, Everything, Combat,
       Menu); -- Types of debug mode, which messages log to file
    DebugMode: Debug_Types := None; -- Did game is run in debug mode
-   LogFile: File_Type; -- Debug log file
 
    procedure StartLogging; -- Open/create debug.log file
    procedure LogMessage(Message: String; MessageType: Debug_Types;
