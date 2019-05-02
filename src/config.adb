1c0b93e7825b5bc0f0681927bfdc88ba3745e436
thindil@laeran.pl <thindil@laeran.pl>
2019-05-02 04:41:52 +0000

    added ui to set level of prices in bases
    FossilOrigin-Name: efac4336dea0790c9c6f6faa0b459046ba7e466f746ab43c289dd33752097092

--- src/config.adb
+++ src/config.adb
@@ -41,7 +41,7 @@ package body Config is
          StartingBase => To_Unbounded_String("ANY"), EnemyDamageBonus => 1.0,
          PlayerDamageBonus => 1.0, EnemyMeleeDamageBonus => 1.0,
          PlayerMeleeDamageBonus => 1.0, ExperienceBonus => 1.0,
-         ReputationBonus => 1.0, UpgradeCostBonus => 1.0);
+         ReputationBonus => 1.0, UpgradeCostBonus => 1.0, PricesBonus => 1.0);
       GameSettings :=
         (AutoRest => True, UndockSpeed => FULL_SPEED, AutoCenter => True,
          AutoReturn => True, AutoFinish => True, LowFuel => 100,
@@ -98,6 +98,8 @@ package body Config is
             elsif FieldName = To_Unbounded_String("UpgradeCostBonus") then
                NewGameSettings.UpgradeCostBonus :=
                  Float'Value(To_String(Value));
+            elsif FieldName = To_Unbounded_String("PricesBonus") then
+               NewGameSettings.PricesBonus := Float'Value(To_String(Value));
             elsif FieldName = To_Unbounded_String("AutoRest") then
                GameSettings.AutoRest := LoadBoolean;
             elsif FieldName = To_Unbounded_String("UndockSpeed") then
@@ -217,6 +219,9 @@ package body Config is
       Put_Line
         (ConfigFile,
          "UpgradeCostBonus =" & Float'Image(NewGameSettings.UpgradeCostBonus));
+      Put_Line
+        (ConfigFile,
+         "PricesBonus =" & Float'Image(NewGameSettings.PricesBonus));
       SaveBoolean(GameSettings.AutoRest, "AutoRest");
       Put_Line
         (ConfigFile,
