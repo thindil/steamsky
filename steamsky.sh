317304c6857255e3011a16d08ae65effe19e6727
thindil@laeran.pl <thindil@laeran.pl>
2019-05-08 04:36:27 +0000

    fixed starting script for Linux
    FossilOrigin-Name: 0e42bb092b7873f8c4a71569680668d76ab8de0ad753dbed83170a20c81df100

--- steamsky.sh
+++ steamsky.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-prefix=`dirname $0`
-cd "$prefix"
-prefix=`pwd`
+prefix=$(dirname "$0")
+cd "$prefix" || exit
+prefix=$(pwd)
 
 if [ -d lib ]; then
+   # distributed
    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$prefix/lib
    export GDK_PIXBUF_MODULE_FILE=$prefix/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache
    export GDK_PIXBUF_MODULEDIR=$prefix/lib/gdk-pixbuf-2.0/2.10.0/loaders/
@@ -12,10 +13,12 @@ if [ -d lib ]; then
    export XDG_DATA_DIRS=$XDG_DATA_DIRS:$prefix/share
    export GSETTINGS_BACKEND=memory
 else
-   eval `gtkada-env.sh --print-only`
+   # built-locally
+   eval "$(gtkada-env.sh --print-only)"
 fi
 
-cd bin
+cd bin || exit
 export GTK_THEME=Adwaita
 export RUNFROMSCRIPT=1
+
 ./steamsky "$@"
