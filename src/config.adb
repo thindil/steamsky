c627e5bb78b12f98be91ad3243e007771358c159
thindil@laeran.pl <thindil@laeran.pl>
2019-07-16 04:36:47 +0000

    fixed fonts size after first run of the game
    FossilOrigin-Name: e2257bca47f06885892ec7535cd2eed0a3ac91db05fcd3b725bbeaa2e7f76873

--- src/config.adb
+++ src/config.adb
@@ -48,7 +48,7 @@ package body Config is
          LowDrinks => 50, LowFood => 25, AutoMoveStop => NEVER,
          WindowWidth => 800, WindowHeight => 600, AnimationsEnabled => 1,
          AnimationType => 1, MessagesLimit => 500, SavedMessages => 10,
-         HelpFontSize => 12, MapFontSize => 12, InterfaceFontSize => 16,
+         HelpFontSize => 14, MapFontSize => 16, InterfaceFontSize => 14,
          InterfaceTheme => To_Unbounded_String("default"),
          MessagesOrder => OLDER_FIRST, AutoAskForBases => False,
          AutoAskForEvents => False, ShowTooltips => True,
