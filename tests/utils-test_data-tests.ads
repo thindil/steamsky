4fb08aaa591829556752d83035c87cdbdf910964
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2019-08-10 04:33:30 +0000

    added unit test
    FossilOrigin-Name: e9e3aa8b23b226cf083edf75a953f5adddaf7b2be49a05d9a9469ab67ac48b0b

---
+++ tests/utils-test_data-tests.ads
@@ -0,0 +1,16 @@
+--  This package has been generated automatically by GNATtest.
+--  Do not edit any part of it, see GNATtest documentation for more details.
+
+--  begin read only
+with Gnattest_Generated;
+
+package Utils.Test_Data.Tests is
+
+   type Test is new GNATtest_Generated.GNATtest_Standard.Utils.Test_Data.Test
+   with null record;
+
+   procedure Test_GetRandom_9cea97_28ba02 (Gnattest_T : in out Test);
+   --  utils.ads:37:4:GetRandom:Test_GetRandom
+
+end Utils.Test_Data.Tests;
+--  end read only
