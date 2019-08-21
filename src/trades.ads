ed58c7447c3bc43f994a547a83db6ff3f33b7d32
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2019-08-21 04:34:35 +0000

    added some formal verificaiton
    FossilOrigin-Name: 2eee9f6341153eb71e396fc0c0a9f9d7002b8adf8bfe6d331ea7d6e68c87a8ef

--- src/trades.ads
+++ src/trades.ads
@@ -32,60 +32,70 @@ package Trades is
    -- SOURCE
    TraderCargo: BaseCargo_Container.Vector;
    -- ****
+
    -- ****e* Trades/Trade_Cant_Buy
    -- FUNCTION
    -- Raised when items is not available to buy
    -- SOURCE
    Trade_Cant_Buy: exception;
    -- ****
+
    -- ****e* Trades/Trade_Not_For_Sale_Now
    -- FUNCTION
    -- Raised when no items available at this time for sale
    -- SOURCE
    Trade_Not_For_Sale_Now: exception;
    -- ****
+
    -- ****e* Trades/Trade_Buying_Too_Much
    -- FUNCTION
    -- Raised when player trying buy more than can
    -- SOURCE
    Trade_Buying_Too_Much: exception;
    -- ****
+
    -- ****e* Trades/Trade_No_Free_Cargo
    -- FUNCTION
    -- Raised when no enough free cargo in ship
    -- SOURCE
    Trade_No_Free_Cargo: exception;
    -- ****
+
    -- ****e* Trades/Trade_No_Money
    -- FUNCTION
    -- Raised when player don't have money
    -- SOURCE
    Trade_No_Money: exception;
    -- ****
+
    -- ****e* Trades/Trade_Not_Enough_Money
    -- FUNCTION
    -- Raised when player don't have enough money
    -- SOURCE
    Trade_Not_Enough_Money: exception;
    -- ****
+
    -- ****e* Trades/Trade_Invalid_Amount
    -- FUNCTION
    -- Raised when player enter invalid amount
    -- SOURCE
    Trade_Invalid_Amount: exception;
    -- ****
+
    -- ****e* Trades/Trade_Too_Much_For_Sale
    -- FUNCTION
    -- Raised when player try sell more than have
    -- SOURCE
    Trade_Too_Much_For_Sale: exception;
    -- ****
+
    -- ****e* Trades/Trade_No_Money_In_Base
    -- FUNCTION
    -- Raised when base don't have enough money for buy item
    -- SOURCE
    Trade_No_Money_In_Base: exception;
    -- ****
+
    -- ****e* Trades/Trade_No_Trader
    -- FUNCTION
    -- Raised when no one is assigned to talk in bases duty
@@ -102,6 +112,7 @@ package Trades is
    -- SOURCE
    procedure BuyItems(BaseItemIndex: Positive; Amount: String);
    -- ****
+
    -- ****f* Trades/SellItems
    -- FUNCTION
    -- Sell items from bases or trader
@@ -109,15 +120,17 @@ package Trades is
    -- ItemIndex - Player ship cargo index of item to sell
    -- Amount    - Amount of items to sell
    -- SOURCE
-   procedure SellItems(ItemIndex: Positive; Amount: String);
-   -- ****
-   -- ****f* Trades/GenerateTraderCargo
-   -- FUNCTION
-   -- Generate list of cargo to trade
-   -- PARAMETERS
-   -- ProtoIndex - Index of prototype ship which will be used to generate
-   --              cargo
-   -- SOURCE
+   procedure SellItems(ItemIndex: Positive; Amount: String) with
+      Pre => ItemIndex <= PlayerShip.Cargo.Last_Index;
+      -- ****
+
+      -- ****f* Trades/GenerateTraderCargo
+      -- FUNCTION
+      -- Generate list of cargo to trade
+      -- PARAMETERS
+      -- ProtoIndex - Index of prototype ship which will be used to generate
+      --              cargo
+      -- SOURCE
    procedure GenerateTraderCargo(ProtoIndex: Unbounded_String) with
       Pre => ProtoShips_Container.Contains(ProtoShips_List, ProtoIndex);
       -- ****
