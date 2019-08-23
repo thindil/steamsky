b03cc8983896f5256d3addf9a3a36930ab447696
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2019-08-23 04:40:10 +0000

    added new build system
    FossilOrigin-Name: a6b495b5f847b1d720c89787e68f86dc2880b03f690fead374e3e648b6574847

---
+++ build.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env sh
+
+case $1 in
+   release)
+      gprclean -P steamsky.gpr
+      gprbuild -p steamsky.gpr -XMode=release
+      mkdir -p others/Output/release/bin
+      cp -r share others/Output/release
+      cp bin/steamsky others/Output/release/bin
+      cp -r bin/data others/Output/release/share
+      rm -r others/Output/release/share/data/mods
+      rm -r others/Output/release/share/data/saves
+      rm -r others/Output/release/share/data/themes
+      cp -r bin/doc others/Output/release/share
+      cp README.md others/Output/release/share/doc
+      gprclean -P steamsky.gpr
+      ;;
+   debug)
+      gprclean -P steamsky.gpr
+      gprbuild -P steamsky.gpr
+      ;;
+   analyze)
+      gprclean -P steamsky.gpr
+      gprbuild -p steamsky.gpr -XMode=analyze
+      ;;
+   createtests)
+      gnattest -P steamsky.gpr
+      ;;
+   tests)
+      gprbuild -P tests/driver/test_driver.gpr
+      ;;
+   docs)
+      ./generatedocs.py
+      ;;
+   windows)
+      wineconsole build.cmd
+      ;;
+   help)
+      echo "release       - Build the game in release mode"
+      echo "debug         - Build the game in debug mode"
+      echo "analyze       - Build the game in analyze mode for gcov and gprof"
+      echo "createtests   - Regenerate unit tests"
+      echo "tests         - Build unit tests"
+      echo "docs          - Generate code documentation"
+      echo "windows       - Build the game in release mode for Windows"
+      echo "help          - This screen"
+      ;;
+   *)
+      echo "Unknown command, possible options are: release, debug, createtests, tests, docs, windows, help"
+      ;;
+esac
