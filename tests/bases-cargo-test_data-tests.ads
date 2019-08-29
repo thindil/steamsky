4091deec16c916da783c3ddd92898dcc1adb6b78
Bartek thindil Jasicki <thindil@laeran.pl>
2019-08-29 06:29:54 +0200

    added unit test

---
+++ tests/bases-cargo-test_data-tests.ads
@@ -0,0 +1,16 @@
+--  This package has been generated automatically by GNATtest.
+--  Do not edit any part of it, see GNATtest documentation for more details.
+
+--  begin read only
+with Gnattest_Generated;
+
+package Bases.Cargo.Test_Data.Tests is
+
+   type Test is new GNATtest_Generated.GNATtest_Standard.Bases.Cargo.Test_Data.Test
+   with null record;
+
+   procedure Test_GenerateCargo_1e6cf6_021eea (Gnattest_T : in out Test);
+   --  bases-cargo.ads:29:4:GenerateCargo:Test_GenerateCargo
+
+end Bases.Cargo.Test_Data.Tests;
+--  end read only
