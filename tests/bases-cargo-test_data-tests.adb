53bca111fd6d996c16e92b81819610ce159a73e4
Bartek thindil Jasicki <thindil@laeran.pl>
2019-08-30 06:29:06 +0200

    added unit test

--- tests/bases-cargo-test_data-tests.adb
+++ tests/bases-cargo-test_data-tests.adb
@@ -61,6 +61,43 @@ package body Bases.Cargo.Test_Data.Tests is
 --  end read only
 
 --  begin read only
+   procedure Wrap_Test_UpdateBaseCargo_8dbba5_1e1787 (ProtoIndex: Unbounded_String := Null_Unbounded_String; Amount: Integer; Durability: Natural := 100; CargoIndex: Natural := 0) 
+   is
+   begin
+      GNATtest_Generated.GNATtest_Standard.Bases.Cargo.UpdateBaseCargo (ProtoIndex, Amount, Durability, CargoIndex);
+   end Wrap_Test_UpdateBaseCargo_8dbba5_1e1787;
+--  end read only
+
+--  begin read only
+   procedure Test_UpdateBaseCargo_test_updatebasecargo (Gnattest_T : in out Test);
+   procedure Test_UpdateBaseCargo_8dbba5_1e1787 (Gnattest_T : in out Test) renames Test_UpdateBaseCargo_test_updatebasecargo;
+--  id:2.2/8dbba5e239df57ed/UpdateBaseCargo/1/0/test_updatebasecargo/
+   procedure Test_UpdateBaseCargo_test_updatebasecargo (Gnattest_T : in out Test) is
+   procedure UpdateBaseCargo (ProtoIndex: Unbounded_String := Null_Unbounded_String; Amount: Integer; Durability: Natural := 100; CargoIndex: Natural := 0) renames Wrap_Test_UpdateBaseCargo_8dbba5_1e1787;
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+      BaseIndex: constant Positive :=
+        SkyMap(PlayerShip.SkyX, PlayerShip.SkyY).BaseIndex;
+      Amount: Positive;
+      ProtoIndex: Unbounded_String;
+
+   begin
+
+      SkyBases(BaseIndex).Cargo.Clear;
+      GenerateCargo;
+      Amount := SkyBases(BaseIndex).Cargo(1).Amount - 1;
+      ProtoIndex := SkyBases(BaseIndex).Cargo(1).ProtoIndex;
+      UpdateBaseCargo(ProtoIndex, -1);
+      Assert(SkyBases(BaseIndex).Cargo(1).Amount = Amount, "Failed to update base cargo with proto index.");
+      UpdateBaseCargo(CargoIndex => 1, Amount => -1);
+      Assert(SkyBases(BaseIndex).Cargo(1).Amount = Amount - 1, "Failed to update base cargo with cargo index.");
+
+--  begin read only
+   end Test_UpdateBaseCargo_test_updatebasecargo;
+--  end read only
+
+--  begin read only
 --  id:2.2/02/
 --
 --  This section can be used to add elaboration code for the global state.
