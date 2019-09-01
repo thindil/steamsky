b9471c56c585de50cf4701015645680cedcfd180
thindil@laeran.pl <thindil@laeran.pl>
2019-09-01 04:42:45 +0000

    added unit test
    FossilOrigin-Name: 5bf470439f2714e27ce0a424310d053e15abf89202aa09377ff6c36ef78ff228

---
+++ tests/bases-ship-test_data-tests.ads
@@ -0,0 +1,16 @@
+--  This package has been generated automatically by GNATtest.
+--  Do not edit any part of it, see GNATtest documentation for more details.
+
+--  begin read only
+with Gnattest_Generated;
+
+package Bases.Ship.Test_Data.Tests is
+
+   type Test is new GNATtest_Generated.GNATtest_Standard.Bases.Ship.Test_Data.Test
+   with null record;
+
+   procedure Test_RepairShip_41c4af_d40bee (Gnattest_T : in out Test);
+   --  bases-ship.ads:60:4:RepairShip:Test_RepairShip
+
+end Bases.Ship.Test_Data.Tests;
+--  end read only
