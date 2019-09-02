740238bfb0bd1b7cf335343b2061030d8bc8a624
thindil@laeran.pl <thindil@laeran.pl>
2019-09-02 04:29:45 +0000

    added unit test
    FossilOrigin-Name: 050daf2306f98495d9195ad8c056b899944188833e87e4fd1433bc8190570fd5

--- tests/bases-ship-test_data-tests.ads
+++ tests/bases-ship-test_data-tests.ads
@@ -12,5 +12,8 @@ package Bases.Ship.Test_Data.Tests is
    procedure Test_RepairShip_41c4af_d40bee (Gnattest_T : in out Test);
    --  bases-ship.ads:60:4:RepairShip:Test_RepairShip
 
+   procedure Test_UpgradeShip_62a16e_73d66b (Gnattest_T : in out Test);
+   --  bases-ship.ads:73:4:UpgradeShip:Test_UpdgradeShip
+
 end Bases.Ship.Test_Data.Tests;
 --  end read only
