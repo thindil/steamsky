a3575626554a73d46472ae1d5c992b9bf6c746b0
Bartek thindil Jasicki <thindil@laeran.pl>
2019-09-10 06:41:44 +0200

    updated code documentation

--- src/ui/baseslist.ads
+++ src/ui/baseslist.ads
@@ -15,7 +15,7 @@
 --    You should have received a copy of the GNU General Public License
 --    along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
--- ****h* BasesList/BasesList
+-- ****h* Steamsky/BasesList
 -- FUNCTION
 -- Provides code for bases list
 -- SOURCE
