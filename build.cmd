b233ef50d4b0aa6ce0124a14fa35f1b1bcb4e3e2
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2019-09-15 04:32:42 +0000

    moved creating releases to more obvious directories
    FossilOrigin-Name: ef8df4256d397af46c9c3ccd4299dc7e814e2500b63b88339c66ec9c207eeb0e

--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
 @ECHO OFF
 set PATH=%PATH%;C:/GNAT/2019/bin
 gprbuild -P steamsky.gpr -XMode=release
-XCOPY /S bin others\Output\release\bin\
-XCOPY README.md others\Output\release\bin\doc\
+XCOPY /S bin release\bin\
+XCOPY README.md release\bin\doc\
 gprclean -P steamsky.gpr
