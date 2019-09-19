0d06484c81279cf9cd3b15734e96bf70e36c6c16
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2019-09-19 04:40:25 +0000

    added unit test
    FossilOrigin-Name: e253fe3aea1592484431a9a33aabfba873a30f6fb65db5de55280cb906f66e5a

---
+++ tests/basestypes-test_data-tests.ads
@@ -0,0 +1,16 @@
+--  This package has been generated automatically by GNATtest.
+--  Do not edit any part of it, see GNATtest documentation for more details.
+
+--  begin read only
+with Gnattest_Generated;
+
+package BasesTypes.Test_Data.Tests is
+
+   type Test is new GNATtest_Generated.GNATtest_Standard.BasesTypes.Test_Data.Test
+   with null record;
+
+   procedure Test_Is_Buyable_ff76e9_0acf10 (Gnattest_T : in out Test);
+   --  basestypes.ads:104:4:Is_Buyable:Test_Is_Buyable
+
+end BasesTypes.Test_Data.Tests;
+--  end read only
