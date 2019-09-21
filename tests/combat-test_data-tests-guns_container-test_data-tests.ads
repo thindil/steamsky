d727ff315f499e08a7bd5b54232282ee7882804e
Bartek thindil Jasicki <thindil@laeran.pl>
2019-09-21 06:32:23 +0200

    added unit tests

---
+++ tests/combat-test_data-tests-guns_container-test_data-tests.ads
@@ -0,0 +1,13 @@
+--  This package has been generated automatically by GNATtest.
+--  Do not edit any part of it, see GNATtest documentation for more details.
+
+--  begin read only
+with Gnattest_Generated;
+
+package Combat.Test_Data.Tests.Guns_Container.Test_Data.Tests is
+
+   type Test is new GNATtest_Generated.GNATtest_Standard.Combat.Test_Data.Tests.Guns_Container.Test_Data.New_Test
+   with null record;
+
+end Combat.Test_Data.Tests.Guns_Container.Test_Data.Tests;
+--  end read only
