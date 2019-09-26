e589505978ece46230048faed6174c3ad93b0288
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2019-09-26 04:46:42 +0000

    added unit test
    FossilOrigin-Name: 41b733e6219c59de93cd5da66f6f16eace2731f507ab3248ced01df014d989b8

---
+++ tests/crew-inventory-test_data-tests.ads
@@ -0,0 +1,16 @@
+--  This package has been generated automatically by GNATtest.
+--  Do not edit any part of it, see GNATtest documentation for more details.
+
+--  begin read only
+with Gnattest_Generated;
+
+package Crew.Inventory.Test_Data.Tests is
+
+   type Test is new GNATtest_Generated.GNATtest_Standard.Crew.Inventory.Test_Data.Test
+   with null record;
+
+   procedure Test_UpdateInventory_e20eca_ea0095 (Gnattest_T : in out Test);
+   --  crew-inventory.ads:41:4:UpdateInventory:Test_UpdateInventory
+
+end Crew.Inventory.Test_Data.Tests;
+--  end read only
