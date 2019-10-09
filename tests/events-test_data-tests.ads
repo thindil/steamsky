c339b48fd8babf83b0070cf4092ecee8feed618c
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2019-10-09 04:42:36 +0000

    added unit test
    FossilOrigin-Name: fdac087d47f52aeffd7535401e715723208500a8b239f31f467000cbbf98b398

---
+++ tests/events-test_data-tests.ads
@@ -0,0 +1,16 @@
+--  This package has been generated automatically by GNATtest.
+--  Do not edit any part of it, see GNATtest documentation for more details.
+
+--  begin read only
+with Gnattest_Generated;
+
+package Events.Test_Data.Tests is
+
+   type Test is new GNATtest_Generated.GNATtest_Standard.Events.Test_Data.Test
+   with null record;
+
+   procedure Test_CheckForEvent_1c4562_e01b25 (Gnattest_T : in out Test);
+   --  events.ads:99:4:CheckForEvent:Test_CheckForEvent
+
+end Events.Test_Data.Tests;
+--  end read only
