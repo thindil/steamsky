c2a86f456440ef7b493f058abb4d41734c7f7ec1
Bartek thindil Jasicki <thindil@laeran.pl>
2019-10-19 06:30:48 +0200

    updated version number

--- src/game.ads
+++ src/game.ads
@@ -53,7 +53,7 @@ package Game is
    -- FUNCTION
    -- Current the game version
    -- SOURCE
-   GameVersion: constant String := "Version: 4.0.5";
+   GameVersion: constant String := "Version: 4.0.6";
    -- ****
    -- ****t* Game/UnboundedString_Container
    -- FUNCTION
