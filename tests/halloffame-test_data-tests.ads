d556ba01d673ab9eac07ba93afb97f50c11a68fb
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2019-10-26 04:29:12 +0000

    added unit test
    FossilOrigin-Name: 3bf8bab650f9aabc6971301e34935882be5bde1629d9d23401bded2510737504

---
+++ tests/halloffame-test_data-tests.ads
@@ -0,0 +1,16 @@
+--  This package has been generated automatically by GNATtest.
+--  Do not edit any part of it, see GNATtest documentation for more details.
+
+--  begin read only
+with Gnattest_Generated;
+
+package HallOfFame.Test_Data.Tests is
+
+   type Test is new GNATtest_Generated.GNATtest_Standard.HallOfFame.Test_Data.Test
+   with null record;
+
+   procedure Test_UpdateHallOfFame_117948_539c16 (Gnattest_T : in out Test);
+   --  halloffame.ads:66:4:UpdateHallOfFame:Test_UpdateHallOfFame
+
+end HallOfFame.Test_Data.Tests;
+--  end read only
