2d2816819cd233793b365728bc12ec2ac0edf7b0
Bartek thindil Jasicki <thindil@laeran.pl>
2019-11-06 06:33:26 +0100

    added unit test

---
+++ tests/ships-cargo-test_data-tests.ads
@@ -0,0 +1,16 @@
+--  This package has been generated automatically by GNATtest.
+--  Do not edit any part of it, see GNATtest documentation for more details.
+
+--  begin read only
+with Gnattest_Generated;
+
+package Ships.Cargo.Test_Data.Tests is
+
+   type Test is new GNATtest_Generated.GNATtest_Standard.Ships.Cargo.Test_Data.Test
+   with null record;
+
+   procedure Test_UpdateCargo_87d3a7_53988c (Gnattest_T : in out Test);
+   --  ships-cargo.ads:40:4:UpdateCargo:Test_UpdateCargo
+
+end Ships.Cargo.Test_Data.Tests;
+--  end read only
