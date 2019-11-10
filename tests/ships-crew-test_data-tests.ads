39aa36aea2aed507e34acf03ef1b02f92e4f99ab
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2019-11-10 05:38:22 +0000

    added unit test
    FossilOrigin-Name: d12b63fc5c8efeaedae7505d443a4cf5b0799b01982f02619c4a32bf4e2bda91

---
+++ tests/ships-crew-test_data-tests.ads
@@ -0,0 +1,16 @@
+--  This package has been generated automatically by GNATtest.
+--  Do not edit any part of it, see GNATtest documentation for more details.
+
+--  begin read only
+with Gnattest_Generated;
+
+package Ships.Crew.Test_Data.Tests is
+
+   type Test is new GNATtest_Generated.GNATtest_Standard.Ships.Crew.Test_Data.Test
+   with null record;
+
+   procedure Test_GetSkillLevel_f7e690_019f8b (Gnattest_T : in out Test);
+   --  ships-crew.ads:34:4:GetSkillLevel:Test_GetSkillLevel
+
+end Ships.Crew.Test_Data.Tests;
+--  end read only
