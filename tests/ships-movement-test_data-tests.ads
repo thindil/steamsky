048f1dec9466b6daff64b91c713bcc20d165f4b4
Bartek thindil Jasicki <thindil@laeran.pl>
2019-11-18 06:31:00 +0100

    added unit test

---
+++ tests/ships-movement-test_data-tests.ads
@@ -0,0 +1,16 @@
+--  This package has been generated automatically by GNATtest.
+--  Do not edit any part of it, see GNATtest documentation for more details.
+
+--  begin read only
+with Gnattest_Generated;
+
+package Ships.Movement.Test_Data.Tests is
+
+   type Test is new GNATtest_Generated.GNATtest_Standard.Ships.Movement.Test_Data.Test
+   with null record;
+
+   procedure Test_MoveShip_143def_3bb6cb (Gnattest_T : in out Test);
+   --  ships-movement.ads:36:4:MoveShip:Test_MoveShip
+
+end Ships.Movement.Test_Data.Tests;
+--  end read only
