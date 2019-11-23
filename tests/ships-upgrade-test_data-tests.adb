95d4ea8a9fe248f18eb444f148c7a6e3d7050c57
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2019-11-23 05:34:10 +0000

    added unit test
    FossilOrigin-Name: 5b1a863f9bd26f3f94b110f231e5601d9f9102e175808401c1226bac4dada857

---
+++ tests/ships-upgrade-test_data-tests.adb
@@ -0,0 +1,88 @@
+--  This package has been generated automatically by GNATtest.
+--  You are allowed to add your code to the bodies of test routines.
+--  Such changes will be kept during further regeneration of this file.
+--  All code placed outside of test routine bodies will be lost. The
+--  code intended to set up and tear down the test environment should be
+--  placed into Ships.Upgrade.Test_Data.
+
+with AUnit.Assertions; use AUnit.Assertions;
+with System.Assertions;
+
+--  begin read only
+--  id:2.2/00/
+--
+--  This section can be used to add with clauses if necessary.
+--
+--  end read only
+
+--  begin read only
+--  end read only
+package body Ships.Upgrade.Test_Data.Tests is
+
+--  begin read only
+--  id:2.2/01/
+--
+--  This section can be used to add global variables and other elements.
+--
+--  end read only
+
+--  begin read only
+--  end read only
+--  begin read only
+   procedure Wrap_Test_StartUpgrading_50d3a9_d84296 (ModuleIndex, UpgradeType: Positive) 
+   is
+   begin
+      begin
+         pragma Assert
+           ((ModuleIndex <= PlayerShip.Modules.Last_Index and UpgradeType < 5));
+         null;
+      exception
+         when System.Assertions.Assert_Failure =>
+            AUnit.Assertions.Assert
+              (False,
+               "req_sloc(ships-upgrade.ads:0):Test_StartUpgrading test requirement violated");
+      end;
+      GNATtest_Generated.GNATtest_Standard.Ships.Upgrade.StartUpgrading (ModuleIndex, UpgradeType);
+      begin
+         pragma Assert
+           (True);
+         null;
+      exception
+         when System.Assertions.Assert_Failure =>
+            AUnit.Assertions.Assert
+              (False,
+               "ens_sloc(ships-upgrade.ads:0:):Test_StartUpgrading test commitment violated");
+      end;
+   end Wrap_Test_StartUpgrading_50d3a9_d84296;
+--  end read only
+
+--  begin read only
+   procedure Test_StartUpgrading_test_startupgrading (Gnattest_T : in out Test);
+   procedure Test_StartUpgrading_50d3a9_d84296 (Gnattest_T : in out Test) renames Test_StartUpgrading_test_startupgrading;
+--  id:2.2/50d3a9c354e40966/StartUpgrading/1/0/test_startupgrading/
+   procedure Test_StartUpgrading_test_startupgrading (Gnattest_T : in out Test) is
+   procedure StartUpgrading (ModuleIndex, UpgradeType: Positive) renames Wrap_Test_StartUpgrading_50d3a9_d84296;
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      StartUpgrading(1, 1);
+      Assert(True, "This test can only crash.");
+
+--  begin read only
+   end Test_StartUpgrading_test_startupgrading;
+--  end read only
+
+--  begin read only
+--  id:2.2/02/
+--
+--  This section can be used to add elaboration code for the global state.
+--
+begin
+--  end read only
+   null;
+--  begin read only
+--  end read only
+end Ships.Upgrade.Test_Data.Tests;
---
+++ tests/ships-upgrade-test_data-tests.ads
@@ -0,0 +1,16 @@
+--  This package has been generated automatically by GNATtest.
+--  Do not edit any part of it, see GNATtest documentation for more details.
+
+--  begin read only
+with Gnattest_Generated;
+
+package Ships.Upgrade.Test_Data.Tests is
+
+   type Test is new GNATtest_Generated.GNATtest_Standard.Ships.Upgrade.Test_Data.Test
+   with null record;
+
+   procedure Test_StartUpgrading_50d3a9_d84296 (Gnattest_T : in out Test);
+   --  ships-upgrade.ads:39:4:StartUpgrading:Test_StartUpgrading
+
+end Ships.Upgrade.Test_Data.Tests;
+--  end read only
