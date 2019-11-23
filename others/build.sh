40ac3f53adb944f7e931907eae30e78548daa769
Bartek thindil Jasicki <thindil@laeran.pl>
2019-11-23 06:28:53 +0100

    updated Linux build script

--- others/build.sh
+++ others/build.sh
@@ -2,6 +2,7 @@
 
 gprbuild -p steamsky.gpr -XMode=release
 mkdir -p usr/bin
+mkdir -p usr/share/metainfo/
 cp -r share usr
 cp bin/steamsky usr/bin
 cp -r bin/data usr/share
@@ -10,3 +11,4 @@ rm -r usr/share/data/saves
 rm -r usr/share/data/themes
 cp -r bin/doc usr/share
 cp README.md usr/share/doc
+cp others/pl.laeran.steamsky.appdata.xml usr/share/metainfo
