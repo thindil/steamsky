64964947eee387862d62554e9d5f5258b7e029d3
thindil@laeran.pl <thindil@laeran.pl>
2019-12-14 05:27:17 +0000

    fixed printing tests output
    FossilOrigin-Name: 25d0c2d17fde4bf6e7e3b3222be5a360706136f2966df54f137776ea32fc8a9e

--- others/tests.sh
+++ others/tests.sh
@@ -4,8 +4,8 @@ cd "tests/driver"
 
 for i in `seq 1 $1`;
 do
-   result=`./test_runner`
-   echo $result
+   result=$(./test_runner)
+   echo "$result"
    if [[ "$result" =~ "FAILED" ]] || [[ "$result" =~ "CRASHED" ]]; then
       exit 1
    fi
