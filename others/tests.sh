8120514cfe8894128e4ed56d20db3944d08056be
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2019-12-16 05:24:37 +0000

    added info about test iteration number
    FossilOrigin-Name: c41d0899d99b43e9f87a924da4758e662aa7be920cb2f30ff38e899085be93ef

--- others/tests.sh
+++ others/tests.sh
@@ -5,7 +5,7 @@ cd "tests/driver"
 for i in `seq 1 $1`;
 do
    result=$(./test_runner)
-   echo "$result"
+   echo "$i: $result"
    if [[ "$result" =~ "FAILED" ]] || [[ "$result" =~ "CRASHED" ]]; then
       exit 1
    fi
