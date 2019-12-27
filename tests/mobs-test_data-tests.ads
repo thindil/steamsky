4c5f3719e07dbdb62da88239810a7ee94e060b83
thindil@laeran.pl <thindil@laeran.pl>
2019-12-27 05:41:27 +0000

    added unit test
    FossilOrigin-Name: 6153d22351d3c0b545200167608e53e93efc5c197ac88dac6582dca9232102d8

---
+++ tests/mobs-test_data-tests.ads
@@ -0,0 +1,17 @@
+--  This package has been generated automatically by GNATtest.
+--  Do not edit any part of it, see GNATtest documentation for more details.
+
+--  begin read only
+with Gnattest_Generated;
+
+package Mobs.Test_Data.Tests is
+
+   type Test is new GNATtest_Generated.GNATtest_Standard.Mobs.Test_Data
+     .Test with
+   null record;
+
+   procedure Test_GenerateMob_520182_3c2c38(Gnattest_T: in out Test);
+   --  mobs.ads:121:4:GenerateMob:Test_GenearateMob
+
+end Mobs.Test_Data.Tests;
+--  end read only
