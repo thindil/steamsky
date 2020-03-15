675912c8f756928485a4b71ac178b756515e0e99
thindil@laeran.pl <thindil@laeran.pl>
2020-03-15 05:44:37 +0000

    fixed crash when healing last crew member in the bases
    FossilOrigin-Name: cb81625690cff7597b9c47ea5997ae6c71d41245220923a964037aa213fcbdb2

--- src/bases-trade.ads
+++ src/bases-trade.ads
@@ -1,4 +1,4 @@
---    Copyright 2017-2019 Bartek thindil Jasicki
+--    Copyright 2017-2020 Bartek thindil Jasicki
 --
 --    This file is part of Steam Sky.
 --
@@ -75,7 +75,7 @@ package Bases.Trade is
       --               all wounded crew members
       -- SOURCE
    procedure HealWounded(MemberIndex: Crew_Container.Extended_Index) with
-      Pre => (MemberIndex < PlayerShip.Crew.Last_Index);
+      Pre => (MemberIndex <= PlayerShip.Crew.Last_Index);
       -- ****
       -- ****f* Bases.Trade/HealCost
       -- FUNCTION
@@ -91,7 +91,7 @@ package Bases.Trade is
    procedure HealCost
      (Cost, Time: in out Natural;
       MemberIndex: Crew_Container.Extended_Index) with
-      Pre => (MemberIndex < PlayerShip.Crew.Last_Index);
+      Pre => (MemberIndex <= PlayerShip.Crew.Last_Index);
       -- ****
       -- ****f* Bases.Trade/TrainCost
       -- FUNCTION
