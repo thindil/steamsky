90c4d8bc1e83c938b519df861b98cdfdd1ed14f2
thindil@laeran.pl <thindil@laeran.pl>
2020-03-25 05:28:31 +0000

    fixed checking for money during installing/removing modules
    FossilOrigin-Name: 48e9464d8494c3f42d7e070508924006f45bc36ab8838ac7068b7bb1e4a82074

--- src/bases-ship.adb
+++ src/bases-ship.adb
@@ -151,13 +151,13 @@ package body Bases.Ship is
             end if;
             PlayerShip.Modules.Delete(HullIndex);
          end if;
-         UpdateGame(Modules_List(ModuleIndex).InstallTime);
          UpdateCargo
            (Ship => PlayerShip, CargoIndex => MoneyIndex2,
             Amount => (0 - Price));
          UpdateBaseCargo(MoneyIndex, Price);
          GainExp(1, TalkingSkill, TraderIndex);
          GainRep(SkyMap(PlayerShip.SkyX, PlayerShip.SkyY).BaseIndex, 1);
+         UpdateGame(Modules_List(ModuleIndex).InstallTime);
          if Modules_List(ModuleIndex).MType /= HULL then
             for I in 1 .. Modules_List(ModuleIndex).MaxOwners loop
                Owners.Append(0);
@@ -406,9 +406,6 @@ package body Bases.Ship is
                end if;
             end loop;
          end if;
-         UpdateGame
-           (Modules_List(PlayerShip.Modules(ShipModuleIndex).ProtoIndex)
-              .InstallTime);
          if PlayerShip.Modules(ShipModuleIndex).MType /= CABIN then
             for Owner of PlayerShip.Modules(ShipModuleIndex).Owner loop
                if Owner > 0 then
@@ -423,6 +420,9 @@ package body Bases.Ship is
          UpdateBaseCargo(MoneyIndex, Price);
          GainExp(1, TalkingSkill, TraderIndex);
          GainRep(SkyMap(PlayerShip.SkyX, PlayerShip.SkyY).BaseIndex, 1);
+         UpdateGame
+           (Modules_List(PlayerShip.Modules(ShipModuleIndex).ProtoIndex)
+              .InstallTime);
          AddMessage
            ("You removed " &
             To_String(PlayerShip.Modules(ShipModuleIndex).Name) &
