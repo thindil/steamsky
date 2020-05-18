8640e3e64d9cd402c42394d5b20d2ee558719216
thindil@laeran.pl <thindil@laeran.pl>
2020-05-18 04:27:23 +0000

    replaced generating docs script with new version
    FossilOrigin-Name: 0ef8728b2b764e8fcb369db42d4f7e9ed5981e52112d45516516d893b9242736

---
+++ others/generatedocs.tcl
@@ -0,0 +1,79 @@
+#!/bin/sh
+# the next line restarts using tclsh \
+exec tclsh "$0" ${1+"$@"}
+
+# Copyright (c) 2019 Bartek thindil Jasicki <thindil@laeran.pl>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+proc fixdocs {dir} {
+   foreach filename [glob -directory $dir *.html] {
+      # read whole file to list of lines
+      set fp [open $filename r]
+      set data [read $fp]
+      close $fp
+      # write a new version of file
+      set fp [open $filename w]
+      set check false
+      lmap line [split $data \n] {
+         if [string equal [string range $line 0 19] "<pre class=\"source\">"] {
+            set check true
+         }
+         if [string equal [string range $line 0 5] "</pre>"] {
+            set check false
+         }
+         if $check {
+            if [regexp {<span class="squote">'\w+'*} $line result] {
+               if {[string equal [string index $result end] "'"] == 0} {
+                  set keyword [string range $result 21 end]
+                  regsub {<span class="squote">'\w+'*} $line "<span class=\"keyword\">$keyword</span>" line
+               }
+            }
+            regsub -all {(?!\">)(\)|:=|:|\(|&gt;|&lt;|;|,)+(?!</span>)} $line "<span class=\"sign\">&</span>" line
+            regsub -all {(?!\">)(\mis\M|\mpragma\M|\mreturn\M|\mconstant\M|\mwith\M|\maccess\M|\mfunction\M|\mprocedure\M|\min\M|\mabstract\M)+(?!</span>)} $line "<span class=\"keyword\">&</span>" line
+            if [regexp {</span>\"\w+\"} $line result] {
+               set quote [string range $result 7 end]
+               regsub {</span>\"\w+\"} $line "</span><span class=\"quote\">$quote</span>" line
+            }
+         }
+         puts $fp $line
+      }
+      close $fp
+   }
+   # fix documenation in any subdirectory too
+   set subdirs [glob -type d -directory $dir -nocomplain *]
+   foreach dirname $subdirs {
+      fixdocs $dirname
+   }
+}
+
+set docsdir docs
+set configfile [file join scripts robodocada.rc]
+if {$argc > 0} {
+   set configfile [lindex $argv 0]
+}
+if {$argc > 1} {
+   set docsdir [lindex $argv 1]
+}
+
+if [file exists docsdir ] {
+   file delete docsdir
+}
+exec robodoc --rc $configfile
+fixdocs $docsdir
