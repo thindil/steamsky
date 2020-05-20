9b4d1b19125711fe59a0091a53347c56f95b4007
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-05-20 04:38:33 +0000

    started work on main menu
    FossilOrigin-Name: f128499d56d98888914d369d3bfb8c36cdc317ee9eec526e151ee4a394bd1926

---
+++ src/ui/mainmenu.ads
@@ -0,0 +1,37 @@
+-- Copyright (c) 2020 Bartek thindil Jasicki <thindil@laeran.pl>
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+-- ****h* Steamsky/MainMenu
+-- FUNCTION
+-- Provide code for manipulate the game main menu
+-- SOURCE
+package MainMenu is
+-- ****
+
+   -- ****f* MainMenu/CreateMainMenu
+   -- FUNCTION
+   -- Create main menu UI
+   -- SOURCE
+   procedure CreateMainMenu;
+   -- ****
+
+   -- ****f* MainMenu/ShowMainMenu
+   -- FUNCTION
+   -- Show main menu to a player
+   -- SOURCE
+   procedure ShowMainMenu;
+   -- ****
+
+end MainMenu;
