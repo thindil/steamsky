1233813adaf0100bed78df062d72ec408bbd60cc
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-05-20 05:29:13 +0000

    updated main menu
    FossilOrigin-Name: 0756107853f7e35eb68cc85b9634fbe1ebcd0110a29d2b9c821ff89a391528de

--- bin/data/ui/mainmenu.tcl
+++ bin/data/ui/mainmenu.tcl
@@ -1,9 +1,9 @@
 ttk::frame .mainmenu
+ttk::button .mainmenu.loadgame -text {Load game}
+ttk::button .mainmenu.halloffame -text {Hall of Fame}
 pack [ttk::label .mainmenu.logo -text {Steam Sky} -font {Rye 70}]
 pack [ttk::label .mainmenu.version -text {Version 5.1 development}]
 pack [ttk::button .mainmenu.newgame -text {New game}]
-pack [ttk::button .mainmenu.loadgame -text {Load game}]
-pack [ttk::button .mainmenu.halloffame -text {Hall of Fame}]
 pack [ttk::button .mainmenu.news -text {News}]
 pack [ttk::button .mainmenu.about -text {About}]
 pack [ttk::button .mainmenu.quit -text {Quit} -command exit]
