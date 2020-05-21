eb0405772f9cc60c99a77519ae656466ae96c4c5
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-05-21 05:22:44 +0000

    started work on opening links
    FossilOrigin-Name: aa32516114f2a7bf2bcf913ff1a30e38a8002f5fdee8d57670a18f9dc8264001

--- bin/data/ui/mainmenu.tcl
+++ bin/data/ui/mainmenu.tcl
@@ -13,7 +13,13 @@ pack [ttk::button .mainmenu.newgame -text {New game} -underline 0]
 ttk::button .mainmenu.loadgame -text {Load game} -underline 0
 ttk::button .mainmenu.halloffame -text {Hall of Fame} -underline 0
 pack [ttk::button .mainmenu.news -text {News} -underline 1]
-pack [ttk::button .mainmenu.about -text {About} -underline 0 -command {bind . <Alt-s> {InvokeButton .aboutmenu.showlicense}; bind . <Alt-b> {InvokeButton .aboutmenu.back}; bind . <Escape> {InvokeButton .aboutmenu.back}; pack forget .mainmenu; pack .aboutmenu -fill both -expand true}]
+pack [ttk::button .mainmenu.about -text {About} -underline 0 -command {
+   bind . <Alt-s> {InvokeButton .aboutmenu.showlicense}
+   bind . <Alt-b> {InvokeButton .aboutmenu.back}
+   bind . <Escape> {InvokeButton .aboutmenu.back}
+   pack forget .mainmenu
+   pack .aboutmenu -fill both -expand true
+}]
 pack [ttk::button .mainmenu.quit -text {Quit} -command exit -underline 0]
 bind . <Alt-n> {InvokeButton .mainmenu.newgame}
 bind . <Alt-l> {InvokeButton .mainmenu.loadgame}
@@ -25,12 +31,18 @@ bind . <Alt-q> {InvokeButton .mainmenu.quit}
 # About menu
 ttk::frame .aboutmenu
 grid [ttk::label .aboutmenu.about -text {Roguelike in the sky with a steampunk theme}] -columnspan 3
-grid [ttk::button .aboutmenu.website -text {Website} -style Toolbutton] -row 1 -columnspan 3
-grid [ttk::button .aboutmenu.mail -text {(c)2016-2020 Bartek thindil Jasicki} -style Toolbutton] -row 2 -columnspan 3
+grid [ttk::button .aboutmenu.website -text {Website} -style Toolbutton -command {OpenLink https://thindil.itch.io/steam-sky}] -row 1 -columnspan 3
+grid [ttk::button .aboutmenu.mail -text {(c)2016-2020 Bartek thindil Jasicki} -style Toolbutton -command {OpenLink mailto:thindil@laeran.pl}] -row 2 -columnspan 3
 grid [ttk::button .aboutmenu.getinvolved -text {Get involved}] -row 3 -sticky e
 grid [ttk::button .aboutmenu.modify -text {Modify game}] -row 3 -column 1
 grid [ttk::button .aboutmenu.readme -text {README}] -row 3 -column 2 -sticky w
 grid [ttk::label .aboutmenu.license -text {Steam Sky is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.} -wraplength 590] -row 4 -columnspan 3
 grid [ttk::label .aboutmenu.license2 -text {Steam Sky is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.} -wraplength 590] -row 5 -columnspan 3
 grid [ttk::button .aboutmenu.showlicense -text {Show full license} -underline 0] -row 6 -column 1 -sticky e
-grid [ttk::button .aboutmenu.back -text {Back to main menu} -underline 0 -command {bind . <Alt-s> {}; bind . <Alt-b> {}; bind . <Escape> {}; pack forget .aboutmenu; pack .mainmenu -fill both -expand true}] -row 6 -column 2 -sticky e
+grid [ttk::button .aboutmenu.back -text {Back to main menu} -underline 0 -command {
+   bind . <Alt-s> {}
+   bind . <Alt-b> {}
+   bind . <Escape> {}
+   pack forget .aboutmenu
+   pack .mainmenu -fill both -expand true
+}] -row 6 -column 2 -sticky e
