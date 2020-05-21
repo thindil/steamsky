4d493869474fcafd405d304d9360e46d3502de78
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-05-21 04:32:38 +0000

    continue work on main menu
    FossilOrigin-Name: d1cb3320168138801475812bb612388807fbaaa224b124629c9f954ca00ae870

--- bin/data/ui/mainmenu.tcl
+++ bin/data/ui/mainmenu.tcl
@@ -1,9 +1,23 @@
+proc InvokeButton {name} {
+   if {[winfo ismapped $name] == "1"} {
+      focus $name
+      $name invoke
+   }
+}
+
+# Main Menu
 ttk::frame .mainmenu
-ttk::button .mainmenu.loadgame -text {Load game}
-ttk::button .mainmenu.halloffame -text {Hall of Fame}
 pack [ttk::label .mainmenu.logo -text {Steam Sky} -font {Rye 70}]
 pack [ttk::label .mainmenu.version -text {Version 5.1 development}]
-pack [ttk::button .mainmenu.newgame -text {New game}]
-pack [ttk::button .mainmenu.news -text {News}]
-pack [ttk::button .mainmenu.about -text {About}]
-pack [ttk::button .mainmenu.quit -text {Quit} -command exit]
+pack [ttk::button .mainmenu.newgame -text {New game} -underline 0]
+ttk::button .mainmenu.loadgame -text {Load game} -underline 0
+ttk::button .mainmenu.halloffame -text {Hall of Fame} -underline 0
+pack [ttk::button .mainmenu.news -text {News} -underline 1]
+pack [ttk::button .mainmenu.about -text {About} -underline 0]
+pack [ttk::button .mainmenu.quit -text {Quit} -command exit -underline 0]
+bind . <Alt-n> {InvokeButton .mainmenu.newgame}
+bind . <Alt-l> {InvokeButton .mainmenu.loadgame}
+bind . <Alt-h> {InvokeButton .mainmenu.halloffame}
+bind . <Alt-e> {InvokeButton .mainmenu.news}
+bind . <Alt-a> {InvokeButton .mainmenu.about}
+bind . <Alt-q> {InvokeButton .mainmenu.quit}
