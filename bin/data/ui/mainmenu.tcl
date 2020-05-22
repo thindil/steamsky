bda22c54b7f87bcd456f2c0059a1049119cd288e
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-05-22 04:29:23 +0000

    added showing the selected file
    FossilOrigin-Name: a4c0dbe16ff357fcd45c7d0f42960c05ca8796b3adb7d8c0cac3b89ee2894f3d

--- bin/data/ui/mainmenu.tcl
+++ bin/data/ui/mainmenu.tcl
@@ -33,12 +33,28 @@ ttk::frame .aboutmenu
 grid [ttk::label .aboutmenu.about -text {Roguelike in the sky with a steampunk theme}] -columnspan 3
 grid [ttk::button .aboutmenu.website -text {Website} -style Toolbutton -command {OpenLink https://thindil.itch.io/steam-sky}] -row 1 -columnspan 3
 grid [ttk::button .aboutmenu.mail -text {(c)2016-2020 Bartek thindil Jasicki} -style Toolbutton -command {OpenLink mailto:thindil@laeran.pl}] -row 2 -columnspan 3
-grid [ttk::button .aboutmenu.getinvolved -text {Get involved}] -row 3 -sticky e
-grid [ttk::button .aboutmenu.modify -text {Modify game}] -row 3 -column 1
-grid [ttk::button .aboutmenu.readme -text {README}] -row 3 -column 2 -sticky w
+grid [ttk::button .aboutmenu.getinvolved -text {Get involved} -command {
+   pack forget .aboutmenu
+   pack .showfilemenu -fill both -expand true
+   ShowFile CONTRIBUTING.md
+}] -row 3 -sticky e
+grid [ttk::button .aboutmenu.modify -text {Modify game} -command {
+   pack forget .aboutmenu
+   pack .showfilemenu -fill both -expand true
+   ShowFile MODDING.md
+}] -row 3 -column 1
+grid [ttk::button .aboutmenu.readme -text {README} -command {
+   pack forget .aboutmenu
+   pack .showfilemenu -fill both -expand true
+   ShowFile README.md
+}] -row 3 -column 2 -sticky w
 grid [ttk::label .aboutmenu.license -text {Steam Sky is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.} -wraplength 590] -row 4 -columnspan 3
 grid [ttk::label .aboutmenu.license2 -text {Steam Sky is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.} -wraplength 590] -row 5 -columnspan 3
-grid [ttk::button .aboutmenu.showlicense -text {Show full license} -underline 0] -row 6 -column 1 -sticky e
+grid [ttk::button .aboutmenu.showlicense -text {Show full license} -underline 0 -command {
+   pack forget .aboutmenu
+   pack .showfilemenu -fill both -expand true
+   ShowFile COPYING
+}] -row 6 -column 1 -sticky e
 grid [ttk::button .aboutmenu.back -text {Back to main menu} -underline 0 -command {
    bind . <Alt-s> {}
    bind . <Alt-b> {}
@@ -49,11 +65,11 @@ grid [ttk::button .aboutmenu.back -text {Back to main menu} -underline 0 -comman
 
 # Show file content
 ttk::frame .showfilemenu
-grid [text .showfilemenu.text -wrap word] -sticky nesw
-grid [ttk::scrollbar .showfilemenu.scroll -orient vertical -command [list .showfilemenu.text yview]] -column 1 -row 0
-grid [ttk::button .showfilemenu.back -text {Back} -command {
+pack [ttk::button .showfilemenu.back -text {Back} -underline 0 -command {
    bind . <Alt-b> {}
    bind . <Escape> {}
    pack forget .showfilemenu
    pack .mainmenu -fill both -expand true
-}] -row 1 -columnspan 2 -sticky e
+}] -side bottom -anchor e
+pack [ttk::scrollbar .showfilemenu.scroll -orient vertical -command [list .showfilemenu.text yview]] -side right -fill y
+pack [text .showfilemenu.text -wrap char -yscrollcommand {.showfilemenu.scroll set}] -side top -fill both -expand true
