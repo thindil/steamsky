726c5e341ba9bbee9dc236db0c1cb3d83873c4e9
thindil@laeran.pl <thindil@laeran.pl>
2020-05-25 05:10:06 +0000

    started work on better message dialog
    FossilOrigin-Name: 20e471a9ed20e5b57065dd7ce1c5239055e68435e484c9002e635b712f05926a

--- src/ui/utils-ui.adb
+++ src/ui/utils-ui.adb
@@ -13,15 +13,137 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+with Ada.Strings; use Ada.Strings;
+with Ada.Strings.Fixed; use Ada.Strings.Fixed;
+with Interfaces.C; use Interfaces.C;
+with Interfaces.C.Strings; use Interfaces.C.Strings;
+with CArgv;
+with Tcl; use Tcl;
+with Tcl.Ada; use Tcl.Ada;
+with Tcl.Tk.Ada; use Tcl.Tk.Ada;
+with Tcl.Tk.Ada.Busy; use Tcl.Tk.Ada.Busy;
 with Tcl.Tk.Ada.Dialogs; use Tcl.Tk.Ada.Dialogs;
+with Tcl.Tk.Ada.Widgets; use Tcl.Tk.Ada.Widgets;
+with Tcl.Tk.Ada.Widgets.Toplevel; use Tcl.Tk.Ada.Widgets.Toplevel;
+with Tcl.Tk.Ada.Widgets.Toplevel.MainWindow;
+use Tcl.Tk.Ada.Widgets.Toplevel.MainWindow;
+with Tcl.Tk.Ada.Widgets.TtkButton; use Tcl.Tk.Ada.Widgets.TtkButton;
+with Tcl.Tk.Ada.Widgets.TtkFrame; use Tcl.Tk.Ada.Widgets.TtkFrame;
+with Tcl.Tk.Ada.Widgets.TtkLabel; use Tcl.Tk.Ada.Widgets.TtkLabel;
+with Tcl.Tk.Ada.Winfo; use Tcl.Tk.Ada.Winfo;
+with Tcl.Tk.Ada.Wm; use Tcl.Tk.Ada.Wm;
+with Config; use Config;
 
 package body Utils.UI is
 
-   procedure ShowMessage(Text, Icon: String) is
-      Dummy: constant String := MessageBox
-                ("-message {" & Text & "} -icon " & Icon &" -type ok");
+   Timer_Token: Tcl_TimerToken;
+
+   function Close_Dialog_Command
+     (ClientData: in Integer; Interp: in Tcl.Tcl_Interp;
+      Argc: in Interfaces.C.int; Argv: in CArgv.Chars_Ptr_Ptr)
+      return Interfaces.C.int with
+      Convention => C;
+
+   function Close_Dialog_Command
+     (ClientData: in Integer; Interp: in Tcl.Tcl_Interp;
+      Argc: in Interfaces.C.int; Argv: in CArgv.Chars_Ptr_Ptr)
+      return Interfaces.C.int is
+      pragma Unreferenced(ClientData, Argc);
+      Dialog: Tk_Toplevel;
+      MainWindow: constant Tk_Toplevel := Get_Main_Window(Interp);
    begin
-      null;
+      Dialog.Interp := Interp;
+      Dialog.Name := New_String(CArgv.Arg(Argv, 1));
+      Destroy(Dialog);
+      if Winfo_Get(MainWindow, "exists") = "1"
+        and then Status(MainWindow) = "1" then
+         Forget(MainWindow);
+      end if;
+      return TCL_OK;
+   end Close_Dialog_Command;
+
+   procedure CloseMessage(data: ClientData) with
+      Convention => C;
+
+   procedure CloseMessage(data: ClientData) is
+      pragma Unreferenced(data);
+      Dialog: Tk_Toplevel;
+      MainWindow: constant Tk_Toplevel := Get_Main_Window(Get_Context);
+   begin
+      Dialog.Interp := Get_Context;
+      Dialog.Name := New_String(".message");
+      Destroy(Dialog);
+      if Winfo_Get(MainWindow, "exists") = "1"
+        and then Status(MainWindow) = "1" then
+         Forget(MainWindow);
+      end if;
+   end CloseMessage;
+
+   procedure ShowMessage(Text: String) is
+      MessageDialog: constant Tk_Toplevel :=
+        Create(".message", "-class Dialog");
+      MainWindow: constant Tk_Toplevel := Get_Main_Window(Get_Context);
+      X, Y: Integer;
+      MessageLabel: constant Ttk_Label :=
+        Create(".message.text", "-text {" & Text & "} -wraplength 300");
+      Width: constant Positive :=
+        Positive'Value(Winfo_Get(MessageLabel, "reqwidth"));
+      Height: constant Positive :=
+        Positive'Value(Winfo_Get(MessageLabel, "reqheight"));
+   begin
+      Tcl.Tk.Ada.Busy.Busy(MainWindow);
+      if Timer_Token /= null then
+         Tcl_DeleteTimerHandler(Timer_Token);
+      end if;
+      Wm_Set(MessageDialog, "title", "{Steam Sky - Message}");
+      Wm_Set(MessageDialog, "transient", ".");
+      if Tcl_GetVar(Get_Context, "tcl_platform(os)") = "Linux" then
+         Wm_Set(MessageDialog, "attributes", "-type dialog");
+      end if;
+      X :=
+        (Positive'Value(Winfo_Get(MessageDialog, "vrootwidth")) - Width) / 2;
+      if X < 0 then
+         X := 0;
+      end if;
+      Y :=
+        (Positive'Value(Winfo_Get(MessageDialog, "vrootheight")) - Height) / 2;
+      if Y < 0 then
+         Y := 0;
+      end if;
+      Wm_Set
+        (MessageDialog, "geometry",
+         Trim(Positive'Image(Width), Left) & "x" &
+         Trim(Positive'Image(Height), Left) & "+" &
+         Trim(Positive'Image(X), Left) & "+" & Trim(Positive'Image(Y), Left));
+      Bind
+        (MessageDialog, "<Destroy>",
+         "{CloseDialog " & Value(MessageDialog.Name) & "}");
+      if GameSettings.AutoCloseMessagesTime > 0 then
+         Timer_Token :=
+           Tcl_CreateTimerHandler
+             (int(GameSettings.AutoCloseMessagesTime) * 1_000,
+              CloseMessage'Access, Null_ClientData);
+      end if;
    end ShowMessage;
+
+   package CreateCommands is new Tcl.Ada.Generic_Command(Integer);
+
+   procedure AddCommand
+     (Name: String; AdaCommand: not null CreateCommands.Tcl_CmdProc) is
+      Command: Tcl.Tcl_Command;
+      Aziptk_Add_Command_Error: exception;
+   begin
+      Command :=
+        CreateCommands.Tcl_CreateCommand
+          (Get_Context, Name, AdaCommand, 0, null);
+      if Command = null then
+         raise Aziptk_Add_Command_Error with "Can't add command " & Name;
+      end if;
+   end AddCommand;
+
+   procedure AddCommands is
+   begin
+      AddCommand("CloseDialog", Close_Dialog_Command'Access);
+   end AddCommands;
 
 end Utils.UI;
