f655c55b4b66e2bc908c3691e5f134ed2ab70d30
thindil@laeran.pl <thindil@laeran.pl>
2020-05-30 05:52:56 +0000

    added error report UI
    FossilOrigin-Name: 6a441456c7a222726638303e60ea6f050ff09a13bce793101942adc6dc76f550

--- bin/data/ui/errordialog.tcl
+++ bin/data/ui/errordialog.tcl
@@ -1,7 +1,12 @@
 wm title . {Steam Sky - Error info}
 wm geometry . 800x600+[expr ([winfo vrootwidth .] - 800) / 2]+[expr ([winfo vrootheight .] - 600) / 2]
-grid [ttk::label .generalinfo -text {Oops, something bad happens and the game has crashed. Game should save your progress, but better verify this yourself. Also, please, remember what you were doing before the crash and report this problem at} -wraplength 790] -columnspan 2
-grid [ttk::button .reportlink -text {https://github.com/thindil/steamsky/issues} -style Toolbutton] -columnspan 2
-grid [ttk::label .generalinfo2 -text {or if you prefer, on one of the game community options:}] -columnspan 2
-grid [ttk::button .reportlink2 -text {https://thindil.itch.io/steam-sky}] -columnspan 2
-grid [ttk::label .generalinfo3 -text {and attach (if possible) file 'error.log'}] -columnspan 2
+pack [ttk::label .generalinfo -text {Oops, something bad happens and the game has crashed. Game should save your progress, but better verify this yourself. Also, please, remember what you were doing before the crash and report this problem at} -wraplength 790]
+pack [ttk::button .reportlink -text {https://github.com/thindil/steamsky/issues} -command {OpenLink https://github.com/thindil/steamsky/issues} -style Toolbutton]
+pack [ttk::label .generalinfo2 -text {or if you prefer, on one of the game community options:}]
+pack [ttk::button .reportlink2 -text {https://thindil.itch.io/steam-sky} -command {OpenLink https://thindil.itch.io/steam-sky} -style Toolbutton]
+pack [ttk::label .generalinfo3 -text {and attach (if possible) file 'error.log'}]
+pack [ttk::button .close -text {Close} -command {exit}]
+ttk::labelframe .technical -text {Technical information:}
+pack [ttk::scrollbar .technical.scroll -orient vertical -command [list .technical.text yview]] -side right -fill y
+pack [text .technical.text -wrap word -yscrollcommand [list .technical.scroll set]] -fill both -expand true -side top
+pack .technical -fill both -expand true
