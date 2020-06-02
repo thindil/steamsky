1ba5c21270a0e40d7b682d1b3b3575ac77d51235
thindil@laeran.pl <thindil@laeran.pl>
2020-06-02 04:39:19 +0000

    added creating main game menu
    FossilOrigin-Name: ed85c5189862498ed8fdff64f03c1e596221f093cf2551c6de2290d11850b486

--- src/ui/maps-ui.adb
+++ src/ui/maps-ui.adb
@@ -20,21 +20,60 @@ with Tcl.Tk.Ada; use Tcl.Tk.Ada;
 with Tcl.Tk.Ada.Widgets.Menu; use Tcl.Tk.Ada.Widgets.Menu;
 with Tcl.Tk.Ada.Widgets.Toplevel.MainWindow;
 use Tcl.Tk.Ada.Widgets.Toplevel.MainWindow;
+with Tcl.Tk.Ada.Winfo; use Tcl.Tk.Ada.Winfo;
 with Game; use Game;
+with Ships; use Ships;
 
 package body Maps.UI is
 
-   procedure CreateGameUI is
-      Menu: Tk_Menu;
+   -- ****iv* MUI/GameMenu
+   -- FUNCTION
+   -- The main game menu
+   -- SOURCE
+   GameMenu: Tk_Menu;
+   -- ****
+
+   -- ****if* MUI/CreateGameMenu
+   -- FUNCTION
+   -- Create the main game menu. Clear old elements and add all default
+   -- SOURCE
+   procedure CreateGameMenu is
+      -- ****
    begin
-      Tcl_EvalFile
-        (Get_Context,
-         To_String(DataDirectory) & "ui" & Dir_Separator & "game.tcl");
-      Menu.Interp := Get_Context;
-      Menu.Name := New_String(".gamemenu");
+      Delete(GameMenu, "0", "end");
+      Add(GameMenu, "command", "-label {Ship information}");
+      Add(GameMenu, "command", "-label {Ship cargo}");
+      Add(GameMenu, "command", "-label {Crew information}");
+      Add
+        (GameMenu, "command",
+         "-label {Ship orders} -command {tk_popup .orders [expr [winfo width .paned.mapframe] / 3] [expr [winfo height .paned.mapframe] / 3]}");
+      Add(GameMenu, "command", "-label {Crafting}");
+      Add(GameMenu, "command", "-label {Last messages}");
+      Add(GameMenu, "command", "-label {List of known bases}");
+      Add(GameMenu, "command", "-label {List of known events}");
+      Add(GameMenu, "command", "-label {Accepted missions}");
+      Add(GameMenu, "command", "-label {Stories}");
+      Add(GameMenu, "command", "-label {Wait orders}");
+      Add(GameMenu, "command", "-label {Game statistics}");
+      Add(GameMenu, "command", "-label {Help}");
+      Add(GameMenu, "command", "-label {Game options}");
+      Add(GameMenu, "command", "-label {Quit from game}");
+      Add(GameMenu, "command", "-label {Resign from game}");
+   end CreateGameMenu;
+
+   procedure CreateGameUI is
+   begin
+      GameMenu.Interp := Get_Context;
+      GameMenu.Name := New_String(".gamemenu");
+      if Winfo_Get(GameMenu, "exists") = "0" then
+         Tcl_EvalFile
+           (Get_Context,
+            To_String(DataDirectory) & "ui" & Dir_Separator & "game.tcl");
+      end if;
+      CreateGameMenu;
       for I in MenuAccelerators'Range loop
          Entry_Configure
-           (Menu, Positive'Image(I),
+           (GameMenu, Positive'Image(I),
             "-accelerator {" & To_String(MenuAccelerators(I)) & "}");
          Bind_To_Main_Window
            (Get_Context, "<" & To_String(MenuAccelerators(I)) & ">",
