293f580972f5a6e8b9212ea6de9a7b204bb3d494
thindil@laeran.pl <thindil@laeran.pl>
2020-06-13 04:35:24 +0000

    updated game stats UI
    FossilOrigin-Name: d43ffbb0bbb025dfb5e6da4c110a02db77b2f689e052a25501915dea07331376

--- bin/data/ui/stats.tcl
+++ bin/data/ui/stats.tcl
@@ -1,25 +1,34 @@
 ttk::frame .paned.statsframe
-grid [ttk::frame .paned.statsframe.left] -sticky nwes
-grid [ttk::label .paned.statsframe.left.stats]
-grid [ttk::label .paned.statsframe.left.crafts]
-grid [ttk::treeview .paned.statsframe.left.craftsview -show headings -columns [list name amount] -selectmode none]
-.paned.statsframe.left.craftsview heading name -text {Name}
-.paned.statsframe.left.craftsview heading amount -text {Amount}
-grid [ttk::label .paned.statsframe.left.missions]
-grid [ttk::treeview .paned.statsframe.left.missionsview -show headings -columns [list name amount] -selectmode none]
-.paned.statsframe.left.missionsview heading name -text {Name}
-.paned.statsframe.left.missionsview heading amount -text {Amount}
-grid [ttk::button .paned.statsframe.left.goal -text {Goals}]
-grid [ttk::label .paned.statsframe.left.goals]
-grid [ttk::treeview .paned.statsframe.left.goalsview -show headings -columns [list name amount] -selectmode none]
-.paned.statsframe.left.goalsview heading name -text {Name}
-.paned.statsframe.left.goalsview heading amount -text {Amount}
-grid [ttk::frame .paned.statsframe.right] -row 0 -column 1 -sticky nwes
-grid [ttk::label .paned.statsframe.right.destroyed]
-grid [ttk::treeview .paned.statsframe.right.destroyedview -show headings -columns [list name amount] -selectmode none]
-.paned.statsframe.right.destroyedview heading name -text {Name}
-.paned.statsframe.right.destroyedview heading amount -text {Amount}
-grid [ttk::label .paned.statsframe.right.killed]
-grid [ttk::treeview .paned.statsframe.right.killedview -show headings -columns [list name amount] -selectmode none]
-.paned.statsframe.right.killedview heading name -text {Name}
-.paned.statsframe.right.killedview heading amount -text {Amount}
+set statscanvas [canvas .paned.statsframe.canvas -yscrollcommand [list .paned.statsframe.scrolly set] -xscrollcommand [list .paned.statsframe.scrollx set]]
+grid $statscanvas -sticky nwes
+grid [ttk::scrollbar .paned.statsframe.scrollx -orient horizontal -command [list $statscanvas xview]] -row 1 -column 0 -columnspan 2 -sticky we
+grid [ttk::scrollbar .paned.statsframe.scrolly -orient vertical -command [list $statscanvas yview]] -row 0 -column 1 -sticky ns
+set statsframe [ttk::frame $statscanvas.stats]
+grid [ttk::frame $statsframe.left] -sticky nwes
+grid [ttk::label $statsframe.left.stats]
+grid [ttk::label $statsframe.left.crafts]
+grid [ttk::treeview $statsframe.left.craftsview -show headings -columns [list name amount] -selectmode none]
+$statsframe.left.craftsview heading name -text {Name}
+$statsframe.left.craftsview heading amount -text {Amount}
+grid [ttk::label $statsframe.left.missions]
+grid [ttk::treeview $statsframe.left.missionsview -show headings -columns [list name amount] -selectmode none]
+$statsframe.left.missionsview heading name -text {Name}
+$statsframe.left.missionsview heading amount -text {Amount}
+grid [ttk::button $statsframe.left.goal -text {Goals}]
+grid [ttk::label $statsframe.left.goals]
+grid [ttk::treeview $statsframe.left.goalsview -show headings -columns [list name amount] -selectmode none]
+$statsframe.left.goalsview heading name -text {Name}
+$statsframe.left.goalsview heading amount -text {Amount}
+grid [ttk::frame $statsframe.right] -row 0 -column 1 -sticky nwes
+grid [ttk::label $statsframe.right.destroyed]
+grid [ttk::treeview $statsframe.right.destroyedview -show headings -columns [list name amount] -selectmode none]
+$statsframe.right.destroyedview heading name -text {Name}
+$statsframe.right.destroyedview heading amount -text {Amount}
+grid [ttk::label $statsframe.right.killed]
+grid [ttk::treeview $statsframe.right.killedview -show headings -columns [list name amount] -selectmode none]
+$statsframe.right.killedview heading name -text {Name}
+$statsframe.right.killedview heading amount -text {Amount}
+$statscanvas create window [expr [winfo reqwidth $statsframe] / 2] [expr [winfo reqheight $statsframe] / 2] -window $statsframe
+grid columnconfigure .paned.statsframe 0 -weight 1
+# Move line below to Ada (need to get position of paned sash)
+$statscanvas configure -width [winfo reqwidth $statsframe] -height [winfo reqheight $statsframe] -scrollregion [$statscanvas bbox all]
