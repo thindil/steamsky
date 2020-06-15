cc4ddeb37f70d4b4915b9953d88c0bf270783adc
thindil@laeran.pl <thindil@laeran.pl>
2020-06-15 05:04:12 +0000

    started work on showing known events UI
    FossilOrigin-Name: 1cd8de30a7924473f3edd42ce452362b4455b4d9340c60b95fef4b052843d5fa

---
+++ bin/data/ui/events.tcl
@@ -0,0 +1,14 @@
+ttk::frame .paned.eventsframe
+set eventscanvas [canvas .paned.eventsframe.canvas -yscrollcommand [list .paned.eventsframe.scrolly set] -xscrollcommand [list .paned.eventsframe.scrollx set]]
+grid $eventscanvas -sticky nwes
+grid [ttk::scrollbar .paned.eventsframe.scrollx -orient horizontal -command [list $eventscanvas xview]] -row 1 -column 0 -columnspan 2 -sticky we
+grid [ttk::scrollbar .paned.eventsframe.scrolly -orient vertical -command [list $eventscanvas yview]] -row 0 -column 1 -sticky ns
+set eventsframe [ttk::frame $eventscanvas.events]
+grid [ttk::treeview $eventsframe.eventsview -show headings -columns [list name distance]]
+$eventsframe.eventsview heading name -text {Name}
+$eventsframe.eventsview heading distance -text {Distance}
+grid [ttk::frame $eventsframe.info] -column 1 -row 0
+grid [ttk::labelframe $eventsframe.info.info -text {Event Info:}]
+grid [ttk::label $eventsframe.info.info.label]
+grid [ttk::button $eventsframe.info.show -text {Show event on map}]
+grid [ttk::button $eventsframe.info.set -text {Set event as destination for ship}]
