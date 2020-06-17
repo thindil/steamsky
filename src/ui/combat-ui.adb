f268c1cd831a00c0776e0c6fa2a3f4010e49b3d0
thindil@laeran.pl <thindil@laeran.pl>
2020-06-17 04:58:35 +0000

    continue work on showing combat UI
    FossilOrigin-Name: 7b4d23b6096bbeb4dec657401616ad1287ab7da033ebc068b088c7b97ae089de

--- src/ui/combat-ui.adb
+++ src/ui/combat-ui.adb
@@ -15,10 +15,14 @@
 
 with Interfaces.C.Strings; use Interfaces.C.Strings;
 with GNAT.Directory_Operations; use GNAT.Directory_Operations;
+with GNAT.String_Split; use GNAT.String_Split;
 with Tcl.Ada; use Tcl.Ada;
 with Tcl.Tk.Ada; use Tcl.Tk.Ada;
+with Tcl.Tk.Ada.Grid;
 with Tcl.Tk.Ada.Widgets; use Tcl.Tk.Ada.Widgets;
 with Tcl.Tk.Ada.Widgets.Canvas; use Tcl.Tk.Ada.Widgets.Canvas;
+with Tcl.Tk.Ada.Widgets.TtkEntry.TtkComboBox;
+use Tcl.Tk.Ada.Widgets.TtkEntry.TtkComboBox;
 with Tcl.Tk.Ada.Widgets.TtkFrame; use Tcl.Tk.Ada.Widgets.TtkFrame;
 with Tcl.Tk.Ada.Widgets.TtkLabel; use Tcl.Tk.Ada.Widgets.TtkLabel;
 with Tcl.Tk.Ada.Widgets.TtkPanedWindow; use Tcl.Tk.Ada.Widgets.TtkPanedWindow;
@@ -26,6 +30,46 @@ with Tcl.Tk.Ada.Winfo; use Tcl.Tk.Ada.Winfo;
 with Utils.UI; use Utils.UI;
 
 package body Combat.UI is
+
+   -- ****if* CUI/UpdateCombatUI
+   -- FUNCTION
+   -- Update information about combat: remove old UI and create new elements
+   -- SOURCE
+   procedure UpdateCombatUI is
+      -- ****
+      Tokens: Slice_Set;
+      Frame, Item: Ttk_Frame;
+      Rows: Positive;
+      Label: Ttk_Label;
+      CrewList: Unbounded_String;
+      ComboBox: Ttk_ComboBox;
+   begin
+      Frame.Interp := Get_Context;
+      Item.Interp := Get_Context;
+      Frame.Name := New_String(".paned.combatframe.canvas.combat.left");
+      Create(Tokens, Tcl.Tk.Ada.Grid.Grid_Size(Frame), " ");
+      Rows := Positive'Value(Slice(Tokens, 2));
+      for I in 1 .. Rows loop
+         Create
+           (Tokens,
+            Tcl.Tk.Ada.Grid.Grid_Slaves(Frame, "-row" & Positive'Image(I)),
+            " ");
+         for J in 1 .. Slice_Count(Tokens) loop
+            Item.Name := New_String(Slice(Tokens, J));
+            Tcl.Tk.Ada.Grid.Grid_Remove(Item);
+         end loop;
+      end loop;
+      for Member of PlayerShip.Crew loop
+         Append(CrewList, " " & Member.Name);
+      end loop;
+      Label := Create(Widget_Image(Frame) & ".pilotlabel", "-text {Pilot:}");
+      Tcl.Tk.Ada.Grid.Grid(Label, "-row 1");
+      ComboBox :=
+        Create
+          (Widget_Image(Frame) & ".pilotcrew",
+           "-values [list" & To_String(CrewList) & "]");
+      Tcl.Tk.Ada.Grid.Grid(ComboBox, "-row 1 -column 1");
+   end UpdateCombatUI;
 
    procedure ShowCombatUI is
       Label: Ttk_Label;
@@ -48,6 +92,7 @@ package body Combat.UI is
             To_String(DataDirectory) & "ui" & Dir_Separator & "combat.tcl");
          Bind(CombatFrame, "<Configure>", "{ResizeCanvas %W.canvas %w %h}");
       end if;
+      UpdateCombatUI;
       configure
         (CombatCanvas,
          "-height [expr " & SashPos(Paned, "0") & " - 20] -width " &
