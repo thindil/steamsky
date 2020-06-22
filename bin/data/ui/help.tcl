8006e39633482590317022bf519751ff2dd42163
thindil@laeran.pl <thindil@laeran.pl>
2020-06-22 06:09:53 +0000

    change help UI to toplevel
    FossilOrigin-Name: 0943d808160e866eb74b6dfd3607177619cfd9f2921d3804b114deb419e081f2

--- bin/data/ui/help.tcl
+++ bin/data/ui/help.tcl
@@ -1,10 +1,13 @@
-grid [ttk::panedwindow .help] -sticky nwes
-.help add [ttk::treeview .help.topics -show tree]
-.help add [ttk::frame .help.content]
-pack [ttk::scrollbar .help.content.scroll -orient vertical -command [list .help.content.view yview]] -side right -fill y
-set helpview [text .help.content.view -wrap char -yscrollcommand [list .help.content.scroll set] -font {-family Roboto -size 14}]
+toplevel .help
+wm title .help {Steam Sky - Help}
+grid [ttk::panedwindow .help.paned]
+.help.paned add [ttk::treeview .help.paned.topics -show tree]
+.help.paned add [ttk::frame .help.paned.content]
+pack [ttk::scrollbar .help.paned.content.scroll -orient vertical -command [list .help.paned.content.view yview]] -side right -fill y
+set helpview [text .help.paned.content.view -wrap char -yscrollcommand [list .help.paned.content.scroll set] -font {-family Roboto -size 14}]
 $helpview tag configure special -foreground yellow -font {-family Roboto -size 14 -weight bold}
 $helpview tag configure bold -font {-family Roboto -size 14 -weight bold}
 $helpview tag configure underline -font {-family Roboto -size 14 -underline true}
 $helpview tag configure italic -font {-family Roboto -size 14 -slant italic}
 pack $helpview -side top -fill both
+bind .help <Escape> {destroy .help}
