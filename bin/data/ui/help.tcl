1c29254eff93fd91e02673020b910fd64a8d40de
thindil@laeran.pl <thindil@laeran.pl>
2020-06-24 04:24:13 +0000

    added scrollbar for topics list
    FossilOrigin-Name: e691edff595b3db0c4b81b5d99b57ea7bdc7a8d0bc4795b344b51ca6452da49a

--- bin/data/ui/help.tcl
+++ bin/data/ui/help.tcl
@@ -5,10 +5,12 @@ if {$tcl_platform(os) == "Linux"} {
    wm attributes .help -type dialog
 }
 grid [ttk::panedwindow .help.paned]
-.help.paned add [ttk::treeview .help.paned.topics -show tree]
+.help.paned add [ttk::frame .help.paned.topics]
+pack [ttk::scrollbar .help.paned.topics.scroll -orient vertical -command [list .help.paned.topics.view yview]] -side right -fill y
+pack [ttk::treeview .help.paned.topics.view -show tree -yscrollcommand [list .help.paned.topics.scroll set]] -side top -fill both
 .help.paned add [ttk::frame .help.paned.content]
 pack [ttk::scrollbar .help.paned.content.scroll -orient vertical -command [list .help.paned.content.view yview]] -side right -fill y
-set helpview [text .help.paned.content.view -wrap char -yscrollcommand [list .help.paned.content.scroll set] -font {-family Roboto -size 14}]
+set helpview [text .help.paned.content.view -wrap word -yscrollcommand [list .help.paned.content.scroll set] -font {-family Roboto -size 14} -width 70]
 $helpview tag configure special -foreground yellow -font {-family Roboto -size 14 -weight bold}
 $helpview tag configure bold -font {-family Roboto -size 14 -weight bold}
 $helpview tag configure underline -font {-family Roboto -size 14 -underline true}
