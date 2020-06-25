04ddebd7875c12c9c48949266b58a529719e42ef
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-06-25 05:22:55 +0000

    started work on showing module info
    FossilOrigin-Name: a1234f290f2fdd7babd862f62c8e10cd183d2a435947d6a0c427dc5b728eb553

--- bin/data/ui/shipinfo.tcl
+++ bin/data/ui/shipinfo.tcl
@@ -9,9 +9,10 @@ grid [ttk::label $shipinfoframe.left.namelbl -text {Name:}]
 grid [ttk::entry $shipinfoframe.left.name] -column 1 -row 0
 bind $shipinfoframe.left.name <Enter> SetShipName
 ttk::label $shipinfoframe.left.upgradelabel
-ttk::progressbar $shipinfoframe.left.upgrade -orient horizontal
+ttk::progressbar $shipinfoframe.left.upgrade -orient horizontal -maximum 1.0
 grid [ttk::label $shipinfoframe.left.info] -row 2 -columnspan 2
 grid [ttk::treeview $shipinfoframe.left.modules -show tree] -row 3 -columnspan 2
+bind $shipinfoframe.left.modules <<TreeviewSelect>> ShowModuleInfo
 grid [ttk::frame $shipinfoframe.right] -column 1 -row 0 -sticky nwes
 grid [ttk::labelframe $shipinfoframe.right.crew -text {Crew Info:}]
 grid [ttk::label $shipinfoframe.right.crew.name -text {Name}]
@@ -23,14 +24,14 @@ grid [ttk::label $shipinfoframe.right.crew.hunter -text {Hunger}] -column 5 -row
 grid [ttk::label $shipinfoframe.right.crew.morale -text {Morale}] -column 6 -row 0
 grid [ttk::labelframe $shipinfoframe.right.module -text {Module Info:}]
 ttk::label $shipinfoframe.right.module.damagelbl -text {Damage:}
-ttk::progressbar $shipinfoframe.right.module.damage
+ttk::progressbar $shipinfoframe.right.module.damage -orient horizontal -maximum 1.0
 grid [ttk::label $shipinfoframe.right.module.info] -row 1
 ttk::label $shipinfoframe.right.module.cleanlbl -text {Clean:}
-ttk::progressbar $shipinfoframe.right.module.clean
+ttk::progressbar $shipinfoframe.right.module.clean -orient horizontal -maximum 1.0
 ttk::label $shipinfoframe.right.module.qualitylbl -text {Quality:}
-ttk::progressbar $shipinfoframe.right.module.quality
+ttk::progressbar $shipinfoframe.right.module.quality -orient horizontal -maximum 1.0
 ttk::label $shipinfoframe.right.module.upgradelbl -text {Upgrade:}
-ttk::progressbar $shipinfoframe.right.module.upgrade
+ttk::progressbar $shipinfoframe.right.module.upgrade -orient horizontal -maximum 1.0
 grid [ttk::labelframe $shipinfoframe.right.options -text {Module Options:}]
 ttk::button $shipinfoframe.right.options.durability -text {Upgrade durability}
 ttk::button $shipinfoframe.right.options.durability1 -text {Upgrade1}
