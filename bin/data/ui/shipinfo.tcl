93e194329f508ead4fb054a23ff8e9689f0de9ff
thindil@laeran.pl <thindil@laeran.pl>
2020-06-26 05:21:50 +0000

    started work on showing module options
    FossilOrigin-Name: 21ff6a189d8ec70db7e095ca226400ab0db219b3c6b6fc4903f03d6a3fe19953

--- bin/data/ui/shipinfo.tcl
+++ bin/data/ui/shipinfo.tcl
@@ -3,17 +3,23 @@ set shipinfocanvas [canvas .paned.shipinfoframe.canvas -yscrollcommand [list .pa
 grid $shipinfocanvas -sticky nwes
 grid [ttk::scrollbar .paned.shipinfoframe.scrollx -orient horizontal -command [list $shipinfocanvas xview]] -row 1 -column 0 -columnspan 2 -sticky we
 grid [ttk::scrollbar .paned.shipinfoframe.scrolly -orient vertical -command [list $shipinfocanvas yview]] -row 0 -column 1 -sticky ns
+# General ship info
 set shipinfoframe [ttk::frame $shipinfocanvas.shipinfo]
 grid [ttk::frame $shipinfoframe.left] -sticky nwes
+# Ship name
 grid [ttk::label $shipinfoframe.left.namelbl -text {Name:}]
 grid [ttk::entry $shipinfoframe.left.name] -column 1 -row 0
 bind $shipinfoframe.left.name <Enter> SetShipName
+# Upgrade progress
 ttk::label $shipinfoframe.left.upgradelabel
 ttk::progressbar $shipinfoframe.left.upgrade -orient horizontal -maximum 1.0
+# Ship info
 grid [ttk::label $shipinfoframe.left.info] -row 2 -columnspan 2
+# Ship modules
 grid [ttk::treeview $shipinfoframe.left.modules -show tree] -row 3 -columnspan 2
 bind $shipinfoframe.left.modules <<TreeviewSelect>> ShowModuleInfo
 grid [ttk::frame $shipinfoframe.right] -column 1 -row 0 -sticky nwes
+# Crew info
 grid [ttk::labelframe $shipinfoframe.right.crew -text {Crew Info:}]
 grid [ttk::label $shipinfoframe.right.crew.name -text {Name}]
 grid [ttk::label $shipinfoframe.right.crew.order -text {Order}] -column 1 -row 0
@@ -22,6 +28,7 @@ grid [ttk::label $shipinfoframe.right.crew.fatigue -text {Fatigue}] -column 3 -r
 grid [ttk::label $shipinfoframe.right.crew.thirst -text {Thirst}] -column 4 -row 0
 grid [ttk::label $shipinfoframe.right.crew.hunter -text {Hunger}] -column 5 -row 0
 grid [ttk::label $shipinfoframe.right.crew.morale -text {Morale}] -column 6 -row 0
+# Detailed info about the selected ship's module
 grid [ttk::labelframe $shipinfoframe.right.module -text {Module Info:}]
 ttk::label $shipinfoframe.right.module.damagelbl -text {Damage:}
 ttk::progressbar $shipinfoframe.right.module.damage -orient horizontal -maximum 1.0
@@ -34,6 +41,7 @@ ttk::progressbar $shipinfoframe.right.module.upgrade -orient horizontal -maximum
 set moduleinfo [text $shipinfoframe.right.module.info -wrap char -height 10 -width 40]
 $moduleinfo tag configure red -foreground red
 grid $moduleinfo -row 4 -columnspan 2
+# Buttons with ship's module actions
 grid [ttk::labelframe $shipinfoframe.right.options -text {Module Options:}]
 ttk::button $shipinfoframe.right.options.durability -text {Upgrade durability}
 ttk::button $shipinfoframe.right.options.durability1 -text {Upgrade1}
