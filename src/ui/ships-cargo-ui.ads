1d3ab3b7920f5d6d6e68d98d69a535e7e9727269
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-06-29 04:34:24 +0000

    started work on player ship cargo UI
    FossilOrigin-Name: 9d84595e5b7f0178457831b6127fb12d1ac7cdafd152ce8270f33012df357f77

---
+++ src/ui/ships-cargo-ui.ads
@@ -0,0 +1,30 @@
+-- Copyright (c) 2020 Bartek thindil Jasicki <thindil@laeran.pl>
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+-- ****h* Cargo/CUI2
+-- FUNCTION
+-- Provide code to show information about the player's ship to the player
+-- SOURCE
+package Ships.Cargo.UI is
+-- ****
+
+   -- ****f* CUI2/AddCommands
+   -- FUNCTION
+   -- Add Tcl commands related to the player's ship cargo
+   -- SOURCE
+   procedure AddCommands;
+   -- ****
+
+end Ships.Cargo.UI;
