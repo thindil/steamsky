67d2a0befe45470b284a1824098439eb8ec953a5
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-07-02 04:40:00 +0000

    started work on crew UI
    FossilOrigin-Name: 92697ac9eace02992b7d28e3d9ce10eaa93576164361035adf8bae0d59172a2c

---
+++ bin/data/ui/crew.tcl
@@ -0,0 +1,31 @@
+ttk::frame .paned.crewframe
+set crewcanvas [canvas .paned.crewframe.canvas -yscrollcommand [list .paned.crewframe.scrolly set] -xscrollcommand [list .paned.crewframe.scrollx set]]
+grid $crewcanvas -sticky nwes
+grid [ttk::scrollbar .paned.crewframe.scrollx -orient horizontal -command [list $crewcanvas xview]] -row 1 -column 0 -columnspan 2 -sticky we
+grid [ttk::scrollbar .paned.crewframe.scrolly -orient vertical -command [list $crewcanvas yview]] -row 0 -column 1 -sticky ns
+set crewframe [ttk::frame $crewcanvas.crew]
+# Crew list
+grid [ttk::frame $crewframe.crew] -sticky nwes
+grid [ttk::label $crewframe.crew.name -text {Name}]
+grid [ttk::label $crewframe.crew.order -text {Order}] -row 0 -column 1
+# Crew member info
+grid [ttk::frame $crewframe.info] -sticky nwes -row 0 -column 1
+grid [ttk::labelframe $crewframe.info.info -text {Member Info}]
+grid [ttk::progressbar $crewframe.info.info.health]
+grid [ttk::progressbar $crewframe.info.info.tired]
+grid [ttk::progressbar $crewframe.info.info.thirst]
+grid [ttk::progressbar $crewframe.info.info.hunger]
+grid [ttk::progressbar $crewframe.info.info.morale]
+grid [ttk::label $crewframe.info.info.statslbl -text Statistics]
+grid [ttk::frame $crewframe.info.info.stats]
+grid [ttk::label $crewframe.info.info.skillslbl -text Skills]
+grid [ttk::frame $crewframe.info.info.skills]
+# Crew member priorities
+grid [ttk::labelframe $crewframe.info.priorities -text {Orders priorities}]
+grid [ttk::label $crewframe.info.priorities.name -text {Priority}]
+grid [ttk::label $crewframe.info.priorities.level -text {Level}] -row 0 -column 1
+# Options
+grid [ttk::button $crewframe.info.inventory -text {Inventory}]
+grid [ttk::button $crewframe.info.dismiss -text {Dismiss}]
+grid [ttk::button $crewframe.info.clean -text {Clean ship everyone}]
+grid [ttk::button $crewframe.info.repair -text {Repair ship everyone}]
