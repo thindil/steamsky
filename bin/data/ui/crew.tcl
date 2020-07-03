422c87ab0da11b913bfb59a6faf6114b0c8880c6
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-07-03 05:15:13 +0000

    finished work on setting priorities
    FossilOrigin-Name: 3590f79b87b509228a797da271a7d187d10b1f00cc9fd4e4a8c15e14f2f41b51

--- bin/data/ui/crew.tcl
+++ bin/data/ui/crew.tcl
@@ -33,37 +33,37 @@ grid [ttk::combobox $crewframe.info.priorities.level1 -values [list None Normal 
 bind $crewframe.info.priorities.level1 <<ComboboxSelected>> {SetPriority 1 [$crewframe.info.priorities.level1 current]}
 grid [ttk::label $crewframe.info.priorities.name2 -text {Engineering}]
 grid [ttk::combobox $crewframe.info.priorities.level2 -values [list None Normal Highest] -state readonly] -row 2 -column 1
-bind $crewframe.info.priorities.level1 <<ComboboxSelected>> {SetPriority 2 [$crewframe.info.priorities.level2 current]}
+bind $crewframe.info.priorities.level2 <<ComboboxSelected>> {SetPriority 2 [$crewframe.info.priorities.level2 current]}
 grid [ttk::label $crewframe.info.priorities.name3 -text {Operating guns}]
 grid [ttk::combobox $crewframe.info.priorities.level3 -values [list None Normal Highest] -state readonly] -row 3 -column 1
-bind $crewframe.info.priorities.level1 <<ComboboxSelected>> {SetPriority 3 [$crewframe.info.priorities.level3 current]}
+bind $crewframe.info.priorities.level3 <<ComboboxSelected>> {SetPriority 3 [$crewframe.info.priorities.level3 current]}
 grid [ttk::label $crewframe.info.priorities.name4 -text {Repair ship}]
 grid [ttk::combobox $crewframe.info.priorities.level4 -values [list None Normal Highest] -state readonly] -row 4 -column 1
-bind $crewframe.info.priorities.level1 <<ComboboxSelected>> {SetPriority 4 [$crewframe.info.priorities.level4 current]}
+bind $crewframe.info.priorities.level4 <<ComboboxSelected>> {SetPriority 4 [$crewframe.info.priorities.level4 current]}
 grid [ttk::label $crewframe.info.priorities.name5 -text {Manufacturing}]
 grid [ttk::combobox $crewframe.info.priorities.level5 -values [list None Normal Highest] -state readonly] -row 5 -column 1
-bind $crewframe.info.priorities.level1 <<ComboboxSelected>> {SetPriority 5 [$crewframe.info.priorities.level5 current]}
+bind $crewframe.info.priorities.level5 <<ComboboxSelected>> {SetPriority 5 [$crewframe.info.priorities.level5 current]}
 grid [ttk::label $crewframe.info.priorities.name6 -text {Upgrading ship}]
 grid [ttk::combobox $crewframe.info.priorities.level6 -values [list None Normal Highest] -state readonly] -row 6 -column 1
-bind $crewframe.info.priorities.level1 <<ComboboxSelected>> {SetPriority 6 [$crewframe.info.priorities.level6 current]}
+bind $crewframe.info.priorities.level6 <<ComboboxSelected>> {SetPriority 6 [$crewframe.info.priorities.level6 current]}
 grid [ttk::label $crewframe.info.priorities.name7 -text {Talking in bases}]
 grid [ttk::combobox $crewframe.info.priorities.level7 -values [list None Normal Highest] -state readonly] -row 7 -column 1
-bind $crewframe.info.priorities.level1 <<ComboboxSelected>> {SetPriority 7 [$crewframe.info.priorities.level7 current]}
+bind $crewframe.info.priorities.level7 <<ComboboxSelected>> {SetPriority 7 [$crewframe.info.priorities.level7 current]}
 grid [ttk::label $crewframe.info.priorities.name8 -text {Healing wounded}]
 grid [ttk::combobox $crewframe.info.priorities.level8 -values [list None Normal Highest] -state readonly] -row 8 -column 1
-bind $crewframe.info.priorities.level1 <<ComboboxSelected>> {SetPriority 8 [$crewframe.info.priorities.level8 current]}
+bind $crewframe.info.priorities.level8 <<ComboboxSelected>> {SetPriority 8 [$crewframe.info.priorities.level8 current]}
 grid [ttk::label $crewframe.info.priorities.name9 -text {Cleaning ship}]
 grid [ttk::combobox $crewframe.info.priorities.level9 -values [list None Normal Highest] -state readonly] -row 9 -column 1
-bind $crewframe.info.priorities.level1 <<ComboboxSelected>> {SetPriority 9 [$crewframe.info.priorities.level9 current]}
+bind $crewframe.info.priorities.level9 <<ComboboxSelected>> {SetPriority 9 [$crewframe.info.priorities.level9 current]}
 grid [ttk::label $crewframe.info.priorities.name10 -text {Defend ship}]
 grid [ttk::combobox $crewframe.info.priorities.level10 -values [list None Normal Highest] -state readonly] -row 10 -column 1
-bind $crewframe.info.priorities.level1 <<ComboboxSelected>> {SetPriority 10 [$crewframe.info.priorities.level10 current]}
+bind $crewframe.info.priorities.level10 <<ComboboxSelected>> {SetPriority 10 [$crewframe.info.priorities.level10 current]}
 grid [ttk::label $crewframe.info.priorities.name11 -text {Board enemy ship}]
 grid [ttk::combobox $crewframe.info.priorities.level11 -values [list None Normal Highest] -state readonly] -row 11 -column 1
-bind $crewframe.info.priorities.level1 <<ComboboxSelected>> {SetPriority 11 [$crewframe.info.priorities.level11 current]}
+bind $crewframe.info.priorities.level11 <<ComboboxSelected>> {SetPriority 11 [$crewframe.info.priorities.level11 current]}
 grid [ttk::label $crewframe.info.priorities.name12 -text {Train skill}]
 grid [ttk::combobox $crewframe.info.priorities.level12 -values [list None Normal Highest] -state readonly] -row 12 -column 1
-bind $crewframe.info.priorities.level1 <<ComboboxSelected>> {SetPriority 12 [$crewframe.info.priorities.level12 current]}
+bind $crewframe.info.priorities.level12 <<ComboboxSelected>> {SetPriority 12 [$crewframe.info.priorities.level12 current]}
 # Options
 grid [ttk::button $crewframe.info.inventory -text {Inventory}]
 grid [ttk::button $crewframe.info.dismiss -text {Dismiss}]
