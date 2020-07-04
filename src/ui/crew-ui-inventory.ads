6b4b1da2a59119198f2ec1089366bdacd5d5f60d
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-07-04 04:30:47 +0000

    started work on a crew member inventory UI
    FossilOrigin-Name: c09d40ccf6c5b422df0d32b5ce1c0275a8f0fc4768fc900e89a24816b0a9c79f

---
+++ src/ui/crew-ui-inventory.ads
@@ -0,0 +1,30 @@
+-- Copyright (c) 2020 Bartek thindil Jasicki <thindil@laeran.pl>
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+-- ****h* CUI3/Inventory
+-- FUNCTION
+-- Provide code to show information about the selected crew member inventory
+-- SOURCE
+package Crew.UI.Inventory is
+-- ****
+
+   -- ****f* Inventory/AddCommands
+   -- FUNCTION
+   -- Add Tcl commands related to the crew member's inventory
+   -- SOURCE
+   procedure AddCommands;
+   -- ****
+
+end Crew.UI.Inventory;
