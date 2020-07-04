651e04fa336c432bdfdae91d4159971e6446a76f
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-07-04 05:49:51 +0000

    added moving items to ship cargo
    FossilOrigin-Name: e312101460cf6aa250c9a9501fb48e2086a5f48f491d240d733d724551adc3a0

--- bin/data/ui/inventory.tcl
+++ bin/data/ui/inventory.tcl
@@ -21,7 +21,7 @@ set itemframe [ttk::frame $inventoryframe.item]
 grid [ttk::labelframe $itemframe.info -text {Item Info:}] -columnspan 3
 grid [text $itemframe.info.text -wrap char -height 10 -width 40]
 # Item actions
-grid [ttk::checkbutton $itemframe.use -variable useitem -text {Used} -command {SetUseItem}] -columnspan 3
-grid [ttk::button $itemframe.button -text {Move}]
+grid [ttk::checkbutton $itemframe.use -variable useitem -text Used -command SetUseItem] -columnspan 3
+grid [ttk::button $itemframe.button -text Move -command MoveItem]
 grid [ttk::spinbox $itemframe.amount -from 1 -increment 1 -validate key] -row 2 -column 1
 grid [ttk::label $itemframe.label -text {to ship cargo.}] -row 2 -column 2
