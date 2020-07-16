4a9c42614822a522162b8b9965c7cfd91b166a69
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-07-16 05:34:03 +0000

    added settings for fonts
    FossilOrigin-Name: 136aae4c917d5b4e2b12bcd1f3f892b299587cfb96180c7d7f1b4b3d095850e2

--- bin/data/ui/help.tcl
+++ bin/data/ui/help.tcl
@@ -10,7 +10,7 @@ pack [ttk::scrollbar .help.paned.topics.scroll -orient vertical -command [list .
 pack [ttk::treeview .help.paned.topics.view -show tree -yscrollcommand [list .help.paned.topics.scroll set]] -side top -fill both
 .help.paned add [ttk::frame .help.paned.content]
 pack [ttk::scrollbar .help.paned.content.scroll -orient vertical -command [list .help.paned.content.view yview]] -side right -fill y
-set helpview [text .help.paned.content.view -wrap word -yscrollcommand [list .help.paned.content.scroll set] -font {-family Roboto -size 14} -width 70]
+set helpview [text .help.paned.content.view -wrap word -yscrollcommand [list .help.paned.content.scroll set] -font HelpFont -width 70]
 $helpview tag configure special -foreground yellow -font {-family Roboto -size 14 -weight bold}
 $helpview tag configure bold -font {-family Roboto -size 14 -weight bold}
 $helpview tag configure underline -font {-family Roboto -size 14 -underline true}
