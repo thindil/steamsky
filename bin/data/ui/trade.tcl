745a7ae638b4b0d60a5f74100b8c420ebd1aaee9
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-07-18 05:36:09 +0000

    added UI for trade
    FossilOrigin-Name: e2c9f1e23825d2d5b7c8fd5d1ff1a1dcf73625b48bec39444b1d1bf47bd1fb92

---
+++ bin/data/ui/trade.tcl
@@ -0,0 +1,45 @@
+ttk::frame .paned.tradeframe
+set tradecanvas [canvas .paned.tradeframe.canvas -yscrollcommand [list .paned.tradeframe.scrolly set] -xscrollcommand [list .paned.tradeframe.scrollx set]]
+grid $tradecanvas -sticky nwes
+grid [ttk::scrollbar .paned.tradeframe.scrollx -orient horizontal -command [list $tradecanvas xview]] -row 1 -column 0 -columnspan 2 -sticky we
+grid [ttk::scrollbar .paned.tradeframe.scrolly -orient vertical -command [list $tradecanvas yview]] -row 0 -column 1 -sticky ns
+set tradeframe [ttk::frame $tradecanvas.trade]
+# Type of items to show
+grid [ttk::frame $tradeframe.options]
+grid [ttk::label $tradeframe.options.typelabel -text {Type:}]
+grid [ttk::combobox $tradeframe.options.type -state readonly] -column 1 -row 0
+grid [ttk::entry $tradeframe.options.search -validate key] -column 2 -row 0
+# Trade list
+grid [ttk::frame $tradeframe.trade] -sticky nwes
+set tradeview [ttk::treeview $tradeframe.trade.view -columns [list name type durability price profit owned available] -show headings -yscrollcommand [list $tradeframe.trade.scrolly set]]
+$tradeview heading name -text {Name}
+$tradeview heading type -text {Type}
+$tradeview heading durability -text {Durability}
+$tradeview heading price -text {Price}
+$tradeview heading profit -text {Profit}
+$tradeview heading owned -text {Owned}
+$tradeview heading available -text {Available}
+grid $tradeview -sticky nwes
+grid [ttk::scrollbar $tradeframe.trade.scrolly -orient vertical -command [list $tradeview yview]] -row 0 -column 1 -sticky ns
+# Item info
+set itemframe [ttk::frame $tradeframe.item]
+grid [ttk::labelframe $itemframe.info -text {Item Info:}]
+grid [text $itemframe.info.text -wrap char -height 10 -width 40]
+# Item actions
+grid [ttk::label $itemframe.shipmoney]
+grid [ttk::label $itemframe.shipspace]
+grid [ttk::label $itemframe.basemoney]
+grid [ttk::frame $itemframe.buyframe]
+grid [ttk::button $itemframe.buyframe.buy -text {Buy}]
+grid [ttk::label $itemframe.buyframe.amountlbl] -column 1 -row 0
+grid [ttk::spinbox $itemframe.buyframe.amount -from 1 -validate key] -column 2 -row 0
+grid [ttk::label $itemframe.buyframe.orlbl -text {or}] -column 3 -row 0
+grid [ttk::button $itemframe.buyframe.buymax -text {Buy max}] -column 4 -row 0
+grid [ttk::frame $itemframe.sellframe]
+grid [ttk::button $itemframe.sellframe.sell -text {Sell}]
+grid [ttk::label $itemframe.sellframe.amountlbl] -column 1 -row 0
+grid [ttk::spinbox $itemframe.sellframe.amount -from 1 -validate key] -column 2 -row 0
+grid [ttk::label $itemframe.sellframe.orlbl -text {or}] -column 3 -row 0
+grid [ttk::button $itemframe.sellframe.buymax -text {Sell all}] -column 4 -row 0
+grid [ttk::label $itemframe.sellframe.error -style Headerred.TLabel] -columnspan 4
+grid $itemframe -row 1 -column 1 -sticky nwes
