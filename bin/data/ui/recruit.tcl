6b66ec700baf9ab1d91d68595414fad2b5992708
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-07-22 05:25:28 +0000

    started work on recruit UI
    FossilOrigin-Name: bcd3804ddfa6b85a53247732e022c1fadca77eddd861bb785e8531ffa7e5242e

---
+++ bin/data/ui/recruit.tcl
@@ -0,0 +1,34 @@
+# test code
+ttk::frame .paned
+set recruitframe [ttk::frame .paned.recruit]
+# normal code
+#ttk::frame .paned.recruitframe
+#set recruitcanvas [canvas .paned.recruitframe.canvas -yscrollcommand [list .paned.recruitframe.scrolly set] -xscrollcommand [list .paned.recruitframe.scrollx set]]
+#grid $recruitcanvas -sticky nwes
+#grid [ttk::scrollbar .paned.recruitframe.scrollx -orient horizontal -command [list $recruitcanvas xview]] -row 1 -column 0 -columnspan 2 -sticky we
+#grid [ttk::scrollbar .paned.recruitframe.scrolly -orient vertical -command [list $recruitcanvas yview]] -row 0 -column 1 -sticky ns
+#set recruitframe [ttk::frame $recruitcanvas.recruit]
+# Recruits list
+grid [ttk::frame $recruitframe.recruits] -sticky nwes
+set recruitview [ttk::treeview $recruitframe.recruits.view -yscrollcommand [list $recruitframe.recruits.scrolly set]]
+$recruitview heading #0 -text {Name}
+grid $recruitview -sticky nwes
+grid [ttk::scrollbar $recruitframe.recruits.scrolly -orient vertical -command [list $recruitview yview]] -row 0 -column 1 -sticky ns
+# Recruit info
+set infoframe [ttk::frame $recruitframe.recruit]
+grid [ttk::labelframe $infoframe.info -text {Recruit Info:}]
+grid [ttk::label $infoframe.info.label]
+grid [ttk::labelframe $infoframe.info.stats -text {Statistics:}]
+grid [ttk::treeview $infoframe.info.stats.view -yscrollcommand [list $infoframe.info.stats.scrolly set]] -sticky nwes
+grid [ttk::scrollbar $infoframe.info.stats.scrolly -orient vertical -command [list $infoframe.info.stats.view yview]] -row 0 -column 1 -sticky ns
+grid [ttk::labelframe $infoframe.info.skills -text {Skills:}]
+grid [ttk::treeview $infoframe.info.skills.view -yscrollcommand [list $infoframe.info.skills.scrolly set]] -sticky nwes
+grid [ttk::scrollbar $infoframe.info.skills.scrolly -orient vertical -command [list $infoframe.info.skills.view yview]] -row 0 -column 1 -sticky ns
+grid [ttk::labelframe $infoframe.info.equipment -text {Equipment:}]
+grid [ttk::treeview $infoframe.info.equipment.view -yscrollcommand [list $infoframe.info.equipment.scrolly set]] -sticky nwes
+grid [ttk::scrollbar $infoframe.info.equipment.scrolly -orient vertical -command [list $infoframe.info.equipment.view yview]] -row 0 -column 1 -sticky ns
+# Recruit actions
+grid $infoframe -column 1 -row 0
+# test code
+pack $recruitframe -fill both -expand true
+pack .paned -fill both -expand true
