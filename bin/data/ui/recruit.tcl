d65a1e181781fbd6cab76e65758835bbedc4ac57
thindil@laeran.pl <thindil@laeran.pl>
2020-07-23 05:13:18 +0000

    updated recruit info UI
    FossilOrigin-Name: d49f358141dc8d6fd08c443a5018c453336b126b56d634e495128cda192cd87f

--- bin/data/ui/recruit.tcl
+++ bin/data/ui/recruit.tcl
@@ -9,20 +9,18 @@ grid [ttk::frame $recruitframe.recruits] -sticky nwes
 set recruitview [ttk::treeview $recruitframe.recruits.view -yscrollcommand [list $recruitframe.recruits.scrolly set]]
 $recruitview heading #0 -text {Name}
 grid $recruitview -sticky nwes
+bind $recruitview <<TreeviewSelect>> ShowRecruitInfo
 grid [ttk::scrollbar $recruitframe.recruits.scrolly -orient vertical -command [list $recruitview yview]] -row 0 -column 1 -sticky ns
 # Recruit info
 set infoframe [ttk::frame $recruitframe.recruit]
 grid [ttk::labelframe $infoframe.info -text {Recruit Info:}]
-grid [ttk::label $infoframe.info.label]
+grid [ttk::label $infoframe.info.info]
 grid [ttk::labelframe $infoframe.info.stats -text {Statistics:}]
-grid [ttk::treeview $infoframe.info.stats.view -yscrollcommand [list $infoframe.info.stats.scrolly set]] -sticky nwes
-grid [ttk::scrollbar $infoframe.info.stats.scrolly -orient vertical -command [list $infoframe.info.stats.view yview]] -row 0 -column 1 -sticky ns
 grid [ttk::labelframe $infoframe.info.skills -text {Skills:}]
-grid [ttk::treeview $infoframe.info.skills.view -yscrollcommand [list $infoframe.info.skills.scrolly set]] -sticky nwes
-grid [ttk::scrollbar $infoframe.info.skills.scrolly -orient vertical -command [list $infoframe.info.skills.view yview]] -row 0 -column 1 -sticky ns
 grid [ttk::labelframe $infoframe.info.equipment -text {Equipment:}]
-grid [ttk::treeview $infoframe.info.equipment.view -yscrollcommand [list $infoframe.info.equipment.scrolly set]] -sticky nwes
+grid [ttk::treeview $infoframe.info.equipment.view -yscrollcommand [list $infoframe.info.equipment.scrolly set] -show tree] -sticky nwes
 grid [ttk::scrollbar $infoframe.info.equipment.scrolly -orient vertical -command [list $infoframe.info.equipment.view yview]] -row 0 -column 1 -sticky ns
+grid [ttk::label $infoframe.info.initialcost]
 # Recruit actions
 grid [ttk::label $infoframe.dailylbl -text {Daily payment: }]
 grid [ttk::scale $infoframe.daily]
