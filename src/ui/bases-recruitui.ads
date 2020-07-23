31655da46013cc8005d0cd2dcfb3cc0184cf0288
thindil@laeran.pl <thindil@laeran.pl>
2020-07-23 04:27:53 +0000

    added show recruit UI
    FossilOrigin-Name: 0680d6bb9c41b695b942c2821cfabd0edecb4e5c3eed12f55f41ddb410662197

---
+++ src/ui/bases-recruitui.ads
@@ -0,0 +1,30 @@
+-- Copyright (c) 2020 Bartek thindil Jasicki <thindil@laeran.pl>
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+-- ****h* Bases/RercuitUI
+-- FUNCTION
+-- Provide code to train skills in bases
+-- SOURCE
+package Bases.RecruitUI is
+-- ****
+
+   -- ****f* RecruitUI/AddCommands
+   -- FUNCTION
+   -- Add Tcl commands related to recruit new crew members in bases
+   -- SOURCE
+   procedure AddCommands;
+   -- ****
+
+end Bases.RecruitUI;
