18012383fe376350359964e9f9c62227fd6e4b7f
thindil@laeran.pl <thindil@laeran.pl>
2020-07-25 04:27:15 +0000

    fixed command for setting base as destination
    FossilOrigin-Name: 1607012c49cac55945760dd891b0202936d7300cab07616a59a8713dc89be2fe

--- src/ui/baseslist.adb
+++ src/ui/baseslist.adb
@@ -494,7 +494,7 @@ package body BasesList is
       AddCommand("ShowBases", Show_Bases_Command'Access);
       AddCommand("ShowBaseInfo", Show_Base_Info_Command'Access);
       AddCommand("ShowBase", Show_Base_Command'Access);
-      AddCommand("SetBase", Set_Base_Command'Access);
+      AddCommand("SetBase2", Set_Base_Command'Access);
    end AddCommands;
 
 end BasesList;
