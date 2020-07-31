df747853903cbef3e3618d1957609b9a365204d4
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-07-31 04:56:21 +0000

    added crew UI
    FossilOrigin-Name: 29bdd4659f912d44b24ebc48e2f586694519b778211b19147b08def41d230339

--- bin/data/ui/debug.tcl
+++ bin/data/ui/debug.tcl
@@ -7,9 +7,8 @@ if {$tcl_platform(os) == "Linux"} {
 grid [ttk::frame .debugdialog.buttons]
 grid [ttk::frame .debugdialog.main] -column 1 -row 0
 proc ShowFrame {framename} {
-   global mainframe
    grid remove [grid slaves .debugdialog.main]
-   grid .debugdialog.main
+   grid $framename
 }
 grid [ttk::button .debugdialog.buttons.ship -text Ship -command {ShowFrame .debugdialog.main.ship}]
 grid [ttk::button .debugdialog.buttons.crew -text Crew -command {ShowFrame .debugdialog.main.crew}]
@@ -19,7 +18,7 @@ grid [ttk::button .debugdialog.buttons.world -text World -command {ShowFrame .de
 grid [ttk::button .debugdialog.buttons.refresh -text Refresh]
 grid [ttk::button .debugdialog.buttons.save -text {Save game}]
 # Ship options
-set shipframe [ttk::frame .debugdialog.main.shipframe]
+set shipframe [ttk::frame .debugdialog.main.ship]
 grid [ttk::button $shipframe.move -text {Move ship}]
 grid [ttk::label $shipframe.lblx -text {X:}] -column 1 -row 0
 grid [ttk::spinbox $shipframe.x -from 1 -to 1024] -column 2 -row 0
@@ -39,6 +38,31 @@ grid [ttk::label $shipframe.upgradelbl -text {Upgrade progress:}]
 grid [ttk::spinbox $shipframe.upgrade -from 0 -to 1000000] -column 1 -row 6 -columnspan 3
 grid [ttk::button $shipframe.change -text Change] -columnspan 4
 # Crew options
+set crewframe [ttk::frame .debugdialog.main.crew]
+grid [ttk::label $crewframe.memberlbl -text Member]
+grid [ttk::combobox $crewframe.member -state readonly] -column 1 -row 0
+grid [ttk::label $crewframe.healthlbl -text Health]
+grid [ttk::spinbox $crewframe.health -from 1 -to 100] -column 1 -row 1
+grid [ttk::label $crewframe.thirstlbl -text Thirst]
+grid [ttk::spinbox $crewframe.thirst -from 0 -to 100] -column 1 -row 2
+grid [ttk::label $crewframe.hungerlbl -text Hunger]
+grid [ttk::spinbox $crewframe.hunger -from 0 -to 100] -column 1 -row 3
+grid [ttk::label $crewframe.tiredlbl -text Tired]
+grid [ttk::spinbox $crewframe.tired -from 0 -to 100] -column 1 -row 4
+grid [ttk::label $crewframe.moralelbl -text Morale]
+grid [ttk::spinbox $crewframe.morale -from 0 -to 100] -column 1 -row 5
+grid [ttk::label $crewframe.loyaltylbl -text Loyalty]
+grid [ttk::spinbox $crewframe.loyalty -from 0 -to 100] -column 1 -row 6
+grid [ttk::frame $crewframe.stats] -column 2 -row 1
+grid [ttk::label $crewframe.stats.name -text Name]
+grid [ttk::label $crewframe.stats.level -text Level] -column 1 -row 0
+grid [ttk::frame $crewframe.skills] -column 3 -row 1
+grid [ttk::label $crewframe.skills.name -text Name]
+grid [ttk::label $crewframe.skills.level -text Level] -column 1 -row 0
+grid [ttk::frame $crewframe.addskill] -column 3 -row 6
+grid [ttk::button $crewframe.addskill.add -text Add]
+grid [ttk::combobox $crewframe.addskill.skills -state readonly] -column 1 -row 0
+grid [ttk::button $crewframe.change -text Change] -columnspan 4
 # Cargo options
 # Bases options
 # World options
