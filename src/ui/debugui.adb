b27017dad7852931771eb1dea8dfcc2e3832ad9f
thindil@laeran.pl <thindil@laeran.pl>
2020-08-01 04:41:28 +0000

    started work on showing the game data
    FossilOrigin-Name: d981476ee6eb4649328d927d24911ebf0b6956a5ef2db6960f608de0de596b18

--- src/ui/debugui.adb
+++ src/ui/debugui.adb
@@ -15,19 +15,71 @@
 
 with Ada.Strings; use Ada.Strings;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with Interfaces.C;
+with Interfaces.C.Strings; use Interfaces.C.Strings;
 with GNAT.Directory_Operations; use GNAT.Directory_Operations;
+with CArgv;
 with Tcl; use Tcl;
 with Tcl.Ada; use Tcl.Ada;
 with Tcl.Tk.Ada; use Tcl.Tk.Ada;
+with Tcl.Tk.Ada.Widgets; use Tcl.Tk.Ada.Widgets;
+with Tcl.Tk.Ada.Widgets.TtkEntry.TtkComboBox;
+use Tcl.Tk.Ada.Widgets.TtkEntry.TtkComboBox;
+with Tcl.Tk.Ada.Widgets.TtkEntry.TtkSpinBox;
+use Tcl.Tk.Ada.Widgets.TtkEntry.TtkSpinBox;
 with Game; use Game;
+with Ships; use Ships;
+with Utils.UI; use Utils.UI;
 
 package body DebugUI is
+
+   -- ****f* DebugUI/Refresh_Command
+   -- FUNCTION
+   -- Refresh the whole game information
+   -- PARAMETERS
+   -- ClientData - Custom data send to the command.
+   -- Interp     - Tcl interpreter in which command was executed.
+   -- Argc       - Number of arguments passed to the command.
+   -- Argv       - Values of arguments passed to the command.
+   -- SOURCE
+   function Refresh_Command
+     (ClientData: in Integer; Interp: in Tcl.Tcl_Interp;
+      Argc: in Interfaces.C.int; Argv: in CArgv.Chars_Ptr_Ptr)
+      return Interfaces.C.int with
+      Convention => C;
+      -- ****
+
+   function Refresh_Command
+     (ClientData: in Integer; Interp: in Tcl.Tcl_Interp;
+      Argc: in Interfaces.C.int; Argv: in CArgv.Chars_Ptr_Ptr)
+      return Interfaces.C.int is
+      pragma Unreferenced(ClientData, Argc, Argv);
+      SpinBox: Ttk_SpinBox;
+      ComboBox: Ttk_ComboBox;
+      ValuesList: Unbounded_String;
+   begin
+      SpinBox.Interp := Interp;
+      SpinBox.Name := New_String(".debugdialog.main.ship.x");
+      Set(SpinBox, Positive'Image(PlayerShip.SkyX));
+      SpinBox.Name := New_String(".debugdialog.main.ship.y");
+      Set(SpinBox, Positive'Image(PlayerShip.SkyY));
+      ComboBox.Interp := Interp;
+      ComboBox.Name := New_String(".debugdialog.main.ship.module");
+      for Module of PlayerShip.Modules loop
+         Append(ValuesList, " {" & Module.Name & "}");
+      end loop;
+      configure(ComboBox, "-values [list" & To_String(ValuesList) & "]");
+      Current(ComboBox, "0");
+      return TCL_OK;
+   end Refresh_Command;
 
    procedure ShowDebugUI is
    begin
       Tcl_EvalFile
         (Get_Context,
          To_String(DataDirectory) & "ui" & Dir_Separator & "debug.tcl");
+      AddCommand("Refresh", Refresh_Command'Access);
+      Tcl_Eval(Get_Context, "Refresh");
    end ShowDebugUI;
 
 end DebugUI;
