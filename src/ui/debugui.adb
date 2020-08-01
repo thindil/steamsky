1493ec0c407449c982dbf39442004c0d4f15826e
thindil@laeran.pl <thindil@laeran.pl>
2020-08-01 04:25:39 +0000

    added showing debug UI
    FossilOrigin-Name: dbdd1fddedd95da10757ef05d26c3d0f6d7f1271d5c30b26860fd4440dbcdb49

---
+++ src/ui/debugui.adb
@@ -0,0 +1,33 @@
+-- Copyright (c) 2020 Bartek thindil Jasicki <thindil@laeran.pl>
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+with Ada.Strings; use Ada.Strings;
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with GNAT.Directory_Operations; use GNAT.Directory_Operations;
+with Tcl; use Tcl;
+with Tcl.Ada; use Tcl.Ada;
+with Tcl.Tk.Ada; use Tcl.Tk.Ada;
+with Game; use Game;
+
+package body DebugUI is
+
+   procedure ShowDebugUI is
+   begin
+      Tcl_EvalFile
+        (Get_Context,
+         To_String(DataDirectory) & "ui" & Dir_Separator & "debug.tcl");
+   end ShowDebugUI;
+
+end DebugUI;
