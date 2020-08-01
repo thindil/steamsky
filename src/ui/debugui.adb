a8028d419c397fb64fa638d6dabe608326171c2e
thindil@laeran.pl <thindil@laeran.pl>
2020-08-01 04:54:36 +0000

    added showing player ship module info
    FossilOrigin-Name: ff978671964820a8b88cf4d8a6867ce4441f4499739d0650c2aba2e5492ed8fa

--- src/ui/debugui.adb
+++ src/ui/debugui.adb
@@ -23,15 +23,69 @@ with Tcl; use Tcl;
 with Tcl.Ada; use Tcl.Ada;
 with Tcl.Tk.Ada; use Tcl.Tk.Ada;
 with Tcl.Tk.Ada.Widgets; use Tcl.Tk.Ada.Widgets;
+with Tcl.Tk.Ada.Widgets.TtkEntry; use Tcl.Tk.Ada.Widgets.TtkEntry;
 with Tcl.Tk.Ada.Widgets.TtkEntry.TtkComboBox;
 use Tcl.Tk.Ada.Widgets.TtkEntry.TtkComboBox;
 with Tcl.Tk.Ada.Widgets.TtkEntry.TtkSpinBox;
 use Tcl.Tk.Ada.Widgets.TtkEntry.TtkSpinBox;
 with Game; use Game;
+with ShipModules; use ShipModules;
 with Ships; use Ships;
 with Utils.UI; use Utils.UI;
 
 package body DebugUI is
+
+   -- ****f* DebugUI/Refresh_Module_Command
+   -- FUNCTION
+   -- Refresh the information about selected module
+   -- PARAMETERS
+   -- ClientData - Custom data send to the command.
+   -- Interp     - Tcl interpreter in which command was executed.
+   -- Argc       - Number of arguments passed to the command.
+   -- Argv       - Values of arguments passed to the command.
+   -- SOURCE
+   function Refresh_Module_Command
+     (ClientData: in Integer; Interp: in Tcl.Tcl_Interp;
+      Argc: in Interfaces.C.int; Argv: in CArgv.Chars_Ptr_Ptr)
+      return Interfaces.C.int with
+      Convention => C;
+      -- ****
+
+   function Refresh_Module_Command
+     (ClientData: in Integer; Interp: in Tcl.Tcl_Interp;
+      Argc: in Interfaces.C.int; Argv: in CArgv.Chars_Ptr_Ptr)
+      return Interfaces.C.int is
+      pragma Unreferenced(ClientData, Argc, Argv);
+      ProtoEntry: Ttk_Entry;
+      ModuleCombo: Ttk_ComboBox;
+      ModuleIndex: Positive;
+      SpinBox: Ttk_SpinBox;
+   begin
+      ModuleCombo.Interp := Interp;
+      ModuleCombo.Name := New_String(".debugdialog.main.ship.module");
+      ModuleIndex := Natural'Value(Current(ModuleCombo)) + 1;
+      ProtoEntry.Interp := Interp;
+      ProtoEntry.Name := New_String(".debugdialog.main.ship.proto");
+      Delete(ProtoEntry, "0", "end");
+      Insert
+        (ProtoEntry, "0",
+         To_String
+           (Modules_List(PlayerShip.Modules(ModuleIndex).ProtoIndex).Name));
+      SpinBox.Interp := Interp;
+      SpinBox.Name := New_String(".debugdialog.main.ship.weight");
+      Set(SpinBox, Positive'Image(PlayerShip.Modules(ModuleIndex).Weight));
+      SpinBox.Name := New_String(".debugdialog.main.ship.dur");
+      Set(SpinBox, Integer'Image(PlayerShip.Modules(ModuleIndex).Durability));
+      SpinBox.Name := New_String(".debugdialog.main.ship.maxdur");
+      Set
+        (SpinBox,
+         Positive'Image(PlayerShip.Modules(ModuleIndex).MaxDurability));
+      SpinBox.Name := New_String(".debugdialog.main.ship.upgrade");
+      Set
+        (SpinBox,
+         Natural'Image(PlayerShip.Modules(ModuleIndex).UpgradeProgress));
+      return TCL_OK;
+   end Refresh_Module_Command;
 
    -- ****f* DebugUI/Refresh_Command
    -- FUNCTION
@@ -70,6 +124,7 @@ package body DebugUI is
       end loop;
       configure(ComboBox, "-values [list" & To_String(ValuesList) & "]");
       Current(ComboBox, "0");
+      Tcl_Eval(Get_Context, "RefreshModule");
       return TCL_OK;
    end Refresh_Command;
 
@@ -79,6 +134,7 @@ package body DebugUI is
         (Get_Context,
          To_String(DataDirectory) & "ui" & Dir_Separator & "debug.tcl");
       AddCommand("Refresh", Refresh_Command'Access);
+      AddCommand("RefreshModule", Refresh_Module_Command'Access);
       Tcl_Eval(Get_Context, "Refresh");
    end ShowDebugUI;
 
