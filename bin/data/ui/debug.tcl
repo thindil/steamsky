876b1883eacbde51cc3a2fd5065dd009f0e9d409
thindil@laeran.pl <thindil@laeran.pl>
2020-08-04 04:37:42 +0000

    added adding items to the player ship cargo
    FossilOrigin-Name: 974d418bda922f271917fc3ed7e607f338ab2bbca630d4148c579ab5648155e6

--- bin/data/ui/debug.tcl
+++ bin/data/ui/debug.tcl
@@ -67,7 +67,7 @@ grid [ttk::combobox $crewframe.addskill.skills -state readonly] -column 1 -row 0
 grid [ttk::button $crewframe.change -text Change -command DebugUpdateMember] -columnspan 4
 # Cargo options
 set cargoframe [ttk::frame .debugdialog.main.cargo]
-grid [ttk::button $cargoframe.addbutton -text Add]
+grid [ttk::button $cargoframe.addbutton -text Add -command DebugAddItem]
 grid [ttk::entry $cargoframe.add] -column 1 -row 0
 grid [ttk::label $cargoframe.amountlbl -text {Amount:}] -column 2 -row 0
 grid [ttk::spinbox $cargoframe.amount -from 1 -to 1000000 -validate key -validatecommand {ValidateSpinbox %S %s 1000000}] -column 3 -row 0
