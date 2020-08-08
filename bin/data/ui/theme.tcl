cdb6a9ef14187c46659388f09c125f2cc4a19d86
thindil@laeran.pl <thindil@laeran.pl>
2020-08-08 04:29:44 +0000

    better visibility of cursor in entries
    FossilOrigin-Name: e8cccc13b5b4073560c08b66625df2e8115b8665a7a9d7ff9ee1c6b7498a811a

--- bin/data/ui/theme.tcl
+++ bin/data/ui/theme.tcl
@@ -100,17 +100,19 @@ namespace eval ttk::theme::steamsky {
       ttk::style configure Headergreen.TLabel -foreground green
       # Progress bar setting
       ttk::style configure TProgressbar -background red
+      # Entry setting
+      ttk::style configure TEntry -insertcolor $colors(-fg)
 
       # Paned window
       ttk::style map TPanedwindow -background [list hover $colors(-checklight)]
 
+      # Combo box setting
       ttk::style map TCombobox -selectbackground [list \
          !focus         $colors(-window) \
          {readonly hover} $colors(-bg) \
          {readonly focus} $colors(-selectbg) \
          ]
 
-      # Combo box setting
       ttk::style map TCombobox -selectforeground [list \
          !focus $colors(-fg) \
          {readonly hover} $colors(-fg) \
@@ -136,7 +138,8 @@ namespace eval ttk::theme::steamsky {
          selectBackground [ttk::style lookup . -selectbackground] \
          selectForeground [ttk::style lookup . -selectforeground] \
          activeBackground [ttk::style lookup . -selectbackground] \
-         activeForeground [ttk::style lookup . -selectforeground]
+         activeForeground [ttk::style lookup . -selectforeground] \
+         insertbackground [ttk::style lookup . -foreground]
       option add *font [ttk::style lookup . -font]
    }
 
