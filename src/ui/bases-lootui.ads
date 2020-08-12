f6cd8db332c21a733dd4e1bc8d205d98b1dfab57
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-08-12 04:31:21 +0000

    updated code documentation
    FossilOrigin-Name: 01cd0bf8cfffffa3a11b38e6032ec2c0ed565cc35724035bf0b32f0cda298f8a

--- src/ui/bases-lootui.ads
+++ src/ui/bases-lootui.ads
@@ -13,7 +13,7 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
--- ****h* Trade/LUI
+-- ****h* Bases/LUI
 -- FUNCTION
 -- Provide code to loot bases
 -- SOURCE
