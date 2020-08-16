cdf7b034b60b436bdbe762d6c5a640c22f3a4a6b
thindil@laeran.pl <thindil@laeran.pl>
2020-08-16 04:22:22 +0000

    updated code documentation
    FossilOrigin-Name: 19dbf34c11d1a62b8ac279a654aef426ffa5ded5fc34f1c80d981d06f701a35b

--- src/ui/debugui.ads
+++ src/ui/debugui.ads
@@ -13,7 +13,7 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
--- ****h* Steamsky/DebugUI
+-- ****h* DebugUI/DebugUI
 -- FUNCTION
 -- Provide code to modify the game data
 -- SOURCE
