933aa1f5540e1860023dc6aeec8bf85f7451e4c5
thindil@laeran.pl <thindil@laeran.pl>
2020-08-31 04:35:33 +0000

    moved modules UI to separated package
    FossilOrigin-Name: 1f6f73a4a8b6364c09f97127b5587891d9ffb407ac3d96936ab958cc0d4a1bd8

--- src/ui/ships-ui.ads
+++ src/ui/ships-ui.ads
@@ -13,12 +13,36 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+with Interfaces.C; use Interfaces.C;
+with CArgv; use CArgv;
+with Tcl; use Tcl;
+
 -- ****h* Ships/SUI2
 -- FUNCTION
--- Provide code to show the game statistics to the player
+-- Provide code to show the player ship information to the player
 -- SOURCE
 package Ships.UI is
 -- ****
+
+   -- ****o* SUI2/Show_Ship_Info_Command
+   -- FUNCTION
+   -- Show information about the player's ship
+   -- PARAMETERS
+   -- ClientData - Custom data send to the command. Unused
+   -- Interp     - Tcl interpreter in which command was executed.
+   -- Argc       - Number of arguments passed to the command.
+   -- Argv       - Values of arguments passed to the command. Unused
+   -- RESULT
+   -- This function always return TCL_OK
+   -- COMMANDS
+   -- ShowShipInfo
+   -- SOURCE
+   function Show_Ship_Info_Command
+     (ClientData: in Integer; Interp: in Tcl.Tcl_Interp;
+      Argc: in Interfaces.C.int; Argv: in CArgv.Chars_Ptr_Ptr)
+      return Interfaces.C.int with
+      Convention => C;
+      -- ****
 
    -- ****f* SUI2/AddCommands
    -- FUNCTION
