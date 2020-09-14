19ea6b4ed24381c5f9eb007a8190dc94821b8371
thindil@laeran.pl <thindil@laeran.pl>
2020-09-14 04:25:48 +0000

    replaced treeview with text for showing messages
    FossilOrigin-Name: 803487fa712b85006485b10e7b67c79b51cf2b49d2ec8b16f6761d8326b3df31

--- bin/data/ui/messages.tcl
+++ bin/data/ui/messages.tcl
@@ -5,7 +5,7 @@ pack $messagescanvas -side top -fill both
 pack [ttk::scrollbar .paned.messagesframe.scrollx -orient horizontal -command [list $messagescanvas xview]] -fill x
 set messagesframe [ttk::frame $messagescanvas.messages]
 # Messages options
-grid [ttk::frame $messagesframe.options]
+grid [ttk::frame $messagesframe.options] -sticky w
 grid [ttk::combobox $messagesframe.options.types -values [list All Combat Trade Orders Craft Others Missions] -state readonly]
 bind $messagesframe.options.types <<ComboboxSelected>> SelectMessages
 $messagesframe.options.types current 0
@@ -13,12 +13,11 @@ grid [ttk::entry $messagesframe.options.search -validate key -validatecommand {S
 grid [ttk::button $messagesframe.options.delete -text {Delete all messages} -command DeleteMessages] -row 0 -column 2
 # Messages list
 grid [ttk::frame $messagesframe.list] -sticky nwes
-set messagesview [ttk::treeview $messagesframe.list.view -xscrollcommand [list $messagesframe.list.scrollx set] -yscrollcommand [list $messagesframe.list.scrolly set] -show tree -selectmode none]
-$messagesview tag configure yellow -foreground yellow
-$messagesview tag configure green -foreground green
-$messagesview tag configure red -foreground red
-$messagesview tag configure blue -foreground blue
-$messagesview tag configure cyan -foreground cyan
-pack [ttk::scrollbar $messagesframe.list.scrolly -orient vertical -command [list $messagesview yview]] -side right -fill y
-pack [ttk::scrollbar $messagesframe.list.scrollx -orient horizontal -command [list $messagesview xview]] -fill x -side bottom
-pack $messagesview -side top -fill both -expand true
+set messagesview2 [text $messagesframe.list.view]
+$messagesview2 tag configure yellow -foreground yellow
+$messagesview2 tag configure green -foreground #4e9a06
+$messagesview2 tag configure red -foreground red
+$messagesview2 tag configure cyan -foreground cyan
+$messagesview2 tag configure blue -foreground #3465a4
+$messagesview2 tag configure gray -foreground {dim gray}
+pack $messagesview2 -side top -fill both -expand true
