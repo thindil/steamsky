955d1c609c4d3228f38b941769d213b9eaef330e
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-10-12 04:32:06 +0000

    added script to run the game
    FossilOrigin-Name: 7ef53de719c4905cb4ed3948498b559d0a0920d421b16e6ff3135b0bc92bc01a

---
+++ run.tcl
@@ -0,0 +1,18 @@
+#!/bin/sh
+# the next line restarts using tclsh \
+exec tclsh "$0" ${1+"$@"}
+
+if {$tcl_platform(os) == "Linux"} {
+   set env(FONTCONFIG_FILE) [pwd]/share/fonts/fonts.conf
+   set executable [pwd]/bin/steamsky
+} else {
+   set executable [pwd]\bin\steamsky.exe
+}
+
+cd bin
+
+if {$argc > 0} {
+   exec $executable [list $argv]
+} else {
+   exec $executable
+}
