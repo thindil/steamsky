235f97bb628eda4b15e92a32db7e3fd810a71a88
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-10-24 04:49:51 +0000

    started work on showing info about the selected base
    FossilOrigin-Name: a2568b79c607bccff62dcaa04f8c6204f11d8b28813a78c6c80686f2d8e7f7e4

--- src/ui/knowledge-bases.adb
+++ src/ui/knowledge-bases.adb
@@ -14,6 +14,7 @@
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 with Ada.Characters.Handling; use Ada.Characters.Handling;
+with Ada.Characters.Latin_1; use Ada.Characters.Latin_1;
 with Ada.Strings; use Ada.Strings;
 with Ada.Strings.Fixed; use Ada.Strings.Fixed;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
@@ -21,10 +22,13 @@ with Interfaces.C.Strings; use Interfaces.C.Strings;
 with GNAT.String_Split; use GNAT.String_Split;
 with Tcl.Ada; use Tcl.Ada;
 with Tcl.Tk.Ada; use Tcl.Tk.Ada;
+with Tcl.Tk.Ada.Busy;
 with Tcl.Tk.Ada.Grid;
+with Tcl.Tk.Ada.Pack;
 with Tcl.Tk.Ada.Widgets; use Tcl.Tk.Ada.Widgets;
 with Tcl.Tk.Ada.Widgets.Canvas; use Tcl.Tk.Ada.Widgets.Canvas;
 with Tcl.Tk.Ada.Widgets.Menu; use Tcl.Tk.Ada.Widgets.Menu;
+with Tcl.Tk.Ada.Widgets.Toplevel; use Tcl.Tk.Ada.Widgets.Toplevel;
 with Tcl.Tk.Ada.Widgets.Toplevel.MainWindow;
 use Tcl.Tk.Ada.Widgets.Toplevel.MainWindow;
 with Tcl.Tk.Ada.Widgets.TtkButton; use Tcl.Tk.Ada.Widgets.TtkButton;
@@ -33,7 +37,11 @@ with Tcl.Tk.Ada.Widgets.TtkEntry.TtkComboBox;
 use Tcl.Tk.Ada.Widgets.TtkEntry.TtkComboBox;
 with Tcl.Tk.Ada.Widgets.TtkFrame; use Tcl.Tk.Ada.Widgets.TtkFrame;
 with Tcl.Tk.Ada.Widgets.TtkLabel; use Tcl.Tk.Ada.Widgets.TtkLabel;
+with Tcl.Tk.Ada.Widgets.TtkProgressBar; use Tcl.Tk.Ada.Widgets.TtkProgressBar;
+with Tcl.Tk.Ada.Widgets.TtkScrollbar; use Tcl.Tk.Ada.Widgets.TtkScrollbar;
+with Tcl.Tk.Ada.Wm; use Tcl.Tk.Ada.Wm;
 with Tcl.Tk.Ada.Winfo; use Tcl.Tk.Ada.Winfo;
+with Tcl.Tklib.Ada.Autoscroll; use Tcl.Tklib.Ada.Autoscroll;
 with Tcl.Tklib.Ada.Tooltip; use Tcl.Tklib.Ada.Tooltip;
 with Bases; use Bases;
 with BasesTypes; use BasesTypes;
@@ -42,6 +50,7 @@ with Maps; use Maps;
 with Maps.UI; use Maps.UI;
 with Messages; use Messages;
 with Ships; use Ships;
+with Utils; use Utils;
 with Utils.UI; use Utils.UI;
 
 package body Knowledge.Bases is
@@ -343,12 +352,262 @@ package body Knowledge.Bases is
       return TCL_OK;
    end Set_Base_Command;
 
+   -- ****o* KBases/Show_Base_Info_Command
+   -- FUNCTION
+   -- Show information about the selected base
+   -- PARAMETERS
+   -- ClientData - Custom data send to the command. Unused
+   -- Interp     - Tcl interpreter in which command was executed.
+   -- Argc       - Number of arguments passed to the command. Unused
+   -- Argv       - Values of arguments passed to the command.
+   -- RESULT
+   -- This function always return TCL_OK
+   -- COMMANDS
+   -- ShowBaseInfo baseindex
+   -- BaseIndex is the index of the base to show
+   -- SOURCE
+   function Show_Base_Info_Command
+     (ClientData: in Integer; Interp: in Tcl.Tcl_Interp;
+      Argc: in Interfaces.C.int; Argv: in CArgv.Chars_Ptr_Ptr)
+      return Interfaces.C.int with
+      Convention => C;
+      -- ****
+
+   function Show_Base_Info_Command
+     (ClientData: in Integer; Interp: in Tcl.Tcl_Interp;
+      Argc: in Interfaces.C.int; Argv: in CArgv.Chars_Ptr_Ptr)
+      return Interfaces.C.int is
+      pragma Unreferenced(ClientData, Argc);
+      BaseIndex: constant Positive := Positive'Value(CArgv.Arg(Argv, 1));
+      BaseDialog: constant Tk_Toplevel :=
+        Create
+          (".basedialog",
+           "-class Dialog -background [ttk::style lookup . -background] -relief solid -borderwidth 2");
+      MainWindow: constant Tk_Toplevel := Get_Main_Window(Interp);
+      XScroll: constant Ttk_Scrollbar :=
+        Create
+          (BaseDialog & ".xscroll",
+           "-orient horizontal -command [list .basedialog.canvas xview]");
+      YScroll: constant Ttk_Scrollbar :=
+        Create
+          (BaseDialog & ".yscroll",
+           "-orient vertical -command [list .moduledialog.canvas yview]");
+      BaseCanvas: constant Tk_Canvas :=
+        Create
+          (BaseDialog & ".canvas",
+           "-yscrollcommand [list " & YScroll &
+           " set] -xscrollcommand [list " & XScroll & " set]");
+      BaseFrame: constant Ttk_Frame := Create(BaseCanvas & ".frame");
+      CloseButton: constant Ttk_Button :=
+        Create
+          (BaseFrame & ".button",
+           "-text Close -command {CloseDialog " & BaseDialog & "}");
+      Height, Width: Positive := 10;
+      BaseInfo: Unbounded_String;
+      procedure SetReputationText(ReputationText: String) is
+         ReputationBar: Ttk_ProgressBar :=
+           Get_Widget
+             (".paned.basesframe.canvas.bases.base.info.minusreputation",
+              Interp);
+         ReputationLabel: constant Ttk_Label :=
+           Get_Widget
+             (".paned.basesframe.canvas.bases.base.info.reputationlbl",
+              Interp);
+      begin
+         if SkyBases(BaseIndex).Reputation(1) < 0 then
+            configure
+              (ReputationBar,
+               "-value" &
+               Positive'Image(abs (SkyBases(BaseIndex).Reputation(1))));
+            Tcl.Tk.Ada.Grid.Grid(ReputationBar);
+         else
+            Tcl.Tk.Ada.Grid.Grid_Remove(ReputationBar);
+         end if;
+         Add(ReputationBar, ReputationText);
+         ReputationBar.Name :=
+           New_String
+             (".paned.basesframe.canvas.bases.base.info.plusreputation");
+         if SkyBases(BaseIndex).Reputation(1) > 0 then
+            configure
+              (ReputationBar,
+               "-value" & Positive'Image(SkyBases(BaseIndex).Reputation(1)));
+            Tcl.Tk.Ada.Grid.Grid(ReputationBar);
+         else
+            Tcl.Tk.Ada.Grid.Grid_Remove(ReputationBar);
+         end if;
+         Add(ReputationBar, ReputationText);
+         if SkyBases(BaseIndex).Reputation(1) = 0 then
+            configure(ReputationLabel, "-text {Reputation: Unknown}");
+         else
+            configure(ReputationLabel, "-text {Reputation:}");
+         end if;
+         if SkyBases(BaseIndex).Visited.Year > 0 then
+            Tcl.Tk.Ada.Grid.Grid(ReputationLabel);
+         else
+            Tcl.Tk.Ada.Grid.Grid_Remove(ReputationLabel);
+            Tcl.Tk.Ada.Grid.Grid_Remove(ReputationBar);
+            ReputationBar.Name :=
+              New_String
+                (".paned.basesframe.canvas.bases.base.info.minusreputation");
+            Tcl.Tk.Ada.Grid.Grid_Remove(ReputationLabel);
+         end if;
+      end SetReputationText;
+   begin
+      Tcl.Tk.Ada.Busy.Busy(MainWindow);
+      Wm_Set(BaseDialog, "title", "{Steam Sky - Base Info}");
+      Wm_Set(BaseDialog, "transient", ".");
+      if Tcl_GetVar(Interp, "tcl_platform(os)") = "Linux" then
+         Wm_Set(BaseDialog, "attributes", "-type dialog");
+      end if;
+      Tcl.Tk.Ada.Pack.Pack(YScroll, " -side right -fill y");
+      Tcl.Tk.Ada.Pack.Pack(BaseCanvas, "-expand true -fill both");
+      Tcl.Tk.Ada.Pack.Pack(XScroll, "-fill x");
+      Autoscroll(YScroll);
+      Autoscroll(XScroll);
+      if SkyBases(BaseIndex).Visited.Year > 0 then
+         BaseInfo :=
+           To_Unbounded_String
+             ("X:" & Positive'Image(SkyBases(BaseIndex).SkyX) & " Y:" &
+              Positive'Image(SkyBases(BaseIndex).SkyY));
+         Append
+           (BaseInfo,
+            LF & "Last visited: " & FormatedTime(SkyBases(BaseIndex).Visited));
+         declare
+            TimeDiff: Integer;
+         begin
+            if SkyBases(BaseIndex).Population > 0 and
+              SkyBases(BaseIndex).Reputation(1) > -25 then
+               TimeDiff :=
+                 30 - DaysDifference(SkyBases(BaseIndex).RecruitDate);
+               if TimeDiff > 0 then
+                  Append
+                    (BaseInfo,
+                     LF & "New recruits available in" &
+                     Natural'Image(TimeDiff) & " days.");
+               else
+                  Append(BaseInfo, LF & "New recruits available now.");
+               end if;
+            else
+               Append
+                 (BaseInfo,
+                  LF & "You can't recruit crew members at this base.");
+            end if;
+            if SkyBases(BaseIndex).Population > 0 and
+              SkyBases(BaseIndex).Reputation(1) > -25 then
+               TimeDiff := DaysDifference(SkyBases(BaseIndex).AskedForEvents);
+               if TimeDiff < 7 then
+                  Append
+                    (BaseInfo,
+                     LF & "You asked for events" & Natural'Image(TimeDiff) &
+                     " days ago.");
+               else
+                  Append(BaseInfo, LF & "You can ask for events again.");
+               end if;
+            else
+               Append(BaseInfo, LF & "You can't ask for events at this base.");
+            end if;
+            if SkyBases(BaseIndex).Population > 0 and
+              SkyBases(BaseIndex).Reputation(1) > -1 then
+               TimeDiff :=
+                 7 - DaysDifference(SkyBases(BaseIndex).MissionsDate);
+               if TimeDiff > 0 then
+                  Append
+                    (BaseInfo,
+                     LF & "New missions available in" &
+                     Natural'Image(TimeDiff) & " days.");
+               else
+                  Append(BaseInfo, LF & "New missions available now.");
+               end if;
+            else
+               Append(BaseInfo, LF & "You can't take missions at this base.");
+            end if;
+         end;
+         case SkyBases(BaseIndex).Reputation(1) is
+            when -100 .. -75 =>
+               SetReputationText("Hated");
+            when -74 .. -50 =>
+               SetReputationText("Outlaw");
+            when -49 .. -25 =>
+               SetReputationText("Hostile");
+            when -24 .. -1 =>
+               SetReputationText("Unfriendly");
+            when 0 =>
+               SetReputationText("Unknown");
+            when 1 .. 25 =>
+               SetReputationText("Visitor");
+            when 26 .. 50 =>
+               SetReputationText("Trader");
+            when 51 .. 75 =>
+               SetReputationText("Friend");
+            when 76 .. 100 =>
+               SetReputationText("Well known");
+            when others =>
+               null;
+         end case;
+         if BaseIndex = PlayerShip.HomeBase then
+            Append(BaseInfo, LF & "It is your home base.");
+         end if;
+      else
+         BaseInfo := To_Unbounded_String("Not visited yet.");
+         SetReputationText("Unknown");
+      end if;
+      Tcl.Tk.Ada.Grid.Grid(CloseButton, "-columnspan 2");
+      Height := Height + Positive'Value(Winfo_Get(CloseButton, "reqheight"));
+      Focus(CloseButton);
+      if Height > 500 then
+         Height := 500;
+      end if;
+      configure
+        (BaseFrame,
+         "-height" & Positive'Image(Height) & " -width" &
+         Positive'Image(Width));
+      Canvas_Create
+        (BaseCanvas, "window",
+         "0 0 -anchor nw -window " & Widget_Image(BaseFrame));
+      configure
+        (BaseCanvas,
+         "-scrollregion [list " & BBox(BaseCanvas, "all") & "]");
+      Height := Height + 20;
+      declare
+         X, Y: Integer;
+      begin
+         Width :=
+           Positive'Value(Winfo_Get(YScroll, "reqwidth")) + 5;
+         X :=
+           (Positive'Value(Winfo_Get(BaseDialog, "vrootwidth")) - Width) / 2;
+         if X < 0 then
+            X := 0;
+         end if;
+         Y :=
+           (Positive'Value(Winfo_Get(BaseDialog, "vrootheight")) - Height) /
+           2;
+         if Y < 0 then
+            Y := 0;
+         end if;
+         Wm_Set
+           (BaseDialog, "geometry",
+            Trim(Positive'Image(Width), Left) & "x" &
+            Trim(Positive'Image(Height), Left) & "+" &
+            Trim(Positive'Image(X), Left) & "+" &
+            Trim(Positive'Image(Y), Left));
+         Bind
+           (BaseDialog, "<Destroy>",
+            "{CloseDialog " & BaseDialog & "}");
+         Bind
+           (BaseDialog, "<Escape>",
+            "{CloseDialog " & BaseDialog & "}");
+         Tcl_Eval(Interp, "update");
+      end;
+      return TCL_OK;
+   end Show_Base_Info_Command;
+
    procedure AddCommands is
    begin
       AddCommand("ShowBases", Show_Bases_Command'Access);
       AddCommand("ShowBasesMenu", Show_Bases_Menu_Command'Access);
       AddCommand("ShowBase", Show_Base_Command'Access);
       AddCommand("SetBase2", Set_Base_Command'Access);
+      AddCommand("ShowBaseInfo", Show_Base_Info_Command'Access);
    end AddCommands;
 
 end Knowledge.Bases;
