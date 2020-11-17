a7ee7f824c6a43ce837df3019410adce8bffa144
thindil@laeran.pl <thindil@laeran.pl>
2020-11-17 05:34:46 +0000

    moved wait dialog a bit higher
    FossilOrigin-Name: 24b22dfd5dcf022092b50324d4bdd6262fd4552cfcc9aac6437c7168d2f0d12a

--- src/ui/waitmenu.adb
+++ src/ui/waitmenu.adb
@@ -148,7 +148,7 @@ package body WaitMenu is
       Tcl.Tk.Ada.Grid.Grid
         (Button, "-sticky we -columnspan 3 -padx 5 -pady {0 5}");
       Bind(Button, "<Escape>", "{CloseDialog " & WaitDialog & "}");
-      Tcl.Tk.Ada.Place.Place(WaitDialog, "-in .gameframe -relx 0.3 -rely 0.3");
+      Tcl.Tk.Ada.Place.Place(WaitDialog, "-in .gameframe -relx 0.3 -rely 0.15");
       Focus(Button);
       Bind(Button, "<Tab>", "{focus " & WaitDialog & ".wait1;break}");
       return TCL_OK;
