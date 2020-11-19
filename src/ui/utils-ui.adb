11eb5690a8f5e6b31eddaa86389689fa37703beb
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-11-19 05:33:02 +0000

    fixed how Escape key works in messages window
    FossilOrigin-Name: f1dcfad92975760599baba3dedf083269f2fdb22ca8a67886c8231aa73918a7c

--- src/ui/utils-ui.adb
+++ src/ui/utils-ui.adb
@@ -145,7 +145,7 @@ package body Utils.UI is
         (MessageDialog, "-in " & ParentFrame & " -relx 0.3 -rely 0.3");
       Focus(MessageButton);
       Bind(MessageButton, "<Tab>", "{break}");
-      Bind(MessageButton, "<Escape>", "{" & MessageButton & " invoke}");
+      Bind(MessageButton, "<Escape>", "{" & MessageButton & " invoke;break}");
       TimerId :=
         To_Unbounded_String
           (After(1_000, "UpdateDialog " & ParentFrame & ".message"));
@@ -550,7 +550,6 @@ package body Utils.UI is
       SubWindows: Unbounded_String;
       MessagesFrame: constant Ttk_Frame :=
         Get_Widget(Paned & ".controls.messages");
-      PanedPosition: Natural;
    begin
       SubWindows := To_Unbounded_String(Panes(Paned));
       if Index(SubWindows, " ") = 0 then
@@ -561,13 +560,6 @@ package body Utils.UI is
       Forget(Paned, SubWindow);
       SubWindow.Name := New_String(".gameframe.paned." & NewScreenName);
       Insert(Paned, "0", SubWindow, "-weight 1");
-      if GameSettings.WindowHeight - GameSettings.MessagesPosition < 0 then
-         PanedPosition := GameSettings.WindowHeight;
-      else
-         PanedPosition :=
-           GameSettings.WindowHeight - GameSettings.MessagesPosition;
-      end if;
-      SashPos(Paned, "0", Natural'Image(PanedPosition));
       if NewScreenName in "optionsframe" | "messagesframe" or
         not GameSettings.ShowLastMessages then
          Tcl.Tk.Ada.Grid.Grid_Remove(MessagesFrame);
