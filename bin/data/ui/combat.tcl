dd34e60851695591e15ed2dcac18914a20deae27
thindil@laeran.pl <thindil@laeran.pl>
2020-11-22 05:36:58 +0000

    fixed look of combat UI
    FossilOrigin-Name: 6f45a97de4e497edddfb7e4df4ca2ba8ced8fa6db36236486e599fd7980328f5

--- bin/data/ui/combat.tcl
+++ bin/data/ui/combat.tcl
@@ -6,25 +6,24 @@ pack [ttk::scrollbar .gameframe.paned.combatframe.scrollx -orient horizontal -co
 
 # Ship to ship combat
 set combatframe [ttk::frame $combatcanvas.combat]
-grid [ttk::frame $combatframe.left]
+grid [ttk::frame $combatframe.left] -sticky nw
 grid [ttk::frame $combatframe.left.crew]
 grid [ttk::label $combatframe.left.crew.position -text {Position}]
 grid [ttk::label $combatframe.left.crew.name -text {Name}] -row 0 -column 1
 grid [ttk::label $combatframe.left.crew.order -text {Order}] -row 0 -column 2
-grid [ttk::label $combatframe.left.crew.pilotlabel -text {Pilot:}] -row 1
-grid [ttk::combobox $combatframe.left.crew.pilotcrew -state readonly] -row 1 -column 1
+grid [ttk::label $combatframe.left.crew.pilotlabel -text {Pilot:}] -row 1 -sticky w
+grid [ttk::combobox $combatframe.left.crew.pilotcrew -state readonly -width 10] -row 1 -column 1
 grid [ttk::combobox $combatframe.left.crew.pilotorder -state readonly -values [list {Go closer} {Keep distance} {Evade} {Escape}]] -row 1 -column 2
 bind $combatframe.left.crew.pilotorder <<ComboboxSelected>> {SetCombatOrder pilot}
-grid [ttk::label $combatframe.left.crew.engineerlabel -text {Engineer:}] -row 2
-grid [ttk::combobox $combatframe.left.crew.engineercrew -state readonly] -row 2 -column 1
+grid [ttk::label $combatframe.left.crew.engineerlabel -text {Engineer:}] -row 2 -sticky w
+grid [ttk::combobox $combatframe.left.crew.engineercrew -state readonly -width 10] -row 2 -column 1
 grid [ttk::combobox $combatframe.left.crew.engineerorder -state readonly -values [list {All stop} {Quarter speed} {Half speed} {Full speed}]] -row 2 -column 2
 bind $combatframe.left.crew.engineerorder <<ComboboxSelected>> {SetCombatOrder engineer}
-grid [ttk::labelframe $combatframe.left.damage -text {Ship damage:}]
+grid [ttk::labelframe $combatframe.left.damage -text {Ship damage:}] -sticky nw
 grid [ttk::frame $combatframe.right] -row 0 -column 1
 grid [ttk::labelframe $combatframe.right.enemy -text {Enemy info:}]
-grid [ttk::label $combatframe.right.enemy.info]
-grid [ttk::label $combatframe.right.enemy.description]
-grid [ttk::frame $combatframe.right.enemy.damage]
+grid [ttk::label $combatframe.right.enemy.info -wraplength 350]
+grid [ttk::frame $combatframe.right.enemy.damage] -sticky w
 grid [ttk::labelframe $combatframe.right.boarding -text {Boarding party:}]
 grid [ttk::button $combatframe.next -text {Next turn [Space]} -command NextTurn] -columnspan 2 -sticky we
 bind . <space> {InvokeButton $combatframe.next}
