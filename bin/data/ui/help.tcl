c4044b662e3c0ac7f84d50a5be45d374bba02efc
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-12-03 05:31:04 +0000

    use help font on help topics list
    FossilOrigin-Name: d90656f475722f34cb717f664bda8f2654b355c198ed832d972d614fb3fc7b49

--- bin/data/ui/help.tcl
+++ bin/data/ui/help.tcl
@@ -9,7 +9,7 @@ grid columnconfigure .help .help.paned -weight 1
 grid rowconfigure .help .help.paned -weight 1
 .help.paned add [ttk::frame .help.paned.topics]
 pack [ttk::scrollbar .help.paned.topics.scroll -orient vertical -command [list .help.paned.topics.view yview]] -side right -fill y
-pack [ttk::treeview .help.paned.topics.view -show tree -yscrollcommand [list .help.paned.topics.scroll set]] -side top -fill both
+pack [ttk::treeview .help.paned.topics.view -show tree -yscrollcommand [list .help.paned.topics.scroll set] -style Help.Treeview] -side top -fill both
 .help.paned add [ttk::frame .help.paned.content]
 pack [ttk::scrollbar .help.paned.content.scroll -orient vertical -command [list .help.paned.content.view yview]] -side right -fill y
 set helpview [text .help.paned.content.view -wrap word -yscrollcommand [list .help.paned.content.scroll set] -font HelpFont -width 70]
