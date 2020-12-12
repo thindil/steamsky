65d6b21f3c5d2a0197890a886ac71860a78b508e
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-12-12 05:31:12 +0000

    changed keyboard shortcut for the next turn in combat to Enter key
    FossilOrigin-Name: 94180c2de0c72c7988c56614856353cc9e69c565e6f00adc2d56870e0377a4c6

--- bin/data/ui/combat.tcl
+++ bin/data/ui/combat.tcl
@@ -6,7 +6,8 @@ pack [ttk::scrollbar .gameframe.paned.combatframe.scrollx -orient horizontal -co
 
 # Ship to ship combat
 set combatframe [ttk::frame $combatcanvas.combat]
-grid [ttk::button $combatframe.next -text {Next turn [Space]} -command NextTurn] -columnspan 2 -sticky we
+grid [ttk::button $combatframe.next -text {Next turn [Enter]} -command NextTurn] -columnspan 2 -sticky we
+bind $combatframe.next <Return> {InvokeButton $combatframe.next}
 focus $combatframe.next
 grid [ttk::frame $combatframe.left] -sticky nw
 grid [ttk::labelframe $combatframe.left.crew -text {Your ship crew orders:}] -padx 5 -pady {0 5}
@@ -15,15 +16,15 @@ grid [ttk::label $combatframe.left.crew.name -text {Name}] -row 0 -column 1
 grid [ttk::label $combatframe.left.crew.order -text {Order}] -row 0 -column 2
 grid [ttk::label $combatframe.left.crew.pilotlabel -text {Pilot:}] -row 1 -sticky w
 grid [ttk::combobox $combatframe.left.crew.pilotcrew -state readonly -width 10] -row 1 -column 1
-bind $combatframe.left.crew.pilotcrew <space> {InvokeButton $combatframe.next}
+bind $combatframe.left.crew.pilotcrew <Return> {InvokeButton $combatframe.next}
 grid [ttk::combobox $combatframe.left.crew.pilotorder -state readonly -values [list {Go closer} {Keep distance} {Evade} {Escape}]] -row 1 -column 2
-bind $combatframe.left.crew.pilotorder <space> {InvokeButton $combatframe.next}
+bind $combatframe.left.crew.pilotorder <Return> {InvokeButton $combatframe.next}
 bind $combatframe.left.crew.pilotorder <<ComboboxSelected>> {SetCombatOrder pilot}
 grid [ttk::label $combatframe.left.crew.engineerlabel -text {Engineer:}] -row 2 -sticky w
 grid [ttk::combobox $combatframe.left.crew.engineercrew -state readonly -width 10] -row 2 -column 1
-bind $combatframe.left.crew.engineercrew <space> {InvokeButton $combatframe.next}
+bind $combatframe.left.crew.engineercrew <Return> {InvokeButton $combatframe.next}
 grid [ttk::combobox $combatframe.left.crew.engineerorder -state readonly -values [list {All stop} {Quarter speed} {Half speed} {Full speed}]] -row 2 -column 2
-bind $combatframe.left.crew.engineerorder <space> {InvokeButton $combatframe.next}
+bind $combatframe.left.crew.engineerorder <Return> {InvokeButton $combatframe.next}
 bind $combatframe.left.crew.engineerorder <<ComboboxSelected>> {SetCombatOrder engineer}
 grid [ttk::labelframe $combatframe.left.damage -text {Your ship damage:}] -sticky we -padx 5 -pady {5 0}
 grid [ttk::frame $combatframe.right] -row 1 -column 1
@@ -44,5 +45,5 @@ grid [ttk::frame $boardingframe.right.enemy]
 grid [ttk::label $boardingframe.right.enemy.name -text {Name}] -row 0 -column 0
 grid [ttk::label $boardingframe.right.enemy.health -text {Health}] -row 0 -column 1
 grid [ttk::label $boardingframe.right.enemy.order -text {Order}] -row 0 -column 2
-grid [ttk::button $boardingframe.next -text {Next turn [Space]} -command NextTurn] -columnspan 2 -sticky we
-bind . <space> {InvokeButton $boardingframe.next}
+grid [ttk::button $boardingframe.next -text {Next turn [Enter]} -command NextTurn] -columnspan 2 -sticky we
+bind . <Return> {InvokeButton $boardingframe.next}
