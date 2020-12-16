064dee73015d4707ab8ee833eda8da75ee3636da
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-12-16 05:22:37 +0000

    made boarding and defenders lists longer
    FossilOrigin-Name: bf654e6e338d8020b6ffdcaaa7565388b81b5b778f0230958240c39cc2876e7e

--- src/ui/combat-ui.adb
+++ src/ui/combat-ui.adb
@@ -406,7 +406,7 @@ package body Combat.UI is
             Label :=
               Create
                 (Frame & ".boardparty",
-                 "-text {" & To_String(BoardingParty) & "} -wraplength 200");
+                 "-text {" & To_String(BoardingParty) & "} -wraplength 400");
             Tcl.Tk.Ada.Grid.Grid
               (Label,
                "-row" & Positive'Image(Positive(Guns.Length) + 3) &
@@ -418,7 +418,7 @@ package body Combat.UI is
             Label :=
               Create
                 (Frame & ".defenders",
-                 "-text {" & To_String(Defenders) & "} -wraplength 200");
+                 "-text {" & To_String(Defenders) & "} -wraplength 400");
             Tcl.Tk.Ada.Grid.Grid
               (Label,
                "-row" & Positive'Image(Positive(Guns.Length) + 4) &
