304744be5fa5c48341f1ef68c1d6374aa1195505
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-12-17 05:26:56 +0000

    added padding to comboboxes
    FossilOrigin-Name: 013b4701ada6397c5cf2b84c8f9221677167a991697171d89b62d13d2270ab1b

--- bin/data/ui/combat.tcl
+++ bin/data/ui/combat.tcl
@@ -16,16 +16,16 @@ grid [ttk::labelframe $combatframe.left.crew -text {Your ship crew orders:}] -pa
 grid [ttk::label $combatframe.left.crew.position -text {Position}]
 grid [ttk::label $combatframe.left.crew.name -text {Name}] -row 0 -column 1
 grid [ttk::label $combatframe.left.crew.order -text {Order}] -row 0 -column 2
-grid [ttk::label $combatframe.left.crew.pilotlabel -text {Pilot:}] -row 1 -sticky w -padx {5 0}
-grid [ttk::combobox $combatframe.left.crew.pilotcrew -state readonly -width 10] -row 1 -column 1
+grid [ttk::label $combatframe.left.crew.pilotlabel -text {Pilot:}] -row 1 -sticky w -padx {5 0} -pady {0 5}
+grid [ttk::combobox $combatframe.left.crew.pilotcrew -state readonly -width 10] -row 1 -column 1 -pady {0 5}
 bind $combatframe.left.crew.pilotcrew <Return> {InvokeButton $combatframe.next}
-grid [ttk::combobox $combatframe.left.crew.pilotorder -state readonly -values [list {Go closer} {Keep distance} {Evade} {Escape}]] -row 1 -column 2 -padx {0 5}
+grid [ttk::combobox $combatframe.left.crew.pilotorder -state readonly -values [list {Go closer} {Keep distance} {Evade} {Escape}]] -row 1 -column 2 -padx {0 5} -pady {0 5}
 bind $combatframe.left.crew.pilotorder <Return> {InvokeButton $combatframe.next}
 bind $combatframe.left.crew.pilotorder <<ComboboxSelected>> {SetCombatOrder pilot}
-grid [ttk::label $combatframe.left.crew.engineerlabel -text {Engineer:}] -row 2 -sticky w -padx {5 0}
-grid [ttk::combobox $combatframe.left.crew.engineercrew -state readonly -width 10] -row 2 -column 1
+grid [ttk::label $combatframe.left.crew.engineerlabel -text {Engineer:}] -row 2 -sticky w -padx {5 0} -pady {5 0}
+grid [ttk::combobox $combatframe.left.crew.engineercrew -state readonly -width 10] -row 2 -column 1 -pady {5 0}
 bind $combatframe.left.crew.engineercrew <Return> {InvokeButton $combatframe.next}
-grid [ttk::combobox $combatframe.left.crew.engineerorder -state readonly -values [list {All stop} {Quarter speed} {Half speed} {Full speed}]] -row 2 -column 2 -padx {0 5}
+grid [ttk::combobox $combatframe.left.crew.engineerorder -state readonly -values [list {All stop} {Quarter speed} {Half speed} {Full speed}]] -row 2 -column 2 -padx {0 5} -pady {5 0}
 bind $combatframe.left.crew.engineerorder <Return> {InvokeButton $combatframe.next}
 bind $combatframe.left.crew.engineerorder <<ComboboxSelected>> {SetCombatOrder engineer}
 grid [ttk::labelframe $combatframe.left.damage -text {Your ship damage:}] -sticky we -padx 5 -pady {5 0}
