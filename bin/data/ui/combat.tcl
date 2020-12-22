cf9352c2f638ef5992c5783e21fd1a69e39c74f4
thindil@laeran.pl <thindil@laeran.pl>
2020-12-22 05:26:43 +0000

    started work on redesign combat UI
    FossilOrigin-Name: 1893d9f41db720877524fd66f541edaeffdf5dbe0fc3b86943b2f59624f63561

--- bin/data/ui/combat.tcl
+++ bin/data/ui/combat.tcl
@@ -1,42 +1,68 @@
 ttk::frame .gameframe.paned.combatframe
-set combatcanvas [canvas .gameframe.paned.combatframe.canvas -yscrollcommand [list .gameframe.paned.combatframe.scrolly set] -xscrollcommand [list .gameframe.paned.combatframe.scrollx set]]
-pack [ttk::scrollbar .gameframe.paned.combatframe.scrolly -orient vertical -command [list $combatcanvas yview]] -side right -fill y
-pack [ttk::scrollbar .gameframe.paned.combatframe.scrollx -orient horizontal -command [list $combatcanvas xview]] -fill x -side bottom
-pack $combatcanvas -side top -fill both
-::autoscroll::autoscroll .gameframe.paned.combatframe.scrolly
-::autoscroll::autoscroll .gameframe.paned.combatframe.scrollx
 
 # Ship to ship combat
-set combatframe [ttk::frame $combatcanvas.combat]
+set combatframe [ttk::frame .gameframe.paned.combatframe.combat]
+# Player ship crew orders
+grid [ttk::labelframe $combatframe.crew -text {Your ship crew orders:}] -padx 5 -pady {0 5}
+set combatcanvas [canvas $combatframe.crew.canvas -yscrollcommand [list $combatframe.crew.scrolly set] -xscrollcommand [list $combatframe.crew.scrollx set]]
+pack [ttk::scrollbar $combatframe.crew.scrolly -orient vertical -command [list $combatcanvas yview]] -side right -fill y
+pack [ttk::scrollbar $combatframe.crew.scrollx -orient horizontal -command [list $combatcanvas xview]] -fill x -side bottom
+pack $combatcanvas -side top -fill both -expand true
+ttk::frame $combatcanvas.frame
+grid [ttk::label $combatcanvas.frame.position -text {Position}]
+grid [ttk::label $combatcanvas.frame.name -text {Name}] -row 0 -column 1
+grid [ttk::label $combatcanvas.frame.order -text {Order}] -row 0 -column 2
+grid [ttk::label $combatcanvas.frame.pilotlabel -text {Pilot:}] -row 1 -sticky w -padx {5 0} -pady {0 5}
+grid [ttk::combobox $combatcanvas.frame.pilotcrew -state readonly -width 10] -row 1 -column 1 -pady {0 5}
+bind $combatcanvas.frame.pilotcrew <Return> {InvokeButton $combatframe.next}
+grid [ttk::combobox $combatcanvas.frame.pilotorder -state readonly -values [list {Go closer} {Keep distance} {Evade} {Escape}]] -row 1 -column 2 -padx {0 5} -pady {0 5}
+bind $combatcanvas.frame.pilotorder <Return> {InvokeButton $combatframe.next}
+bind $combatcanvas.frame.pilotorder <<ComboboxSelected>> {SetCombatOrder pilot}
+grid [ttk::label $combatcanvas.frame.engineerlabel -text {Engineer:}] -row 2 -sticky w -padx {5 0} -pady {5 0}
+grid [ttk::combobox $combatcanvas.frame.engineercrew -state readonly -width 10] -row 2 -column 1 -pady {5 0}
+bind $combatcanvas.frame.engineercrew <Return> {InvokeButton $combatframe.next}
+grid [ttk::combobox $combatcanvas.frame.engineerorder -state readonly -values [list {All stop} {Quarter speed} {Half speed} {Full speed}]] -row 2 -column 2 -padx {0 5} -pady {5 0}
+bind $combatcanvas.frame.engineerorder <Return> {InvokeButton $combatframe.next}
+bind $combatcanvas.frame.engineerorder <<ComboboxSelected>> {SetCombatOrder engineer}
+$combatcanvas create window 0 0 -anchor nw -window $combatcanvas.frame
+::autoscroll::autoscroll $combatframe.crew.scrolly
+::autoscroll::autoscroll $combatframe.crew.scrollx
+# Player ship damage
+grid [ttk::labelframe $combatframe.damage -text {Your ship damage:}] -sticky we -padx 5 -pady {5 0}
+set combatcanvas [canvas $combatframe.damage.canvas -yscrollcommand [list $combatframe.damage.scrolly set] -xscrollcommand [list $combatframe.damage.scrollx set]]
+pack [ttk::scrollbar $combatframe.damage.scrolly -orient vertical -command [list $combatcanvas yview]] -side right -fill y
+pack [ttk::scrollbar $combatframe.damage.scrollx -orient horizontal -command [list $combatcanvas xview]] -fill x -side bottom
+pack $combatcanvas -side top -fill both -expand true
+ttk::frame $combatcanvas.frame
+$combatcanvas create window 0 0 -anchor nw -window $combatcanvas.frame
+::autoscroll::autoscroll $combatframe.damage.scrolly
+::autoscroll::autoscroll $combatframe.damage.scrollx
+# Enemy ship info
+grid [ttk::labelframe $combatframe.enemy -text {Enemy info:}] -sticky we -padx 5 -pady {0 5} -column 1 -row 0
+set combatcanvas [canvas $combatframe.enemy.canvas -yscrollcommand [list $combatframe.enemy.scrolly set] -xscrollcommand [list $combatframe.enemy.scrollx set]]
+pack [ttk::scrollbar $combatframe.enemy.scrolly -orient vertical -command [list $combatcanvas yview]] -side right -fill y
+pack [ttk::scrollbar $combatframe.enemy.scrollx -orient horizontal -command [list $combatcanvas xview]] -fill x -side bottom
+pack $combatcanvas -side top -fill both -expand true
+ttk::label $combatcanvas.info -wraplength 350
+$combatcanvas create window 0 0 -anchor nw -window $combatcanvas.info
+::autoscroll::autoscroll $combatframe.enemy.scrolly
+::autoscroll::autoscroll $combatframe.enemy.scrollx
+# Enemy ship info damage
+grid [ttk::labelframe $combatframe.status -text {Enemy ship status:}] -sticky we -padx 5 -pady {5 0} -column 1 -row 1
+set combatcanvas [canvas $combatframe.status.canvas -yscrollcommand [list $combatframe.status.scrolly set] -xscrollcommand [list $combatframe.status.scrollx set]]
+pack [ttk::scrollbar $combatframe.status.scrolly -orient vertical -command [list $combatcanvas yview]] -side right -fill y
+pack [ttk::scrollbar $combatframe.status.scrollx -orient horizontal -command [list $combatcanvas xview]] -fill x -side bottom
+pack $combatcanvas -side top -fill both -expand true
+ttk::frame $combatcanvas.frame
+$combatcanvas create window 0 0 -anchor nw -window $combatcanvas.frame
+::autoscroll::autoscroll $combatframe.status.scrolly
+::autoscroll::autoscroll $combatframe.status.scrollx
 grid [ttk::button $combatframe.next -text {Next turn [Enter]} -command NextTurn] -columnspan 2 -sticky we
 bind $combatframe.next <Return> {InvokeButton $combatframe.next}
 focus $combatframe.next
-grid [ttk::frame $combatframe.left] -sticky nw
-grid [ttk::labelframe $combatframe.left.crew -text {Your ship crew orders:}] -padx 5 -pady {0 5}
-grid [ttk::label $combatframe.left.crew.position -text {Position}]
-grid [ttk::label $combatframe.left.crew.name -text {Name}] -row 0 -column 1
-grid [ttk::label $combatframe.left.crew.order -text {Order}] -row 0 -column 2
-grid [ttk::label $combatframe.left.crew.pilotlabel -text {Pilot:}] -row 1 -sticky w -padx {5 0} -pady {0 5}
-grid [ttk::combobox $combatframe.left.crew.pilotcrew -state readonly -width 10] -row 1 -column 1 -pady {0 5}
-bind $combatframe.left.crew.pilotcrew <Return> {InvokeButton $combatframe.next}
-grid [ttk::combobox $combatframe.left.crew.pilotorder -state readonly -values [list {Go closer} {Keep distance} {Evade} {Escape}]] -row 1 -column 2 -padx {0 5} -pady {0 5}
-bind $combatframe.left.crew.pilotorder <Return> {InvokeButton $combatframe.next}
-bind $combatframe.left.crew.pilotorder <<ComboboxSelected>> {SetCombatOrder pilot}
-grid [ttk::label $combatframe.left.crew.engineerlabel -text {Engineer:}] -row 2 -sticky w -padx {5 0} -pady {5 0}
-grid [ttk::combobox $combatframe.left.crew.engineercrew -state readonly -width 10] -row 2 -column 1 -pady {5 0}
-bind $combatframe.left.crew.engineercrew <Return> {InvokeButton $combatframe.next}
-grid [ttk::combobox $combatframe.left.crew.engineerorder -state readonly -values [list {All stop} {Quarter speed} {Half speed} {Full speed}]] -row 2 -column 2 -padx {0 5} -pady {5 0}
-bind $combatframe.left.crew.engineerorder <Return> {InvokeButton $combatframe.next}
-bind $combatframe.left.crew.engineerorder <<ComboboxSelected>> {SetCombatOrder engineer}
-grid [ttk::labelframe $combatframe.left.damage -text {Your ship damage:}] -sticky we -padx 5 -pady {5 0}
-grid [ttk::frame $combatframe.right] -row 1 -column 1
-grid [ttk::labelframe $combatframe.right.enemy -text {Enemy info:}] -sticky we -padx 5 -pady {0 5}
-grid [ttk::label $combatframe.right.enemy.info -wraplength 350]
-grid [ttk::labelframe $combatframe.right.status -text {Enemy ship status:}] -sticky we -padx 5 -pady {5 0}
-grid [ttk::labelframe $combatframe.right.boarding -text {Boarding party:}]
 
 # Boarding combat
-set boardingframe [ttk::frame $combatcanvas.boarding]
+set boardingframe [ttk::frame .gameframe.paned.combatframe.boarding]
 grid [ttk::button $boardingframe.next -text {Next turn [Enter]} -command NextTurn] -columnspan 2 -sticky we
 bind . <Return> {InvokeButton $boardingframe.next}
 grid [ttk::labelframe $boardingframe.left -text {Your crew:}] -sticky n
