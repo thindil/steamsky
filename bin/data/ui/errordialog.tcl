7c9c7eb2003af9597911b0e45013891ea5d56c57
thindil@laeran.pl <thindil@laeran.pl>
2020-12-25 05:35:19 +0000

    added GPL header
    FossilOrigin-Name: 849ed82865c018da32198425d1365e4b92a6cad93489f9f350ca50e74d5ef9de

--- bin/data/ui/errordialog.tcl
+++ bin/data/ui/errordialog.tcl
@@ -1,3 +1,18 @@
+# Copyright (c) 2020 Bartek thindil Jasicki <thindil@laeran.pl>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 wm title . {Steam Sky - Error info}
 wm geometry . 800x600+[expr ([winfo vrootwidth .] - 800) / 2]+[expr ([winfo vrootheight .] - 600) / 2]
 pack [ttk::label .generalinfo -text {Oops, something bad happens and the game has crashed. Game should save your progress, but better verify this yourself. Also, please, remember what you were doing before the crash and report this problem at} -wraplength 790]
