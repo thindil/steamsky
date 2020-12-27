f34f43df05ed92e810641ec0bd8ceb7fc91ae01b
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-12-27 05:19:54 +0000

    added padding to frames
    FossilOrigin-Name: 1acf51a3d0373c1c706b667f64878eac5b8d87bd0b3db7c3cf12b073cfb3c501

--- bin/data/ui/combat.tcl
+++ bin/data/ui/combat.tcl
@@ -79,7 +79,7 @@ focus $combatframe.next
 
 # Boarding combat
 # Player boarding team
-grid [ttk::labelframe $combatframe.left -text {Your crew:}] -sticky nwes -row 0 -column 0 -rowspan 2
+grid [ttk::labelframe $combatframe.left -text {Your crew:}] -sticky nwes -row 0 -column 0 -rowspan 2 -padx 5 -pady 5
 set combatcanvas [canvas $combatframe.left.canvas -yscrollcommand [list $combatframe.left.scrolly set] -xscrollcommand [list $combatframe.left.scrollx set]]
 pack [ttk::scrollbar $combatframe.left.scrolly -orient vertical -command [list $combatcanvas yview]] -side right -fill y
 pack [ttk::scrollbar $combatframe.left.scrollx -orient horizontal -command [list $combatcanvas xview]] -fill x -side bottom
@@ -92,7 +92,7 @@ $combatcanvas create window 0 0 -anchor nw -window $combatcanvas.frame
 ::autoscroll::autoscroll $combatframe.left.scrolly
 ::autoscroll::autoscroll $combatframe.left.scrollx
 # Enemy defending party
-grid [ttk::labelframe $combatframe.right -text {Enemy's crew:}] -row 0 -column 1 -sticky nwes -rowspan 2
+grid [ttk::labelframe $combatframe.right -text {Enemy's crew:}] -row 0 -column 1 -sticky nwes -rowspan 2 -padx 5 -pady 5
 set combatcanvas [canvas $combatframe.right.canvas -yscrollcommand [list $combatframe.right.scrolly set] -xscrollcommand [list $combatframe.right.scrollx set]]
 pack [ttk::scrollbar $combatframe.right.scrolly -orient vertical -command [list $combatcanvas yview]] -side right -fill y
 pack [ttk::scrollbar $combatframe.right.scrollx -orient horizontal -command [list $combatcanvas xview]] -fill x -side bottom
