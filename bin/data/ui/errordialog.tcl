f1e87f215f7a3758cd13c7ed535d487c21e56d00
thindil <thindil@laeran.pl>
2020-12-29 05:31:26 +0000

    updated link to the program error reports page
    FossilOrigin-Name: 5fcdc74815a319b67a55bc7e5216185c9d312d9fe63430cf9e45681d51166d46

--- bin/data/ui/errordialog.tcl
+++ bin/data/ui/errordialog.tcl
@@ -16,7 +16,7 @@
 wm title . {Steam Sky - Error info}
 wm geometry . 800x600+[expr ([winfo vrootwidth .] - 800) / 2]+[expr ([winfo vrootheight .] - 600) / 2]
 pack [ttk::label .generalinfo -text {Oops, something bad happens and the game has crashed. Game should save your progress, but better verify this yourself. Also, please, remember what you were doing before the crash and report this problem at} -wraplength 790]
-pack [ttk::button .reportlink -text {https://github.com/thindil/steamsky/issues} -command {OpenLink https://github.com/thindil/steamsky/issues} -style Toolbutton]
+pack [ttk::button .reportlink -text {https://www.laeran.pl/repositories/steamsky/ticket} -command {OpenLink https://www.laeran.pl/repositories/steamsky/ticket} -style Toolbutton]
 pack [ttk::label .generalinfo2 -text {or if you prefer, on one of the game community options:}]
 pack [ttk::button .reportlink2 -text {https://thindil.itch.io/steam-sky} -command {OpenLink https://thindil.itch.io/steam-sky} -style Toolbutton]
 pack [ttk::label .generalinfo3 -text {and attach (if possible) file 'error.log'}]
