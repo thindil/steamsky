cebaf1c9d0baa93a340bb6efb3c70feffdd02e73
thindil <thindil@laeran.pl>
2021-01-05 05:27:01 +0000

    added GPL header to file
    FossilOrigin-Name: 2b0775dbb2a23ea0593cde73c3b7386ad60646292d99b8b470e66a6846a5a140

--- bin/data/ui/school.tcl
+++ bin/data/ui/school.tcl
@@ -1,4 +1,18 @@
-# normal code
+# Copyright (c) 2020-2021 Bartek thindil Jasicki <thindil@laeran.pl>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ttk::frame .gameframe.paned.schoolframe
 set schoolcanvas [canvas .gameframe.paned.schoolframe.canvas -yscrollcommand [list .gameframe.paned.schoolframe.scrolly set] -xscrollcommand [list .gameframe.paned.schoolframe.scrollx set]]
 pack [ttk::scrollbar .gameframe.paned.schoolframe.scrolly -orient vertical -command [list $schoolcanvas yview]] -side right -fill y
