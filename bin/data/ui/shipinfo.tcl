3b29808f9b1ec95876ae14f083d7303c00b631e3
thindil <thindil@noemail.net>
2021-01-06 05:34:48 +0000

    added GPL header to file
    FossilOrigin-Name: 2ab0ca95425319ae2ff87cbb95f2640aa608f60da815a0cf384f6f8fbdf470f8

--- bin/data/ui/shipinfo.tcl
+++ bin/data/ui/shipinfo.tcl
@@ -1,3 +1,18 @@
+# Copyright (c) 2020-2021 Bartek thindil Jasicki <thindil@laeran.pl>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 set shipinfoframe [ttk::frame .gameframe.paned.shipinfoframe]
 # General ship info
 grid [ttk::labelframe $shipinfoframe.general -text {General Info:}] -sticky nwes -padx 4
