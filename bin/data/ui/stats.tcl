9c99526583ba0776a0c3ed9de95eafb3430d1d5c
thindil <thindil@laeran.pl>
2021-01-08 05:39:16 +0000

    added GPL header to file
    FossilOrigin-Name: af6f0e67774db54bbd048e6b9299986345f03e439a2013b66beb01ff8f658292

--- bin/data/ui/stats.tcl
+++ bin/data/ui/stats.tcl
@@ -1,3 +1,18 @@
+# Copyright (c) 2020-2021 Bartek thindil Jasicki <thindil@laeran.pl>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ttk::frame .gameframe.paned.statsframe
 set statscanvas [canvas .gameframe.paned.statsframe.canvas -yscrollcommand [list .gameframe.paned.statsframe.scrolly set] -xscrollcommand [list .gameframe.paned.statsframe.scrollx set]]
 pack [ttk::scrollbar .gameframe.paned.statsframe.scrolly -orient vertical -command [list $statscanvas yview]] -side right -fill y
