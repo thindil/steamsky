214f2dc8f9d086af37aab13528234e9e0d23ff9a
thindil <thindil@noemail.net>
2021-01-10 05:22:22 +0000

    added GPL header to file
    FossilOrigin-Name: 8184701ccb02f5f53ac3c529665bb1c46d681763c329b77d037838b959865de6

--- bin/data/ui/trade.tcl
+++ bin/data/ui/trade.tcl
@@ -1,3 +1,18 @@
+# Copyright (c) 2020-2021 Bartek thindil Jasicki <thindil@laeran.pl>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ttk::frame .gameframe.paned.tradeframe
 set tradecanvas [canvas .gameframe.paned.tradeframe.canvas -yscrollcommand [list .gameframe.paned.tradeframe.scrolly set] -xscrollcommand [list .gameframe.paned.tradeframe.scrollx set]]
 pack [ttk::scrollbar .gameframe.paned.tradeframe.scrolly -orient vertical -command [list $tradecanvas yview]] -side right -fill y
