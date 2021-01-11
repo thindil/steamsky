e63ad22e9cbb6f415dadc3d4bd7e695350fff89c
thindil <thindil@laeran.pl>
2021-01-11 05:37:52 +0000

    delete search entry content for buying recipes when entering it
    FossilOrigin-Name: 39fa8cd3ec4fea362271a286aa357f84eec3f625916b2b6dbd11b475c4a5d9a9

--- bin/data/ui/missions.tcl
+++ bin/data/ui/missions.tcl
@@ -18,18 +18,24 @@ set missionscanvas [canvas .gameframe.paned.missionsframe.canvas -yscrollcommand
 pack [ttk::scrollbar .gameframe.paned.missionsframe.scrolly -orient vertical -command [list $missionscanvas yview]] -side right -fill y
 pack $missionscanvas -side top -fill both
 pack [ttk::scrollbar .gameframe.paned.missionsframe.scrollx -orient horizontal -command [list $missionscanvas xview]] -fill x
+::autoscroll::autoscroll .gameframe.paned.missionsframe.scrolly
+::autoscroll::autoscroll .gameframe.paned.missionsframe.scrollx
 set missionsframe [ttk::frame $missionscanvas.missions]
-grid [ttk::treeview $missionsframe.missionsview -show headings -columns [list name distance]]
+grid [ttk::treeview $missionsframe.missionsview -show headings -columns [list name distance]] -sticky n -padx 5 -pady 5
 $missionsframe.missionsview heading name -text {Name}
+$missionsframe.missionsview column name -width 250
 $missionsframe.missionsview heading distance -text {Distance}
+$missionsframe.missionsview column distance -width 100 -anchor center
 bind $missionsframe.missionsview <<TreeviewSelect>> ShowMissionInfo
 grid [ttk::frame $missionsframe.info] -column 1 -row 0
-grid [ttk::labelframe $missionsframe.info.info -text {Mission Info:}]
-grid [text $missionsframe.info.info.text -wrap char -height 10 -width 40]
+grid [ttk::labelframe $missionsframe.info.info -text {Mission Info:}] -padx 5 -pady 5
+grid [text $missionsframe.info.info.text -wrap char -height 10 -width 40] -padx 5 -pady 5
 $missionsframe.info.info.text tag configure red -foreground red
 $missionsframe.info.info.text tag configure yellow -foreground yellow
 set reward 1.0
-grid [ttk::scale $missionsframe.info.reward -from 0.0 -to 2.0 -variable reward -command ShowMissionInfo]
+grid [ttk::frame $missionsframe.info.reward] -sticky we
+grid [ttk::label $missionsframe.info.reward.label -text {Reward:}] -padx 5
+grid [ttk::scale $missionsframe.info.reward.amount -from 0.0 -to 2.0 -variable reward -command ShowMissionInfo -length 300] -column 1 -row 0 -padx 5
 tooltip::tooltip $missionsframe.info.reward "Move left - more reputation from mission but less money,\nmove right - more money from mission but less reputation."
 grid [ttk::label $missionsframe.info.missioninfo]
 grid [ttk::button $missionsframe.info.show -text {Show mission on map} -command {ShowMission}]
