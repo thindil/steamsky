cd188adc4364ddca0f205269e57c843c9dcc9ac5
thindil <thindil@noemail.net>
2021-01-16 05:53:54 +0000

    updated code documentation
    FossilOrigin-Name: 01347df4816536778dbf5406a9a61dfd6461111bea4d3616639f099aa31fd7fd

--- src/ui/knowledge-missions.ads
+++ src/ui/knowledge-missions.ads
@@ -1,4 +1,4 @@
--- Copyright (c) 2020 Bartek thindil Jasicki <thindil@laeran.pl>
+-- Copyright (c) 2020-2021 Bartek thindil Jasicki <thindil@laeran.pl>
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@
 package Knowledge.Missions is
 -- ****
 
-   -- ****f* KMissions/AddCommands
+   -- ****f* KMissions/KMissions.AddCommands
    -- FUNCTION
    -- Add Tcl commands related to the list of known bases
    -- SOURCE
