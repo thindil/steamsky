01d8a49b07f59da2d192d38d5e64e2a07869aa00
thindil <thindil@noemail.net>
2021-01-22 05:29:01 +0000

    updated code documentation
    FossilOrigin-Name: 411d71459a03e629b16a1bc2962fe025fef1927ea3ae2a208a653302c44a0671

--- src/ui/maps-ui-commands.ads
+++ src/ui/maps-ui-commands.ads
@@ -1,4 +1,4 @@
--- Copyright (c) 2020 Bartek thindil Jasicki <thindil@laeran.pl>
+-- Copyright (c) 2020-2021 Bartek thindil Jasicki <thindil@laeran.pl>
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@
 package Maps.UI.Commands is
 -- ****
 
-   -- ****f* MapCommands/AddCommands
+   -- ****f* MapCommands/MapCommands.AddCommands
    -- FUNCTION
    -- Add Tcl commands related to map to the game
    -- SOURCE
