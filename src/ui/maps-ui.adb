02ab77ccb42063004451f9f5b6cabce16a2363fa
thindil <thindil@laeran.pl>
2021-01-23 05:25:58 +0000

    updated code documentation
    FossilOrigin-Name: 49ca4d9acf5ad358055b3570a76a07ae6eaf538181cb05768e723bb06afe1aa0

--- src/ui/maps-ui.adb
+++ src/ui/maps-ui.adb
@@ -1,4 +1,4 @@
--- Copyright (c) 2020 Bartek thindil Jasicki <thindil@laeran.pl>
+-- Copyright (c) 2020-2021 Bartek thindil Jasicki <thindil@laeran.pl>
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -386,7 +386,7 @@ package body Maps.UI is
       end if;
    end UpdateHeader;
 
-   -- ****iv* MUI/MapView
+   -- ****iv* MUI/MUI.MapView
    -- FUNCTION
    -- Text widget with the sky map
    -- SOURCE
