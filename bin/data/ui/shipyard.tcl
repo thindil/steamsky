fa3a8e1c903b8d5fb7aface780bdc809873502ce
thindil <>
2021-01-23 05:50:41 +0000

    redesigned look of remove modules list in shipyard
    FossilOrigin-Name: 388dc5b1b78fd4f087de24c6c165c8b66b58ccd631e0c59e2714429fe66b5eb2

--- bin/data/ui/shipyard.tcl
+++ bin/data/ui/shipyard.tcl
@@ -31,25 +31,4 @@ grid [ttk::entry $sinstall.options.search -validate key -validatecommand {Search
 $shipyardframe.notebook add $sinstall -text {Install}
 # Remove modules
 set sremove [ttk::frame $shipyardframe.notebook.remove]
-grid [ttk::frame $sremove.modules] -sticky nwes
-set shipyardview [ttk::treeview $sremove.modules.view -show headings -columns [list name type size material] -yscrollcommand [list $sremove.modules.scrolly set]]
-$shipyardview heading name -text {Name}
-$shipyardview heading type -text {Type}
-$shipyardview heading size -text {Size}
-$shipyardview heading material -text {Material}
-grid $shipyardview -sticky nwes
-bind $shipyardview <<TreeviewSelect>> ShowRemoveInfo
-grid [ttk::scrollbar $sremove.modules.scrolly -orient vertical -command [list $shipyardview yview]] -row 0 -column 1 -sticky ns
-# Module info
-set infoframe [ttk::frame $sremove.info]
-grid [ttk::labelframe $infoframe.info -text {Module info:}]
-set moduleinfo [text $infoframe.info.info -wrap char -height 10 -width 40]
-$moduleinfo tag configure red -foreground red
-grid $moduleinfo -sticky nwes
-grid [ttk::label $infoframe.info.damagelbl]
-grid [ttk::progressbar $infoframe.info.damage -orient horizontal -maximum 1.0] -row 1 -column 1
-grid [ttk::label $infoframe.info.description]
-grid [ttk::label $infoframe.money]
-grid [ttk::button $infoframe.remove -text {Remove module} -command {ManipulateModule remove}]
-grid $infoframe -column 1 -row 0
 $shipyardframe.notebook add $sremove -text {Remove}
