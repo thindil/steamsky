e98ac7e14bf93b4d6cc998b2c001334423f991c7
thindil <thindil@laeran.pl>
2021-02-04 05:33:08 +0000

    added labels to loops
    FossilOrigin-Name: 23f9994ccbbb6f476af9aa290d0422c959b347a22f59336d5655109f85ffbebb

--- src/halloffame.adb
+++ src/halloffame.adb
@@ -1,4 +1,4 @@
---    Copyright 2017-2020 Bartek thindil Jasicki
+--    Copyright 2017-2021 Bartek thindil Jasicki
 --
 --    This file is part of Steam Sky.
 --
@@ -51,6 +51,7 @@ package body HallOfFame is
       HoFData := Get_Tree(Reader);
       EntriesList :=
         DOM.Core.Documents.Get_Elements_By_Tag_Name(HoFData, "entry");
+      Load_Hall_Of_Fame_Loop :
       for I in 0 .. Length(EntriesList) - 1 loop
          EntryNode := Item(EntriesList, I);
          HallOfFame_Array(I + 1).Name :=
@@ -59,7 +60,7 @@ package body HallOfFame is
            Natural'Value(Get_Attribute(EntryNode, "points"));
          HallOfFame_Array(I + 1).DeathReason :=
            To_Unbounded_String(Get_Attribute(EntryNode, "deathreason"));
-      end loop;
+      end loop Load_Hall_Of_Fame_Loop;
       Free(Reader);
    exception
       when Ada.Directories.Name_Error =>
@@ -73,27 +74,30 @@ package body HallOfFame is
       EntryNode, MainNode: DOM.Core.Element;
       RawValue: Unbounded_String;
    begin
+      Find_New_Index_Loop :
       for I in HallOfFame_Array'Range loop
          if HallOfFame_Array(I).Points < GetGamePoints then
             NewIndex := I;
-            exit;
+            exit Find_New_Index_Loop;
          end if;
-      end loop;
+      end loop Find_New_Index_Loop;
       if NewIndex = 0 then
          return;
       end if;
+      Move_Hall_Of_Fame_Loop :
       for I in reverse NewIndex .. 9 loop
          HallOfFame_Array(I + 1) := HallOfFame_Array(I);
-      end loop;
+      end loop Move_Hall_Of_Fame_Loop;
       HallOfFame_Array(NewIndex) :=
         (Name => PlayerName, Points => GetGamePoints,
          DeathReason => DeathReason);
       HoFData := Create_Document(HoF);
       MainNode := Create_Element(HoFData, "halloffame");
       MainNode := Append_Child(HoFData, MainNode);
+      Update_Hall_Of_Fame_Loop :
       for I in HallOfFame_Array'Range loop
          if HallOfFame_Array(I).Name = Null_Unbounded_String then
-            exit;
+            exit Update_Hall_Of_Fame_Loop;
          end if;
          EntryNode := Create_Element(HoFData, "entry");
          EntryNode := Append_Child(MainNode, EntryNode);
@@ -105,7 +109,7 @@ package body HallOfFame is
          Set_Attribute
            (EntryNode, "deathreason",
             To_String(HallOfFame_Array(I).DeathReason));
-      end loop;
+      end loop Update_Hall_Of_Fame_Loop;
       Create(HoFFile, Out_File, To_String(SaveDirectory) & "halloffame.dat");
       Write(Stream => Stream(HoFFile), N => HoFData, Pretty_Print => True);
       Close(HoFFile);
