60b50af4202f6d4e0e4d7576b4bc12f036c0004c
thindil <thindil@laeran.pl>
2021-02-13 05:32:00 +0000

    removed addtext from the Table package
    FossilOrigin-Name: f82b1163d7b39ce0b7af8de23be9020147228b584533e9c5ff6d065aaaa0902e

--- src/ui/table.ads
+++ src/ui/table.ads
@@ -87,28 +87,6 @@ package Table is
    procedure ClearTable(Table: in out Table_Widget);
    -- ****
 
-   -- ****f* Table/Table.AddText
-   -- FUNCTION
-   -- Add text item to the selected Table_Widget
-   -- PARAMETERS
-   -- Table   - The Table_Widget in which text will be added
-   -- Text    - The text displayed on the item
-   -- Tooltip - The tooltip show when user hover mouse over text
-   -- Column  - The column in which the text will be added
-   -- NewRow  - If True, increase current number of row in the Table_Widget.
-   --           Default value is False.
-   -- Color   - The color of the text which will be added. If empty, use
-   --           default interface color. Default value is empty
-   -- OUTPUT
-   -- Updated Table parameter Table_Widget
-   -- HISTORY
-   -- 5.7 - Added
-   -- SOURCE
-   procedure AddText
-     (Table: in out Table_Widget; Text, Tooltip: String; Column: Positive;
-      NewRow: Boolean := False; Color: String := "");
-   -- ****
-
    -- ****f* Table/Table.AddButton
    -- FUNCTION
    -- Add button item to the selected Table_Widget
