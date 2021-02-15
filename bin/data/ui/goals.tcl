be077588e28fd859b9d200b7471b1f00b9867e99
thindil <thindil@laeran.pl>
2021-02-15 05:24:39 +0000

    started work on replacing dialog with frame for goals list
    FossilOrigin-Name: 9722678e37a24eafb21748ec15774bfa1916082b2e06c34b6bb2995c898b2174

--- bin/data/ui/goals.tcl
+++ bin/data/ui/goals.tcl
@@ -1,4 +1,4 @@
-# Copyright (c) 2020 Bartek thindil Jasicki <thindil@laeran.pl>
+# Copyright (c) 2020-2021 Bartek thindil Jasicki <thindil@laeran.pl>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -13,16 +13,11 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-toplevel .goalsdialog -class Dialog
-wm title .goalsdialog {Steam Sky - Select Goal}
-wm transient .goalsdialog .
-if {$tcl_platform(os) == "Linux"} {
-   wm attributes .goalsdialog -type dialog
-}
-set view [ttk::treeview .goalsdialog.view -show tree -yscrollcommand [list .goalsdialog.yscroll set] -xscrollcommand [list .goalsdialog.xscroll set]]
+ttk::frame .goalsdialog -style Dialog.TFrame
+set view [ttk::treeview .goalsdialog.view -show tree -yscrollcommand [list .goalsdialog.yscroll set] -height 9]
 set selectbutton [ttk::button .goalsdialog.selectbutton -text {Select goal}]
-grid $view -sticky nwes
-$view column #0 -minwidth 400 -stretch 1
+grid $view
+$view column #0 -width 450 -stretch 1
 $view insert {} end -id 0 -text Random
 $view insert {} end -id REPUTATION -text {Gain max reputation in bases}
 $view insert {} end -id DESTROY -text {Destroy enemy ships}
@@ -43,12 +38,8 @@ bind $view <<TreeviewSelect>> {
 bind $view <Double-1> {$selectbutton invoke}
 bind $view <Return> {$selectbutton invoke}
 grid [ttk::scrollbar .goalsdialog.yscroll -orient vertical -command [list $view yview]] -column 1 -row 0 -sticky ns
-grid [ttk::scrollbar .goalsdialog.xscroll -orient horizontal -command [list $view xview]] -row 1 -column 0 -sticky we -columnspan 2
-grid columnconfigure .goalsdialog $view -weight 1
-grid rowconfigure .goalsdialog $view -weight 1
 grid $selectbutton -row 2 -columnspan 2 -sticky we
 grid [ttk::button .goalsdialog.closebutton -text {Close (Escape)} -command {CloseDialog .goalsdialog}] -row 3 -columnspan 2 -sticky we
 bind .goalsdialog <Escape> {.goalsdialog.closebutton invoke}
-wm geometry .goalsdialog =400x400+[expr ([winfo vrootwidth .goalsdialog] / 2) - 200]+[expr [winfo vrootheight .goalsdialog] / 3]
 ::autoscroll::autoscroll .goalsdialog.yscroll
-::autoscroll::autoscroll .goalsdialog.xscroll
+place .goalsdialog -in . -relx 0.1 -rely 0.1
