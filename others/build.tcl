9bcd5c100d783a6170751e88a2a5710d929e514b
thindil <thindil@laeran.pl>
2021-03-25 05:28:57 +0000

    fixed location of files during build releases
    FossilOrigin-Name: 6dad0da2576973c22f384a205519ba106328c95914f70d5ed3a3b3e51f271b74

--- others/build.tcl
+++ others/build.tcl
@@ -30,13 +30,15 @@ exec gprclean -P steamsky.gpr --target=$target >@stdout
 exec gprbuild -p -P steamsky.gpr -XMode=release --target=$target >@stdout
 puts -nonewline {Copying files and directories ... }
 if {$target == "x86_64-linux-gnu"} {
-   file mkdir usr/bin usr/share/metainfo
+   file mkdir usr/bin usr/share/metainfo usr/share/doc/steamsky usr/share/steamsky
    file copy share/fonts usr/share/
    file copy bin/steamsky usr/bin
-   file copy bin/data usr/share/
-   file delete -force usr/share/data/mods usr/share/data/saves usr/share/data/themes
-   file copy bin/doc usr/share/
-   file copy README.md usr/share/doc
+   file copy bin/data usr/share/steamsky
+   file delete -force usr/share/steamsky/data/mods usr/share/steamsky/data/saves usr/share/steamsky/data/themes
+   foreach file [glob bin/doc/*] {
+      file copy $file usr/share/doc/steamsky
+   }
+   file copy README.md usr/share/doc/steamsky
    file copy others/steamsky.appdata.xml usr/share/metainfo
 } else {
    file mkdir release
