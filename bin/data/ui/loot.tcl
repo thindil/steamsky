6c3546e2b8d8d221cbc69671c8d4abfb95d19bd6
thindil <thindil@laeran.pl>
2021-05-04 04:21:01 +0000

    removed unused code
    FossilOrigin-Name: 1dd90e8c39e1179c07355b61080e3040543f7faaa55ee7344eea2760357441af

--- bin/data/ui/loot.tcl
+++ bin/data/ui/loot.tcl
@@ -26,23 +26,3 @@ grid [ttk::frame $lootframe.options] -sticky w -padx 5 -pady 5
 grid [ttk::label $lootframe.options.typelabel -text {Type:}]
 grid [ttk::combobox $lootframe.options.type -state readonly] -column 1 -row 0
 bind $lootframe.options.type <<ComboboxSelected>> {ShowLoot [$lootframe.options.type get]}
-# Item info
-#set itemframe [ttk::frame $lootframe.item]
-#grid [ttk::labelframe $itemframe.info -text {Item Info:}]
-#grid [text $itemframe.info.text -wrap char -height 10 -width 40]
-## Item actions
-#grid [ttk::label $itemframe.shipspace]
-#grid [ttk::frame $itemframe.takeframe]
-#grid [ttk::button $itemframe.takeframe.take -text {Take} -command {LootItem take}]
-#grid [ttk::label $itemframe.takeframe.amountlbl] -column 1 -row 0
-#grid [ttk::spinbox $itemframe.takeframe.amount -from 1 -validate key] -column 2 -row 0
-#grid [ttk::label $itemframe.takeframe.orlbl -text {or}] -column 3 -row 0
-#grid [ttk::button $itemframe.takeframe.takemax -text {Take all} -command {LootItem takeall}] -column 4 -row 0
-#grid [ttk::frame $itemframe.dropframe]
-#grid [ttk::button $itemframe.dropframe.drop -text {Drop} -command {LootItem drop}]
-#grid [ttk::label $itemframe.dropframe.amountlbl] -column 1 -row 0
-#grid [ttk::spinbox $itemframe.dropframe.amount -from 1 -validate key] -column 2 -row 0
-#grid [ttk::label $itemframe.dropframe.orlbl -text {or}] -column 3 -row 0
-#grid [ttk::button $itemframe.dropframe.dropmax -text {Drop all} -command {LootItem dropall}] -column 4 -row 0
-#grid [ttk::label $itemframe.dropframe.error -style Headerred.TLabel] -columnspan 4
-#grid $itemframe -row 1 -column 1 -sticky nwes
