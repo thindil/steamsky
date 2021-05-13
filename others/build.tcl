f8dc6fec406210e3c0f000f186869d757c51052d
thindil <thindil@laeran.pl>
2021-05-13 04:42:21 +0000

    updated build script
    FossilOrigin-Name: 3b63fc13f81c9d116994152105f1d68145e4bc384008229385b86ecc2c0fa220

--- others/build.tcl
+++ others/build.tcl
@@ -29,22 +29,19 @@ if {$target != "x86_64-linux-gnu" && $target != "x86_64-windows"} {
 exec gprclean -P steamsky.gpr --target=$target >@stdout
 if {$target == "x86_64-linux-gnu"} {
    exec gprbuild -p -P steamsky.gpr -XMode=release -XOS=Unix --target=$target >@stdout
-   puts -nonewline {Copying files and directories ... }
-   file mkdir usr/bin usr/share/metainfo usr/share/doc/steamsky usr/share/steamsky
-   file copy bin/steamsky usr/bin
-   file copy bin/data usr/share/steamsky
-   file delete -force usr/share/steamsky/data/mods usr/share/steamsky/data/saves usr/share/steamsky/data/themes
-   foreach file [glob bin/doc/*] {
-      file copy $file usr/share/doc/steamsky
-   }
-   file copy README.md usr/share/doc/steamsky
-   file copy others/steamsky.appdata.xml usr/share/metainfo
+   set os linux
+   set extension {}
 } else {
    exec gprbuild -p -P steamsky.gpr -XMode=release -XOS=Windows --target=$target >@stdout
-   puts -nonewline {Copying files and directories ... }
-   file mkdir release
-   file copy bin [file join release bin]
-   file copy README.md [file join release bin doc]
+   set os windows
+   set extension .exe
 }
+puts -nonewline {Copying files and directories ... }
+file mkdir [file join release $os]
+file copy [file join bin steamsky$extension] [file join release $os steamsky$extension]
+file copy [file join bin data] [file join release $os data]
+file delete -force [file join release $os data mods] [file join release $os data saves] [file join release $os data themes]
+file copy [file join bin doc] [file join release $os doc]
+file copy README.md [file join release $os doc]
 puts {done}
 exec gprclean -P steamsky.gpr --target=$target >@stdout
