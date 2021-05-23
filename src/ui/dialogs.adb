b4734bba96e5924efd667fac783f6a0ec6cb06cc
thindil <thindil@laeran.pl>
2021-05-23 04:28:44 +0000

    started work on moving dialogs to separated package
    FossilOrigin-Name: 05903b54b3fab6b9cec83b66a9243096b0e2a90065d1e9fbb1757e6b95b30fab

---
+++ src/ui/dialogs.adb
@@ -0,0 +1,50 @@
+-- Copyright (c) 2021 Bartek thindil Jasicki <thindil@laeran.pl>
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+with Tcl.Tk.Ada; use Tcl.Tk.Ada;
+with Tcl.Tk.Ada.Busy;
+with Tcl.Tk.Ada.Grid;
+with Tcl.Tk.Ada.Widgets; use Tcl.Tk.Ada.Widgets;
+with Tcl.Tk.Ada.Widgets.TtkLabel; use Tcl.Tk.Ada.Widgets.TtkLabel;
+with CoreUI; use CoreUI;
+
+package body Dialogs is
+
+   function Create_Dialog
+     (Name, Title: String; Title_Width: Positive := 275;
+      Columns: Positive := 1; Parent_Name: String := ".gameframe")
+      return Ttk_Frame is
+      New_Dialog: constant Ttk_Frame := Create(Name, "-style Dialog.TFrame");
+      Dialog_Header: constant Ttk_Label :=
+        Create
+          (New_Dialog & ".header",
+           "-text {" & Title & "} -wraplength" & Positive'Image(Title_Width) &
+           " -style Header.TLabel");
+   begin
+      if Parent_Name = ".gameframe" then
+         Tcl.Tk.Ada.Busy.Busy(Game_Header);
+         Tcl.Tk.Ada.Busy.Busy(Main_Paned);
+      else
+         Tcl.Tk.Ada.Busy.Busy(Ttk_Frame'(Get_Widget(Parent_Name)));
+      end if;
+      Tcl.Tk.Ada.Grid.Grid
+        (Dialog_Header,
+         "-sticky we -padx 2 -pady {2 0}" &
+         (if Columns > 1 then " -columnspan" & Positive'Image(Columns)
+          else ""));
+      return New_Dialog;
+   end Create_Dialog;
+
+end Dialogs;
---
+++ src/ui/dialogs.ads
@@ -0,0 +1,44 @@
+-- Copyright (c) 2021 Bartek thindil Jasicki <thindil@laeran.pl>
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+with Tcl.Tk.Ada.Widgets.TtkFrame; use Tcl.Tk.Ada.Widgets.TtkFrame;
+
+-- ****h* Dialogs/Dialogs
+-- FUNCTION
+-- Provide code to show various in game dialogs (messages, questins, etc)
+-- SOURCE
+package Dialogs is
+-- ****
+
+   -- ****f* Dialogs/Dialogs.Create_Dialog
+   -- FUNCTION
+   -- Create a new dialog with the selected title
+   -- PARAMETERS
+   -- Name        - The Tk path name of the new dialog
+   -- Title       - The title of the new dialog
+   -- Title_Width - The maximum width of the title. Used to set wrapping for
+   --               it. Default value is 275 pixels. Can be empty.
+   -- Columns     - The amount of columns which dialog will have. Used to set
+   --               the title. Default value is 1 column. Can be empty.
+   -- RESULT
+   -- The newly created Dialog as Ttk_Frame
+   -- SOURCE
+   function Create_Dialog
+     (Name, Title: String; Title_Width: Positive := 275;
+      Columns: Positive := 1; Parent_Name: String := ".gameframe")
+      return Ttk_Frame;
+      -- ****
+
+end Dialogs;
