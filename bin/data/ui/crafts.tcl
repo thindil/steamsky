f3edbca624ad613ea9ead0097aa1535d475db880
thindil <thindil@laeran.pl>
2021-06-01 04:31:16 +0000

    removed unused code
    FossilOrigin-Name: f628eddca04d91a3e95aaa908a3d69a0e5ecd781fae9ed68320603a594afe0ec

--- bin/data/ui/crafts.tcl
+++ bin/data/ui/crafts.tcl
@@ -21,34 +21,3 @@ pack [ttk::scrollbar .gameframe.paned.craftframe.scrollx -orient horizontal -com
 ::autoscroll::autoscroll .gameframe.paned.craftframe.scrolly
 ::autoscroll::autoscroll .gameframe.paned.craftframe.scrollx
 set craftframe [ttk::frame $craftcanvas.craft]
-# Recipes list
-grid [ttk::frame $craftframe.list] -sticky nwes -padx 5 -pady 5
-set craftview [ttk::treeview $craftframe.list.view -yscrollcommand [list $craftframe.list.scrolly set] -height 12]
-$craftview heading #0 -text {Name}
-$craftview column #0 -width 300 -minwidth 300
-$craftview tag configure gray -foreground gray -font OverstrikedFont
-grid $craftview -sticky nwes
-bind $craftview <<TreeviewSelect>> ShowRecipeInfo
-tooltip::tooltip $craftview {Select the recipe to craft}
-grid [ttk::scrollbar $craftframe.list.scrolly -orient vertical -command [list $craftview yview]] -row 0 -column 1 -sticky ns
-grid columnconfigure $craftframe 0 -weight 1
-grid rowconfigure $craftframe 0 -weight 1
-# Recipe info
-set recipeframe [ttk::frame $craftframe.item]
-grid $recipeframe -row 0 -column 1 -sticky n -padx 5 -pady 5
-grid [ttk::labelframe $recipeframe.info -text {Recipe Info:}]
-grid [text $recipeframe.info.text -wrap char -height 10 -width 40]
-$recipeframe.info.text tag configure red -foreground red
-update
-# Recipe actions
-grid [ttk::frame $recipeframe.set] -sticky w -pady 5
-grid [ttk::button $recipeframe.set.button -text Craft -command SetCrafting]
-tooltip::tooltip $recipeframe.set.button "Set the recipe for craft. After this, you will have to\nassign crew member to the work"
-grid [ttk::button $recipeframe.set.maxamount -command {$recipeframe.set.amount set [lindex [split [$recipeframe.set.maxamount cget -text]] 1]}] -column 1 -row 0
-tooltip::tooltip $recipeframe.set.maxamount "Set the maximum amount of how many times the recipe\nwill be made"
-grid [ttk::spinbox $recipeframe.set.amount -from 1 -increment 1 -validate key -validatecommand {ValidateSpinbox %W %P} -width 5] -column 2 -row 0
-tooltip::tooltip $recipeframe.set.amount {Set how many times the recipe should be crafted}
-grid [ttk::label $recipeframe.set.label -text {in workshop:}]
-grid [ttk::combobox $recipeframe.set.workshop -state readonly] -column 1 -row 1 -columnspan 2
-tooltip::tooltip $recipeframe.set.workshop {Select the workshop in which the recipe will be crafted}
-grid [ttk::label $recipeframe.error -style Headerred.TLabel -wraplength [winfo reqwidth $recipeframe]] -sticky w
