5139788b8cb887e6e4348b4cf7b3f31e6d583c33
thindil <thindil@laeran.pl>
2021-06-05 04:40:18 +0000

    updated unit tests
    FossilOrigin-Name: a253cbf5899a518ee144e7001ef60e1994a0df2987d8d84a3c54deec8829450f

--- tests/ships-test_data-tests.ads
+++ tests/ships-test_data-tests.ads
@@ -11,22 +11,22 @@ package Ships.Test_Data.Tests is
    null record;
 
    procedure Test_Create_Ship_14edf1_c9230b(Gnattest_T: in out Test);
-   --  ships.ads:329:4:Create_Ship:Test_CreateShip
+   --  ships.ads:392:4:Create_Ship:Test_CreateShip
 
    procedure Test_Count_Ship_Weight_dec0b9_0591fd(Gnattest_T: in out Test);
-   --  ships.ads:355:4:Count_Ship_Weight:Test_CountShipWeight
+   --  ships.ads:418:4:Count_Ship_Weight:Test_CountShipWeight
 
    procedure Test_Generate_Ship_Name_7b8806_7313c0(Gnattest_T: in out Test);
-   --  ships.ads:367:4:Generate_Ship_Name:Test_GenerateShipName
+   --  ships.ads:430:4:Generate_Ship_Name:Test_GenerateShipName
 
    procedure Test_Count_Combat_Value_145322_424a30(Gnattest_T: in out Test);
-   --  ships.ads:379:4:Count_Combat_Value:Test_CountCombatValue
+   --  ships.ads:442:4:Count_Combat_Value:Test_CountCombatValue
 
    procedure Test_Get_Cabin_Quality_3a9d5d_0027a0(Gnattest_T: in out Test);
-   --  ships.ads:391:4:Get_Cabin_Quality:Test_GetCabinQuality
+   --  ships.ads:454:4:Get_Cabin_Quality:Test_GetCabinQuality
 
    procedure Test_Damage_Module_222cf0_bb3bbf(Gnattest_T: in out Test);
-   --  ships.ads:405:4:Damage_Module:Test_DamageModule
+   --  ships.ads:468:4:Damage_Module:Test_DamageModule
 
 end Ships.Test_Data.Tests;
 --  end read only
