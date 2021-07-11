1f2c8c68bdd3e1d5ecf35f7e7d22fd716b1c8482
thindil <thindil@laeran.pl>
2021-07-11 04:29:05 +0000

    updated unit tests
    FossilOrigin-Name: 7c2cca5c0d162dd36564c726702474b57df127041869b9892499b134d88c07ad

--- tests/maps-test_data-tests.ads
+++ tests/maps-test_data-tests.ads
@@ -13,7 +13,7 @@ package Maps.Test_Data.Tests is
    procedure Test_CountDistance_ecd188_2a2146(Gnattest_T: in out Test);
    --  maps.ads:63:4:CountDistance:Test_CountDistance
 
-   procedure Test_NormalizeCoord_6338a5_63c4fc(Gnattest_T: in out Test);
+   procedure Test_NormalizeCoord_6338a5_1a8ae8(Gnattest_T: in out Test);
    --  maps.ads:77:4:NormalizeCoord:Test_NormalizeCoord
 
 end Maps.Test_Data.Tests;
