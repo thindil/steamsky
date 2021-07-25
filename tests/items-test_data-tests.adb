d8a65b347713a57ad63c5a1f4b4a5286cdc12186
thindil <thindil@laeran.pl>
2021-07-25 04:18:14 +0000

    updated unit tests
    FossilOrigin-Name: 01807ab926439085b8a32c42c26da0d0f8c4deb549968382a67bd73c5f4067c4

--- tests/items-test_data-tests.adb
+++ tests/items-test_data-tests.adb
@@ -197,7 +197,8 @@ package body Items.Test_Data.Tests is
       pragma Unreferenced(Gnattest_T);
       Item: InventoryData :=
         (ProtoIndex => To_Unbounded_String("2"), Amount => 1,
-         Name => Null_Unbounded_String, Durability => 80, Price => 0);
+         Name => Null_Unbounded_String, Durability => 80, Price => 0,
+         Used => False);
 
    begin
 
