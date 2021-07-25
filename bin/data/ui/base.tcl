cbc3ebad5918f6c9ce1a2c387fb7370b07038761
thindil <thindil@laeran.pl>
2021-07-25 04:18:30 +0000

    code formatting
    FossilOrigin-Name: c87d01565f70d69b1ba5a0bfa692e7a50fb3ae716e40039155aae8b244dcd2e7

--- bin/data/ui/base.tcl
+++ bin/data/ui/base.tcl
@@ -14,10 +14,14 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ttk::frame .gameframe.paned.baseframe
-set basecanvas [canvas .gameframe.paned.baseframe.canvas -yscrollcommand [list .gameframe.paned.baseframe.scrolly set] -xscrollcommand [list .gameframe.paned.baseframe.scrollx set]]
-pack [ttk::scrollbar .gameframe.paned.baseframe.scrolly -orient vertical -command [list $basecanvas yview]] -side right -fill y -pady 5
+set basecanvas [canvas .gameframe.paned.baseframe.canvas \
+   -yscrollcommand [list .gameframe.paned.baseframe.scrolly set] \
+   -xscrollcommand [list .gameframe.paned.baseframe.scrollx set]]
+pack [ttk::scrollbar .gameframe.paned.baseframe.scrolly -orient vertical \
+   -command [list $basecanvas yview]] -side right -fill y -pady 5
 pack $basecanvas -side top -fill both -pady 5
-pack [ttk::scrollbar .gameframe.paned.baseframe.scrollx -orient horizontal -command [list $basecanvas xview]] -fill x
+pack [ttk::scrollbar .gameframe.paned.baseframe.scrollx -orient horizontal \
+   -command [list $basecanvas xview]] -fill x
 SetScrollbarBindings $basecanvas .gameframe.paned.baseframe.scrolly
 ::autoscroll::autoscroll .gameframe.paned.baseframe.scrolly
 ::autoscroll::autoscroll .gameframe.paned.baseframe.scrollx
@@ -26,6 +30,7 @@ SetScrollbarBindings $baseframe .gameframe.paned.baseframe.scrolly
 grid [ttk::frame $baseframe.searchframe] -sticky w -padx 5 -pady 5
 grid [ttk::label $baseframe.searchframe.searchlabel -text {Name:}] -padx {0 5}
 tooltip::tooltip $baseframe.searchframe.searchlabel "Search for the selected recipe."
-grid [ttk::entry $baseframe.searchframe.search -validate key -validatecommand {SearchRecipes %P}] -row 0 -column 1
+grid [ttk::entry $baseframe.searchframe.search -validate key \
+   -validatecommand {SearchRecipes %P}] -row 0 -column 1
 tooltip::tooltip $baseframe.searchframe.search "Search for the selected recipe."
 grid [ttk::label $baseframe.lblmoney] -sticky w -padx 5
