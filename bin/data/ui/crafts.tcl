1b9624a3ef28b32482ba250cb3d87584563d58ed
thindil <thindil@laeran.pl>
2021-07-27 03:55:19 +0000

    code formatting
    FossilOrigin-Name: 3ad46a3114e491040ce615b0bf1376d4f2bb17ce9660612f9297697cb11dfe2c

--- bin/data/ui/crafts.tcl
+++ bin/data/ui/crafts.tcl
@@ -14,17 +14,23 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ttk::frame .gameframe.paned.craftframe
-set craftcanvas [canvas .gameframe.paned.craftframe.canvas -yscrollcommand [list .gameframe.paned.craftframe.scrolly set] -xscrollcommand [list .gameframe.paned.craftframe.scrollx set]]
-pack [ttk::scrollbar .gameframe.paned.craftframe.scrolly -orient vertical -command [list $craftcanvas yview]] -side right -fill y
+set craftcanvas [canvas .gameframe.paned.craftframe.canvas \
+   -yscrollcommand [list .gameframe.paned.craftframe.scrolly set] \
+   -xscrollcommand [list .gameframe.paned.craftframe.scrollx set]]
+pack [ttk::scrollbar .gameframe.paned.craftframe.scrolly -orient vertical \
+   -command [list $craftcanvas yview]] -side right -fill y
 pack $craftcanvas -side top -fill both
-pack [ttk::scrollbar .gameframe.paned.craftframe.scrollx -orient horizontal -command [list $craftcanvas xview]] -fill x
+pack [ttk::scrollbar .gameframe.paned.craftframe.scrollx -orient horizontal \
+   -command [list $craftcanvas xview]] -fill x
 ::autoscroll::autoscroll .gameframe.paned.craftframe.scrolly
 ::autoscroll::autoscroll .gameframe.paned.craftframe.scrollx
 set craftframe [ttk::frame $craftcanvas.craft]
 grid [ttk::frame $craftframe.sframe] -sticky w
 grid [ttk::label $craftframe.sframe.searchlabel -text {Name:}]
-tooltip::tooltip $craftframe.sframe.searchlabel {Search for the selected recipe.}
-grid [ttk::entry $craftframe.sframe.search -validate key -validatecommand {ShowCrafting 1 %P} -width 30] -sticky w -row 0 -column 1
+tooltip::tooltip $craftframe.sframe.searchlabel \
+   {Search for the selected recipe.}
+grid [ttk::entry $craftframe.sframe.search -validate key \
+   -validatecommand {ShowCrafting 1 %P} -width 30] -sticky w -row 0 -column 1
 tooltip::tooltip $craftframe.sframe.search {Search for the selected recipe.}
 SetScrollbarBindings $craftcanvas .gameframe.paned.craftframe.scrolly
 SetScrollbarBindings $craftframe .gameframe.paned.craftframe.scrolly
