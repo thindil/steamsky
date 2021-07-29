e249dc8885793a9e9060187a6b8e2da68ea7b0e1
thindil <thindil@laeran.pl>
2021-07-29 04:01:52 +0000

    code formatting
    FossilOrigin-Name: 0270484c463c29fec1d2753baa8009545c9bf835097552d8c9a3165f34355414

--- bin/data/ui/errordialog.tcl
+++ bin/data/ui/errordialog.tcl
@@ -1,4 +1,4 @@
-# Copyright (c) 2020 Bartek thindil Jasicki <thindil@laeran.pl>
+# Copyright (c) 2020-2021 Bartek thindil Jasicki <thindil@laeran.pl>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,14 +14,26 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 wm title . {Steam Sky - Error info}
-wm geometry . 800x600+[expr ([winfo vrootwidth .] - 800) / 2]+[expr ([winfo vrootheight .] - 600) / 2]
-pack [ttk::label .generalinfo -text {Oops, something bad happens and the game has crashed. Game should save your progress, but better verify this yourself. Also, please, remember what you were doing before the crash and report this problem at} -wraplength 790]
-pack [ttk::button .reportlink -text {https://www.laeran.pl/repositories/steamsky/ticket} -command {OpenLink https://www.laeran.pl/repositories/steamsky/ticket} -style Toolbutton]
-pack [ttk::label .generalinfo2 -text {or if you prefer, on one of the game community options:}]
-pack [ttk::button .reportlink2 -text {https://thindil.itch.io/steam-sky} -command {OpenLink https://thindil.itch.io/steam-sky} -style Toolbutton]
-pack [ttk::label .generalinfo3 -text {and attach (if possible) file 'error.log'}]
+wm geometry . 800x600+[expr ([winfo vrootwidth .] - 800) / 2]+[expr \
+   ([winfo vrootheight .] - 600) / 2]
+pack [ttk::label .generalinfo \
+   -text {Oops, something bad happens and the game has crashed. Game should save your progress, but better verify this yourself. Also, please, remember what you were doing before the crash and report this problem at} \
+   -wraplength 790]
+pack [ttk::button .reportlink \
+   -text {https://www.laeran.pl/repositories/steamsky/ticket} \
+   -command {OpenLink https://www.laeran.pl/repositories/steamsky/ticket} \
+   -style Toolbutton]
+pack [ttk::label .generalinfo2 \
+   -text {or if you prefer, on one of the game community options:}]
+pack [ttk::button .reportlink2 -text {https://thindil.itch.io/steam-sky} \
+   -command {OpenLink https://thindil.itch.io/steam-sky} -style Toolbutton]
+pack [ttk::label .generalinfo3 \
+   -text {and attach (if possible) file 'error.log'}]
 pack [ttk::button .close -text {Close} -command {exit}]
 ttk::labelframe .technical -text {Technical information:}
-pack [ttk::scrollbar .technical.scroll -orient vertical -command [list .technical.text yview]] -side right -fill y
-pack [text .technical.text -wrap word -yscrollcommand [list .technical.scroll set]] -fill both -expand true -side top
+pack [ttk::scrollbar .technical.scroll -orient vertical \
+   -command [list .technical.text yview]] -side right -fill y
+pack [text .technical.text -wrap word \
+   -yscrollcommand [list .technical.scroll set]] -fill both -expand true \
+   -side top
 pack .technical -fill both -expand true
