5ff5dbae92e0cb5780ba63e27f487394f572a035
thindil <thindil@laeran.pl>
2021-08-02 04:13:24 +0000

    updated unit tests
    FossilOrigin-Name: ad4408e15fe0197c34d10c3a9261c736e6aae71ea9e7f224aa0d35fc565ed416

--- tests/items-test_data-tests.ads
+++ tests/items-test_data-tests.ads
@@ -11,22 +11,22 @@ package Items.Test_Data.Tests is
    null record;
 
    procedure Test_FindProtoItem_f36791_112bba(Gnattest_T: in out Test);
-   --  items.ads:197:4:FindProtoItem:Test_FindProtoItem
+   --  items.ads:186:4:FindProtoItem:Test_FindProtoItem
 
    procedure Test_GetItemDamage_dedcfa_216935(Gnattest_T: in out Test);
-   --  items.ads:213:4:GetItemDamage:Test_GetItemDamage
+   --  items.ads:202:4:GetItemDamage:Test_GetItemDamage
 
    procedure Test_GetItemName_2db285_611409(Gnattest_T: in out Test);
-   --  items.ads:231:4:GetItemName:Test_GetItemName
+   --  items.ads:220:4:GetItemName:Test_GetItemName
 
    procedure Test_DamageItem_f848d1_f75741(Gnattest_T: in out Test);
-   --  items.ads:251:4:DamageItem:Test_DamageItem
+   --  items.ads:240:4:DamageItem:Test_DamageItem
 
    procedure Test_FindItem_5f6068_73ae4d(Gnattest_T: in out Test);
-   --  items.ads:270:4:FindItem:Test_FindItem
+   --  items.ads:259:4:FindItem:Test_FindItem
 
    procedure Test_GetItemChanceToDamage_71801d_14d19a(Gnattest_T: in out Test);
-   --  items.ads:294:4:GetItemChanceToDamage:Test_GetItemChanceToDamage
+   --  items.ads:283:4:GetItemChanceToDamage:Test_GetItemChanceToDamage
 
 end Items.Test_Data.Tests;
 --  end read only
--- tests/ships-test_data-tests.ads
+++ tests/ships-test_data-tests.ads
@@ -11,22 +11,22 @@ package Ships.Test_Data.Tests is
    null record;
 
    procedure Test_Create_Ship_14edf1_469250(Gnattest_T: in out Test);
-   --  ships.ads:525:4:Create_Ship:Test_CreateShip
+   --  ships.ads:398:4:Create_Ship:Test_CreateShip
 
    procedure Test_Count_Ship_Weight_dec0b9_0591fd(Gnattest_T: in out Test);
-   --  ships.ads:550:4:Count_Ship_Weight:Test_CountShipWeight
+   --  ships.ads:423:4:Count_Ship_Weight:Test_CountShipWeight
 
    procedure Test_Generate_Ship_Name_7b8806_7313c0(Gnattest_T: in out Test);
-   --  ships.ads:562:4:Generate_Ship_Name:Test_GenerateShipName
+   --  ships.ads:435:4:Generate_Ship_Name:Test_GenerateShipName
 
    procedure Test_Count_Combat_Value_145322_424a30(Gnattest_T: in out Test);
-   --  ships.ads:574:4:Count_Combat_Value:Test_CountCombatValue
+   --  ships.ads:447:4:Count_Combat_Value:Test_CountCombatValue
 
    procedure Test_Get_Cabin_Quality_3a9d5d_bc7a0e(Gnattest_T: in out Test);
-   --  ships.ads:586:4:Get_Cabin_Quality:Test_GetCabinQuality
+   --  ships.ads:459:4:Get_Cabin_Quality:Test_GetCabinQuality
 
    procedure Test_Damage_Module_222cf0_819b51(Gnattest_T: in out Test);
-   --  ships.ads:601:4:Damage_Module:Test_DamageModule
+   --  ships.ads:474:4:Damage_Module:Test_DamageModule
 
 end Ships.Test_Data.Tests;
 --  end read only
