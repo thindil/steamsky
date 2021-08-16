39464860c0640bc6e84783b2a21e08d85be1a0b8
thindil <thindil@laeran.pl>
2021-08-16 03:45:29 +0000

    code formatting
    FossilOrigin-Name: b6f4e2b05a400624c96fb013cf4b5608211c81c78a4461a4c525775279726c72

--- bin/data/ui/stats.tcl
+++ bin/data/ui/stats.tcl
@@ -14,10 +14,14 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ttk::frame .gameframe.paned.statsframe
-set statscanvas [canvas .gameframe.paned.statsframe.canvas -yscrollcommand [list .gameframe.paned.statsframe.scrolly set] -xscrollcommand [list .gameframe.paned.statsframe.scrollx set]]
-pack [ttk::scrollbar .gameframe.paned.statsframe.scrolly -orient vertical -command [list $statscanvas yview]] -side right -fill y
+set statscanvas [canvas .gameframe.paned.statsframe.canvas \
+   -yscrollcommand [list .gameframe.paned.statsframe.scrolly set] \
+   -xscrollcommand [list .gameframe.paned.statsframe.scrollx set]]
+pack [ttk::scrollbar .gameframe.paned.statsframe.scrolly -orient vertical \
+   -command [list $statscanvas yview]] -side right -fill y
 pack $statscanvas -side top -fill both
-pack [ttk::scrollbar .gameframe.paned.statsframe.scrollx -orient horizontal -command [list $statscanvas xview]] -fill x
+pack [ttk::scrollbar .gameframe.paned.statsframe.scrollx -orient horizontal \
+   -command [list $statscanvas xview]] -fill x
 ::autoscroll::autoscroll .gameframe.paned.statsframe.scrolly
 ::autoscroll::autoscroll .gameframe.paned.statsframe.scrollx
 SetScrollbarBindings $statscanvas .gameframe.paned.statsframe.scrolly
@@ -25,71 +29,108 @@ set statsframe [ttk::frame $statscanvas.stats]
 grid [ttk::frame $statsframe.left] -sticky nwes -padx 5
 SetScrollbarBindings $statsframe.left .gameframe.paned.statsframe.scrolly
 grid [ttk::label $statsframe.left.points] -sticky w
-SetScrollbarBindings $statsframe.left.points .gameframe.paned.statsframe.scrolly
+SetScrollbarBindings $statsframe.left.points \
+   .gameframe.paned.statsframe.scrolly
 grid [ttk::label $statsframe.left.time] -sticky w
 SetScrollbarBindings $statsframe.left.time .gameframe.paned.statsframe.scrolly
 grid [ttk::label $statsframe.left.bases] -sticky w
-SetScrollbarBindings $statsframe.left.bases .gameframe.paned.statsframe.scrolly
+SetScrollbarBindings $statsframe.left.bases \
+   .gameframe.paned.statsframe.scrolly
 grid [ttk::label $statsframe.left.map] -sticky w
 SetScrollbarBindings $statsframe.left.map .gameframe.paned.statsframe.scrolly
 grid [ttk::label $statsframe.left.distance] -sticky w
-SetScrollbarBindings $statsframe.left.distance .gameframe.paned.statsframe.scrolly
+SetScrollbarBindings $statsframe.left.distance \
+   .gameframe.paned.statsframe.scrolly
 grid [ttk::label $statsframe.left.crafts] -sticky w
 SetScrollbarBindings $statsframe.left.crafts .gameframe.paned.statsframe.scrolly
 grid [ttk::frame $statsframe.left.craftsframe] -sticky w
-grid [ttk::treeview $statsframe.left.craftsframe.craftsview -show headings -columns [list name amount] -selectmode none -yscrollcommand [list $statsframe.left.craftsframe.scrolly set]] -sticky nwes
+grid [ttk::treeview $statsframe.left.craftsframe.craftsview -show headings \
+   -columns [list name amount] -selectmode none \
+   -yscrollcommand [list $statsframe.left.craftsframe.scrolly set]] \
+   -sticky nwes
 $statsframe.left.craftsframe.craftsview heading name -text {Name}
 $statsframe.left.craftsframe.craftsview column name -width 250
 $statsframe.left.craftsframe.craftsview heading amount -text {Amount}
 $statsframe.left.craftsframe.craftsview column amount -width 75 -anchor center
-tooltip::tooltip $statsframe.left.craftsframe.craftsview {The list of finished crafting orders}
-grid [ttk::scrollbar $statsframe.left.craftsframe.scrolly -orient vertical -command [list $statsframe.left.craftsframe.craftsview yview]] -row 0 -column 1 -sticky ns
+tooltip::tooltip $statsframe.left.craftsframe.craftsview \
+   {The list of finished crafting orders}
+grid [ttk::scrollbar $statsframe.left.craftsframe.scrolly -orient vertical \
+   -command [list $statsframe.left.craftsframe.craftsview yview]] -row 0 \
+   -column 1 -sticky ns
 ::autoscroll::autoscroll $statsframe.left.craftsframe.scrolly
 grid [ttk::label $statsframe.left.missions] -sticky w
-SetScrollbarBindings $statsframe.left.missions .gameframe.paned.statsframe.scrolly
+SetScrollbarBindings $statsframe.left.missions \
+   .gameframe.paned.statsframe.scrolly
 grid [ttk::frame $statsframe.left.missionsframe] -sticky w
-grid [ttk::treeview $statsframe.left.missionsframe.missionsview -show headings -columns [list name amount] -selectmode none -yscrollcommand [list $statsframe.left.missionsframe.scrolly set]]
+grid [ttk::treeview $statsframe.left.missionsframe.missionsview \
+   -show headings -columns [list name amount] -selectmode none \
+   -yscrollcommand [list $statsframe.left.missionsframe.scrolly set]]
 $statsframe.left.missionsframe.missionsview heading name -text {Name}
 $statsframe.left.missionsframe.missionsview column name -width 250
 $statsframe.left.missionsframe.missionsview heading amount -text {Amount}
-$statsframe.left.missionsframe.missionsview column amount -width 75 -anchor center
-tooltip::tooltip $statsframe.left.missionsframe.missionsview {The list of finished missions}
-grid [ttk::scrollbar $statsframe.left.missionsframe.scrolly -orient vertical -command [list $statsframe.left.missionsframe.missionsview yview]] -row 0 -column 1 -sticky ns
+$statsframe.left.missionsframe.missionsview column amount -width 75 \
+   -anchor center
+tooltip::tooltip $statsframe.left.missionsframe.missionsview \
+   {The list of finished missions}
+grid [ttk::scrollbar $statsframe.left.missionsframe.scrolly -orient vertical \
+   -command [list $statsframe.left.missionsframe.missionsview yview]] -row 0 \
+   -column 1 -sticky ns
 ::autoscroll::autoscroll $statsframe.left.missionsframe.scrolly
-grid [ttk::button $statsframe.left.goal -text {Goals} -command {ShowGoals $statsframe.left.goal}] -sticky w
+grid [ttk::button $statsframe.left.goal -text {Goals} \
+   -command {ShowGoals $statsframe.left.goal}] -sticky w
 grid [ttk::label $statsframe.left.goals] -sticky w
 SetScrollbarBindings $statsframe.left.goals .gameframe.paned.statsframe.scrolly
 grid [ttk::frame $statsframe.left.goalsframe] -sticky w
-grid [ttk::treeview $statsframe.left.goalsframe.goalsview -show headings -columns [list name amount] -selectmode none -yscrollcommand [list $statsframe.left.goalsframe.scrolly set]]
+grid [ttk::treeview $statsframe.left.goalsframe.goalsview -show headings \
+   -columns [list name amount] -selectmode none \
+   -yscrollcommand [list $statsframe.left.goalsframe.scrolly set]]
 $statsframe.left.goalsframe.goalsview heading name -text {Name}
 $statsframe.left.goalsframe.goalsview column name -width 250
 $statsframe.left.goalsframe.goalsview heading amount -text {Amount}
 $statsframe.left.goalsframe.goalsview column amount -width 75 -anchor center
-tooltip::tooltip $statsframe.left.goalsframe.goalsview {The list of finished goals}
-grid [ttk::scrollbar $statsframe.left.goalsframe.scrolly -orient vertical -command [list $statsframe.left.goalsframe.goalsview yview]] -row 0 -column 1 -sticky ns
+tooltip::tooltip $statsframe.left.goalsframe.goalsview \
+   {The list of finished goals}
+grid [ttk::scrollbar $statsframe.left.goalsframe.scrolly -orient vertical \
+   -command [list $statsframe.left.goalsframe.goalsview yview]] -row 0 \
+   -column 1 -sticky ns
 ::autoscroll::autoscroll $statsframe.left.goalsframe.scrolly
 grid [ttk::frame $statsframe.right] -row 0 -column 1 -sticky nwes -padx 5
 SetScrollbarBindings $statsframe.right .gameframe.paned.statsframe.scrolly
 grid [ttk::label $statsframe.right.destroyed] -sticky w
-SetScrollbarBindings $statsframe.right.destroyed .gameframe.paned.statsframe.scrolly
+SetScrollbarBindings $statsframe.right.destroyed \
+   .gameframe.paned.statsframe.scrolly
 grid [ttk::frame $statsframe.right.destroyedframe] -sticky w
-grid [ttk::treeview $statsframe.right.destroyedframe.destroyedview -show headings -columns [list name amount] -selectmode none -yscrollcommand [list $statsframe.right.destroyedframe.scrolly set]]
+grid [ttk::treeview $statsframe.right.destroyedframe.destroyedview \
+   -show headings -columns [list name amount] -selectmode none \
+   -yscrollcommand [list $statsframe.right.destroyedframe.scrolly set]]
 $statsframe.right.destroyedframe.destroyedview heading name -text {Name}
 $statsframe.right.destroyedframe.destroyedview column name -width 250
 $statsframe.right.destroyedframe.destroyedview heading amount -text {Amount}
-$statsframe.right.destroyedframe.destroyedview column amount -width 75 -anchor center
-tooltip::tooltip $statsframe.right.destroyedframe.destroyedview {The list of destroyed ships}
-grid [ttk::scrollbar $statsframe.right.destroyedframe.scrolly -orient vertical -command [list $statsframe.right.destroyedframe.destroyedview yview]] -row 0 -column 1 -sticky ns
+$statsframe.right.destroyedframe.destroyedview column amount -width 75 \
+   -anchor center
+tooltip::tooltip $statsframe.right.destroyedframe.destroyedview \
+   {The list of destroyed ships}
+grid [ttk::scrollbar $statsframe.right.destroyedframe.scrolly \
+   -orient vertical \
+   -command [list $statsframe.right.destroyedframe.destroyedview yview]] \
+   -row 0 -column 1 -sticky ns
 ::autoscroll::autoscroll $statsframe.right.destroyedframe.scrolly
 grid [ttk::label $statsframe.right.killed] -sticky w
-SetScrollbarBindings $statsframe.right.killed .gameframe.paned.statsframe.scrolly
+SetScrollbarBindings $statsframe.right.killed \
+   .gameframe.paned.statsframe.scrolly
 grid [ttk::frame $statsframe.right.killedframe] -sticky w
-grid [ttk::treeview $statsframe.right.killedframe.killedview -show headings -columns [list name amount] -selectmode none -yscrollcommand [list $statsframe.left.killedframe.scrolly set]]
+grid [ttk::treeview $statsframe.right.killedframe.killedview -show headings \
+   -columns [list name amount] -selectmode none \
+   -yscrollcommand [list $statsframe.left.killedframe.scrolly set]]
 $statsframe.right.killedframe.killedview heading name -text {Name}
 $statsframe.right.killedframe.killedview column name -width 250
 $statsframe.right.killedframe.killedview heading amount -text {Amount}
-$statsframe.right.killedframe.killedview column amount -width 75 -anchor center
-tooltip::tooltip $statsframe.right.killedframe.killedview {The list of killed enemies in melee combat}
-grid [ttk::scrollbar $statsframe.right.killedframe.scrolly -orient vertical -command [list $statsframe.right.killedframe.destroyedview yview]] -row 0 -column 1 -sticky ns
+$statsframe.right.killedframe.killedview column amount -width 75 \
+   -anchor center
+tooltip::tooltip $statsframe.right.killedframe.killedview \
+   {The list of killed enemies in melee combat}
+grid [ttk::scrollbar $statsframe.right.killedframe.scrolly -orient vertical \
+   -command [list $statsframe.right.killedframe.destroyedview yview]] -row 0 \
+   -column 1 -sticky ns
 ::autoscroll::autoscroll $statsframe.right.killedframe.scrolly
 grid columnconfigure .gameframe.paned.statsframe 0 -weight 1
