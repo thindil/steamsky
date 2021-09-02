32302d7db2ac2950033bab533836f8ba0718e531
thindil <thindil@laeran.pl>
2021-09-02 03:53:48 +0000

    updated unit tests
    FossilOrigin-Name: d793ffa499aff1e77fe3c26503af436c16a744f5f501d5f6f42abc92a886304b

--- tests/game-test_data-tests.ads
+++ tests/game-test_data-tests.ads
@@ -11,13 +11,13 @@ package Game.Test_Data.Tests is
    null record;
 
    procedure Test_Update_Game_25a566_5306b6(Gnattest_T: in out Test);
-   --  game.ads:536:4:Update_Game:Test_UpdateGame
+   --  game.ads:559:4:Update_Game:Test_UpdateGame
 
    procedure Test_End_Game_29871f_745ef4(Gnattest_T: in out Test);
-   --  game.ads:546:4:End_Game:Test_EndGame
+   --  game.ads:569:4:End_Game:Test_EndGame
 
    procedure Test_Find_Skill_Index_2a5948_39ca51(Gnattest_T: in out Test);
-   --  game.ads:558:4:Find_Skill_Index:Test_FindSkillIndex
+   --  game.ads:581:4:Find_Skill_Index:Test_FindSkillIndex
 
 end Game.Test_Data.Tests;
 --  end read only
