3b4f598bf241913635fb083be9645556d0bb2e81
thindil <thindil@laeran.pl>
2021-09-14 03:43:25 +0000

    fixed some AdaControl errors
    FossilOrigin-Name: 5ca3b04bcf7ca6ce2e6f89f8caa7e06e5534e3feb6d077203b7146b30124e8e5

--- src/utils.ads
+++ src/utils.ads
@@ -15,7 +15,9 @@
 --    You should have received a copy of the GNU General Public License
 --    along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
+--## rule off REDUCEABLE_SCOPE
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+--## rule on REDUCEABLE_SCOPE
 with Game; use Game;
 
 -- ****h* Utils/Utils
