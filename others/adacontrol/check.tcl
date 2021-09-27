b9a0aaf570c7e8f5122217840357349fa199edfe
thindil <thindil@laeran.pl>
2021-09-27 03:22:16 +0000

    added check for required name of AdaControl file
    FossilOrigin-Name: fac08d0a0376d6003f0000587e5b03d818d3c25695e555ae4b59fd7138a3fc98

--- others/adacontrol/check.tcl
+++ others/adacontrol/check.tcl
@@ -2,8 +2,15 @@
 # the next line restarts using tclsh \
 exec tclsh "$0" ${1+"$@"}
 
+# Check if the script is run from the project's main directory
 if {![file exists steamsky.gpr]} {
    puts {This script must be run in the directory where steamsky.gpr file is}
+   return
+}
+
+# Check if the user entered at least name of the file with AdaControl rules
+if {[llength $argv] == 0} {
+   puts {You have to specify at least file with rules to use}
    return
 }
 
