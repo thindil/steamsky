720d918dcf674e504a77b5d896ba7773f00379f1
thindil <thindil@laeran.pl>
2021-10-03 03:45:25 +0000

    updated code documentation
    FossilOrigin-Name: 092770368eb0fff54bd65285aa60156709111bc819cb1acf7dbb2d99ecd3b0bc

---
+++ docs/robodoc.js
@@ -0,0 +1,10 @@
+/****h* ROBODoc/ROBODoc Javascript support
+ * FUNCTION
+ *   This is the default Javascript library for documentation
+ *   generated with ROBODoc.
+ *   You can edit this file to your own liking and then use
+ *   it with the option
+ *      --js <filename>
+ ******
+ * $Id: html_generator.c,v 1.95 2019/01/04 23:58:00 cashy Exp $
+ */
