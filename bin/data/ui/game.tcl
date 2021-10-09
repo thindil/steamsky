2bd172a995beb7e3076b1c172c9663d8442353c5
thindil <thindil@laeran.pl>
2021-10-09 03:39:07 +0000

    fixed hiding the main game menu when using keyboard shortcut
    FossilOrigin-Name: f56de04879499916b3594b91057322a80c0afa158e1075da33340919d727b742

--- bin/data/ui/game.tcl
+++ bin/data/ui/game.tcl
@@ -20,7 +20,10 @@ proc InvokeMenu {shortcut} {
       for {set i 0} {$i <= [.gamemenu index last]} {incr i} {
          if {[.gamemenu entrycget $i -accelerator] == $shortcut} {
             .gamemenu invoke $i
-            focus .gamemenu
+            if {[focus] == ".gamemenu"} {
+               .gamemenu unpost
+               focus .
+            }
          }
       }
    }
