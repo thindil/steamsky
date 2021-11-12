dc73be3755c6c3f622b8b4e701699e48567f9f69
thindil <thindil@laeran.pl>
2021-11-12 04:24:25 +0000

    style: fixed some AdaControl errors
    FossilOrigin-Name: 5a1e0a231db8103b3b3312cfb06a88ee2a309817ccbbb588afe71cd59653a9f1

--- src/events.adb
+++ src/events.adb
@@ -142,46 +142,63 @@ package body Events is
                   if Time_Passed < 1 then
                      Time_Passed := 1;
                   end if;
-                  Gain_Exp(Amount => 1, Skill_Number => Piloting_Skill, Crew_Index => Crew_Index);
+                  Gain_Exp
+                    (Amount => 1, Skill_Number => Piloting_Skill,
+                     Crew_Index => Crew_Index);
                   UpdateCargo
-                    (Ship => Player_Ship, ProtoIndex => FindProtoItem(ItemType => Fuel_Type),
+                    (Ship => Player_Ship,
+                     ProtoIndex => FindProtoItem(ItemType => Fuel_Type),
                      Amount => CountFuelNeeded);
                   Update_Game(Minutes => Time_Passed);
                end if;
             when 21 .. 23 => -- Friendly trader
                Events_List.Append
                  (New_Item =>
-                    (E_Type => TRADER, Sky_X => Player_Ship.Sky_X, Sky_Y => Player_Ship.Sky_Y,
+                    (E_Type => TRADER, Sky_X => Player_Ship.Sky_X,
+                     Sky_Y => Player_Ship.Sky_Y,
                      Time => Get_Random(Min => 30, Max => 45),
-                     Ship_Index => Traders
-                       (Get_Random(Min => Traders.First_Index, Max => Traders.Last_Index))));
+                     Ship_Index =>
+                       Traders
+                         (Get_Random
+                            (Min => Traders.First_Index,
+                             Max => Traders.Last_Index))));
                SkyMap(Player_Ship.Sky_X, Player_Ship.Sky_Y).EventIndex :=
                  Events_List.Last_Index;
-               AddMessage(Message => "You've meet a friendly trader.", MType => OtherMessage);
+               AddMessage
+                 (Message => "You've meet a friendly trader.",
+                  MType => OtherMessage);
                Gain_Perception;
                UpdateOrders(Ship => Player_Ship);
             when 24 .. 30 => -- Friendly ship
                Events_List.Append
                  (New_Item =>
-                    (FRIENDLYSHIP, Player_Ship.Sky_X, Player_Ship.Sky_Y,
-                     Get_Random(30, 45),
-                     Friendly_Ships
-                       (Get_Random
-                          (Friendly_Ships.First_Index,
-                           Friendly_Ships.Last_Index))));
+                    (E_Type => FRIENDLYSHIP, Sky_X => Player_Ship.Sky_X,
+                     Sky_Y => Player_Ship.Sky_Y,
+                     Time => Get_Random(Min => 30, Max => 45),
+                     Ship_Index =>
+                       Friendly_Ships
+                         (Get_Random
+                            (Min => Friendly_Ships.First_Index,
+                             Max => Friendly_Ships.Last_Index))));
                SkyMap(Player_Ship.Sky_X, Player_Ship.Sky_Y).EventIndex :=
                  Events_List.Last_Index;
-               AddMessage("You've spotted a friendly ship.", OtherMessage);
+               AddMessage
+                 (Message => "You've spotted a friendly ship.",
+                  MType => OtherMessage);
                Gain_Perception;
-               UpdateOrders(Player_Ship);
+               UpdateOrders(Ship => Player_Ship);
             when others => -- Combat
-               Generate_Enemies(Enemies);
+               Generate_Enemies(Enemies => Enemies);
                Events_List.Append
                  (New_Item =>
-                    (ENEMYSHIP, Player_Ship.Sky_X, Player_Ship.Sky_Y,
-                     Get_Random(30, 45),
-                     Enemies
-                       (Get_Random(Enemies.First_Index, Enemies.Last_Index))));
+                    (E_Type => ENEMYSHIP, Sky_X => Player_Ship.Sky_X,
+                     Sky_Y => Player_Ship.Sky_Y,
+                     Time => Get_Random(Min => 30, Max => 45),
+                     Ship_Index =>
+                       Enemies
+                         (Get_Random
+                            (Min => Enemies.First_Index,
+                             Max => Enemies.Last_Index))));
                SkyMap(Player_Ship.Sky_X, Player_Ship.Sky_Y).EventIndex :=
                  Events_List.Last_Index;
                return
