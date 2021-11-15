7e6e79ffdad0b3cefbfdd0250cd382fb0c244f7c
thindil <thindil@laeran.pl>
2021-11-15 04:40:10 +0000

    feat: added option to set title of the selected dialog
    FossilOrigin-Name: f0d0a2274551fbc47ea11cf0609a2263ba748c9f32f0aad354de6ad5f8dbc5d6

--- src/ui/dialogs.adb
+++ src/ui/dialogs.adb
@@ -152,6 +152,12 @@ package body Dialogs is
       return TCL_OK;
    end Close_Dialog_Command;
 
+   procedure Change_Title(Dialog: Ttk_Frame; New_Title: String) is
+      Dialog_Header: constant Ttk_Label := Get_Widget(Dialog & ".header");
+   begin
+      configure(Dialog_Header, "-text {" & New_Title & "}");
+   end Change_Title;
+
    -- ****o* Dialogs/Dialogs.Update_Dialog_Command
    -- FUNCTION
    -- Update countdown timer on the selected dialog. If timer reach 0, close
