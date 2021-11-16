1f9ab09dcf4780000e122b36de2c9258f59c20af
thindil <thindil@laeran.pl>
2021-11-16 04:30:49 +0000

    tests: updated unit tests
    FossilOrigin-Name: 469e1b0cf609182bf56e8d063147f54325206dcedcb3f401134a3fecb4ed802c

--- tests/factions-test_data-tests.ads
+++ tests/factions-test_data-tests.ads
@@ -11,13 +11,13 @@ package Factions.Test_Data.Tests is
    null record;
 
    procedure Test_GetReputation_f138bb_ad6480(Gnattest_T: in out Test);
-   --  factions.ads:180:4:GetReputation:Test_GetReputation
+   --  factions.ads:183:4:GetReputation:Test_GetReputation
 
    procedure Test_IsFriendly_868bec_4ae8b6(Gnattest_T: in out Test);
-   --  factions.ads:197:4:IsFriendly:Test_IsFriendly
+   --  factions.ads:200:4:IsFriendly:Test_IsFriendly
 
    procedure Test_GetRandomFaction_47dd81_103989(Gnattest_T: in out Test);
-   --  factions.ads:211:4:GetRandomFaction:Test_GetRandomFaction
+   --  factions.ads:214:4:GetRandomFaction:Test_GetRandomFaction
 
 end Factions.Test_Data.Tests;
 --  end read only
