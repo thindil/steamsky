cb15b952779c14d0065b50b1bb42c008a30fe68f
thindil <thindil@laeran.pl>
2022-01-15 04:00:25 +0000

    style: fixed some AdaControl errors
    FossilOrigin-Name: fc984863e9ff294c490196dae6f9bf0cd317c7072025a989362ba867e1c5278a

--- src/ui/goals-ui.ads
+++ src/ui/goals-ui.ads
@@ -1,4 +1,4 @@
--- Copyright (c) 2020-2021 Bartek thindil Jasicki <thindil@laeran.pl>
+-- Copyright (c) 2020-2022 Bartek thindil Jasicki <thindil@laeran.pl>
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -20,11 +20,11 @@
 package Goals.UI is
 -- ****
 
-   -- ****f* GUI/GUI.AddCommands
+   -- ****f* GUI/GUI.Add_Commands
    -- FUNCTION
    -- Add Tcl commands related to the goals UI
    -- SOURCE
-   procedure AddCommands;
+   procedure Add_Commands;
    -- ****
 
 end Goals.UI;
