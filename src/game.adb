491c3d9d8398b021d09cb9058c89b04f91bd1904
thindil <thindil@laeran.pl>
2022-03-07 04:15:59 +0000

    refactor: updated code to the new version of Male_Syllables_End
    FossilOrigin-Name: 54122295bd0f84de0f7485c7c6a07789305984d85cca3d8f60c1ec896b9475db

--- src/game.adb
+++ src/game.adb
@@ -821,7 +821,7 @@ package body Game is
                     "malessyllableend" then
                      Male_Syllables_End.Append
                        (New_Item =>
-                          To_Unbounded_String
+                          To_Bounded_String
                             (Source =>
                                Get_Attribute
                                  (Elem => Data_Node, Name => "value")));
