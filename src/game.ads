2e7f70654bc94259cd6718af14bfbff61a364b3f
thindil <thindil@laeran.pl>
2022-04-08 03:04:49 +0000

    docs: updated version number
    FossilOrigin-Name: 3c031f29ad46987e1f1b88441ada8b0cb343827b60103806bb655be6bb8af244

--- src/game.ads
+++ src/game.ads
@@ -67,7 +67,7 @@ package Game is
    -- FUNCTION
    -- Current the game version
    -- SOURCE
-   Game_Version: constant String := "Version: 7.0.4";
+   Game_Version: constant String := "Version: 7.0.5";
    -- ****
 
    -- ****t* Game/Game.UnboundedString_Container
