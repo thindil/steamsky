c9b502dda52e020e9904d2e8775ad114fc3a1dde
thindil <thindil@laeran.pl>
2022-05-04 03:07:09 +0000

    style: fixed some AdaControl errors
    FossilOrigin-Name: 0eb85dc4b5cd476be8c19d7fa6da6a8100943d5f21d26300955fa1dca7c89417

--- src/ui/ships-ui.ads
+++ src/ui/ships-ui.ads
@@ -1,4 +1,4 @@
--- Copyright (c) 2020-2021 Bartek thindil Jasicki <thindil@laeran.pl>
+-- Copyright (c) 2020-2022 Bartek thindil Jasicki <thindil@laeran.pl>
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -28,26 +28,26 @@ package Ships.UI is
    -- FUNCTION
    -- Show information about the player's ship
    -- PARAMETERS
-   -- ClientData - Custom data send to the command. Unused
-   -- Interp     - Tcl interpreter in which command was executed.
-   -- Argc       - Number of arguments passed to the command.
-   -- Argv       - Values of arguments passed to the command. Unused
+   -- Client_Data - Custom data send to the command. Unused
+   -- Interp      - Tcl interpreter in which command was executed.
+   -- Argc        - Number of arguments passed to the command.
+   -- Argv        - Values of arguments passed to the command. Unused
    -- RESULT
    -- This function always return TCL_OK
    -- COMMANDS
    -- ShowShipInfo
    -- SOURCE
    function Show_Ship_Info_Command
-     (ClientData: Integer; Interp: Tcl.Tcl_Interp; Argc: Interfaces.C.int;
+     (Client_Data: Integer; Interp: Tcl.Tcl_Interp; Argc: Interfaces.C.int;
       Argv: CArgv.Chars_Ptr_Ptr) return Interfaces.C.int with
       Convention => C;
       -- ****
 
-   -- ****f* SUI2/SUI2.AddCommands
+   -- ****f* SUI2/SUI2.Add_Commands
    -- FUNCTION
    -- Add Tcl commands related to the player's ship information
    -- SOURCE
-   procedure AddCommands;
+   procedure Add_Commands;
    -- ****
 
 end Ships.UI;
