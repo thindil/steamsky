669ca6e8ff20794e9024a5d1dd5c9937ac2db85d
thindil <thindil@laeran.pl>
2022-05-10 02:59:56 +0000

    tests: updated unit tests
    FossilOrigin-Name: d8ea97119cd96906732e8e73057478181d0cdb6c99cfc2efade6d78bcf24715a

--- tests/items-test_data-tests.ads
+++ tests/items-test_data-tests.ads
@@ -11,23 +11,23 @@ package Items.Test_Data.Tests is
    null record;
 
    procedure Test_Find_Proto_Item_3e747a_0bae5a(Gnattest_T: in out Test);
-   --  items.ads:188:4:Find_Proto_Item:Test_FindProtoItem
+   --  items.ads:197:4:Find_Proto_Item:Test_FindProtoItem
 
    procedure Test_Get_Item_Damage_5414f2_efab16(Gnattest_T: in out Test);
-   --  items.ads:205:4:Get_Item_Damage:Test_GetItemDamage
+   --  items.ads:214:4:Get_Item_Damage:Test_GetItemDamage
 
    procedure Test_Get_Item_Name_1f8ef2_6e935f(Gnattest_T: in out Test);
-   --  items.ads:223:4:Get_Item_Name:Test_GetItemName
+   --  items.ads:232:4:Get_Item_Name:Test_GetItemName
 
    procedure Test_Damage_Item_397a3e_4cae84(Gnattest_T: in out Test);
-   --  items.ads:246:4:Damage_Item:Test_DamageItem
+   --  items.ads:255:4:Damage_Item:Test_DamageItem
 
    procedure Test_Find_Item_53de69_9d4eb9(Gnattest_T: in out Test);
-   --  items.ads:267:4:Find_Item:Test_FindItem
+   --  items.ads:276:4:Find_Item:Test_FindItem
 
    procedure Test_Get_Item_Chance_To_Damage_32e0c6_2bbc61
      (Gnattest_T: in out Test);
-   --  items.ads:294:4:Get_Item_Chance_To_Damage:Test_GetItemChanceToDamage
+   --  items.ads:303:4:Get_Item_Chance_To_Damage:Test_GetItemChanceToDamage
 
 end Items.Test_Data.Tests;
 --  end read only
