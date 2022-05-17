ffac98e3a572e200af4e3a97c9a2be2ccdd6dcaa
thindil <thindil@laeran.pl>
2022-05-17 03:05:34 +0000

    tests: updated unit tests
    FossilOrigin-Name: 2ecc28941a5fa990e2e74b3daa2e4eb76714b9d4a0eaad4d8960207d834ed03e

--- tests/bases-test_data-tests.ads
+++ tests/bases-test_data-tests.ads
@@ -11,28 +11,28 @@ package Bases.Test_Data.Tests is
    null record;
 
    procedure Test_Gain_Rep_e2afe4_901e58(Gnattest_T: in out Test);
-   --  bases.ads:190:4:Gain_Rep:Test_GainRep
+   --  bases.ads:199:4:Gain_Rep:Test_GainRep
 
    procedure Test_Count_Price_d382e2_cce97c(Gnattest_T: in out Test);
-   --  bases.ads:206:4:Count_Price:Test_CountPrice
+   --  bases.ads:215:4:Count_Price:Test_CountPrice
 
    procedure Test_Generate_Base_Name_e67ce0_969f68(Gnattest_T: in out Test);
-   --  bases.ads:221:4:Generate_Base_Name:Test_GenerateBaseName
+   --  bases.ads:230:4:Generate_Base_Name:Test_GenerateBaseName
 
    procedure Test_Generate_Recruits_05fabc_06ea09(Gnattest_T: in out Test);
-   --  bases.ads:234:4:Generate_Recruits:Test_GenerateRecruits
+   --  bases.ads:243:4:Generate_Recruits:Test_GenerateRecruits
 
    procedure Test_Ask_For_Bases_023c5f_f3f6c6(Gnattest_T: in out Test);
-   --  bases.ads:242:4:Ask_For_Bases:Test_AskForBases
+   --  bases.ads:251:4:Ask_For_Bases:Test_AskForBases
 
    procedure Test_Ask_For_Events_4f17f6_3e359b(Gnattest_T: in out Test);
-   --  bases.ads:250:4:Ask_For_Events:Test_AskForEvents
+   --  bases.ads:259:4:Ask_For_Events:Test_AskForEvents
 
    procedure Test_Update_Population_31557d_10dec8(Gnattest_T: in out Test);
-   --  bases.ads:258:4:Update_Population:Test_UpdatePopulation
+   --  bases.ads:267:4:Update_Population:Test_UpdatePopulation
 
    procedure Test_Update_Prices_56b29f_f6cd8d(Gnattest_T: in out Test);
-   --  bases.ads:266:4:Update_Prices:Test_UpdatePrices
+   --  bases.ads:275:4:Update_Prices:Test_UpdatePrices
 
 end Bases.Test_Data.Tests;
 --  end read only
