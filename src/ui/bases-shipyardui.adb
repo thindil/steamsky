de1fc7938d68bbfd1bef15feb17e9e4a7574b060
thindil <thindil@laeran.pl>
2022-05-27 03:30:09 +0000

    feat: added info about difference in cabin's max owners when comparing modules during installation in shipyards
    FossilOrigin-Name: 1e5779c5a97ca51e620306ad855da6bd324e75b9f790006333e1eae8094c1845

--- src/ui/bases-shipyardui.adb
+++ src/ui/bases-shipyardui.adb
@@ -796,9 +796,31 @@ package body Bases.ShipyardUI is
                   Insert(ModuleText, "end", "{luxury}");
                end if;
             end if;
-            Insert
-              (ModuleText, "end",
-               "{" & LF & "Max owners:" & Natural'Image(MaxOwners) & "}");
+            Insert(ModuleText, "end", "{" & LF & "Max owners:}");
+            if Installing and then ShipModuleIndex > 0 then
+               if BaseModules_Container.Element
+                   (Container => Modules_List,
+                    Index => Player_Ship.Modules(ShipModuleIndex).Proto_Index)
+                   .Max_Owners >
+                 MaxOwners then
+                  Insert
+                    (ModuleText, "end",
+                     "{" & Natural'Image(MaxOwners) & " (less)} [list red]");
+               elsif BaseModules_Container.Element
+                   (Container => Modules_List,
+                    Index => Player_Ship.Modules(ShipModuleIndex).Proto_Index)
+                   .Max_Owners <
+                 MaxOwners then
+                  Insert
+                    (ModuleText, "end",
+                     "{" & Natural'Image(MaxOwners) & " (more)} [list green]");
+               else
+                  Insert
+                    (ModuleText, "end", "{" & Natural'Image(MaxOwners) & "}");
+               end if;
+            else
+               Insert(ModuleText, "end", "{" & Natural'Image(MaxOwners) & "}");
+            end if;
          when ALCHEMY_LAB .. GREENHOUSE =>
             Insert
               (ModuleText, "end",
