799461fd9513db8f33ec5587b568a0be69a63f9e
thindil <thindil@laeran.pl>
2022-06-15 02:57:51 +0000

    style: fixed some AdaControl errors
    FossilOrigin-Name: 62df3d6ce9033001ef3232ad99b08c5cecf64aa19d1c985e7968e59906f92f43

--- src/ui/bases-recruitui.ads
+++ src/ui/bases-recruitui.ads
@@ -1,4 +1,4 @@
--- Copyright (c) 2020 Bartek thindil Jasicki <thindil@laeran.pl>
+-- Copyright (c) 2020-2022 Bartek thindil Jasicki <thindil@laeran.pl>
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -20,11 +20,11 @@
 package Bases.RecruitUI is
 -- ****
 
-   -- ****f* RecruitUI/RecruitUI.AddCommands
+   -- ****f* RecruitUI/RecruitUI.Add_Commands
    -- FUNCTION
    -- Add Tcl commands related to recruit new crew members in bases
    -- SOURCE
-   procedure AddCommands;
+   procedure Add_Commands;
    -- ****
 
 end Bases.RecruitUI;
