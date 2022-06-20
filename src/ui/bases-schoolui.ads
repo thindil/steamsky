05a72d9b1753a139cff7461224fdc19df2452959
thindil <thindil@laeran.pl>
2022-06-20 02:59:00 +0000

    style: fixed some AdaControl errors
    FossilOrigin-Name: 90d7a862028841e088eff5565b4ce87217a5d3c6dbf951fbdb65e87045aebab4

--- src/ui/bases-schoolui.ads
+++ src/ui/bases-schoolui.ads
@@ -1,4 +1,4 @@
--- Copyright (c) 2020 Bartek thindil Jasicki <thindil@laeran.pl>
+-- Copyright (c) 2020-2022 Bartek thindil Jasicki <thindil@laeran.pl>
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -20,11 +20,11 @@
 package Bases.SchoolUI is
 -- ****
 
-   -- ****f* SchoolUI/SchoolUI.AddCommands
+   -- ****f* SchoolUI/SchoolUI.Add_Commands
    -- FUNCTION
    -- Add Tcl commands related to training skills in bases
    -- SOURCE
-   procedure AddCommands;
+   procedure Add_Commands;
    -- ****
 
 end Bases.SchoolUI;
