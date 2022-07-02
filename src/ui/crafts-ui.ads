bca36f836a83537cbb8dd7903b93d50568244a41
thindil <thindil@laeran.pl>
2022-07-02 03:17:14 +0000

    style: fixed some AdaControl errors
    FossilOrigin-Name: a0d26918dce3e2da28f1956347aacd81dbbca40827b80d7ed8e8ef323b8474af

--- src/ui/crafts-ui.ads
+++ src/ui/crafts-ui.ads
@@ -1,4 +1,4 @@
--- Copyright (c) 2020-2021 Bartek thindil Jasicki <thindil@laeran.pl>
+-- Copyright (c) 2020-2022 Bartek thindil Jasicki <thindil@laeran.pl>
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -20,11 +20,11 @@
 package Crafts.UI is
 -- ****
 
-   -- ****f* CUI4/CUI4.AddCommands
+   -- ****f* CUI4/CUI4.Add_Commands
    -- FUNCTION
    -- Add Tcl commands related to the crafting
    -- SOURCE
-   procedure AddCommands;
+   procedure Add_Commands;
    -- ****
 
 end Crafts.UI;
