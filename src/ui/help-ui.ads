34231fdb24bd376b5a0cbba7b366bb9c237c3c67
thindil <thindil@laeran.pl>
2022-07-22 03:13:15 +0000

    style: fixed some AdaControl errors
    FossilOrigin-Name: 07a41f825188c2ce0844a0f57d9d9bb9f56d532ab8ca0e54b93a58fbefe20de2

--- src/ui/help-ui.ads
+++ src/ui/help-ui.ads
@@ -1,4 +1,4 @@
--- Copyright (c) 2020-2021 Bartek thindil Jasicki <thindil@laeran.pl>
+-- Copyright (c) 2020-2022 Bartek thindil Jasicki <thindil@laeran.pl>
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -20,11 +20,11 @@
 package Help.UI is
 -- ****
 
-   -- ****f* HUI/HUI.AddCommands
+   -- ****f* HUI/HUI.Add_Commands
    -- FUNCTION
    -- Add Tcl commands related to the help UI
    -- SOURCE
-   procedure AddCommands;
+   procedure Add_Commands;
    -- ****
 
 end Help.UI;
