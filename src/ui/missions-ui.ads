1e82e6331dbcdd69bf0fe7c6a8bb7dff555c20c3
thindil <thindil@laeran.pl>
2022-08-02 03:16:46 +0000

    style: fixed some AdaControl errors
    FossilOrigin-Name: a1a599ab2b74fa12e8a3558c97e9a770e2d10837c74393a475184ad31241cf6a

--- src/ui/missions-ui.ads
+++ src/ui/missions-ui.ads
@@ -1,4 +1,4 @@
--- Copyright (c) 2020-2021 Bartek thindil Jasicki <thindil@laeran.pl>
+-- Copyright (c) 2020-2022 Bartek thindil Jasicki <thindil@laeran.pl>
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -20,11 +20,11 @@
 package Missions.UI is
 -- ****
 
-   -- ****f* MUI3/MUI3.AddCommands
+   -- ****f* MUI3/MUI3.Add_Commands
    -- FUNCTION
    -- Add Tcl commands related to the available missions list
    -- SOURCE
-   procedure AddCommands;
+   procedure Add_Commands;
    -- ****
 
 end Missions.UI;
