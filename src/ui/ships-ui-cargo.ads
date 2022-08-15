99df86047cb8b73e5a376fa7e2fd9d8b38199230
thindil <thindil@laeran.pl>
2022-08-15 03:01:12 +0000

    style: fixed some AdaControl errors
    FossilOrigin-Name: 2f5feb942a0885e088cd763786168724f82c87e0cbc6b5ad52ba0251cf95860e

--- src/ui/ships-ui-cargo.ads
+++ src/ui/ships-ui-cargo.ads
@@ -1,4 +1,4 @@
--- Copyright (c) 2020-2021 Bartek thindil Jasicki <thindil@laeran.pl>
+-- Copyright (c) 2020-2022 Bartek thindil Jasicki <thindil@laeran.pl>
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -20,11 +20,11 @@
 package Ships.UI.Cargo is
 -- ****
 
-   -- ****f* SUCargo/SUCargo.AddCommands
+   -- ****f* SUCargo/SUCargo.Add_Commands
    -- FUNCTION
    -- Add Tcl commands related to the player's ship cargo information
    -- SOURCE
-   procedure AddCommands;
+   procedure Add_Commands;
    -- ****
 
 end Ships.UI.Cargo;
