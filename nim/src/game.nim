9c3b14fe87283ba13296e150f2a700394cd8bc48
thindil <thindil@laeran.pl>
2022-09-06 02:56:52 +0000

    refactor: added DateRecord in Nim
    FossilOrigin-Name: d4f470d771fd69bb8bb5ef5c552b9df43cbb652678a14d0cd8c175141373739c

---
+++ nim/src/game.nim
@@ -0,0 +1,27 @@
+# Copyright 2022 Bartek thindil Jasicki
+#
+# This file is part of Steam Sky.
+#
+# Steam Sky is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Steam Sky is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
+
+type
+  DateRecord* = object
+    ## FUNCTION
+    ##
+    ## Used to store the game's time
+    year*: range[0..4_000_000] ## The game's year
+    month*: range[0..24] ## The game's month
+    day*: range[0..62] ## The game's day
+    hour*: range[0..48] ## The game's hour
+    minutes*: range[0..120] ## The game's minutes
