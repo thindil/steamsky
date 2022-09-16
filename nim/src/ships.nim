ca2dc3a6362db9515e35d2720c5f1b156cb7030b
thindil <thindil@laeran.pl>
2022-09-16 02:55:30 +0000

    feat: added ShipSpeed enum in Nim code
    FossilOrigin-Name: d0a49e5d6282fbd37aa58ae100d58f695daae715b71694dec82d534aa5ab7536

---
+++ nim/src/ships.nim
@@ -0,0 +1,23 @@
+# Copyright 2022 Bartek thindil Jasicki
+#
+# This file is part of Steam Sky.
+#
+# Steam Sky is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Steam Sky is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
+
+type
+  ShipSpeed* = enum
+    ## FUNCTION
+    ##
+    ## Ships's state of speed, how much engines are used
+    docked, fullStop, quarterSpeed, halfSpeed, fullSpeed
