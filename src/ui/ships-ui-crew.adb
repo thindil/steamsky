d3df2ae13531f184f657a824f004631a7522a874
thindil <thindil@laeran.pl>
2022-09-29 03:14:16 +0000

    feat: changed title for the crew member info dialog
    FossilOrigin-Name: 143ccab90cafbc0e25532216411f54abe53637a356665e1f31af2aaeb92511a5

--- src/ui/ships-ui-crew.adb
+++ src/ui/ships-ui-crew.adb
@@ -566,7 +566,7 @@ package body Ships.UI.Crew is
       Member: constant Member_Data := Player_Ship.Crew(Member_Index);
       Member_Dialog: constant Ttk_Frame :=
         Create_Dialog
-          (Name => ".memberdialog", Title => To_String(Source => Member.Name),
+          (Name => ".memberdialog", Title => "Crew member details",
            Columns => 2);
       Y_Scroll: constant Ttk_Scrollbar :=
         Create
