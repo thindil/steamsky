f0624cca5b7f0b9edfeac4c8573d81c80b055345
thindil <thindil@laeran.pl>
2022-10-10 03:07:14 +0000

    tests: updated unit tests
    FossilOrigin-Name: c1cce429f3c87702bb112568ceab2826c1fd1769565637b31914629afc393956

--- tests/messages-test_data-tests.ads
+++ tests/messages-test_data-tests.ads
@@ -10,20 +10,17 @@ package Messages.Test_Data.Tests is
      .Test with
    null record;
 
-   procedure Test_Add_Message_da0d2a_c15a00(Gnattest_T: in out Test);
-   --  messages.ads:97:4:Add_Message:Test_AddMessage
-
    procedure Test_Get_Message_6b9b1e_0b2a8d(Gnattest_T: in out Test);
-   --  messages.ads:115:4:Get_Message:Test_GetMessage
+   --  messages.ads:114:4:Get_Message:Test_GetMessage
 
    procedure Test_Clear_Messages_c573e1_267040(Gnattest_T: in out Test);
-   --  messages.ads:125:4:Clear_Messages:Test_ClearMessages
+   --  messages.ads:124:4:Clear_Messages:Test_ClearMessages
 
    procedure Test_Messages_Amount_a330a8_8e4cbf(Gnattest_T: in out Test);
-   --  messages.ads:137:4:Messages_Amount:Test_MessagesAmount
+   --  messages.ads:136:4:Messages_Amount:Test_MessagesAmount
 
    procedure Test_Restore_Message_2b3d02_0f207a(Gnattest_T: in out Test);
-   --  messages.ads:150:4:Restore_Message:Test_RestoreMessage
+   --  messages.ads:149:4:Restore_Message:Test_RestoreMessage
 
 end Messages.Test_Data.Tests;
 --  end read only
