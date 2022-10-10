7ea5de4fc84dae6c4b23c3d25c027eed696b394e
thindil <thindil@laeran.pl>
2022-10-10 03:03:05 +0000

    tests: updated unit tests
    FossilOrigin-Name: ec4ef684a03e01976c3bd806e6d0c842c1ff6387cf2c67cb3a2d789c01d2f8e3

--- tests/messages-test_data-tests.ads
+++ tests/messages-test_data-tests.ads
@@ -11,23 +11,19 @@ package Messages.Test_Data.Tests is
    null record;
 
    procedure Test_Add_Message_da0d2a_c15a00(Gnattest_T: in out Test);
-   --  messages.ads:104:4:Add_Message:Test_AddMessage
+   --  messages.ads:97:4:Add_Message:Test_AddMessage
 
    procedure Test_Get_Message_6b9b1e_0b2a8d(Gnattest_T: in out Test);
-   --  messages.ads:122:4:Get_Message:Test_GetMessage
+   --  messages.ads:115:4:Get_Message:Test_GetMessage
 
    procedure Test_Clear_Messages_c573e1_267040(Gnattest_T: in out Test);
-   --  messages.ads:132:4:Clear_Messages:Test_ClearMessages
+   --  messages.ads:125:4:Clear_Messages:Test_ClearMessages
 
    procedure Test_Messages_Amount_a330a8_8e4cbf(Gnattest_T: in out Test);
-   --  messages.ads:144:4:Messages_Amount:Test_MessagesAmount
+   --  messages.ads:137:4:Messages_Amount:Test_MessagesAmount
 
    procedure Test_Restore_Message_2b3d02_0f207a(Gnattest_T: in out Test);
-   --  messages.ads:157:4:Restore_Message:Test_RestoreMessage
-
-   procedure Test_Get_Last_Message_Index_211a31_517343
-     (Gnattest_T: in out Test);
-   --  messages.ads:170:4:Get_Last_Message_Index:Test_GetLastMessageIndex
+   --  messages.ads:150:4:Restore_Message:Test_RestoreMessage
 
 end Messages.Test_Data.Tests;
 --  end read only
