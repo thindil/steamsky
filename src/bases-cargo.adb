9a7d820f2768ca7e5a35b254570be7affa72fd5a
thindil <thindil@laeran.pl>
2022-10-13 02:51:30 +0000

    refactor: reordered with clauses
    FossilOrigin-Name: 7e5d74509648f1f1b1e6ad857efa1e749603ffa3fb1cb159ee467d2e9fa5013a

--- src/bases-cargo.adb
+++ src/bases-cargo.adb
@@ -15,10 +15,10 @@
 --    You should have received a copy of the GNU General Public License
 --    along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
-with Utils;
-with Trades;
 with BasesTypes; use BasesTypes;
 with Maps; use Maps;
+with Trades;
+with Utils;
 
 package body Bases.Cargo is
 
