c142a4e32c6496f56626aa05b02a3d6749097a3c
thindil <thindil@laeran.pl>
2022-10-14 03:20:18 +0000

    feat: added AttributesArray type
    FossilOrigin-Name: aa9f757809ba3ace50dcb6e71539e442ad08955a9b18ebe48dc0a10418a7a630

--- nim/src/game.nim
+++ nim/src/game.nim
@@ -30,6 +30,7 @@ type
   MapXRange* = range[1..1_024] ## The size of the game map in X axis
   MapYRange* = range[1..1_024] ## The size of the game map in Y axis
   ReputationRange* = range[-100..100] ## The range of possible reputation levels
+  AttributesArray* = array[1 .. 2, Natural] ## 1 - Attribute level, 2 - Attribute experience
 
 var
   saveDirectory*: string = "data" & DirSep & "saves" & DirSep
