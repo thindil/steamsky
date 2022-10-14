490cf9ae890740b197fafa201eaa7a7641a902c9
thindil <thindil@laeran.pl>
2022-10-14 03:20:55 +0000

    refactor: started work on move factions code to Nim
    FossilOrigin-Name: 25c14f0bf6b2bb19e72c8ebc01ed937f3b5cdc92885807041ed4cc13f3306922

---
+++ nim/src/factions.nim
@@ -0,0 +1,44 @@
+# Copyright 2022 Bartek thindil Jasicki
+#
+# This file is part of Steam Sky.
+#
+# Steam Sky is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Steam Sky is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
+
+{.used.}
+
+import game
+
+type
+  NamesTypes = enum
+    normal, robotic
+
+  ReputationRanges = object
+    min: ReputationRange
+    max: ReputationRange
+
+  RelationsData = object
+    reputation: ReputationRange
+    friendly: bool
+
+  CareerData = object
+    shipIndex: Positive
+    playerIndex: string
+    description: string
+    name: string
+
+  FactionData = object
+    name: string
+    memberName: string
+    pluralMemberName: string
+    spawnChance: Natural
