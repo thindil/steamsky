8cae0805d34bcece30246776ae5662eb5f4ca0cc
thindil <thindil@laeran.pl>
2022-10-28 03:01:12 +0000

    tests: updated unit tests
    FossilOrigin-Name: 2ce612f042a663ef1c9edb0bb7dd83716e8b57fbfbe5d8d7a8fd8bf57f14cffe

--- tests/bases-test_data-tests.ads
+++ tests/bases-test_data-tests.ads
@@ -11,25 +11,25 @@ package Bases.Test_Data.Tests is
    null record;
 
    procedure Test_Gain_Rep_e2afe4_901e58(Gnattest_T: in out Test);
-   --  bases.ads:219:4:Gain_Rep:Test_GainRep
+   --  bases.ads:191:4:Gain_Rep:Test_GainRep
 
    procedure Test_Count_Price_d382e2_cce97c(Gnattest_T: in out Test);
-   --  bases.ads:235:4:Count_Price:Test_CountPrice
+   --  bases.ads:207:4:Count_Price:Test_CountPrice
 
    procedure Test_Generate_Recruits_05fabc_06ea09(Gnattest_T: in out Test);
-   --  bases.ads:262:4:Generate_Recruits:Test_GenerateRecruits
+   --  bases.ads:234:4:Generate_Recruits:Test_GenerateRecruits
 
    procedure Test_Ask_For_Bases_023c5f_f3f6c6(Gnattest_T: in out Test);
-   --  bases.ads:270:4:Ask_For_Bases:Test_AskForBases
+   --  bases.ads:242:4:Ask_For_Bases:Test_AskForBases
 
    procedure Test_Ask_For_Events_4f17f6_3e359b(Gnattest_T: in out Test);
-   --  bases.ads:278:4:Ask_For_Events:Test_AskForEvents
+   --  bases.ads:250:4:Ask_For_Events:Test_AskForEvents
 
    procedure Test_Update_Population_31557d_10dec8(Gnattest_T: in out Test);
-   --  bases.ads:286:4:Update_Population:Test_UpdatePopulation
+   --  bases.ads:258:4:Update_Population:Test_UpdatePopulation
 
    procedure Test_Update_Prices_56b29f_f6cd8d(Gnattest_T: in out Test);
-   --  bases.ads:294:4:Update_Prices:Test_UpdatePrices
+   --  bases.ads:266:4:Update_Prices:Test_UpdatePrices
 
 end Bases.Test_Data.Tests;
 --  end read only
--- tests/crew-test_data-tests.ads
+++ tests/crew-test_data-tests.ads
@@ -13,31 +13,28 @@ package Crew.Test_Data.Tests is
    procedure Test_Gain_Exp_2f784d_7ccf15(Gnattest_T: in out Test);
    --  crew.ads:286:4:Gain_Exp:Test_GainExp
 
-   procedure Test_Generate_Member_Name_c820d7_af04e0(Gnattest_T: in out Test);
-   --  crew.ads:302:4:Generate_Member_Name:Test_GenerateMemberName
-
    procedure Test_Find_Cabin_6a58e0_006804(Gnattest_T: in out Test);
-   --  crew.ads:319:4:Find_Cabin:Test_FindCabin
+   --  crew.ads:318:4:Find_Cabin:Test_FindCabin
 
    procedure Test_Update_Crew_f9e489_011eae(Gnattest_T: in out Test);
-   --  crew.ads:331:4:Update_Crew:Test_UpdateCrew
+   --  crew.ads:330:4:Update_Crew:Test_UpdateCrew
 
    procedure Test_Wait_For_Rest_2f3023_b046aa(Gnattest_T: in out Test);
-   --  crew.ads:341:4:Wait_For_Rest:Test_WaitForRest
+   --  crew.ads:340:4:Wait_For_Rest:Test_WaitForRest
 
    procedure Test_Get_Skill_Level_Name_ac7e2a_35c4c0(Gnattest_T: in out Test);
-   --  crew.ads:353:4:Get_Skill_Level_Name:Test_GetSkillLevelName
+   --  crew.ads:352:4:Get_Skill_Level_Name:Test_GetSkillLevelName
 
    procedure Test_Get_Attribute_Level_Name_e6c169_613ac1
      (Gnattest_T: in out Test);
-   --  crew.ads:365:4:Get_Attribute_Level_Name:Test_GetAttributeLevelName
+   --  crew.ads:364:4:Get_Attribute_Level_Name:Test_GetAttributeLevelName
 
    procedure Test_Daily_Payment_f0944a_0bfd06(Gnattest_T: in out Test);
-   --  crew.ads:375:4:Daily_Payment:Test_DailyPayment
+   --  crew.ads:374:4:Daily_Payment:Test_DailyPayment
 
    procedure Test_Get_Training_Tool_Quality_6d83ce_af65c0
      (Gnattest_T: in out Test);
-   --  crew.ads:390:4:Get_Training_Tool_Quality:Test_GetTrainingToolQuality
+   --  crew.ads:389:4:Get_Training_Tool_Quality:Test_GetTrainingToolQuality
 
 end Crew.Test_Data.Tests;
 --  end read only
