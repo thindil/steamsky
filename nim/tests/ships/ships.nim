01bf6998b78752fe1d08a2097570b4f48375e21b
thindil <thindil@laeran.pl>
2022-10-28 03:19:35 +0000

    tests: added unit test for generateShipName
    FossilOrigin-Name: 0afba0a772cc6e4599f6f93fd2f3c74fbdab5a4feb6bf200cd07b195b5a60250

--- nim/tests/ships/ships.nim
+++ nim/tests/ships/ships.nim
@@ -2,6 +2,13 @@ discard """
   exitcode: 0
 """
 
-import ../../src/ships
+import ../../src/[careers, factions, game, items, ships]
+
+loadData("../bin/data/game.dat")
+loadItems("../bin/data/items.dat")
+loadCareers("../bin/data/careers.dat")
+loadFactions("../bin/data/factions.dat")
 
 assert getCabinQuality(10) == "Empty room"
+
+assert generateShipName("POLEIS").len() > 0
