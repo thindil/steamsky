6e489a0bca9df337edea1ab3271401f071723664
thindil <thindil@laeran.pl>
2022-11-10 04:49:11 +0000

    tests: added unit test for getItemDamage
    FossilOrigin-Name: 099f07501c5a4836328915189ad08bd94854c01fb2c48dfe564ea7fe7eb4b2b2

--- nim/tests/items/items.nim
+++ nim/tests/items/items.nim
@@ -5,5 +5,9 @@ discard """
 import ../../src/items
 
 loadItems("../bin/data/items.dat")
+
 assert findProtoItem("Iron") > 0
 assert findProtoItem("sfdsfsdfsdf") == 0
+
+assert getItemDamage(60) == "Damaged"
+assert getItemDamage(60, true) == "damaged"
