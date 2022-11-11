8f177c9fcb49225d4ce9f4a7969a14cd16d1334b
thindil <thindil@laeran.pl>
2022-11-11 04:58:08 +0000

    tests: added unit test for getItemName
    FossilOrigin-Name: 0903f7fc568d7d41bc22b9446bf36a73605ac321a7e9f41a42174129c8ded120

--- nim/tests/items/items.nim
+++ nim/tests/items/items.nim
@@ -11,3 +11,11 @@ assert findProtoItem("sfdsfsdfsdf") == 0
 
 assert getItemDamage(60) == "Damaged"
 assert getItemDamage(60, true) == "damaged"
+
+block:
+  var item = InventoryData(protoIndex: 2, amount: 1, name: "", durability: 80, price: 0)
+  assert getItemName(item) == "Basic Ration (slightly used)"
+  assert getItemName(item, false) == "Basic Ration"
+  assert getItemName(item, true, false) == "Basic Ration (Slightly used)"
+  item.name = "New name"
+  assert getItemName(item, false) == "New name"
