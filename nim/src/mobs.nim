7ebd40dd04bb61d85e6ca7f9e49d27d0b2ab3dd7
thindil <thindil@laeran.pl>
2022-11-22 04:35:29 +0000

    refactor: continue work on move getRandomItem to Nim
    FossilOrigin-Name: da01271d4d3cd51d1c03fc05e63653c76cd238d0bca6b9129c70b7bdccb9e585

--- nim/src/mobs.nim
+++ nim/src/mobs.nim
@@ -18,7 +18,7 @@
 {.used.}
 
 import std/tables
-import crew, factions, items, utils
+import crafts, crew, factions, items, utils
 
 proc getRandomItem*(itemsIndexes: seq[Positive], equipIndex: EquipmentLocations,
     highestLevel, weaponSkillLevel: Positive, factionIndex: string): Natural =
@@ -74,7 +74,7 @@ proc getRandomItem*(itemsIndexes: seq[Positive], equipIndex: EquipmentLocations,
 # Temporary code for interfacing with Ada
 
 proc getAdaRandomItem(items: cstring, equipIndex, highestLevel,
-    weaponSkillLevel: cint; factionIndex: cstring): cint {.exportc.} =
+    weaponSkillLevel: cint; factionIndex: cstring; highestSkill: cint): cint {.exportc.} =
   case $items
   of "weapon":
     return getRandomItem(itemsIndexes = weaponsList,
@@ -98,5 +98,16 @@ proc getAdaRandomItem(items: cstring, equipIndex, highestLevel,
         weaponSkillLevel = weaponSkillLevel, factionIndex = $factionIndex).cint
   of "legs":
     return getRandomItem(itemsIndexes = legsArmorsList,
+        equipIndex = equipIndex.EquipmentLocations, highestLevel = highestLevel,
+        weaponSkillLevel = weaponSkillLevel, factionIndex = $factionIndex).cint
+  of "tool":
+    var tempToolsList: seq[Positive]
+    for recipe in recipesList.values:
+      if highestSkill == recipe.skill:
+        for j in 0..<itemsList.len:
+          if itemsList[j].itemType == recipe.tool:
+            tempToolsList.add(y = j)
+        break
+    return getRandomItem(itemsIndexes = tempToolsList,
         equipIndex = equipIndex.EquipmentLocations, highestLevel = highestLevel,
         weaponSkillLevel = weaponSkillLevel, factionIndex = $factionIndex).cint
