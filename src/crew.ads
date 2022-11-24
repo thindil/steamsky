baf88bbe87e5263b6651c96a9213de0046e0160d
thindil <thindil@laeran.pl>
2022-11-24 04:21:09 +0000

    style: removed unnecessary parentheses
    FossilOrigin-Name: 7b183a3a4cae1fc6914721bf96c8005af238cac30ba431c81000db1d3553a659

--- src/crew.ads
+++ src/crew.ads
@@ -300,7 +300,7 @@ package Crew is
       -- SOURCE
    function Get_Attribute_Level_Name
      (Attribute_Level: Positive) return String with
-      Pre => (Attribute_Level <= 50),
+      Pre => Attribute_Level <= 50,
       Test_Case => (Name => "Test_GetAttributeLevelName", Mode => Nominal);
       -- ****
 
