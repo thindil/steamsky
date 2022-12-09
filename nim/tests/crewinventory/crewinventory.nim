f553c4e8915e6a7b644af0caf589eeb8839b9b10
thindil <thindil@laeran.pl>
2022-12-09 04:15:44 +0000

    tests: added unit test for itemIsUsed procedure
    FossilOrigin-Name: 92feb0d3b6dff426c239dff391b4092ff548a6fa92e8a13059d75a27dbe3e1e7

--- nim/tests/crewinventory/crewinventory.nim
+++ nim/tests/crewinventory/crewinventory.nim
@@ -8,6 +8,13 @@ var member = MemberData(homeBase: 1)
 const attribute = MobAttributeRecord(level: 1, experience: 0)
 member.attributes = @[attribute, attribute, attribute, attribute]
 member.inventory.add(InventoryData(amount: 1, protoIndex: 1))
+member.inventory.add(InventoryData(amount: 1, protoIndex: 2))
+for index, _ in member.equipment.mpairs:
+  member.equipment[index] = -1
+member.equipment[weapon] = 1
 playerShip.crew.add(member)
 
 discard freeInventory(0, 0)
+
+assert not itemIsUsed(0, 0)
+assert itemIsUsed(0, 1)
