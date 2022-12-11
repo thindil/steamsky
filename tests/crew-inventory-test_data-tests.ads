59dad28b23b1d707ab2972b366faa6e11b3b6c1d
thindil <thindil@laeran.pl>
2022-12-11 04:20:11 +0000

    tests: updated unit tests
    FossilOrigin-Name: 0cd3cd27ec011dfaeb01e80c6b2ff8e426e788cbdf93556e0d11da92f1e295db

--- tests/crew-inventory-test_data-tests.ads
+++ tests/crew-inventory-test_data-tests.ads
@@ -14,9 +14,6 @@ package Crew.Inventory.Test_Data.Tests is
    procedure Test_Update_Inventory_98f7ee_774caf(Gnattest_T: in out Test);
    --  crew-inventory.ads:46:4:Update_Inventory:Test_UpdateInventory
 
-   procedure Test_Take_Off_Item_a8430e_5a9c8c(Gnattest_T: in out Test);
-   --  crew-inventory.ads:89:4:Take_Off_Item:Test_TakeOffItem
-
    procedure Test_Find_Tools_2a8899_b022e2(Gnattest_T: in out Test);
    --  crew-inventory.ads:135:4:Find_Tools:Test_FindTools
 
