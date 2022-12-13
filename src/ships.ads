a265b1687ebd0dd2f3c70232a045927963e90f50
thindil <thindil@laeran.pl>
2022-12-13 04:07:11 +0000

    refactor: added argument to take inventory from the player ship
    FossilOrigin-Name: 22e36f494e4461502e729127a6c551c7c7ec52538c7727a599cd3d372af9c7ee

--- src/ships.ads
+++ src/ships.ads
@@ -484,13 +484,15 @@ package Ships is
    procedure Get_Ada_Crew;
 
    procedure Get_Ada_Crew_Inventory
-     (Inventory: Nim_Inventory_Array; Member_Index: Positive) with
+     (Inventory: Nim_Inventory_Array; Member_Index: Positive;
+      Get_Player_Ship: Natural := 1) with
       Import => True,
       Convention => C,
       External_Name => "getAdaCrewInventory";
 
    procedure Set_Ada_Crew_Inventory
-     (Inventory: out Nim_Inventory_Array; Member_Index: Positive) with
+     (Inventory: out Nim_Inventory_Array; Member_Index: Positive;
+      Get_Player_Ship: Natural := 1) with
       Import => True,
       Convention => C,
       External_Name => "setAdaCrewInventory";
