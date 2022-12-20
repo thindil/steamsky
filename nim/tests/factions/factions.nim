949c390bf35b65e1bc068be4234f4431c8667c9e
thindil <thindil@laeran.pl>
2022-12-20 04:23:05 +0000

    tests: added unit test for getReputation procedure
    FossilOrigin-Name: da12eca3371ea625ba5e6ed8cb3828b4579e618338698cfa37b75b31c4646a53

---
+++ nim/tests/factions/factions.nim
@@ -0,0 +1,13 @@
+discard """
+  exitcode: 0
+"""
+
+import ../../src/[factions, careers, game, items]
+
+loadData("../bin/data/game.dat")
+loadItems("../bin/data/items.dat")
+loadCareers("../bin/data/careers.dat")
+loadFactions("../bin/data/factions.dat")
+
+assert getReputation("POLEIS", "POLEIS") == 0
+assert getReputation("POLEIS", "PIRATES") == -10
