02442c9502e4aeac1908b551c2bbf57177c0c8e7
thindil <thindil@laeran.pl>
2022-12-27 04:13:05 +0000

    tests: added unit test for getPrice procedure
    FossilOrigin-Name: 6df96f25373c618590ff69c28ba37d19591428f204964b52fcb0b441427f5703

---
+++ nim/tests/tbasestypes/tbasestypes.nim
@@ -0,0 +1,12 @@
+discard """
+  exitcode: 0
+"""
+
+import ../../src/[basestypes, game, items]
+
+loadData("../bin/data/game.dat")
+loadItems("../bin/data/items.dat")
+loadBasesTypes("../bin/data/bases.dat")
+
+assert getPrice("0", 1) == 0
+assert getPrice("1", 2) > 0
