9ce94ec8e9f2b8ad04a6e77da8f95d96017c414e
thindil <thindil@laeran.pl>
2022-12-31 04:11:19 +0000

    tests: updated unit tests
    FossilOrigin-Name: fed8dc2e966610ae28b7d6eb4009a2b2a69abbeed9b5428f040dd449c9a6f4fa

--- tests/basestypes-test_data-tests-basestrade_container.ads
+++
@@ -1,2 +0,0 @@
-package BasesTypes.Test_Data.Tests.BasesTrade_Container is
-end BasesTypes.Test_Data.Tests.BasesTrade_Container;
--- tests/basestypes-test_data-tests-basestypes_container.ads
+++
@@ -1,2 +0,0 @@
-package BasesTypes.Test_Data.Tests.BasesTypes_Container is
-end BasesTypes.Test_Data.Tests.BasesTypes_Container;
