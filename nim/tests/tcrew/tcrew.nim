95bd36129b3887af37344179ab5ae7c695123ee1
thindil <thindil@laeran.pl>
2023-01-03 03:57:01 +0000

    tests: fixed loading data for crew module tests during merging in megatest
    FossilOrigin-Name: dbe67a4ee43a16365ea785a4167ab3e0b3fdd99d3d0ad5820fb95c883912284b

--- nim/tests/tcrew/tcrew.nim
+++ nim/tests/tcrew/tcrew.nim
@@ -2,11 +2,13 @@ discard """
   exitcode: 0
 """
 
+import std/tables
 import ../../src/[careers, crew, factions, game, items]
 
-loadData("../bin/data/game.dat")
-loadItems("../bin/data/items.dat")
-loadCareers("../bin/data/careers.dat")
-loadFactions("../bin/data/factions.dat")
+if itemsList.len == 0:
+  loadData("../bin/data/game.dat")
+  loadItems("../bin/data/items.dat")
+  loadCareers("../bin/data/careers.dat")
+  loadFactions("../bin/data/factions.dat")
 
 assert generateMemberName('M', "POLEIS").len() > 0
