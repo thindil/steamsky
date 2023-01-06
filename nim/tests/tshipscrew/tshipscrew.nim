87f3b9f00a24dcc5e9528fd45724af326139e637
thindil <thindil@laeran.pl>
2023-01-06 04:54:53 +0000

    tests: added unit test for updateMorale procedure
    FossilOrigin-Name: 43a9f0891e776455110951814002dd1e8cfe9a3cdf2ae7e04466e233b08df72c

---
+++ nim/tests/tshipscrew/tshipscrew.nim
@@ -0,0 +1,20 @@
+discard """
+  exitcode: 0
+"""
+
+import std/tables
+import ../../src/[careers, factions, game, items, ships, shipscrew, types]
+
+if itemsList.len == 0:
+  loadData("../bin/data/game.dat")
+  loadItems("../bin/data/items.dat")
+  loadCareers("../bin/data/careers.dat")
+  loadFactions("../bin/data/factions.dat")
+
+playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural], homeBase: 1, faction: "POLEIS"))
+let oldMorale = playerShip.crew[0].morale[2]
+let oldLevel = playerShip.crew[0].morale[1]
+updateMorale(playerShip, 0, 1)
+assert playerShip.crew[0].morale[2] == oldMorale + 1 or playerShip.crew[0].morale[1] == oldLevel + 1
+updateMorale(playerShip, 0, -1)
+assert playerShip.crew[0].morale[2] == oldMorale
