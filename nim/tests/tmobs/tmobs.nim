c6ad5f05ced9a3914b55d540b01f99173fba3ca1
thindil <thindil@laeran.pl>
2023-01-07 03:52:58 +0000

    tests: fixed loading data for mobs module tests during merging in megatest
    FossilOrigin-Name: 2d8928d2152359fd3d028f13b7dc53c3ebe9eeae8fedeba828835ed55957f50f

--- nim/tests/tmobs/tmobs.nim
+++ nim/tests/tmobs/tmobs.nim
@@ -5,10 +5,11 @@ discard """
 import std/tables
 import ../../src/[careers, factions, game, items, mobs, types]
 
-loadData("../bin/data/game.dat")
-loadItems("../bin/data/items.dat")
-loadCareers("../bin/data/careers.dat")
-loadFactions("../bin/data/factions.dat")
+if itemsList.len == 0:
+  loadData("../bin/data/game.dat")
+  loadItems("../bin/data/items.dat")
+  loadCareers("../bin/data/careers.dat")
+  loadFactions("../bin/data/factions.dat")
 
 let itemIndex = getRandomItem(weaponsList, weapon, 20, 20, "POLEIS")
 assert itemIndex > 0 and itemsList.hasKey(itemIndex)
