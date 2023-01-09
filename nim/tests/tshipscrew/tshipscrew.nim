13e9cb2685919d9642b94bc188403dd820900989
thindil <thindil@laeran.pl>
2023-01-09 04:15:11 +0000

    tests: fixed unit test for shipscrew module
    FossilOrigin-Name: 5d01f51330100c3a1d0133c6d142d04e10a68b0465f835cc1d0dcb6f6530e3bb

--- nim/tests/tshipscrew/tshipscrew.nim
+++ nim/tests/tshipscrew/tshipscrew.nim
@@ -11,6 +11,7 @@ if itemsList.len == 0:
   loadCareers("../bin/data/careers.dat")
   loadFactions("../bin/data/factions.dat")
 
+playerShip.crew = @[]
 playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural], homeBase: 1, faction: "POLEIS"))
 let oldMorale = playerShip.crew[0].morale[2]
 let oldLevel = playerShip.crew[0].morale[1]
