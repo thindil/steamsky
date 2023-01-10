59729964354ddb8a36ae345762531c89b84b9f98
thindil <thindil@laeran.pl>
2023-01-10 04:01:57 +0000

    refactor: added BaseModuleData to Nim
    FossilOrigin-Name: 439a64a542298308289d7d773ee9f1a904509838cc0d49ff6d9e7c16ca9920a7

--- nim/src/shipmodules.nim
+++ nim/src/shipmodules.nim
@@ -15,9 +15,29 @@
 # You should have received a copy of the GNU General Public License
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
+import types
+
 type
   ModuleType* = enum
     any, engine, cabin, cockpit, turret, gun, cargo, hull, armor, batteringRam,
     alchemyLab, furnace, waterCollector, workshop, greenhouse, medicalRoom,
     harpoonGun, trainingRoom
     ## Types of available prototypes of ships modules
+
+  BaseModuleData* = object
+    name*: string
+    mType*: ModuleType
+    weight*: Natural
+    value*: int
+    maxValue*: int
+    durability*: int
+    repairMaterial*: string
+    repairSkill*: Positive
+    price*: Natural
+    installTime*: Positive
+    unique*: bool
+    size*: range[1..10]
+    description*: string
+    maxOwners*: range[0..10]
+    speed*: int
+    reputation*: ReputationRange
