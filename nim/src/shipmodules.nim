0549ffcb2b9f2c60174ea5086137fb5724341004
thindil <thindil@laeran.pl>
2023-01-11 04:21:25 +0000

    style: code formatting
    FossilOrigin-Name: 0d7de27a769d4562e2ddfe8919ec82feb20920b6385681f1e354b600affd8198

--- nim/src/shipmodules.nim
+++ nim/src/shipmodules.nim
@@ -21,21 +21,21 @@ import game, items, log, types
 type
   BaseModuleData* = object
     ## Used to store information about prototypes of ships' modules
-    name*: string ## The name of the module
-    mType*: ModuleType ## The type of the module
-    weight*: Natural ## The weight of the module
-    value*: int ## Additional data for the module, for engines it is power
-    maxValue*: int ## Additional data for the mode, for guns it is damage
-    durability*: int ## The base durability of the module
-    repairMaterial*: string ## The index of the material used to repair the module
-    repairSkill*: Positive ## The index of the skill used to repair the module
-    price*: Natural ## The base price of the module in shipyards
-    installTime*: Positive ## The amount of time needed to install the module
-    unique*: bool ## If true, only one that module can be installed on the ship
-    size*: range[1..10] ## The size of the module
-    description*: string ## The description of the module
+    name*: string            ## The name of the module
+    mType*: ModuleType       ## The type of the module
+    weight*: Natural         ## The weight of the module
+    value*: int              ## Additional data for the module, for engines it is power
+    maxValue*: int           ## Additional data for the mode, for guns it is damage
+    durability*: int         ## The base durability of the module
+    repairMaterial*: string  ## The index of the material used to repair the module
+    repairSkill*: Positive   ## The index of the skill used to repair the module
+    price*: Natural          ## The base price of the module in shipyards
+    installTime*: Positive   ## The amount of time needed to install the module
+    unique*: bool            ## If true, only one that module can be installed on the ship
+    size*: range[1..10]      ## The size of the module
+    description*: string     ## The description of the module
     maxOwners*: range[0..10] ## The amount of users of the module
-    speed*: int ## How fast the gun shoots in the combat
+    speed*: int              ## How fast the gun shoots in the combat
     reputation*: ReputationRange ## The minumum amount of reputation needed for buy the module
 
 var modulesList* = initTable[Positive, BaseModuleData]() ## The list of prototypes of all ships' modules available in the game
