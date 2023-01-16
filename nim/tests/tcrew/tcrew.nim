964df272ed818390d79e2dca5bf031682f0377a5
thindil <thindil@laeran.pl>
2023-01-16 04:01:24 +0000

    tests: added unit test for getTrainingToolsQuality procedure
    FossilOrigin-Name: ce78450f34a654329db6ed1733b9e2bd1febca0796173ee163f8d88884057be9

--- nim/tests/tcrew/tcrew.nim
+++ nim/tests/tcrew/tcrew.nim
@@ -3,7 +3,7 @@ discard """
 """
 
 import std/tables
-import ../../src/[careers, crew, factions, game, items]
+import ../../src/[careers, crew, factions, game, items, ships, types]
 
 if itemsList.len == 0:
   loadData("../bin/data/game.dat")
@@ -12,3 +12,9 @@ if itemsList.len == 0:
   loadFactions("../bin/data/factions.dat")
 
 assert generateMemberName('M', "POLEIS").len() > 0
+
+playerShip.crew = @[]
+playerShip.crew.add(MemberData(skills: @[SkillInfo(index: 4, level: 1,
+    experience: 0)], homeBase: 1))
+
+assert getTrainingToolQuality(0, 1) == 100
