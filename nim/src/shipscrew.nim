10088ac1cc282607145274bc80865e73ca15a240
thindil <thindil@laeran.pl>
2023-01-17 04:15:09 +0000

    refactor: moved giveOrders to Nim
    FossilOrigin-Name: 1a5c50d0f69ac9b515699d46a4e6646d0e7f5ee384de646d6b7245dca3a19ffb

--- nim/src/shipscrew.nim
+++ nim/src/shipscrew.nim
@@ -19,6 +19,62 @@ import std/tables
 import crew, crewinventory, game, messages, shipmodules, ships, shipscargo,
     types, utils
 
+proc updateMorale*(ship: var ShipRecord; memberIndex: Natural;
+    value: int) {.sideEffect, raises: [KeyError], tags: [].} =
+  ## FUNCTION
+  ##
+  ## Update the morale of the selected crew member in the selected ship
+  ##
+  ## PARAMETERS
+  ##
+  ## * ship        - the ship in which the crew member's morale will be changed
+  ## * memberIndex - the index of the crew member which morale will be changed
+  ## * value       - the value with which the morale will be changed
+  ##
+  ## RETURNS
+  ##
+  ## The modified ship parameter with updated morale of the selected crew
+  ## member.
+  var newMorale, newLoyalty, newValue: int
+  let factionIndex = ship.crew[memberIndex].faction
+  if "nomorale" in factionsList[factionIndex].flags:
+    return
+  newValue = value
+  if "fanaticism" in factionsList[factionIndex].flags:
+    if value > 0:
+      newValue = value * 5
+    else:
+      newValue = (value / 10).int
+      if newValue == 0 and getRandom(min = 1, max = 10) <= value.abs:
+        newValue = -1
+      if newValue == 0:
+        return
+  newValue = ship.crew[memberIndex].morale[2] + newValue
+  newMorale = ship.crew[memberIndex].morale[1]
+  while newValue >= 5:
+    newValue = newValue - 5
+    newMorale = newMorale + 1
+  while newValue < 0:
+    newValue = newValue + 5
+    newMorale = newMorale - 1
+  if newMorale > 100:
+    newMorale = 100
+  elif newMorale < 0:
+    newMorale = 0
+  ship.crew[memberIndex].morale = [1: newMorale.Natural, 2: newValue.Natural]
+  if ship.crew == playerShip.crew and memberIndex == 1:
+    return
+  newLoyalty = ship.crew[memberIndex].loyalty
+  if newMorale > 74 and newLoyalty < 100:
+    newLoyalty.inc
+  if newMorale < 25 and newLoyalty > 0:
+    newLoyalty = newLoyalty - getRandom(min = 5, max = 10)
+  if newLoyalty > 100:
+    newLoyalty = 100
+  elif newLoyalty < 0:
+    newLoyalty = 0
+  ship.crew[memberIndex].loyalty = newLoyalty
+
 proc updateOrders*(ship: var ShipRecord; combat: bool = false)
 
 proc giveOrders*(ship: var ShipRecord; memberIndex: Natural;
@@ -217,65 +273,15 @@ proc giveOrders*(ship: var ShipRecord; memberIndex: Natural;
           if owner == 0:
             ship.modules[moduleIndex2].owner[index] = memberIndex
             break
+  ship.crew[memberIndex].order = givenOrder
+  ship.crew[memberIndex].orderTime = 15
+  if givenOrder != rest:
+    updateMorale(ship = ship, memberIndex = memberIndex, value = -1)
+  if checkPriorities:
+    updateOrders(ship = ship)
 
 proc updateOrders(ship: var ShipRecord; combat: bool = false) =
   discard
-
-proc updateMorale*(ship: var ShipRecord; memberIndex: Natural;
-    value: int) {.sideEffect, raises: [KeyError], tags: [].} =
-  ## FUNCTION
-  ##
-  ## Update the morale of the selected crew member in the selected ship
-  ##
-  ## PARAMETERS
-  ##
-  ## * ship        - the ship in which the crew member's morale will be changed
-  ## * memberIndex - the index of the crew member which morale will be changed
-  ## * value       - the value with which the morale will be changed
-  ##
-  ## RETURNS
-  ##
-  ## The modified ship parameter with updated morale of the selected crew
-  ## member.
-  var newMorale, newLoyalty, newValue: int
-  let factionIndex = ship.crew[memberIndex].faction
-  if "nomorale" in factionsList[factionIndex].flags:
-    return
-  newValue = value
-  if "fanaticism" in factionsList[factionIndex].flags:
-    if value > 0:
-      newValue = value * 5
-    else:
-      newValue = (value / 10).int
-      if newValue == 0 and getRandom(min = 1, max = 10) <= value.abs:
-        newValue = -1
-      if newValue == 0:
-        return
-  newValue = ship.crew[memberIndex].morale[2] + newValue
-  newMorale = ship.crew[memberIndex].morale[1]
-  while newValue >= 5:
-    newValue = newValue - 5
-    newMorale = newMorale + 1
-  while newValue < 0:
-    newValue = newValue + 5
-    newMorale = newMorale - 1
-  if newMorale > 100:
-    newMorale = 100
-  elif newMorale < 0:
-    newMorale = 0
-  ship.crew[memberIndex].morale = [1: newMorale.Natural, 2: newValue.Natural]
-  if ship.crew == playerShip.crew and memberIndex == 1:
-    return
-  newLoyalty = ship.crew[memberIndex].loyalty
-  if newMorale > 74 and newLoyalty < 100:
-    newLoyalty.inc
-  if newMorale < 25 and newLoyalty > 0:
-    newLoyalty = newLoyalty - getRandom(min = 5, max = 10)
-  if newLoyalty > 100:
-    newLoyalty = 100
-  elif newLoyalty < 0:
-    newLoyalty = 0
-  ship.crew[memberIndex].loyalty = newLoyalty
 
 # Temporary code for interfacing with Ada
 
