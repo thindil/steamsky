791859b859aa8ca697cb2d4b5a201f3a688c8412
thindil <thindil@laeran.pl>
2023-01-18 04:42:13 +0000

    refactor: started work on getting event from Ada
    FossilOrigin-Name: a69099446f32fc3b18715d11210dce7f7096aad5ec3a813b47afee69ce5ac359

--- nim/src/events.nim
+++ nim/src/events.nim
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
+import std/tables
 import types
 
 type
@@ -25,15 +26,30 @@ type
 
   EventData* = object
     ## Used to store data about an event
-    skyX*: MapXRange ## The X coordinate of the event on the map
-    skyY*: MapYRange ## The Y coordinate of the event on the map
-    time*: Positive ## The time in minutes by how long the event will be available
+    skyX*: MapXRange         ## The X coordinate of the event on the map
+    skyY*: MapYRange         ## The Y coordinate of the event on the map
+    time*: Positive          ## The time in minutes by how long the event will be available
     case eType*: EventsTypes ## The type of the event
     of doublePrice:
-      itemIndex: Positive ## The index of the prototype item used by the event
+      itemIndex: int         ## The index of the prototype item used by the event
     of attackOnBase, enemyShip, enemyPatrol, trader, friendlyShip:
-      shipIndex: Positive ## The index of the prototype ship used by the event
+      shipIndex: int         ## The index of the prototype ship used by the event
     else:
-      data: Natural ## General data of the event
+      data: int              ## General data of the event
 
-var eventsList*: seq[EventData] ## The list of available events in the game
+var eventsList* = initTable[Positive, EventData]() ## The list of available events in the game
+
+
+# Temporary code for interfacing with Ada
+
+proc getAdaEvent(index, x, y, time, eType, data: cint) {.exportc.} =
+  var event = EventData(skyX: x, skyY: y, time: time, eType: eType.EventsTypes)
+  case event.eType
+    of doublePrice:
+      event.itemIndex = data
+    of attackOnBase, enemyShip, enemyPatrol, trader, friendlyShip:
+      event.shipIndex = data
+    else:
+      event.data = data
+  eventsList[index] = event
+
