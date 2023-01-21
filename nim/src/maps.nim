83ce79f77c1566b1daca2c482f4870bd1ad07a0f
thindil <thindil@laeran.pl>
2023-01-21 03:59:51 +0000

    docs: reorganized code documentation
    FossilOrigin-Name: a43a8044c0c01dc9cc755767803e7b8193bedf2edc63ad1b2edcc25144b719b0

--- nim/src/maps.nim
+++ nim/src/maps.nim
@@ -28,19 +28,13 @@ var skyMap*: array[MapXRange, array[MapYRange, SkyCell]] ## The list of all map'
 
 func normalizeCoord*(coord: var cint; isXAxis: cint = 1) {.gcsafe, raises: [],
     tags: [], exportc.} =
-  ## FUNCTION
-  ##
   ## Normalize (fix to be in range of) the map's coordinates
-  ##
-  ## PARAMETERS
   ##
   ## * coord   - The coordinate which will be normalized
   ## * isXAxis - If 1 the coordinate to be normalized is in X axis, otherwise
   ##             it is in Y axis
   ##
-  ## RETURNS
-  ##
-  ## The updated coord argument
+  ## Returns the updated coord argument
   if isXAxis == 1:
     if coord < MapXRange.low:
       coord = MapXRange.low
