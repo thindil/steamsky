0f7dbe76057d94018589e47af27d2dbb8b889e7f
thindil <thindil@laeran.pl>
2023-01-25 03:59:04 +0000

    tests: added unit test for giveOrders procedure
    FossilOrigin-Name: c4971f5e2e5d0b526ce4e1be9213f28900d80158f307ef0900ce44edafa05269

--- nim/tests/tshipscrew/tshipscrew.nim
+++ nim/tests/tshipscrew/tshipscrew.nim
@@ -3,7 +3,7 @@ discard """
 """
 
 import std/tables
-import ../../src/[careers, factions, game, items, ships, shipscrew, types]
+import ../../src/[careers, factions, game, maps, items, ships, shipscrew, types]
 
 if itemsList.len == 0:
   loadData("../bin/data/game.dat")
@@ -11,11 +11,29 @@ if itemsList.len == 0:
   loadCareers("../bin/data/careers.dat")
   loadFactions("../bin/data/factions.dat")
 
+playerShip.skyX = 1
+playerShip.skyY = 1
 playerShip.crew = @[]
-playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural], homeBase: 1, faction: "POLEIS"))
+playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural],
+    homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 2, 1, 1,
+    1, 0, 0], order: talk, loyalty: 100))
+playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural],
+    homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 1, 1, 1,
+    1, 0, 0], order: gunner, loyalty: 100))
+skyMap[1][1].baseIndex = 1
+
 let oldMorale = playerShip.crew[0].morale[2]
 let oldLevel = playerShip.crew[0].morale[1]
 updateMorale(playerShip, 0, 1)
-assert playerShip.crew[0].morale[2] == oldMorale + 1 or playerShip.crew[0].morale[1] == oldLevel + 1
+assert playerShip.crew[0].morale[2] == oldMorale + 1 or playerShip.crew[
+    0].morale[1] == oldLevel + 1
 updateMorale(playerShip, 0, -1)
 assert playerShip.crew[0].morale[2] == oldMorale
+
+updateMorale(playerShip, 0, 10)
+giveOrders(playerShip, 0, rest)
+assert playerShip.crew[0].order == talk
+
+updateMorale(playerShip, 1, 10)
+giveOrders(playerShip, 1, rest)
+assert playerShip.crew[1].order == rest
