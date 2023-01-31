5aedbb2ec485ab2d3816226c2b789c80cca30be6
thindil <thindil@laeran.pl>
2023-01-31 04:33:41 +0000

    tests: added unit test for freeCargo procedure
    FossilOrigin-Name: eebea86d464e9602101f0c39c2e52e08e55bd164e1cbb9e2b5107a08c86961fb

---
+++ nim/tests/tshipscargo/tshipscargo.nim
@@ -0,0 +1,19 @@
+discard """
+  exitcode: 0
+"""
+
+import std/tables
+import ../../src/[careers, factions, game, items, ships, shipscargo, types]
+
+if itemsList.len == 0:
+  loadData("../bin/data/game.dat")
+  loadItems("../bin/data/items.dat")
+  loadCareers("../bin/data/careers.dat")
+  loadFactions("../bin/data/factions.dat")
+
+playerShip.modules = @[]
+playerShip.modules.add(ModuleData(mType: cargoRoom, protoIndex: 7))
+playerShip.cargo = @[]
+playerShip.cargo.add(InventoryData(protoIndex: 1, amount: 100))
+
+assert freeCargo(1) > freeCargo(0)
