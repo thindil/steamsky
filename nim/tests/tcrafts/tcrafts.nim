7c34c4b061a673e5b6d16835775b07445050145b
thindil <thindil@laeran.pl>
2023-02-01 04:18:11 +0000

    tests: added unit test for checkRecipe procedure
    FossilOrigin-Name: 747f3d278d5d807dd4f9fb3b0432629800647457bd19aa3c48c12b48c3ccab19

--- nim/tests/tcrafts/tcrafts.nim
+++ nim/tests/tcrafts/tcrafts.nim
@@ -3,19 +3,25 @@ discard """
 """
 
 import std/tables
-import ../../src/[crafts, game, items, ships, shipscargo, types]
+import ../../src/[crafts, game, items, shipmodules, ships, shipscargo, types]
 
 if itemsList.len == 0:
   loadData("../bin/data/game.dat")
   loadItems("../bin/data/items.dat")
 if recipesList.len == 0:
   loadRecipes("../bin/data/recipes.dat")
+  loadModules("../bin/data/shipmodules.dat")
 
 playerShip.modules = @[]
-playerShip.modules.add(ModuleData(mType: ModuleType2.workshop, protoIndex: 6))
+playerShip.modules.add(ModuleData(mType: ModuleType2.workshop, protoIndex: 6,
+    durability: 100))
+playerShip.modules.add(ModuleData(mType: ModuleType2.cargoRoom, protoIndex: 7,
+    durability: 100))
 updateCargo(playerShip, 6, 10)
 
 setRecipe(0, 10, "1")
 assert playerShip.modules[0].craftingAmount == 10
 assert playerShip.modules[0].craftingIndex == "1"
+
+assert checkRecipe("1") == 10
 
