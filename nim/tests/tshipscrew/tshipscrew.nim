5bb80c022293fa70a629bba9d2ef369e68a33968
thindil <thindil@laeran.pl>
2023-02-07 04:13:15 +0000

    tests: added unit test for getSkillLevel procedure
    FossilOrigin-Name: d86d7db6fa566b198d16766e092bf3a3a4a8a25aa690fa65d1265e9867fb7132

--- nim/tests/tshipscrew/tshipscrew.nim
+++ nim/tests/tshipscrew/tshipscrew.nim
@@ -16,7 +16,10 @@ playerShip.skyY = 1
 playerShip.crew = @[]
 playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural],
     homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 2, 1, 1,
-    1, 0, 0], order: talk, loyalty: 100))
+    1, 0, 0], order: talk, loyalty: 100, skills: @[SkillInfo(index: 4, level: 4,
+    experience: 0)], attributes: @[MobAttributeRecord(level: 3, experience: 0),
+    MobAttributeRecord(level: 3, experience: 0), MobAttributeRecord(level: 3,
+    experience: 0), MobAttributeRecord(level: 3, experience: 0)]))
 playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural],
     homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 0, 1, 1,
     1, 0, 0], order: gunner, loyalty: 100))
@@ -39,3 +42,6 @@ assert playerShip.crew[1].order == rest
 giveOrders(playerShip, 0, rest, -1, false)
 updateOrders(playerShip)
 assert playerShip.crew[0].order == talk
+
+assert getSkillLevel(playerShip.crew[0], 1) == 0
+assert getSkillLevel(playerShip.crew[0], 4) == 1
