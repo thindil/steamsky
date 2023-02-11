5ad27f3566e26c7be86a5ff774512a1e08be5786
thindil <thindil@laeran.pl>
2023-02-11 03:58:50 +0000

    refactor: started work on move loadGoals to Nim
    FossilOrigin-Name: 91d9272a8a8fb2cd17b9a90b89cef861dcca7a3993410d5940a0f0cf210d8e2a

--- nim/src/goals.nim
+++ nim/src/goals.nim
@@ -15,7 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
-import std/strutils
+import std/[strutils, tables, xmlparser, xmltree]
+import game, log
 
 type
   GoalTypes = enum
@@ -31,7 +32,57 @@ type
                         ## means all targets of the selected type (bases, ships, etc.)
     multiplier: Positive ## The muliplier for points awarded for finishing the goal
 
-var currentGoal* = GoalData(multiplier: 1) ## The player's current goal
+var
+  currentGoal* = GoalData(multiplier: 1) ## The player's current goal
+  goalsList* = initTable[Positive, GoalData]() ## The list of available goals in the game
+
+proc loadGoals*(fileName: string) {.sideEffect, raises: [DataLoadingError],
+    tags: [WriteIOEffect, ReadIOEffect, RootEffect].} =
+  ## Load the goals data from the file
+  ##
+  ## * fileName - the name of the file to load
+  let goalsXml = try:
+      loadXml(path = fileName)
+    except XmlError, ValueError, IOError, OSError, Exception:
+      raise newException(exceptn = DataLoadingError,
+          message = "Can't load goals data file. Reason: " &
+          getCurrentExceptionMsg())
+  for goalNode in goalsXml:
+    if goalNode.kind != xnElement:
+      continue
+    let
+      goalIndex: Natural = try:
+          goalNode.attr(name = "index").parseInt()
+        except ValueError:
+          raise newException(exceptn = DataLoadingError,
+              message = "Can't add goal '" & goalNode.attr(name = "index") & "', invalid index.")
+      goalAction: DataAction = try:
+          parseEnum[DataAction](goalNode.attr(name = "action").toLowerAscii)
+        except ValueError:
+          DataAction.add
+    if goalAction in [update, remove]:
+      if goalIndex > goalsList.len():
+        raise newException(exceptn = DataLoadingError,
+            message = "Can't " & $goalAction & " goal '" & $goalIndex & "', there is no goal with that index.")
+    elif goalIndex < goalsList.len():
+      raise newException(exceptn = DataLoadingError,
+          message = "Can't add goal '" & $goalIndex & "', there is an goal with that index.")
+    if goalAction == DataAction.remove:
+      {.warning[ProveInit]: off.}
+      {.warning[UnsafeDefault]: off.}
+      goalsList.del(key = goalIndex)
+      {.warning[ProveInit]: on.}
+      {.warning[UnsafeDefault]: on.}
+      logMessage(message = "Goal removed: '" & $goalIndex & "'",
+          debugType = everything)
+      continue
+    var goal: GoalData = if goalAction == DataAction.update:
+        try:
+          goalsList[goalIndex]
+        except ValueError:
+          GoalData(multiplier: 1)
+      else:
+        GoalData(multiplier: 1)
 
 proc updateGoal*(goalType: GoalTypes; targetIndex: string;
     amount: Positive = 1) =
