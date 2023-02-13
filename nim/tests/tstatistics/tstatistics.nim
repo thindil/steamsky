ef755d1014818af1437a2df32046b9db2b4d3536
thindil <thindil@laeran.pl>
2023-02-13 04:02:03 +0000

    tests: added unit test for updateFinishedGoals procedure
    FossilOrigin-Name: 9554d4e4abde88e1ed9e1a92ebba9a17d0f709832e8addb7febf41d38a7920a6

--- nim/tests/tstatistics/tstatistics.nim
+++ nim/tests/tstatistics/tstatistics.nim
@@ -2,8 +2,18 @@ discard """
   exitcode: 0
 """
 
-import ../../src/statistics
+import std/tables
+import ../../src/[goals, statistics]
+
+if goalsList.len == 0:
+  loadGoals("../bin/data/goals.dat")
 
 gameStats.craftingOrders = @[]
 updateCraftingOrders("1")
 assert gameStats.craftingOrders.len == 1
+
+gameStats.finishedGoals = @[]
+updateFinishedGoals("1")
+assert gameStats.finishedGoals.len == 1
+updateFinishedGoals("Sdfdsf")
+assert gameStats.finishedGoals.len == 1
