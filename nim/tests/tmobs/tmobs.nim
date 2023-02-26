5fa1eebf7c2bf6518b79a820cacf64654e770c3c
thindil <thindil@laeran.pl>
2023-02-26 04:15:33 +0000

    tests: added unit test for generateMob procedure
    FossilOrigin-Name: 32bf0f4f876477c0e4de44010b9c24029215426536611569a44f56bb70218222

--- nim/tests/tmobs/tmobs.nim
+++ nim/tests/tmobs/tmobs.nim
@@ -10,6 +10,11 @@ if itemsList.len == 0:
   loadItems("../bin/data/items.dat")
   loadCareers("../bin/data/careers.dat")
   loadFactions("../bin/data/factions.dat")
+  loadMobs("../bin/data/mobs.dat")
 
 let itemIndex = getRandomItem(weaponsList, weapon, 20, 20, "POLEIS")
 assert itemIndex > 0 and itemsList.hasKey(itemIndex)
+
+let newMob = generateMob(5, "POLEIS")
+assert newMob.attributes[0].level == 2
+assert newMob.orderTime == 15
