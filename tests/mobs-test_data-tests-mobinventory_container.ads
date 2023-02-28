2d86c9bca5d5154002d1e1eb9d62ba3176955fe9
thindil <thindil@laeran.pl>
2023-02-28 03:51:52 +0000

    tests: updated unit tests
    FossilOrigin-Name: 1010bae87f3f3db14348cd17dd269da40d712c7079a40d0c07a06d68f0c8fd1a

--- tests/mobs-test_data-tests-mobinventory_container.ads
+++
@@ -1,2 +0,0 @@
-package Mobs.Test_Data.Tests.MobInventory_Container is
-end Mobs.Test_Data.Tests.MobInventory_Container;
--- tests/mobs-test_data-tests-protomobs_container.ads
+++
@@ -1,2 +0,0 @@
-package Mobs.Test_Data.Tests.ProtoMobs_Container is
-end Mobs.Test_Data.Tests.ProtoMobs_Container;
