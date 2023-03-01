6658ac55d3c60d8215a9e80ada33cd67f4da7d50
thindil <thindil@laeran.pl>
2023-03-01 04:13:26 +0000

    refactor: moved use clauses to proper location
    FossilOrigin-Name: e25e1dc9f941cf2e3c9be706287b86c3c1d4c649b0f446082768945c77cd3b41

--- src/mobs.adb
+++ src/mobs.adb
@@ -16,7 +16,7 @@
 --    along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
 with Interfaces.C.Strings; use Interfaces.C.Strings;
-with Items; use Items;
+with Items;
 
 package body Mobs is
 
@@ -39,6 +39,7 @@ package body Mobs is
    function Generate_Mob
      (Mob_Index: Positive; Faction_Index: Tiny_String.Bounded_String)
       return Member_Data is
+      use Items;
       use Tiny_String;
 
       --## rule off IMPROPER_INITIALIZATION
