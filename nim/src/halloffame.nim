71a9ada2c6438de389ee589d84e235f7f4cecb62
thindil <thindil@laeran.pl>
2023-03-03 05:55:41 +0000

    refactor: started work on move updateHallOfFame to Nim
    FossilOrigin-Name: 20dbbaeaafadc1e5b11ff882eee763c33ad643b8d6d245ba0e64c163150b5d88

--- nim/src/halloffame.nim
+++ nim/src/halloffame.nim
@@ -16,7 +16,7 @@
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
 import std/[strutils, xmlparser, xmltree]
-import game
+import game, statistics
 
 type
   HallOfFameData = object
@@ -50,6 +50,69 @@ proc loadHallOfFame*() {.sideEffect, raises: [DataLoadingError], tags: [
     hallOfFameArray[index].deathReason = hofNode.attr(name = "Death_Reason")
     index.inc
 
+proc updateHallOfFame*(playerName, deathReason: string) =
+  var newIndex: Natural = 0
+  for index, entry in hallOfFameArray.pairs:
+    if entry.points < getGamePoints():
+      newIndex = index
+      break
+  if newIndex == 0:
+    return
+  hallOfFameArray[newIndex + 1 .. hallOfFameArray.high] = hallOfFameArray[
+      newIndex .. hallOfFameArray.high - 1]
+  hallOfFameArray[newIndex] = HallOfFameData(name: playerName,
+      points: getGamePoints(), deathReason: deathReason)
+  for entry in hallOfFameArray:
+    var element = newElement("entry")
+    let values = {"name": entry.name, "points": $entry.points,
+        "Death_Reason": entry.deathReason}.toXmlAttributes
+    element.attrs = values
+
+#   procedure Update_Hall_Of_Fame
+#     (Player_Name, Death_Reason: Unbounded_String) is
+#      New_Index: Natural range 0 .. 10 := 0;
+#      Hof_File: File_Type;
+#      Hall_Of_Fame: DOM_Implementation; --## rule line off IMPROPER_INITIALIZATION
+#      Entry_Node, Main_Node: DOM.Core.Element;
+#      Hof_Data: Document;
+#   begin
+#      Hof_Data := Create_Document(Implementation => Hall_Of_Fame);
+#      Main_Node :=
+#        Append_Child
+#          (N => Hof_Data,
+#           New_Child =>
+#             Create_Element(Doc => Hof_Data, Tag_Name => "halloffame"));
+#      Update_Hall_Of_Fame_Loop :
+#      for Element of Hall_Of_Fame_Array loop
+#         if Element.Name = Null_Unbounded_String then
+#            exit Update_Hall_Of_Fame_Loop;
+#         end if;
+#         Entry_Node :=
+#           Append_Child
+#             (N => Main_Node,
+#              New_Child =>
+#                Create_Element(Doc => Hof_Data, Tag_Name => "entry"));
+#         Set_Attribute
+#           (Elem => Entry_Node, Name => "name",
+#            Value => To_String(Source => Element.Name));
+#         Set_Attribute
+#           (Elem => Entry_Node, Name => "points",
+#            Value =>
+#              Trim
+#                (Source => Integer'Image(Element.Points),
+#                 Side => Ada.Strings.Left));
+#         Set_Attribute
+#           (Elem => Entry_Node, Name => "Death_Reason",
+#            Value => To_String(Source => Element.Death_Reason));
+#      end loop Update_Hall_Of_Fame_Loop;
+#      Create
+#        (File => Hof_File, Mode => Out_File,
+#         Name => To_String(Source => Save_Directory) & "halloffame.dat");
+#      Write
+#        (Stream => Stream(File => Hof_File), N => Hof_Data,
+#         Pretty_Print => True);
+#      Close(File => Hof_File);
+#   end Update_Hall_Of_Fame;
 # Temporary code for interfacing with Ada
 
 type
