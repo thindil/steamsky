78a8d8302dbc1ace3d849f005a263bc43ed82c3f
thindil <thindil@laeran.pl>
2023-03-04 04:11:15 +0000

    refactor: get the game statistics before update the hall of fame
    FossilOrigin-Name: 30fc019823c0c822752eb103d47b3cfb401e2c741b85cd47b3c0ff9090b96485

--- src/halloffame.adb
+++ src/halloffame.adb
@@ -17,6 +17,7 @@
 
 with Interfaces.C.Strings; use Interfaces.C.Strings;
 with Game; use Game;
+with Statistics;
 
 package body HallOfFame is
 
@@ -65,11 +66,14 @@ package body HallOfFame is
 
    procedure Update_Hall_Of_Fame
      (Player_Name, Death_Reason: Unbounded_String) is
+      use Statistics;
+
       procedure Update_Ada_Hall_Of_Fame(P_Name, D_Reason: chars_ptr) with
          Import => True,
          Convention => C,
          External_Name => "updateAdaHallOfFame";
    begin
+      Get_Game_Stats;
       Update_Ada_Hall_Of_Fame
         (P_Name => New_String(Str => To_String(Source => Player_Name)),
          D_Reason => New_String(Str => To_String(Source => Death_Reason)));
