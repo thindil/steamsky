94f1fb81c50a38cc24caba97d8238f077c69278c
thindil <thindil@laeran.pl>
2023-03-04 04:16:12 +0000

    refactor: read hall of fame from Nim after update it
    FossilOrigin-Name: 60bf311cf9c624e02449f9527d71e4c2a49e9a8a6fe195ad8de669a151715df0

--- src/halloffame.adb
+++ src/halloffame.adb
@@ -21,16 +21,22 @@ with Statistics;
 
 package body HallOfFame is
 
+   --## rule off TYPE_INITIAL_VALUES
+   type Nim_Hall_Of_Fame_Data is record
+      Name: chars_ptr;
+      Points: Natural := 0;
+      Death_Reason: chars_ptr;
+   end record;
+   --## rule on TYPE_INITIAL_VALUES
+   procedure Get_Ada_Hof_Entry
+     (Index: Natural; N_Entry: out Nim_Hall_Of_Fame_Data) with
+      Import => True,
+      Convention => C,
+      External_Name => "getAdaHofEntry";
+
    procedure Load_Hall_Of_Fame is
       use Interfaces.C;
 
-      --## rule off TYPE_INITIAL_VALUES
-      type Nim_Hall_Of_Fame_Data is record
-         Name: chars_ptr;
-         Points: Natural := 0;
-         Death_Reason: chars_ptr;
-      end record;
-      --## rule on TYPE_INITIAL_VALUES
       --## rule off IMPROPER_INITIALIZATION
       Nim_Entry: Nim_Hall_Of_Fame_Data;
       Result: chars_ptr;
@@ -39,11 +45,6 @@ package body HallOfFame is
          Import => True,
          Convention => C,
          External_Name => "loadAdaHallOfFame";
-      procedure Get_Ada_Hof_Entry
-        (Index: Natural; N_Entry: out Nim_Hall_Of_Fame_Data) with
-         Import => True,
-         Convention => C,
-         External_Name => "getAdaHofEntry";
    begin
       Result := Load_Ada_Hall_Of_Fame;
       if Strlen(Item => Result) > 0 then
@@ -66,8 +67,12 @@ package body HallOfFame is
 
    procedure Update_Hall_Of_Fame
      (Player_Name, Death_Reason: Unbounded_String) is
+      use Interfaces.C;
       use Statistics;
 
+      --## rule off IMPROPER_INITIALIZATION
+      Nim_Entry: Nim_Hall_Of_Fame_Data;
+      --## rule on IMPROPER_INITIALIZATION
       procedure Update_Ada_Hall_Of_Fame(P_Name, D_Reason: chars_ptr) with
          Import => True,
          Convention => C,
@@ -77,6 +82,19 @@ package body HallOfFame is
       Update_Ada_Hall_Of_Fame
         (P_Name => New_String(Str => To_String(Source => Player_Name)),
          D_Reason => New_String(Str => To_String(Source => Death_Reason)));
+      Load_Hof_Loop :
+      for I in 1 .. 10 loop
+         Get_Ada_Hof_Entry(Index => I, N_Entry => Nim_Entry);
+         if Strlen(Item => Nim_Entry.Name) > 0 then
+            Hall_Of_Fame_Array(I) :=
+              (Name =>
+                 To_Unbounded_String(Source => Value(Item => Nim_Entry.Name)),
+               Points => Nim_Entry.Points,
+               Death_Reason =>
+                 To_Unbounded_String
+                   (Source => Value(Item => Nim_Entry.Death_Reason)));
+         end if;
+      end loop Load_Hof_Loop;
    end Update_Hall_Of_Fame;
 
 end HallOfFame;
