3e01ad9170bd3ed2763c66a4695bb316ee633230
thindil <thindil@laeran.pl>
2023-03-05 04:13:15 +0000

    tests: added unit test for updateHallOfFame procedure
    FossilOrigin-Name: da5df75f4ebf88ea05a57a5232abf105b57b96110bf0f9d2cecc2818edbd5015

---
+++ nim/tests/thalloffame/thalloffame.nim
@@ -0,0 +1,13 @@
+discard """
+  exitcode: 0
+"""
+
+import ../../src/[game, halloffame, statistics]
+
+for entry in hallOfFameArray.mitems:
+  entry = HallOfFameData(name: "", points: 0, deathReason: "")
+gameStats.points = 100
+saveDirectory = "."
+
+updateHallOfFame("TestPlayer", "TestDeath")
+assert hallOfFameArray[1].name == "TestPlayer"
