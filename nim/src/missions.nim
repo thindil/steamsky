3a26796eb8cc87a9c9b07d0922763739aa519b6d
thindil <thindil@laeran.pl>
2023-03-06 04:36:47 +0000

    refactor: continue work on add acceptedMissions list to Nim
    FossilOrigin-Name: 7c767f4e7924fba89712c02ee975cd285eec19dc9e7823d4595a08bf43d209b1

--- nim/src/missions.nim
+++ nim/src/missions.nim
@@ -33,8 +33,8 @@ type
     mType: cint
     data: cint
 
-proc getAdaAcceptedMissions(adaMissions: array[10, AdaMissionData]) {.raises: [],
-    tags: [], exportc.} =
+proc getAdaAcceptedMissions(adaMissions: array[50, AdaMissionData]) {.raises: [
+    ], tags: [], exportc.} =
   acceptedMissions = @[]
   for mission in adaMissions.items:
     if mission.time == 0:
@@ -46,5 +46,53 @@ proc getAdaAcceptedMissions(adaMissions: array[10, AdaMissionData]) {.raises: []
           reward: mission.reward, startBase: mission.startBase, finished: (
           if mission.finished == 1: true else: false),
           multiplier: mission.multiplier, itemIndex: mission.data))
+    of 1:
+      acceptedMissions.add(y = MissionData(mType: destroy, time: mission.time,
+          targetX: mission.targetX, targetY: mission.targetY,
+          reward: mission.reward, startBase: mission.startBase, finished: (
+          if mission.finished == 1: true else: false),
+          multiplier: mission.multiplier, shipIndex: mission.data))
+    of 2:
+      acceptedMissions.add(y = MissionData(mType: patrol, time: mission.time,
+          targetX: mission.targetX, targetY: mission.targetY,
+          reward: mission.reward, startBase: mission.startBase, finished: (
+          if mission.finished == 1: true else: false),
+          multiplier: mission.multiplier, target: mission.data))
+    of 3:
+      acceptedMissions.add(y = MissionData(mType: explore, time: mission.time,
+          targetX: mission.targetX, targetY: mission.targetY,
+          reward: mission.reward, startBase: mission.startBase, finished: (
+          if mission.finished == 1: true else: false),
+          multiplier: mission.multiplier, target: mission.data))
+    of 4:
+      acceptedMissions.add(y = MissionData(mType: passenger, time: mission.time,
+          targetX: mission.targetX, targetY: mission.targetY,
+          reward: mission.reward, startBase: mission.startBase, finished: (
+          if mission.finished == 1: true else: false),
+          multiplier: mission.multiplier, data: mission.data))
     else:
       discard
+
+proc setAdaAcceptedMissions(adaMissions: var array[50,
+    AdaMissionData]) {.raises: [], tags: [], exportc.} =
+  for mission in adaMissions.mitems:
+    mission = AdaMissionData(time: 0, targetX: 0, targetY: 0, reward: 0,
+        startBase: 0, finished: 0, multiplier: 0.0, mtype: 0, data: 0)
+  for index, mission in acceptedMissions.pairs:
+    adaMissions[index].time = mission.time.cint
+    adaMissions[index].targetX = mission.targetX
+    adaMissions[index].targetY = mission.targetY
+    adaMissions[index].reward = mission.reward.cint
+    adaMissions[index].startBase = mission.startBase
+    adaMissions[index].finished = (if mission.finished: 1 else: 0)
+    adaMissions[index].multiplier = mission.multiplier
+    adaMissions[index].mType = mission.mType.ord.cint
+    adaMissions[index].data = case mission.mType
+      of deliver:
+        mission.itemIndex.cint
+      of destroy:
+        mission.shipIndex.cint
+      of passenger:
+        mission.data.cint
+      else:
+        mission.target.cint
