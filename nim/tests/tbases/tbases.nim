df40a71e6b223acf0cc81bfa10b209928b88c96d
thindil <thindil@laeran.pl>
2023-03-08 03:58:08 +0000

    tests: added unit test for gainRep procedure
    FossilOrigin-Name: 17703ffe7049c0973d5c71bb602745a007726e48414bc8baed1739c6d3f3166f

--- nim/tests/tbases/tbases.nim
+++ nim/tests/tbases/tbases.nim
@@ -2,7 +2,7 @@ discard """
   exitcode: 0
 """
 
-import ../../src/[bases, careers, factions, game, items]
+import ../../src/[bases, careers, factions, game, items, types]
 
 loadData("../bin/data/game.dat")
 loadItems("../bin/data/items.dat")
@@ -10,3 +10,9 @@ loadCareers("../bin/data/careers.dat")
 loadFactions("../bin/data/factions.dat")
 
 assert generateBaseName("POLEIS").len() > 0
+
+skyBases[1].reputation = ReputationData(level: 1, experience: 1)
+gainRep(1, 1)
+assert skyBases[1].reputation.experience == 2
+gainRep(1, -1)
+assert skyBases[1].reputation.experience == 1
