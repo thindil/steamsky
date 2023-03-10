c9295a57e31f39f553042147855c2c7d1c0b8bd1
thindil <thindil@laeran.pl>
2023-03-10 03:51:18 +0000

    tests: added unit test for countPrice
    FossilOrigin-Name: 2912b811ba4aa441048f753c77736b6b3913d1a664da00c09c8eb74479c2f756

--- nim/tests/tbases/tbases.nim
+++ nim/tests/tbases/tbases.nim
@@ -2,7 +2,7 @@ discard """
   exitcode: 0
 """
 
-import ../../src/[bases, careers, factions, game, items, types]
+import ../../src/[bases, careers, factions, game, items, maps, types]
 
 loadData("../bin/data/game.dat")
 loadItems("../bin/data/items.dat")
@@ -16,3 +16,21 @@ gainRep(1, 1)
 assert skyBases[1].reputation.experience == 2
 gainRep(1, -1)
 assert skyBases[1].reputation.experience == 1
+
+playerShip.skyX = 1
+playerShip.skyY = 1
+playerShip.crew = @[]
+playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural],
+    homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 2, 1, 1,
+    1, 0, 0], order: talk, loyalty: 100, skills: @[SkillInfo(index: 4, level: 4,
+    experience: 0)], attributes: @[MobAttributeRecord(level: 3, experience: 0),
+    MobAttributeRecord(level: 3, experience: 0), MobAttributeRecord(level: 3,
+    experience: 0), MobAttributeRecord(level: 3, experience: 0)], health: 100))
+skyMap[1][1].baseIndex = 1
+var price: Natural = 100
+
+countPrice(price, 0, false)
+assert price > 100
+price = 100
+countPrice(price, 0)
+assert price < 100
