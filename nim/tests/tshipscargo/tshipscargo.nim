84fc6dcff3738dae56c959eb71514f35caf74d62
thindil <thindil@laeran.pl>
2023-03-10 03:53:19 +0000

    tests: added unit test for updateCargo
    FossilOrigin-Name: f398a2b3b530375f5bb12f601b20d0436bcafe541d12aa3ef422464fd67a7a57

--- nim/tests/tshipscargo/tshipscargo.nim
+++ nim/tests/tshipscargo/tshipscargo.nim
@@ -3,7 +3,7 @@ discard """
 """
 
 import std/tables
-import ../../src/[careers, factions, game, items, ships, shipscargo, types]
+import ../../src/[careers, factions, game, items, shipscargo, types]
 
 if itemsList.len == 0:
   loadData("../bin/data/game.dat")
@@ -14,6 +14,12 @@ if itemsList.len == 0:
 playerShip.modules = @[]
 playerShip.modules.add(ModuleData(mType: cargoRoom, protoIndex: 7))
 playerShip.cargo = @[]
-playerShip.cargo.add(InventoryData(protoIndex: 1, amount: 100))
+playerShip.cargo.add(InventoryData(protoIndex: 1, amount: 100, durability: 100))
+
+updateCargo(playerShip, 1, -1)
+assert playerShip.cargo[0].amount == 99
+updateCargo(playerShip, 1, 1)
+assert playerShip.cargo[0].amount == 100
+updateCargo(playerShip, 40, -1)
 
 assert freeCargo(1) > freeCargo(0)
