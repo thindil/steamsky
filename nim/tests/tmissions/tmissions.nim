53e42b3aadbca969a1edb93b5bb11bed2e46f978
thindil <thindil@laeran.pl>
2023-03-19 04:07:03 +0000

    tests: added unit test for deleteMission procedure
    FossilOrigin-Name: 2caf373399c6b129704f394c8e9155c38781529bfc04f43815f9fbe705a3d706

---
+++ nim/tests/tmissions/tmissions.nim
@@ -0,0 +1,33 @@
+discard """
+  exitcode: 0
+"""
+
+import std/tables
+import ../../src/[careers, factions, game, items, maps, missions, types]
+
+if itemsList.len == 0:
+  loadData("../bin/data/game.dat")
+  loadItems("../bin/data/items.dat")
+  loadCareers("../bin/data/careers.dat")
+  loadFactions("../bin/data/factions.dat")
+
+playerShip.skyX = 1
+playerShip.skyY = 1
+playerShip.crew = @[]
+playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural],
+    homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 2, 1, 1,
+    1, 0, 0], order: talk, loyalty: 100, skills: @[SkillInfo(index: 4, level: 4,
+    experience: 0)], attributes: @[MobAttributeRecord(level: 3, experience: 0),
+    MobAttributeRecord(level: 3, experience: 0), MobAttributeRecord(level: 3,
+    experience: 0), MobAttributeRecord(level: 3, experience: 0)]))
+playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural],
+    homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 0, 1, 1,
+    1, 0, 0], order: gunner, loyalty: 100))
+skyMap[1][1].baseIndex = 1
+skyBases[1] = BaseRecord(skyX: 1, skyY: 1)
+
+acceptedMissions = @[]
+acceptedMissions.add(y = MissionData(mType: explore, time: 1, targetX: 1,
+    targetY: 1, reward: 1, startBase: 1, finished: true, multiplier: 0.0, target: 0))
+deleteMission(0, false)
+assert acceptedMissions.len == 0
