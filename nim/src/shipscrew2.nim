112c55c80f2950e86b40f3af8bb524e027e84ae0
thindil <thindil@laeran.pl>
2023-03-21 04:03:44 +0000

    refactor: started work on move deleteMember to Nim
    FossilOrigin-Name: 04eabd769ff85950eff2d93f8ebdc4c172b0b1c1a68796ec1da7b05f92c3daf0

---
+++ nim/src/shipscrew2.nim
@@ -0,0 +1,50 @@
+# Copyright 2023 Bartek thindil Jasicki
+#
+# This file is part of Steam Sky.
+#
+# Steam Sky is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Steam Sky is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
+
+import game, missions, types
+
+proc deleteMember*(memberIndex: Natural; ship: var ShipRecord) =
+  var deleted = false
+  if ship.crew == playerShip.crew:
+    for index, mission in acceptedMissions.pairs:
+      if mission.mType == passenger and mission.data == memberIndex:
+        deleteMission(missionIndex = index)
+        deleted = true
+        break
+    for mission in acceptedMissions.mitems:
+      if mission.mType == passenger and mission.data > memberIndex:
+        mission.data.inc
+    if deleted:
+      return
+  ship.crew.delete(i = memberIndex)
+  for module in ship.modules.mitems:
+    for owner in module.owner.mitems:
+      if owner == memberIndex:
+        owner = 0
+      elif owner > memberIndex:
+        owner.dec
+
+# Temporary code for interfacing with Ada
+
+proc deleteAdaMember(memberIndex, inPlayerShip: cint) {.raises: [], tags: [], exportc.} =
+  try:
+    if inPlayerShip == 1:
+      deleteMember(memberIndex = memberIndex - 1, ship = playerShip)
+    else:
+      deleteMember(memberIndex = memberIndex - 1, ship = npcShip)
+  except KeyError:
+    discard
