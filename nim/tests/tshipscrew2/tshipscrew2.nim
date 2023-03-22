80991f75b73c76acab35566808b8819e172a0641
thindil <thindil@laeran.pl>
2023-03-22 04:01:33 +0000

    tests: added unit test for deleteMember procedure
    FossilOrigin-Name: 437fdc500a4f367d70229b73d8c62d85ff4e61cbf295e2947ad9d89c1c17b344

---
+++ nim/tests/tshipscrew2/tshipscrew2.nim
@@ -0,0 +1,30 @@
+discard """
+  exitcode: 0
+"""
+
+import std/tables
+import ../../src/[careers, factions, game, maps, items, shipscrew2, types]
+
+if itemsList.len == 0:
+  loadData("../bin/data/game.dat")
+  loadItems("../bin/data/items.dat")
+  loadCareers("../bin/data/careers.dat")
+  loadFactions("../bin/data/factions.dat")
+
+playerShip.skyX = 1
+playerShip.skyY = 1
+playerShip.crew = @[]
+playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural],
+    homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 2, 1, 1,
+    1, 0, 0], order: talk, loyalty: 100, skills: @[SkillInfo(index: 4, level: 4,
+    experience: 0)], attributes: @[MobAttributeRecord(level: 3, experience: 0),
+    MobAttributeRecord(level: 3, experience: 0), MobAttributeRecord(level: 3,
+    experience: 0), MobAttributeRecord(level: 3, experience: 0)]))
+playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural],
+    homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 0, 1, 1,
+    1, 0, 0], order: gunner, loyalty: 100))
+skyMap[1][1].baseIndex = 1
+
+var oldLength = playerShip.crew.len
+deleteMember(1, playerShip)
+assert playerShip.crew.len == (oldLength - 1)
