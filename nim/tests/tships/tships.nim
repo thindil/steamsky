e4b406763a45fe6f42b6cc35c25f756f5183395f
thindil <thindil@laeran.pl>
2023-03-24 04:11:16 +0000

    tests: added unit test for damageModule procedure
    FossilOrigin-Name: ea7d6a786f18f34e5625aab0483a7b21632dd07cf2175539755b5a231772477f

--- nim/tests/tships/tships.nim
+++ nim/tests/tships/tships.nim
@@ -3,14 +3,21 @@ discard """
 """
 
 import std/tables
-import ../../src/[careers, factions, game, items, ships]
+import ../../src/[careers, factions, game, items, ships, shipmodules, types]
 
 if itemsList.len == 0:
   loadData("../bin/data/game.dat")
   loadItems("../bin/data/items.dat")
   loadCareers("../bin/data/careers.dat")
   loadFactions("../bin/data/factions.dat")
+  loadModules("../bin/data/shipmodules.dat")
 
 assert getCabinQuality(10) == "Empty room"
 
 assert generateShipName("POLEIS").len() > 0
+
+playerShip.modules = @[]
+playerShip.modules.add(ModuleData(mType: cargoRoom, protoIndex: 7,
+    durability: 100))
+damageModule(playerShip, 0, 10, "during tests")
+assert playerShip.modules[0].durability == 90
