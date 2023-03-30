18716b1d64fd5c3eb4c67dc4bebcf813a14009e5
thindil <thindil@laeran.pl>
2023-03-30 03:10:38 +0000

    tests: added unit test for createShip procedure
    FossilOrigin-Name: be5c44f74d490fe234dd3b0ee5aa74f8f6c4a8dd41b269ec80ba76338fff108c

--- nim/tests/tships/tships.nim
+++ nim/tests/tships/tships.nim
@@ -3,7 +3,7 @@ discard """
 """
 
 import std/tables
-import ../../src/[careers, factions, game, items, ships, shipmodules, types]
+import ../../src/[careers, crafts, factions, game, items, maps, mobs, ships, shipmodules, types]
 
 if itemsList.len == 0:
   loadData("../bin/data/game.dat")
@@ -11,6 +11,9 @@ if itemsList.len == 0:
   loadCareers("../bin/data/careers.dat")
   loadFactions("../bin/data/factions.dat")
   loadModules("../bin/data/shipmodules.dat")
+  loadRecipes("../bin/data/recipes.dat")
+  loadMobs("../bin/data/mobs.dat")
+  loadShips("../bin/data/ships.dat")
 
 assert getCabinQuality(10) == "Empty room"
 
@@ -25,3 +28,11 @@ assert playerShip.modules[0].durability == 90
 discard countCombatValue()
 
 discard countShipWeight(playerShip)
+
+for base in skyBases.mitems:
+  base.owner = "POLEIS"
+for x in MapXRange.low .. MapXRange.high:
+  for y in MapYRange.low .. MapYRange.high:
+    skyMap[x][y].baseIndex = 1
+let newShip = createShip(2, "", 5, 5, fullSpeed)
+assert newShip.name == "Tiny pirates ship"
