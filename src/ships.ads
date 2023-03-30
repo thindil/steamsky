fac07f7daeb1492e0b72ae05d2d6bfa9a91d3297
thindil <thindil@laeran.pl>
2023-03-30 03:32:40 +0000

    refactor: moved getting proto ships to separated function
    FossilOrigin-Name: dec9e490c120fea074adb919f19edd902150f99ae51d9b363989e039211bf68a

--- src/ships.ads
+++ src/ships.ads
@@ -517,4 +517,6 @@ package Ships is
    procedure Set_Ship_In_Nim(Ship: Ship_Record := Player_Ship);
 
    procedure Get_Ship_From_Nim(Ship: in out Ship_Record);
+
+   function Get_Proto_Ship(Proto_Index: Positive) return Proto_Ship_Data;
 end Ships;
