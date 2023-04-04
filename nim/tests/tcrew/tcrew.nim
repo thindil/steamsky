8e02aae0ba42f141d6f0440523d36e79ef969f6e
thindil <thindil@laeran.pl>
2023-04-04 03:03:53 +0000

    tests: added unit test for dailyPayment procedure
    FossilOrigin-Name: b44f3df69e48e5ee8a0bd8202de03a1e49b8ef6309a47fb61aba9f70a4287776

--- nim/tests/tcrew/tcrew.nim
+++ nim/tests/tcrew/tcrew.nim
@@ -3,7 +3,7 @@ discard """
 """
 
 import std/tables
-import ../../src/[careers, crew, factions, game, items]
+import ../../src/[careers, crew, factions, game, items, maps, types]
 
 if itemsList.len == 0:
   loadData("../bin/data/game.dat")
@@ -12,3 +12,19 @@ if itemsList.len == 0:
   loadFactions("../bin/data/factions.dat")
 
 assert generateMemberName('M', "POLEIS").len() > 0
+
+playerShip.skyX = 1
+playerShip.skyY = 1
+playerShip.crew = @[]
+playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural],
+    homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 2, 1, 1,
+    1, 0, 0], order: talk, loyalty: 100, skills: @[SkillInfo(index: 4, level: 4,
+    experience: 0)], attributes: @[MobAttributeRecord(level: 3, experience: 0),
+    MobAttributeRecord(level: 3, experience: 0), MobAttributeRecord(level: 3,
+    experience: 0), MobAttributeRecord(level: 3, experience: 0)]))
+playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural],
+    homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 0, 1, 1,
+    1, 0, 0], order: gunner, loyalty: 100))
+skyMap[1][1].baseIndex = 1
+
+dailyPayment()
