8498445bf6890d9e8d02be0e837e8af83294453b
thindil <thindil@laeran.pl>
2023-04-19 02:56:08 +0000

    refactor: moved getRandomItem to items module
    FossilOrigin-Name: 8502486bbb0f32c74a9171355319bd677d9de207a9c657c9a629937abd2b80bb

--- nim/src/items.nim
+++ nim/src/items.nim
@@ -16,7 +16,7 @@
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
 import std/[strutils, tables, xmlparser, xmltree]
-import config, crewinventory, game, log, messages, shipscargo, shipscrew, types
+import config, crewinventory, game, log, messages, shipscargo, shipscrew, types, utils
 
 var
   weaponsList*: seq[Positive]
@@ -310,6 +310,78 @@ proc findTools*(memberIndex: Natural; itemType: string; order: CrewOrders;
         return -1
   playerShip.crew[memberIndex].equipment[tool] = result
 
+proc getRandomItem*(itemsIndexes: seq[Positive]; equipIndex: EquipmentLocations;
+    highestLevel, weaponSkillLevel: Positive;
+    factionIndex: string): Natural {.sideEffect, raises: [], tags: [].} =
+  ## Get the random index of the item of the selected type
+  ##
+  ## * itemsIndexes     - the list of indexes of the items from which the index will be get
+  ## * equipIndex       - the position of the item in equipment (like weapon, tools, etc)
+  ## * highestLevel     - the highest level of skills for the selected mob
+  ## * weaponSkillLevel - the index of the weapon skill for the selected mob
+  ## * factionIndex     - the index of the faction to which the mob belongs
+  ##
+  ## Returns the random index from the selected itemsIndexes list of 0 if the item
+  ## can't be get
+  var
+    itemIndex, maxIndex: Natural
+    newIndexes: seq[Positive]
+    added: bool
+  if equipIndex > weapon:
+    try:
+      for index in itemsIndexes:
+        added = false
+        for j in 0..<newIndexes.len:
+          if itemsList[index].price < itemsList[newIndexes[j]].price:
+            {.warning[UnsafeSetLen]: off.}
+            newIndexes.insert(item = index, i = j)
+            {.warning[UnsafeSetLen]: on.}
+            added = true
+            break
+        if not added:
+          newIndexes.add(y = index)
+    except KeyError:
+      return 0
+    maxIndex = ((newIndexes.len - 1).float * (highestLevel.float / 100.0) + 1.0).Positive
+    if maxIndex > newIndexes.len - 1:
+      maxIndex = newIndexes.len - 1
+    itemIndex = getRandom(min = 0, max = maxIndex)
+  else:
+    try:
+      for index in itemsIndexes:
+        added = false
+        for j in 0..<newIndexes.len:
+          if itemsList[index].price < itemsList[newIndexes[j]].price and
+              itemsList[index].value[3] == factionsList[
+                  factionIndex].weaponSkill:
+            {.warning[UnsafeSetLen]: off.}
+            newIndexes.insert(item = index, i = j)
+            {.warning[UnsafeSetLen]: on.}
+            added = true
+            break
+        if not added and itemsList[index].value[3] == factionsList[
+            factionIndex].weaponSkill:
+          newIndexes.add(y = index)
+    except KeyError:
+      return 0
+    if newIndexes.len == 0:
+      return 0
+    maxIndex = ((newIndexes.len - 1).float * (weaponSkillLevel.float / 100.0) + 1.0).Positive
+    if maxIndex > newIndexes.len - 1:
+      maxIndex = newIndexes.len - 1
+    try:
+      while true:
+        itemIndex = getRandom(min = 0, max = maxIndex)
+        if itemsList[newIndexes[itemIndex]].value[3] == factionsList[
+            factionIndex].weaponSkill:
+          break
+    except KeyError:
+      return 0
+  for index in itemsIndexes:
+    if index == newIndexes[itemIndex]:
+      return newIndexes[itemIndex]
+  return 0
+
 # Temporary code for interfacing with Ada
 
 type
@@ -391,3 +463,46 @@ proc findAdaTools(memberIndex: cint; itemType: cstring; order,
         toolQuality = toolQuality.Positive).cint + 1
   except KeyError, Exception:
     return 0
+
+proc getAdaRandomItem(items: cstring, equipIndex, highestLevel,
+    weaponSkillLevel: cint; factionIndex: cstring;
+        highestSkill: cint): cint {.sideEffect, raises: [], tags: [], exportc.} =
+  case $items
+  of "weapon":
+    return getRandomItem(itemsIndexes = weaponsList,
+        equipIndex = equipIndex.EquipmentLocations, highestLevel = highestLevel,
+        weaponSkillLevel = weaponSkillLevel, factionIndex = $factionIndex).cint
+  of "shield":
+    return getRandomItem(itemsIndexes = shieldsList,
+        equipIndex = equipIndex.EquipmentLocations, highestLevel = highestLevel,
+        weaponSkillLevel = weaponSkillLevel, factionIndex = $factionIndex).cint
+  of "helmet":
+    return getRandomItem(itemsIndexes = headArmorsList,
+        equipIndex = equipIndex.EquipmentLocations, highestLevel = highestLevel,
+        weaponSkillLevel = weaponSkillLevel, factionIndex = $factionIndex).cint
+  of "torso":
+    return getRandomItem(itemsIndexes = chestArmorsList,
+        equipIndex = equipIndex.EquipmentLocations, highestLevel = highestLevel,
+        weaponSkillLevel = weaponSkillLevel, factionIndex = $factionIndex).cint
+  of "arms":
+    return getRandomItem(itemsIndexes = armsArmorsList,
+        equipIndex = equipIndex.EquipmentLocations, highestLevel = highestLevel,
+        weaponSkillLevel = weaponSkillLevel, factionIndex = $factionIndex).cint
+  of "legs":
+    return getRandomItem(itemsIndexes = legsArmorsList,
+        equipIndex = equipIndex.EquipmentLocations, highestLevel = highestLevel,
+        weaponSkillLevel = weaponSkillLevel, factionIndex = $factionIndex).cint
+  of "tool":
+    var tempToolsList: seq[Positive]
+    for recipe in recipesList.values:
+      if highestSkill == recipe.skill:
+        for index, item in itemsList.pairs:
+          if item.itemType == recipe.tool:
+            tempToolsList.add(y = index)
+        break
+    if tempToolsList.len == 0:
+      return 0
+    return getRandomItem(itemsIndexes = tempToolsList,
+        equipIndex = equipIndex.EquipmentLocations, highestLevel = highestLevel,
+        weaponSkillLevel = weaponSkillLevel, factionIndex = $factionIndex).cint
+
--- nim/src/mobs.nim
+++ nim/src/mobs.nim
@@ -249,78 +249,6 @@ proc loadMobs*(fileName: string) {.sideEffect, raises: [DataLoadingError],
           debugType = everything)
     protoMobsList[mobIndex] = mob
 
-proc getRandomItem*(itemsIndexes: seq[Positive], equipIndex: EquipmentLocations,
-    highestLevel, weaponSkillLevel: Positive,
-    factionIndex: string): Natural {.sideEffect, raises: [], tags: [].} =
-  ## Get the random index of the item of the selected type
-  ##
-  ## * itemsIndexes     - the list of indexes of the items from which the index will be get
-  ## * equipIndex       - the position of the item in equipment (like weapon, tools, etc)
-  ## * highestLevel     - the highest level of skills for the selected mob
-  ## * weaponSkillLevel - the index of the weapon skill for the selected mob
-  ## * factionIndex     - the index of the faction to which the mob belongs
-  ##
-  ## Returns the random index from the selected itemsIndexes list of 0 if the item
-  ## can't be get
-  var
-    itemIndex, maxIndex: Natural
-    newIndexes: seq[Positive]
-    added: bool
-  if equipIndex > weapon:
-    try:
-      for index in itemsIndexes:
-        added = false
-        for j in 0..<newIndexes.len:
-          if itemsList[index].price < itemsList[newIndexes[j]].price:
-            {.warning[UnsafeSetLen]: off.}
-            newIndexes.insert(item = index, i = j)
-            {.warning[UnsafeSetLen]: on.}
-            added = true
-            break
-        if not added:
-          newIndexes.add(y = index)
-    except KeyError:
-      return 0
-    maxIndex = ((newIndexes.len - 1).float * (highestLevel.float / 100.0) + 1.0).Positive
-    if maxIndex > newIndexes.len - 1:
-      maxIndex = newIndexes.len - 1
-    itemIndex = getRandom(min = 0, max = maxIndex)
-  else:
-    try:
-      for index in itemsIndexes:
-        added = false
-        for j in 0..<newIndexes.len:
-          if itemsList[index].price < itemsList[newIndexes[j]].price and
-              itemsList[index].value[3] == factionsList[
-                  factionIndex].weaponSkill:
-            {.warning[UnsafeSetLen]: off.}
-            newIndexes.insert(item = index, i = j)
-            {.warning[UnsafeSetLen]: on.}
-            added = true
-            break
-        if not added and itemsList[index].value[3] == factionsList[
-            factionIndex].weaponSkill:
-          newIndexes.add(y = index)
-    except KeyError:
-      return 0
-    if newIndexes.len == 0:
-      return 0
-    maxIndex = ((newIndexes.len - 1).float * (weaponSkillLevel.float / 100.0) + 1.0).Positive
-    if maxIndex > newIndexes.len - 1:
-      maxIndex = newIndexes.len - 1
-    try:
-      while true:
-        itemIndex = getRandom(min = 0, max = maxIndex)
-        if itemsList[newIndexes[itemIndex]].value[3] == factionsList[
-            factionIndex].weaponSkill:
-          break
-    except KeyError:
-      return 0
-  for index in itemsIndexes:
-    if index == newIndexes[itemIndex]:
-      return newIndexes[itemIndex]
-  return 0
-
 proc generateMob*(mobIndex: Natural, factionIndex: string): MemberData {.sideEffect,
     raises: [KeyError], tags: [].} =
   ## Generate random mob from the selected prototype and the faction.
@@ -454,48 +382,6 @@ proc getAdaMob(index: cint; adaMob: var AdaMobData) {.sideEffect, raises: [
         item.maxAmount.cint]
   for index, item in mob.equipment.pairs:
     adaMob.equipment[index.ord] = item.cint + 1
-
-proc getAdaRandomItem(items: cstring, equipIndex, highestLevel,
-    weaponSkillLevel: cint; factionIndex: cstring;
-        highestSkill: cint): cint {.sideEffect, raises: [], tags: [], exportc.} =
-  case $items
-  of "weapon":
-    return getRandomItem(itemsIndexes = weaponsList,
-        equipIndex = equipIndex.EquipmentLocations, highestLevel = highestLevel,
-        weaponSkillLevel = weaponSkillLevel, factionIndex = $factionIndex).cint
-  of "shield":
-    return getRandomItem(itemsIndexes = shieldsList,
-        equipIndex = equipIndex.EquipmentLocations, highestLevel = highestLevel,
-        weaponSkillLevel = weaponSkillLevel, factionIndex = $factionIndex).cint
-  of "helmet":
-    return getRandomItem(itemsIndexes = headArmorsList,
-        equipIndex = equipIndex.EquipmentLocations, highestLevel = highestLevel,
-        weaponSkillLevel = weaponSkillLevel, factionIndex = $factionIndex).cint
-  of "torso":
-    return getRandomItem(itemsIndexes = chestArmorsList,
-        equipIndex = equipIndex.EquipmentLocations, highestLevel = highestLevel,
-        weaponSkillLevel = weaponSkillLevel, factionIndex = $factionIndex).cint
-  of "arms":
-    return getRandomItem(itemsIndexes = armsArmorsList,
-        equipIndex = equipIndex.EquipmentLocations, highestLevel = highestLevel,
-        weaponSkillLevel = weaponSkillLevel, factionIndex = $factionIndex).cint
-  of "legs":
-    return getRandomItem(itemsIndexes = legsArmorsList,
-        equipIndex = equipIndex.EquipmentLocations, highestLevel = highestLevel,
-        weaponSkillLevel = weaponSkillLevel, factionIndex = $factionIndex).cint
-  of "tool":
-    var tempToolsList: seq[Positive]
-    for recipe in recipesList.values:
-      if highestSkill == recipe.skill:
-        for index, item in itemsList.pairs:
-          if item.itemType == recipe.tool:
-            tempToolsList.add(y = index)
-        break
-    if tempToolsList.len == 0:
-      return 0
-    return getRandomItem(itemsIndexes = tempToolsList,
-        equipIndex = equipIndex.EquipmentLocations, highestLevel = highestLevel,
-        weaponSkillLevel = weaponSkillLevel, factionIndex = $factionIndex).cint
 
 proc adaGenerateMob(mobIndex: cint, factionIndex: cstring;
     adaMember: var AdaMemberData, adaInventory: var array[128,
