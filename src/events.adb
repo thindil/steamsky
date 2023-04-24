0a22e07000e70ff06796cacd95649841cf4c6e0d
thindil <thindil@laeran.pl>
2023-04-24 03:07:23 +0000

    refactor: use Nim code to get player's ships
    FossilOrigin-Name: e9285e9809b931f43f3c5c314b0a1a075fbafd69bc32958a13a026a140fb6ef7

--- src/events.adb
+++ src/events.adb
@@ -517,17 +517,20 @@ package body Events is
    -- SOURCE
    procedure Get_Player_Ships
      (Player_Ships: in out Positive_Container.Vector) is
-   -- ****
-      Faction: Faction_Record; --## rule line off IMPROPER_INITIALIZATION
+     -- ****
+      type Nim_Ships_Array is array(0 .. 29) of Natural;
+      Nim_Ships: Nim_Ships_Array;
+      procedure Get_Ada_Players_Ships(P_Ships: out Nim_Ships_Array) with
+         Import => True,
+         Convention => C,
+         External_Name => "getAdaPlayerShips";
    begin
-      Get_Faction_Loop :
-      for I in 1 .. Get_Factions_Amount loop
-         Faction := Get_Faction(Number => I);
-         Get_Career_Loop :
-         for Career of Faction.Careers loop
-            Player_Ships.Append(New_Item => Career.Ship_Index);
-         end loop Get_Career_Loop;
-      end loop Get_Faction_Loop;
+      Get_Ada_Players_Ships(P_Ships => Nim_Ships);
+      Convert_Ships_Loop :
+      for Ship of Nim_Ships loop
+         exit Convert_Ships_Loop when Ship = 0;
+         Player_Ships.Append(New_Item => Ship);
+      end loop Convert_Ships_Loop;
    end Get_Player_Ships;
 
    procedure Generate_Traders is
