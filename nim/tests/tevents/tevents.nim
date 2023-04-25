9130fd40fc4d9bae17f1f69d94b602616c103878
thindil <thindil@laeran.pl>
2023-04-25 03:25:16 +0000

    tests: added unit test for generateEnemies procedure
    FossilOrigin-Name: a5e1ca7e597019473709305c0463ecef4fffe24c9fb48eabed1f6f169e390a90

---
+++ nim/tests/tevents/tevents.nim
@@ -0,0 +1,42 @@
+discard """
+  exitcode: 0
+"""
+
+import std/tables
+import ../../src/[careers, crafts, events, factions, game, items, maps, mobs,
+    ships, shipmodules, types]
+
+if factionsList.len == 0:
+  loadData("../bin/data/game.dat")
+  loadItems("../bin/data/items.dat")
+  loadCareers("../bin/data/careers.dat")
+  loadFactions("../bin/data/factions.dat")
+  loadModules("../bin/data/shipmodules.dat")
+  loadRecipes("../bin/data/recipes.dat")
+  loadMobs("../bin/data/mobs.dat")
+  loadShips("../bin/data/ships.dat")
+
+playerShip.skyX = 1
+playerShip.skyY = 1
+playerShip.crew = @[]
+playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural],
+    homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 2, 1, 1,
+    1, 0, 0], order: talk, loyalty: 100, skills: @[SkillInfo(index: 4, level: 4,
+    experience: 0)], attributes: @[MobAttributeRecord(level: 3, experience: 0),
+    MobAttributeRecord(level: 3, experience: 0), MobAttributeRecord(level: 3,
+    experience: 0), MobAttributeRecord(level: 3, experience: 0)]))
+playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural],
+    homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 0, 1, 1,
+    1, 0, 0], order: gunner, loyalty: 100))
+playerShip.modules = @[]
+playerShip.modules.add(ModuleData(mType: ModuleType2.armor, protoIndex: 2,
+    durability: 100))
+playerShip.modules.add(ModuleData(mType: ModuleType2.turret, protoIndex: 8,
+    durability: 100))
+playerShip.modules.add(ModuleData(mType: ModuleType2.gun, protoIndex: 9,
+    durability: 100, damage: 100))
+skyMap[1][1].baseIndex = 1
+
+var enemies: seq[Positive] = @[]
+generateEnemies(enemies)
+assert enemies.len > 0
