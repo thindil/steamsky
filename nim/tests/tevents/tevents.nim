8261472871b9d7eb264bdb6aab8f3ac9d3a5be74
thindil <thindil@laeran.pl>
2023-04-26 03:01:19 +0000

    tests: fixed unit test for events module
    FossilOrigin-Name: 3b883d36f10559e693697f616e8c58ed119cc066fa26dd4639e75c7112c08cc3

--- nim/tests/tevents/tevents.nim
+++ nim/tests/tevents/tevents.nim
@@ -12,8 +12,11 @@ if factionsList.len == 0:
   loadCareers("../bin/data/careers.dat")
   loadFactions("../bin/data/factions.dat")
   loadModules("../bin/data/shipmodules.dat")
+if recipesList.len == 0:
   loadRecipes("../bin/data/recipes.dat")
+if protoMobsList.len == 0:
   loadMobs("../bin/data/mobs.dat")
+if protoShipsList.len == 0:
   loadShips("../bin/data/ships.dat")
 
 playerShip.skyX = 1
@@ -29,11 +32,11 @@ playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural],
     homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 0, 1, 1,
     1, 0, 0], order: gunner, loyalty: 100))
 playerShip.modules = @[]
-playerShip.modules.add(ModuleData(mType: ModuleType2.armor, protoIndex: 2,
+playerShip.modules.add(ModuleData(mType: ModuleType2.armor, protoIndex: 57,
     durability: 100))
-playerShip.modules.add(ModuleData(mType: ModuleType2.turret, protoIndex: 8,
+playerShip.modules.add(ModuleData(mType: ModuleType2.turret, protoIndex: 86,
     durability: 100))
-playerShip.modules.add(ModuleData(mType: ModuleType2.gun, protoIndex: 9,
+playerShip.modules.add(ModuleData(mType: ModuleType2.gun, protoIndex: 160,
     durability: 100, damage: 100))
 skyMap[1][1].baseIndex = 1
 
