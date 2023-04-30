c2a5f0a0b1a9e0dbb7b5a2a6f3b3577d38b4cd5b
thindil <thindil@laeran.pl>
2023-04-30 03:01:10 +0000

    tests: updated unit tests
    FossilOrigin-Name: bdf17a9e158059c3f7497e4f21a67a9fa3b3e5471414c916bf0d7a0d208483f1

--- tests/bases-cargo-test_data-tests.adb
+++ tests/bases-cargo-test_data-tests.adb
@@ -31,38 +31,6 @@ package body Bases.Cargo.Test_Data.Tests is
 --  begin read only
 --  end read only
 --  begin read only
-   procedure Wrap_Test_Generate_Cargo_bedd31_021eea is
-   begin
-      GNATtest_Generated.GNATtest_Standard.Bases.Cargo.Generate_Cargo;
-   end Wrap_Test_Generate_Cargo_bedd31_021eea;
---  end read only
-
---  begin read only
-   procedure Test_Generate_Cargo_test_generatecargo(Gnattest_T: in out Test);
-   procedure Test_Generate_Cargo_bedd31_021eea(Gnattest_T: in out Test) renames
-     Test_Generate_Cargo_test_generatecargo;
---  id:2.2/bedd31198f3fb6a9/Generate_Cargo/1/0/test_generatecargo/
-   procedure Test_Generate_Cargo_test_generatecargo(Gnattest_T: in out Test) is
-      procedure Generate_Cargo renames Wrap_Test_Generate_Cargo_bedd31_021eea;
---  end read only
-
-      pragma Unreferenced(Gnattest_T);
-      BaseIndex: constant Positive :=
-        Sky_Map(Player_Ship.Sky_X, Player_Ship.Sky_Y).Base_Index;
-
-   begin
-
-      BaseCargo_Container.Clear(Container => Sky_Bases(BaseIndex).Cargo);
-      Generate_Cargo;
-      Assert
-        (BaseCargo_Container.Length(Sky_Bases(BaseIndex).Cargo) > 0,
-         "Failed to generate base cargo.");
-
---  begin read only
-   end Test_Generate_Cargo_test_generatecargo;
---  end read only
-
---  begin read only
    procedure Wrap_Test_Update_Base_Cargo_a5e235_1e1787
      (Proto_Index: Natural := 0; Amount: Integer;
       Durability: Items_Durability := Default_Item_Durability;
