e6bd1e004479bcba43a49118c864f0bcef7fb657
thindil <thindil@laeran.pl>
2023-05-01 03:24:58 +0000

    refactor: started work on move updateEvents to Nim
    FossilOrigin-Name: 6c023b81c253e364619a48ec8b6568e7e78d570187fdcddcf6eda215553080f3

--- nim/src/events.nim
+++ nim/src/events.nim
@@ -16,7 +16,7 @@
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
 import std/[strutils, tables]
-import factions, game, ships2, types, utils
+import factions, game, maps, ships2, types, utils
 
 proc getPlayerShips(playerShips: var seq[Positive]) {.sideEffect, raises: [],
     tags: [].} =
@@ -53,6 +53,30 @@ proc generateEnemies*(enemies: var seq[Positive]; owner: string = "Any";
         withTraders or tradersName notin ship.name):
       enemies.add(y = index)
 
+proc updateEvents*(minutes: Positive) =
+  let eventsAmount = eventsList.len
+  if eventsAmount == 0:
+    return
+  for key in eventsList.keys:
+    let newTime = eventsList[key].time - minutes
+    if newTime < 1:
+      if eventsList[key].eType in {disease, attackOnBase} and getRandom(min = 1,
+          max = 100) < 10:
+        let baseIndex = skyMap[eventsList[key].skyX][eventsList[
+            key].skyY].baseIndex
+        var populationLost = getRandom(min = 1, max = 10)
+        if populationLost > skyBases[baseIndex].population:
+          populationLost = skyBases[baseIndex].population
+          skyBases[baseIndex].reputation = ReputationData(level: 0, experience: 0)
+        skyBases[baseIndex].population = skyBases[baseIndex].population - populationLost
+      skyMap[eventsList[key].skyX][eventsList[key].skyY].eventIndex = 0
+      {.warning[UnsafeDefault]: off.}
+      eventsList.del(key)
+      {.warning[UnsafeDefault]: on.}
+  if eventsAmount < eventsList.len:
+    for key in eventsList.keys:
+      skyMap[eventsList[key].skyX][eventsList[key].skyY].eventIndex = key
+
 # Temporary code for interfacing with Ada
 
 proc getAdaEvent(index, x, y, time, eType, data: cint) {.raises: [], tags: [], exportc.} =
@@ -86,3 +110,9 @@ proc generateAdaEnemies(enemies: var array[300, cint]; owner: cstring;
     return
   for index, ship in nimShips.pairs:
     enemies[index] = ship.cint
+
+proc updateAdaEvents(minutes: cint) {.raises: [], tags: [], exportc.} =
+  try:
+    updateEvents(minutes = minutes)
+  except KeyError:
+    discard
