251f59ccd9e9a821f31428f978777204716e02f5
thindil <thindil@laeran.pl>
2023-05-04 02:56:31 +0000

    refactor: added variable traderCargo to Nim
    FossilOrigin-Name: a8baf35ca1b67664454c72aa43cc3e05e96404e3cccf2205c63653b02d929159

--- nim/src/trades.nim
+++ nim/src/trades.nim
@@ -15,6 +15,10 @@
 # You should have received a copy of the GNU General Public License
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
+import types
+
 type
   TradeNoFreeCargoError* = object of CatchableError
     ## Used to mark problems with free cargo space in the ship
+
+var traderCargo*: seq[BaseCargo]
