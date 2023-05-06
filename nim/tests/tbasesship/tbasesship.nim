195481399d3dfccc59330d73399a212aa71c89f3
thindil <thindil@laeran.pl>
2023-05-06 03:18:08 +0000

    tests: added unit test for payForDock procedure
    FossilOrigin-Name: 75b63b0cc966bc7efc65b8bb2800cd1303c41517c30a715440d7ac612ef7ef2a

---
+++ nim/tests/tbasesship/tbasesship.nim
@@ -0,0 +1,39 @@
+discard """
+  exitcode: 0
+"""
+
+import std/tables
+import ../../src/[basesship, basestypes, careers, factions, game, items, maps, types]
+
+if basesTypesList.len == 0:
+  loadData("../bin/data/game.dat")
+  loadItems("../bin/data/items.dat")
+  loadCareers("../bin/data/careers.dat")
+  loadFactions("../bin/data/factions.dat")
+  loadBasesTypes("../bin/data/bases.dat")
+
+skyBases[1].reputation = ReputationData(level: 1, experience: 1)
+playerShip.skyX = 1
+playerShip.skyY = 1
+playerShip.crew = @[]
+playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural],
+    homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 2, 1, 1,
+    1, 0, 0], order: talk, loyalty: 100, skills: @[SkillInfo(index: 4, level: 4,
+    experience: 0)], attributes: @[MobAttributeRecord(level: 3, experience: 0),
+    MobAttributeRecord(level: 3, experience: 0), MobAttributeRecord(level: 3,
+    experience: 0), MobAttributeRecord(level: 3, experience: 0)], health: 100))
+playerShip.modules = @[]
+playerShip.modules.add(ModuleData(mType: ModuleType2.hull, protoIndex: 1,
+    durability: 100, maxModules: 10))
+playerShip.cargo = @[]
+playerShip.cargo.add(InventoryData(protoIndex: 1, amount: 100, durability: 100))
+playerShip.speed = docked
+skyMap[1][1].baseIndex = 1
+skyBases[1].population = 100
+skyBases[1].baseType = "1"
+skyBases[1].owner = "POLEIS"
+gameDate = DateRecord(year: 1600, month: 1, day: 1, hour: 8, minutes: 0)
+
+payForDock()
+echo playerShip.cargo[0].amount
+assert playerShip.cargo[0].amount == 90
