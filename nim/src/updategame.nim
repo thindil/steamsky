981797fd6832163edf8f1268e4d321e46481cf19
thindil <thindil@laeran.pl>
2023-05-07 02:56:47 +0000

    refactor: continue work on move updateGame procedure to Nim
    FossilOrigin-Name: 1d1188bc1028e5cc02dc87b8891fd79da70cc6fd6c746e1a8d2d1cfeec21af7f

--- nim/src/updategame.nim
+++ nim/src/updategame.nim
@@ -15,10 +15,11 @@
 # You should have received a copy of the GNU General Public License
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
-import game, shipscrew, types
+import basesship, config, crew, game, shipscrew, types
 
 proc updateGame*(minutes: Positive; inCombat: bool = false) =
-  var needCleaning = false
+  var needCleaning, needSaveGame = false
+
   proc updateDay() =
     gameDate.day.inc
     for module in playerShip.modules.mitems:
@@ -27,3 +28,26 @@ proc updateGame*(minutes: Positive; inCombat: bool = false) =
         needCleaning = true
     if needCleaning:
       updateOrders(ship = playerShip)
+    if playerShip.speed == docked:
+      payForDock()
+    dailyPayment()
+    if $gameSettings.autoSave == $daily:
+      needSaveGame = true
+
+  var tiredPoints = 0
+  for i in 1 .. minutes:
+    if gameDate.minutes + i mod 15 == 0:
+      tiredPoints.inc
+  let addedMinutes = minutes mod 60
+  gameDate.minutes = gameDate.minutes + addedMinutes
+  if gameDate.minutes > 59:
+    gameDate.minutes = gameDate.minutes - 60
+    gameDate.hour.inc
+  var addedHours = (minutes / 60).int
+  while addedHours > 23:
+    addedHours = addedHours - 24
+    updateDay()
+  gameDate.hour = gameDate.hour + addedHours
+  while gameDate.hour > 23:
+    gameDate.hour = gameDate.hour - 24
+    updateDay()
