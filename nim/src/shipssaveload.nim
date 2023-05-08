eb9c5e31b1865e7c1a96b1339dd620640bed85eb
thindil <thindil@laeran.pl>
2023-05-08 03:50:17 +0000

    refactor: continue work on move savePlayerShip to Nim
    FossilOrigin-Name: 4f2b9688b1060dcd75db9020d2ca2365be3c41489fb2c3f3a414a1a106225505

--- nim/src/shipssaveload.nim
+++ nim/src/shipssaveload.nim
@@ -16,7 +16,7 @@
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
 import std/xmltree
-import game
+import game, types
 
 proc savePlayerShip*(saveData: var XmlNode) =
   var shipTree = newXmlTree("playership", [], {"name": playerShip.name,
@@ -26,4 +26,14 @@ proc savePlayerShip*(saveData: var XmlNode) =
       "destinationy": $playerShip.destinationY,
       "repairpriority": $playerShip.repairModule,
       "homebase": $playerShip.homeBase}.toXmlAttributes)
+  for module in playerShip.modules:
+    var attrs: seq[tuple[key, val: string]] = @[("name", module.name), ("index",
+        $module.protoIndex), ("weight", $module.weight), ("durability",
+        $module.durability), ("maxdurability", $module.maxDurability)]
+    if module.upgradeProgress > 0:
+      attrs.add(("ugpradeprogress", $module.upgradeProgress))
+    if module.upgradeAction != none:
+      attrs.add(("upgradeaction", $module.upgradeAction))
+    var moduleTree = newXmlTree("module", [], attrs.toXmlAttributes)
+    shipTree.add(moduleTree)
   saveData.add(shipTree)
