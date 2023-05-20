0f0099737548ac5bf5d3644251dd2fec6b1c8e7b
thindil <thindil@laeran.pl>
2023-05-20 02:58:00 +0000

    refactor: continue work on move updateGame procedure to Nim
    FossilOrigin-Name: 0a2dd459db449d4b9939d53dc21d377d024bc7f72004ebbf3bc36aab2d42e19f

--- nim/src/updategame.nim
+++ nim/src/updategame.nim
@@ -15,7 +15,9 @@
 # You should have received a copy of the GNU General Public License
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
-import basesship, config, crew, game, shipscrew, types
+import bases, basescargo, basesship, config, crafts, crew, events, game,
+    gamesaveload, goals, maps, messages, missions, shipscrew, shipsrepairs,
+    shipsupgrade, statistics, types
 
 proc updateGame*(minutes: Positive; inCombat: bool = false) =
   var needCleaning, needSaveGame = false
@@ -51,3 +53,47 @@ proc updateGame*(minutes: Positive; inCombat: bool = false) =
   while gameDate.hour > 23:
     gameDate.hour = gameDate.hour - 24
     updateDay()
+  if needSaveGame:
+    saveGame()
+  if gameDate.month > 12:
+    gameDate.month = 1
+    gameDate.year.inc
+    if $gameSettings.autoSave == $yearly:
+      saveGame()
+  updateCrew(minutes = minutes, tiredPoints = tiredPoints, inCombat = inCombat)
+  repairShip(minutes = minutes)
+  manufacturing(minutes = minutes)
+  upgradeShip(minutes = minutes)
+  let baseIndex = skyMap[playerShip.skyX][playerShip.skyY].baseIndex
+  if baseIndex > 0:
+    if skyBases[baseIndex].visited.year == 0:
+      gameStats.basesVisited.inc
+      gameStats.points.inc
+      updateGoal(goalType = visit, targetIndex = skyBases[baseIndex].owner)
+    skyBases[baseIndex].visited = gameDate
+    if not skyBases[baseIndex].known:
+      skyBases[baseIndex].known = true
+      addMessage(message = "You discovered base " & skyBases[baseIndex].name &
+          ".", mType = otherMessage)
+    updatePopulation()
+    generateRecruits()
+    generateMissions()
+    generateCargo()
+    updatePrices()
+    updateOrders(ship = playerShip)
+  if not skyMap[playerShip.skyX][playerShip.skyY].visited:
+    gameStats.mapVisited.inc
+    gameStats.points.inc
+    updateGoal(goalType = discover, targetIndex = "")
+    skyMap[playerShip.skyX][playerShip.skyY].visited = true
+  updateEvents(minutes = minutes)
+  updateMissions(minutes = minutes)
+
+# Temporary code for interfacing with Ada
+
+proc updateAdaGame(minutes, inCombat: cint) {.raises: [], tags: [WriteIOEffect,
+    RootEffect], exportc.} =
+  try:
+    updateGame(minutes = minutes, inCombat = inCombat == 1)
+  except ValueError, IOError, Exception:
+    discard
