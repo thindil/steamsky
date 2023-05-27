76a388323e63d69588ef9ebc9cf301750b83bb7c
thindil <thindil@laeran.pl>
2023-05-27 03:00:44 +0000

    tests: updated unit tests
    FossilOrigin-Name: 4fe43c25ca8696c8e10f3878ae45b7e9a3bfaf38f80bc497f579d2cb8ca13812

---
+++ tests/crew-member_data_test_data-member_data_tests.ads
@@ -0,0 +1,15 @@
+--  This package has been generated automatically by GNATtest.
+--  Do not edit any part of it, see GNATtest documentation for more details.
+
+--  begin read only
+with GNATtest_Generated;
+
+package Crew.Member_Data_Test_Data.Member_Data_Tests is
+
+   type Test_Member_Data is new GNATtest_Generated.GNATtest_Standard.Crew
+     .Member_Data_Test_Data
+     .Test_Member_Data with
+   null record;
+
+end Crew.Member_Data_Test_Data.Member_Data_Tests;
+--  end read only
