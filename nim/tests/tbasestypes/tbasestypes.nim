57bc97122a7278f0d18841958764785b8c1411b3
thindil <thindil@laeran.pl>
2023-05-31 03:13:01 +0000

    tests: added failure messages to tests
    FossilOrigin-Name: f424146daef452c913319e4499ececf6df6597ec969783e9621d79e4b9ce7313

--- nim/tests/tbasestypes/tbasestypes.nim
+++ nim/tests/tbasestypes/tbasestypes.nim
@@ -11,8 +11,8 @@ if itemsList.len == 0:
 if basesTypesList.len == 0:
   loadBasesTypes("../bin/data/bases.dat")
 
-assert getPrice("0", 1) == 0
-assert getPrice("1", 2) > 0
+assert getPrice("0", 1) == 0, "Failed to get the price of a non-buyable item."
+assert getPrice("1", 2) > 0, "Failed to get the price of a buyable item."
 
-assert not isBuyable("0", 1)
-assert isBuyable("1", 2)
+assert not isBuyable("0", 1), "Failed to check if an item is non-buyable."
+assert isBuyable("1", 2), "Failed to check if an item is buyable."
