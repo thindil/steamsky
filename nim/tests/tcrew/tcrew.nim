4737e6f348960b05a79c1f0f382d83c142329100
thindil <thindil@laeran.pl>
2023-06-02 03:13:44 +0000

    tests: added failure messages to tests
    FossilOrigin-Name: e1f39e2c97f65c221629c492d1cee9c2ab9214868d8e6c2d485deaa9bb32e156

--- nim/tests/tcrew/tcrew.nim
+++ nim/tests/tcrew/tcrew.nim
@@ -31,25 +31,25 @@ skyMap[1][1].baseIndex = 1
 dailyPayment()
 
 gameSettings.showNumbers = 0
-assert getAttributeLevelName(3) == "Very low"
-assert getAttributeLevelName(12) == "Below average"
-assert getAttributeLevelName(48) == "Very high"
+assert getAttributeLevelName(3) == "Very low", "Failed to get attribute level name for value 3."
+assert getAttributeLevelName(12) == "Below average", "Failed to get attribute level name for value 12."
+assert getAttributeLevelName(48) == "Very high", "Failed to get attribute level name for value 48."
 gameSettings.showNumbers = 1
-assert getAttributeLevelName(3) == "3"
-assert getAttributeLevelName(12) == "12"
-assert getAttributeLevelName(48) == "48"
+assert getAttributeLevelName(3) == "3", "Failed to get attribute level number for value 3."
+assert getAttributeLevelName(12) == "12", "Failed to get attribute level number for value 12."
+assert getAttributeLevelName(48) == "48", "Failed to get attribute level number for value 48."
 
 gameSettings.showNumbers = 0
-assert getSkillLevelName(9) == "Beginner"
-assert getSkillLevelName(54) == "Respected"
-assert getSkillLevelName(92) == "Legendary"
+assert getSkillLevelName(9) == "Beginner", "Failed to get skill level name for value 9."
+assert getSkillLevelName(54) == "Respected", "Failed to get skill level name for value 54."
+assert getSkillLevelName(92) == "Legendary", "Failed to get skill level name for value 92."
 gameSettings.showNumbers = 1
-assert getSkillLevelName(9) == "9"
-assert getSkillLevelName(54) == "54"
-assert getSkillLevelName(92) == "92"
+assert getSkillLevelName(9) == "9", "Failed to get skill level number for value 9."
+assert getSkillLevelName(54) == "54", "Failed to get skill level number for value 54."
+assert getSkillLevelName(92) == "92", "Failed to get skill level number for value 92."
 
-assert findCabin(0) == 0
-assert findCabin(100) == -1
+assert findCabin(0) == 0, "Failed to find a cabin of the player."
+assert findCabin(100) == -1, "Failed to not find a cabin for a non-existing crew member."
 
 updateCrew(1, 1)
 playerShip.crew[0].health = 0
