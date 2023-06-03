ee2aa76d841c812c3e1eacc4f258f5a7e64fd80a
thindil <thindil@laeran.pl>
2023-06-03 03:01:33 +0000

    tests: added failure messages to tests
    FossilOrigin-Name: 9e4d42ba93f7f311f63bc5437b636029f3171d34e8c2223a4f21fb2bb97a59af

--- nim/tests/tcrewinventory/tcrewinventory.nim
+++ nim/tests/tcrewinventory/tcrewinventory.nim
@@ -21,21 +21,21 @@ playerShip.crew.add(member)
 
 discard freeInventory(0, 0)
 
-assert not itemIsUsed(0, 0)
-assert itemIsUsed(0, 1)
+assert not itemIsUsed(0, 0), "Failed to check if an item is not used by the player."
+assert itemIsUsed(0, 1), "Failed to check if an item is used by the player"
 
 takeOffItem(0, 1)
-assert not itemIsUsed(0, 1)
+assert not itemIsUsed(0, 1), "Failed to take off an item from the player."
 
 updateInventory(0, 1, 1, ship = playerShip)
-assert playerShip.crew[0].inventory[0].amount == 2
+assert playerShip.crew[0].inventory[0].amount == 2, "Failed to add an item to the player's inventory."
 updateInventory(0, -1, 1, ship = playerShip)
-assert playerShip.crew[0].inventory[0].amount == 1
+assert playerShip.crew[0].inventory[0].amount == 1, "Failed to remove an item from the player's inventory."
 try:
   updateInventory(0, 10_000, 1, ship = playerShip)
 except CrewNoSpaceError:
   discard
-assert playerShip.crew[0].inventory[0].amount == 1
+assert playerShip.crew[0].inventory[0].amount == 1, "Failed to add too much items to the player's inventory."
 
 for i in 1..100:
   if playerShip.crew[0].inventory.len == 0:
@@ -48,9 +48,9 @@ block:
       durability: defaultItemDurability, price: 0))
   inventory.add(InventoryData(protoIndex: 67, amount: 1, name: "",
       durability: defaultItemDurability, price: 0))
-  assert findItem(inventory, 67) == 1
+  assert findItem(inventory, 67) == 1, "Failed to find an item in an inventory."
   assert findItem(inventory = inventory, itemType = "Weapon") == 0
-  assert findItem(inventory, 500) == -1
+  assert findItem(inventory, 500) == -1, "Failed to not find an item in an inventory"
   assert findItem(inventory = inventory, itemType = "asdasdas") == -1
 
-assert getTrainingToolQuality(0, 1) == 100
+assert getTrainingToolQuality(0, 1) == 100, "Failed to get an tool's quality required by a skill."
