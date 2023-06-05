82feaea32d88e403fed9490f52accb2e1ded50f4
thindil <thindil@laeran.pl>
2023-06-05 03:12:43 +0000

    tests: added failure messages to tests
    FossilOrigin-Name: e4a413aaf78900cf3e572f9299ac0567a53a0f2fcd9c8aafe42113d4d837c668

--- nim/tests/tfactions/tfactions.nim
+++ nim/tests/tfactions/tfactions.nim
@@ -11,12 +11,12 @@ if itemsList.len == 0:
   loadCareers("../bin/data/careers.dat")
   loadFactions("../bin/data/factions.dat")
 
-assert getReputation("POLEIS", "POLEIS") == 0
-assert getReputation("POLEIS", "PIRATES") == -10
+assert getReputation("POLEIS", "POLEIS") == 0, "Failed to get reputation for the same faction."
+assert getReputation("POLEIS", "PIRATES") == -10, "Failed to get reputation for enemy factions."
 
-assert isFriendly("POLEIS", "INDEPENDENT")
-assert not isFriendly("POLEIS", "PIRATES")
+assert isFriendly("POLEIS", "INDEPENDENT"), "Failed to check if friendly factions are friendly."
+assert not isFriendly("POLEIS", "PIRATES"), "Failed to check if enemies factions are unfriendly."
 
 let factionIndex = getRandomFaction()
-assert factionIndex.len > 0
-assert factionIndex in factionsList
+assert factionIndex.len > 0, "Failed to get random faction index."
+assert factionIndex in factionsList, "Failed to get existing random faction index."
