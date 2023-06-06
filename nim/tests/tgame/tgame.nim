ea75745cb03fa158d55148780fd62f52739582d3
thindil <thindil@laeran.pl>
2023-06-06 03:02:59 +0000

    tests: added failure messages to tests
    FossilOrigin-Name: a35a97410890056b008a567bfdce912b4db95068e6dfa677809535639eb21746

--- nim/tests/tgame/tgame.nim
+++ nim/tests/tgame/tgame.nim
@@ -6,5 +6,5 @@ import ../../src/[game]
 
 loadData("../bin/data/game.dat")
 
-assert findSkillIndex(skillName = "Piloting") == 1
-assert findSkillIndex(skillName = "sdfwerwerwe") == 0
+assert findSkillIndex(skillName = "Piloting") == 1, "Failed to find the index of the skill."
+assert findSkillIndex(skillName = "sdfwerwerwe") == 0, "Failed to not find the index of the non-existing skill."
