d37e711337d3d01d152bfa6b14b4f18038c76a5f
thindil <thindil@laeran.pl>
2023-06-07 03:16:31 +0000

    tests: updated unit tests
    FossilOrigin-Name: 096086cb5d3f97fe7d8733baa166a52fef8e93104ed6355dfc09cf98c9f521aa

--- tests/bases-test_data-tests.ads
+++ tests/bases-test_data-tests.ads
@@ -11,10 +11,7 @@ package Bases.Test_Data.Tests is
    null record;
 
    procedure Test_Ask_For_Bases_023c5f_f3f6c6(Gnattest_T: in out Test);
-   --  bases.ads:256:4:Ask_For_Bases:Test_AskForBases
-
-   procedure Test_Ask_For_Events_4f17f6_3e359b(Gnattest_T: in out Test);
-   --  bases.ads:264:4:Ask_For_Events:Test_AskForEvents
+   --  bases.ads:253:4:Ask_For_Bases:Test_AskForBases
 
 end Bases.Test_Data.Tests;
 --  end read only
