d5766281a20b713c070f853f4a1a96a1fedb759d
thindil <thindil@laeran.pl>
2023-06-07 03:16:01 +0000

    tests: added unit test for askForEvents procedure
    FossilOrigin-Name: defbff0171c2719b59b97eb581930a78fa7accecff80b0c9136eb9af75e2abf9

--- nim/tests/tbases2/tbases2.nim
+++ nim/tests/tbases2/tbases2.nim
@@ -23,8 +23,8 @@ if protoShipsList.len == 0:
 
 assert generateBaseName("POLEIS").len() > 0, "Failed to generate a base's name."
 
-playerShip.skyX = 1
-playerShip.skyY = 1
+playerShip.skyX = 200
+playerShip.skyY = 200
 playerShip.crew = @[]
 playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural],
     homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 2, 1, 1,
@@ -38,13 +38,19 @@ playerShip.modules.add(ModuleData(mType: ModuleType2.armor, protoIndex: 57,
 playerShip.modules.add(ModuleData(mType: ModuleType2.turret, protoIndex: 86,
     durability: 100))
 playerShip.modules.add(ModuleData(mType: ModuleType2.gun, protoIndex: 160,
-    durability: 100, damage: 100))
-skyMap[1][1].baseIndex = 1
+    durability: 100, damage: 100, owner: @[-1]))
 skyBases[1].population = 100
 skyBases[1].baseType = "1"
 skyBases[1].owner = "POLEIS"
 skyBases[1].askedForEvents = DateRecord(year: 0, month: 0, day: 0, hour: 0, minutes: 0)
 gameDate = DateRecord(year: 1600, month: 1, day: 1, hour: 8, minutes: 0)
+skyBases[1].visited = gameDate
+skyBases[1].known = true
+for x in 1 .. 1024:
+  for y in 1 .. 1024:
+    skyMap[x][y].eventIndex = -1
+    skyMap[x][y].baseIndex = 0
+skyMap[playerShip.skyX][playerShip.skyY].baseIndex = 1
 for index, base in skyBases.mpairs:
   if index == 1:
     continue
@@ -52,10 +58,8 @@ for index, base in skyBases.mpairs:
   base.skyY = getRandom(1, 1_024)
   base.baseType = $getRandom(0, 4)
   base.owner = "POLEIS"
+  base.population = getRandom(100, 400)
   skyMap[base.skyX][base.skyY].baseIndex = index
-for x in 1 .. 1024:
-  for y in 1 .. 1024:
-    skyMap[x][y].eventIndex = -1
 
-#askForEvents()
-#assert eventsList.len > 0, "Failed to generate new events."
+askForEvents()
+assert eventsList.len > 0, "Failed to generate new events."
