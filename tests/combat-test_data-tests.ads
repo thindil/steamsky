44125e29ee23ac56df5b57b652c8875a7924e44c
thindil <thindil@laeran.pl>
2023-06-09 03:04:24 +0000

    tests: updated unit tests
    FossilOrigin-Name: ac514797463da8a4fe0cc0053ef0e402d108a614a6ddf84c861b0ccf295e1be0

--- tests/combat-test_data-tests.ads
+++ tests/combat-test_data-tests.ads
@@ -11,10 +11,10 @@ package Combat.Test_Data.Tests is
    null record;
 
    procedure Test_Start_Combat_68a6d6_4a322d(Gnattest_T: in out Test);
-   --  combat.ads:155:4:Start_Combat:Test_StartCombat
+   --  combat.ads:169:4:Start_Combat:Test_StartCombat
 
    procedure Test_Combat_Turn_77b950_e12d30(Gnattest_T: in out Test);
-   --  combat.ads:164:4:Combat_Turn:Test_CombatTurn
+   --  combat.ads:178:4:Combat_Turn:Test_CombatTurn
 
 end Combat.Test_Data.Tests;
 --  end read only
