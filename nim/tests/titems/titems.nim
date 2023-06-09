a5fd59a0ad94e5b1dbf1b43e12032a4a0e1d819f
thindil <thindil@laeran.pl>
2023-06-09 03:04:42 +0000

    tests: added failure messages to tests
    FossilOrigin-Name: 4c33c0581691ac65badfc6d41f84be1c0755bb303cfa9bc1d032bf0d6de3cca6

--- nim/tests/titems/titems.nim
+++ nim/tests/titems/titems.nim
@@ -11,26 +11,26 @@ if itemsList.len == 0:
   loadCareers("../bin/data/careers.dat")
   loadFactions("../bin/data/factions.dat")
 
-assert findProtoItem("Iron") > 0
-assert findProtoItem("sfdsfsdfsdf") == 0
+assert findProtoItem("Iron") > 0, "Failed to find an existing item."
+assert findProtoItem("sfdsfsdfsdf") == 0, "Failed to not find a non-existsing item."
 
-assert getItemDamage(60) == "Damaged"
-assert getItemDamage(60, true) == "damaged"
+assert getItemDamage(60) == "Damaged", "Failed to get an item damage level."
+assert getItemDamage(60, true) == "damaged", "Failed to get lowered an item damage level."
 
 block:
   var item = InventoryData(protoIndex: 2, amount: 1, name: "", durability: 80, price: 0)
-  assert getItemName(item) == "Basic Ration (slightly used)"
-  assert getItemName(item, false) == "Basic Ration"
-  assert getItemName(item, true, false) == "Basic Ration (Slightly used)"
+  assert getItemName(item) == "Basic Ration (slightly used)", "Failed to get an item name with lowered damage info."
+  assert getItemName(item, false) == "Basic Ration", "Failed to get an item name."
+  assert getItemName(item, true, false) == "Basic Ration (Slightly used)", "Failed to get an item name with damage info."
   item.name = "New name"
-  assert getItemName(item, false) == "New name"
+  assert getItemName(item, false) == "New name", "Failed to get an item with new name."
 
 gameSettings.showNumbers = 0
-assert getItemChanceToDamage(3) == "Small"
-assert getItemChanceToDamage(30) == "Very high"
+assert getItemChanceToDamage(3) == "Small", "Failed to get chance to damage as string for 3."
+assert getItemChanceToDamage(30) == "Very high", "Failed to get chance to damage as string for 10."
 gameSettings.showNumbers = 1
-assert getItemChanceToDamage(3) == " 3%"
-assert getItemChanceToDamage(30) == " 30%"
+assert getItemChanceToDamage(3) == " 3%", "Failed to get chance to damage as percent for 3."
+assert getItemChanceToDamage(30) == " 30%", "Failed to get chance to damage as percent for 30."
 
 playerShip.modules = @[]
 playerShip.modules.add(ModuleData(mType: cargoRoom, protoIndex: 7))
@@ -47,8 +47,8 @@ for index, _ in member.equipment.mpairs:
 member.equipment[weapon] = 1
 playerShip.crew.add(member)
 
-assert findTools(0, "Bucket", clean) > -1
-assert findTools(0, "sfewrwer", talk) == -1
+assert findTools(0, "Bucket", clean) > -1, "Failed to find an existing tool."
+assert findTools(0, "sfewrwer", talk) == -1, "Failed to not find a non-existing tool."
 
 let itemIndex = getRandomItem(weaponsList, weapon, 20, 20, "POLEIS")
-assert itemIndex > 0 and itemsList.hasKey(itemIndex)
+assert itemIndex > 0 and itemsList.hasKey(itemIndex), "Failed to get a random item."
