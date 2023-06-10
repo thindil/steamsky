e9fa93c3b12a8c2cfc69a764ae56a4aebca234ba
thindil <thindil@laeran.pl>
2023-06-10 03:07:41 +0000

    tests: added failure messages to tests
    FossilOrigin-Name: 7304cd7340a5c5bc93ec45c8c330293b3461bab2e0e2d91970247514f80cef81

--- nim/tests/tmaps/tmaps.nim
+++ nim/tests/tmaps/tmaps.nim
@@ -6,6 +6,6 @@ import ../../src/maps
 
 var coord: cint = 0
 normalizeCoord(coord)
-assert coord == 1
+assert coord == 1, "Failed to normalize an invalid coordinate."
 normalizeCoord(coord)
-assert coord == 1
+assert coord == 1, "Failed to not normalize a valid coordinate."
