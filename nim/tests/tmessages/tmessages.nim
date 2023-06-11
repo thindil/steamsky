d5d416bedd2043968a2c6f2bdc5e79e74bfc8ab6
thindil <thindil@laeran.pl>
2023-06-11 03:02:36 +0000

    tests: added failure messages to tests
    FossilOrigin-Name: dba68c1e0384dc857328eef33e64e2627e2e65fa8d2a902c0248fc1bacd0f377

--- nim/tests/tmessages/tmessages.nim
+++ nim/tests/tmessages/tmessages.nim
@@ -6,18 +6,18 @@ import ../../src/[types, messages]
 
 let messageIndex = getLastMessageIndex()
 addMessage("my message", ord(MessageType.default), ord(white))
-assert getLastMessageIndex() == messageIndex + 1
+assert getLastMessageIndex() == messageIndex + 1, "Failed to get the index of the last message."
 
 clearMessages()
-assert getLastMessageIndex() == -1
+assert getLastMessageIndex() == -1, "Failed to get index of the last message from empty list."
 
-assert formattedTime(1600, 1, 1, 10, 1) == "1600-01-01 10:01"
+assert formattedTime(1600, 1, 1, 10, 1) == "1600-01-01 10:01", "Failed to format the game's time."
 
 addMessage("my message", ord(MessageType.othermessage), ord(green))
-assert getMessage(1, 0).message == "my message"
-assert getMessage(10_000, 0).message.len() == 0
+assert getMessage(1, 0).message == "my message", "Failed to get an existing message."
+assert getMessage(10_000, 0).message.len() == 0, "Failed to not get a non-existing message."
 
-assert messagesAmount(ord(MessageType.default)) == 1
+assert messagesAmount(ord(MessageType.default)) == 1, "Failed to get the amount of messages."
 
 restoreMessage("my message".cstring, ord(MessageType.default), ord(white))
-assert messagesAmount(ord(MessageType.default)) == 2
+assert messagesAmount(ord(MessageType.default)) == 2, "Failed to restore the message in the list."
