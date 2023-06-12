3c971b01b525e0a7a633cf9a49198d80144d019a
thindil <thindil@laeran.pl>
2023-06-12 03:08:57 +0000

    refactor: continue work on move getItemsAmount to Nim
    FossilOrigin-Name: be145cceef7f25470ab98813075f08c8bda0e80956aa30d3c3b83751b9c4a7b1

--- src/ships-cargo.adb
+++ src/ships-cargo.adb
@@ -15,9 +15,7 @@
 --    You should have received a copy of the GNU General Public License
 --    along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
-with Interfaces.C.Strings;
-with Factions; use Factions;
-with Config; use Config;
+with Interfaces.C.Strings; use Interfaces.C.Strings;
 
 package body Ships.Cargo is
 
@@ -70,7 +68,6 @@ package body Ships.Cargo is
 
    function Get_Item_Amount
      (Item_Type: Tiny_String.Bounded_String) return Natural is
-      use Interfaces.C.Strings;
       use Tiny_String;
 
       function Get_Ada_Item_Amount(I_Type: chars_ptr) return Natural with
@@ -87,45 +84,16 @@ package body Ships.Cargo is
    end Get_Item_Amount;
 
    function Get_Items_Amount(I_Type: String) return Natural is
-      Items_Amount: Natural;
-      Faction: Faction_Record;
+      function Get_Ada_Items_Amount(It: chars_ptr) return Natural with
+         Import => True,
+         Convention => C,
+         External_Name => "getAdaItemsAmount";
    begin
-      if I_Type = "Drinks" then
-         Get_Drinks_Amount_Loop :
-         for Member of Player_Ship.Crew loop
-            Faction := Get_Faction(Index => Member.Faction);
-            if Faction.Drinks_Types.Length = 0 then
-               Items_Amount := Game_Settings.Low_Drinks + 1;
-            else
-               Items_Amount := 0;
-               Get_Selected_Drinks_Amount_Loop :
-               for DrinkType of Faction.Drinks_Types loop
-                  Items_Amount :=
-                    Items_Amount + Get_Item_Amount(Item_Type => DrinkType);
-               end loop Get_Selected_Drinks_Amount_Loop;
-               exit Get_Drinks_Amount_Loop when Items_Amount <
-                 Game_Settings.Low_Drinks;
-            end if;
-         end loop Get_Drinks_Amount_Loop;
-      else
-         Get_Items_Amount_Loop :
-         for Member of Player_Ship.Crew loop
-            Faction := Get_Faction(Index => Member.Faction);
-            if Faction.Food_Types.Length = 0 then
-               Items_Amount := Game_Settings.Low_Food + 1;
-            else
-               Items_Amount := 0;
-               Get_Food_Amount_Loop :
-               for FoodType of Faction.Food_Types loop
-                  Items_Amount :=
-                    Items_Amount + Get_Item_Amount(Item_Type => FoodType);
-               end loop Get_Food_Amount_Loop;
-               exit Get_Items_Amount_Loop when Items_Amount <
-                 Game_Settings.Low_Food;
-            end if;
-         end loop Get_Items_Amount_Loop;
-      end if;
-      return Items_Amount;
+      Get_Ada_Ship_Cargo
+        (Cargo => Inventory_To_Nim(Inventory => Player_Ship.Cargo),
+         Get_Player_Ship => 1);
+      Get_Ada_Crew;
+      return Get_Ada_Items_Amount(It => New_String(Str => I_Type));
    end Get_Items_Amount;
 
 end Ships.Cargo;
