868d8f8732494e965599086b131f08642da627bd
thindil <thindil@laeran.pl>
2023-06-13 03:10:49 +0000

    tests: added unit test for getItemsAmount procedure
    FossilOrigin-Name: 6a6dc4cce76ef2b11fcee5829bf552cf51fdc337f0e307616a9cc04560b486c3

--- nim/tests/tshipscargo/tshipscargo.nim
+++ nim/tests/tshipscargo/tshipscargo.nim
@@ -15,6 +15,14 @@ playerShip.modules = @[]
 playerShip.modules.add(ModuleData(mType: cargoRoom, protoIndex: 7))
 playerShip.cargo = @[]
 playerShip.cargo.add(InventoryData(protoIndex: 1, amount: 100, durability: 100))
+playerShip.cargo.add(InventoryData(protoIndex: 3, amount: 200, durability: 100))
+playerShip.crew = @[]
+playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural],
+    homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 2, 1, 1,
+    1, 0, 0], order: talk, loyalty: 100, skills: @[SkillInfo(index: 4, level: 4,
+    experience: 0)], attributes: @[MobAttributeRecord(level: 3, experience: 0),
+    MobAttributeRecord(level: 3, experience: 0), MobAttributeRecord(level: 3,
+    experience: 0), MobAttributeRecord(level: 3, experience: 0)], health: 100))
 
 updateCargo(playerShip, 1, -1)
 assert playerShip.cargo[0].amount == 99
@@ -25,3 +33,5 @@ updateCargo(playerShip, 40, -1)
 assert freeCargo(1) > freeCargo(0)
 
 assert getItemAmount("Fuel") == 100
+
+assert getItemsAmount("Drinks") == 200
