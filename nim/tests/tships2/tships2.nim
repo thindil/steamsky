d6c3d456f106cf89de3c294ffd9bfb45c04f770c
thindil <thindil@laeran.pl>
2023-06-15 02:57:43 +0000

    tests: added failure messages to tests
    FossilOrigin-Name: d422e3d474356874e574d15bf387ff3e10a05c6cb00d47ef3ab13f07749e451b

--- nim/tests/tships2/tships2.nim
+++ nim/tests/tships2/tships2.nim
@@ -3,7 +3,8 @@ discard """
 """
 
 import std/tables
-import ../../src/[careers, crafts, factions, game, items, mobs, ships, ships2, shipmodules, types]
+import ../../src/[careers, crafts, factions, game, items, mobs, ships, ships2,
+    shipmodules, types]
 
 if itemsList.len == 0:
   loadData("../bin/data/game.dat")
@@ -22,9 +23,9 @@ playerShip.modules = @[]
 playerShip.modules.add(ModuleData(mType: cargoRoom, protoIndex: 7,
     durability: 100))
 damageModule(playerShip, 0, 10, "during tests")
-assert playerShip.modules[0].durability == 90
+assert playerShip.modules[0].durability == 90, "Failed to damage the player's ship's module."
 
 discard countCombatValue()
 
-assert generateShipName("POLEIS").len() > 0
+assert generateShipName("POLEIS").len() > 0, "Failed to generate random name for a ship."
 
