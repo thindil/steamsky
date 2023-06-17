6c916ada0d9a542d20c995101de9b336e2994fb9
thindil <thindil@laeran.pl>
2023-06-17 03:04:56 +0000

    tests: added failure messages to tests
    FossilOrigin-Name: cd5ca4350a7ca73ed7a63419173cdf7bc8186d6804a1373137c11559de2144d0

--- nim/tests/tshipscrew/tshipscrew.nim
+++ nim/tests/tshipscrew/tshipscrew.nim
@@ -32,25 +32,25 @@ let oldMorale = playerShip.crew[0].morale[2]
 let oldLevel = playerShip.crew[0].morale[1]
 updateMorale(playerShip, 0, 1)
 assert playerShip.crew[0].morale[2] == oldMorale + 1 or playerShip.crew[
-    0].morale[1] == oldLevel + 1
+    0].morale[1] == oldLevel + 1, "Failed to raise morale of the crew member."
 updateMorale(playerShip, 0, -1)
-assert playerShip.crew[0].morale[2] == oldMorale
+assert playerShip.crew[0].morale[2] == oldMorale, "Failed to lower morale of the crew member."
 
 giveOrders(playerShip, 0, rest)
-assert playerShip.crew[0].order == talk
+assert playerShip.crew[0].order == talk, "Failed to give order to the player."
 
 giveOrders(playerShip, 1, rest)
-assert playerShip.crew[1].order == rest
+assert playerShip.crew[1].order == rest, "Failed to give order to the crew member."
 
 giveOrders(playerShip, 0, rest, -1, false)
 updateOrders(playerShip)
-assert playerShip.crew[0].order == talk
+assert playerShip.crew[0].order == talk, "Failed to update the player's ship's crew orders."
 
-assert getSkillLevel(playerShip.crew[0], 1) == 0
-assert getSkillLevel(playerShip.crew[0], 4) == 1
+assert getSkillLevel(playerShip.crew[0], 1) == 0, "Failed to get the level of the player's skill."
+assert getSkillLevel(playerShip.crew[0], 4) == 1, "Failed to get the level of the crew member's skill."
 
-assert findMember(talk) == 0
-assert findMember(defend) == -1
+assert findMember(talk) == 0, "Failed to find a crew member with the selected order."
+assert findMember(defend) == -1, "Failed to not find a crew member with the selected order."
 
 gainExp(10, 4, 0)
-assert playerShip.crew[0].skills[0].experience == 10
+assert playerShip.crew[0].skills[0].experience == 10, "Failed to gain experience for a crew member."
