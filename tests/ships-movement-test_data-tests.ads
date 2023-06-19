3760628d73218473ffd6d3c712ce843ecc206f67
thindil <thindil@laeran.pl>
2023-06-19 03:03:59 +0000

    tests: updated unit tests
    FossilOrigin-Name: 8f4d3ee821db35ee9bb964a99198911f107248bef2ea733c70d6da7a566f0d6c

--- tests/ships-movement-test_data-tests.ads
+++ tests/ships-movement-test_data-tests.ads
@@ -20,11 +20,8 @@ package Ships.Movement.Test_Data.Tests is
    procedure Test_Change_Ship_Speed_e4d52a_17b968(Gnattest_T: in out Test);
    --  ships-movement.ads:67:4:Change_Ship_Speed:Test_ChangeShipSpeed
 
-   procedure Test_Real_Speed_df0c39_f7fd56(Gnattest_T: in out Test);
-   --  ships-movement.ads:81:4:Real_Speed:Test_RealSpeed
-
    procedure Test_Count_Fuel_Needed_88966b_18e85d(Gnattest_T: in out Test);
-   --  ships-movement.ads:92:4:Count_Fuel_Needed:Test_CountFuelNeeded
+   --  ships-movement.ads:91:4:Count_Fuel_Needed:Test_CountFuelNeeded
 
 end Ships.Movement.Test_Data.Tests;
 --  end read only
