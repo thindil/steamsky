9ca82fb58274975901286d845228180a711549f7
thindil <thindil@laeran.pl>
2023-06-21 02:56:56 +0000

    tests: added failure messages to tests
    FossilOrigin-Name: dde6e21eda5918a0ffe5ec6775f76f9dc313e265770be2288d1654e7d9a7d2c9

--- nim/tests/tstatistics/tstatistics.nim
+++ nim/tests/tstatistics/tstatistics.nim
@@ -10,17 +10,17 @@ if goalsList.len == 0:
 
 gameStats.craftingOrders = @[]
 updateCraftingOrders("1")
-assert gameStats.craftingOrders.len == 1
+assert gameStats.craftingOrders.len == 1, "Failed to update the amount of finished crafting orders."
 
 gameStats.finishedGoals = @[]
 updateFinishedGoals("1")
-assert gameStats.finishedGoals.len == 1
+assert gameStats.finishedGoals.len == 1, "Failed to update the amount of finished goals."
 updateFinishedGoals("Sdfdsf")
-assert gameStats.finishedGoals.len == 1
+assert gameStats.finishedGoals.len == 1, "Failed to not update the amount of finished goals with non-existing goal."
 
 gameStats.points = 0
-assert getGamePoints() == 0
+assert getGamePoints() == 0, "Failed to get the player's game points."
 
 gameStats.finishedMissions = @[]
 updateFinishedMissions("DESTROY")
-assert gameStats.finishedMissions.len == 1
+assert gameStats.finishedMissions.len == 1, "Failed to update the amount of finished missions."
