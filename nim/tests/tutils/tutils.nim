cba3819255d48b65f41585902a65239097d2e425
thindil <thindil@laeran.pl>
2023-06-22 03:03:01 +0000

    tests: added failure messages to tests
    FossilOrigin-Name: 42a0376d7ddd667269af8f8ee0483722509e12b742d76b82ca62523b12fb350d

--- nim/tests/tutils/tutils.nim
+++ nim/tests/tutils/tutils.nim
@@ -5,10 +5,11 @@ discard """
 import ../../src/[types, utils]
 
 assert daysDifference(DateRecord(year: 1, month: 1, day: 1, hour: 0,
-    minutes: 0), DateRecord(year: 1, month: 1, day: 2, hour: 0, minutes: 0)) == 1
+    minutes: 0), DateRecord(year: 1, month: 1, day: 2, hour: 0, minutes: 0)) ==
+    1, "Failed to count days difference between two dates."
 
-assert generateRoboticName().len() > 0
+assert generateRoboticName().len() > 0, "Failed to generate a robotic name."
 
 for i in 1..5:
-  assert getrandom(1, 5) in 1..5
-assert getrandom(5, 5) == 5
+  assert getrandom(1, 5) in 1..5, "Failed to generate a random number between 1 and 5"
+assert getrandom(5, 5) == 5, "Failed to generate a random number between 5 and 5"
