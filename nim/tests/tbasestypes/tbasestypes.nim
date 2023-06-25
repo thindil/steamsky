eb0e987e5a87c9fbdc23413904d58283ab355761
thindil <thindil@laeran.pl>
2023-06-25 03:10:33 +0000

    tests: added more messages to tests
    FossilOrigin-Name: d1137ca2ecfe62a80d29b80c69df10bd215eed111d9ead1753db75989adf760c

--- nim/tests/tbasestypes/tbasestypes.nim
+++ nim/tests/tbasestypes/tbasestypes.nim
@@ -5,14 +5,17 @@ discard """
 import std/tables
 import ../../src/[basestypes, game, items]
 
+echo "Loading the game data."
 if itemsList.len == 0:
   loadData("../bin/data/game.dat")
   loadItems("../bin/data/items.dat")
 if basesTypesList.len == 0:
   loadBasesTypes("../bin/data/bases.dat")
 
+echo "Testing getPrice."
 assert getPrice("0", 1) == 0, "Failed to get the price of a non-buyable item."
 assert getPrice("1", 2) > 0, "Failed to get the price of a buyable item."
 
+echo "Testing isBuyable."
 assert not isBuyable("0", 1), "Failed to check if an item is non-buyable."
 assert isBuyable("1", 2), "Failed to check if an item is buyable."
