15f533eacba5dafb22bc095304beba33a94b9a7e
thindil <thindil@laeran.pl>
2023-06-27 03:06:53 +0000

    tests: added more messages to tests
    FossilOrigin-Name: 8996ec65196a04c0467cb5ba754833f450a885be17e2cf9141d12df97544305e

--- nim/tests/tcrew/tcrew.nim
+++ nim/tests/tcrew/tcrew.nim
@@ -1,10 +1,16 @@
 discard """
   exitcode: 0
+  output: '''Loading the game data.
+Testing getAttributeLevelName.
+Testing getSkillLevelName.
+Testing findCabin.
+Testing updateCrew.'''
 """
 
 import std/tables
 import ../../src/[careers, config, crew, factions, game, items, maps, types]
 
+echo "Loading the game data."
 if itemsList.len == 0:
   loadData("../bin/data/game.dat")
   loadItems("../bin/data/items.dat")
@@ -30,6 +36,7 @@ skyMap[1][1].baseIndex = 1
 
 dailyPayment()
 
+echo "Testing getAttributeLevelName."
 gameSettings.showNumbers = 0
 assert getAttributeLevelName(3) == "Very low", "Failed to get attribute level name for value 3."
 assert getAttributeLevelName(12) == "Below average", "Failed to get attribute level name for value 12."
@@ -39,6 +46,7 @@ assert getAttributeLevelName(3) == "3", "Failed to get attribute level number fo
 assert getAttributeLevelName(12) == "12", "Failed to get attribute level number for value 12."
 assert getAttributeLevelName(48) == "48", "Failed to get attribute level number for value 48."
 
+echo "Testing getSkillLevelName."
 gameSettings.showNumbers = 0
 assert getSkillLevelName(9) == "Beginner", "Failed to get skill level name for value 9."
 assert getSkillLevelName(54) == "Respected", "Failed to get skill level name for value 54."
@@ -48,9 +56,11 @@ assert getSkillLevelName(9) == "9", "Failed to get skill level number for value 
 assert getSkillLevelName(54) == "54", "Failed to get skill level number for value 54."
 assert getSkillLevelName(92) == "92", "Failed to get skill level number for value 92."
 
+echo "Testing findCabin."
 assert findCabin(0) == 0, "Failed to find a cabin of the player."
 assert findCabin(100) == -1, "Failed to not find a cabin for a non-existing crew member."
 
+echo "Testing updateCrew."
 playerShip.crew[0].health = 100
 updateCrew(1, 1)
 playerShip.crew[0].health = 0
