2089e7f079a46456b29858f02f585b1ebc4ac51f
thindil <thindil@laeran.pl>
2023-07-01 03:14:02 +0000

    refactor: removed unneeded code
    FossilOrigin-Name: 75376eaa0f7e082bd1cdaa357fdbef094a1d1e88def3605bf49458e0915bb000

--- src/help.adb
+++ src/help.adb
@@ -20,8 +20,6 @@ with Ada.Strings;
 with Ada.Strings.Fixed;
 with Interfaces.C.Strings;
 with BasesTypes;
-with Careers;
-with Factions;
 with Game;
 with Log;
 
@@ -34,7 +32,6 @@ package body Help is
       use Interfaces.C;
       use Interfaces.C.Strings;
       use BasesTypes;
-      use Factions;
       use Game;
       use Log;
       use Tiny_String;
@@ -73,91 +70,7 @@ package body Help is
          Index := Index + 1;
       end loop Load_Help_Loop;
       -- Add help page about available careers and factions
-      Tmp_Help.Index := To_Unbounded_String(Source => "factions2");
-      Help_Title :=
-        To_Unbounded_String
-          (Source =>
-             Trim
-               (Source => Positive'Image(Positive(Help_List.Length) + 1),
-                Side => Left) &
-             ". Factions and careers");
-      Tmp_Help.Text :=
-        To_Unbounded_String
-          (Source =>
-             "Here you will find information about all available factions and careers in the game" &
-             LF & LF & "{u}Factions{/u}" & LF & LF);
-      Load_Factions_Info_Loop :
-      for I in 1 .. Get_Factions_Amount loop
-         Load_Faction_Block :
-         declare
-            Faction: constant Faction_Record := Get_Faction(Number => I);
-         begin
-            if Faction.Careers.Length > 0 then
-               Append
-                 (Source => Tmp_Help.Text,
-                  New_Item =>
-                    "{b}" & To_String(Source => Faction.Name) & "{/b}" & LF &
-                    "    " & To_String(Source => Faction.Description) & LF &
-                    "    {i}Relations{/b}" & LF);
-               Show_Relations_Loop :
-               for J in Faction.Relations.Iterate loop
-                  Append
-                    (Source => Tmp_Help.Text,
-                     New_Item =>
-                       "        " &
-                       To_String
-                         (Source =>
-                            Get_Faction
-                              (Index => Relations_Container.Key(Position => J))
-                              .Name) &
-                       ": " &
-                       (if Faction.Relations(J).Friendly then "Friendly"
-                        else "Enemies") &
-                       LF);
-               end loop Show_Relations_Loop;
-               Append(Source => Tmp_Help.Text, New_Item => LF);
-            end if;
-         end Load_Faction_Block;
-      end loop Load_Factions_Info_Loop;
-      Append
-        (Source => Tmp_Help.Text, New_Item => LF & "{u}Careers{/u}" & LF & LF);
-      Show_Careers_Block :
-      declare
-         use Careers;
-
-         Faction: constant Faction_Record :=
-           Get_Faction(Index => To_Bounded_String(Source => "POLEIS"));
-      begin
-         Load_Careers_Info_Loop :
-         for I in Careers_List.Iterate loop
-            Append
-              (Source => Tmp_Help.Text,
-               New_Item =>
-                 "{b}" & To_String(Source => Careers_List(I).Name) & "{/b}" &
-                 LF &
-                 Faction.Careers(Careers.Careers_Container.Key(Position => I))
-                   .Description &
-                 LF);
-            if Careers_List(I).Skills.Length > 0 then
-               Append
-                 (Source => Tmp_Help.Text,
-                  New_Item => "    {i}Bonus to skills{/b}" & LF);
-               Show_Skills_Loop :
-               for Skill of Careers_List(I).Skills loop
-                  Append
-                    (Source => Tmp_Help.Text,
-                     New_Item => "        " & To_String(Source => Skill) & LF);
-               end loop Show_Skills_Loop;
-            end if;
-            Append(Source => Tmp_Help.Text, New_Item => LF);
-         end loop Load_Careers_Info_Loop;
-      end Show_Careers_Block;
-      Help_List.Include(Key => Help_Title, New_Item => Tmp_Help);
-      Log_Message
-        (Message => "Help added: " & To_String(Source => Help_Title),
-         Message_Type => EVERYTHING);
-      -- Add help page about available careers and factions
-      Tmp_Help.Index := To_Unbounded_String(Source => "basestypes");
+      Tmp_Help.Index := To_Unbounded_String(Source => "basestypes2");
       Help_Title :=
         To_Unbounded_String
           (Source =>
