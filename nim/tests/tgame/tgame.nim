5a96975a2877ffb5c78df45858e645724b0e0ca9
thindil <thindil@laeran.pl>
2023-07-01 03:15:00 +0000

    tests: added more messages to tests
    FossilOrigin-Name: 99f633788e4066602a6b799dc6474982a79bf9286952749359f0d6be1fcc0366

--- nim/tests/tgame/tgame.nim
+++ nim/tests/tgame/tgame.nim
@@ -1,10 +1,14 @@
 discard """
   exitcode: 0
+  output: '''Loading the game data.
+Testing findSkillIndex.'''
 """
 
 import ../../src/[game]
 
+echo "Loading the game data."
 loadData("../bin/data/game.dat")
 
+echo "Testing findSkillIndex."
 assert findSkillIndex(skillName = "Piloting") == 1, "Failed to find the index of the skill."
 assert findSkillIndex(skillName = "sdfwerwerwe") == 0, "Failed to not find the index of the non-existing skill."
