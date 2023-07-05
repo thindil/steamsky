f5467c3b35dead8e9d76affceace20de3a3a8ee6
thindil <thindil@laeran.pl>
2023-07-05 03:06:33 +0000

    tests: added more messages to tests
    FossilOrigin-Name: 4f6d3b6d6ca19e406c1f15792b79db4c3082abd80c0aada9fa9940b59ff4b3be

--- nim/tests/titems/titems.nim
+++ nim/tests/titems/titems.nim
@@ -1,22 +1,33 @@
 discard """
   exitcode: 0
+  output: '''Loading the game data.
+Testing findProtoItem.
+Testing getItemDamage.
+Testing getItemName.
+Testing getItemChanceToDamage.
+Testing findTools.
+Testing getRandomItem.'''
 """
 
 import std/tables
 import ../../src/[careers, config, factions, game, items, types]
 
+echo "Loading the game data."
 if itemsList.len == 0:
   loadData("../bin/data/game.dat")
   loadItems("../bin/data/items.dat")
   loadCareers("../bin/data/careers.dat")
   loadFactions("../bin/data/factions.dat")
 
+echo "Testing findProtoItem."
 assert findProtoItem("Iron") > 0, "Failed to find an existing item."
 assert findProtoItem("sfdsfsdfsdf") == 0, "Failed to not find a non-existsing item."
 
+echo "Testing getItemDamage."
 assert getItemDamage(60) == "Damaged", "Failed to get an item damage level."
 assert getItemDamage(60, true) == "damaged", "Failed to get lowered an item damage level."
 
+echo "Testing getItemName."
 block:
   var item = InventoryData(protoIndex: 2, amount: 1, name: "", durability: 80, price: 0)
   assert getItemName(item) == "Basic Ration (slightly used)", "Failed to get an item name with lowered damage info."
@@ -25,6 +36,7 @@ block:
   item.name = "New name"
   assert getItemName(item, false) == "New name", "Failed to get an item with new name."
 
+echo "Testing getItemChanceToDamage."
 gameSettings.showNumbers = 0
 assert getItemChanceToDamage(3) == "Small", "Failed to get chance to damage as string for 3."
 assert getItemChanceToDamage(30) == "Very high", "Failed to get chance to damage as string for 10."
@@ -47,8 +59,10 @@ for index, _ in member.equipment.mpairs:
 member.equipment[weapon] = 1
 playerShip.crew.add(member)
 
+echo "Testing findTools."
 assert findTools(0, "Bucket", clean) > -1, "Failed to find an existing tool."
 assert findTools(0, "sfewrwer", talk) == -1, "Failed to not find a non-existing tool."
 
+echo "Testing getRandomItem."
 let itemIndex = getRandomItem(weaponsList, weapon, 20, 20, "POLEIS")
 assert itemIndex > 0 and itemsList.hasKey(itemIndex), "Failed to get a random item."
