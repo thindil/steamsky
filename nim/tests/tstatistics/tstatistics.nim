aa9fe5d5b4da5d50cc6f0f5c77052af894d96901
thindil <thindil@laeran.pl>
2023-07-06 03:08:08 +0000

    tests: added unit test for clearGameStats procedure
    FossilOrigin-Name: bbd79f17f1046492e1c43720a21d50fac9801ca77ab89f90929403f4429367bf

--- nim/tests/tstatistics/tstatistics.nim
+++ nim/tests/tstatistics/tstatistics.nim
@@ -24,3 +24,7 @@ assert getGamePoints() == 0, "Failed to get the player's game points."
 gameStats.finishedMissions = @[]
 updateFinishedMissions("DESTROY")
 assert gameStats.finishedMissions.len == 1, "Failed to update the amount of finished missions."
+
+gameStats.points = 100
+clearGameStats()
+assert gameStats.points == 0, "Failed to clear the game statistics."
