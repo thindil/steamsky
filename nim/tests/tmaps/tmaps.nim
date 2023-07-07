7e10309c2d989eae45b3293866c758d5de5808da
thindil <thindil@laeran.pl>
2023-07-07 03:05:51 +0000

    tests: added more messages to tests
    FossilOrigin-Name: d268914f109e0f7450b4719982e7286e48edb9b5192629f296e6b630e5cbbc9f

--- nim/tests/tmaps/tmaps.nim
+++ nim/tests/tmaps/tmaps.nim
@@ -1,9 +1,11 @@
 discard """
   exitcode: 0
+  output: '''Testing normalizeCoord.'''
 """
 
 import ../../src/maps
 
+echo "Testing normalizeCoord."
 var coord: cint = 0
 normalizeCoord(coord)
 assert coord == 1, "Failed to normalize an invalid coordinate."
