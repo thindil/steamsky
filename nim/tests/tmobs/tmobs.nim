affe2b02b9850f5f5b06e2d5325ce1a47c1dfb92
thindil <thindil@laeran.pl>
2023-07-11 03:08:19 +0000

    tests: added more messages to tests
    FossilOrigin-Name: 4599b625ca713412f6f8ddd5dd39a619da5a10032ff970327ca31ec789f71315

--- nim/tests/tmobs/tmobs.nim
+++ nim/tests/tmobs/tmobs.nim
@@ -1,10 +1,13 @@
 discard """
   exitcode: 0
+  output: '''Loading the game data.
+Testing generateMob.'''
 """
 
 import std/tables
 import ../../src/[careers, factions, game, items, mobs, types]
 
+echo "Loading the game data."
 if itemsList.len == 0:
   loadData("../bin/data/game.dat")
   loadItems("../bin/data/items.dat")
@@ -13,6 +16,7 @@ if itemsList.len == 0:
 if protoMobsList.len == 0:
   loadMobs("../bin/data/mobs.dat")
 
+echo "Testing generateMob."
 let newMob = generateMob(5, "POLEIS")
 assert newMob.attributes[0].level == 2, "Failed to set the mob's attributes."
 assert newMob.orderTime == 15, "Failed to set the mob's order's time."
