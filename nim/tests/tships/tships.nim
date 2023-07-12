315b988bade6f0f2eb093add6884a3aa07355ebe
thindil <thindil@laeran.pl>
2023-07-12 03:00:32 +0000

    tests: added more messages to tests
    FossilOrigin-Name: fe4e681c94aa7267a15773da7ec21f993d978c15400c92db056cff8a280d83a1

--- nim/tests/tships/tships.nim
+++ nim/tests/tships/tships.nim
@@ -1,11 +1,17 @@
 discard """
   exitcode: 0
+  output: '''Loading the game data.
+Testing getCabinQuality.
+Testing damageModule.
+Testing countShipWeigth.
+Testing createShip.'''
 """
 
 import std/tables
 import ../../src/[careers, crafts, factions, game, items, maps, mobs, ships,
     shipmodules, types]
 
+echo "Loading the game data."
 if itemsList.len == 0:
   loadData("../bin/data/game.dat")
   loadItems("../bin/data/items.dat")
@@ -19,16 +25,20 @@ if protoMobsList.len == 0:
 if protoShipsList.len == 0:
   loadShips("../bin/data/ships.dat")
 
+echo "Testing getCabinQuality."
 assert getCabinQuality(10) == "Empty room", "Failed to get the cabin's quality."
 
+echo "Testing damageModule."
 playerShip.modules = @[]
 playerShip.modules.add(ModuleData(mType: cargoRoom, protoIndex: 7,
     durability: 100))
 damageModule(playerShip, 0, 10, "during tests")
 assert playerShip.modules[0].durability == 90, "Failed to damage the player's ship's module."
 
+echo "Testing countShipWeigth."
 discard countShipWeight(playerShip)
 
+echo "Testing createShip."
 for base in skyBases.mitems:
   base.owner = "POLEIS"
 for x in MapXRange.low .. MapXRange.high:
