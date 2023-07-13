7dd4cec167c7e31e81a2fdcb8917b3fbdbe9d127
thindil <thindil@laeran.pl>
2023-07-13 03:03:03 +0000

    tests: added more messages to tests
    FossilOrigin-Name: e05afad0a2e763e2a85c015d1d18fda1744cf4048b209ee4b6519b82a2f7f1d9

--- nim/tests/tships2/tships2.nim
+++ nim/tests/tships2/tships2.nim
@@ -1,11 +1,16 @@
 discard """
   exitcode: 0
+  output: '''Loading the game data.
+Testing damageModule.
+Testing countCombatValue.
+Testing generateShipName.'''
 """
 
 import std/tables
 import ../../src/[careers, crafts, factions, game, items, mobs, ships, ships2,
     shipmodules, types]
 
+echo "Loading the game data."
 if itemsList.len == 0:
   loadData("../bin/data/game.dat")
   loadItems("../bin/data/items.dat")
@@ -19,13 +24,16 @@ if protoMobsList.len == 0:
 if protoShipsList.len == 0:
   loadShips("../bin/data/ships.dat")
 
+echo "Testing damageModule."
 playerShip.modules = @[]
 playerShip.modules.add(ModuleData(mType: cargoRoom, protoIndex: 7,
     durability: 100))
 damageModule(playerShip, 0, 10, "during tests")
 assert playerShip.modules[0].durability == 90, "Failed to damage the player's ship's module."
 
+echo "Testing countCombatValue."
 discard countCombatValue()
 
+echo "Testing generateShipName."
 assert generateShipName("POLEIS").len() > 0, "Failed to generate random name for a ship."
 
