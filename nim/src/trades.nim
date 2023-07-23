a1604ac7d03cefbabf9d5542f224dad95c8873dc
thindil <thindil@laeran.pl>
2023-07-23 02:54:28 +0000

    refactor: started work on move generateTraderCargo to Nim
    FossilOrigin-Name: 4fc4f25e2788d30d4dece15b561af3b1aefbb2cf171dc461ea727107ebf1d2e5

--- nim/src/trades.nim
+++ nim/src/trades.nim
@@ -15,10 +15,19 @@
 # You should have received a copy of the GNU General Public License
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
-import types
+import std/tables
+import game, ships, types, utils
 
-type
-  TradeNoFreeCargoError* = object of CatchableError
-    ## Used to mark problems with free cargo space in the ship
-
-var traderCargo*: seq[BaseCargo]
+proc generateTraderCargo*(protoIndex: Positive) =
+  let traderShip = createShip(protoIndex = protoIndex, name = "",
+        x = playerShip.skyX, y = playerShip.skyY, speed = fullStop)
+  traderCargo = @[]
+  for item in traderShip.cargo:
+    traderCargo.add(BaseCargo(protoIndex: item.protoIndex, amount: item.amount,
+        durability: defaultItemDurability, price: itemsList[
+        item.protoIndex].price))
+  var cargoAmount = if traderShip.crew.len < 5: getRandom(min = 1, max = 3)
+      elif traderShip.crew.len < 10: getRandom(min = 1, max = 5)
+      else: getRandom(min = 1, max = 10)
+  while cargoAmount > 0:
+    cargoAmount.dec
