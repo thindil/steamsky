6dcb462f7c2c5b387ae092c9bcdf3d8cb937b5bf
thindil <thindil@laeran.pl>
2023-07-24 03:07:43 +0000

    tests: added unit test for generateTraderCargo
    FossilOrigin-Name: 4d89f90e23a4617c1bb71710536689a6c8678b8e3f473e36ea87b45c0eb7e2ca

---
+++ nim/tests/ttrades/ttrades.nim
@@ -0,0 +1,49 @@
+discard """
+  exitcode: 0
+  output: '''Loading the game data.
+Testing generateTraderCargo.'''
+"""
+
+import std/tables
+import ../../src/[careers, crafts, factions, game, items, maps, mobs, ships,
+    shipmodules, trades, types]
+
+echo "Loading the game data."
+if itemsList.len == 0:
+  loadData("../bin/data/game.dat")
+  loadItems("../bin/data/items.dat")
+  loadCareers("../bin/data/careers.dat")
+  loadFactions("../bin/data/factions.dat")
+  loadModules("../bin/data/shipmodules.dat")
+if recipesList.len == 0:
+  loadRecipes("../bin/data/recipes.dat")
+if protoMobsList.len == 0:
+  loadMobs("../bin/data/mobs.dat")
+if protoShipsList.len == 0:
+  loadShips("../bin/data/ships.dat")
+
+skyBases[1].reputation = ReputationData(level: 1, experience: 1)
+playerShip.skyX = 1
+playerShip.skyY = 1
+playerShip.crew = @[]
+playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural],
+    homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 2, 1, 1,
+    1, 0, 0], order: talk, loyalty: 100, skills: @[SkillInfo(index: 4, level: 4,
+    experience: 0)], attributes: @[MobAttributeRecord(level: 3, experience: 0),
+    MobAttributeRecord(level: 3, experience: 0), MobAttributeRecord(level: 3,
+    experience: 0), MobAttributeRecord(level: 3, experience: 0)], health: 100))
+playerShip.modules = @[]
+playerShip.modules.add(ModuleData(mType: ModuleType2.hull, protoIndex: 1,
+    durability: 100, maxModules: 10))
+playerShip.cargo = @[]
+playerShip.cargo.add(InventoryData(protoIndex: 1, amount: 100, durability: 100))
+playerShip.speed = docked
+skyMap[1][1].baseIndex = 1
+skyBases[1].population = 100
+skyBases[1].baseType = "1"
+skyBases[1].owner = "POLEIS"
+gameDate = DateRecord(year: 1600, month: 1, day: 1, hour: 8, minutes: 0)
+
+echo "Testing generateTraderCargo."
+generateTraderCargo(96)
+assert traderCargo.len > 0, "Failed to generate the trader's cargo."
