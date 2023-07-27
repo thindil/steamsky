61f16085daaf30d0cee0c19400bf5ba74c748c27
thindil <thindil@laeran.pl>
2023-07-27 03:02:52 +0000

    refactor: updated code to the new version of combat package
    FossilOrigin-Name: 675a7df709f9b649b016490aa0952cab3975266d8c68e8268dc86a6fe8f97135

--- src/ui/combat-ui.adb
+++ src/ui/combat-ui.adb
@@ -17,6 +17,7 @@ with Ada.Characters.Handling;
 with Ada.Characters.Latin_1; use Ada.Characters.Latin_1;
 with Ada.Strings; use Ada.Strings;
 with Ada.Strings.Fixed; use Ada.Strings.Fixed;
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with Interfaces.C.Strings; use Interfaces.C.Strings;
 with GNAT.Directory_Operations;
 with GNAT.String_Split; use GNAT.String_Split;
