f5471d4673e953b83b0f2252ce140f04f4bb902c
thindil <thindil@laeran.pl>
2023-07-28 03:17:25 +0000

    refactor: changed name of Add_Commands procedure
    FossilOrigin-Name: 713367a7baabfbdd173cbc352eb9252e099268f05e970afbc010a519dd02406c

--- src/ui/ships-ui-modules.ads
+++ src/ui/ships-ui-modules.ads
@@ -1,4 +1,4 @@
--- Copyright (c) 2020-2022 Bartek thindil Jasicki <thindil@laeran.pl>
+-- Copyright (c) 2020-2023 Bartek thindil Jasicki <thindil@laeran.pl>
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -20,11 +20,11 @@
 package Ships.UI.Modules is
 -- ****
 
-   -- ****f* SUModules/SUModules.Add_Commands
+   -- ****f* SUModules/SUModules.Add_Modules_Commands
    -- FUNCTION
    -- Add Tcl commands related to the player's ship modules information
    -- SOURCE
-   procedure Add_Commands;
+   procedure Add_Modules_Commands;
    -- ****
 
    -- ****f* SUModules/SUModules.Update_Modules_Info
