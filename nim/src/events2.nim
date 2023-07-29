6b77585f6b92e7247247c315c61d64a172587561
thindil <thindil@laeran.pl>
2023-07-29 03:04:21 +0000

    refactor: continue work on move checkForEvent to Nim
    FossilOrigin-Name: 42e42d743b3265e59d21ae6c7efc453d47ee013e212e2c052482498c250ed4af

--- nim/src/events2.nim
+++ nim/src/events2.nim
@@ -15,12 +15,13 @@
 # You should have received a copy of the GNU General Public License
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
-import combat, game, maps, shipscrew, types, utils
+import combat, game, maps, messages, shipscrew, types, utils
 
 proc checkForEvent*(): bool =
   if skyMap[playerShip.skyX][playerShip.skyY].eventIndex > -1:
     if eventsList[skyMap[playerShip.skyX][playerShip.skyY].eventIndex].eType == enemyShip:
-      return startCombat(enemyIndex = eventsList[skyMap[playerShip.skyX][playerShip.skyY].eventIndex].shipIndex)
+      return startCombat(enemyIndex = eventsList[skyMap[playerShip.skyX][
+          playerShip.skyY].eventIndex].shipIndex)
     else:
       return false
   if getRandom(min = 1, max = 100) > 6:
@@ -35,7 +36,30 @@ proc checkForEvent*(): bool =
     of 1 .. 5:
       let engineerIndex = findMember(order = engineer)
       if engineerIndex > -1 and playerShip.speed != fullStop:
-        discard
+        var roll2 = getRandom(min = 1, max = 100)
+        case playerShip.speed
+        of quarterSpeed:
+          roll2 = if roll2 < 21: 1 else: roll2 - 20
+        of fullSpeed:
+          roll2 = roll2 + 20
+        else:
+          discard
+        if roll2 > getSkillLevel(member = playerShip.crew[engineerIndex],
+            skillIndex = engineeringSkill):
+          addMessage(message = "One of your engines is taking damage.",
+              mType = otherMessage, color = red)
+          var engines: seq[Natural]
+          for index, module in playerShip.modules:
+            if module.mType == ModuleType2.engine and not module.disabled:
+              engines.add(y = index)
+          let engineIndex = engines[getRandom(min = 0, max = engines.high)]
+          playerShip.modules[engineIndex].durability.dec
+          updateOrders(ship = playerShip)
+        else:
+          addMessage(message = playerShip.crew[engineerIndex].name &
+              " has prevented engine damage.", mType = otherMessage, color = green)
+        gainExp(amount = 1, skillNumber = engineeringSkill,
+            crewIndex = engineerIndex)
     else:
       discard
 
