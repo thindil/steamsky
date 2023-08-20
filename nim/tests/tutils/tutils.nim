8a26d6d4f8a7631d7de6ca5f60c3156298718ca1
thindil <thindil@laeran.pl>
2023-08-20 02:54:15 +0000

    tests: made test to continue after one's fail
    FossilOrigin-Name: dcef993709f48bd089e4ed050b01bc0409df0acea5f5d21449708750b5c22553

--- nim/tests/tutils/tutils.nim
+++ nim/tests/tutils/tutils.nim
@@ -8,14 +8,26 @@ Testing getRandom.'''
 import ../../src/[types, utils]
 
 echo "Testing daysDifference."
-assert daysDifference(DateRecord(year: 1, month: 1, day: 1, hour: 0,
-    minutes: 0), DateRecord(year: 1, month: 1, day: 2, hour: 0, minutes: 0)) ==
-    1, "Failed to count days difference between two dates."
+try:
+  assert daysDifference(DateRecord(year: 1, month: 1, day: 1, hour: 0,
+      minutes: 0), DateRecord(year: 1, month: 1, day: 2, hour: 0, minutes: 0)) ==
+      1
+except AssertionDefect:
+  echo "Failed to count days difference between two dates."
 
 echo "Testing generateRoboticName."
-assert generateRoboticName().len() > 0, "Failed to generate a robotic name."
+try:
+  assert generateRoboticName().len() > 0
+except AssertionDefect:
+  echo "Failed to generate a robotic name."
 
 echo "Testing getRandom."
 for i in 1..5:
-  assert getrandom(1, 5) in 1..5, "Failed to generate a random number between 1 and 5"
-assert getrandom(5, 5) == 5, "Failed to generate a random number between 5 and 5"
+  try:
+    assert getrandom(1, 5) in 1..5
+  except AssertionDefect:
+    echo "Failed to generate a random number between 1 and 5"
+try:
+  assert getrandom(5, 5) == 5
+except AssertionDefect:
+  echo "Failed to generate a random number between 5 and 5"
