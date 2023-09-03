4de2b8d939d73c0bb7b85d3e656d074e932e664f
thindil <thindil@laeran.pl>
2023-09-03 02:59:40 +0000

    tests: send error messages to standard error
    FossilOrigin-Name: bf79b79afa4c5905fef02697ae00ea8ce9948507ea9a9f6912e06a7471ef1988

--- nim/tests/thalloffame/thalloffame.nim
+++ nim/tests/thalloffame/thalloffame.nim
@@ -15,4 +15,4 @@ updateHallOfFame("TestPlayer", "TestDeath")
 try:
   assert hallOfFameArray[1].name == "TestPlayer"
 except AssertionDefect:
-  echo "Failed to add entry to Hall of Fame."
+  writeLine(stderr, "Failed to add entry to Hall of Fame.")
