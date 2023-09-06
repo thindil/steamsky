d9b34ce0ed76b5fd46c2a65c245dc5f84db84f5d
thindil <thindil@laeran.pl>
2023-09-06 03:30:15 +0000

    tests: send error messages to standard error
    FossilOrigin-Name: 207cddbce76036de142d69a73cfe57661cc54e69510009cc42a6dc172cd34651

--- nim/tests/tmessages/tmessages.nim
+++ nim/tests/tmessages/tmessages.nim
@@ -16,20 +16,20 @@ addMessage("my message", ord(MessageType.default), ord(white))
 try:
   assert getLastMessageIndex() == messageIndex + 1
 except AssertionDefect:
-  echo "Failed to get the index of the last message."
+  writeLine(stderr, "Failed to get the index of the last message.")
 
 echo "Testing clearMessages."
 clearMessages()
 try:
   assert getLastMessageIndex() == -1
 except AssertionDefect:
-  echo "Failed to get index of the last message from empty list."
+  writeLine(stderr, "Failed to get index of the last message from empty list.")
 
 echo "Testing formattedTime."
 try:
   assert formattedTime(1600, 1, 1, 10, 1) == "1600-01-01 10:01"
 except AssertionDefect:
-  echo "Failed to format the game's time."
+  writeLine(stderr, "Failed to format the game's time.")
 
 echo "Testing getMessage."
 gameDate = DateRecord(year: 1600, month: 1, day: 1, hour: 8, minutes: 1)
@@ -37,21 +37,21 @@ addMessage("my message", ord(MessageType.othermessage), ord(green))
 try:
   assert getMessage(1, 0).message == "[1600-01-01 08:01] my message"
 except AssertionDefect:
-  echo "Failed to get an existing message."
+  writeLine(stderr, "Failed to get an existing message.")
 try:
   assert getMessage(10_000, 0).message.len() == 0
 except AssertionDefect:
-  echo "Failed to not get a non-existing message."
+  writeLine(stderr, "Failed to not get a non-existing message.")
 
 echo "Testing messagesAmount."
 try:
   assert messagesAmount(ord(MessageType.default)) == 1
 except AssertionDefect:
-  echo "Failed to get the amount of messages."
+  writeLine(stderr, "Failed to get the amount of messages.")
 
 echo "Testing restoreMessage."
 restoreMessage("my message".cstring, ord(MessageType.default), ord(white))
 try:
   assert messagesAmount(ord(MessageType.default)) == 2
 except AssertionDefect:
-  echo "Failed to restore the message in the list."
+  writeLine(stderr, "Failed to restore the message in the list.")
