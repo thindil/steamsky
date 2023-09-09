512658209c6711de422fb54fd79afe6696685138
thindil <thindil@laeran.pl>
2023-09-09 04:00:06 +0000

    tests: added unit test for startStory procedure
    FossilOrigin-Name: 97e5a928f7535280f6439a772b98064bb3eb9ed4328c64ec632bb2c158a6b6d6

--- nim/tests/tstories/tstories.nim
+++ nim/tests/tstories/tstories.nim
@@ -1,13 +1,19 @@
 discard """
   exitcode: 0
   output: '''Loading the game data.
-Testing getStepData.'''
+Testing getStepData.
+Testing startStory.'''
 """
 
 import std/tables
-import ../../src/[stories]
+import ../../src/[factions, careers, game, items, stories, types]
 
 echo "Loading the game data."
+if itemsList.len == 0:
+  loadData("../bin/data/game.dat")
+  loadItems("../bin/data/items.dat")
+  loadCareers("../bin/data/careers.dat")
+  loadFactions("../bin/data/factions.dat")
 if storiesList.len == 0:
   loadStories("../bin/data/stories.dat")
 
@@ -20,3 +26,20 @@ try:
   assert getStepData(storiesList["1"].steps[0].finishData, "sdfdsf").len == 0
 except AssertionDefect:
   echo "Failed to not get non existing finish data of selected step."
+
+echo "Testing startStory."
+playerShip.crew = @[]
+playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural],
+    homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 2, 1, 1,
+    1, 0, 0], order: talk, loyalty: 100, skills: @[SkillInfo(index: 4, level: 4,
+    experience: 0)], attributes: @[MobAttributeRecord(level: 3, experience: 0),
+    MobAttributeRecord(level: 3, experience: 0), MobAttributeRecord(level: 3,
+    experience: 0), MobAttributeRecord(level: 3, experience: 0)]))
+for i in 1 .. 1_000_000:
+  startStory("Undead", dropItem)
+  if currentStory.index.len > 0:
+    break
+try:
+  assert currentStory.index.len > 0
+except AssertionDefect:
+  echo "Failed to start a new story."
