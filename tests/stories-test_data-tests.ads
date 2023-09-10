2ccdbe89babb3f7ed4d99855460f7a1f6e9b194f
thindil <thindil@laeran.pl>
2023-09-10 03:13:45 +0000

    tests: updated unit tests
    FossilOrigin-Name: cc67a3bebaf9c070975a85f4880e72d163180afb36cfed63b96cdf32a46a7fc7

--- tests/stories-test_data-tests.ads
+++ tests/stories-test_data-tests.ads
@@ -10,21 +10,18 @@ package Stories.Test_Data.Tests is
      .Test with
    null record;
 
-   procedure Test_Start_Story_542c86_eea028(Gnattest_T: in out Test);
-   --  stories.ads:299:4:Start_Story:Test_StartStory
-
    procedure Test_Clear_Current_Story_40d661_0d61b8(Gnattest_T: in out Test);
-   --  stories.ads:309:4:Clear_Current_Story:Test_ClearCurrentStory
+   --  stories.ads:308:4:Clear_Current_Story:Test_ClearCurrentStory
 
    procedure Test_Progress_Story_323e5c_14aed6(Gnattest_T: in out Test);
-   --  stories.ads:323:4:Progress_Story:Test_ProgressStory
+   --  stories.ads:322:4:Progress_Story:Test_ProgressStory
 
    procedure Test_Get_Current_Story_Text_893370_8f71b5
      (Gnattest_T: in out Test);
-   --  stories.ads:333:4:Get_Current_Story_Text:Tets_GetCurrentStoryText
+   --  stories.ads:332:4:Get_Current_Story_Text:Tets_GetCurrentStoryText
 
    procedure Test_Get_Story_Location_5f0f68_b0f396(Gnattest_T: in out Test);
-   --  stories.ads:361:4:Get_Story_Location:Test_GetStoryLocation
+   --  stories.ads:360:4:Get_Story_Location:Test_GetStoryLocation
 
 end Stories.Test_Data.Tests;
 --  end read only
