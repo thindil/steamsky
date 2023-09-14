c94a2075300459e3dd0cd8d09e03312f409b2ebf
thindil <thindil@laeran.pl>
2023-09-14 03:17:54 +0000

    tests: send error messages to standard error
    FossilOrigin-Name: 1deb959b027bef32cf0f2f102d503efbbebe38dd15e89f192c7dfa5b1807e923

--- nim/tests/tshipscrew/tshipscrew.nim
+++ nim/tests/tshipscrew/tshipscrew.nim
@@ -46,24 +46,24 @@ try:
   assert playerShip.crew[0].morale[2] == oldMorale + 1 or playerShip.crew[
     0].morale[1] == oldLevel + 1
 except AssertionDefect:
-  echo "Failed to raise morale of the crew member."
+  writeLine(stderr, "Failed to raise morale of the crew member.")
 updateMorale(playerShip, 0, -1)
 try:
   assert playerShip.crew[0].morale[2] == oldMorale
 except AssertionDefect:
-  echo "Failed to lower morale of the crew member."
+  writeLine(stderr, "Failed to lower morale of the crew member.")
 
 echo "Testing giveOrders."
 giveOrders(playerShip, 0, rest)
 try:
   assert playerShip.crew[0].order == talk
 except AssertionDefect:
-  echo "Failed to give order to the player."
+  writeLine(stderr, "Failed to give order to the player.")
 giveOrders(playerShip, 1, rest)
 try:
   assert playerShip.crew[1].order == rest
 except AssertionDefect:
-  echo "Failed to give order to the crew member."
+  writeLine(stderr, "Failed to give order to the crew member.")
 
 echo "Testing updateOrders."
 giveOrders(playerShip, 0, rest, -1, false)
@@ -71,31 +71,31 @@ updateOrders(playerShip)
 try:
   assert playerShip.crew[0].order == talk
 except AssertionDefect:
-  echo "Failed to update the player's ship's crew orders."
+  writeLine(stderr, "Failed to update the player's ship's crew orders.")
 
 echo "Testing getSkillLevel."
 try:
   assert getSkillLevel(playerShip.crew[0], 1) == 0
 except AssertionDefect:
-  echo "Failed to get the level of the player's skill."
+  writeLine(stderr, "Failed to get the level of the player's skill.")
 try:
   assert getSkillLevel(playerShip.crew[0], 4) == 1
 except AssertionDefect:
-  echo "Failed to get the level of the crew member's skill."
+  writeLine(stderr, "Failed to get the level of the crew member's skill.")
 
 echo "Testing findMember."
 try:
   assert findMember(talk) == 0
 except AssertionDefect:
-  echo "Failed to find a crew member with the selected order."
+  writeLine(stderr, "Failed to find a crew member with the selected order.")
 try:
   assert findMember(defend) == -1
 except AssertionDefect:
-  echo "Failed to not find a crew member with the selected order."
+  writeLine(stderr, "Failed to not find a crew member with the selected order.")
 
 echo "Testing gainExp."
 gainExp(10, 4, 0)
 try:
   assert playerShip.crew[0].skills[0].experience == 10
 except AssertionDefect:
-  echo "Failed to gain experience for a crew member."
+  writeLine(stderr, "Failed to gain experience for a crew member.")
