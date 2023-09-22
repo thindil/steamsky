a25978eb9e01697a62071b08cb562468fe0a0c38
thindil <thindil@laeran.pl>
2023-09-22 03:12:19 +0000

    tests: send error messages to standard error
    FossilOrigin-Name: 825b8ba474d916425986ecb3ffd96f56cc96b13a400685a3a6b78992acecddfb

--- nim/tests/tutils/tutils.nim
+++ nim/tests/tutils/tutils.nim
@@ -13,21 +13,21 @@ try:
       minutes: 0), DateRecord(year: 1, month: 1, day: 2, hour: 0, minutes: 0)) ==
       1
 except AssertionDefect:
-  echo "Failed to count days difference between two dates."
+  writeLine(stderr, "Failed to count days difference between two dates.")
 
 echo "Testing generateRoboticName."
 try:
   assert generateRoboticName().len() > 0
 except AssertionDefect:
-  echo "Failed to generate a robotic name."
+  writeLine(stderr, "Failed to generate a robotic name.")
 
 echo "Testing getRandom."
 for i in 1..5:
   try:
     assert getrandom(1, 5) in 1..5
   except AssertionDefect:
-    echo "Failed to generate a random number between 1 and 5"
+    writeLine(stderr, "Failed to generate a random number between 1 and 5")
 try:
   assert getrandom(5, 5) == 5
 except AssertionDefect:
-  echo "Failed to generate a random number between 5 and 5"
+  writeLine(stderr, "Failed to generate a random number between 5 and 5")
