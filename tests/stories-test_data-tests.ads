66b89c6412319968c634c27c58b0078479a327e7
thindil <thindil@laeran.pl>
2023-09-30 03:18:49 +0000

    tests: updated unit tests
    FossilOrigin-Name: 3405e8e46d7afebd467b81dad1c1c70d694d0146c8bc5dcb90d6b62c6f61bb9b

--- tests/stories-test_data-tests.ads
+++ tests/stories-test_data-tests.ads
@@ -10,11 +10,8 @@ package Stories.Test_Data.Tests is
      .Test with
    null record;
 
-   procedure Test_Clear_Current_Story_40d661_0d61b8(Gnattest_T: in out Test);
-   --  stories.ads:308:4:Clear_Current_Story:Test_ClearCurrentStory
-
    procedure Test_Get_Story_Location_5f0f68_b0f396(Gnattest_T: in out Test);
-   --  stories.ads:358:4:Get_Story_Location:Test_GetStoryLocation
+   --  stories.ads:352:4:Get_Story_Location:Test_GetStoryLocation
 
 end Stories.Test_Data.Tests;
 --  end read only
