560517eba139103efad6d8733ca9864d627c9d5c
thindil <thindil@laeran.pl>
2023-10-11 03:07:20 +0000

    refactor: use Nim code to move the player's ship
    FossilOrigin-Name: c1d03c07c6e75f9eb9afd79306365f9b676038a00072908a7df265012fc30126

--- src/ships-movement.adb
+++ src/ships-movement.adb
@@ -17,179 +17,44 @@
 
 with Interfaces.C.Strings; use Interfaces.C.Strings;
 with Bases;
-with Config;
-with Factions;
-with Maps;
-with Messages;
-with Ships.Cargo;
-with Ships.Crew;
-with Statistics;
+with Maps; use Maps;
 
 package body Ships.Movement is
 
    function Move_Ship
      (X, Y: Integer; Message: in out Unbounded_String) return Natural is
-      use Config;
-      use Factions;
-      use Messages;
-      use Ships.Cargo;
-      use Ships.Crew;
-      use Statistics;
-      use Tiny_String;
-
-      --## rule off TYPE_INITIAL_VALUES
-      type Speed_Type is digits 2;
-      --## rule on TYPE_INITIAL_VALUES
-      New_X, New_Y: Integer;
-      Time_Passed, Fuel_Needed: Integer;
-      Speed: Speed_Type;
-      Fuel_Index: Inventory_Container.Extended_Index;
-      function Need_Rest(Order: Crew_Orders) return Boolean is
-         Member_Index: Crew_Container.Extended_Index;
-      begin
-         Member_Index := Find_Member(Order => Order);
-         if Member_Index = 0 then
-            Find_Member_Loop :
-            for Member of Player_Ship.Crew loop
-               if Member.Previous_Order = Order then
-                  return True;
-               end if;
-            end loop Find_Member_Loop;
-         end if;
-         return False;
-      end Need_Rest;
-      function Have_Order_Requirements return String is
-         function Have_Ada_Order_Requirements return chars_ptr with
-            Import => True,
-            Convention => C,
-            External_Name => "haveAdaOrderRequirements";
-      begin
-         return Value(Item => Have_Ada_Order_Requirements);
-      end Have_Order_Requirements;
+      Nim_Message: chars_ptr;
+      Result: Natural := 0;
+      function Move_Ada_Ship
+        (Nx, Ny: Integer; Msg: out chars_ptr) return Natural with
+         Import => True,
+         Convention => C,
+         External_Name => "moveAdaShip";
    begin
-      case Player_Ship.Speed is
-         when DOCKED =>
-            Message :=
-              To_Unbounded_String
-                (Source => "First you must undock your ship from the base.");
-            return 0;
-         when FULL_STOP =>
-            Message :=
-              To_Unbounded_String
-                (Source => "First you must set the speed of your ship.");
-            return 0;
-         when others =>
-            null;
-      end case;
-      Message := To_Unbounded_String(Source => Have_Order_Requirements);
-      if Length(Source => Message) > 0 then
-         return 0;
-      end if;
-      Fuel_Index :=
-        Find_Item(Inventory => Player_Ship.Cargo, Item_Type => Fuel_Type);
-      if Fuel_Index = 0 then
-         Message := To_Unbounded_String(Source => "You don't have any fuel.");
-         return 0;
-      end if;
-      Fuel_Needed := Count_Fuel_Needed;
-      if Inventory_Container.Element
-          (Container => Player_Ship.Cargo, Index => Fuel_Index)
-          .Amount <
-        abs Fuel_Needed then
-         Message :=
-           To_Unbounded_String
-             (Source =>
-                "You don't have enough fuel (" &
-                To_String
-                  (Source =>
-                     Get_Proto_Item
-                       (Index =>
-                          Inventory_Container.Element
-                            (Container => Player_Ship.Cargo,
-                             Index => Fuel_Index)
-                            .Proto_Index)
-                       .Name) &
-                ").");
-         return 0;
-      end if;
-      --## rule off SIMPLIFIABLE_EXPRESSIONS
-      Speed := (Speed_Type(Real_Speed(Ship => Player_Ship)) / 1_000.0);
-      --## rule on SIMPLIFIABLE_EXPRESSIONS
-      if Speed < 0.5 then
-         Message :=
-           To_Unbounded_String
-             (Source => "You can't fly because your ship is overloaded.");
-         return 0;
-      end if;
-      New_X := Player_Ship.Sky_X + X;
-      New_Y := Player_Ship.Sky_Y + Y;
-      if New_X < 1 or New_X > 1_024 or New_Y < 1 or New_Y > 1_024 then
-         return 0;
-      end if;
-      Player_Ship.Sky_X := New_X;
-      Player_Ship.Sky_Y := New_Y;
-      Update_Cargo
-        (Ship => Player_Ship,
-         Proto_Index =>
-           Inventory_Container.Element
-             (Container => Player_Ship.Cargo, Index => Fuel_Index)
-             .Proto_Index,
-         Amount => Fuel_Needed);
-      Time_Passed := Integer(100.0 / Speed);
-      if Time_Passed > 0 then
-         case Player_Ship.Speed is
-            when QUARTER_SPEED =>
-               if Time_Passed < 60 then
-                  Time_Passed := 60;
-               end if;
-            when HALF_SPEED =>
-               if Time_Passed < 30 then
-                  Time_Passed := 30;
-               end if;
-            when FULL_SPEED =>
-               if Time_Passed < 15 then
-                  Time_Passed := 15;
-               end if;
-            when others =>
-               null;
-         end case;
-         Get_Game_Stats(Value => 1, Stat => 0);
-         Update_Game(Minutes => Time_Passed);
-         Fuel_Index :=
-           Find_Item(Inventory => Player_Ship.Cargo, Item_Type => Fuel_Type);
-         if Fuel_Index = 0 then
-            Add_Message
-              (Message => "Ship falls from the sky due to a lack of fuel.",
-               M_Type => OTHERMESSAGE, Color => RED);
-            Death
-              (Member_Index => 1,
-               Reason => To_Unbounded_String(Source => "fall of the ship"),
-               Ship => Player_Ship);
-            return 0;
-         end if;
-      end if;
-      if not Get_Faction(Index => Player_Ship.Crew(1).Faction).Flags.Contains
-          (Item => To_Unbounded_String(Source => "sentientships")) then
-         if Need_Rest(Order => PILOT) then
-            if not Get_Boolean_Setting(Name => "autoRest") then
-               return 6;
-            end if;
-            return 8;
-         end if;
-         if Need_Rest(Order => ENGINEER) then
-            if not Get_Boolean_Setting(Name => "autoRest") then
-               return 7;
-            end if;
-            return 8;
-         end if;
-      end if;
-      return 1;
+      Get_Game_Date;
+      Set_Ship_In_Nim;
+      Get_Ada_Map_Cell
+        (X => Player_Ship.Sky_X, Y => Player_Ship.Sky_Y,
+         Base_Index =>
+           Sky_Map(Player_Ship.Sky_X, Player_Ship.Sky_Y).Base_Index,
+         Visited =>
+           (if Sky_Map(Player_Ship.Sky_X, Player_Ship.Sky_Y).Visited then 1
+            else 0),
+         Event_Index =>
+           Sky_Map(Player_Ship.Sky_X, Player_Ship.Sky_Y).Event_Index,
+         Mission_Index =>
+           Sky_Map(Player_Ship.Sky_X, Player_Ship.Sky_Y).Mission_Index);
+      Result := Move_Ada_Ship(Nx => X, Ny => Y, Msg => Nim_Message);
+      Message := To_Unbounded_String(Source => Value(Item => Nim_Message));
+      Get_Ship_From_Nim(Ship => Player_Ship);
+      Set_Game_Date;
+      Set_Map_Cell(X => Player_Ship.Sky_X, Y => Player_Ship.Sky_Y);
+      return Result;
    end Move_Ship;
 
    function Dock_Ship
      (Docking: Boolean; Escape: Boolean := False) return String is
       use Bases;
-      use Maps;
       function Dock_Ada_Ship(D, E: Integer) return chars_ptr with
          Import => True,
          Convention => C,
