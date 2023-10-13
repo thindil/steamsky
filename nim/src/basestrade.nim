9506ea659b0a9e9a58247f53365214bf56af5a78
thindil <thindil@laeran.pl>
2023-10-13 03:22:14 +0000

    refactor: continue work on move hireRecruit to Nim
    FossilOrigin-Name: 6fd8cbd151bd77f0595d35dcecee082a48d9862751fca913782f2861b8ededbf

--- nim/src/basestrade.nim
+++ nim/src/basestrade.nim
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
+import std/tables
 import bases, game, crewinventory, maps, shipscrew, trades, types
 
 proc checkMoney(price: Positive; message: string = ""): Positive =
@@ -24,7 +25,8 @@ proc checkMoney(price: Positive; message: string = ""): Positive =
   if playerShip.cargo[result].amount < price:
     raise newException(exceptn = NotEnoughMoneyError, message = message)
 
-proc hireRecruit*(recruitIndex: Natural; cost: Positive; dailyPayment, tradePayment: Natural, contractLength: int) =
+proc hireRecruit*(recruitIndex: Natural; cost: Positive; dailyPayment,
+    tradePayment: Natural; contractLength: int) =
   let traderIndex = findMember(order = talk)
   if traderIndex == -1:
     raise newException(exceptn = NoTraderError, message = "")
@@ -33,4 +35,23 @@ proc hireRecruit*(recruitIndex: Natural; cost: Positive; dailyPayment, tradePaym
   let
     baseIndex = skyMap[playerShip.skyX][playerShip.skyY].baseIndex
     recruit = skyBases[baseIndex].recruits[recruitIndex]
+  var inventory: seq[InventoryData]
+  for item in recruit.inventory:
+    inventory.add(InventoryData(protoIndex: item, amount: 1, name: "",
+        durability: defaultItemDurability, price: 0))
+  var morale: Natural = 0
+  if "nomorale" in factionsList[skyBases[baseIndex].owner].flags:
+    morale = 50
+  else:
+    morale = 50 + skyBases[baseIndex].reputation.level
+    if morale > 100:
+      morale = 100
+  playerShip.crew.add(MemberData(name: recruit.name, gender: recruit.gender,
+      health: 100, tired: 0, skills: recruit.skills, hunger: 0, thirst: 0,
+      order: rest, previousOrder: rest, orderTime: 15,
+      attributes: recruit.attributes, inventory: inventory,
+      equipment: recruit.equipment, payment: [1: dailyPayment, 2: tradePayment],
+      contractLength: contractLength, morale: [1: morale, 2: 0],
+      loyalty: morale, homeBase: recruit.homeBase, faction: recruit.faction))
+  let
     moneyIndex2 = checkMoney(price = price, message = recruit.name)
