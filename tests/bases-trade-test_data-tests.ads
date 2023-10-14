e8ac1725c45292d48294641d8f4bf62440ac8d7f
thindil <thindil@laeran.pl>
2023-10-14 03:04:04 +0000

    tests: updated unit tests
    FossilOrigin-Name: 9232f1b7ead5ca0c7fcef5c4575c1e785098577fca60208e7d6406141b1aafa7

--- tests/bases-trade-test_data-tests.ads
+++ tests/bases-trade-test_data-tests.ads
@@ -10,23 +10,20 @@ package Bases.Trade.Test_Data.Tests is
      .Test with
    null record;
 
-   procedure Test_Hire_Recruit_6d7d9a_6a9998(Gnattest_T: in out Test);
-   --  bases-trade.ads:52:4:Hire_Recruit:Test_HireRecruit
-
    procedure Test_Buy_Recipe_f9dd3c_e2270a(Gnattest_T: in out Test);
-   --  bases-trade.ads:64:4:Buy_Recipe:Test_BuyRecipe
+   --  bases-trade.ads:63:4:Buy_Recipe:Test_BuyRecipe
 
    procedure Test_Heal_Wounded_aad41c_6975e9(Gnattest_T: in out Test);
-   --  bases-trade.ads:76:4:Heal_Wounded:Test_HealWounded
+   --  bases-trade.ads:75:4:Heal_Wounded:Test_HealWounded
 
    procedure Test_Heal_Cost_115ffd_52d375(Gnattest_T: in out Test);
-   --  bases-trade.ads:92:4:Heal_Cost:Test_HealCost
+   --  bases-trade.ads:91:4:Heal_Cost:Test_HealCost
 
    procedure Test_Train_Cost_7284bc_f49c70(Gnattest_T: in out Test);
-   --  bases-trade.ads:111:4:Train_Cost:Test_TrainCost
+   --  bases-trade.ads:110:4:Train_Cost:Test_TrainCost
 
    procedure Test_Train_Skill_30157e_5fedf8(Gnattest_T: in out Test);
-   --  bases-trade.ads:130:4:Train_Skill:Test_TrainSkill
+   --  bases-trade.ads:129:4:Train_Skill:Test_TrainSkill
 
 end Bases.Trade.Test_Data.Tests;
 --  end read only
