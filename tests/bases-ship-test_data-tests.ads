0823ec7eba390eea7131b9621e074e275f3cede1
thindil <thindil@laeran.pl>
2023-10-23 03:03:35 +0000

    tests: updated unit tests
    FossilOrigin-Name: a9345a1a83e137a73eed6cfe99d051d21c300be50616d59c24e73891a4081d52

--- tests/bases-ship-test_data-tests.ads
+++ tests/bases-ship-test_data-tests.ads
@@ -10,11 +10,8 @@ package Bases.Ship.Test_Data.Tests is
      .Test with
    null record;
 
-   procedure Test_Repair_Ship_a28a55_b1413a(Gnattest_T: in out Test);
-   --  bases-ship.ads:60:4:Repair_Ship:Test_RepairShip
-
    procedure Test_Upgrade_Ship_5494ec_bcac9d(Gnattest_T: in out Test);
-   --  bases-ship.ads:73:4:Upgrade_Ship:Test_UpdgradeShip
+   --  bases-ship.ads:72:4:Upgrade_Ship:Test_UpdgradeShip
 
 end Bases.Ship.Test_Data.Tests;
 --  end read only
