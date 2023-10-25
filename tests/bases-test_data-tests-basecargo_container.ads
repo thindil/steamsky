22f7db35493d9f5e26dbc80c303c6d1d306fb790
thindil <thindil@laeran.pl>
2023-10-25 03:07:35 +0000

    tests: updated unit tests
    FossilOrigin-Name: 43bfd8371d1d56fd76a96d34348f0d1d2ee1cdd77cc312590d4502ac135d06e9

--- tests/bases-test_data-tests-basecargo_container.ads
+++
@@ -1,2 +0,0 @@
-package Bases.Test_Data.Tests.BaseCargo_Container is
-end Bases.Test_Data.Tests.BaseCargo_Container;
--- tests/bases-test_data-tests-recruit_container.ads
+++
@@ -1,2 +0,0 @@
-package Bases.Test_Data.Tests.Recruit_Container is
-end Bases.Test_Data.Tests.Recruit_Container;
