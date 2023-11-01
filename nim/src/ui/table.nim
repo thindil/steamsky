53de5f53ca75547f438119476cf100a5c03d8336
thindil <thindil@laeran.pl>
2023-11-01 04:21:23 +0000

    refactor: updated code to the new version of tclEval procedures
    FossilOrigin-Name: 446578ff8884584447bfc7ba1d02c7b576936ec1452654f4f27a65f86537ad38

--- nim/src/ui/table.nim
+++ nim/src/ui/table.nim
@@ -48,81 +48,80 @@ proc createTable*(parent: string; headers: HeadersList; scrollbar: string = ".";
   ## * tooltipText - the text to show when the player hover mouse above the table
   ##
   ## Returns the newly created TableWidget with information about the table
-  let interp = getInterp()
   result = TableWidget(canvas: parent & ".table")
   if scrollbar == ".":
     result.scrollbar = parent & ".scrolly"
-    interp.tclEval("ttk::scrollbar " & result.scrollbar &
+    tclEval(script = "ttk::scrollbar " & result.scrollbar &
         " -orient vertical -command [list " & result.canvas & " yview]")
-    interp.tclEval("ttk::scrollbar " & parent &
+    tclEval(script = "ttk::scrollbar " & parent &
         ".scrollx -orient horizontal -command [list " & result.canvas & " xview]")
-    interp.tclEval("canvas " & result.canvas & " -yscrollcommand [list " &
+    tclEval(script = "canvas " & result.canvas & " -yscrollcommand [list " &
         result.scrollbar & " set] -xscrollcommand [list " & parent & ".scrollx set]")
-    interp.tclEval("pack " & result.scrollbar & " -side right -fill y")
-    interp.tclEval("pack " & result.canvas & " -side top -fill both -padx {5 0}")
-    interp.tclEval("pack " & parent & ".scrollx -side bottom -fill x")
-    interp.tclEval("::autoscroll::autoscroll " & parent & ".scrollx")
-    interp.tclEval("::autoscroll::autoscroll " & result.scrollbar)
+    tclEval(script = "pack " & result.scrollbar & " -side right -fill y")
+    tclEval(script = "pack " & result.canvas & " -side top -fill both -padx {5 0}")
+    tclEval(script = "pack " & parent & ".scrollx -side bottom -fill x")
+    tclEval(script = "::autoscroll::autoscroll " & parent & ".scrollx")
+    tclEval(script = "::autoscroll::autoscroll " & result.scrollbar)
   else:
     result.scrollbar = scrollbar
-    interp.tclEval("canvas " & result.canvas)
-    interp.tclEval("grid " & result.canvas & " -sticky nwes -padx {5 0}")
-    interp.tclEval("grid columnconfigure " & parent & " " & result.canvas & " -weight 1")
-    interp.tclEval("grid rowconfigure " & parent & " " & result.canvas & " -weight 1")
+    tclEval(script = "canvas " & result.canvas)
+    tclEval(script = "grid " & result.canvas & " -sticky nwes -padx {5 0}")
+    tclEval(script = "grid columnconfigure " & parent & " " & result.canvas & " -weight 1")
+    tclEval(script = "grid rowconfigure " & parent & " " & result.canvas & " -weight 1")
   var x = 0
-  let headerColor = interp.tclEval2("ttk::style lookup Table -headerforecolor")
-  let headerBackColor = interp.tclEval2("ttk::style lookup Table -headerbackcolor")
-  let borderStyle = interp.tclEval2("ttk::style lookup Table -headerbordercolor")
+  let headerColor = tclEval2(script = "ttk::style lookup Table -headerforecolor")
+  let headerBackColor = tclEval2(script = "ttk::style lookup Table -headerbackcolor")
+  let borderStyle = tclEval2(script = "ttk::style lookup Table -headerbordercolor")
   for index, header in headers:
-    let headerId = interp.tclEval2(result.canvas & " create text " & $x &
+    let headerId = tclEval2(script = result.canvas & " create text " & $x &
         " 2 -anchor nw -text {" & header &
         "} -font InterfaceFont -justify center -fill " & headerColor &
         " -tags [list header" & $(index + 1) & "]")
     if command.len > 0:
-      interp.tclEval(result.canvas & " bind " & headerId & " <Enter> {" &
+      tclEval(script = result.canvas & " bind " & headerId & " <Enter> {" &
           result.canvas & " configure -cursor hand1}")
-      interp.tclEval(result.canvas & " bind " & headerId & " <Leave> {" &
+      tclEval(script = result.canvas & " bind " & headerId & " <Leave> {" &
           result.canvas & " configure -cursor left_ptr}")
-      interp.tclEval(result.canvas & " bind " & headerId & " <Button-1> {" &
+      tclEval(script = result.canvas & " bind " & headerId & " <Button-1> {" &
           command & " %x}")
     if tooltipText.len > 0:
-      interp.tclEval("tooltip::tooltip " & result.canvas & " -item " &
+      tclEval(script = "tooltip::tooltip " & result.canvas & " -item " &
           headerId & " \"" & tooltipText & "\"")
     let
-      tclResult = interp.tclEval2(result.canvas & " bbox header" & $(index + 1))
+      tclResult = tclEval2(script = result.canvas & " bbox header" & $(index + 1))
       coords = tclResult.split
     var oldX = x - 5
     x = coords[2].parseInt + 5
     result.columnsWidth.add(x - coords[0].parseInt)
     if index == 0:
       result.rowHeight = coords[3].parseInt + 5
-    let backgroundId = interp.tclEval2(result.canvas & " create rectangle " &
+    let backgroundId = tclEval2(script = result.canvas & " create rectangle " &
         $oldX & " 0 " & $(x - 2) & " " & $(result.rowHeight - 3) & " -fill " &
             headerBackColor &
         " -outline " & $borderStyle & " -width 2 -tags [list headerback" &
         $(index + 1) & "]")
-    interp.tclEval(result.canvas & " lower headerback" & $(index + 1))
+    tclEval(script = result.canvas & " lower headerback" & $(index + 1))
     if command.len > 0:
-      interp.tclEval(result.canvas & " bind " & backgroundId & " <Enter> {" &
+      tclEval(script = result.canvas & " bind " & backgroundId & " <Enter> {" &
           result.canvas & " configure -cursor hand1}")
-      interp.tclEval(result.canvas & " bind " & backgroundId & " <Leave> {" &
+      tclEval(script = result.canvas & " bind " & backgroundId & " <Leave> {" &
           result.canvas & " configure -cursor left_ptr}")
-      interp.tclEval(result.canvas & " bind " & backgroundId & " <Button-1> {" &
-          command & " %x}")
+      tclEval(script = result.canvas & " bind " & backgroundId &
+          " <Button-1> {" & command & " %x}")
     if tooltipText.len > 0:
-      interp.tclEval("tooltip::tooltip " & result.canvas & " -item " &
+      tclEval(script = "tooltip::tooltip " & result.canvas & " -item " &
           backgroundId & " \"" & tooltipText & "\"")
-  interp.tclEval("SetScrollBarBindings " & result.canvas & " " &
+  tclEval(script = "SetScrollBarBindings " & result.canvas & " " &
       result.scrollbar)
-  interp.tclEval("bind " & result.canvas & " <Up> {UpdateCurrentRow " &
+  tclEval(script = "bind " & result.canvas & " <Up> {UpdateCurrentRow " &
       result.canvas & " lower}")
-  interp.tclEval("bind " & result.canvas & " <Down> {UpdateCurrentRow " &
+  tclEval(script = "bind " & result.canvas & " <Down> {UpdateCurrentRow " &
       result.canvas & " raise}")
-  interp.tclEval("bind " & result.canvas & " <Key-space> {ExecuteCurrentRow " &
+  tclEval(script = "bind " & result.canvas &
+      " <Key-space> {ExecuteCurrentRow " & result.canvas & "}")
+  tclEval(script = "bind " & result.canvas & " <FocusOut> {HideCurrentRow " &
       result.canvas & "}")
-  interp.tclEval("bind " & result.canvas & " <FocusOut> {HideCurrentRow " &
-      result.canvas & "}")
-  interp.tclEval("bind " & result.canvas & " <Leave> {HideCurrentRow " &
+  tclEval(script = "bind " & result.canvas & " <Leave> {HideCurrentRow " &
       result.canvas & "}")
 
 proc clearTable*(table: var TableWidget) {.sideEffect, raises: [], tags: [].} =
@@ -131,19 +130,17 @@ proc clearTable*(table: var TableWidget) {.sideEffect, raises: [], tags: [].} =
   ## * table - the TableWidget which data will be cleared
   ##
   ## Returns the modified parameter table
-  let
-    buttonsFrame = table.canvas & ".buttonframe"
-    interp = getInterp()
-  if interp.tclEval2("winfo exists " & buttonsFrame) == "1":
-    interp.tclEval("destroy " & buttonsFrame & ".previous")
-    interp.tclEval("destroy " & buttonsFrame & ".next")
-    interp.tclEval("destroy " & buttonsFrame)
+  let buttonsFrame = table.canvas & ".buttonframe"
+  if tclEval2(script = "winfo exists " & buttonsFrame) == "1":
+    tclEval(script = "destroy " & buttonsFrame & ".previous")
+    tclEval(script = "destroy " & buttonsFrame & ".next")
+    tclEval(script = "destroy " & buttonsFrame)
   for row in 1 .. table.row:
     for column in 1 .. table.columnsWidth.len:
-      interp.tclEval(table.canvas & " delete row" & $row & "col" & $column)
-      interp.tclEval(table.canvas & " delete row" & $row)
-      interp.tclEval(table.canvas & " delete progressbar" & $row & "back" & $column)
-      interp.tclEval(table.canvas & " delete progressbar" & $row & "bar" & $column)
+      tclEval(script = table.canvas & " delete row" & $row & "col" & $column)
+      tclEval(script = table.canvas & " delete row" & $row)
+      tclEval(script = table.canvas & " delete progressbar" & $row & "back" & $column)
+      tclEval(script = table.canvas & " delete progressbar" & $row & "bar" & $column)
   table.row = 1
 
 proc addBindings(canvas, itemId, row, command, color: string) {.sideEffect,
@@ -154,15 +151,14 @@ proc addBindings(canvas, itemId, row, command, color: string) {.sideEffect,
   ## * itemId  - the Id of the item to which the events will be added
   ## * row     - the number of the row in which the events will be added
   ## * command - the Tcl command which will be executed on mouse button event
-  let interp = getInterp()
-  interp.tclEval(canvas & " bind " & itemId & " <Enter> {" & canvas &
+  tclEval(script = canvas & " bind " & itemId & " <Enter> {" & canvas &
       " itemconfigure row$currentrow -fill " & color & ";" & canvas &
-      " itemconfigure row" & row & " -fill " & interp.tclEval2(
-      "ttk::style lookup -selectbackground") & (if command.len > 0: ";" &
-      canvas & " configure -cursor hand1" else: "") & ";set currentrow " & row & "}")
-  interp.tclEval(canvas & " bind " & itemId & " <Leave> {" & canvas & " configure -cursor left_ptr}")
+      " itemconfigure row" & row & " -fill " & tclEval2(script =
+    "ttk::style lookup -selectbackground") & (if command.len > 0: ";" &
+    canvas & " configure -cursor hand1" else: "") & ";set currentrow " & row & "}")
+  tclEval(script = canvas & " bind " & itemId & " <Leave> {" & canvas & " configure -cursor left_ptr}")
   if command.len > 0:
-    interp.tclEval(canvas & " bind " & itemId & " <Button-" & (
+    tclEval(script = canvas & " bind " & itemId & " <Button-" & (
         if gameSettings.rightButton: "3" else: "1") & "> {" & command & "}")
 
 # Temporary code for interfacing with Ada
