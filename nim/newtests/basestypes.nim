1f60e155cefd567bcdd12536956ee4b608eda355
thindil <thindil@laeran.pl>
2023-11-04 04:13:14 +0000

    tests: rewritten unit tests for basestypes module
    FossilOrigin-Name: 3ccb6ff511e0e207a0e0883b9873a2fe77ae78539c4ec1f93cc5dafb287612b4

---
+++ nim/newtests/basestypes.nim
@@ -0,0 +1,24 @@
+import ../src/[basestypes, game, items]
+import unittest2
+
+suite "Unit tests for basestypes module":
+  checkpoint "Loading the game data."
+  loadData("../bin/data/game.dat")
+  loadItems("../bin/data/items.dat")
+  loadBasesTypes("../bin/data/bases.dat")
+
+  test "Testing getPrice.":
+    checkpoint "Get the price of a non-buyable item."
+    check:
+      getPrice("0", 1) == 0
+    checkpoint "Get the price of a buyable item."
+    check:
+      getPrice("1", 2) > 0
+
+  test "Testing isBuyable.":
+    checkpoint "Check if the item is non-buyable."
+    check:
+      not isBuyable("0", 1)
+    checkpoint "Check if the item is buyable."
+    check:
+      isBuyable("1", 2)
