89cba3cf80054b883c28ce1b36a967d1cb7d4c5a
thindil <thindil@laeran.pl>
2023-11-10 04:10:39 +0000

    tests: rewritten unit tests for events module
    FossilOrigin-Name: fc9bba24cd00bef2be360c0929dcd3285c620e653c2028be544ff2f545c79c40

---
+++ nim/newtests/events.nim
@@ -0,0 +1,60 @@
+import ../src/[careers, crafts, events, factions, game, items, maps, mobs, ships, shipmodules, types]
+import unittest2
+
+suite "Unit tests for events module":
+
+  checkpoint "Loading the game data."
+  loadData("../bin/data/game.dat")
+  loadItems("../bin/data/items.dat")
+  loadModules("../bin/data/shipmodules.dat")
+  loadCareers("../bin/data/careers.dat")
+  loadFactions("../bin/data/factions.dat")
+  loadRecipes("../bin/data/recipes.dat")
+  loadMobs("../bin/data/mobs.dat")
+  loadShips("../bin/data/ships.dat")
+
+  playerShip.skyX = 1
+  playerShip.skyY = 1
+  playerShip.crew = @[]
+  playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural],
+      homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 2, 1, 1,
+      1, 0, 0], order: talk, loyalty: 100, skills: @[SkillInfo(index: 4, level: 4,
+      experience: 0)], attributes: @[MobAttributeRecord(level: 3, experience: 0),
+      MobAttributeRecord(level: 3, experience: 0), MobAttributeRecord(level: 3,
+      experience: 0), MobAttributeRecord(level: 3, experience: 0)]))
+  playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural],
+      homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 0, 1, 1,
+      1, 0, 0], order: gunner, loyalty: 100))
+  playerShip.modules = @[]
+  playerShip.modules.add(ModuleData(mType: ModuleType2.armor, protoIndex: 57,
+      durability: 100))
+  playerShip.modules.add(ModuleData(mType: ModuleType2.turret, protoIndex: 86,
+      durability: 100))
+  playerShip.modules.add(ModuleData(mType: ModuleType2.gun, protoIndex: 160,
+      durability: 100, damage: 100))
+  skyMap[1][1].baseIndex = 1
+  eventsList = @[]
+
+  test "Testing generateEnemies.":
+    var enemies: seq[Positive] = @[]
+    generateEnemies(enemies)
+    check:
+      enemies.len > 0
+
+  test "Testing updateEvents.":
+    updateEvents(1)
+
+  test "Testing deleteEvent.":
+    eventsList.add(EventData(eType: doublePrice, skyX: 1, skyY: 1, itemIndex: 1, time: 10))
+    deleteEvent(0)
+    check:
+      eventsList.len == 0
+
+  test "Testing recoverBase.":
+    skyBases[2].population = 0
+    recoverBase(2)
+    check:
+      skyBases[2].population > 0
+
+  test "Testing generateTraders.":
+    generateTraders()
