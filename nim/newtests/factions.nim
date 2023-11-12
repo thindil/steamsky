d5d2dfb30aba54a075f248f230b376fdba4e00d8
thindil <thindil@laeran.pl>
2023-11-12 04:03:58 +0000

    tests: rewritten unit tests for factions module
    FossilOrigin-Name: fb386fba1db63d19025a5c6997cd0f38362742ccf0836bdf99797ecf3928f3d5

---
+++ nim/newtests/factions.nim
@@ -0,0 +1,36 @@
+import std/tables
+import ../src/[factions, careers, game, items]
+import unittest2
+
+suite "Unit tests for factions module":
+
+  checkpoint "Loading the game data."
+  loadData("../bin/data/game.dat")
+  loadItems("../bin/data/items.dat")
+  loadCareers("../bin/data/careers.dat")
+  loadFactions("../bin/data/factions.dat")
+
+  test "Testing getReputation.":
+    checkpoint "Gaining reputation in the same faction."
+    check:
+      getReputation("POLEIS", "POLEIS") == 0
+    checkpoint "Gaining reputation in enemy faction."
+    check:
+      getReputation("POLEIS", "PIRATES") == -10
+
+  test "Testing isFriendly.":
+    checkpoint "Checking if friendly factions are friendly"
+    check:
+      isFriendly("POLEIS", "INDEPENDENT")
+    checkpoint "Checking if enemy factions are unfriendly"
+    check:
+      not isFriendly("POLEIS", "PIRATES")
+
+  test "Testing getRandomFaction.":
+    let factionIndex = getRandomFaction()
+    checkpoint "Get random faction index"
+    check:
+      factionIndex.len > 0
+    checkpoint "Get existing random faction index"
+    check:
+      factionIndex in factionsList
