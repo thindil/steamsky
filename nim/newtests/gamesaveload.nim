0d3c966cbc3a7d970a8c4d900ad92ce2fa230f89
thindil <thindil@laeran.pl>
2023-11-15 04:09:00 +0000

    tests: rewritten unit tests for gamesaveload module
    FossilOrigin-Name: 5cc9158c74508e5a00b913074a4d61af834d6e93610c4c96f3b5b805c0edcbb3

---
+++ nim/newtests/gamesaveload.nim
@@ -0,0 +1,19 @@
+import ../src/[game, gamesaveload, types]
+import unittest2
+
+suite "Unit tests for gamesaveload module":
+
+  playerShip.crew = @[]
+  playerShip.crew.add(MemberData(name: "Laeran", morale: [1: 50.Natural, 2: 0.Natural],
+      homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 2, 1, 1,
+      1, 0, 0], order: talk, loyalty: 100, skills: @[SkillInfo(index: 4, level: 4,
+      experience: 0)], attributes: @[MobAttributeRecord(level: 3, experience: 0),
+      MobAttributeRecord(level: 3, experience: 0), MobAttributeRecord(level: 3,
+      experience: 0), MobAttributeRecord(level: 3, experience: 0)]))
+  playerShip.name = "Anaria"
+
+  test "Testing generateSaveName.":
+    let oldName = saveName
+    generateSaveName()
+    check:
+      oldName != saveName
