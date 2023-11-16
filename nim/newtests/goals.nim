57ad0ba0104519641f50cfdfe01effc954234a70
thindil <thindil@laeran.pl>
2023-11-16 04:13:29 +0000

    tests: rewritten unit tests for goals module
    FossilOrigin-Name: 518608954f649c024f89ef8702cfc1c56405faf2cd84aa9ec79c540edf4a19bd

---
+++ nim/newtests/goals.nim
@@ -0,0 +1,32 @@
+import std/tables
+import ../src/[game, goals, types]
+import unittest2
+
+suite "Unit tests for goals module":
+
+  checkpoint "Loading the game data."
+  loadGoals("../bin/data/goals.dat")
+
+  currentGoal = goalsList[2]
+
+  test "Testing updateGoal.":
+    var amount = currentGoal.amount
+    checkpoint "Updating the current goal"
+    updateGoal(GoalTypes.destroy, "PIRATES", 1)
+    check:
+      currentGoal.amount == (amount - 1)
+    checkpoint "Not updating the current goal"
+    amount = currentGoal.amount
+    updateGoal(reputation, "PIRATES", 1)
+    check:
+      currentGoal.amount == amount
+
+  test "Testing clearCurrentGoal.":
+    clearCurrentGoal()
+    check:
+      currentGoal.index.len == 0
+
+  test "Testing goalText.":
+    currentGoal = goalsList[1]
+    check:
+      goalText(1) == "Gain max reputation in 1 base"
