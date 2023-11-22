5f330d41ca6173aacf342a9a6050698f93d63e9c
thindil <thindil@laeran.pl>
2023-11-22 04:02:56 +0000

    tests: rewritten unit tests for missions module
    FossilOrigin-Name: 62db9d6d12232d8a1c2e5b59c5e7c30b3ec1614d702c590af1ef56f6d6c1ea09

---
+++ nim/newtests/missions.nim
@@ -0,0 +1,70 @@
+import ../src/[careers, factions, game, items, maps, missions, shipmodules, types]
+import unittest2
+
+suite "Unit tests for missions module":
+
+  checkpoint "Loading the game data."
+  loadData("../bin/data/game.dat")
+  loadItems("../bin/data/items.dat")
+  loadCareers("../bin/data/careers.dat")
+  loadFactions("../bin/data/factions.dat")
+  loadModules("../bin/data/shipmodules.dat")
+
+  playerShip.skyX = 1
+  playerShip.skyY = 1
+  playerShip.crew = @[]
+  playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural],
+      homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 2, 1,
+      1, 1, 0, 0], order: talk, loyalty: 100, skills: @[SkillInfo(index: 4,
+      level: 4, experience: 0)], attributes: @[MobAttributeRecord(level: 3,
+      experience: 0), MobAttributeRecord(level: 3, experience: 0),
+      MobAttributeRecord(level: 3, experience: 0), MobAttributeRecord(level: 3,
+      experience: 0)]))
+  playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural],
+      homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 0, 1, 1,
+      1, 0, 0], order: gunner, loyalty: 100))
+  playerShip.modules = @[]
+  playerShip.modules.add(ModuleData(mType: cargoRoom, protoIndex: 7,
+      durability: 100))
+  playerShip.cargo = @[]
+  playerShip.cargo.add(InventoryData(protoIndex: 1, amount: 100,
+      durability: 100))
+  skyMap[1][1].baseIndex = 1
+  skyBases[1] = BaseRecord(skyX: 1, skyY: 1)
+
+  test "Testing deleteMission.":
+    acceptedMissions = @[]
+    acceptedMissions.add(y = MissionData(mType: explore, time: 1, targetX: 1,
+        targetY: 1, reward: 1, startBase: 1, finished: true, multiplier: 0.0, target: 0))
+    deleteMission(0, false)
+    check:
+      acceptedMissions.len == 0
+
+  test "Testing generateMissions.":
+    skyBases[1].missionsDate = DateRecord(year: 0, month: 0, day: 0, hour: 0, minutes: 0)
+    generateMissions()
+
+  test "Testing updateMission.":
+    acceptedMissions = @[]
+    acceptedMissions.add(y = MissionData(mType: explore, time: 10, targetX: 1,
+        targetY: 1, reward: 1, startBase: 1, finished: true, multiplier: 0.0, target: 0))
+    checkpoint "Update accepted missions"
+    updateMissions(8)
+    check:
+      acceptedMissions[0].time == 2
+    checkpoint "Remove accepted mission"
+    updateMissions(2)
+    check:
+      acceptedMissions.len == 0
+
+  test "Testing getMissionType.":
+    check:
+      getMissionType(patrol) == "Patrol area"
+
+  test "Testing updateMission.":
+    acceptedMissions = @[]
+    acceptedMissions.add(y = MissionData(mType: explore, time: 10, targetX: 1,
+        targetY: 1, reward: 1, startBase: 1, finished: true, multiplier: 0.0, target: 0))
+    updateMission(0)
+    check:
+      acceptedMissions[0].finished
