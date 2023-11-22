2a2a8ae0ed988c2d30683194fa6bccef8686fe44
thindil <thindil@laeran.pl>
2023-11-22 04:05:56 +0000

    refactor: removed unneeded code
    FossilOrigin-Name: c4e34a2410e3246fbf92dd862ce37736f63c5b90dde72bf3ac947f1bb2506f70

--- nim/src/ui/utilsui.nim
+++ nim/src/ui/utilsui.nim
@@ -19,25 +19,6 @@ import std/[tables, strutils]
 import ../../src/[bases, config, game, shipscargo, shipscrew, tk, types]
 import coreui, shipsuimodules
 
-type AddingCommandError* = object of CatchableError
-  ## Raised when there is problem with adding a Tcl command
-
-proc addCommand*(name: string; nimProc: TclCmdProc) {.sideEffect, raises: [
-    AddingCommandError], tags: [].} =
-  ## Add the selected Nim procedure as a Tcl command.
-  ##
-  ## * name    - the name of the Tcl command
-  ## * nimProc - the Nim procedure which will be executed as the Tcl command
-  ##
-  ## Raises AddingCommandError exception if the command can't be added.
-  if tclEval2(script = "info commands " & name).len > 0:
-    raise newException(exceptn = AddingCommandError,
-        message = "Command with name " & name & " exists.")
-  if tclCreateCommand(interp = getInterp(), cmdName = name.cstring,
-      cproc = nimProc, clientData = 0, deleteProc = nil) == nil:
-    raise newException(exceptn = AddingCommandError,
-        message = "Can't add command " & name)
-
 proc minutesToDate*(minutes: cint; infoText: var cstring) {.exportc, gcsafe,
     sideEffect, raises: [], tags: [].} =
   ## Convert the game minutes to the game time in days, hours, etc
