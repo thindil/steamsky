b152bf8230d93ac008382159327374975b9240f9
thindil <thindil@laeran.pl>
2023-11-30 04:39:37 +0000

    refactor: started work on move createDialog to Nim
    FossilOrigin-Name: 11451515ca150af268e623fb3019ce6014c782a89bd2fb74d7136890b37cf1fb

---
+++ nim/src/ui/dialogs.nim
@@ -0,0 +1,40 @@
+# Copyright 2023 Bartek thindil Jasicki
+#
+# This file is part of Steam Sky.
+#
+# Steam Sky is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Steam Sky is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
+
+import ../tk
+import coreui
+
+var timerId: string = "" ## Id of the timer for auto close command
+
+proc createDialog*(name, title: string; titleWidth: Positive = 275;
+    columns: Positive = 1; parentName: string = ".gameframe"): string =
+  if parentName == ".gameframe":
+    tclEval(script = "tk busy " & gameHeader)
+    tclEval(script = "tk busy " & mainPaned)
+  else:
+    tclEval(script = "tk busy " & parentName)
+  if timerId.len > 0:
+    tclEval(script = "after cancel " & timerId)
+    timerId = ""
+  tclEval(script = "update")
+  result = name
+  tclEval(script = "ttk::frame " & result & " -style Dialog.TFrame")
+  let dialogHeader = result & ".header"
+  tclEval(script = "ttk::label " & dialogHeader & " -text {" & title &
+      "} -wraplength " & $titleWidth & " -style Header.TLabel -cursor hand1")
+  tclEval(script = "grid " & dialogHeader & " -sticky we -padx 2 -pady {2 0}" &
+      (if columns > 1: " -columnspan " & $columns else: ""))
