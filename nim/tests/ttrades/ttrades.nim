180f3920640dcc61bbae4e89157d7968c9ef5d9e
thindil <thindil@laeran.pl>
2023-12-04 04:00:32 +0000

    tests: fixed unit tests for trades module
    FossilOrigin-Name: aaf34b2f61cbc0979edae9325e6428cadff0e3349a618539e26c828917043c3c

--- nim/tests/ttrades/ttrades.nim
+++ nim/tests/ttrades/ttrades.nim
@@ -16,6 +16,7 @@ if itemsList.len == 0:
   loadItems("../bin/data/items.dat")
   loadCareers("../bin/data/careers.dat")
   loadFactions("../bin/data/factions.dat")
+if modulesList.len == 0:
   loadModules("../bin/data/shipmodules.dat")
 if recipesList.len == 0:
   loadRecipes("../bin/data/recipes.dat")
@@ -37,7 +38,9 @@ playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural],
     experience: 0), MobAttributeRecord(level: 3, experience: 0)], health: 100))
 playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural],
     homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 0, 1, 1,
-    1, 0, 0], order: gunner, loyalty: 100, health: 100))
+    1, 0, 0], order: gunner, loyalty: 100, attributes: @[MobAttributeRecord(level: 3, experience: 0),
+    MobAttributeRecord(level: 3, experience: 0), MobAttributeRecord(level: 3,
+    experience: 0), MobAttributeRecord(level: 3, experience: 0)], health: 100))
 playerShip.modules = @[]
 playerShip.modules.add(ModuleData(mType: ModuleType2.hull, protoIndex: 1,
     durability: 100, maxModules: 10))
