6e3618c62856b0448723d77a294498bea379597d
thindil <thindil@laeran.pl>
2023-12-04 04:01:12 +0000

    style: removed mail from legal header
    FossilOrigin-Name: a3d41029917cf500005396a71555a2abb368bf2113c34c70d5061c021234e305

--- src/ui/knowledge-stories.ads
+++ src/ui/knowledge-stories.ads
@@ -1,4 +1,4 @@
--- Copyright (c) 2020-2023 Bartek thindil Jasicki <thindil@laeran.pl>
+-- Copyright (c) 2020-2023 Bartek thindil Jasicki
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
