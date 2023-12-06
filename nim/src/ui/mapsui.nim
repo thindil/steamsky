bcc8b749332c32a28c9ecf83e338c95a5763b8d5
thindil <thindil@laeran.pl>
2023-12-06 04:15:27 +0000

    refactor: started work on move updateHeaders to Nim
    FossilOrigin-Name: 21356e3490ff3670ab98e38dc55338ae0b1c478071288e778ced541a8921a243

--- nim/src/ui/mapsui.nim
+++ nim/src/ui/mapsui.nim
@@ -15,8 +15,35 @@
 # You should have received a copy of the GNU General Public License
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
-import ../tk
-import utilsui2
+import ../[config, game, messages, shipscargo, shipsmovement, tk]
+import coreui, utilsui2
+
+proc updateHeader*() =
+  var label = gameHeader & ".time"
+  tclEval(script = label & " configure -text {" & formattedTime() & "}")
+  if gameSettings.showNumbers:
+    tclEval(script = label & " configure -text {" & formattedTime() &
+        " Speed: " & $((realSpeed(ship = playerShip) * 60) / 1_000) & " km/h}")
+    tclEval(script = "tooltip::tooltip " & label & " \"Game time and current ship speed.\"")
+  label = gameHeader & ".nofuel"
+  tclEval(script = "grid remove " & label)
+  var itemAmount = getItemAmount(itemType = fuelType)
+  if itemAmount == 0:
+    tclEval(script = label & " configure -image nofuelicon")
+    tclEval(script = "tooltip::tooltip " & label & " \"You can't travel anymore, because you don't have any fuel for ship.\"")
+    tclEval(script = "grid " & label)
+  elif itemAmount <= gameSettings.lowFuel:
+    tclEval(script = label & " configure -image lowfuelicon")
+    tclEval(script = "tooltip::tooltip " & label &
+        " \"Low level of fuel on ship. Only " & $itemAmount & " left.\"")
+    tclEval(script = "grid " & label)
+  label = gameHeader & ".nodrink"
+  tclEval(script = "grid remove " & label)
+  itemAmount = getItemsAmount(iType = "Drinks")
+  if itemAmount == 0:
+    tclEval(script = label & " configure -image nodrinksicon")
+    tclEval(script = "tooltip::tooltip " & label & " \"You don't have any drinks in ship but your crew needs them to live..\"")
+    tclEval(script = "grid " & label)
 
 proc showSkyMap*(clear: bool = false) =
   tclSetVar(varName = "refreshmap", newValue = "1")
