da2007ca88236a64473e78764dcf9776bcc0d187
thindil <thindil@laeran.pl>
2023-12-06 03:58:47 +0000

    tests: rewritten unit tests for stories2 module
    FossilOrigin-Name: 39a7d4690edb5c687b54fae645c69ea3d859b3c19f2e2d6384ccb278120d204a

---
+++ nim/newtests/stories2.nim
@@ -0,0 +1,26 @@
+import ../src/[factions, careers, game, items, stories, stories2, types]
+import unittest2
+
+suite "Unit tests for stories2 module":
+
+  checkpoint "Loading the game data."
+  loadData("../bin/data/game.dat")
+  loadItems("../bin/data/items.dat")
+  loadCareers("../bin/data/careers.dat")
+  loadFactions("../bin/data/factions.dat")
+  loadStories("../bin/data/stories.dat")
+
+  playerShip.crew = @[]
+  playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural],
+      homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 2, 1, 1,
+      1, 0, 0], order: talk, loyalty: 100, skills: @[SkillInfo(index: 4, level: 4,
+      experience: 0)], attributes: @[MobAttributeRecord(level: 3, experience: 0),
+      MobAttributeRecord(level: 3, experience: 0), MobAttributeRecord(level: 3,
+      experience: 0), MobAttributeRecord(level: 3, experience: 0)]))
+  for i in 1 .. 1_000_000:
+    startStory("Undead", dropItem)
+    if currentStory.index.len > 0:
+      break
+
+  test "Testing progressStory.":
+    discard progressStory()
