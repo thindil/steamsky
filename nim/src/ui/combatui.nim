bb483fac9e553ad708bdafd56d3c168abe1e1b36
thindil <thindil@laeran.pl>
2023-12-25 04:09:32 +0000

    docs: added code documentation to nextTurnCommand procedure
    FossilOrigin-Name: 59dfe63c7529472f52f09f884cc8b63ceae2c902fc0b3bfb0e8915bc3f27df84

--- nim/src/ui/combatui.nim
+++ nim/src/ui/combatui.nim
@@ -552,12 +552,23 @@ proc updateBoardingUi() {.sideEffect, raises: [], tags: [].} =
 proc nextTurnCommand(clientData: cint; interp: PInterp; argc: cint;
     argv: openArray[cstring]): TclResults {.sideEffect, raises: [], tags: [
         WriteIOEffect, RootEffect].} =
+  ## Excecute the combat orders and go the next turn
+  ##
+  ## * clientData - the additional data for the Tcl command
+  ## * interp     - the Tcl interpreter on which the command was executed
+  ## * argc       - the amount of arguments entered for the command
+  ## * argv       - the list of the command's arguments
+  ##
+  ## The procedure always return tclOk
+  ##
+  ## Tcl:
+  ## NextTurn
   try:
     combatTurn()
   except:
     tclEval(script = "bgerror {Can't make next turn in combat, reason: " &
         getCurrentExceptionMsg() & "}")
-    return
+    return tclOk
   updateHeader()
   let combatFrame = mainPaned & ".combatframe"
   var frame = combatFrame & ".crew"
