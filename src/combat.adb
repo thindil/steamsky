88c78710dcfa5cb763ae1e9b08535bbf8891aff9
thindil <thindil@laeran.pl>
2023-12-30 04:23:05 +0000

    refactor: removed unused code
    FossilOrigin-Name: b7d7a5e99f04f54cd5439b1f92d408272ac57b8eb92f679ef8c3587a2e51fd14

--- src/combat.adb
+++ src/combat.adb
@@ -98,68 +98,19 @@ package body Combat is
       use Events;
       use Maps;
 
-      --## rule off TYPE_INITIAL_VALUES
-      type Nim_Orders_Array is array(0 .. 49) of Integer;
-      --## rule on TYPE_INITIAL_VALUES
       Base_Index: constant Extended_Base_Range :=
         Sky_Map(Player_Ship.Sky_X, Player_Ship.Sky_Y).Base_Index;
-      --## rule off IMPROPER_INITIALIZATION
-      Nim_Enemy: Nim_Enemy_Record;
-      Nim_G: Nim_Guns;
-      --## rule on IMPROPER_INITIALIZATION
-      Nim_Orders: Nim_Orders_Array := (others => -1);
-      Index: Natural := 0;
       procedure Combat_Ada_Turn with
          Import => True,
          Convention => C,
          External_Name => "combatAdaTurn";
-      procedure Set_Ada_Guns(G: Nim_Guns) with
-         Import => True,
-         Convention => C,
-         External_Name => "setAdaGuns";
-      procedure Get_Ada_Boarding_Orders(Orders: out Nim_Orders_Array) with
-         Import => True,
-         Convention => C,
-         External_Name => "getAdaBoardingOrders";
-      procedure Set_Ada_Boarding_Orders(Orders: Nim_Orders_Array) with
-         Import => True,
-         Convention => C,
-         External_Name => "setAdaBoardingOrders";
    begin
       Get_Game_Date;
-      Set_Player_Guns_Loop :
-      for Gun of Guns loop
-         Nim_G(Index, 0) := Gun(1);
-         Nim_G(Index, 1) := Gun(2);
-         Nim_G(Index, 2) := Gun(3);
-         Index := Index + 1;
-      end loop Set_Player_Guns_Loop;
-      Fill_Player_Guns_Loop :
-      for I in Index .. 9 loop
-         Nim_G(I, 0) := -1;
-         Nim_G(I, 1) := -1;
-         Nim_G(I, 2) := -1;
-      end loop Fill_Player_Guns_Loop;
-      Set_Ada_Guns(G => Nim_G);
       Set_Ship_In_Nim;
       if Base_Index > 0 then
          Set_Base_In_Nim(Base_Index => Base_Index);
       end if;
-      Index := 0;
-      Fill_Nim_Boarding_Orders_Loop :
-      for Order of Boarding_Orders loop
-         Nim_Orders(Index) := Order;
-         Index := Index + 1;
-      end loop Fill_Nim_Boarding_Orders_Loop;
-      Set_Ada_Boarding_Orders(Orders => Nim_Orders);
       Combat_Ada_Turn;
-      Get_Ada_Boarding_Orders(Orders => Nim_Orders);
-      Boarding_Orders.Clear;
-      Fill_Boarding_Orders_Loop :
-      for Order of Nim_Orders loop
-         exit Fill_Boarding_Orders_Loop when Order = -1;
-         Boarding_Orders.Append(New_Item => Order);
-      end loop Fill_Boarding_Orders_Loop;
       if Base_Index > 0 then
          Get_Base_From_Nim(Base_Index => Base_Index);
       end if;
@@ -171,29 +122,6 @@ package body Combat is
          Set_Event(Index => I);
       end loop Set_Events_In_Ada_Loop;
       Set_Map_Cell(X => Player_Ship.Sky_X, Y => Player_Ship.Sky_Y);
-      Get_Ada_Enemy(N_Enemy => Nim_Enemy);
-      Enemy.Distance := Nim_Enemy.Distance;
-      Harpoon_Duration := Nim_Enemy.Harpoon_Duration;
-      Enemy.Harpoon_Duration := Nim_Enemy.Enemy_Harpoon_Duration;
-      Enemy.Guns.Clear;
-      Convert_Enemy_Guns_Loop :
-      for I in 0 .. 9 loop
-         exit Convert_Enemy_Guns_Loop when Nim_Enemy.Guns(I, 0) = -1;
-         Enemy.Guns.Append
-           (New_Item =>
-              (1 => Nim_Enemy.Guns(I, 0), 2 => Nim_Enemy.Guns(I, 1),
-               3 => Nim_Enemy.Guns(I, 2)));
-      end loop Convert_Enemy_Guns_Loop;
-      Guns.Clear;
-      Convert_Player_Guns_Loop :
-      for I in 0 .. 9 loop
-         exit Convert_Player_Guns_Loop when Nim_Enemy.Player_Guns(I, 0) = -1;
-         Guns.Append
-           (New_Item =>
-              (1 => Nim_Enemy.Player_Guns(I, 0),
-               2 => Nim_Enemy.Player_Guns(I, 1),
-               3 => Nim_Enemy.Player_Guns(I, 2)));
-      end loop Convert_Player_Guns_Loop;
    end Combat_Turn;
 
    function Get_Enemy_Name return Tiny_String.Bounded_String is
