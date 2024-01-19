ba396f9b42d0d29351c5d686cac8802c647a3654
thindil <thindil@laeran.pl>
2024-01-19 04:10:31 +0000

    refactor: continue work on move updateMapInfo to Nim
    FossilOrigin-Name: a37611855d6c5add11ad9d2ee56387d404ab966faeed2dce180d1892d9e0564a

--- nim/src/ui/mapsui.nim
+++ nim/src/ui/mapsui.nim
@@ -571,6 +571,17 @@ proc updateMapInfo*(x: Positive = playerShip.skyX; y: Positive = playerShip.skyY
     insertText(newText = $distance, tagName = "yellow2")
     if travelValues[1] > 0:
       insertText(newText = "\nETA:")
+      var distanceText = ""
+      minutesToDate(minutes = travelValues[1], infoText = distanceText)
+      insertText(newText = distanceText, tagName = "yellow2")
+      insertText(newText = "\nApprox fuel usage:")
+      insertText(newText = $travelValues[2], tagName = "yellow2")
+  if skyMap[x][y].baseIndex > 0:
+    let baseIndex = skyMap[x][y].baseIndex
+    if skyBases[baseIndex].known:
+      insertText(newText = "\nBase info:", tagName = "pink underline")
+      insertText(newText = "\nName:")
+      insertText(newText = skyBases[baseIndex].name, tagName = "yellow2")
 
 proc createGameUi*() =
   let
