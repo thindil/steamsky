99143255a91a4311bb463a6a543ae5ea5ca37032
thindil <thindil@laeran.pl>
2024-01-22 04:15:03 +0000

    refactor: started work on move closeDialogCommand to Nim
    FossilOrigin-Name: e0dc1bf0a0c3a9c1575e44b93eaa36a1a958e1b9867dafebfa5aa5d197ded553

---
+++ nim/src/ui/dialogs2.nim
@@ -0,0 +1,51 @@
+# Copyright 2024 Bartek thindil Jasicki
+#
+# This file is part of Steam Sky.
+#
+# Steam Sky is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Steam Sky is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
+
+import ../tk
+import dialogs
+
+proc closeDialogCommand*(clientData: cint; interp: PInterp; argc: cint;
+    argv: openArray[cstring]): TclResults =
+  if timerId.len > 0:
+    tclEval(script = "after cancel " & timerId)
+    timerId = ""
+  var
+    frame = ".gameframe.header"
+    dialog = $argv[1]
+  if argc == 3:
+    frame = $argv[2]
+    tclEval(script = "busy forget " & frame)
+    if argv[2] == ".memberdialog":
+      frame = frame & ".buttons.button"
+      if tclEval2(script = "winfo exists " & frame) == "0":
+        frame = ".memberdialog.button"
+    tclEval(script = "focus " & frame)
+    tclEval(script = "destroy " & dialog)
+    return tclOk
+  return tclOk
+
+proc addCommands*() =
+  addCommand("CloseDialog", closeDialogCommand)
+
+# Temporary code for interfacing with Ada
+
+proc addAdaDialogsCommands() {.raises: [], tags: [RootEffect], exportc.} =
+  try:
+    addCommands()
+  except:
+    echo getCurrentExceptionMsg()
+
