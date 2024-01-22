1305b8461a24568ca848110c46e3f81707a1ca74
thindil <thindil@laeran.pl>
2024-01-22 04:21:51 +0000

    docs: added code documentation to closeDialogCommand procedure
    FossilOrigin-Name: c6becd1ec21426e6b1a20696afdb63b3897651299c1c8e81837c375d8e4e0ee2

--- nim/src/ui/dialogs2.nim
+++ nim/src/ui/dialogs2.nim
@@ -20,6 +20,18 @@ import dialogs
 
 proc closeDialogCommand*(clientData: cint; interp: PInterp; argc: cint;
     argv: openArray[cstring]): TclResults {.sideEffect, raises: [], tags: [].} =
+  ## Close the selected dialog
+  ##
+  ## * clientData - the additional data for the Tcl command
+  ## * interp     - the Tcl interpreter on which the command was executed
+  ## * argc       - the amount of arguments entered for the command
+  ## * argv       - the list of the command's arguments
+  ##
+  ## The procedure always return tclOk
+  ##
+  ## Tcl:
+  ## CloseDialog dialogname
+  ## Dialogname is name of the dialog to close
   if timerId.len > 0:
     tclEval(script = "after cancel " & timerId)
     timerId = ""
