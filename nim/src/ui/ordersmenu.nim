c6d6cb476e601bf1cb0521aad9d98132cd6549a1
thindil <thindil@laeran.pl>
2024-01-23 04:10:06 +0000

    refactor: continue work on move showOrdersCommand to Nim
    FossilOrigin-Name: 67ca1627152dd76350956ef87697df1fb5760e192f6455ad73127b574cdd92a6

--- nim/src/ui/ordersmenu.nim
+++ nim/src/ui/ordersmenu.nim
@@ -15,12 +15,30 @@
 # You should have received a copy of the GNU General Public License
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
-import ../tk
-import dialogs
+import ../[shipscrew, stories, tk, types]
+import coreui, dialogs, dialogs2
 
 proc showOrdersCommand(clientData: cint; interp: PInterp; argc: cint;
     argv: openArray[cstring]): TclResults =
-  var ordersMenu = createDialog(name = ".gameframe.orders", title = "Ship orders")
+  var ordersMenu = createDialog(name = ".gameframe.orders",
+      title = "Ship orders")
+  if tclEval2(script = "winfo ismapped " & ordersMenu) == "1":
+    return closeDialogCommand(clientData = clientData, interp = interp,
+        argc = argc, argv = argv)
+  if tclGetVar(varName = "gamestate") == "combat":
+    tclEval(script = "busy forget " & mainPaned)
+    tclEval(script = "busy forget " & gameHeader)
+    tclEval(script = "destroy " & ordersMenu)
+    return tclOk
+  var haveTrader = false
+  if findMember(order = talk) > -1:
+    haveTrader = true
+  if currentStory.index.len > 0:
+    let step = (if currentStory.currentStep == -1: storiesList[
+        currentStory.index].startingStep elif currentStory.currentStep >
+        -1: storiesList[currentStory.index].steps[
+        currentStory.currentStep] else: storiesList[
+        currentStory.index].finalStep)
   return tclOk
 
 proc addCommands*() =
