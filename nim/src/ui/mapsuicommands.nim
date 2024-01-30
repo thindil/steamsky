bcf4df0159302249ca6bcc8616656c4d3a57fda2
thindil <thindil@laeran.pl>
2024-01-30 04:13:38 +0000

    docs: added code documentation to quitGameCommand procedure
    FossilOrigin-Name: 540b43c1f64a177eeb27f40c5468adcc6784912ea735c64983e76d8627190edd

--- nim/src/ui/mapsuicommands.nim
+++ nim/src/ui/mapsuicommands.nim
@@ -216,6 +216,18 @@ proc moveShipCommand(clientData: cint; interp: PInterp; argc: cint;
 
 proc quitGameCommand(clientData: cint; interp: PInterp; argc: cint;
     argv: openArray[cstring]): TclResults {.sideEffect, raises: [], tags: [].} =
+  ## Ask the player if they wants to quit from the game and if yes, save the
+  ## game and show the main menu
+  ##
+  ## * clientData - the additional data for the Tcl command
+  ## * interp     - the Tcl interpreter on which the command was executed
+  ## * argc       - the amount of arguments entered for the command
+  ## * argv       - the list of the command's arguments
+  ##
+  ## The procedure always return tclOk
+  ##
+  ## Tcl:
+  ## QuitGame
   showQuestion(question = "Are you sure want to quit?", res = "quit")
   return tclOk
 
