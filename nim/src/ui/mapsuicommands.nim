c5786fef77db9a10ddfd4315a1e440548cc0abf3
thindil <thindil@laeran.pl>
2024-01-31 04:28:14 +0000

    docs: added code documentation to resignGameCommand procedure
    FossilOrigin-Name: e2fed1c00e0451348b2835027e9a3df345e55a4950d236ff9f5e2e1c9fd8daaa

--- nim/src/ui/mapsuicommands.nim
+++ nim/src/ui/mapsuicommands.nim
@@ -233,6 +233,18 @@ proc quitGameCommand(clientData: cint; interp: PInterp; argc: cint;
 
 proc resignGameCommand(clientData: cint; interp: PInterp; argc: cint;
     argv: openArray[cstring]): TclResults {.sideEffect, raises: [], tags: [].} =
+  ## Resing from the game - if the player resigned, kill they character and
+  ## continue as for death of the player's character
+  ##
+  ## * clientData - the additional data for the Tcl command
+  ## * interp     - the Tcl interpreter on which the command was executed
+  ## * argc       - the amount of arguments entered for the command
+  ## * argv       - the list of the command's arguments
+  ##
+  ## The procedure always return tclOk
+  ##
+  ## Tcl:
+  ## ResignGame
   showQuestion(question = "Are you sure want to resign from game?",
       res = "resign")
   return tclOk
