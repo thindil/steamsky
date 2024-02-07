8e30bf41a8f34402d1fccc9338ee44bf752477a0
thindil <thindil@laeran.pl>
2024-02-07 04:17:27 +0000

    docs: added code documentation to resizeLastMessagesCommand procedure
    FossilOrigin-Name: 1938dd1e9eb9a71576264b4aa56dc363d9e3e33927f70f19e3c54bc0f52f01fa

--- nim/src/ui/mapsuicommands.nim
+++ nim/src/ui/mapsuicommands.nim
@@ -354,6 +354,17 @@ proc toggleFullScreenCommand(clientData: cint; interp: PInterp; argc: cint;
 
 proc resizeLastMessagesCommand(clientData: cint; interp: PInterp; argc: cint;
     argv: openArray[cstring]): TclResults {.sideEffect, raises: [], tags: [].} =
+  ## Resize the last messages window
+  ##
+  ## * clientData - the additional data for the Tcl command
+  ## * interp     - the Tcl interpreter on which the command was executed
+  ## * argc       - the amount of arguments entered for the command
+  ## * argv       - the list of the command's arguments
+  ##
+  ## The procedure always return tclOk
+  ##
+  ## Tcl:
+  ## ResizeLastMessages
   try:
     gameSettings.windowWidth = tclEval2(script = "winfo width .").parseInt
   except:
