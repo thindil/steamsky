3de7a57896b54384d74c936dd576a51656fd87ec
thindil <thindil@laeran.pl>
2024-02-13 04:16:46 +0000

    refactor: removed unused code
    FossilOrigin-Name: 547c1087f2b9ed4f62db83480d967e03e7f008ae65847efea6f4eff31c5399c0

--- src/ui/ships-ui-crew.adb
+++ src/ui/ships-ui-crew.adb
@@ -13,7 +13,6 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
--- with Ada.Characters.Handling;
 with Ada.Characters.Latin_1;
 with Ada.Containers.Generic_Array_Sort;
 with Ada.Exceptions; use Ada.Exceptions;
