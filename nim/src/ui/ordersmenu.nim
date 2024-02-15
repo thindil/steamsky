647e797ffab295f5cb2feb3549661694ec47fff1
thindil <thindil@laeran.pl>
2024-02-15 04:26:16 +0000

    docs: added code documentation to prayCommand procedure
    FossilOrigin-Name: e8657d2893cd3bcedc7ffcb695286cf23359d162888909f31ddea0175862a726

--- nim/src/ui/ordersmenu.nim
+++ nim/src/ui/ordersmenu.nim
@@ -462,6 +462,17 @@ proc attackCommand(clientData: cint; interp: PInterp; argc: cint;
 proc prayCommand(clientData: cint; interp: PInterp; argc: cint;
     argv: openArray[cstring]): TclResults {.sideEffect, raises: [], tags: [
         WriteIOEffect, RootEffect].}
+  ## Pray in the selected base
+  ##
+  ## * clientData - the additional data for the Tcl command
+  ## * interp     - the Tcl interpreter on which the command was executed
+  ## * argc       - the amount of arguments entered for the command
+  ## * argv       - the list of the command's arguments
+  ##
+  ## The procedure always return tclOk
+  ##
+  ## Tcl:
+  ## Pray
 
 proc addCommands*() =
   addCommand("ShowOrders", showOrdersCommand)
