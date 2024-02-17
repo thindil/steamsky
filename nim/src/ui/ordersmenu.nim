ffcf15a3dead177846275f1d8f27b94fe4ad8ab3
thindil <thindil@laeran.pl>
2024-02-17 04:13:59 +0000

    docs: added code documentation to showTraderCommand procedure
    FossilOrigin-Name: 4d75aa9f204ae788580e367e85960a0b4fafcaf896b22d8596633b877b9bbd2f

--- nim/src/ui/ordersmenu.nim
+++ nim/src/ui/ordersmenu.nim
@@ -501,6 +501,18 @@ proc setAsHomeCommand(clientData: cint; interp: PInterp; argc: cint;
 
 proc showTraderCommand(clientData: cint; interp: PInterp; argc: cint;
     argv: openArray[cstring]): TclResults {.sideEffect, raises: [], tags: [].} =
+  ## Generate cargo for the trader and show the trading UI
+  ##
+  ## * clientData - the additional data for the Tcl command
+  ## * interp     - the Tcl interpreter on which the command was executed
+  ## * argc       - the amount of arguments entered for the command
+  ## * argv       - the list of the command's arguments
+  ##
+  ## The procedure always return tclOk
+  ##
+  ## Tcl:
+  ## ShowTrader protoindex
+  ## Protoindex is the index of ship prototype on which trader cargo will be
   try:
     generateTraderCargo(protoIndex = ($argv[1]).parseInt)
   except:
