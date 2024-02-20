04fa65b334b3f7bc47e09dc1c38ac8c42b415857
thindil <thindil@laeran.pl>
2024-02-20 04:21:02 +0000

    docs: added code documentation to executeStoryCommand
    FossilOrigin-Name: 7d769f6cb48eb4e7ccad5b76287580d5c8bacdc6e0f462f62244465bcae319e1

--- nim/src/ui/ordersmenu.nim
+++ nim/src/ui/ordersmenu.nim
@@ -556,6 +556,17 @@ proc completeMissionCommand(clientData: cint; interp: PInterp; argc: cint;
 proc executeStoryCommand(clientData: cint; interp: PInterp; argc: cint;
     argv: openArray[cstring]): TclResults {.sideEffect, raises: [], tags: [
         WriteIOEffect, RootEffect].}
+  ## Execute the current step in the current story
+  ##
+  ## * clientData - the additional data for the Tcl command
+  ## * interp     - the Tcl interpreter on which the command was executed
+  ## * argc       - the amount of arguments entered for the command
+  ## * argv       - the list of the command's arguments
+  ##
+  ## The procedure always return tclOk
+  ##
+  ## Tcl:
+  ## ExecuteStory
 
 proc addCommands*() =
   addCommand("ShowOrders", showOrdersCommand)
