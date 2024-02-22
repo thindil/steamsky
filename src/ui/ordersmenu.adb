4b7155455d764316bf33d8e3b30115670fcdf801
thindil <thindil@laeran.pl>
2024-02-22 04:06:09 +0000

    refactor: use Nim code to deliver medicines to bases
    FossilOrigin-Name: e8068a6f3ff51e8795bb060b8be65e718a800b032a4b96fd3bdde41f4f3bc2f5

--- src/ui/ordersmenu.adb
+++ src/ui/ordersmenu.adb
@@ -13,23 +13,23 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-with Bases;
-with Dialogs;
-with Events;
-with Factions;
-with Game;
-with Items;
-with Maps;
-with Maps.UI;
-with Messages;
-with Ships;
-with Ships.Cargo;
-with Trades;
-with Utils;
-with Utils.UI; use Utils.UI;
-with Interfaces.C;
-with CArgv;
-with Tcl;
+-- with Bases;
+-- with Dialogs;
+-- with Events;
+-- with Factions;
+-- with Game;
+-- with Items;
+-- with Maps;
+-- with Maps.UI;
+-- with Messages;
+-- with Ships;
+-- with Ships.Cargo;
+-- with Trades;
+-- with Utils;
+-- with Utils.UI; use Utils.UI;
+-- with Interfaces.C;
+-- with CArgv;
+-- with Tcl;
 
 package body OrdersMenu is
 
@@ -48,126 +48,126 @@ package body OrdersMenu is
    -- If argument type is free, deliver medicines for free, otherwise deliver
    -- medicines for a price
    -- SOURCE
-   function Deliver_Medicines_Command
-     (Client_Data: Integer; Interp: Tcl.Tcl_Interp; Argc: Interfaces.C.int;
-      Argv: CArgv.Chars_Ptr_Ptr) return Interfaces.C.int with
-      Convention => C;
-      -- ****
-
-   function Deliver_Medicines_Command
-     (Client_Data: Integer; Interp: Tcl.Tcl_Interp; Argc: Interfaces.C.int;
-      Argv: CArgv.Chars_Ptr_Ptr) return Interfaces.C.int is
-      pragma Unreferenced(Client_Data, Interp, Argc);
-      use Tcl;
-      use Bases;
-      use Events;
-      use Factions;
-      use Items;
-      use Maps;
-      use Maps.UI;
-      use Messages;
-      use Ships;
-      use Ships.Cargo;
-      use Game.Tiny_String;
-
-      Base_Index: constant Positive :=
-        Sky_Map(Player_Ship.Sky_X, Player_Ship.Sky_Y).Base_Index;
-      Event_Index: constant Natural :=
-        Sky_Map(Player_Ship.Sky_X, Player_Ship.Sky_Y).Event_Index;
-      Item_Index: constant Natural :=
-        Find_Item
-          (Inventory => Player_Ship.Cargo,
-           Item_Type =>
-             Get_Faction(Index => Sky_Bases(Base_Index).Owner).Healing_Tools);
-      Event: constant Event_Data := Get_Event(Index => Event_Index);
-      New_Time: constant Integer :=
-        Event.Time -
-        Inventory_Container.Element
-          (Container => Player_Ship.Cargo, Index => Item_Index)
-          .Amount;
-   begin
-      if New_Time < 1 then
-         Delete_Event(Event_Index => Event_Index);
-      else
-         Get_Ada_Event
-           (Index => Event_Index, X => Event.Sky_X, Y => Event.Sky_Y,
-            Time => Event.Time, E_Type => Events_Types'Pos(Event.E_Type),
-            Data => Event.Data);
-      end if;
-      if CArgv.Arg(Argv => Argv, N => 1) = "free" then
-         Gain_Rep
-           (Base_Index => Base_Index,
-            Points =>
-              Inventory_Container.Element
-                (Container => Player_Ship.Cargo, Index => Item_Index)
-                .Amount /
-              10);
-         Add_Message
-           (Message =>
-              "You gave " &
-              To_String
-                (Source =>
-                   Get_Proto_Item
-                     (Index =>
-                        Inventory_Container.Element
-                          (Container => Player_Ship.Cargo, Index => Item_Index)
-                          .Proto_Index)
-                     .Name) &
-              " for free to base.",
-            M_Type => TRADEMESSAGE);
-         --## rule off SIMPLIFIABLE_EXPRESSIONS
-         Update_Cargo
-           (Ship => Player_Ship,
-            Proto_Index =>
-              Inventory_Container.Element
-                (Container => Player_Ship.Cargo, Index => Item_Index)
-                .Proto_Index,
-            Amount =>
-              -(Inventory_Container.Element
-                 (Container => Player_Ship.Cargo, Index => Item_Index)
-                 .Amount));
-         --## rule on SIMPLIFIABLE_EXPRESSIONS
-      else
-         Sell_Medicines_Block :
-         declare
-            use Trades;
-            use Dialogs;
-         begin
-            --## rule off SIMPLIFIABLE_EXPRESSIONS
-            Gain_Rep
-              (Base_Index => Base_Index,
-               Points =>
-                 ((Inventory_Container.Element
-                     (Container => Player_Ship.Cargo, Index => Item_Index)
-                     .Amount /
-                   20) *
-                  (-1)));
-            --## rule on SIMPLIFIABLE_EXPRESSIONS
-            Sell_Items
-              (Item_Index => Item_Index,
-               Amount =>
-                 Integer'Image
-                   (Inventory_Container.Element
-                      (Container => Player_Ship.Cargo, Index => Item_Index)
-                      .Amount));
-         exception
-            when Trade_No_Free_Cargo =>
-               Show_Message
-                 (Text =>
-                    "You can't sell medicines to the base because you don't have enough free cargo space for money.",
-                  Title => "No free cargo space");
-            when Trade_No_Money_In_Base =>
-               Show_Message
-                 (Text =>
-                    "You can't sell medicines to the base because the base don't have enough money to buy them.",
-                  Title => "Can't sell medicines");
-         end Sell_Medicines_Block;
-      end if;
-      Update_Header;
-      Update_Messages;
-      Show_Sky_Map;
-      return TCL_OK;
-   end Deliver_Medicines_Command;
+--   function Deliver_Medicines_Command
+--     (Client_Data: Integer; Interp: Tcl.Tcl_Interp; Argc: Interfaces.C.int;
+--      Argv: CArgv.Chars_Ptr_Ptr) return Interfaces.C.int with
+--      Convention => C;
+--      -- ****
+--
+--   function Deliver_Medicines_Command
+--     (Client_Data: Integer; Interp: Tcl.Tcl_Interp; Argc: Interfaces.C.int;
+--      Argv: CArgv.Chars_Ptr_Ptr) return Interfaces.C.int is
+--      pragma Unreferenced(Client_Data, Interp, Argc);
+--      use Tcl;
+--      use Bases;
+--      use Events;
+--      use Factions;
+--      use Items;
+--      use Maps;
+--      use Maps.UI;
+--      use Messages;
+--      use Ships;
+--      use Ships.Cargo;
+--      use Game.Tiny_String;
+--
+--      Base_Index: constant Positive :=
+--        Sky_Map(Player_Ship.Sky_X, Player_Ship.Sky_Y).Base_Index;
+--      Event_Index: constant Natural :=
+--        Sky_Map(Player_Ship.Sky_X, Player_Ship.Sky_Y).Event_Index;
+--      Item_Index: constant Natural :=
+--        Find_Item
+--          (Inventory => Player_Ship.Cargo,
+--           Item_Type =>
+--             Get_Faction(Index => Sky_Bases(Base_Index).Owner).Healing_Tools);
+--      Event: constant Event_Data := Get_Event(Index => Event_Index);
+--      New_Time: constant Integer :=
+--        Event.Time -
+--        Inventory_Container.Element
+--          (Container => Player_Ship.Cargo, Index => Item_Index)
+--          .Amount;
+--   begin
+--      if New_Time < 1 then
+--         Delete_Event(Event_Index => Event_Index);
+--      else
+--         Get_Ada_Event
+--           (Index => Event_Index, X => Event.Sky_X, Y => Event.Sky_Y,
+--            Time => Event.Time, E_Type => Events_Types'Pos(Event.E_Type),
+--            Data => Event.Data);
+--      end if;
+--      if CArgv.Arg(Argv => Argv, N => 1) = "free" then
+--         Gain_Rep
+--           (Base_Index => Base_Index,
+--            Points =>
+--              Inventory_Container.Element
+--                (Container => Player_Ship.Cargo, Index => Item_Index)
+--                .Amount /
+--              10);
+--         Add_Message
+--           (Message =>
+--              "You gave " &
+--              To_String
+--                (Source =>
+--                   Get_Proto_Item
+--                     (Index =>
+--                        Inventory_Container.Element
+--                          (Container => Player_Ship.Cargo, Index => Item_Index)
+--                          .Proto_Index)
+--                     .Name) &
+--              " for free to base.",
+--            M_Type => TRADEMESSAGE);
+--         --## rule off SIMPLIFIABLE_EXPRESSIONS
+--         Update_Cargo
+--           (Ship => Player_Ship,
+--            Proto_Index =>
+--              Inventory_Container.Element
+--                (Container => Player_Ship.Cargo, Index => Item_Index)
+--                .Proto_Index,
+--            Amount =>
+--              -(Inventory_Container.Element
+--                 (Container => Player_Ship.Cargo, Index => Item_Index)
+--                 .Amount));
+--         --## rule on SIMPLIFIABLE_EXPRESSIONS
+--      else
+--         Sell_Medicines_Block :
+--         declare
+--            use Trades;
+--            use Dialogs;
+--         begin
+--            --## rule off SIMPLIFIABLE_EXPRESSIONS
+--            Gain_Rep
+--              (Base_Index => Base_Index,
+--               Points =>
+--                 ((Inventory_Container.Element
+--                     (Container => Player_Ship.Cargo, Index => Item_Index)
+--                     .Amount /
+--                   20) *
+--                  (-1)));
+--            --## rule on SIMPLIFIABLE_EXPRESSIONS
+--            Sell_Items
+--              (Item_Index => Item_Index,
+--               Amount =>
+--                 Integer'Image
+--                   (Inventory_Container.Element
+--                      (Container => Player_Ship.Cargo, Index => Item_Index)
+--                      .Amount));
+--         exception
+--            when Trade_No_Free_Cargo =>
+--               Show_Message
+--                 (Text =>
+--                    "You can't sell medicines to the base because you don't have enough free cargo space for money.",
+--                  Title => "No free cargo space");
+--            when Trade_No_Money_In_Base =>
+--               Show_Message
+--                 (Text =>
+--                    "You can't sell medicines to the base because the base don't have enough money to buy them.",
+--                  Title => "Can't sell medicines");
+--         end Sell_Medicines_Block;
+--      end if;
+--      Update_Header;
+--      Update_Messages;
+--      Show_Sky_Map;
+--      return TCL_OK;
+--   end Deliver_Medicines_Command;
 
    procedure Add_Commands is
       procedure Add_Ada_Commands with
@@ -176,9 +176,9 @@ package body OrdersMenu is
          External_Name => "addAdaOrdersMenuCommands";
    begin
       Add_Ada_Commands;
-      Add_Command
-        (Name => "DeliverMedicines",
-         Ada_Command => Deliver_Medicines_Command'Access);
+--      Add_Command
+--        (Name => "DeliverMedicines",
+--         Ada_Command => Deliver_Medicines_Command'Access);
    end Add_Commands;
 
 end OrdersMenu;
