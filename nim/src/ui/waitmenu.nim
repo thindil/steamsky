5b2fd34c6352a2cf53c08b6e1e2ba4bf2a3711cb
thindil <thindil@laeran.pl>
2024-02-24 04:08:30 +0000

    docs: added code documentation to waitCommand procedure
    FossilOrigin-Name: e3d6e18d312fb25171c14e71ec53170cbec3787f3823f8d3cd2676f7ec68e7b6

--- nim/src/ui/waitmenu.nim
+++ nim/src/ui/waitmenu.nim
@@ -114,6 +114,17 @@ proc showWaitCommand*(clientData: cint; interp: PInterp; argc: cint;
 proc waitCommand*(clientData: cint; interp: PInterp; argc: cint;
     argv: openArray[cstring]): TclResults {.sideEffect, raises: [], tags: [
         WriteIOEffect, RootEffect].}
+  ## Wait the selected amount of time
+  ##
+  ## * clientData - the additional data for the Tcl command
+  ## * interp     - the Tcl interpreter on which the command was executed
+  ## * argc       - the amount of arguments entered for the command
+  ## * argv       - the list of the command's arguments
+  ##
+  ## The procedure always return tclOk
+  ##
+  ## Tcl:
+  ## Wait
 
 proc addCommands*() =
   addCommand("ShowWait", showWaitCommand)
