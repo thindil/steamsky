a4f1fe6a615b93c668bc47d4a70fd9e802368b69
thindil <thindil@laeran.pl>
2024-02-25 04:08:59 +0000

    chore: fixed some errors reported by nimalyzer
    FossilOrigin-Name: 813fbdcaebc3a1cf2bd93ef3d6f766825431eb8b2ed5b94b39aadb957d30a42a

--- nim/src/items.nim
+++ nim/src/items.nim
@@ -16,6 +16,7 @@
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
 import std/[strutils, tables, xmlparser, xmltree]
+import contracts
 import config, crewinventory, game, log, messages, shipscargo, shipscrew, types, utils
 
 var
@@ -35,132 +36,147 @@ var
     ## The list of all tools prototypes indexes
 
 proc loadItems*(fileName: string) {.sideEffect, raises: [DataLoadingError],
-    tags: [WriteIOEffect, ReadIOEffect, RootEffect].} =
+    tags: [WriteIOEffect, ReadIOEffect, RootEffect], contractual.} =
   ## Load the items data from the file
   ##
   ## * fileName - the name of the file to load
-  let itemsXml = try:
-      loadXml(path = fileName)
-    except XmlError, ValueError, IOError, OSError, Exception:
-      raise newException(exceptn = DataLoadingError,
-          message = "Can't load items data file. Reason: " &
-          getCurrentExceptionMsg())
-  for itemNode in itemsXml:
-    if itemNode.kind != xnElement:
-      continue
-    let
-      itemIndex: Natural = try:
-          itemNode.attr(name = "index").parseInt()
-        except ValueError:
-          raise newException(exceptn = DataLoadingError,
-              message = "Can't add item '" & itemNode.attr(name = "index") & "', invalid index.")
-      itemAction: DataAction = try:
-          parseEnum[DataAction](itemNode.attr(name = "action").toLowerAscii)
-        except ValueError:
-          DataAction.add
-    if itemAction in [update, remove]:
-      if itemIndex > itemsList.len():
+  require:
+    fileName.len > 0
+  ensure:
+    itemsList.len > 0
+    weaponsList.len > 0
+    shieldsList.len > 0
+    headArmorsList.len > 0
+    chestArmorsList.len > 0
+    armsArmorsList.len > 0
+    legsArmorsList.len > 0
+  body:
+    let itemsXml = try:
+        loadXml(path = fileName)
+      except XmlError, ValueError, IOError, OSError, Exception:
         raise newException(exceptn = DataLoadingError,
-            message = "Can't " & $itemAction & " item '" & $itemIndex & "', there is no item with that index.")
-    elif itemIndex < itemsList.len():
-      raise newException(exceptn = DataLoadingError,
-          message = "Can't add item '" & $itemIndex & "', there is an item with that index.")
-    if itemAction == DataAction.remove:
-      {.warning[ProveInit]: off.}
-      {.warning[UnsafeDefault]: off.}
-      itemsList.del(key = itemIndex)
-      {.warning[ProveInit]: on.}
-      {.warning[UnsafeDefault]: on.}
-      logMessage(message = "Item removed: '" & $itemIndex & "'",
-          debugType = everything)
-      continue
-    var item: ObjectData = if itemAction == DataAction.update:
-        try:
-          itemsList[itemIndex]
-        except ValueError:
+            message = "Can't load items data file. Reason: " &
+            getCurrentExceptionMsg())
+    for itemNode in itemsXml:
+      if itemNode.kind != xnElement:
+        continue
+      let
+        itemIndex: Natural = try:
+            itemNode.attr(name = "index").parseInt()
+          except ValueError:
+            raise newException(exceptn = DataLoadingError,
+                message = "Can't add item '" & itemNode.attr(name = "index") & "', invalid index.")
+        itemAction: DataAction = try:
+            parseEnum[DataAction](itemNode.attr(name = "action").toLowerAscii)
+          except ValueError:
+            DataAction.add
+      if itemAction in [update, remove]:
+        if itemIndex > itemsList.len():
+          raise newException(exceptn = DataLoadingError,
+              message = "Can't " & $itemAction & " item '" & $itemIndex & "', there is no item with that index.")
+      elif itemIndex < itemsList.len():
+        raise newException(exceptn = DataLoadingError,
+            message = "Can't add item '" & $itemIndex & "', there is an item with that index.")
+      if itemAction == DataAction.remove:
+        {.warning[ProveInit]: off.}
+        {.warning[UnsafeDefault]: off.}
+        itemsList.del(key = itemIndex)
+        {.warning[ProveInit]: on.}
+        {.warning[UnsafeDefault]: on.}
+        logMessage(message = "Item removed: '" & $itemIndex & "'",
+            debugType = everything)
+        continue
+      var item: ObjectData = if itemAction == DataAction.update:
+          try:
+            itemsList[itemIndex]
+          except ValueError:
+            ObjectData(weight: 1, reputation: -100)
+        else:
           ObjectData(weight: 1, reputation: -100)
-      else:
-        ObjectData(weight: 1, reputation: -100)
-    var attribute = itemNode.attr(name = "name")
-    if attribute.len() > 0:
-      item.name = attribute
-    attribute = itemNode.attr(name = "weight")
-    if attribute.len() > 0:
-      item.weight = try:
-          attribute.parseInt()
-      except ValueError:
-        raise newException(exceptn = DataLoadingError,
-            message = "Can't " & $itemAction & " item '" & $itemIndex & "', invalid value for item weight.")
-    attribute = itemNode.attr(name = "type")
-    if attribute.len() > 0:
-      item.itemType = attribute
-    attribute = itemNode.attr(name = "showtype")
-    if attribute.len() > 0:
-      item.showType = attribute
-    attribute = itemNode.attr(name = "reputation")
-    if attribute.len() > 0:
-      item.reputation = try:
-          attribute.parseInt()
-      except ValueError:
-        raise newException(exceptn = DataLoadingError,
-            message = "Can't " & $itemAction & " item '" & $itemIndex & "', invalid value for item reputation.")
-    else:
-      item.reputation = -100
-    attribute = itemNode.attr(name = "price")
-    if attribute.len() > 0:
-      item.price = try:
-          attribute.parseInt()
-      except ValueError:
-        raise newException(exceptn = DataLoadingError,
-            message = "Can't " & $itemAction & " item '" & $itemIndex & "', invalid value for item price.")
-    var valueIndex: Positive = 1
-    for data in itemNode.findAll(tag = "data"):
-      item.value[valueIndex] = try:
-          data.attr(name = "value").parseInt()
+      var attribute = itemNode.attr(name = "name")
+      if attribute.len() > 0:
+        item.name = attribute
+      attribute = itemNode.attr(name = "weight")
+      if attribute.len() > 0:
+        item.weight = try:
+            attribute.parseInt()
         except ValueError:
           raise newException(exceptn = DataLoadingError,
-            message = "Can't " & $itemAction & " item '" & $itemIndex &
-                "', invalid value for item data.")
-      valueIndex.inc
-    attribute = itemNode.child(name = "description").innerText()
-    if attribute.len() > 0:
-      item.description = attribute
-    if itemAction == DataAction.add:
-      logMessage(message = "Item added: '" & $itemIndex & "'",
-          debugType = everything)
-    else:
-      logMessage(message = "Item updated: '" & $itemIndex & "'",
-          debugType = everything)
-    itemsList[itemIndex] = item
-    if itemIndex == moneyIndex:
-      moneyName = item.name
-    if item.itemType == weaponType:
-      weaponsList.add(y = itemIndex)
-    elif item.itemType == shieldType:
-      shieldsList.add(y = itemIndex)
-    elif item.itemType == headArmor:
-      headArmorsList.add(y = itemIndex)
-    elif item.itemType == chestArmor:
-      chestArmorsList.add(y = itemIndex)
-    elif item.itemType == armsArmor:
-      armsArmorsList.add(y = itemIndex)
-    elif item.itemType == legsArmor:
-      legsArmorsList.add(y = itemIndex)
+              message = "Can't " & $itemAction & " item '" & $itemIndex & "', invalid value for item weight.")
+      attribute = itemNode.attr(name = "type")
+      if attribute.len() > 0:
+        item.itemType = attribute
+      attribute = itemNode.attr(name = "showtype")
+      if attribute.len() > 0:
+        item.showType = attribute
+      attribute = itemNode.attr(name = "reputation")
+      if attribute.len() > 0:
+        item.reputation = try:
+            attribute.parseInt()
+        except ValueError:
+          raise newException(exceptn = DataLoadingError,
+              message = "Can't " & $itemAction & " item '" & $itemIndex & "', invalid value for item reputation.")
+      else:
+        item.reputation = -100
+      attribute = itemNode.attr(name = "price")
+      if attribute.len() > 0:
+        item.price = try:
+            attribute.parseInt()
+        except ValueError:
+          raise newException(exceptn = DataLoadingError,
+              message = "Can't " & $itemAction & " item '" & $itemIndex & "', invalid value for item price.")
+      var valueIndex: Positive = 1
+      for data in itemNode.findAll(tag = "data"):
+        item.value[valueIndex] = try:
+            data.attr(name = "value").parseInt()
+          except ValueError:
+            raise newException(exceptn = DataLoadingError,
+              message = "Can't " & $itemAction & " item '" & $itemIndex &
+                  "', invalid value for item data.")
+        valueIndex.inc
+      attribute = itemNode.child(name = "description").innerText()
+      if attribute.len() > 0:
+        item.description = attribute
+      if itemAction == DataAction.add:
+        logMessage(message = "Item added: '" & $itemIndex & "'",
+            debugType = everything)
+      else:
+        logMessage(message = "Item updated: '" & $itemIndex & "'",
+            debugType = everything)
+      itemsList[itemIndex] = item
+      if itemIndex == moneyIndex:
+        moneyName = item.name
+      if item.itemType == weaponType:
+        weaponsList.add(y = itemIndex)
+      elif item.itemType == shieldType:
+        shieldsList.add(y = itemIndex)
+      elif item.itemType == headArmor:
+        headArmorsList.add(y = itemIndex)
+      elif item.itemType == chestArmor:
+        chestArmorsList.add(y = itemIndex)
+      elif item.itemType == armsArmor:
+        armsArmorsList.add(y = itemIndex)
+      elif item.itemType == legsArmor:
+        legsArmorsList.add(y = itemIndex)
 
-proc findProtoItem*(itemType: string): Natural {.sideEffect, raises: [], tags: [].} =
+proc findProtoItem*(itemType: string): Natural {.sideEffect, raises: [], tags: [
+    ], contractual.} =
   ## Get the index of prototype of the selected item type
   ##
   ## * itemType - the type of items which prototype's index will be get
   ##
   ## Returns the index of the prototype of the selected item's type or 0 if no prototype
   ## found
-  for index, item in itemsList.pairs():
-    if item.itemType == itemType:
-      return index
-  return 0
+  require:
+    itemType.len > 0
+  body:
+    for index, item in itemsList.pairs():
+      if item.itemType == itemType:
+        return index
+    return 0
 
 func getItemDamage*(itemDurability: ItemsDurability; toLower: bool = false;
-    withColors: bool = false): string {.raises: [], tags: [].} =
+    withColors: bool = false): string {.raises: [], tags: [], contractual.} =
   ## Get the description of the item damage level
   ##
   ## * itemDurability - the durability of the item which description will be get
@@ -187,7 +203,8 @@ func getItemDamage*(itemDurability: ItemsDurability; toLower: bool = false;
     result = toLowerAscii(s = result)
 
 proc getItemName*(item: InventoryData; damageInfo: bool = true;
-    toLower: bool = true): string {.sideEffect, raises: [], tags: [].} =
+    toLower: bool = true): string {.sideEffect, raises: [], tags: [],
+        contractual.} =
   ## Get the name of the selected item with optional info about the item's
   ## damage
   ##
@@ -209,7 +226,7 @@ proc getItemName*(item: InventoryData; damageInfo: bool = true;
         toLower = toLower) & ")"
 
 proc getItemChanceToDamage*(itemData: Natural): string {.sideEffect, raises: [],
-    tags: [].} =
+    tags: [], contractual.} =
   ## Get the string with textual information about the item's chance for take
   ## damage during usage
   ##
@@ -235,23 +252,27 @@ proc getItemChanceToDamage*(itemData: Natural): string {.sideEffect, raises: [],
   else:
     return "Very high"
 
-proc setToolsList*() {.sideEffect, raises: [], tags: [].} =
+proc setToolsList*() {.sideEffect, raises: [], tags: [], contractual.} =
   ## Set the list of all available tools in the game
-  if toolsList.len() > 0:
-    return
-  toolsList.add(y = repairTools)
-  toolsList.add(y = cleaningTools)
-  toolsList.add(y = alchemyTools)
-  for recipe in recipesList.values:
-    if recipe.tool notin toolsList:
-      toolsList.add(y = recipe.tool)
-  for skill in skillsList.values:
-    if skill.tool notin toolsList:
-      toolsList.add(y = skill.tool)
+  ensure:
+    toolsList.len > 0
+  body:
+    if toolsList.len() > 0:
+      return
+    toolsList.add(y = repairTools)
+    toolsList.add(y = cleaningTools)
+    toolsList.add(y = alchemyTools)
+    for recipe in recipesList.values:
+      if recipe.tool notin toolsList:
+        toolsList.add(y = recipe.tool)
+    for skill in skillsList.values:
+      if skill.tool notin toolsList:
+        toolsList.add(y = skill.tool)
 
 proc findTools*(memberIndex: Natural; itemType: string; order: CrewOrders;
     toolQuality: Positive = 100): int {.sideEffect, raises: [KeyError,
-    CrewNoSpaceError, CrewOrderError, Exception], tags: [RootEffect].} =
+    CrewNoSpaceError, CrewOrderError, Exception], tags: [RootEffect],
+    contractual.} =
   ## Search for specified tools in the crew member and the ship cargo
   ##
   ## * memberIndex - The index of the crew member which will be checked
@@ -262,58 +283,64 @@ proc findTools*(memberIndex: Natural; itemType: string; order: CrewOrders;
   ##
   ## Returns the selected crew member inventory index of the tool or -1 if
   ## tool was not found
-  result = playerShip.crew[memberIndex].equipment[tool]
-  if result > -1:
-    let protoIndex = playerShip.crew[memberIndex].inventory[result].protoIndex
-    if itemsList[protoIndex].itemType != itemType or itemsList[
-        protoIndex].value[1] < toolQuality:
-      updateCargo(ship = playerShip, protoIndex = protoIndex, amount = 1,
-          durability = playerShip.crew[memberIndex].inventory[
-          result].durability)
-      updateInventory(memberIndex = memberIndex, amount = -1,
-          inventoryIndex = result, ship = playerShip)
-      result = -1
-  result = findItem(inventory = playerShip.crew[memberIndex].inventory,
-      itemType = itemType, quality = toolQuality)
-  if result == -1:
-    result = findItem(inventory = playerShip.cargo, itemType = itemType,
-        quality = toolQuality)
+  require:
+    memberIndex < playerShip.crew.len
+  ensure:
+    result < playerShip.crew[memberIndex].inventory.len
+  body:
+    result = playerShip.crew[memberIndex].equipment[tool]
     if result > -1:
-      try:
-        updateInventory(memberIndex = memberIndex, amount = 1,
-            protoIndex = playerShip.cargo[result].protoIndex,
-            durability = playerShip.cargo[result].durability, ship = playerShip)
-        updateCargo(ship = playerShip, amount = -1, cargoIndex = result)
-        result = findItem(inventory = playerShip.crew[memberIndex].inventory,
-            itemType = itemType, quality = toolQuality)
-      except CrewNoSpaceError:
-        case order:
-        of repair:
-          addMessage(message = playerShip.crew[memberIndex].name &
-              " can't continue repairs because they don't have free space in their inventory for repair tools.",
-              mType = orderMessage, color = red)
-        of upgrading:
-          addMessage(message = playerShip.crew[memberIndex].name &
-              " can't continue upgrading module because they don't have free space in their inventory for repair tools.",
-              mType = orderMessage, color = red)
-        of clean:
-          addMessage(message = playerShip.crew[memberIndex].name &
-              " can't continue cleaning ship because they don't have free space in their inventory for cleaning tools.",
-              mType = orderMessage, color = red)
-        of craft:
-          addMessage(message = playerShip.crew[memberIndex].name &
-              " can't continue manufacturing because they don't have free space in their inventory for the proper tools.",
-              mType = orderMessage, color = red)
-        of train:
-          addMessage(message = playerShip.crew[memberIndex].name &
-              " can't continue training because they don't have free space in their inventory for the proper tools.",
-              mType = orderMessage, color = red)
-        else:
-          discard
-        giveOrders(ship = playerShip, memberIndex = memberIndex,
-            givenOrder = rest)
-        return -1
-  playerShip.crew[memberIndex].equipment[tool] = result
+      let protoIndex = playerShip.crew[memberIndex].inventory[result].protoIndex
+      if itemsList[protoIndex].itemType != itemType or itemsList[
+          protoIndex].value[1] < toolQuality:
+        updateCargo(ship = playerShip, protoIndex = protoIndex, amount = 1,
+            durability = playerShip.crew[memberIndex].inventory[
+            result].durability)
+        updateInventory(memberIndex = memberIndex, amount = -1,
+            inventoryIndex = result, ship = playerShip)
+        result = -1
+    result = findItem(inventory = playerShip.crew[memberIndex].inventory,
+        itemType = itemType, quality = toolQuality)
+    if result == -1:
+      result = findItem(inventory = playerShip.cargo, itemType = itemType,
+          quality = toolQuality)
+      if result > -1:
+        try:
+          updateInventory(memberIndex = memberIndex, amount = 1,
+              protoIndex = playerShip.cargo[result].protoIndex,
+              durability = playerShip.cargo[result].durability,
+                  ship = playerShip)
+          updateCargo(ship = playerShip, amount = -1, cargoIndex = result)
+          result = findItem(inventory = playerShip.crew[memberIndex].inventory,
+              itemType = itemType, quality = toolQuality)
+        except CrewNoSpaceError:
+          case order:
+          of repair:
+            addMessage(message = playerShip.crew[memberIndex].name &
+                " can't continue repairs because they don't have free space in their inventory for repair tools.",
+                mType = orderMessage, color = red)
+          of upgrading:
+            addMessage(message = playerShip.crew[memberIndex].name &
+                " can't continue upgrading module because they don't have free space in their inventory for repair tools.",
+                mType = orderMessage, color = red)
+          of clean:
+            addMessage(message = playerShip.crew[memberIndex].name &
+                " can't continue cleaning ship because they don't have free space in their inventory for cleaning tools.",
+                mType = orderMessage, color = red)
+          of craft:
+            addMessage(message = playerShip.crew[memberIndex].name &
+                " can't continue manufacturing because they don't have free space in their inventory for the proper tools.",
+                mType = orderMessage, color = red)
+          of train:
+            addMessage(message = playerShip.crew[memberIndex].name &
+                " can't continue training because they don't have free space in their inventory for the proper tools.",
+                mType = orderMessage, color = red)
+          else:
+            discard
+          giveOrders(ship = playerShip, memberIndex = memberIndex,
+              givenOrder = rest)
+          return -1
+    playerShip.crew[memberIndex].equipment[tool] = result
 
 proc getRandomItem*(itemsIndexes: seq[Positive]; equipIndex: EquipmentLocations;
     highestLevel, weaponSkillLevel: Positive;
