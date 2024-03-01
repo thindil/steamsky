b6282acba6cc93e327f73bb38d069180150f8cf3
thindil <thindil@laeran.pl>
2024-03-01 04:09:00 +0000

    docs: added code documentation to showHelpCommand procedure
    FossilOrigin-Name: a253ff0f38487a16d08d3ce2e529accb802fee17084beff8343eab4e3ad64a01

--- nim/src/ui/helpui.nim
+++ nim/src/ui/helpui.nim
@@ -62,6 +62,18 @@ proc closeHelpCommand(clientData: cint; interp: PInterp; argc: cint;
 
 proc showHelpCommand(clientData: cint; interp: PInterp; argc: cint;
     argv: openArray[cstring]): TclResults {.sideEffect, raises: [], tags: [].} =
+  ## Show the help window to the player
+  ##
+  ## * clientData - the additional data for the Tcl command
+  ## * interp     - the Tcl interpreter on which the command was executed
+  ## * argc       - the amount of arguments entered for the command
+  ## * argv       - the list of the command's arguments
+  ##
+  ## The procedure always return tclOk
+  ##
+  ## Tcl:
+  ## ShowHelp topicindex
+  ## Topicindex is the index of the help topic which content will be show
   let helpWindow = ".help"
   if tclEval2(script = "winfo exists " & helpWindow) == "1":
     return closeHelpCommand(clientData = clientData, interp = interp,
