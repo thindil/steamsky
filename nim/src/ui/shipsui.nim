4b0bda9e4f73d994bc28e4bdcaf49feae804f385
thindil <thindil@laeran.pl>
2024-03-02 04:15:05 +0000

    refactor: started work on move showShipInfoCommand to Nim
    FossilOrigin-Name: 43872f558b8cffd17f708a4ea548310c0d57eb1a3197190a8754b33414eb61fd

--- nim/src/ui/shipsui.nim
+++ nim/src/ui/shipsui.nim
@@ -15,12 +15,37 @@
 # You should have received a copy of the GNU General Public License
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
-import ../tk
+import std/os
+import ../[game, tk]
+import coreui
+
+proc showShipInfoCommand*(clientData: cint; interp: PInterp; argc: cint;
+    argv: openArray[cstring]): TclResults {.sideEffect, raises: [], tags: [
+        WriteIOEffect, RootEffect].} =
+  var
+    shipInfoFrame = mainPaned & ".shipinfoframe"
+    button = mainPaned & ".shipinfoframe.general.canvas.frame.rename"
+  if tclEval2(script = "winfo exists " & shipInfoFrame) == "0":
+    tclEvalFile(dataDirectory & "ui" & DirSep & "shipinfo.tcl")
+    tclEval(script = button & " configure -image editicon")
+    button = mainPaned & ".shipinfoframe.general.canvas.frame.showhome"
+    tclEval(script = button & " configure -image showicon")
+    button = mainPaned & ".shipinfoframe.general.canvas.frame.cancelupgrade"
+    tclEval(script = button & " configure -image cancelicon")
+    button = mainPaned & ".shipinfoframe.general.canvas.frame.canceldestination"
+    tclEval(script = button & " configure -image cancelicon")
+  elif tclEval2(script = "winfo exists " & shipInfoFrame) == "1" and argc == 1:
+    tclEval(script = "InvokeButton " & closeButton)
+    tclEval(script = "grid remove " & closeButton)
+    for i in 1 .. 4:
+      tclEval(script = "bind . <" & generalAccelerators[i] & "> {}")
+    return tclOk
+  return tclOk
 
 proc addCommands*() {.sideEffect, raises: [], tags: [].} =
   ## Adds Tcl commands related to the wait menu
   try:
-    discard
+    addCommand("ShowShipInfo", showShipInfoCommand)
   except:
     tclEval(script = "bgerror {Can't add a Tcl command. Reason: " &
         getCurrentExceptionMsg() & "}")
