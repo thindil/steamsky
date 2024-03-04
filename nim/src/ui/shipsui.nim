d6e2384e9f8cce628f7deb12fd116194ae1e17ea
thindil <thindil@laeran.pl>
2024-03-04 04:12:24 +0000

    docs: added code documentation to showShipInfoCommand procedure
    FossilOrigin-Name: 30d03e0cd59923239269629c1cb63fc626a8f900ef307b18fd54769208326514

--- nim/src/ui/shipsui.nim
+++ nim/src/ui/shipsui.nim
@@ -21,6 +21,17 @@ import coreui, shipsuicrew, shipsuimodules, utilsui2
 
 proc showShipInfoCommand*(clientData: cint; interp: PInterp; argc: cint;
     argv: openArray[cstring]): TclResults {.sideEffect, raises: [], tags: [].} =
+  ## Show information about the player's ship
+  ##
+  ## * clientData - the additional data for the Tcl command
+  ## * interp     - the Tcl interpreter on which the command was executed
+  ## * argc       - the amount of arguments entered for the command
+  ## * argv       - the list of the command's arguments
+  ##
+  ## The procedure always return tclOk
+  ##
+  ## Tcl:
+  ## ShowShipInfo
   var
     shipInfoFrame = mainPaned & ".shipinfoframe"
     button = mainPaned & ".shipinfoframe.general.canvas.frame.rename"
