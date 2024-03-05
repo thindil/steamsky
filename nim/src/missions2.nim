57e6a108bd0ba9283c977c6e525d9d609e462869
thindil <thindil@laeran.pl>
2024-03-05 04:08:40 +0000

    chore: fixed errors reported by nimalyzer
    FossilOrigin-Name: 4bbf734e35a30543834674c17174dd361e2dbde58d1edfcb87a05cf851b16fb5

--- nim/src/missions2.nim
+++ nim/src/missions2.nim
@@ -16,6 +16,7 @@
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
 import std/tables
+import contracts
 import game, game2, goals, maps, messages, missions, shipscrew, shipscargo,
     shipsmovement, statistics, types, utils
 
@@ -28,43 +29,46 @@ type
 
 proc finishMission*(missionIndex: Natural) {.sideEffect, raises: [
     MissionFinishingError, KeyError, IOError, Exception], tags: [WriteIOEffect,
-    RootEffect].} =
+    RootEffect], contractual.} =
   ## Finish the selected accepted mission
   ##
   ## * missionIndex - the index of the accepted mission to finish
-  let missionsAmount = acceptedMissions.len
-  if playerShip.speed != docked:
-    let message = dockShip(docking = true)
-    if message.len > 0:
-      raise newException(MissionFinishingError, message)
-  updateGame(minutes = 5)
-  if missionsAmount > acceptedMissions.len:
-    return
-  case acceptedMissions[missionIndex].mType
-  of deliver:
-    addMessage(message = "You finished mission 'Deliver " & itemsList[
-        acceptedMissions[missionIndex].itemIndex].name & "'.",
-        mType = missionMessage, color = green)
-  of destroy:
-    addMessage(message = "You finished mission 'Destroy " & protoShipsList[
-        acceptedMissions[missionIndex].shipIndex].name & ".'",
-        mType = missionMessage, color = green)
-  of patrol:
-    addMessage(message = "You finished mission 'Patrol selected area'.",
-        mType = missionMessage, color = green)
-  of explore:
-    addMessage(message = "You finished mission 'Explore selected area'.",
-        mType = missionMessage, color = green)
-  of passenger:
-    addMessage(message = "You finished mission 'Transport passenger to base'.",
-        mType = missionMessage, color = green)
-  updateGoal(goalType = mission, targetIndex = $acceptedMissions[
-      missionIndex].mType)
-  updateFinishedMissions(mType = $acceptedMissions[missionIndex].mType)
-  deleteMission(missionIndex = missionIndex, failed = false)
+  require:
+    missionIndex < acceptedMissions.len
+  body:
+    let missionsAmount = acceptedMissions.len
+    if playerShip.speed != docked:
+      let message = dockShip(docking = true)
+      if message.len > 0:
+        raise newException(exceptn = MissionFinishingError, message = message)
+    updateGame(minutes = 5)
+    if missionsAmount > acceptedMissions.len:
+      return
+    case acceptedMissions[missionIndex].mType
+    of deliver:
+      addMessage(message = "You finished mission 'Deliver " & itemsList[
+          acceptedMissions[missionIndex].itemIndex].name & "'.",
+          mType = missionMessage, color = green)
+    of destroy:
+      addMessage(message = "You finished mission 'Destroy " & protoShipsList[
+          acceptedMissions[missionIndex].shipIndex].name & ".'",
+          mType = missionMessage, color = green)
+    of patrol:
+      addMessage(message = "You finished mission 'Patrol selected area'.",
+          mType = missionMessage, color = green)
+    of explore:
+      addMessage(message = "You finished mission 'Explore selected area'.",
+          mType = missionMessage, color = green)
+    of passenger:
+      addMessage(message = "You finished mission 'Transport passenger to base'.",
+          mType = missionMessage, color = green)
+    updateGoal(goalType = mission, targetIndex = $acceptedMissions[
+        missionIndex].mType)
+    updateFinishedMissions(mType = $acceptedMissions[missionIndex].mType)
+    deleteMission(missionIndex = missionIndex, failed = false)
 
 proc autoFinishMissions*(): string {.sideEffect, raises: [KeyError, IOError,
-    Exception], tags: [WriteIOEffect, RootEffect].} =
+    Exception], tags: [WriteIOEffect, RootEffect], contractual.} =
   ## Finish all possible missions if the player's ship is on the base.
   ##
   ## Returns empty string if finishing was successfull, otherwise returns
@@ -92,13 +96,14 @@ proc autoFinishMissions*(): string {.sideEffect, raises: [KeyError, IOError,
 
 proc acceptMission*(missionIndex: Natural) {.sideEffect, raises: [
     MissionAcceptingError, KeyError, IOError, Exception], tags: [WriteIOEffect,
-    RootEffect].} =
+    RootEffect], contractual.} =
   ## Accept the selected mission from the base
   ##
   ## * missionIndex - the index of the mission in the base which will be accepted
   let baseIndex = skyMap[playerShip.skyX][playerShip.skyY].baseIndex
   if skyBases[baseIndex].reputation.level < 0:
-    raise newException(MissionAcceptingError, "Your reputation in this base is too low to receive any mission.")
+    raise newException(exceptn = MissionAcceptingError,
+        message = "Your reputation in this base is too low to receive any mission.")
   var missionsLimit = case skyBases[baseIndex].reputation.level
     of 0..25:
       1
@@ -116,11 +121,13 @@ proc acceptMission*(missionIndex: Natural) {.sideEffect, raises: [
     if missionsLimit <= 0:
       break
   if missionsLimit < 1:
-    raise newException(MissionAcceptingError, "You can't take any more missions from this base.")
+    raise newException(exceptn = MissionAcceptingError,
+        message = "You can't take any more missions from this base.")
   var mission = skyBases[baseIndex].missions[missionIndex]
   if mission.mType == deliver and freeCargo(amount = -(itemsList[
       mission.itemIndex].weight)) < 0:
-    raise newException(MissionAcceptingError, "You don't have enough cargo space for take this mission.")
+    raise newException(exceptn = MissionAcceptingError,
+        message = "You don't have enough cargo space for take this mission.")
   if mission.mType == passenger:
     var haveCabin = false
     for module in playerShip.modules:
@@ -134,22 +141,23 @@ proc acceptMission*(missionIndex: Natural) {.sideEffect, raises: [
         if haveCabin:
           break
     if not haveCabin:
-      raise newException(MissionAcceptingError, "You don't have proper (or free) cabin for this passenger.")
+      raise newException(exceptn = MissionAcceptingError,
+          message = "You don't have proper (or free) cabin for this passenger.")
   mission.startBase = baseIndex
   mission.finished = false
   var acceptMessage = "You accepted the mission to "
   case mission.mType
   of deliver:
-    acceptMessage.add("'Deliver " & itemsList[mission.itemIndex].name & "'.")
+    acceptMessage.add(y = "'Deliver " & itemsList[mission.itemIndex].name & "'.")
     updateCargo(ship = playerShip, protoIndex = mission.itemIndex, amount = 1)
   of destroy:
-    acceptMessage.add("'Destroy " & protoShipsList[mission.shipIndex].name & "'.")
+    acceptMessage.add(y = "'Destroy " & protoShipsList[mission.shipIndex].name & "'.")
   of patrol:
-    acceptMessage.add("'Patrol selected area'.")
+    acceptMessage.add(y = "'Patrol selected area'.")
   of explore:
-    acceptMessage.add("'Explore selected area'.")
+    acceptMessage.add(y = "'Explore selected area'.")
   of passenger:
-    acceptMessage.add("'Transpor passenger to base'.")
+    acceptMessage.add(y = "'Transpor passenger to base'.")
     let
       passengerBase = (if getRandom(min = 1, max = 100) <
           60: baseIndex else: getRandom(min = skyBases.low,
@@ -178,7 +186,7 @@ proc acceptMission*(missionIndex: Natural) {.sideEffect, raises: [
     for j in 1 .. attributesList.len:
       attributes.add(y = MobAttributeRecord(level: getRandom(min = 3,
           max = maxAttributeLevel), experience: 0))
-    playerShip.crew.add(MemberData(name: generateMemberName(gender = gender,
+    playerShip.crew.add(y = MemberData(name: generateMemberName(gender = gender,
         factionIndex = skyBases[passengerBase].owner), gender: gender,
         health: 100, tired: 100, skills: @[], hunger: 0, thirst: 0, order: rest,
         previousOrder: rest, orderTime: 15, orders: [0.Natural, 0, 0, 0, 0, 0,
@@ -193,11 +201,11 @@ proc acceptMission*(missionIndex: Natural) {.sideEffect, raises: [
         module.owner[0] = playerShip.crew.high
         break
     mission.data = playerShip.crew.high
-  skyBases[baseIndex].missions.delete(missionIndex)
-  acceptedMissions.add(mission)
+  skyBases[baseIndex].missions.delete(i = missionIndex)
+  acceptedMissions.add(y = mission)
   skyMap[mission.targetX][mission.targetY].missionIndex = acceptedMissions.high
   addMessage(message = acceptMessage, mType = missionMessage)
-  let traderIndex = findMember(talk)
+  let traderIndex = findMember(order = talk)
   gainExp(amount = 1, skillNumber = talkingSkill, crewIndex = traderIndex)
   gameStats.acceptedMissions.inc
   updateGame(minutes = 5)
@@ -205,7 +213,7 @@ proc acceptMission*(missionIndex: Natural) {.sideEffect, raises: [
 # Temporary code for interfacing with Ada
 
 proc finishAdaMission(missionIndex: cint): cstring {.raises: [], tags: [
-    WriteIOEffect, RootEffect], exportc.} =
+    WriteIOEffect, RootEffect], exportc, contractual.} =
   try:
     finishMission(missionIndex = (missionIndex - 1).Natural)
   except MissionFinishingError:
@@ -215,14 +223,14 @@ proc finishAdaMission(missionIndex: cint): cstring {.raises: [], tags: [
   return ""
 
 proc autoAdaFinishMissions(): cstring {.raises: [], tags: [WriteIOEffect,
-    RootEffect], exportc.} =
+    RootEffect], exportc, contractual.} =
   try:
     return autoFinishMissions().cstring
   except KeyError, IOError, Exception:
     return ""
 
 proc acceptAdaMission(missionIndex: cint): cstring {.raises: [], tags: [
-    WriteIOEffect, RootEffect], exportc.} =
+    WriteIOEffect, RootEffect], exportc, contractual.} =
   try:
     acceptMission(missionIndex = missionIndex - 1)
     return "".cstring
