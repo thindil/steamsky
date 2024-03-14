a9af2aaee1e342802874f0e2881be98deb89db58
thindil <thindil@laeran.pl>
2024-03-14 04:22:51 +0000

    chore: fixed errors reported by nimalyzer
    FossilOrigin-Name: 4871d7100d1bbfa605310b0ab8076a25e699c585b44ff604ebea25d342569059

--- nim/src/crewinventory.nim
+++ nim/src/crewinventory.nim
@@ -1,4 +1,4 @@
-# Copyright 2022-2023 Bartek thindil Jasicki
+# Copyright 2022-2024 Bartek thindil Jasicki
 #
 # This file is part of Steam Sky.
 #
@@ -16,12 +16,14 @@
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
 import std/tables
+import contracts
 import game, shipscargo, types, utils
 
 proc findItem*(inventory: seq[InventoryData];
     protoIndex: Natural = 0; itemType: string = "";
     durability: ItemsDurability = ItemsDurability.high;
-    quality: Positive = 100): int {.sideEffect, raises: [], tags: [].} =
+    quality: Positive = 100): int {.sideEffect, raises: [], tags: [],
+        contractual.} =
   ## Find the index of the selected item in the selected inventory
   ##
   ## * inventory  - the inventory in which the item will be looking for
@@ -34,28 +36,31 @@ proc findItem*(inventory: seq[InventoryData];
   ##
   ## Returns the index of the item in the selected inventory which meet searching
   ## criteria or -1 if item not found.
-  try:
-    if protoIndex > 0:
-      for index, item in inventory.pairs:
-        if item.protoIndex == protoIndex and itemsList[protoIndex].value[1] <= quality:
-          if durability < ItemsDurability.high and item.durability == durability:
-            return index
-          else:
-            return index
-    elif itemType.len > 0:
-      for index, item in inventory.pairs:
-        if itemsList[item.protoIndex].itemType == itemType and itemsList[
-            item.protoIndex].value[1] <= quality:
-          if durability < ItemsDurability.high and item.durability == durability:
-            return index
-          else:
-            return index
-  except KeyError:
-    discard
-  return -1
+  ensure:
+    result in -1 .. inventory.high
+  body:
+    result = -1
+    try:
+      if protoIndex > 0:
+        for index, item in inventory.pairs:
+          if item.protoIndex == protoIndex and itemsList[protoIndex].value[1] <= quality:
+            if durability < ItemsDurability.high and item.durability == durability:
+              return index
+            else:
+              return index
+      elif itemType.len > 0:
+        for index, item in inventory.pairs:
+          if itemsList[item.protoIndex].itemType == itemType and itemsList[
+              item.protoIndex].value[1] <= quality:
+            if durability < ItemsDurability.high and item.durability == durability:
+              return index
+            else:
+              return index
+    except KeyError:
+      discard
 
 proc freeInventory*(memberIndex: Natural; amount: int): int {.sideEffect,
-    raises: [], tags: [].} =
+    raises: [], tags: [], contractual.} =
   ## Get the amount of free space in the selected player ship's crew member's
   ## inventory.
   ##
@@ -63,16 +68,19 @@ proc freeInventory*(memberIndex: Natural; amount: int): int {.sideEffect,
   ## * amount      - the amount of kilograms to add or remove during the check
   ##
   ## Returns the amount of kilograms of free space in the crew member's inventory
-  result = 50 + playerShip.crew[memberIndex].attributes[strengthIndex].level
-  for item in playerShip.crew[memberIndex].inventory:
-    try:
-      result = result - (itemsList[item.protoIndex].weight * item.amount)
-    except KeyError:
-      discard
-  result = result + amount
+  require:
+    memberIndex in playerShip.crew.low .. playerShip.crew.high
+  body:
+    result = 50 + playerShip.crew[memberIndex].attributes[strengthIndex].level
+    for item in playerShip.crew[memberIndex].inventory:
+      try:
+        result = result - (itemsList[item.protoIndex].weight * item.amount)
+      except KeyError:
+        discard
+    result = result + amount
 
 proc itemIsUsed*(memberIndex, itemIndex: Natural): bool {.sideEffect, raises: [
-    ], tags: [].} =
+    ], tags: [], contractual.} =
   ## Check if the item is currently used by the selected crew member in the
   ## player ship crew.
   ##
@@ -80,25 +88,31 @@ proc itemIsUsed*(memberIndex, itemIndex: Natural): bool {.sideEffect, raises: [
   ## * itemIndex   - the index of the item which will be check
   ##
   ## Returns true if the item is used, otherwise false
-  return itemIndex in playerShip.crew[memberIndex].equipment
+  require:
+    memberIndex in playerShip.crew.low .. playerShip.crew.high
+  body:
+    return itemIndex in playerShip.crew[memberIndex].equipment
 
 proc takeOffItem*(memberIndex, itemIndex: Natural) {.sideEffect, raises: [],
-    tags: [].} =
+    tags: [], contractual.} =
   ## Stop using the selected item by the selected the player's ship crew
   ## member.
   ##
   ## * memberIndex - the index of the crew member which will stop using the item
   ## * itemIndex   - the index of the item to stop using
-  for item in playerShip.crew[memberIndex].equipment.mitems:
-    if item == itemIndex:
-      item = -1
-      break
+  require:
+    memberIndex in playerShip.crew.low .. playerShip.crew.high
+  body:
+    for item in playerShip.crew[memberIndex].equipment.mitems:
+      if item == itemIndex:
+        item = -1
+        break
 
 proc updateInventory*(memberIndex: Natural; amount: int;
     protoIndex: Natural = 0; durability: ItemsDurability = 0;
     inventoryIndex: int = -1; price: Natural = 0;
     ship: var ShipRecord) {.sideEffect, raises: [CrewNoSpaceError, KeyError],
-    tags: [].} =
+    tags: [], contractual.} =
   ## Update the inventory of the selected crew member.
   ##
   ## * memberIndex    - the index of the crew member which inventory will be updated
@@ -114,45 +128,48 @@ proc updateInventory*(memberIndex: Natural; amount: int;
   ## * ship           - the ship in which the crew member inventory will be updated
   ##
   ## Returns the updated ship argument
-  var itemIndex: int
-  if inventoryIndex == -1:
-    if durability > 0:
-      itemIndex = findItem(inventory = ship.crew[memberIndex].inventory,
-          protoIndex = protoIndex, durability = durability)
-    else:
-      itemIndex = findItem(inventory = ship.crew[memberIndex].inventory,
-          protoIndex = protoIndex)
-  else:
-    itemIndex = inventoryIndex
-  if amount > 0:
-    let weight = if itemIndex > 0:
-        itemsList[ship.crew[memberIndex].inventory[
-            itemIndex].protoIndex].weight * amount
+  require:
+    memberIndex in playerShip.crew.low .. playerShip.crew.high
+  body:
+    var itemIndex: int
+    if inventoryIndex == -1:
+      if durability > 0:
+        itemIndex = findItem(inventory = ship.crew[memberIndex].inventory,
+            protoIndex = protoIndex, durability = durability)
       else:
-        itemsList[protoIndex].weight * amount
-    if freeInventory(memberIndex = memberIndex, amount = 0 - weight) < 0:
-      raise newException(exceptn = CrewNoSpaceError,
-          message = ship.crew[memberIndex].name & " doesn't have any free space in their inventory.")
-  else:
-    if itemIsUsed(memberIndex = memberIndex, itemIndex = itemIndex):
-      takeOffItem(memberIndex = memberIndex, itemIndex = itemIndex)
-  if itemIndex == -1:
-    ship.crew[memberIndex].inventory.add(y = InventoryData(
-        protoIndex: protoIndex, amount: amount, name: itemsList[
-        protoIndex].name, durability: durability, price: price))
-  else:
-    let newAmount = ship.crew[memberIndex].inventory[itemIndex].amount + amount
-    if newAmount == 0:
-      {.warning[UnsafeSetLen]: off.}
-      ship.crew[memberIndex].inventory.delete(i = itemIndex)
-      {.warning[UnsafeSetLen]: on.}
+        itemIndex = findItem(inventory = ship.crew[memberIndex].inventory,
+            protoIndex = protoIndex)
     else:
-      ship.crew[memberIndex].inventory[itemIndex].amount = newAmount
+      itemIndex = inventoryIndex
+    if amount > 0:
+      let weight = if itemIndex > 0:
+          itemsList[ship.crew[memberIndex].inventory[
+              itemIndex].protoIndex].weight * amount
+        else:
+          itemsList[protoIndex].weight * amount
+      if freeInventory(memberIndex = memberIndex, amount = 0 - weight) < 0:
+        raise newException(exceptn = CrewNoSpaceError,
+            message = ship.crew[memberIndex].name & " doesn't have any free space in their inventory.")
+    else:
+      if itemIsUsed(memberIndex = memberIndex, itemIndex = itemIndex):
+        takeOffItem(memberIndex = memberIndex, itemIndex = itemIndex)
+    if itemIndex == -1:
+      ship.crew[memberIndex].inventory.add(y = InventoryData(
+          protoIndex: protoIndex, amount: amount, name: itemsList[
+          protoIndex].name, durability: durability, price: price))
+    else:
+      let newAmount = ship.crew[memberIndex].inventory[itemIndex].amount + amount
+      if newAmount == 0:
+        {.warning[UnsafeSetLen]: off.}
+        ship.crew[memberIndex].inventory.delete(i = itemIndex)
+        {.warning[UnsafeSetLen]: on.}
+      else:
+        ship.crew[memberIndex].inventory[itemIndex].amount = newAmount
 
 proc damageItem*(inventory: var seq[InventoryData]; itemIndex: Natural;
     skillLevel: Natural = 0; memberIndex: int = -1;
     ship: var ShipRecord) {.sideEffect, raises: [KeyError, CrewNoSpaceError],
-    tags: [].} =
+    tags: [], contractual.} =
   ## Check if item in the inventory was damaged, if yes, update inventory and
   ## the ship cargo
   ##
@@ -208,7 +225,8 @@ proc damageItem*(inventory: var seq[InventoryData]; itemIndex: Natural;
     i.inc
 
 proc getTrainingToolQuality*(memberIndex: Natural;
-    skillIndex: Positive): Positive {.sideEffect, raises: [KeyError], tags: [].} =
+    skillIndex: Positive): Positive {.sideEffect, raises: [KeyError], tags: [],
+        contractual.} =
   ## Get the required tools quality for the selected skill
   ##
   ## * memberIndex - the index of the crew member in the player ship
@@ -216,40 +234,47 @@ proc getTrainingToolQuality*(memberIndex: Natural;
   ##
   ## Returns numeric value for the minimum training tool quality required to
   ## train the selected skill.
-  result = 100
-  for skill in playerShip.crew[memberIndex].skills:
-    if skill.index == skillIndex:
-      for quality in skillsList[skillIndex].toolsQuality:
-        if skill.level <= quality.level:
-          return quality.quality
+  require:
+    memberIndex in playerShip.crew.low .. playerShip.crew.high
+  body:
+    result = 100
+    for skill in playerShip.crew[memberIndex].skills:
+      if skill.index == skillIndex:
+        for quality in skillsList[skillIndex].toolsQuality:
+          if skill.level <= quality.level:
+            return quality.quality
 
 # Temporary code for interfacing with Ada
 
 proc findAdaItem(inventory: array[128, AdaInventoryData]; protoIndex: cint;
     itemType: cstring; durability: cint; quality: cint): cint {.sideEffect,
-    raises: [], tags: [], exportc.} =
+    raises: [], tags: [], exportc, contractual.} =
   return (findItem(inventory = inventoryToNim(inventory = inventory),
       protoIndex = protoIndex, itemType = $itemType, durability = durability,
       quality = quality) + 1).cint
 
-proc freeAdaInventory(memberIndex, amount: cint): cint {.raises: [], tags: [], exportc.} =
+proc freeAdaInventory(memberIndex, amount: cint): cint {.raises: [], tags: [],
+    exportc, contractual.} =
   return freeInventory(memberIndex = (memberIndex - 1).Natural,
       amount = amount).cint
 
-proc itemAdaIsUsed(memberIndex, itemIndex: cint): cint {.raises: [], tags: [], exportc.} =
+proc itemAdaIsUsed(memberIndex, itemIndex: cint): cint {.raises: [], tags: [],
+    exportc, contractual.} =
   return itemIsUsed(memberIndex = (memberIndex - 1), itemIndex = (itemIndex - 1)).ord.cint
 
-proc takeAdaOffItem(memberIndex, itemIndex: cint) {.raises: [], tags: [], exportc.} =
+proc takeAdaOffItem(memberIndex, itemIndex: cint) {.raises: [], tags: [],
+    exportc, contractual.} =
   takeOffItem(memberIndex = (memberIndex - 1), itemIndex = (itemIndex - 1))
 
 proc equipmentToAda(memberIndex: cint; equipment: var array[0..6,
-    cint]) {.raises: [], tags: [], exportc.} =
+    cint]) {.raises: [], tags: [], exportc, contractual.} =
   for i in 0..6:
     equipment[i] = playerShip.crew[(memberIndex - 1)].equipment[
         i.EquipmentLocations].cint + 1
 
 proc updateAdaInventory(memberIndex, amount, protoIndex, durability,
-    inventoryIndex, price, inPlayerShip: cint): cint {.raises: [], tags: [], exportc.} =
+    inventoryIndex, price, inPlayerShip: cint): cint {.raises: [], tags: [],
+        exportc, contractual.} =
   try:
     if inPlayerShip == 1:
       updateInventory(memberIndex = (memberIndex - 1), amount = amount,
@@ -264,7 +289,8 @@ proc updateAdaInventory(memberIndex, amount, protoIndex, durability,
     return 0
 
 proc damageAdaItem(inventory: var array[128, AdaInventoryData]; itemIndex,
-    skillLevel, memberIndex, inPlayerShip: cint) {.raises: [], tags: [], exportc.} =
+    skillLevel, memberIndex, inPlayerShip: cint) {.raises: [], tags: [],
+        exportc, contractual.} =
   var nimInventory = inventoryToNim(inventory = inventory)
   try:
     if inPlayerShip == 1:
@@ -278,7 +304,7 @@ proc damageAdaItem(inventory: var array[128, AdaInventoryData]; itemIndex,
   inventory = inventoryToAda(inventory = nimInventory)
 
 proc getAdaTrainingToolQuality(memberIndex, skillIndex: cint): cint {.raises: [
-    ], tags: [], exportc.} =
+    ], tags: [], exportc, contractual.} =
   try:
     return getTrainingToolQuality(memberIndex = memberIndex - 1,
         skillIndex = skillIndex).cint
