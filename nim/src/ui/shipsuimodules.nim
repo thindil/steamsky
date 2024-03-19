cb82b8231afd2bb55cb5fbf97f63613c7b27840f
thindil <thindil@laeran.pl>
2024-03-19 04:08:22 +0000

    docs: added code documentation to showAssignCrewCommand procedure
    FossilOrigin-Name: c8e8851e3584f038fffba825f169443e466870fe0720086c3730324f0ca1a4e1

--- nim/src/ui/shipsuimodules.nim
+++ nim/src/ui/shipsuimodules.nim
@@ -1103,6 +1103,19 @@ proc updateAssignCrewCommand(clientData: cint; interp: PInterp; argc: cint;
 
 proc showAssignCrewCommand(clientData: cint; interp: PInterp; argc: cint;
     argv: openArray[cstring]): TclResults {.sideEffect, raises: [], tags: [RootEffect].} =
+  ## Show assign the crew member UI
+  ##
+  ## * clientData - the additional data for the Tcl command
+  ## * interp     - the Tcl interpreter on which the command was executed
+  ## * argc       - the amount of arguments entered for the command
+  ## * argv       - the list of the command's arguments
+  ##
+  ## The procedure always return tclOk
+  ##
+  ## Tcl:
+  ## ShowAssignCrew moduleindex
+  ## Moduleindex is the index of the module to which a new crew members will
+  ## be assigned.
   let
     moduleIndex = try:
         ($argv[1]).parseInt - 1
