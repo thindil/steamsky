f630dbd3f8292eff3535bb1167991102310de147
thindil <thindil@laeran.pl>
2024-03-27 04:25:25 +0000

    refactor: removed unused code
    FossilOrigin-Name: abf08217d128307ef4474408a351be8e5d6e512c389ff9d0784ab91f9c3e0f0d

--- src/ui/ships-ui-crew.adb
+++ src/ui/ships-ui-crew.adb
@@ -116,42 +116,6 @@ package body Ships.UI.Crew is
           (pathName => Main_Paned & ".shipinfoframe.crew.canvas.frame.table");
    end Update_Crew_Info;
 
-   -- ****o* SUCrew/SUCrew.Dismiss_Command
-   -- FUNCTION
-   -- Dismiss the selected crew member
-   -- PARAMETERS
-   -- Client_Data - Custom data send to the command. Unused
-   -- Interp      - Tcl interpreter in which command was executed. Unused
-   -- Argc        - Number of arguments passed to the command. Unused
-   -- Argv        - Values of arguments passed to the command.
-   -- RESULT
-   -- This function always return TCL_OK
-   -- COMMANDS
-   -- Dismiss memberindex
-   -- Memberindex is the index of the player ship crew member which will be
-   -- dismissed
-   -- SOURCE
---   function Dismiss_Command
---     (Client_Data: Integer; Interp: Tcl.Tcl_Interp; Argc: Interfaces.C.int;
---      Argv: CArgv.Chars_Ptr_Ptr) return Interfaces.C.int with
---      Convention => C;
---      -- ****
---
---   function Dismiss_Command
---     (Client_Data: Integer; Interp: Tcl.Tcl_Interp; Argc: Interfaces.C.int;
---      Argv: CArgv.Chars_Ptr_Ptr) return Interfaces.C.int is
---      pragma Unreferenced(Client_Data, Interp, Argc);
---      Member_Index: constant Positive :=
---        Positive'Value(CArgv.Arg(Argv => Argv, N => 1));
---   begin
---      Show_Question
---        (Question =>
---           "Are you sure want to dismiss " &
---           To_String(Source => Player_Ship.Crew(Member_Index).Name) & "?",
---         Result => CArgv.Arg(Argv => Argv, N => 1));
---      return TCL_OK;
---   end Dismiss_Command;
-
    -- ****o* SUCrew/SUCrew.Set_Crew_Order_Command
    -- FUNCTION
    -- Set order for the selected crew member
@@ -2326,7 +2290,6 @@ package body Ships.UI.Crew is
          External_Name => "addAdaCrewCommands";
    begin
       Add_Ada_Commands;
---      Add_Command(Name => "Dismiss", Ada_Command => Dismiss_Command'Access);
       Add_Command
         (Name => "SetCrewOrder", Ada_Command => Set_Crew_Order_Command'Access);
       Add_Command
