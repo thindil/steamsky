f9a3e67b54ace4cda28338b27d434e0f4785cfa8
thindil <thindil@laeran.pl>
2024-03-27 04:10:48 +0000

    docs: added code documentation to toggleCrewMemberCommand procedure
    FossilOrigin-Name: 857565354255a9cd81b52093f7236eb824175fa3006b798d14c167554dbc4241

--- nim/src/ui/shipsuicrew.nim
+++ nim/src/ui/shipsuicrew.nim
@@ -292,6 +292,19 @@ proc orderForAllCommand(clientData: cint; interp: PInterp; argc: cint;
 
 proc toggleCrewMemberCommand(clientData: cint; interp: PInterp; argc: cint;
     argv: openArray[cstring]): TclResults {.sideEffect, raises: [], tags: [].} =
+  ## Select or deselect the selected crew member
+  ##
+  ## * clientData - the additional data for the Tcl command
+  ## * interp     - the Tcl interpreter on which the command was executed
+  ## * argc       - the amount of arguments entered for the command
+  ## * argv       - the list of the command's arguments
+  ##
+  ## The procedure always return tclOk
+  ##
+  ## Tcl:
+  ## ToggleCrewMember rowindex crewindex
+  ## Rowindex is the index of the row in which is the selected crew member,
+  ## crewindex is the index of the selected crew member.
   let row = try:
       ($argv[1]).parseInt
     except:
