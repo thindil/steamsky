d2344754ca6292f15dd157d30b2e6a911048f998
thindil <thindil@laeran.pl>
2024-03-28 04:26:17 +0000

    docs: added code documentation to showMemberTabCommand procedure
    FossilOrigin-Name: 9815eece55ef902ab11fb175bb24dbf2e6d210c30f7cb2ba05b8b2f980b78bb9

--- nim/src/ui/shipsuicrew.nim
+++ nim/src/ui/shipsuicrew.nim
@@ -388,6 +388,17 @@ proc setCrewOrderCommand(clientData: cint; interp: PInterp; argc: cint;
 
 proc showMemberTabCommand(clientData: cint; interp: PInterp; argc: cint;
     argv: openArray[cstring]): TclResults {.sideEffect, raises: [], tags: [].} =
+  ## Show the selected information about the selected crew member
+  ##
+  ## * clientData - the additional data for the Tcl command
+  ## * interp     - the Tcl interpreter on which the command was executed
+  ## * argc       - the amount of arguments entered for the command
+  ## * argv       - the list of the command's arguments
+  ##
+  ## The procedure always return tclOk
+  ##
+  ## Tcl:
+  ## ShowMemberTab
   let memberCanvas = ".memberdialog.canvas"
   tclEval(script = memberCanvas & " delete info")
   let
