c37792e129cfa612a61b278320ada78cdfa06894
thindil <thindil@laeran.pl>
2024-04-01 03:18:07 +0000

    docs: added code documentation to showMemberInfoCommand procedure
    FossilOrigin-Name: f6cfd5481faec01e646a51bc3984db0b0526bf27aa1e5194c90670741c81de19

--- nim/src/ui/shipsuicrew.nim
+++ nim/src/ui/shipsuicrew.nim
@@ -425,6 +425,18 @@ proc showMemberTabCommand(clientData: cint; interp: PInterp; argc: cint;
 
 proc showMemberInfoCommand(clientData: cint; interp: PInterp; argc: cint;
     argv: openArray[cstring]): TclResults {.sideEffect, raises: [], tags: [].} =
+  ## Show information about the selected crew member
+  ##
+  ## * clientData - the additional data for the Tcl command
+  ## * interp     - the Tcl interpreter on which the command was executed
+  ## * argc       - the amount of arguments entered for the command
+  ## * argv       - the list of the command's arguments
+  ##
+  ## The procedure always return tclOk
+  ##
+  ## Tcl:
+  ## ShowMemberInfo memberindex
+  ## MemberIndex is the index of the crew member to show
   let
     memberIndex = try:
         ($argv[1]).parseInt - 1
