7794ff9a7880e5b1ae2c737e20c280aaa9b17cbe
thindil <thindil@laeran.pl>
2024-04-12 03:25:51 +0000

    refactor: started work on move updateInventoryCommand to Nim
    FossilOrigin-Name: 0f2bc03659f694ddc8b2d0449661e93c8643f5bd4743cb931368c90615a44769

--- nim/src/ui/shipsuicrewinventory.nim
+++ nim/src/ui/shipsuicrewinventory.nim
@@ -15,6 +15,32 @@
 # You should have received a copy of the GNU General Public License
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
+import std/strutils
+import ../[game, tk]
+import table
+
+var
+  inventoryTable: TableWidget
+    ## The UI table with the list of items in the crew member's inventory
+  memberIndex: Natural
+    ## The index of the selected crew member
+  inventoryIndexes: seq[Natural]
+    ## The list of indexes of items in the crew member's inventory
+
+proc updateInventoryCommand(clientData: cint; interp: PInterp; argc: cint;
+    argv: openArray[cstring]): TclResults =
+  memberIndex = ($argv[1]).parseInt
+  if inventoryTable.row > 1:
+    inventoryTable.clearTable
+  let member = playerShip.crew[memberIndex]
+  if inventoryIndexes.len != member.inventory.len:
+    inventoryIndexes = @[]
+    for index, _ in member.inventory:
+      inventoryIndexes.add(y = index)
+  let
+    page = (if argc == 3: ($argv[2]).parseInt else: 1)
+  return tclOk
+
 proc addCommands*() {.sideEffect, raises: [], tags: [].} =
   ## Adds Tcl commands related to the crew UI
   discard
