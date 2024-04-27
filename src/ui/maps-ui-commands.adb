9a750fd5f9486ac4f90358f37de41c0d1134ad68
thindil <thindil@laeran.pl>
2024-04-27 03:20:25 +0000

    fix: continue work on redesing of creating Tcl command so the game doesn't crash on Windows
    FossilOrigin-Name: 6d249cf384f637a8b6d65c2b485029957a10ca66818263e204e01804744baff8

--- src/ui/maps-ui-commands.adb
+++ src/ui/maps-ui-commands.adb
@@ -125,6 +125,48 @@ package body Maps.UI.Commands is
       Convention => C,
       External_Name => "showSkyMapCommand";
 
+   function Move_Mouse_Command
+     (Client_Data: Integer; Interp: Tcl.Tcl_Interp; Argc: Interfaces.C.int;
+      Argv: CArgv.Chars_Ptr_Ptr) return Interfaces.C.int with
+      Import => True,
+      Convention => C,
+      External_Name => "moveMouseCommand";
+
+   function Toggle_Full_Screen_Command
+     (Client_Data: Integer; Interp: Tcl.Tcl_Interp; Argc: Interfaces.C.int;
+      Argv: CArgv.Chars_Ptr_Ptr) return Interfaces.C.int with
+      Import => True,
+      Convention => C,
+      External_Name => "toggleFullScreenCommand";
+
+   function Resize_Last_Messages_Command
+     (Client_Data: Integer; Interp: Tcl.Tcl_Interp; Argc: Interfaces.C.int;
+      Argv: CArgv.Chars_Ptr_Ptr) return Interfaces.C.int with
+      Import => True,
+      Convention => C,
+      External_Name => "resizeLastMessagesCommand";
+
+   function Show_Game_Menu_Command
+     (Client_Data: Integer; Interp: Tcl.Tcl_Interp; Argc: Interfaces.C.int;
+      Argv: CArgv.Chars_Ptr_Ptr) return Interfaces.C.int with
+      Import => True,
+      Convention => C,
+      External_Name => "showGameMenuCommand";
+
+   function Invoke_Menu_Command
+     (Client_Data: Integer; Interp: Tcl.Tcl_Interp; Argc: Interfaces.C.int;
+      Argv: CArgv.Chars_Ptr_Ptr) return Interfaces.C.int with
+      Import => True,
+      Convention => C,
+      External_Name => "invokeMenuCommand";
+
+   function Set_Ship_Speed_Command
+     (Client_Data: Integer; Interp: Tcl.Tcl_Interp; Argc: Interfaces.C.int;
+      Argv: CArgv.Chars_Ptr_Ptr) return Interfaces.C.int with
+      Import => True,
+      Convention => C,
+      External_Name => "setShipSpeedCommand";
+
    procedure Add_Maps_Commands is
       use Utils.UI;
    begin
@@ -159,6 +201,20 @@ package body Maps.UI.Commands is
         (Name => "ShowStats", Ada_Command => Show_Stats_Command'Access);
       Add_Command
         (Name => "ShowSkyMap", Ada_Command => Show_Sky_Map_Command'Access);
+      Add_Command
+        (Name => "MoveCursor", Ada_Command => Move_Mouse_Command'Access);
+      Add_Command
+        (Name => "ToggleFullScreen",
+         Ada_Command => Toggle_Full_Screen_Command'Access);
+      Add_Command
+        (Name => "ResizeLastMessages",
+         Ada_Command => Resize_Last_Messages_Command'Access);
+      Add_Command
+        (Name => "ShowGameMenu", Ada_Command => Show_Game_Menu_Command'Access);
+      Add_Command
+        (Name => "InvokeMenu", Ada_Command => Invoke_Menu_Command'Access);
+      Add_Command
+        (Name => "SetShipSpeed", Ada_Command => Set_Ship_Speed_Command'Access);
    end Add_Maps_Commands;
 
 end Maps.UI.Commands;
