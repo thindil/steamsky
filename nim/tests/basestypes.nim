ccb222a38dc1f31e28497ac3526d36b23a01ef5c
thindil <thindil@laeran.pl>
2024-04-29 03:10:24 +0000

    tests: split tests on smaller ones
    FossilOrigin-Name: 675eb6c9a8997ff705005be34d837df38a5fb8f5c6695903df2eaa9799181e1a

--- nim/tests/basestypes.nim
+++ nim/tests/basestypes.nim
@@ -8,18 +8,18 @@ suite "Unit tests for basestypes module":
   loadItems("../bin/data/items.dat")
   loadBasesTypes("../bin/data/bases.dat")
 
-  test "Testing getPrice.":
-    checkpoint "Get the price of a non-buyable item."
+  test "Get the price of a non-buyable item.":
     check:
       getPrice("0", 1) == 0
-    checkpoint "Get the price of a buyable item."
+
+  test "Get the price of a buyable item.":
     check:
       getPrice("1", 2) > 0
 
-  test "Testing isBuyable.":
-    checkpoint "Check if the item is non-buyable."
+  test "Check if the item is non-buyable.":
     check:
       not isBuyable("0", 1)
-    checkpoint "Check if the item is buyable."
+
+  test "Check if the item is buyable.":
     check:
       isBuyable("1", 2)
