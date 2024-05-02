126c7ac7c59b2dde5c8d698cdb6c64a4ca46037c
thindil <thindil@laeran.pl>
2024-05-02 03:25:18 +0000

    refactor: started work on move showCargoCommand to Nim
    FossilOrigin-Name: b0c0953bf92fa457b149618f125be1bbce5c785e04117cd604c3229b3b57e5d5

--- nim/src/ui/shipsuicargo.nim
+++ nim/src/ui/shipsuicargo.nim
@@ -15,11 +15,39 @@
 # You should have received a copy of the GNU General Public License
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
-import ../tk
+import std/strutils
+import ../[game, tk]
+import coreui, table
+
+var
+  cargoTable: TableWidget
+    ## The UI table with all items of the player's ship's cargo
+  cargoIndexes: seq[Natural]
+    ## The list of indexes of the items in the cargo
+
+proc showCargoCommand(clientData: cint; interp: PInterp; argc: cint;
+    argv: openArray[cstring]): TclResults {.exportc.} =
+  let
+    shipCanvas = mainPaned & ".shipinfoframe.cargo.canvas"
+    cargoInfoFrame = shipCanvas & ".frame"
+    res = tclEval2(script = "grid size " & cargoInfoFrame).split
+    rows = res[1].parseInt
+  deleteWidgets(startIndex = 3, endIndex = rows - 1, frame = cargoInfoFrame)
+  cargoTable = createTable(parent = cargoInfoFrame, headers = @["", "Name",
+      "Durability", "Type", "Amount", "Weight"],
+      scrollbar = mainPaned & ".shipinfoframe.cargo.scrolly",
+      command = "SortShipCargo",
+      tooltipText = "Press mouse button to sort the cargo.")
+  if cargoIndexes.len != playerShip.cargo.len:
+    cargoIndexes = @[]
+    for index, _ in playerShip.cargo:
+      cargoIndexes.add(y = index)
+  return tclOk
 
 proc addCommands*() {.sideEffect, raises: [], tags: [].} =
   ## Adds Tcl commands related to the crew UI
   try:
     discard
+#    addCommand("ShowCargo", showCargoCommand)
   except:
     showError(message = "Can't add a Tcl command.")
