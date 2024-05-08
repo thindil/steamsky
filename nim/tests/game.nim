c50a4531667b89ca46686f8e956c9a52e5fbebec
thindil <thindil@laeran.pl>
2024-05-08 03:12:18 +0000

    tests: split tests on smaller ones
    FossilOrigin-Name: ce4e7107f83898f7af54343cd610e7667ce07b327d74f85d93720b2bc2e1d4b5

--- nim/tests/game.nim
+++ nim/tests/game.nim
@@ -6,10 +6,10 @@ suite "Unit tests for game module":
   checkpoint "Loading the game data."
   loadData("../bin/data/game.dat")
 
-  test "Testing findSkillIndex.":
-    checkpoint "Find an index of an exisiting skill"
+  test "Find an index of an exisiting skill":
     check:
       findSkillIndex(skillName = "Piloting") == 1
-    checkpoint "Not find an index of a non-existing skill"
+
+  test "Not find an index of a non-existing skill":
     check:
       findSkillIndex(skillName = "sdfwerwerwe") == 0
