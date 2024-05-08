2b269ca83a5b70837e50b393c040cf5663b438fe
thindil <thindil@laeran.pl>
2024-05-08 03:15:35 +0000

    docs: added code documentation to giveItemCommand procedure
    FossilOrigin-Name: 3443b7d2338c6da5bffc988862ece40adf5fde08ccc679be720a0025e9968c69

--- nim/src/ui/shipsuicargo.nim
+++ nim/src/ui/shipsuicargo.nim
@@ -346,6 +346,18 @@ proc showGiveItemCommand(clientData: cint; interp: PInterp; argc: cint;
 proc giveItemCommand(clientData: cint; interp: PInterp; argc: cint;
     argv: openArray[cstring]): TclResults {.sideEffect, raises: [], tags: [
     RootEffect], exportc.} =
+  ## Give selected amount of the selected item from the ship's cargo to the
+  ## selected crew member
+  ##
+  ## * clientData - the additional data for the Tcl command
+  ## * interp     - the Tcl interpreter on which the command was executed
+  ## * argc       - the amount of arguments entered for the command
+  ## * argv       - the list of the command's arguments
+  ##
+  ## The procedure always return tclOk
+  ##
+  ## Tcl:
+  ## GiveItem
   let
     itemDialog = ".itemdialog"
     spinBox = itemDialog & ".giveamount"
