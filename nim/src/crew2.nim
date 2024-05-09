5a0c014e39ed03762fc0fd7626dc00a06d342daa
thindil <thindil@laeran.pl>
2024-05-09 03:14:32 +0000

    chore: fixed some errors reported by nimalyzer
    FossilOrigin-Name: db13d9c34f14704892de006df9fc5c48e09f10f8bc25ee8112cb0745143c1b55

--- nim/src/crew2.nim
+++ nim/src/crew2.nim
@@ -1,4 +1,4 @@
-# Copyright 2023 Bartek thindil Jasicki
+# Copyright 2023-2024 Bartek thindil Jasicki
 #
 # This file is part of Steam Sky.
 #
@@ -15,20 +15,25 @@
 # You should have received a copy of the GNU General Public License
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
+## Provides code related to the player's ship's crew like resting. Split from
+## the crew module to avoid circular dependencies.
+
+import contracts
 import crew, game, game2, types, shipsmovement
 
 proc waitForRest*() {.sideEffect, raises: [KeyError, IOError, Exception],
-    tags: [WriteIOEffect, RootEffect].} =
+    tags: [WriteIOEffect, RootEffect], contractual.} =
   ## Wait until the whole player's ship crew is rested
-  var timeNeeded = 0
-  for index, member in playerShip.crew.pairs:
+  var timeNeeded: Natural = 0
+  for index, member in playerShip.crew:
     if member.tired > 0 and member.order == rest:
-      var tempTimeNeeded = 0
-      let cabinIndex = findCabin(memberIndex = index)
+      var tempTimeNeeded: Natural = 0
+      let cabinIndex: int = findCabin(memberIndex = index)
       if cabinIndex > -1:
-        let damage = 1.0 - (playerShip.modules[cabinIndex].durability.float /
-            playerShip.modules[cabinIndex].maxDurability.float)
-        var cabinBonus = playerShip.modules[cabinIndex].cleanliness - (
+        let damage: float = 1.0 - (playerShip.modules[
+            cabinIndex].durability.float / playerShip.modules[
+            cabinIndex].maxDurability.float)
+        var cabinBonus: Natural = playerShip.modules[cabinIndex].cleanliness - (
             playerShip.modules[cabinIndex].cleanliness.float * damage).Natural
         if cabinBonus == 0:
           cabinBonus = 1
@@ -36,8 +41,8 @@ proc waitForRest*() {.sideEffect, raises: [KeyError, IOError, Exception],
         if tempTimeNeeded == 0:
           tempTimeNeeded = 15
       else:
-        tempTimeNeeded = member.tired * 15
-      tempTimeNeeded = tempTimeNeeded + 15
+        tempTimeNeeded *= 15
+      tempTimeNeeded += 15
       if tempTimeNeeded > timeNeeded:
         timeNeeded = tempTimeNeeded
   if timeNeeded > 0:
@@ -46,7 +51,9 @@ proc waitForRest*() {.sideEffect, raises: [KeyError, IOError, Exception],
 
 # Temporary code for interfacing with Ada
 
-proc waitAdaForRest() {.raises: [], tags: [WriteIOEffect, RootEffect], exportc.} =
+proc waitAdaForRest() {.raises: [], tags: [WriteIOEffect, RootEffect], exportc,
+    contractual.} =
+  ## Temporary C binding
   try:
     waitForRest()
   except KeyError, IOError, Exception:
