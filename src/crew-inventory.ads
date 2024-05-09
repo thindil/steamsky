e6ff89d7801275467e8454cd7cbf33d72a1be824
thindil <thindil@laeran.pl>
2024-05-09 03:15:01 +0000

    chore: fixed AdaControl error
    FossilOrigin-Name: 98e52d6befc329c5857d3c3a4aaef44ef922306b3c02b3eb1b37ae951393c982

--- src/crew-inventory.ads
+++ src/crew-inventory.ads
@@ -15,7 +15,7 @@
 --    You should have received a copy of the GNU General Public License
 --    along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
-with Ships; use Ships;
+with Ships; use Ships; --## rule line off REDUCEABLE_SCOPE
 
 -- ****h* Crew/Inventory
 -- FUNCTION
