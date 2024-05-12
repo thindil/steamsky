d6fdb30aad6f56e4e334f19e145d9816b7650200
thindil <thindil@laeran.pl>
2024-05-12 03:14:48 +0000

    docs: added code documentation to showCargoItemInfoCommand procedure
    FossilOrigin-Name: 8ec8e9ee0a32323b0362cf67c0f218d7a6ddeb562d085fd7775a7b8dd75ea7d5

--- nim/src/ui/shipsuicargo.nim
+++ nim/src/ui/shipsuicargo.nim
@@ -482,6 +482,18 @@ proc dropItemCommand(clientData: cint; interp: PInterp; argc: cint;
 
 proc showCargoItemInfoCommand(clientData: cint; interp: PInterp; argc: cint;
     argv: openArray[cstring]): TclResults {.sideEffect, raises: [], tags: [], exportc.} =
+  ## Drop selected amount of the selected item from the ship's cargo
+  ##
+  ## * clientData - the additional data for the Tcl command
+  ## * interp     - the Tcl interpreter on which the command was executed
+  ## * argc       - the amount of arguments entered for the command
+  ## * argv       - the list of the command's arguments
+  ##
+  ## The procedure always return tclOk
+  ##
+  ## Tcl:
+  ## ShowCargoItemInfo itemindex
+  ## Itemindex is the index of the item which information will be show
   let itemIndex = try:
       ($argv[1]).parseInt - 1
     except:
