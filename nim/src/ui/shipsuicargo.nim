52eb3a326427dc7d5e195ae9405f1f3d89a9a68a
thindil <thindil@laeran.pl>
2024-05-13 03:20:04 +0000

    docs: added code documentation to updateMaxGiveAmountCommand procedure
    FossilOrigin-Name: 3408af1aa1c6d63bfade6ed511beaf15355f43acc07f670c9d0d2a84d78df718

--- nim/src/ui/shipsuicargo.nim
+++ nim/src/ui/shipsuicargo.nim
@@ -511,6 +511,18 @@ proc showCargoItemInfoCommand(clientData: cint; interp: PInterp; argc: cint;
 
 proc updateMaxGiveAmountCommand(clientData: cint; interp: PInterp; argc: cint;
     argv: openArray[cstring]): TclResults {.sideEffect, raises: [], tags: [], exportc.} =
+  ## Update max give amount after selecting the crew member
+  ##
+  ## * clientData - the additional data for the Tcl command
+  ## * interp     - the Tcl interpreter on which the command was executed
+  ## * argc       - the amount of arguments entered for the command
+  ## * argv       - the list of the command's arguments
+  ##
+  ## The procedure always return tclOk
+  ##
+  ## Tcl:
+  ## UpdateMaxGiveAmount itemindex
+  ## ItemIndex is the index of the item to give
   let
     itemIndex = try:
         ($argv[1]).parseInt
