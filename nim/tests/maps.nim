6be8f2986418b750715eafa932042b9989ef3a60
thindil <thindil@laeran.pl>
2024-05-15 03:05:17 +0000

    tests: split tests on smaller ones
    FossilOrigin-Name: cbc934bbdb1676fd714590222ed4f7d5eaa3bb47171f6669539d73030ceb3322

--- nim/tests/maps.nim
+++ nim/tests/maps.nim
@@ -5,18 +5,18 @@ suite "Unit tests for maps module":
 
   playerShip.skyX = 1
   playerShip.skyY = 1
+  var coord: int = 0
 
-  test "Testing normalizeCoord.":
-    var coord: int = 0
-    normalizeCoord(coord)
-    checkpoint "Normalize an invalid coordinate."
-    check:
-      coord == 1
-    checkpoint "Don't normalize a valid coordinate."
+  test "Normalize an invalid coordinate.":
     normalizeCoord(coord)
     check:
       coord == 1
 
-  test "Testing countDistance.":
+  test "Don't normalize a valid coordinate.":
+    normalizeCoord(coord)
+    check:
+      coord == 1
+
+  test "Counting a distance on the map.":
     check:
       countDistance(2, 2) == 1
