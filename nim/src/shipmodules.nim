55556412013b59ee985563486f94aaeae5690786
thindil <thindil@laeran.pl>
2024-05-15 03:05:05 +0000

    refactor: added contracts to procedures
    FossilOrigin-Name: cb53a0570b7d94fe5f7fa745d7af9dc300d0ac712969b1711a08802c700a6ad8

--- nim/src/shipmodules.nim
+++ nim/src/shipmodules.nim
@@ -1,4 +1,4 @@
-# Copyright 2022-2023 Bartek thindil Jasicki
+# Copyright 2022-2024 Bartek thindil Jasicki
 #
 # This file is part of Steam Sky.
 #
@@ -16,194 +16,202 @@
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
 import std/[strutils, tables, xmlparser, xmltree]
+import contracts
 import game, items, log, types
 
 proc loadModules*(fileName: string) {.sideEffect, raises: [DataLoadingError],
-    tags: [WriteIOEffect, ReadIOEffect, RootEffect].} =
+    tags: [WriteIOEffect, ReadIOEffect, RootEffect], contractual.} =
   ## Load available prototypes of ship's modules from the data file
   ##
   ## * fileName - the path to the file with ship's modules data which will be loaded
-  let modulesXml = try:
-      loadXml(path = fileName)
-    except XmlError, ValueError, IOError, OSError, Exception:
-      raise newException(exceptn = DataLoadingError,
-          message = "Can't load modules data file. Reason: " &
-          getCurrentExceptionMsg())
-  for moduleNode in modulesXml:
-    if moduleNode.kind != xnElement:
-      continue
-    let
-      moduleIndex: Natural = try:
-          moduleNode.attr(name = "index").parseInt()
-        except ValueError:
-          raise newException(exceptn = DataLoadingError,
-              message = "Can't add module '" & moduleNode.attr(name = "index") & "', invalid index.")
-      moduleAction: DataAction = try:
-          parseEnum[DataAction](moduleNode.attr(name = "action").toLowerAscii)
-        except ValueError:
-          DataAction.add
-    if moduleAction in [update, remove]:
-      if moduleIndex > modulesList.len():
+  require:
+    fileName.len > 0
+  body:
+    let modulesXml = try:
+        loadXml(path = fileName)
+      except XmlError, ValueError, IOError, OSError, Exception:
         raise newException(exceptn = DataLoadingError,
-            message = "Can't " & $moduleAction & " module '" & $moduleIndex & "', there is no module with that index.")
-    elif moduleIndex < modulesList.len():
-      raise newException(exceptn = DataLoadingError,
-          message = "Can't add module '" & $moduleIndex & "', there is an module with that index.")
-    if moduleAction == DataAction.remove:
-      {.warning[ProveInit]: off.}
-      {.warning[UnsafeDefault]: off.}
-      modulesList.del(key = moduleIndex)
-      {.warning[ProveInit]: on.}
-      {.warning[UnsafeDefault]: on.}
-      logMessage(message = "module removed: '" & $moduleIndex & "'",
-          debugType = everything)
-      continue
-    var module: BaseModuleData = if moduleAction == DataAction.update:
-        try:
-          modulesList[moduleIndex]
-        except ValueError:
+            message = "Can't load modules data file. Reason: " &
+            getCurrentExceptionMsg())
+    for moduleNode in modulesXml:
+      if moduleNode.kind != xnElement:
+        continue
+      let
+        moduleIndex: Natural = try:
+            moduleNode.attr(name = "index").parseInt()
+          except ValueError:
+            raise newException(exceptn = DataLoadingError,
+                message = "Can't add module '" & moduleNode.attr(
+                    name = "index") & "', invalid index.")
+        moduleAction: DataAction = try:
+            parseEnum[DataAction](moduleNode.attr(name = "action").toLowerAscii)
+          except ValueError:
+            DataAction.add
+      if moduleAction in [update, remove]:
+        if moduleIndex > modulesList.len():
+          raise newException(exceptn = DataLoadingError,
+              message = "Can't " & $moduleAction & " module '" & $moduleIndex & "', there is no module with that index.")
+      elif moduleIndex < modulesList.len():
+        raise newException(exceptn = DataLoadingError,
+            message = "Can't add module '" & $moduleIndex & "', there is an module with that index.")
+      if moduleAction == DataAction.remove:
+        {.warning[ProveInit]: off.}
+        {.warning[UnsafeDefault]: off.}
+        modulesList.del(key = moduleIndex)
+        {.warning[ProveInit]: on.}
+        {.warning[UnsafeDefault]: on.}
+        logMessage(message = "module removed: '" & $moduleIndex & "'",
+            debugType = everything)
+        continue
+      var module: BaseModuleData = if moduleAction == DataAction.update:
+          try:
+            modulesList[moduleIndex]
+          except ValueError:
+            BaseModuleData(repairSkill: 1, installTime: 1, size: 1)
+        else:
           BaseModuleData(repairSkill: 1, installTime: 1, size: 1)
+      var attribute = moduleNode.attr(name = "name")
+      if attribute.len() > 0:
+        module.name = attribute
+      attribute = moduleNode.attr(name = "type")
+      if attribute.len() > 0:
+        module.mType = try:
+            parseEnum[ModuleType](attribute.toLowerAscii)
+          except ValueError:
+            raise newException(exceptn = DataLoadingError,
+                message = "Can't " & $moduleAction & " module '" &
+                    $moduleIndex & "', invalid type of module.")
       else:
-        BaseModuleData(repairSkill: 1, installTime: 1, size: 1)
-    var attribute = moduleNode.attr(name = "name")
-    if attribute.len() > 0:
-      module.name = attribute
-    attribute = moduleNode.attr(name = "type")
-    if attribute.len() > 0:
-      module.mType = try:
-          parseEnum[ModuleType](attribute.toLowerAscii)
+        module.mType = ModuleType.any
+      attribute = moduleNode.attr(name = "weight")
+      if attribute.len() > 0:
+        module.weight = try:
+            attribute.parseInt()
         except ValueError:
           raise newException(exceptn = DataLoadingError,
-              message = "Can't " & $moduleAction & " module '" &
-                  $moduleIndex & "', invalid type of module.")
-    else:
-      module.mType = ModuleType.any
-    attribute = moduleNode.attr(name = "weight")
-    if attribute.len() > 0:
-      module.weight = try:
-          attribute.parseInt()
-      except ValueError:
-        raise newException(exceptn = DataLoadingError,
-            message = "Can't " & $moduleAction & " module '" & $moduleIndex & "', invalid value for module weight.")
-    attribute = moduleNode.attr(name = "value")
-    if attribute.len() > 0:
-      module.value = try:
-          attribute.parseInt()
-      except ValueError:
-        raise newException(exceptn = DataLoadingError,
-            message = "Can't " & $moduleAction & " module '" & $moduleIndex & "', invalid value for value field.")
-    attribute = moduleNode.attr(name = "maxvalue")
-    if attribute.len() > 0:
-      module.maxValue = try:
-          attribute.parseInt()
-      except ValueError:
-        raise newException(exceptn = DataLoadingError,
-            message = "Can't " & $moduleAction & " module '" & $moduleIndex & "', invalid value for maxvalue field.")
-    attribute = moduleNode.attr(name = "durability")
-    if attribute.len() > 0:
-      module.durability = try:
-          attribute.parseInt()
-      except ValueError:
-        raise newException(exceptn = DataLoadingError,
-            message = "Can't " & $moduleAction & " module '" & $moduleIndex & "', invalid value for module durability.")
-    attribute = moduleNode.attr(name = "material")
-    if attribute.len() > 0:
-      let itemIndex = findProtoItem(itemType = attribute)
-      if itemIndex == 0:
-        raise newException(exceptn = DataLoadingError,
-            message = "Can't " & $moduleAction & " module '" & $moduleIndex &
-            "', no items with type '" & attribute & "'.")
-      module.repairMaterial = attribute
-    attribute = moduleNode.attr(name = "skill")
-    if attribute.len() > 0:
-      let skillIndex = findSkillIndex(skillName = attribute)
-      if skillIndex == 0:
-        raise newException(exceptn = DataLoadingError,
-            message = "Can't " & $moduleAction & " module '" & $moduleIndex &
-            "', no skill named '" & attribute & "'.")
-      module.repairSkill = skillIndex
-    else:
-      module.repairSkill = 2
-    attribute = moduleNode.attr(name = "price")
-    if attribute.len() > 0:
-      module.price = try:
-          attribute.parseInt()
-      except ValueError:
-        raise newException(exceptn = DataLoadingError,
-            message = "Can't " & $moduleAction & " module '" & $moduleIndex & "', invalid value for module price.")
-    attribute = moduleNode.attr(name = "installtime")
-    if attribute.len() > 0:
-      module.installTime = try:
-          attribute.parseInt()
-      except ValueError:
-        raise newException(exceptn = DataLoadingError,
-            message = "Can't " & $moduleAction & " module '" & $moduleIndex & "', invalid value for module installation time.")
-    else:
-      module.installTime = 60
-    attribute = moduleNode.attr(name = "unique")
-    if attribute.len() > 0:
-      module.unique = true
-    else:
-      module.unique = false
-    attribute = moduleNode.attr(name = "size")
-    if attribute.len() > 0:
-      module.size = try:
-          attribute.parseInt()
-      except ValueError:
-        raise newException(exceptn = DataLoadingError,
-            message = "Can't " & $moduleAction & " module '" & $moduleIndex & "', invalid value for module size.")
-    attribute = moduleNode.attr(name = "maxowners")
-    if attribute.len() > 0:
-      module.maxOwners = try:
-          attribute.parseInt()
-      except ValueError:
-        raise newException(exceptn = DataLoadingError,
-            message = "Can't " & $moduleAction & " module '" & $moduleIndex & "', invalid value for module maxium owners amount.")
-    else:
-      module.maxOwners = 1
-    attribute = moduleNode.attr(name = "speed")
-    if attribute.len() > 0:
-      module.speed = try:
-          attribute.parseInt()
-      except ValueError:
-        raise newException(exceptn = DataLoadingError,
-            message = "Can't " & $moduleAction & " module '" & $moduleIndex & "', invalid value for module speed.")
-    else:
-      module.speed = 4
-    attribute = moduleNode.attr(name = "reputation")
-    if attribute.len() > 0:
-      module.reputation = try:
-          attribute.parseInt()
-      except ValueError:
-        raise newException(exceptn = DataLoadingError,
-            message = "Can't " & $moduleAction & " module '" & $moduleIndex & "', invalid value for module required reputation.")
-    else:
-      module.reputation = -100
-    attribute = moduleNode.innerText()
-    if attribute.len() > 0:
-      module.description = attribute
-    if moduleAction == DataAction.add:
-      logMessage(message = "Module added: '" & $moduleIndex & "'",
-          debugType = everything)
-    else:
-      logMessage(message = "Module updated: '" & $moduleIndex & "'",
-          debugType = everything)
-    modulesList[moduleIndex] = module
+              message = "Can't " & $moduleAction & " module '" & $moduleIndex & "', invalid value for module weight.")
+      attribute = moduleNode.attr(name = "value")
+      if attribute.len() > 0:
+        module.value = try:
+            attribute.parseInt()
+        except ValueError:
+          raise newException(exceptn = DataLoadingError,
+              message = "Can't " & $moduleAction & " module '" & $moduleIndex & "', invalid value for value field.")
+      attribute = moduleNode.attr(name = "maxvalue")
+      if attribute.len() > 0:
+        module.maxValue = try:
+            attribute.parseInt()
+        except ValueError:
+          raise newException(exceptn = DataLoadingError,
+              message = "Can't " & $moduleAction & " module '" & $moduleIndex & "', invalid value for maxvalue field.")
+      attribute = moduleNode.attr(name = "durability")
+      if attribute.len() > 0:
+        module.durability = try:
+            attribute.parseInt()
+        except ValueError:
+          raise newException(exceptn = DataLoadingError,
+              message = "Can't " & $moduleAction & " module '" & $moduleIndex & "', invalid value for module durability.")
+      attribute = moduleNode.attr(name = "material")
+      if attribute.len() > 0:
+        let itemIndex = findProtoItem(itemType = attribute)
+        if itemIndex == 0:
+          raise newException(exceptn = DataLoadingError,
+              message = "Can't " & $moduleAction & " module '" & $moduleIndex &
+              "', no items with type '" & attribute & "'.")
+        module.repairMaterial = attribute
+      attribute = moduleNode.attr(name = "skill")
+      if attribute.len() > 0:
+        let skillIndex = findSkillIndex(skillName = attribute)
+        if skillIndex == 0:
+          raise newException(exceptn = DataLoadingError,
+              message = "Can't " & $moduleAction & " module '" & $moduleIndex &
+              "', no skill named '" & attribute & "'.")
+        module.repairSkill = skillIndex
+      else:
+        module.repairSkill = 2
+      attribute = moduleNode.attr(name = "price")
+      if attribute.len() > 0:
+        module.price = try:
+            attribute.parseInt()
+        except ValueError:
+          raise newException(exceptn = DataLoadingError,
+              message = "Can't " & $moduleAction & " module '" & $moduleIndex & "', invalid value for module price.")
+      attribute = moduleNode.attr(name = "installtime")
+      if attribute.len() > 0:
+        module.installTime = try:
+            attribute.parseInt()
+        except ValueError:
+          raise newException(exceptn = DataLoadingError,
+              message = "Can't " & $moduleAction & " module '" & $moduleIndex & "', invalid value for module installation time.")
+      else:
+        module.installTime = 60
+      attribute = moduleNode.attr(name = "unique")
+      if attribute.len() > 0:
+        module.unique = true
+      else:
+        module.unique = false
+      attribute = moduleNode.attr(name = "size")
+      if attribute.len() > 0:
+        module.size = try:
+            attribute.parseInt()
+        except ValueError:
+          raise newException(exceptn = DataLoadingError,
+              message = "Can't " & $moduleAction & " module '" & $moduleIndex & "', invalid value for module size.")
+      attribute = moduleNode.attr(name = "maxowners")
+      if attribute.len() > 0:
+        module.maxOwners = try:
+            attribute.parseInt()
+        except ValueError:
+          raise newException(exceptn = DataLoadingError,
+              message = "Can't " & $moduleAction & " module '" & $moduleIndex & "', invalid value for module maxium owners amount.")
+      else:
+        module.maxOwners = 1
+      attribute = moduleNode.attr(name = "speed")
+      if attribute.len() > 0:
+        module.speed = try:
+            attribute.parseInt()
+        except ValueError:
+          raise newException(exceptn = DataLoadingError,
+              message = "Can't " & $moduleAction & " module '" & $moduleIndex & "', invalid value for module speed.")
+      else:
+        module.speed = 4
+      attribute = moduleNode.attr(name = "reputation")
+      if attribute.len() > 0:
+        module.reputation = try:
+            attribute.parseInt()
+        except ValueError:
+          raise newException(exceptn = DataLoadingError,
+              message = "Can't " & $moduleAction & " module '" & $moduleIndex & "', invalid value for module required reputation.")
+      else:
+        module.reputation = -100
+      attribute = moduleNode.innerText()
+      if attribute.len() > 0:
+        module.description = attribute
+      if moduleAction == DataAction.add:
+        logMessage(message = "Module added: '" & $moduleIndex & "'",
+            debugType = everything)
+      else:
+        logMessage(message = "Module updated: '" & $moduleIndex & "'",
+            debugType = everything)
+      modulesList[moduleIndex] = module
 
 proc getModuleType*(moduleIndex: Positive): string {.sideEffect, raises: [
-    KeyError], tags: [].} =
+    KeyError], tags: [], contractual.} =
   ## Get the type of the selected module from its name as enumeration
   ##
   ## * moduleIndex - the prototype index of the module
   ##
   ## Returns the string with the human readable name of the type of the
   ## selected module
-  result = $modulesList[moduleIndex].mType
-  let index = result.find({'A'..'Z'})
-  if index > 0:
-    result = result[0 .. index - 1] & " " & result[index].toLowerAscii & result[
-        index + 1 .. ^1]
-  result = result.capitalizeAscii
+  require:
+    moduleIndex in modulesList
+  body:
+    result = $modulesList[moduleIndex].mType
+    let index = result.find({'A'..'Z'})
+    if index > 0:
+      result = result[0 .. index - 1] & " " & result[index].toLowerAscii &
+          result[index + 1 .. ^1]
+    result = result.capitalizeAscii
 
 # Temporary code for interfacing with Ada
 
@@ -227,7 +235,7 @@ type
     reputation: cint
 
 proc getAdaModule(index: cint; adaModule: var AdaBaseModuleData) {.raises: [],
-    tags: [], exportc.} =
+    tags: [], exportc, contractual.} =
   adaModule = AdaBaseModuleData(name: "".cstring, mType: 0, weight: 0, value: 0,
       maxValue: 0, durability: 0, repairMaterial: "".cstring, repairSkill: 0,
       price: 0, installTime: 0, unique: 0, size: 0, description: "".cstring,
@@ -255,10 +263,12 @@ proc getAdaModule(index: cint; adaModule: var AdaBaseModuleData) {.raises: [],
   adaModule.speed = module.speed.cint
   adaModule.reputation = module.reputation.cint
 
-proc getAdaModulesAmount(): cint {.raises: [], tags: [], exportc.} =
+proc getAdaModulesAmount(): cint {.raises: [], tags: [], exportc,
+    contractual.} =
   return modulesList.len.cint
 
-proc getAdaModuleType(moduleIndex: cint): cstring {.raises: [], tags: [], exportc.} =
+proc getAdaModuleType(moduleIndex: cint): cstring {.raises: [], tags: [],
+    exportc, contractual.} =
   try:
     return getModuleType(moduleIndex = moduleIndex.Positive).cstring
   except KeyError:
