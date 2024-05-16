05e779d0eab9c6fb872a07a0436daf2cdfd63d5d
thindil <thindil@laeran.pl>
2024-05-16 03:08:03 +0000

    tests: split tests on smaller ones
    FossilOrigin-Name: ab8c833933ebc046790f3b7683b030583a4bd5e77f837b682bc0ce91eebb8e19

--- nim/tests/messages.nim
+++ nim/tests/messages.nim
@@ -3,36 +3,36 @@ include ../src/messages
 
 suite "Unit tests for messages module":
 
-  test "Testing getLastMessageIndex.":
+  test "Getting the last message index.":
     let messageIndex = getLastMessageIndex()
     addMessage("my message", default, white)
     check:
       getLastMessageIndex() == messageIndex + 1
 
-  test "Testing clearMessages.":
+  test "Clearing messages.":
     clearMessages()
     check:
       getLastMessageIndex() == -1
 
-  test "Testing formattedTime.":
+  test "Getting formatted game time.":
     check:
       formattedTime(1600, 1, 1, 10, 1) == "1600-01-01 10:01"
 
-  test "Testing getMessage.":
+  test "Getting an existent message.":
     gameDate = DateRecord(year: 1600, month: 1, day: 1, hour: 8, minutes: 1)
     addMessage("my message", otherMessage, green)
-    checkpoint "Getting an existent message."
     check:
       getMessage(1).message == "[1600-01-01 08:01] my message"
-    checkpoint "Getting a non-existent message."
+
+  test "Getting a non-existent message.":
     check:
       getMessage(10_000).message.len == 0
 
-  test "Testing messagesAmount.":
+  test "Getting messages amount.":
     check:
       messagesAmount(default) == 1
 
-  test "Testing restoreMessage.":
+  test "Restoring a message.":
     restoreMessage("my message", default, white)
     check:
       messagesAmount(default) == 2
