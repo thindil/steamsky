048fb79ed97a7d651c85b8ca0e7789063cd7d51a
thindil <thindil@laeran.pl>
2024-05-20 03:11:20 +0000

    tests: fixed tests for combat module
    FossilOrigin-Name: 7265d5b7f4445a982a1d8ca012b533c1d7f4e3fca10c02b0faf636bec6f1f9d8

--- nim/tests/combat.nim
+++ nim/tests/combat.nim
@@ -18,13 +18,12 @@ suite "Unit tests for combat module":
   playerShip.skyY = 200
   playerShip.crew = @[]
   playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural],
-      homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 2, 1, 1,
-      1, 0, 0], order: talk, loyalty: 100, skills: @[SkillInfo(index: 4,
-      level: 4,
-      experience: 0)], attributes: @[MobAttributeRecord(level: 3,
-          experience: 0),
+      homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 2, 1,
+      1, 1, 0, 0], order: talk, loyalty: 100, skills: @[SkillInfo(index: 4,
+      level: 4, experience: 0)], attributes: @[MobAttributeRecord(level: 3,
+      experience: 0), MobAttributeRecord(level: 3, experience: 0),
       MobAttributeRecord(level: 3, experience: 0), MobAttributeRecord(level: 3,
-      experience: 0), MobAttributeRecord(level: 3, experience: 0)], health: 100))
+      experience: 0)], health: 100, name: "Laeran"))
   playerShip.modules = @[]
   playerShip.modules.add(ModuleData(mType: ModuleType2.armor, protoIndex: 57,
       durability: 100))
