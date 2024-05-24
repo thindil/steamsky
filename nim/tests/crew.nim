e5c3bdaa685c104025abb62cd9a0eedb9b5a1b4c
thindil <thindil@laeran.pl>
2024-05-24 03:15:05 +0000

    tests: fixed tests for crew module
    FossilOrigin-Name: 5ef0ae46d91989e92e9b069481ac351caaa11879cc9e175a3b804bb1db6e3bdd

--- nim/tests/crew.nim
+++ nim/tests/crew.nim
@@ -18,7 +18,7 @@ suite "Unit tests for crew module":
       1, 0, 0], order: talk, loyalty: 100, skills: @[SkillInfo(index: 4, level: 4,
       experience: 0)], attributes: @[MobAttributeRecord(level: 3, experience: 0),
       MobAttributeRecord(level: 3, experience: 0), MobAttributeRecord(level: 3,
-      experience: 0), MobAttributeRecord(level: 3, experience: 0)]))
+      experience: 0), MobAttributeRecord(level: 3, experience: 0)], name: "Laeran"))
   playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural],
       homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 0, 1, 1,
       1, 0, 0], order: gunner, loyalty: 100, skills: @[SkillInfo(index: 4, level: 4,
