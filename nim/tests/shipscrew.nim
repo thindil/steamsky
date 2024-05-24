9cc82de305226d69cb2eb9065e80cb12098b6686
thindil <thindil@laeran.pl>
2024-05-24 03:14:41 +0000

    tests: split tests on smaller ones
    FossilOrigin-Name: 61b11953b15f9954e5e21e30207fc500cbe17af1a1cdfadc8b6f79636bb1cf42

--- nim/tests/shipscrew.nim
+++ nim/tests/shipscrew.nim
@@ -24,56 +24,56 @@ suite "Unit tests for shipscrew module":
       1, 0, 0], order: gunner, loyalty: 100))
   skyMap[1][1].baseIndex = 1
 
-  test "Testing generateMemberName.":
+  test "Generate a crew member's name.":
     check:
       generateMemberName('M', "POLEIS").len() > 0
 
-  test "Testing updateMorale.":
-    let oldMorale = playerShip.crew[0].morale[2]
+  let oldMorale = playerShip.crew[0].morale[2]
+  test "Raise the level of morale of the crew member.":
     let oldLevel = playerShip.crew[0].morale[1]
-    checkpoint "Raise the level of morale of the crew member."
     updateMorale(playerShip, 0, 1)
     check:
       playerShip.crew[0].morale[2] == oldMorale + 1 or playerShip.crew[
         0].morale[1] == oldLevel + 1
-    checkpoint "Lower the level of morale of the crew member."
+
+  test "Lower the level of morale of the crew member.":
     updateMorale(playerShip, 0, -1)
     check:
       playerShip.crew[0].morale[2] == oldMorale
 
-  test "Testing giveOrders.":
-    checkpoint "Give an order to the player."
+  test "Give an order to the player.":
     giveOrders(playerShip, 0, rest)
     check:
       playerShip.crew[0].order == talk
-    checkpoint "Give an order to the crew member."
+
+  test "Give an order to the crew member.":
     giveOrders(playerShip, 1, rest)
     check:
       playerShip.crew[1].order == rest
 
-  test "Testing updateOrders.":
+  test "Updating a crew members orders.":
     giveOrders(playerShip, 0, rest, -1, false)
     updateOrders(playerShip)
     check:
       playerShip.crew[0].order == talk
 
-  test "Testing getSkillLevel.":
-    checkpoint "Get skill level of the player."
+  test "Get skill level of the player.":
     check:
       getSkillLevel(playerShip.crew[0], 1) == 0
-    checkpoint "Get the skill level of the crew member."
+
+  test "Get the skill level of the crew member.":
     check:
       getSkillLevel(playerShip.crew[0], 4) == 7
 
-  test "Testing findMember.":
-    checkpoint "Find a member with the selected order."
+  test "Find a member with the selected order.":
     check:
       findMember(talk) == 0
-    checkpoint "Not find a member with the selected order."
+
+  test "Not find a member with the selected order.":
     check:
       findMember(defend) == -1
 
-  test "Testing gainExp.":
+  test "Gaining experience in a skill.":
     gainExp(10, 4, 0)
     check:
       playerShip.crew[0].skills[0].experience == 10
