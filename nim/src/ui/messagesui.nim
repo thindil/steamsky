adb57c37d5b1d39bdfc695d7d406ce8c5609b2e1
thindil <thindil@laeran.pl>
2024-05-29 03:27:52 +0000

    refactor: moved showMessage to Nim
    FossilOrigin-Name: 3ff5b80f6ab50d7e9a91ffff030eeccfc405f90ebffa7723c576bfba4a103feb

--- nim/src/ui/messagesui.nim
+++ nim/src/ui/messagesui.nim
@@ -15,7 +15,16 @@
 # You should have received a copy of the GNU General Public License
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
-import ../tk
+import std/strutils
+import ../[tk, types]
+
+proc showMessage(message: MessageData; messageView: string;
+    messagesType: MessageType) =
+  if message.kind != messagesType and messagesType != default:
+    return
+  let messageTag = (if message.color != white: " [list " & (
+      $message.color).toLowerAscii & "]" else: "")
+  tclEval(script = messageView & " insert end {" & message.message & "\n}" & messageTag)
 
 proc addCommands*() {.sideEffect, raises: [], tags: [].} =
   ## Adds Tcl commands related to the crew UI
@@ -24,3 +33,11 @@ proc addCommands*() {.sideEffect, raises: [], tags: [].} =
   except:
     showError(message = "Can't add a Tcl command.")
 
+# Temporary code for interfacing with Ada
+
+proc showAdaMessage(message, messageView: cstring; color, mType,
+    messagesType: cint) {.sideEffect, raises: [], tags: [], exportc.} =
+  let message = MessageData(message: $message, color: color.MessageColor,
+      kind: mType.MessageType)
+  showMessage(message = message, messageView = $messageView,
+      messagesType = messagesType.MessageType)
