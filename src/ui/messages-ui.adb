c8d34ac7aceaff548fc16102a6bc141a97b968af
thindil <thindil@laeran.pl>
2024-06-01 03:11:58 +0000

    refactor: removed unused code
    FossilOrigin-Name: a142a0ddb8526fa4db6537e01db603394452b492e222824af48a375e2fffb72c

--- src/ui/messages-ui.adb
+++ src/ui/messages-ui.adb
@@ -27,7 +27,6 @@ with Tcl.Tk.Ada.Widgets.TtkEntry;
 with Tcl.Tk.Ada.Widgets.TtkEntry.TtkComboBox;
 with Config;
 with CoreUI;
--- with Dialogs;
 with Utils.UI;
 
 package body Messages.UI is
@@ -125,19 +124,6 @@ package body Messages.UI is
       Convention => C,
       External_Name => "deleteMessagesCommand";
       -- ****
-
---   function Delete_Messages_Command
---     (Client_Data: Integer; Interp: Tcl.Tcl_Interp; Argc: Interfaces.C.int;
---      Argv: CArgv.Chars_Ptr_Ptr) return Interfaces.C.int is
---      pragma Unreferenced(Client_Data, Interp, Argc, Argv);
---      use Dialogs;
---
---   begin
---      Show_Question
---        (Question => "Are you sure you want to clear all messages?",
---         Result => "messages");
---      return TCL_OK;
---   end Delete_Messages_Command;
 
    -- ****o* MUI2/MUI2.Search_Messages_Command
    -- FUNCTION
