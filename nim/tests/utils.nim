13ef6497fed12f6860339c9e07aedfe9f4ef9b59
thindil <thindil@laeran.pl>
2024-06-02 03:09:50 +0000

    tests: split tests on smaller ones
    FossilOrigin-Name: 1ad43914ce7eca0d233c5eb8be4d65ca5bfd59a1abd51b50e882f88e53825cea

--- nim/tests/utils.nim
+++ nim/tests/utils.nim
@@ -9,15 +9,15 @@ suite "Unit tests for utils module":
           minutes: 0), DateRecord(year: 1, month: 1, day: 2, hour: 0, minutes: 0)) ==
           1
 
-  test "Testing generateRoboticName.":
+  test "Generating a robotic name.":
     check:
       generateRoboticName().len() > 0
 
-  test "Testing getRandom.":
-    checkpoint "Get a random value between 1 and 5"
+  test "Get a random value between 1 and 5":
     for i in 1..5:
       check:
         getrandom(1, 5) in 1..5
-    checkpoint "Get a random value between 5 and 5"
+
+  test "Get a random value between 5 and 5":
     check:
       getrandom(5, 5) == 5
