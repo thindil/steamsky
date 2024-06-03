366390b6d578beb29c48edfe52ce4da7d1f4b3d7
thindil <thindil@laeran.pl>
2024-06-03 03:01:23 +0000

    refactor: removed unused code
    FossilOrigin-Name: 909a55461d800ea36daad8ca6410ffdb87042440cdba1b1b8ad65faa4d97809c

--- src/ui/messages-ui.adb
+++ src/ui/messages-ui.adb
@@ -13,53 +13,12 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
--- with Ada.Characters.Handling;
--- with Ada.Strings.Fixed;
 with Interfaces.C; use Interfaces.C;
--- with Interfaces.C.Strings;
 with CArgv; use CArgv;
 with Tcl; use Tcl;
--- with Tcl.Ada;
--- with Tcl.Tk.Ada;
--- with Tcl.Tk.Ada.Widgets; use Tcl.Tk.Ada.Widgets;
--- with Tcl.Tk.Ada.Widgets.Text; use Tcl.Tk.Ada.Widgets.Text;
--- with Tcl.Tk.Ada.Widgets.TtkEntry;
--- with Tcl.Tk.Ada.Widgets.TtkEntry.TtkComboBox;
--- with Config;
--- with CoreUI;
 with Utils.UI;
 
 package body Messages.UI is
-
-   --## rule off REDUCEABLE_SCOPE
-   -- ****if* MUI2/MUI2.Show_Message
-   -- FUNCTION
-   -- Show the selected message to a player
-   -- PARAMETERS
-   -- Message       - The message to show
-   -- Messages_View - The treeview in which the message will be shown
-   -- Messages_Type - The selected type of messages to show
-   -- SOURCE
---   procedure Show_Message
---     (Message: Message_Data; Messages_View: Tk_Text;
---      Messages_Type: Message_Type) is
---      -- ****
---      use Interfaces.C.Strings;
---
---      procedure Show_Ada_Message
---        (M, M_View: chars_ptr; Color, M_Type, Me_Type: Integer) with
---         Import => True,
---         Convention => C,
---         External_Name => "showAdaMessageUI";
---   begin
---      Show_Ada_Message
---        (M => New_String(Str => To_String(Source => Message.Message)),
---         M_View => New_String(Str => Widget_Image(Win => Messages_View)),
---         Color => Message_Color'Pos(Message.Color),
---         M_Type => Message_Type'Pos(Message.M_Type),
---         Me_Type => Message_Type'Pos(Messages_Type));
---   end Show_Message;
-   --## rule on REDUCEABLE_SCOPE
 
    -- ****o* MUI2/MUI2.Show_Last_Messages_Command
    -- FUNCTION
@@ -144,99 +103,8 @@ package body Messages.UI is
       Argv: CArgv.Chars_Ptr_Ptr) return Interfaces.C.int with
       Import => True,
       Convention => C,
-      External_Name => "deleteMessagesCommand";
+      External_Name => "searchMessagesCommand";
       -- ****
-
---   function Search_Messages_Command
---     (Client_Data: Integer; Interp: Tcl.Tcl_Interp; Argc: Interfaces.C.int;
---      Argv: CArgv.Chars_Ptr_Ptr) return Interfaces.C.int is
---      pragma Unreferenced(Client_Data, Argc);
---      use Ada.Characters.Handling;
---      use Ada.Strings.Fixed;
---      use Tcl.Ada;
---      use Tcl.Tk.Ada.Widgets.TtkEntry.TtkComboBox;
---      use Config;
---      use CoreUI;
---
---      Frame_Name: constant String :=
---        Main_Paned & ".messagesframe.canvas.messages";
---      Type_Box: constant Ttk_ComboBox :=
---        Get_Widget
---          (pathName => Frame_Name & ".options.types", Interp => Interp);
---      Messages_Type: Message_Type;
---      Messages_View: constant Tk_Text :=
---        Get_Widget(pathName => Frame_Name & ".list.view", Interp => Interp);
---      Search_Text: constant String := CArgv.Arg(Argv => Argv, N => 1);
---   begin
---      Messages_Type :=
---        Message_Type'Val(Natural'Value(Current(ComboBox => Type_Box)));
---      configure(Widgt => Messages_View, options => "-state normal");
---      Delete
---        (TextWidget => Messages_View, StartIndex => "1.0", Indexes => "end");
---      if Search_Text'Length = 0 then
---         if Get_Messages_Order = OLDER_FIRST then
---            Show_Older_First_Loop :
---            for I in 1 .. Messages_Amount loop
---               Show_Message
---                 (Message => Get_Message(Message_Index => I),
---                  Messages_View => Messages_View,
---                  Messages_Type => Messages_Type);
---            end loop Show_Older_First_Loop;
---         else
---            Show_Newer_First_Loop :
---            for I in reverse 1 .. Messages_Amount loop
---               Show_Message
---                 (Message => Get_Message(Message_Index => I),
---                  Messages_View => Messages_View,
---                  Messages_Type => Messages_Type);
---            end loop Show_Newer_First_Loop;
---         end if;
---         Tcl_SetResult(interp => Interp, str => "1");
---         return TCL_OK;
---      end if;
---      if Get_Messages_Order = OLDER_FIRST then
---         Search_Older_First_Loop :
---         for I in 1 .. Messages_Amount loop
---            Show_Selected_Messages_Block :
---            declare
---               Message: constant Message_Data :=
---                 Get_Message(Message_Index => I);
---            begin
---               if Index
---                   (Source =>
---                      To_Lower(Item => To_String(Source => Message.Message)),
---                    Pattern => To_Lower(Item => Search_Text), From => 1) >
---                 0 then
---                  Show_Message
---                    (Message => Message, Messages_View => Messages_View,
---                     Messages_Type => Messages_Type);
---               end if;
---            end Show_Selected_Messages_Block;
---         end loop Search_Older_First_Loop;
---      else
---         Search_Newer_First_Loop :
---         for I in reverse 1 .. Messages_Amount loop
---            Show_Selected_Reverse_Messages_Block :
---            declare
---               Message: constant Message_Data :=
---                 Get_Message(Message_Index => I);
---            begin
---               if Index
---                   (Source =>
---                      To_Lower(Item => To_String(Source => Message.Message)),
---                    Pattern => To_Lower(Item => Search_Text), From => 1) >
---                 0 then
---                  Show_Message
---                    (Message => Message, Messages_View => Messages_View,
---                     Messages_Type => Messages_Type);
---               end if;
---            end Show_Selected_Reverse_Messages_Block;
---         end loop Search_Newer_First_Loop;
---      end if;
---      configure(Widgt => Messages_View, options => "-state disable");
---      Tcl_SetResult(interp => Interp, str => "1");
---      return TCL_OK;
---   end Search_Messages_Command;
 
    procedure Add_Commands is
       use Utils.UI;
