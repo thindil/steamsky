9991275ef3ae97dc27811292b8ab77e5b87364ee
thindil <thindil@laeran.pl>
2024-06-12 03:06:49 +0000

    chore: fixed errors reported by AdaControl
    FossilOrigin-Name: 9966b1b1d3bf7c5167b0ab17a8009d020f5b2afe6156cd823d1428b1c23b6eed

--- src/ui/maps-ui.ads
+++ src/ui/maps-ui.ads
@@ -13,8 +13,6 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
-
 -- ****h* Maps/MUI
 -- FUNCTION
 -- Provide code for showing and interacting with maps
