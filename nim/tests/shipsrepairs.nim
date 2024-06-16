d6aec28ac0adb3e5fa354b85fe4ec7848c68d62f
thindil <thindil@laeran.pl>
2024-06-16 03:11:10 +0000

    tests: fixed tests for shipsrepairs module
    FossilOrigin-Name: 45cd0a5454ff0fe19d7ace19712bcce74b9aed66c960b6beae50f2152bac6908

--- nim/tests/shipsrepairs.nim
+++ nim/tests/shipsrepairs.nim
@@ -4,7 +4,7 @@ include ../src/shipsrepairs
 
 suite "Unit tests for shipsrepairs module":
 
-  checkpoint  "Loading the game data."
+  checkpoint "Loading the game data."
   loadData("../bin/data/game.dat")
   loadItems("../bin/data/items.dat")
   loadCareers("../bin/data/careers.dat")
@@ -16,16 +16,18 @@ suite "Unit tests for shipsrepairs module":
       durability: 99, fuelUsage: 4, power: 2000, disabled: false,
       maxDurability: 100))
   playerShip.cargo = @[]
-  playerShip.cargo.add(InventoryData(protoIndex: 61, amount: 5, durability: 100))
-  playerShip.cargo.add(InventoryData(protoIndex: 5, amount: 100, durability: 100))
+  playerShip.cargo.add(InventoryData(protoIndex: 61, amount: 5,
+      durability: 100))
+  playerShip.cargo.add(InventoryData(protoIndex: 5, amount: 100,
+      durability: 100))
   playerShip.crew = @[]
   playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural],
-      homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 2, 1, 1,
-      1, 0, 0], order: repair, loyalty: 100, skills: @[SkillInfo(index: 4,
-      level: 4,
-      experience: 0)], attributes: @[MobAttributeRecord(level: 3, experience: 0),
+      homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 2, 1,
+      1, 1, 0, 0], order: repair, loyalty: 100, skills: @[SkillInfo(index: 4,
+      level: 4, experience: 0)], attributes: @[MobAttributeRecord(level: 3,
+      experience: 0), MobAttributeRecord(level: 3, experience: 0),
       MobAttributeRecord(level: 3, experience: 0), MobAttributeRecord(level: 3,
-      experience: 0), MobAttributeRecord(level: 3, experience: 0)]))
+      experience: 0)], equipment: [-1, -1, -1, -1, -1, -1, -1]))
   playerShip.crew[0].equipment[tool] = -1
 
   test "Repairing the player's ship.":
