19bd7135de63b60e2b783695d5170163d74f91ba
thindil <thindil@laeran.pl>
2024-06-16 03:21:21 +0000

    refactor: started work on move showTradeCommand to Nim
    FossilOrigin-Name: 7d443ae78b1bfb63b9c5a9c8ceb90239ff9b13b2ad04a37fef6dd9d9b3286b21

--- nim/src/ui/tradesui.nim
+++ nim/src/ui/tradesui.nim
@@ -15,11 +15,35 @@
 # You should have received a copy of the GNU General Public License
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
-import ../tk
+import std/os
+import ../[game, maps, tk]
+import coreui, table
+
+var tradeTable: TableWidget
+
+proc showTradeCommand(clientData: cint; interp: PInterp; argc: cint;
+    argv: cstringArray): TclResults {.exportc.} =
+  var tradeFrame = mainPaned & ".tradeframe"
+  let tradeCanvas = tradeFrame & ".canvas"
+  var label = tradeCanvas & ".trade.options.typelabel"
+  if tclEval2(script = "winfo exists " & label) == "0":
+    tclEvalFile(fileName = dataDirectory & "ui" & DirSep & "trade.tcl")
+    tclEval(script = "bind " & tradeFrame & " <Configure> {ResizeCanvas %W.canvas %w %h}")
+    tradeFrame = tradeCanvas & ".trade"
+    tradeTable = createTable(parent = tradeFrame, headers = @["Name", "Type",
+        "Durability", "Price", "Profit", "Weight", "Owned", "Available"],
+        scrollbar = mainPaned & ".tradeframe.scrolly",
+        command = "SortTradeItems",
+        tooltipTExt = "Press mouse button to sort the items.")
+  let
+    baseIndex = skyMap[playerShip.skyX][playerShip.skyY].baseIndex
+    eventIndex = skyMap[playerShip.skyX][playerShip.skyY].eventIndex
+  return tclOk
 
 proc addCommands*() {.sideEffect, raises: [], tags: [].} =
   ## Adds Tcl commands related to the trades UI
   try:
     discard
+#    addCommand("ShowTrade", showTradeCommand)
   except:
     showError(message = "Can't add a Tcl command.")
