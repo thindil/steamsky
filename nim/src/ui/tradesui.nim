2de18bff43ba93643daa698da297d9c7fd527cc4
thindil <thindil@laeran.pl>
2024-06-22 03:22:38 +0000

    docs: added code documentation to tradeItemCommand procedure
    FossilOrigin-Name: a9fcb724b539296bf5e19da9bc2bad0f40642abe1ece5653ee6a5664cd24bf4d

--- nim/src/ui/tradesui.nim
+++ nim/src/ui/tradesui.nim
@@ -618,6 +618,18 @@ var itemIndex = -1
 proc tradeItemCommand(clientData: cint; interp: PInterp; argc: cint;
     argv: cstringArray): TclResults {.sideEffect, raises: [], tags: [
     WriteIOEffect, RootEffect], exportc.} =
+  ## Buy or sell the selected item
+  ##
+  ## * clientData - the additional data for the Tcl command
+  ## * interp     - the Tcl interpreter on which the command was executed
+  ## * argc       - the amount of arguments entered for the command
+  ## * argv       - the list of the command's arguments
+  ##
+  ## The procedure always return tclOk
+  ##
+  ## Tcl:
+  ## TradeItem tradetype
+  ## Tradetype is type of trade action. Can be buy, buymax, sell, sellmax
   var baseCargoIndex, cargoIndex: int = -1
   if itemIndex < 0:
     baseCargoIndex = itemIndex.abs
