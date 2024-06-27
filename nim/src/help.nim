0ce61c442a02ccbfcf2d7600d792bb59d63d78ee
thindil <thindil@laeran.pl>
2024-06-27 03:01:45 +0000

    refactor: added contracts to procedures
    FossilOrigin-Name: 23643da785543d3fc9e060d48f8770fc10b4bac284ab67847889afee849819ea

--- nim/src/help.nim
+++ nim/src/help.nim
@@ -16,6 +16,7 @@
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
 import std/[strutils, tables, xmlparser, xmltree]
+import contracts
 import basestypes, careers, game, log, types
 
 type HelpData = object
@@ -25,117 +26,120 @@ type HelpData = object
 var helpList* = initOrderedTable[string, HelpData]()
 
 proc loadHelp*(fileName: string) {.sideEffect, raises: [DataLoadingError,
-    KeyError], tags: [WriteIOEffect, ReadIOEffect, RootEffect].} =
+    KeyError], tags: [WriteIOEffect, ReadIOEffect, RootEffect], contractual.} =
   ## Load the help data from the file
   ##
   ## * fileName - the name of the file to load
-  var
-    helpTitle: string
-    helpEntry: HelpData
-  let helpXml = try:
-      loadXml(path = fileName)
-    except XmlError, ValueError, IOError, OSError, Exception:
-      raise newException(exceptn = DataLoadingError,
-          message = "Can't load help data file. Reason: " &
-          getCurrentExceptionMsg())
-  for helpNode in helpXml:
-    if helpNode.kind != xnElement:
-      continue
-    let helpIndex: string = helpNode.attr(name = "index")
-    helpTitle = helpNode.attr(name = "title")
-    let helpAction: DataAction = try:
-          parseEnum[DataAction](helpNode.attr(name = "action").toLowerAscii)
-        except ValueError:
-          DataAction.add
-    if helpAction in [update, remove]:
-      if helpTitle notin helpList:
+  require:
+    fileName.len > 0
+  body:
+    var
+      helpTitle: string
+      helpEntry: HelpData
+    let helpXml = try:
+        loadXml(path = fileName)
+      except XmlError, ValueError, IOError, OSError, Exception:
         raise newException(exceptn = DataLoadingError,
-            message = "Can't " & $helpAction & " help '" & $helpTitle & "', there is no help with that title.")
-    elif helpTitle in helpList:
-      raise newException(exceptn = DataLoadingError,
-          message = "Can't add help '" & $helpTitle & "', there is an help with that title.")
-    if helpAction == DataAction.remove:
-      helpList.del(key = helpTitle)
-      logMessage(message = "Help removed: '" & $helpTitle & "'",
-          debugType = everything)
-      continue
-    helpEntry = if helpAction == DataAction.update:
-        try:
-          helpList[helpTitle]
-        except ValueError:
+            message = "Can't load help data file. Reason: " &
+            getCurrentExceptionMsg())
+    for helpNode in helpXml:
+      if helpNode.kind != xnElement:
+        continue
+      let helpIndex: string = helpNode.attr(name = "index")
+      helpTitle = helpNode.attr(name = "title")
+      let helpAction: DataAction = try:
+            parseEnum[DataAction](helpNode.attr(name = "action").toLowerAscii)
+          except ValueError:
+            DataAction.add
+      if helpAction in [update, remove]:
+        if helpTitle notin helpList:
+          raise newException(exceptn = DataLoadingError,
+              message = "Can't " & $helpAction & " help '" & $helpTitle & "', there is no help with that title.")
+      elif helpTitle in helpList:
+        raise newException(exceptn = DataLoadingError,
+            message = "Can't add help '" & $helpTitle & "', there is an help with that title.")
+      if helpAction == DataAction.remove:
+        helpList.del(key = helpTitle)
+        logMessage(message = "Help removed: '" & $helpTitle & "'",
+            debugType = everything)
+        continue
+      helpEntry = if helpAction == DataAction.update:
+          try:
+            helpList[helpTitle]
+          except ValueError:
+            HelpData(index: helpIndex)
+        else:
           HelpData(index: helpIndex)
+      var text = helpNode.innerText()
+      if text.len() > 0:
+        helpEntry.text = text
+      if helpAction == DataAction.add:
+        logMessage(message = "Help added: '" & helpTitle & "'",
+            debugType = everything)
       else:
-        HelpData(index: helpIndex)
-    var text = helpNode.innerText()
-    if text.len() > 0:
-      helpEntry.text = text
-    if helpAction == DataAction.add:
-      logMessage(message = "Help added: '" & helpTitle & "'",
-          debugType = everything)
-    else:
-      logMessage(message = "Help updated: '" & helpTitle & "'",
-          debugType = everything)
+        logMessage(message = "Help updated: '" & helpTitle & "'",
+            debugType = everything)
+      helpList[helpTitle] = helpEntry
+    # Add help page about available statistics and attributes
+    helpEntry.index = "stats"
+    helpTitle = $(helpList.len + 1) & ". Attributes and skills"
+    helpEntry.text = "Here you will find information about all available attributes and skills in the game\n\n{u}Attributes{/u}\n\n"
+    for attribute in attributesList:
+      helpEntry.text.add("{b}" & attribute.name & "{/b}\n    " &
+          attribute.description & "\n\n")
+    helpEntry.text.add("\n{u}Skills{/u}\n\n")
+    for skill in skillsList.values:
+      helpEntry.text.add("{b}" & skill.name &
+          "{/b}\n    {i}Related attribute:{/i} " & attributesList[
+          skill.attribute].name & "\n")
+      for item in itemsList.values:
+        if item.itemType == skill.tool:
+          helpEntry.text.add("   {i}Training tool:{/i} " & (
+              if item.showType.len == 0: item.itemType else: item.showType) & "\n")
+          break
+      helpEntry.text.add("    " & skill.description & "\n\n")
     helpList[helpTitle] = helpEntry
-  # Add help page about available statistics and attributes
-  helpEntry.index = "stats"
-  helpTitle = $(helpList.len + 1) & ". Attributes and skills"
-  helpEntry.text = "Here you will find information about all available attributes and skills in the game\n\n{u}Attributes{/u}\n\n"
-  for attribute in attributesList:
-    helpEntry.text.add("{b}" & attribute.name & "{/b}\n    " &
-        attribute.description & "\n\n")
-  helpEntry.text.add("\n{u}Skills{/u}\n\n")
-  for skill in skillsList.values:
-    helpEntry.text.add("{b}" & skill.name &
-        "{/b}\n    {i}Related attribute:{/i} " & attributesList[
-        skill.attribute].name & "\n")
-    for item in itemsList.values:
-      if item.itemType == skill.tool:
-        helpEntry.text.add("   {i}Training tool:{/i} " & (
-            if item.showType.len == 0: item.itemType else: item.showType) & "\n")
-        break
-    helpEntry.text.add("    " & skill.description & "\n\n")
-  helpList[helpTitle] = helpEntry
-  logMessage(message = "Help added: '" & helpTitle & "'",
-      debugType = everything)
-  # Add help page about available careers and factions
-  helpEntry.index = "factions"
-  helpTitle = $(helpList.len + 1) & ". Factions and careers"
-  helpEntry.text = "Here you will find information about all available factions and careers in the game\n\n{u}Factions{/u}\n\n"
-  for faction in factionsList.values:
-    if faction.careers.len > 0:
-      helpEntry.text.add("{b}" & faction.name & "{/b}\n    " &
-          faction.description & "\n    {i}Relations{/i}\n")
-      for index, relation in faction.relations:
-        helpEntry.text.add("        " & factionsList[index].name & ": " & (
-            if relation.friendly: "Friendly" else: "Enemies") & "\n")
+    logMessage(message = "Help added: '" & helpTitle & "'",
+        debugType = everything)
+    # Add help page about available careers and factions
+    helpEntry.index = "factions"
+    helpTitle = $(helpList.len + 1) & ". Factions and careers"
+    helpEntry.text = "Here you will find information about all available factions and careers in the game\n\n{u}Factions{/u}\n\n"
+    for faction in factionsList.values:
+      if faction.careers.len > 0:
+        helpEntry.text.add("{b}" & faction.name & "{/b}\n    " &
+            faction.description & "\n    {i}Relations{/i}\n")
+        for index, relation in faction.relations:
+          helpEntry.text.add("        " & factionsList[index].name & ": " & (
+              if relation.friendly: "Friendly" else: "Enemies") & "\n")
+        helpEntry.text.add("\n")
+    helpEntry.text.add("\n{u}Careers{/u}\n\n")
+    for index, career in careersList:
+      helpEntry.text.add("{b}" & career.name & "{/b}\n" & factionsList[
+          "POLEIS"].careers[index].description & "\n")
+      if career.skills.len > 0:
+        helpEntry.text.add("    {i}Bonus to skills{/i}\n")
+        for skill in career.skills:
+          helpEntry.text.add("        " & skill & "\n")
       helpEntry.text.add("\n")
-  helpEntry.text.add("\n{u}Careers{/u}\n\n")
-  for index, career in careersList:
-    helpEntry.text.add("{b}" & career.name & "{/b}\n" & factionsList[
-        "POLEIS"].careers[index].description & "\n")
-    if career.skills.len > 0:
-      helpEntry.text.add("    {i}Bonus to skills{/i}\n")
-      for skill in career.skills:
-        helpEntry.text.add("        " & skill & "\n")
-    helpEntry.text.add("\n")
-  helpList[helpTitle] = helpEntry
-  logMessage(message = "Help added: '" & helpTitle & "'",
-      debugType = everything)
-  # Add help page about available bases types
-  helpEntry.index = "basestypes"
-  helpTitle = $(helpList.len + 1) & ". Bases Types"
-  helpEntry.text = "Here you will find information about all available bases types in the game\n\n"
-  for baseType in basesTypesList.values:
-    helpEntry.text.add("{b}" & baseType.name & "{/b}\n    " &
-        baseType.description & "\n\n")
-  helpList[helpTitle] = helpEntry
-  logMessage(message = "Help added: '" & helpTitle & "'",
-      debugType = everything)
+    helpList[helpTitle] = helpEntry
+    logMessage(message = "Help added: '" & helpTitle & "'",
+        debugType = everything)
+    # Add help page about available bases types
+    helpEntry.index = "basestypes"
+    helpTitle = $(helpList.len + 1) & ". Bases Types"
+    helpEntry.text = "Here you will find information about all available bases types in the game\n\n"
+    for baseType in basesTypesList.values:
+      helpEntry.text.add("{b}" & baseType.name & "{/b}\n    " &
+          baseType.description & "\n\n")
+    helpList[helpTitle] = helpEntry
+    logMessage(message = "Help added: '" & helpTitle & "'",
+        debugType = everything)
 
 # Temporary code for interfacing with Ada
 
 proc getAdaHelp(index: cint; helpIndex, title, text: var cstring) {.raises: [],
-    tags: [], exportc.} =
+    tags: [], exportc, contractual.} =
   helpIndex = ""
   title = ""
   text = ""
@@ -152,7 +156,7 @@ proc getAdaHelp(index: cint; helpIndex, title, text: var cstring) {.raises: [],
     break
 
 proc getAdaHelp2(title: cstring; index, text: var cstring) {.raises: [],
-    tags: [], exportc.} =
+    tags: [], exportc, contractual.} =
   try:
     index = helpList[$title].index.cstring
     text = helpList[$title].text.cstring
