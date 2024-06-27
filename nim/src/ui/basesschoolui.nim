80014c3f851680217ae7d354e2768f928e4c1685
thindil <thindil@laeran.pl>
2024-06-27 03:01:29 +0000

    refactor: started work on move setSchoolSkillsCommand to Nim
    FossilOrigin-Name: 0b90a57e1a765fb0677ba6408e0330ce4b76c5efe48bf05e95b0d84ae79106b1

--- nim/src/ui/basesschoolui.nim
+++ nim/src/ui/basesschoolui.nim
@@ -15,11 +15,26 @@
 # You should have received a copy of the GNU General Public License
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
-import ../tk
+import std/[strutils, tables]
+import ../[game, tk]
+import coreui
+
+proc setSchoolSkillsCommand(clientData: cint; interp: PInterp; argc: cint;
+    argv: cstringArray): TclResults {.exportc.} =
+  let frameName = mainPaned & ".schoolframe.canvas.school"
+  var comboBox = frameName & ".setting.crew"
+  let memberIndex = tclEval2(script = comboBox & " current").parseInt
+  for index, skill in skillsList:
+    var skillLevel = 0
+    for skill2 in playerShip.crew[memberIndex].skills:
+      if skill2.index == index:
+        skillLevel = skill2.level
+  return tclOk
 
 proc addCommands*() {.sideEffect, raises: [], tags: [].} =
   ## Adds Tcl commands related to the trades UI
   try:
     discard
+#    addCommand("SetSchoolSkills", setSchoolSkillsCommand)
   except:
     showError(message = "Can't add a Tcl command.")
