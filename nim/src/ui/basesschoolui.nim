f00f38b8867c3dc6b7ec7c5467246cd1a5e0f112
thindil <thindil@laeran.pl>
2024-06-27 03:12:22 +0000

    refactor: continue work on move setSchoolSkillsCommand to Nim
    FossilOrigin-Name: 5de23efeaeb772ced2a3bf51cae59337df0fa179b5e9e51a11b8ee25042c958e

--- nim/src/ui/basesschoolui.nim
+++ nim/src/ui/basesschoolui.nim
@@ -16,7 +16,7 @@
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
 import std/[strutils, tables]
-import ../[game, tk]
+import ../[crew, game, tk]
 import coreui
 
 proc setSchoolSkillsCommand(clientData: cint; interp: PInterp; argc: cint;
@@ -24,11 +24,25 @@ proc setSchoolSkillsCommand(clientData: cint; interp: PInterp; argc: cint;
   let frameName = mainPaned & ".schoolframe.canvas.school"
   var comboBox = frameName & ".setting.crew"
   let memberIndex = tclEval2(script = comboBox & " current").parseInt
+  var comboList = ""
   for index, skill in skillsList:
     var skillLevel = 0
     for skill2 in playerShip.crew[memberIndex].skills:
       if skill2.index == index:
         skillLevel = skill2.level
+        if skillLevel == 100:
+          break
+    if skillLevel != 100:
+      comboList.add(y = " {" & skill.name & ": " & (if skillLevel ==
+          0: "Untrained" else: getSkillLevelName(skillLevel = skillLevel).strip) & "}")
+  comboBox = frameName & ".setting.skill"
+  let
+    oldComboList = tclEval2(script = comboBox & " cget -values")
+    spinBox = frameName & ".amountbox.amount"
+  if oldComboList.len + 1 != comboList.len:
+    tclEval(script = comboBox & " configure -values [list " & comboList & "]")
+    tclEval(script = comboBox & " current 0")
+    tclEval(script = spinBox & " set 1")
   return tclOk
 
 proc addCommands*() {.sideEffect, raises: [], tags: [].} =
