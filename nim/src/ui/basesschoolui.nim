3ce5f37951853d17299f38b0f773f3ce713b90ab
thindil <thindil@laeran.pl>
2024-06-28 03:13:27 +0000

    docs: added code documentation to showSchoolCommand procedure
    FossilOrigin-Name: 8ecf1ea1ec4c6603b894b7d0b271c06d3d7876f4b45d3eb0e31772b4694302c3

--- nim/src/ui/basesschoolui.nim
+++ nim/src/ui/basesschoolui.nim
@@ -65,6 +65,17 @@ proc setSchoolSkillsCommand(clientData: cint; interp: PInterp; argc: cint;
 
 proc showSchoolCommand(clientData: cint; interp: PInterp; argc: cint;
     argv: cstringArray): TclResults {.sideEffect, raises: [], tags: [], exportc.} =
+  ## Show the selected base school
+  ##
+  ## * clientData - the additional data for the Tcl command
+  ## * interp     - the Tcl interpreter on which the command was executed
+  ## * argc       - the amount of arguments entered for the command
+  ## * argv       - the list of the command's arguments
+  ##
+  ## The procedure always return tclOk
+  ##
+  ## Tcl:
+  ## ShowSchool
   var schoolFrame = mainPaned & ".schoolframe"
   let schoolCanvas = schoolFrame & ".canvas"
   if tclEval2(script = "winfo exists " & schoolCanvas) == "0":
