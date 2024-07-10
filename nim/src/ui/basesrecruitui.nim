baac113fb019c1b812b91058c09e7731785a6590
thindil <thindil@laeran.pl>
2024-07-10 03:30:09 +0000

    docs: added code documentation to hireCommand procedure
    FossilOrigin-Name: e59ced8934d79df367957486c409b625fb28f327044effee467b988917b708e4

--- nim/src/ui/basesrecruitui.nim
+++ nim/src/ui/basesrecruitui.nim
@@ -418,6 +418,17 @@ proc negotiateHireCommand(clientData: cint; interp: PInterp; argc: cint;
 proc hireCommand(clientData: cint; interp: PInterp; argc: cint;
     argv: cstringArray): TclResults {.sideEffect, raises: [], tags: [
         WriteIOEffect, RootEffect], exportc.} =
+  ## Hire the selected recruit
+  ##
+  ## * clientData - the additional data for the Tcl command
+  ## * interp     - the Tcl interpreter on which the command was executed
+  ## * argc       - the amount of arguments entered for the command
+  ## * argv       - the list of the command's arguments
+  ##
+  ## The procedure always return tclOk
+  ##
+  ## Tcl:
+  ## Hire
   const dialogName = ".negotiatedialog"
   var scale = dialogName & ".percent"
   let tradePayment = try:
