f2870a29086d027a9857b75818141e29c4ca8a18
thindil <thindil@laeran.pl>
2024-07-12 03:19:59 +0000

    docs: added code documentation to negotiateCommand procedure
    FossilOrigin-Name: 89a5a67caf90cd6452d667797fc786a4fe58b38a64cc7f2eacbe4e0139669666

--- nim/src/ui/basesrecruitui.nim
+++ nim/src/ui/basesrecruitui.nim
@@ -510,6 +510,17 @@ proc showRecruitTabCommand(clientData: cint; interp: PInterp; argc: cint;
 
 proc negotiateCommand(clientData: cint; interp: PInterp; argc: cint;
     argv: cstringArray): TclResults {.sideEffect, raises: [], tags: [], exportc.} =
+  ## Show negotation UI to the player
+  ##
+  ## * clientData - the additional data for the Tcl command
+  ## * interp     - the Tcl interpreter on which the command was executed
+  ## * argc       - the amount of arguments entered for the command
+  ## * argv       - the list of the command's arguments
+  ##
+  ## The procedure always return tclOk
+  ##
+  ## Tcl:
+  ## Negotiate
   let
     baseIndex = skyMap[playerShip.skyX][playerShip.skyY].baseIndex
     recruit = skyBases[baseIndex].recruits[recruitIndex]
