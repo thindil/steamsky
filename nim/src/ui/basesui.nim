f7325ff4a02c908e4b60276ea573a85fef78275a
thindil <thindil@laeran.pl>
2024-07-14 03:31:26 +0000

    refactor: continue work on move showBaseUiCommand to Nim
    FossilOrigin-Name: 4abf5f3dcd0f759841ffb4f5b966adf87d207c92c477b2559a7937d6f3e5f1a0

--- nim/src/ui/basesui.nim
+++ nim/src/ui/basesui.nim
@@ -15,12 +15,13 @@
 # You should have received a copy of the GNU General Public License
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
-import std/os
+import std/[os, tables]
 import ../[game, tk]
 import coreui, mapsui, table
 
 var
   baseTable: TableWidget
+  itemsIndexes: seq[string]
 
 proc showBaseUiCommand(clientData: cint; interp: PInterp; argc: cint;
     argv: cstringArray): TclResults {.exportc.} =
@@ -36,6 +37,25 @@ proc showBaseUiCommand(clientData: cint; interp: PInterp; argc: cint;
   baseFrame = baseCanvas & ".base"
   if tclEval2(script = "winfo exists " & baseFrame & ".table") == "1":
     tclEval(script = "destroy " & baseTable.canvas)
+  let
+    searchFrame = baseCanvas & ".base.searchframe"
+    searchEntry = searchFrame & ".search"
+  if argv[1] == "recipes":
+    tclEval(script = "grid " & searchFrame)
+    if argc != 3:
+      tclEval(script = searchEntry & " configure -validatecommand {}")
+      tclEval(script = searchEntry & " delete 0 end")
+      tclEval(script = searchEntry & " configure -validatecommand {SearchRecipes %P}")
+    baseTable = createTable(parent = baseFrame, headers = @["Name", "Cost", ""],
+        scrollbar = mainPaned & ".baseframe.scrolly",
+        command = "SortBaseItems " & $argv[1],
+        tooltipText = "Press mouse button to sort the recipes.")
+    if itemsIndexes.len != recipesList.len:
+      itemsIndexes = @[]
+      for index in recipesList.keys:
+        itemsIndexes.add(y = index)
+  else:
+    tclEval(script = "grid remove " & searchFrame)
   return tclOk
 
 proc addCommands*() {.sideEffect, raises: [], tags: [].} =
