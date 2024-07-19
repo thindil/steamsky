647dd79c44a21a84db5513dd1b57c746588ea72f
thindil <thindil@laeran.pl>
2024-07-19 03:18:22 +0000

    refactor: started work on move showShipyardCommand to Nim
    FossilOrigin-Name: ae597c318a58f20acb796f458f4a6b9204c14faf728412f38e1c501e5ae09618

--- nim/src/ui/basesshipyardui.nim
+++ nim/src/ui/basesshipyardui.nim
@@ -15,11 +15,38 @@
 # You should have received a copy of the GNU General Public License
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
-import ../tk
+import std/os
+import ../[game, tk]
+import coreui, table
+
+var
+  installTable, removeTable: TableWidget
+  installIndexes, removeIndexes: seq[Natural]
+
+proc showShipyardCommand(clientData: cint; interp: PInterp; argc: cint;
+    argv: cstringArray): TclResults {.exportc.} =
+  var shipyardFrame = mainPaned & ".shipyardframe"
+  let shipyardCanvas = shipyardFrame & ".canvas"
+  if tclEval2(script = "winfo exists " & shipyardCanvas) == "0":
+    tclEvalFile(fileName = dataDirectory & "ui" & DirSep & "shipyard.tcl")
+    tclEval(script = "bind " & shipyardFrame & " <Configure> {ResizeCanvas %W.canvas %w %h}")
+    shipyardFrame = shipyardCanvas & ".shipyard.install"
+    installTable = createTable(parent = shipyardFrame, headers = @["Name",
+        "Type", "Size", "Materials", "Cost"],
+        scrollbar = ".gameframe.paned.shipyardframe.scrolly", command = "",
+        tooltipText = "Press mouse button to sort the modules.")
+    shipyardFrame = shipyardCanvas & ".shipyard.remove"
+    removeTable = createTable(parent = shipyardFrame, headers = @["Name",
+        "Type", "Size", "Materials", "Price"],
+        scrollbar = ".gameframe.paned.shipyardframe.scrolly",
+        command = "SortShipyardModules remove 0 {}",
+        tooltipText = "Press mouse button to sort the modules.")
+  return tclOk
 
 proc addCommands*() {.sideEffect, raises: [], tags: [].} =
   ## Adds Tcl commands related to the trades UI
   try:
     discard
+#    addCommand("ShowShipyard", showShipyardCommand)
   except:
     showError(message = "Can't add a Tcl command.")
