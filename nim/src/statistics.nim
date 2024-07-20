e4932a69bf196773c88cd9d04a115595b8424438
thindil <thindil@laeran.pl>
2024-07-20 03:15:52 +0000

    refactor: added contracts to procedures
    FossilOrigin-Name: 99b969c12fc89d65ae46418f2ff9f5fbe3299178c6770b3c21b233239346182f

--- nim/src/statistics.nim
+++ nim/src/statistics.nim
@@ -1,4 +1,4 @@
-# Copyright 2023 Bartek thindil Jasicki
+# Copyright 2023-2024 Bartek thindil Jasicki
 #
 # This file is part of Steam Sky.
 #
@@ -16,6 +16,7 @@
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
 import std/[strutils, tables]
+import contracts
 import config, game, types
 
 type
@@ -55,40 +56,49 @@ type
 var gameStats* = GameStatsData(basesVisited: 1, mapVisited: 1,
     distanceTraveled: 0, acceptedMissions: 0, points: 0) ## The player's game's statistics
 
-proc updateCraftingOrders*(index: string) {.sideEffect, raises: [], tags: [].} =
+proc updateCraftingOrders*(index: string) {.sideEffect, raises: [], tags: [],
+    contractual.} =
   ## Update the list of finished crafting orders in the game statistics
   ##
   ## * index - the index of the crafting order to update
-  var updated = false
-  for craftingOrder in gameStats.craftingOrders.mitems:
-    if craftingOrder.index == index:
-      craftingOrder.amount.inc
-      updated = true
-      break
-  if not updated:
-    gameStats.craftingOrders.add(y = StatisticsData(index: index, amount: 1))
-  gameStats.points = gameStats.points + 5
+  require:
+    index.len > 0
+  body:
+    var updated = false
+    for craftingOrder in gameStats.craftingOrders.mitems:
+      if craftingOrder.index == index:
+        craftingOrder.amount.inc
+        updated = true
+        break
+    if not updated:
+      gameStats.craftingOrders.add(y = StatisticsData(index: index, amount: 1))
+    gameStats.points = gameStats.points + 5
 
-proc updateFinishedGoals*(index: string) {.sideEffect, raises: [], tags: [].} =
+proc updateFinishedGoals*(index: string) {.sideEffect, raises: [], tags: [],
+    contractual.} =
   ## Update the list of finished goals in the game statistics
   ##
   ## * index - the index of the goal to update
-  var updated = false
-  for goal in goalsList.values:
-    if goal.index == index:
-      gameStats.points = gameStats.points + (goal.amount * goal.multiplier)
-      break
-  for goal in gameStats.finishedGoals.mitems:
-    if goal.index == index:
-      goal.amount.inc
-      updated = true
-      break
-  if not updated:
+  require:
+    index.len > 0
+  body:
+    var updated = false
     for goal in goalsList.values:
       if goal.index == index:
-        gameStats.finishedGoals.add(y = StatisticsData(index: goal.index, amount: 1))
+        gameStats.points = gameStats.points + (goal.amount * goal.multiplier)
+        break
+    for goal in gameStats.finishedGoals.mitems:
+      if goal.index == index:
+        goal.amount.inc
+        updated = true
+        break
+    if not updated:
+      for goal in goalsList.values:
+        if goal.index == index:
+          gameStats.finishedGoals.add(y = StatisticsData(index: goal.index, amount: 1))
 
-proc getGamePoints*(): Natural {.sideEffect, raises: [], tags: [].} =
+proc getGamePoints*(): Natural {.sideEffect, raises: [], tags: [],
+    contractual.} =
   ## Get the real amount of the player's game's points, multiplied or divided
   ## by the game's difficulty settings
   ##
@@ -114,71 +124,86 @@ proc getGamePoints*(): Natural {.sideEffect, raises: [], tags: [].} =
     pointsBonus = 0.01
   return (gameStats.points.float * pointsBonus).Natural
 
-proc updateFinishedMissions*(mType: string) {.sideEffect, raises: [], tags: [].} =
+proc updateFinishedMissions*(mType: string) {.sideEffect, raises: [], tags: [],
+    contractual.} =
   ## Update the list of finished missions in the game statistics
   ##
   ## * mType - the type of missions which will be updated
-  var updated = false
-  for finishedMission in gameStats.finishedMissions.mitems:
-    if finishedMission.index == mType:
-      finishedMission.amount.inc
-      updated = true
-      break
-  if not updated:
-    gameStats.finishedMissions.add(StatisticsData(index: mType, amount: 1))
-  gameStats.points = gameStats.points + 50
+  require:
+    mType.len > 0
+  body:
+    var updated = false
+    for finishedMission in gameStats.finishedMissions.mitems:
+      if finishedMission.index == mType:
+        finishedMission.amount.inc
+        updated = true
+        break
+    if not updated:
+      gameStats.finishedMissions.add(StatisticsData(index: mType, amount: 1))
+    gameStats.points = gameStats.points + 50
 
-proc clearGameStats*() {.sideEffect, raises: [], tags: [].} =
+proc clearGameStats*() {.sideEffect, raises: [], tags: [], contractual.} =
   ## Reset the game statistics
-  gameStats.destroyedShips = @[]
-  gameStats.basesVisited = 1
-  gameStats.mapVisited = 1
-  gameStats.distanceTraveled = 0
-  gameStats.craftingOrders = @[]
-  gameStats.acceptedMissions = 0
-  gameStats.finishedMissions = @[]
-  gameStats.finishedGoals = @[]
-  gameStats.killedMobs = @[]
-  gameStats.points = 0
+  ensure:
+    gameStats.points == 0
+  body:
+    gameStats.destroyedShips = @[]
+    gameStats.basesVisited = 1
+    gameStats.mapVisited = 1
+    gameStats.distanceTraveled = 0
+    gameStats.craftingOrders = @[]
+    gameStats.acceptedMissions = 0
+    gameStats.finishedMissions = @[]
+    gameStats.finishedGoals = @[]
+    gameStats.killedMobs = @[]
+    gameStats.points = 0
 
 proc updateKilledMobs*(mob: MemberData; factionName: string) {.sideEffect,
-    raises: [], tags: [].} =
+    raises: [], tags: [], contractual.} =
   ## Update the list of killed mobs in the game statistics
   ##
   ## * mob         - the killed mobile data, needed to count the gained points
   ## * factionName - the name of the faction to which the mob belongs
-  for attribute in mob.attributes:
-    gameStats.points = gameStats.points + attribute.level
-  for skill in mob.skills:
-    gameStats.points = gameStats.points + skill.level
-  var updated = false
-  for killedMob in gameStats.killedMobs.mitems:
-    if killedMob.index.toLowerAscii == factionName.tolowerAscii:
-      killedMob.amount.inc
-      updated = true
-      break
-  if not updated:
-    gameStats.killedMobs.add(StatisticsData(index: factionName.capitalizeAscii, amount: 1))
+  require:
+    factionName.len > 0
+  body:
+    for attribute in mob.attributes:
+      gameStats.points = gameStats.points + attribute.level
+    for skill in mob.skills:
+      gameStats.points = gameStats.points + skill.level
+    var updated = false
+    for killedMob in gameStats.killedMobs.mitems:
+      if killedMob.index.toLowerAscii == factionName.tolowerAscii:
+        killedMob.amount.inc
+        updated = true
+        break
+    if not updated:
+      gameStats.killedMobs.add(StatisticsData(
+          index: factionName.capitalizeAscii, amount: 1))
 
-proc updateDestroyedShips*(shipName: string) {.sideEffect, raises: [], tags: [].} =
+proc updateDestroyedShips*(shipName: string) {.sideEffect, raises: [], tags: [],
+    contractual.} =
   ## Update the list of destroyed ships in the game statistics
   ##
   ## * shipName - the name of the prototype of the destroyed NPC ship
-  var shipIndex = 0
-  for index, ship in protoShipsList:
-    if ship.name == shipName:
-      shipIndex = index
-      gameStats.points = gameStats.points + (ship.combatValue / 10).Natural
-  if shipIndex == 0:
-    return
-  var updated = false
-  for destroyedShip in gameStats.destroyedShips.mitems:
-    if destroyedShip.index == $shipIndex:
-      destroyedShip.amount.inc
-      updated = true
-      break
-  if not updated:
-    gameStats.destroyedShips.add(StatisticsData(index: $shipIndex, amount: 1))
+  require:
+    shipName.len > 0
+  body:
+    var shipIndex = 0
+    for index, ship in protoShipsList:
+      if ship.name == shipName:
+        shipIndex = index
+        gameStats.points = gameStats.points + (ship.combatValue / 10).Natural
+    if shipIndex == 0:
+      return
+    var updated = false
+    for destroyedShip in gameStats.destroyedShips.mitems:
+      if destroyedShip.index == $shipIndex:
+        destroyedShip.amount.inc
+        updated = true
+        break
+    if not updated:
+      gameStats.destroyedShips.add(StatisticsData(index: $shipIndex, amount: 1))
 
 # Temporary code for interfacing with Ada
 
@@ -194,10 +219,12 @@ type
     index: cstring
     amount: cint
 
-proc updateAdaCraftingOrders(index: cstring) {.raises: [], tags: [], exportc.} =
+proc updateAdaCraftingOrders(index: cstring) {.raises: [], tags: [], exportc,
+    contractual.} =
   updateCraftingOrders(index = $index)
 
-proc getAdaGameStats(value, stat: cint) {.raises: [], tags: [], exportc.} =
+proc getAdaGameStats(value, stat: cint) {.raises: [], tags: [], exportc,
+    contractual.} =
   case stat
   of 0:
     gameStats.distanceTraveled = gameStats.distanceTraveled + value
@@ -207,7 +234,7 @@ proc getAdaGameStats(value, stat: cint) {.raises: [], tags: [], exportc.} =
     discard
 
 proc getAdaGameStatsList(name: cstring; statsList: array[512,
-    AdaStatisticsData]) {.raises: [], tags: [], exportc.} =
+    AdaStatisticsData]) {.raises: [], tags: [], exportc, contractual.} =
   var list = case $name
     of "destroyedShips":
       gameStats.destroyedShips
@@ -237,14 +264,16 @@ proc getAdaGameStatsList(name: cstring; statsList: array[512,
   else:
     gameStats.killedMobs = list
 
-proc setAdaGameStats(stats: var AdaGameStats) {.raises: [], tags: [], exportc.} =
+proc setAdaGameStats(stats: var AdaGameStats) {.raises: [], tags: [], exportc,
+    contractual.} =
   stats.basesVisited = gameStats.basesVisited.cint
   stats.mapVisited = gameStats.mapVisited.cint
   stats.distanceTraveled = gameStats.distanceTraveled.cint
   stats.acceptedMissions = gameStats.acceptedMissions.cint
   stats.points = gameStats.points.cint
 
-proc setAdaGameStatsNumber(name: cstring; statsValue: var cint) {.raises: [], tags: [], exportc.} =
+proc setAdaGameStatsNumber(name: cstring; statsValue: var cint) {.raises: [],
+    tags: [], exportc, contractual.} =
   case $name
   of "basesVisited":
     statsValue = gameStats.basesVisited.cint
@@ -256,7 +285,7 @@ proc setAdaGameStatsNumber(name: cstring; statsValue: var cint) {.raises: [], ta
     statsValue = gameStats.acceptedMissions.cint
 
 proc setAdaGameStatsList(name: cstring; statsList: var array[512,
-    AdaStatisticsData]) {.raises: [], tags: [], exportc.} =
+    AdaStatisticsData]) {.raises: [], tags: [], exportc, contractual.} =
   var list = case $name
     of "destroyedShips":
       gameStats.destroyedShips
@@ -274,22 +303,25 @@ proc setAdaGameStatsList(name: cstring; statsList: var array[512,
     statsList[index] = AdaStatisticsData(index: stat.index.cstring,
         amount: stat.amount.cint)
 
-proc updateAdaFinishedGoals(index: cstring) {.raises: [], tags: [], exportc.} =
+proc updateAdaFinishedGoals(index: cstring) {.raises: [], tags: [], exportc,
+    contractual.} =
   updateFinishedGoals(index = $index)
 
-proc getAdaGamePoints(): cint {.raises: [], tags: [], exportc.} =
+proc getAdaGamePoints(): cint {.raises: [], tags: [], exportc, contractual.} =
   return getGamePoints().cint
 
-proc updateAdaFinishedMissions(mType: cstring) {.raises: [], tags: [], exportc.} =
+proc updateAdaFinishedMissions(mType: cstring) {.raises: [], tags: [], exportc,
+    contractual.} =
   updateFinishedMissions(mType = $mType)
 
-proc clearAdaGameStats() {.raises: [], tags: [], exportc.} =
+proc clearAdaGameStats() {.raises: [], tags: [], exportc, contractual.} =
   clearGameStats()
 
 proc updateAdaKilledMobs(mob: AdaMemberData; factionName: cstring) {.raises: [],
-    tags: [], exportc.} =
+    tags: [], exportc, contractual.} =
   updateKilledMobs(mob = adaMemberToNim(adaMember = mob),
       factionName = $factionName)
 
-proc updateAdaDestroyedShips(shipName: cstring) {.raises: [], tags: [], exportc.} =
+proc updateAdaDestroyedShips(shipName: cstring) {.raises: [], tags: [], exportc,
+    contractual.} =
   updateDestroyedShips(shipName = $shipName)
