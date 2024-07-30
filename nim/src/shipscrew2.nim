3cb7efd67b85f893d7d5c66da8c9f1133140a85e
thindil <thindil@laeran.pl>
2024-07-30 03:08:26 +0000

    refactor: added contracts to procedures
    FossilOrigin-Name: 9faf8ef29b3ab65d20963890885864c8b023a31bc5208f2f11f12d1cafbc9b22

--- nim/src/shipscrew2.nim
+++ nim/src/shipscrew2.nim
@@ -1,4 +1,4 @@
-# Copyright 2023 Bartek thindil Jasicki
+# Copyright 2023-2024 Bartek thindil Jasicki
 #
 # This file is part of Steam Sky.
 #
@@ -16,10 +16,11 @@
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
 import std/tables
+import contracts
 import crafts, game, halloffame, messages, missions, shipscrew, types, utils
 
 proc deleteMember*(memberIndex: Natural; ship: var ShipRecord) {.sideEffect,
-    raises: [KeyError], tags: [].} =
+    raises: [KeyError], tags: [], contractual.} =
   ## Delete the selected member from the selected ship crew list, update
   ## the ship modules with the new crew list and delete accepted missions
   ## if neccessary.
@@ -28,29 +29,32 @@ proc deleteMember*(memberIndex: Natural; ship: var ShipRecord) {.sideEffect,
   ## * ship        - the ship from which the crew member will be deleted
   ##
   ## Returns parameter ship with the updated list of crew members and modules
-  var deleted = false
-  if ship.crew == playerShip.crew:
-    for index, mission in acceptedMissions.pairs:
-      if mission.mType == passenger and mission.data == memberIndex:
-        deleteMission(missionIndex = index)
-        deleted = true
-        break
-    for mission in acceptedMissions.mitems:
-      if mission.mType == passenger and mission.data > memberIndex:
-        mission.data.inc
-    if deleted:
-      return
-  ship.crew.delete(i = memberIndex)
-  for module in ship.modules.mitems:
-    for owner in module.owner.mitems:
-      if owner == memberIndex:
-        owner = 0
-      elif owner > memberIndex:
-        owner.dec
+  require:
+    memberIndex < ship.crew.len
+  body:
+    var deleted = false
+    if ship.crew == playerShip.crew:
+      for index, mission in acceptedMissions.pairs:
+        if mission.mType == passenger and mission.data == memberIndex:
+          deleteMission(missionIndex = index)
+          deleted = true
+          break
+      for mission in acceptedMissions.mitems:
+        if mission.mType == passenger and mission.data > memberIndex:
+          mission.data.inc
+      if deleted:
+        return
+    ship.crew.delete(i = memberIndex)
+    for module in ship.modules.mitems:
+      for owner in module.owner.mitems:
+        if owner == memberIndex:
+          owner = 0
+        elif owner > memberIndex:
+          owner.dec
 
 proc death*(memberIndex: Natural; reason: string; ship: var ShipRecord;
     createBody: bool = true) {.sideEffect, raises: [KeyError, IOError], tags: [
-    WriteIOEffect].} =
+    WriteIOEffect], contractual.} =
   ## Handle the death of a crew member in ships
   ##
   ## * memberIndex - the crew index of the member which died
@@ -60,86 +64,95 @@ proc death*(memberIndex: Natural; reason: string; ship: var ShipRecord;
   ##                 value is true
   ##
   ## Returns the parameter ship with updated crew, modules and cargo lists
-  let memberName = ship.crew[memberIndex].name
-  if ship.crew == playerShip.crew:
-    if memberIndex == 0:
-      addMessage(message = "You died from " & reason & ".",
+  require:
+    memberIndex < ship.crew.len
+    reason.len > 0
+  body:
+    let memberName = ship.crew[memberIndex].name
+    if ship.crew == playerShip.crew:
+      if memberIndex == 0:
+        addMessage(message = "You died from " & reason & ".",
+            mType = combatMessage, color = red)
+        playerShip.crew[memberIndex].order = rest
+        playerShip.crew[memberIndex].health = 0
+        updateHallOfFame(playerName = playerShip.crew[memberIndex].name,
+            deathReason = reason)
+        return
+      addMessage(message = memberName & " died from " & reason & ".",
           mType = combatMessage, color = red)
-      playerShip.crew[memberIndex].order = rest
-      playerShip.crew[memberIndex].health = 0
-      updateHallOfFame(playerName = playerShip.crew[memberIndex].name,
-          deathReason = reason)
-      return
-    addMessage(message = memberName & " died from " & reason & ".",
-        mType = combatMessage, color = red)
-  if createBody:
-    ship.cargo.add(y = InventoryData(protoIndex: corpseIndex, amount: 1,
-        name: memberName & "'s corpse", durability: 100, price: 0))
-  deleteMember(memberIndex = memberIndex, ship = ship)
-  for index, _ in ship.crew.pairs:
-    updateMorale(ship = ship, memberIndex = index, value = getRandom(min = -25, max = -10))
+    if createBody:
+      ship.cargo.add(y = InventoryData(protoIndex: corpseIndex, amount: 1,
+          name: memberName & "'s corpse", durability: 100, price: 0))
+    deleteMember(memberIndex = memberIndex, ship = ship)
+    for index, _ in ship.crew.pairs:
+      updateMorale(ship = ship, memberIndex = index, value = getRandom(
+          min = -25, max = -10))
 
 proc getCurrentOrder*(memberIndex: Natural): string {.sideEffect, raises: [
-    ValueError], tags: [].} =
+    ValueError], tags: [], contractual.} =
   ## Get the full information about the order of the selected crew member
   ##
   ## * memberIndex - the crew index of the member which order will be checked
   ##
   ## Returns a string with detailed information about the current order of the
   ## selected player's ship's crew member.
-  proc getModuleName(mType: ModuleType2): string =
-    result = ""
-    for module in playerShip.modules:
-      if module.mType == mType:
-        for owner in module.owner:
-          if owner == memberIndex:
-            return module.name
-
-  let member = playerShip.crew[memberIndex]
-  case member.order
-  of pilot:
-    result = "Piloting the ship"
-  of engineer:
-    result = "Engineering the ship"
-  of gunner:
-    result = "Operating " & getModuleName(mType = ModuleType2.gun)
-  of repair:
-    result = "Repairing the ship"
-  of craft:
-    for index, module in playerShip.modules:
-      if module.mType == ModuleType2.workshop:
-        for owner in module.owner:
-          if owner == memberIndex:
-            result = getWorkshopRecipeName(workshop = index) & " in " & module.name
-            break
-  of upgrading:
-    result = "Upgrading " & playerShip.modules[playerShip.upgradeModule].name
-  of talk:
-    result = "Talking with others"
-  of heal:
-    result = "Healig the wounded in " & getModuleName(
-        mType = ModuleType2.medicalRoom)
-  of clean:
-    result = "Cleaning the ship"
-  of rest:
-    result = "Resting in " & getModuleName(mType = ModuleType2.cabin) & ", no order"
-  of defend:
-    result = "Defending the ship"
-  of boarding:
-    result = "Boarding the enemy's ship"
-  of train:
-    block findTrainingRoom:
+  require:
+    memberIndex < playerShip.crew.len
+  body:
+    proc getModuleName(mType: ModuleType2): string =
+      result = ""
       for module in playerShip.modules:
-        if module.mType == ModuleType2.trainingRoom:
+        if module.mType == mType:
           for owner in module.owner:
             if owner == memberIndex:
-              result = "Training " & skillsList[module.trainedSkill].name &
-                  " in " & module.name
-              break findTrainingRoom
+              return module.name
+
+    let member = playerShip.crew[memberIndex]
+    case member.order
+    of pilot:
+      result = "Piloting the ship"
+    of engineer:
+      result = "Engineering the ship"
+    of gunner:
+      result = "Operating " & getModuleName(mType = ModuleType2.gun)
+    of repair:
+      result = "Repairing the ship"
+    of craft:
+      for index, module in playerShip.modules:
+        if module.mType == ModuleType2.workshop:
+          for owner in module.owner:
+            if owner == memberIndex:
+              result = getWorkshopRecipeName(workshop = index) & " in " & module.name
+              break
+    of upgrading:
+      result = "Upgrading " & playerShip.modules[playerShip.upgradeModule].name
+    of talk:
+      result = "Talking with others"
+    of heal:
+      result = "Healig the wounded in " & getModuleName(
+          mType = ModuleType2.medicalRoom)
+    of clean:
+      result = "Cleaning the ship"
+    of rest:
+      result = "Resting in " & getModuleName(mType = ModuleType2.cabin) & ", no order"
+    of defend:
+      result = "Defending the ship"
+    of boarding:
+      result = "Boarding the enemy's ship"
+    of train:
+      block findTrainingRoom:
+        for module in playerShip.modules:
+          if module.mType == ModuleType2.trainingRoom:
+            for owner in module.owner:
+              if owner == memberIndex:
+                result = "Training " & skillsList[module.trainedSkill].name &
+                    " in " & module.name
+                break findTrainingRoom
 
 # Temporary code for interfacing with Ada
 
-proc deleteAdaMember(memberIndex, inPlayerShip: cint) {.raises: [], tags: [], exportc.} =
+proc deleteAdaMember(memberIndex, inPlayerShip: cint) {.raises: [], tags: [],
+    exportc, contractual.} =
   try:
     if inPlayerShip == 1:
       deleteMember(memberIndex = memberIndex - 1, ship = playerShip)
@@ -149,7 +162,8 @@ proc deleteAdaMember(memberIndex, inPlayerShip: cint) {.raises: [], tags: [], ex
     discard
 
 proc deathAda(memberIndex: cint; reason: cstring; inPlayerShip,
-    createBody: cint) {.raises: [], tags: [WriteIOEffect], exportc.} =
+    createBody: cint) {.raises: [], tags: [WriteIOEffect], exportc,
+        contractual.} =
   try:
     if inPlayerShip == 1:
       death(memberIndex = memberIndex - 1, reason = $reason, ship = playerShip,
@@ -160,7 +174,8 @@ proc deathAda(memberIndex: cint; reason: cstring; inPlayerShip,
   except IOError, KeyError:
     discard
 
-proc getAdaCurrentOrder(memberIndex: cint): cstring {.raises: [], tags: [], exportc.} =
+proc getAdaCurrentOrder(memberIndex: cint): cstring {.raises: [], tags: [],
+    exportc, contractual.} =
   try:
     return getCurrentOrder(memberIndex = memberIndex - 1).cstring
   except ValueError:
