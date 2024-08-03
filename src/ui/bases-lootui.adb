ac20b5c66aaceb8034b7d8f7968cd49d2043dc73
thindil <thindil@laeran.pl>
2024-08-03 03:13:04 +0000

    refactor: removed unused code
    FossilOrigin-Name: dd7df02fa22db80272f1dfb6fd571fdd645bca6538520f24ddb6339e5bc4cc03

--- src/ui/bases-lootui.adb
+++ src/ui/bases-lootui.adb
@@ -13,8 +13,6 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-with Ada.Strings;
-with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with Interfaces.C; use Interfaces.C;
 with CArgv; use CArgv;
 with Tcl; use Tcl;
