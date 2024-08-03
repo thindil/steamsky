29ecc9a8399c13034bfe390b046c572d3cb96c01
thindil <thindil@laeran.pl>
2024-08-03 03:26:19 +0000

    refactor: started work on move updateBasesList procedure to Nim
    FossilOrigin-Name: 3713c4ff42d5529e878b892d2c4e4cb03b46abdd914b2ac59f323701b82ed7b8

--- nim/src/ui/knowledgebases.nim
+++ nim/src/ui/knowledgebases.nim
@@ -15,14 +15,16 @@
 # You should have received a copy of the GNU General Public License
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
+import std/strutils
 import ../tk
+import coreui, table
 
 proc getReputationText(reputationLevel: int): string {.sideEffect, raises: [],
     tags: [].} =
   ## Get the name of the reputation level in the selected base
   ##
   ## * reputationLevel - the numerical level of reputation in a base
-  ## 
+  ##
   ## Returns the name of the reputation level in the selected base
   case reputationLevel
   of -100 .. -75:
@@ -46,6 +48,22 @@ proc getReputationText(reputationLevel: int): string {.sideEffect, raises: [],
   else:
     return ""
 
+var basesTable: TableWidget
+
+proc updateBasesList(baseName: string = "", page: Positive = 1) =
+  if basesTable.row > 1:
+    clearTable(table = basesTable)
+  let
+    basesCanvas = mainPaned & ".knowledgeframe.bases.canvas"
+    basesFrame = basesCanvas & ".frame"
+    rows = tclEval2(script = "grid size " & basesFrame).split(" ")[2].parseInt
+  deleteWidgets(startIndex = 2, endIndex = rows - 1, frame = basesFrame)
+  basesTable = createTable(parent = basesFrame, headers = @["Name", "Distance",
+      "Coordinates", "Population", "Size", "Owner", "Type", "Reputation"],
+      scrollbar = ".gameframe.paned.knowledgeframe.bases.scrolly",
+      command = "SortKnownBases {" & baseName & "}",
+      tooltipText = "Press mouse button to sort the bases.")
+
 proc addCommands*() {.sideEffect, raises: [], tags: [].} =
   ## Adds Tcl commands related to the trades UI
   try:
@@ -59,3 +77,10 @@ proc addCommands*() {.sideEffect, raises: [], tags: [].} =
 proc getAdaReputationText(reputationLevel: cint): cstring {.sideEffect,
     raises: [], tags: [], exportc.} =
   return getReputationText(reputationLevel = reputationLevel).cstring
+
+proc updateAdaBasesList(baseName: cstring; page: cint) {.sideEffect, raises: [],
+    tags: [], exportc.} =
+  try:
+    updateBasesList(baseName = $baseName, page = page.Positive)
+  except:
+    discard
