b10e2e27b003789e074106d0eb2d776a586e74d2
thindil <thindil@laeran.pl>
2024-08-06 04:00:59 +0000

    docs: added code documentation to showBaseInfoCommand procedure
    FossilOrigin-Name: c638b9e789de6c4d0ddd979d7f0241c875ceec48aad390ed1408c419b60c79fa

--- nim/src/ui/knowledgebases.nim
+++ nim/src/ui/knowledgebases.nim
@@ -219,6 +219,18 @@ proc showBasesCommand(clientData: cint; interp: PInterp; argc: cint;
 
 proc showBaseInfoCommand(clientData: cint; interp: PInterp; argc: cint;
     argv: cstringArray): TclResults {.sideEffect, raises: [], tags: [], exportc.} =
+  ## Show information about the selected base
+  ##
+  ## * clientData - the additional data for the Tcl command
+  ## * interp     - the Tcl interpreter on which the command was executed
+  ## * argc       - the amount of arguments entered for the command
+  ## * argv       - the list of the command's arguments
+  ##
+  ## The procedure always return tclOk
+  ##
+  ## Tcl:
+  ## ShowBaseInfo baseindex
+  ## BaseIndex is the index of the base to show
   let
     baseIndex = try:
         ($argv[1]).parseInt
