aaa99ddd45b71154682b444cf087537730258991
thindil <thindil@laeran.pl>
2024-08-08 03:08:48 +0000

    refactor: started work on move showEventInfoCommand to Nim
    FossilOrigin-Name: 404c8f05aa9cada1fbc4a141e6fa0040f8aee3b472ea08312ef4f23f475be544

--- nim/src/ui/knowledgeevents.nim
+++ nim/src/ui/knowledgeevents.nim
@@ -15,12 +15,29 @@
 # You should have received a copy of the GNU General Public License
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
-import ../tk
+import std/strutils
+import ../[game, maps, tk]
+
+proc showEventInfoCommand(clientData: cint; interp: PInterp; argc: cint;
+    argv: cstringArray): TclResults {. exportc.} =
+  let
+    eventIndex = ($argv[1]).parseInt - 1
+    baseIndex = skyMap[eventsList[eventIndex].skyX][eventsList[eventIndex].skyX].baseIndex
+  var eventInfo = "X: {gold}" & $eventsList[eventIndex].skyX & "{/gold} Y: {gold}" & $eventsList[eventIndex].skyY
+  case eventsList[eventIndex].eType
+  of enemyShip, enemyPatrol, trader, friendlyShip:
+    eventInfo.add(y = "\nShip type: {gold}" & protoShipsList[eventsList[eventIndex].shipIndex].name & "{/gold}")
+  of fullDocks, attackOnBase, disease:
+    eventInfo.add(y = "\nBase name: {gold}" & skyBases[baseIndex].name & "{/gold}")
+  of doublePrice:
+    eventInfo.add(y = "\nBase name: {gold}" & skyBases[baseIndex].name & "{/gold}")
+    eventInfo.add(y = "\nItem: {gold}" & itemsList[eventsList[eventIndex].itemIndex].name & "{/gold}")
+  return tclOk
 
 proc addCommands*() {.sideEffect, raises: [], tags: [].} =
   ## Adds Tcl commands related to the known events UI
   try:
     discard
-#    addCommand("ShowBases", showBasesCommand)
+#    addCommand("ShowEventInfo", showEventInfoCommand)
   except:
     showError(message = "Can't add a Tcl command.")
