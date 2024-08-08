46e19782507214a0cc03af387f984bf0b996c942
thindil <thindil@laeran.pl>
2024-08-08 03:14:09 +0000

    refactor: continue work on move showEventInfoCommand to Nim
    FossilOrigin-Name: 1dfa467e449b23857ec9ea2dac47544347f3de333b7a287fc66e3a1c67268b60

--- nim/src/ui/knowledgeevents.nim
+++ nim/src/ui/knowledgeevents.nim
@@ -15,23 +15,38 @@
 # You should have received a copy of the GNU General Public License
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
-import std/strutils
+import std/[strutils, tables]
 import ../[game, maps, tk]
+import dialogs
 
 proc showEventInfoCommand(clientData: cint; interp: PInterp; argc: cint;
-    argv: cstringArray): TclResults {. exportc.} =
+    argv: cstringArray): TclResults {.exportc.} =
   let
     eventIndex = ($argv[1]).parseInt - 1
-    baseIndex = skyMap[eventsList[eventIndex].skyX][eventsList[eventIndex].skyX].baseIndex
-  var eventInfo = "X: {gold}" & $eventsList[eventIndex].skyX & "{/gold} Y: {gold}" & $eventsList[eventIndex].skyY
+    baseIndex = skyMap[eventsList[eventIndex].skyX][eventsList[
+        eventIndex].skyX].baseIndex
+  var eventInfo = "X: {gold}" & $eventsList[eventIndex].skyX &
+      "{/gold} Y: {gold}" & $eventsList[eventIndex].skyY
   case eventsList[eventIndex].eType
   of enemyShip, enemyPatrol, trader, friendlyShip:
-    eventInfo.add(y = "\nShip type: {gold}" & protoShipsList[eventsList[eventIndex].shipIndex].name & "{/gold}")
+    eventInfo.add(y = "\nShip type: {gold}" & protoShipsList[eventsList[
+        eventIndex].shipIndex].name & "{/gold}")
   of fullDocks, attackOnBase, disease:
     eventInfo.add(y = "\nBase name: {gold}" & skyBases[baseIndex].name & "{/gold}")
   of doublePrice:
     eventInfo.add(y = "\nBase name: {gold}" & skyBases[baseIndex].name & "{/gold}")
-    eventInfo.add(y = "\nItem: {gold}" & itemsList[eventsList[eventIndex].itemIndex].name & "{/gold}")
+    eventInfo.add(y = "\nItem: {gold}" & itemsList[eventsList[
+        eventIndex].itemIndex].name & "{/gold}")
+  of none, baseRecovery:
+    discard
+  showInfo(text = eventInfo, title = "Event information",
+      button1 = ButtonSettings(tooltip: "Set the event as the ship destination",
+      command: "SetDestination2 " & $eventsList[eventIndex].skyX & " " &
+      $eventsList[eventIndex].skyY, icon: "destinationicon", text: "Target",
+      color: "green"), button2 = ButtonSettings(
+      tooltip: "Show the event on the map", command: "ShowOnMap " & $eventsList[
+      eventIndex].skyX & " " & $eventsList[eventIndex].skyY, icon: "show2icon",
+      text: "Show", color: "green"))
   return tclOk
 
 proc addCommands*() {.sideEffect, raises: [], tags: [].} =
