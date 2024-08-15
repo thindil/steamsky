749d955733ea79baca347291670f36fc19f9c9aa
thindil <thindil@laeran.pl>
2024-08-15 03:08:34 +0000

    docs: added code documentation to showStoryLocationCommand procedure
    FossilOrigin-Name: 0cbb8c3d9f5debfb857669fd2f281be77476200c74f948d2877e518e259093e7

--- nim/src/ui/knowledgestories.nim
+++ nim/src/ui/knowledgestories.nim
@@ -132,6 +132,17 @@ proc showStoryCommand(clientData: cint; interp: PInterp; argc: cint;
 
 proc showStoryLocationCommand(clientData: cint; interp: PInterp; argc: cint;
    argv: cstringArray): TclResults {.sideEffect, raises: [], tags: [], exportc.} =
+  ## Show the current story event on map
+  ##
+  ## * clientData - the additional data for the Tcl command
+  ## * interp     - the Tcl interpreter on which the command was executed
+  ## * argc       - the amount of arguments entered for the command
+  ## * argv       - the list of the command's arguments
+  ##
+  ## The procedure always return tclOk
+  ##
+  ## Tcl:
+  ## ShowStoryLocation
   var (newX, newY) = try:
       getStoryLocation()
     except:
