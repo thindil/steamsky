d1aba3047170e689c5044e3095e6ee5a2735a677
thindil <thindil@laeran.pl>
2024-08-16 03:23:09 +0000

    refactor: continue work on move showKnowledgeCommand to Nim
    FossilOrigin-Name: 6fe0f4d6cb2aa9bd8ea3ebee14af63f781dc5af9ea2f0f253d6046a143cab0f2

--- nim/src/ui/knowledge.nim
+++ nim/src/ui/knowledge.nim
@@ -21,16 +21,44 @@ import coreui, knowledgebases, knowledgeevents, knowledgemissions, knowledgestor
 
 proc showKnowledgeCommand(clientData: cint; interp: PInterp; argc: cint;
    argv: cstringArray): TclResults {.exportc.} =
-  let
-    knowledgeFrame = mainPaned & ".knowledgeframe"
-    knowledgeCanvas = knowledgeFrame & ".bases.canvas"
+  var knowledgeFrame = mainPaned & ".knowledgeframe"
+  let knowledgeCanvas = knowledgeFrame & ".bases.canvas"
   if tclEval2(script = "winfo exists " & knowledgeFrame) == "0":
     tclEvalFile(fileName = dataDirectory & "ui" & DirSep & "knowledge.tcl")
     var comboValues = " {Any}"
     for baseType in basesTypesList.values:
       comboValues.add(y = "{ " & baseType.name & "}")
-    let comboBox = knowledgeCanvas & ".frame.options.types"
+    var comboBox = knowledgeCanvas & ".frame.options.types"
     tclEval(script = comboBox & " configure -values [list" & comboValues & "]")
+    tclEval(script = comboBox & " current 0")
+    comboValues = " {Any}"
+    comboBox = knowledgeCanvas & ".frame.options.owner"
+    for faction in factionsList.values:
+      comboValues.add(y = "{ " & faction.name & "}")
+    tclEval(script = comboBox & " configure -values [list" & comboValues & "]")
+    tclEval(script = comboBox & " current 0")
+  elif tclEval2(script = "winfo ismapped " & knowledgeFrame) == "1" and argc == 1:
+    tclEval(script = "InvokeButton " & closeButton)
+    tclEval(script = "grid remove " & closeButton)
+    tclEval(script = "bind . <" & generalAccelerators[1] & "> {}")
+    tclEval(script = "bind . <" & generalAccelerators[2] & "> {}")
+    tclEval(script = "bind . <" & generalAccelerators[3] & "> {}")
+    tclEval(script = "bind . <" & generalAccelerators[4] & "> {}")
+    return tclOk
+  tclSetVar(varName = "gamestate", newValue = "knowledge")
+  tclEval(script = "bind . <" & generalAccelerators[1] & "> {InvokeButton " & knowledgeCanvas & ".frame.maxmin}")
+  tclEval(script = "bind . <" & generalAccelerators[3] & "> {InvokeButton " & knowledgeFrame & ".missions.canvas.frame.maxmin}")
+  tclEval(script = "bind . <" & generalAccelerators[2] & "> {InvokeButton " & knowledgeFrame & ".events.canvas.frame.maxmin}")
+  tclEval(script = "bind . <" & generalAccelerators[4] & "> {InvokeButton " & knowledgeFrame & ".stories.canvas.frame.maxmin}")
+  tclEval(script = "grid " & closeButton & " -row 0 -column 1")
+  # Setting bases list
+  updateBasesList()
+  # Setting accepted missions info
+  updateMissionsList()
+  # Setting the known events list
+  updateEventsList()
+  # Setting the known stories list
+  knowledgeFrame = mainPaned & ".knowledgeframe.stories.canvas.frame"
   return tclOk
 
 proc addCommands*() {.sideEffect, raises: [], tags: [].} =
