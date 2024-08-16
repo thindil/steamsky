9a5044003c5a230e10946c2f5bfc25c5d0aeed8c
thindil <thindil@laeran.pl>
2024-08-16 03:31:23 +0000

    refactor: continue work on move showKnowledgeCommand to Nim
    FossilOrigin-Name: 8dd5a7d8f4fa785aec012b59557c62469a9b016d6196068c6d4f45832f15ff68

--- nim/src/ui/knowledge.nim
+++ nim/src/ui/knowledge.nim
@@ -15,8 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
-import std/[os, tables]
-import ../[basestypes, game, tk]
+import std/[os, strutils, tables]
+import ../[basestypes, game, stories, tk]
 import coreui, knowledgebases, knowledgeevents, knowledgemissions, knowledgestories
 
 proc showKnowledgeCommand(clientData: cint; interp: PInterp; argc: cint;
@@ -40,16 +40,20 @@ proc showKnowledgeCommand(clientData: cint; interp: PInterp; argc: cint;
   elif tclEval2(script = "winfo ismapped " & knowledgeFrame) == "1" and argc == 1:
     tclEval(script = "InvokeButton " & closeButton)
     tclEval(script = "grid remove " & closeButton)
+    tclEval(script = "bind . <" & generalAccelerators[0] & "> {}")
     tclEval(script = "bind . <" & generalAccelerators[1] & "> {}")
     tclEval(script = "bind . <" & generalAccelerators[2] & "> {}")
     tclEval(script = "bind . <" & generalAccelerators[3] & "> {}")
-    tclEval(script = "bind . <" & generalAccelerators[4] & "> {}")
     return tclOk
   tclSetVar(varName = "gamestate", newValue = "knowledge")
-  tclEval(script = "bind . <" & generalAccelerators[1] & "> {InvokeButton " & knowledgeCanvas & ".frame.maxmin}")
-  tclEval(script = "bind . <" & generalAccelerators[3] & "> {InvokeButton " & knowledgeFrame & ".missions.canvas.frame.maxmin}")
-  tclEval(script = "bind . <" & generalAccelerators[2] & "> {InvokeButton " & knowledgeFrame & ".events.canvas.frame.maxmin}")
-  tclEval(script = "bind . <" & generalAccelerators[4] & "> {InvokeButton " & knowledgeFrame & ".stories.canvas.frame.maxmin}")
+  tclEval(script = "bind . <" & generalAccelerators[0] & "> {InvokeButton " &
+      knowledgeCanvas & ".frame.maxmin}")
+  tclEval(script = "bind . <" & generalAccelerators[2] & "> {InvokeButton " &
+      knowledgeFrame & ".missions.canvas.frame.maxmin}")
+  tclEval(script = "bind . <" & generalAccelerators[1] & "> {InvokeButton " &
+      knowledgeFrame & ".events.canvas.frame.maxmin}")
+  tclEval(script = "bind . <" & generalAccelerators[3] & "> {InvokeButton " &
+      knowledgeFrame & ".stories.canvas.frame.maxmin}")
   tclEval(script = "grid " & closeButton & " -row 0 -column 1")
   # Setting bases list
   updateBasesList()
@@ -59,6 +63,25 @@ proc showKnowledgeCommand(clientData: cint; interp: PInterp; argc: cint;
   updateEventsList()
   # Setting the known stories list
   knowledgeFrame = mainPaned & ".knowledgeframe.stories.canvas.frame"
+  var rows = try:
+      tclEval2(script = "grid size " & knowledgeFrame).split(" ")[1].parseInt
+    except:
+      showError(message = "Can't get the amount of rows.")
+      return
+  deleteWidgets(startIndex = 1, endIndex = rows - 1, frame = knowledgeFrame)
+  if finishedStories.len == 0:
+    let label = knowledgeFrame & ".nostories"
+    tclEval(script = "ttk::label " & label & " -text {You didn't discover any story yet.} -wraplength 400")
+    tclEval(script = "grid " & label & " -padx 10")
+  else:
+    var finishedStoriesList = ""
+    for finishedStory in finishedStories:
+      finishedStoriesList.add(y = "{ " & storiesList[finishedStory.index].name & "}")
+    let optionsFrame = knowledgeFrame & ".options"
+    tclEval(script = "ttk::frame " & optionsFrame)
+    let storiesBox = optionsFrame & ".titles"
+    tclEval(script = "ttk::combobox " & storiesBox &
+        " -state readonly -values [list " & finishedStoriesList & "]")
   return tclOk
 
 proc addCommands*() {.sideEffect, raises: [], tags: [].} =
