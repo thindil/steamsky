4c09d8e86709cd1c7d37032f3f3b09898e07ae56
thindil <thindil@laeran.pl>
2024-08-16 03:13:46 +0000

    refactor: started work on move showKnowledgeCommand to Nim
    FossilOrigin-Name: 776b197ee93695396f4f3bd237643dba125556a93632aa9d6888354f2d55304d

--- nim/src/ui/knowledge.nim
+++ nim/src/ui/knowledge.nim
@@ -15,8 +15,23 @@
 # You should have received a copy of the GNU General Public License
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
-import ../tk
-import knowledgebases, knowledgeevents, knowledgemissions, knowledgestories
+import std/[os, tables]
+import ../[basestypes, game, tk]
+import coreui, knowledgebases, knowledgeevents, knowledgemissions, knowledgestories
+
+proc showKnowledgeCommand(clientData: cint; interp: PInterp; argc: cint;
+   argv: cstringArray): TclResults {.exportc.} =
+  let
+    knowledgeFrame = mainPaned & ".knowledgeframe"
+    knowledgeCanvas = knowledgeFrame & ".bases.canvas"
+  if tclEval2(script = "winfo exists " & knowledgeFrame) == "0":
+    tclEvalFile(fileName = dataDirectory & "ui" & DirSep & "knowledge.tcl")
+    var comboValues = " {Any}"
+    for baseType in basesTypesList.values:
+      comboValues.add(y = "{ " & baseType.name & "}")
+    let comboBox = knowledgeCanvas & ".frame.options.types"
+    tclEval(script = comboBox & " configure -values [list" & comboValues & "]")
+  return tclOk
 
 proc addCommands*() {.sideEffect, raises: [], tags: [].} =
   ## Adds Tcl commands related to the trades UI
@@ -25,6 +40,6 @@ proc addCommands*() {.sideEffect, raises: [], tags: [].} =
     knowledgeevents.addCommands()
     knowledgemissions.addCommands()
     knowledgestories.addCommands()
-#    addCommand("ShowLoot", showLootCommand)
+#    addCommand("ShowKnowledge", showKnowledgeCommand)
   except:
     showError(message = "Can't add a Tcl command.")
