dbbfb6e18293a57f4392c060967f0ee80d3b2287
thindil <thindil@laeran.pl>
2024-08-17 03:17:48 +0000

    docs: added code documentation to showKnowledgeCommand procedure
    FossilOrigin-Name: 3bfd89c280189cb2e0e23927b1e4d41aeaf34543f0b9902dd8ee91b21309990a

--- nim/src/ui/knowledge.nim
+++ nim/src/ui/knowledge.nim
@@ -22,6 +22,17 @@ import coreui, knowledgebases, knowledgeevents, knowledgemissions,
 
 proc showKnowledgeCommand(clientData: cint; interp: PInterp; argc: cint;
    argv: cstringArray): TclResults {.sideEffect, raises: [], tags: [RootEffect], exportc.} =
+  ## Show information about known by player things
+  ##
+  ## * clientData - the additional data for the Tcl command
+  ## * interp     - the Tcl interpreter on which the command was executed
+  ## * argc       - the amount of arguments entered for the command
+  ## * argv       - the list of the command's arguments
+  ##
+  ## The procedure always return tclOk
+  ##
+  ## Tcl:
+  ## ShowKnowledge
   var
     knowledgeFrame = mainPaned & ".knowledgeframe"
     knowledgeCanvas = knowledgeFrame & ".bases.canvas"
