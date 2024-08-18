3dadbe0d98a7db74beaa5f6385ca23a608fab396
thindil <thindil@laeran.pl>
2024-08-18 03:08:53 +0000

    refactor: removed unused code
    FossilOrigin-Name: f1b02ebe6b3bc2cba615dd88151b40ba45b67a139a6e2221f72b24013de421e6

--- src/ui/knowledge.adb
+++ src/ui/knowledge.adb
@@ -13,18 +13,9 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
--- with Ada.Strings;
--- with Ada.Strings.Unbounded;
--- with Interfaces.C.Strings;
 with Interfaces.C;
 with CArgv;
 with Tcl;
--- with Tcl.Tk.Ada;
--- with Tcl.Tk.Ada.Grid;
--- with Tcl.Tk.Ada.Widgets;
--- with Tcl.Tk.Ada.Widgets.TtkButton;
--- with Tcl.Tk.Ada.Widgets.TtkFrame;
--- with CoreUI;
 with Knowledge.Bases;
 with Knowledge.Events;
 with Knowledge.Missions;
@@ -75,95 +66,6 @@ package body Knowledge is
       Convention => C,
       External_Name => "knowledgeMaxMinCommand";
       -- ****
-
---   function Knowledge_Max_Min_Command
---     (Client_Data: Integer; Interp: Tcl.Tcl_Interp; Argc: Interfaces.C.int;
---      Argv: CArgv.Chars_Ptr_Ptr) return Interfaces.C.int is
---      pragma Unreferenced(Client_Data, Argc);
---      use Ada.Strings.Unbounded;
---      use Interfaces.C.Strings;
---      use Tcl.Tk.Ada.Widgets;
---      use Tcl.Tk.Ada.Widgets.TtkButton;
---      use Tcl.Tk.Ada.Widgets.TtkFrame;
---      use CoreUI;
---
---      --## rule off TYPE_INITIAL_VALUES
---      type Frame_Info is record
---         Name: Unbounded_String;
---         Column: Natural range 0 .. 1;
---         Row: Natural range 0 .. 1;
---      end record;
---      --## rule on TYPE_INITIAL_VALUES
---      Frames: constant array(1 .. 4) of Frame_Info :=
---        (1 =>
---           (Name => To_Unbounded_String(Source => "bases"), Column => 0,
---            Row => 0),
---         2 =>
---           (Name => To_Unbounded_String(Source => "missions"), Column => 0,
---            Row => 1),
---         3 =>
---           (Name => To_Unbounded_String(Source => "events"), Column => 1,
---            Row => 0),
---         4 =>
---           (Name => To_Unbounded_String(Source => "stories"), Column => 1,
---            Row => 1));
---      Frame_Name: constant String := Main_Paned & ".knowledgeframe";
---      Frame: Ttk_Frame := Get_Widget(pathName => Frame_Name, Interp => Interp);
---      Button: constant Ttk_Button :=
---        Get_Widget
---          (pathName =>
---             Frame_Name & "." & CArgv.Arg(Argv => Argv, N => 1) &
---             ".canvas.frame.maxmin",
---           Interp => Interp);
---   begin
---      if CArgv.Arg(Argv => Argv, N => 2) = "show" then
---         Show_Manipulate_Frames_Loop :
---         for FrameInfo of Frames loop
---            Frame.Name :=
---              New_String
---                (Str =>
---                   Frame_Name & "." & To_String(Source => FrameInfo.Name));
---            if To_String(Source => FrameInfo.Name) =
---              CArgv.Arg(Argv => Argv, N => 1) then
---               Tcl.Tk.Ada.Grid.Grid_Configure
---                 (Slave => Frame,
---                  Options => "-columnspan 2 -rowspan 2 -row 0 -column 0");
---            else
---               Tcl.Tk.Ada.Grid.Grid_Remove(Slave => Frame);
---            end if;
---         end loop Show_Manipulate_Frames_Loop;
---         configure
---           (Widgt => Button,
---            options =>
---              "-image movemapdownicon -command {KnowledgeMaxMin " &
---              CArgv.Arg(Argv => Argv, N => 1) & " hide}");
---      else
---         Hide_Manipulate_Frames_Loop :
---         for FrameInfo of Frames loop
---            Frame.Name :=
---              New_String
---                (Str =>
---                   Frame_Name & "." & To_String(Source => FrameInfo.Name));
---            if To_String(Source => FrameInfo.Name) =
---              CArgv.Arg(Argv => Argv, N => 1) then
---               Tcl.Tk.Ada.Grid.Grid_Configure
---                 (Slave => Frame,
---                  Options =>
---                    "-columnspan 1 -rowspan 1 -column" &
---                    Natural'Image(FrameInfo.Column) & " -row" &
---                    Natural'Image(FrameInfo.Row));
---            else
---               Tcl.Tk.Ada.Grid.Grid(Slave => Frame);
---            end if;
---         end loop Hide_Manipulate_Frames_Loop;
---         configure
---           (Widgt => Button,
---            options =>
---              "-image movemapupicon -command {KnowledgeMaxMin " &
---              CArgv.Arg(Argv => Argv, N => 1) & " show}");
---      end if;
---      return TCL_OK;
---   end Knowledge_Max_Min_Command;
 
    procedure Add_Commands is
       use Utils.UI;
