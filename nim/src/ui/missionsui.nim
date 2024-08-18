7e57c1623e180e21f1f0d5b0a6d56557330e5817
thindil <thindil@laeran.pl>
2024-08-18 03:19:25 +0000

    refactor: moved showMissionCommand procedure to Nim
    FossilOrigin-Name: 2e53fb51de77249c0ad9aafaaff965bfefe025ac4eee222fc0d01fdb78689f99

--- nim/src/ui/missionsui.nim
+++ nim/src/ui/missionsui.nim
@@ -15,12 +15,28 @@
 # You should have received a copy of the GNU General Public License
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
-import ../tk
+import std/strutils
+import ../[game, tk]
+
+var baseIndex = 0
+
+proc showMissionCommand(clientData: cint; interp: PInterp; argc: cint;
+   argv: cstringArray): TclResults {.exportc.} =
+  let missionIndex = ($argv[1]).parseInt - 1
+  tclEval(script = "ShowOnMap " & $skyBases[baseIndex].missions[
+      missionIndex].targetX & " " & $skyBases[baseIndex].missions[
+      missionIndex].targetX)
+  return tclOk
 
 proc addCommands*() {.sideEffect, raises: [], tags: [].} =
   ## Adds Tcl commands related to the list of available missions
   try:
     discard
-#    addCommand("ShowStory", showStoryCommand)
+#    addCommand("ShowMission", showMissionCommand)
   except:
     showError(message = "Can't add a Tcl command.")
+
+# Temporary code for interfacing with Ada
+
+proc getMissionBaseIndex(bIndex: cint) {.exportc.} =
+  baseIndex = bIndex
