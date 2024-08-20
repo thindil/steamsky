001fb7485b66cf0d9ed3ace61f29b5fbbbbb377a
thindil <thindil@laeran.pl>
2024-08-20 03:41:03 +0000

    docs: added code documentation to showBaseMissionsCommand procedure
    FossilOrigin-Name: 3bbc59d7bb21ad86758b2c01256d046c9ed434dadfe08c1de08c1374bfe941e5

--- nim/src/ui/missionsui.nim
+++ nim/src/ui/missionsui.nim
@@ -193,6 +193,19 @@ proc refreshMissionsList(page: Positive = 1) {.sideEffect, raises: [], tags: [].
 
 proc showBaseMissionsCommand(clientData: cint; interp: PInterp; argc: cint;
    argv: cstringArray): TclResults {.sideEffect, raises: [], tags: [RootEffect], exportc.} =
+  ## Show the list of available missions in the base
+  ##
+  ## * clientData - the additional data for the Tcl command
+  ## * interp     - the Tcl interpreter on which the command was executed
+  ## * argc       - the amount of arguments entered for the command
+  ## * argv       - the list of the command's arguments
+  ##
+  ## The procedure always return tclOk
+  ##
+  ## Tcl:
+  ## ShowBaseMissions ?page?
+  ## Page is the number of page of the missions list to show. If not
+  ## set then it is 1
   var missionsFrame = mainPaned & ".missionsframe"
   let
     missionsCanvas = missionsFrame & ".canvas"
