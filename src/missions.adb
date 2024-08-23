16ca7e22053f33b4069ac71bb0980b2a116c03bf
thindil <thindil@laeran.pl>
2024-08-23 03:16:16 +0000

    chore: fixed some errors reported by AdaControl
    FossilOrigin-Name: 74f0f0087e377910cc9ce033f574940c63e9ba69c12eb582dbe9746eb06b97ad

--- src/missions.adb
+++ src/missions.adb
@@ -16,69 +16,11 @@
 --    along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
 with Interfaces.C.Strings; use Interfaces.C.Strings;
-with Ships; use Ships;
-with Maps; use Maps;
+with Ships;
+with Maps;
 with Bases; use Bases;
 
 package body Missions is
-
-   function Get_Accepted_Mission(Mission_Index: Positive) return Mission_Data;
-   function Get_Accepted_Missions_Amount return Natural with
-      Import => True,
-      Convention => C,
-      External_Name => "getAdaAcceptedMissionsAmount";
-
-   function Auto_Finish_Missions return String is
-      Base_Index: constant Natural :=
-        Sky_Map(Player_Ship.Sky_X, Player_Ship.Sky_Y).Base_Index;
-      Message: chars_ptr;
-      function Auto_Ada_Finish_Mission return chars_ptr with
-         Import => True,
-         Convention => C,
-         External_Name => "autoAdaFinishMissions";
-   begin
-      if Base_Index = 0 then
-         return "";
-      end if;
-      Set_Ship_In_Nim;
-      Get_Ada_Base_Location
-        (Base_Index => Base_Index, X => Sky_Bases(Base_Index).Sky_X,
-         Y => Sky_Bases(Base_Index).Sky_Y);
-      Message := Auto_Ada_Finish_Mission;
-      Get_Ship_From_Nim(Ship => Player_Ship);
-      Sky_Map(Sky_Bases(Base_Index).Sky_X, Sky_Bases(Base_Index).Sky_Y)
-        .Mission_Index :=
-        0;
-      Update_Map_Loop :
-      for I in 1 .. Get_Accepted_Missions_Amount loop
-         if Get_Accepted_Mission(Mission_Index => I).Finished then
-            Sky_Map
-              (Sky_Bases(Get_Accepted_Mission(Mission_Index => I).Start_Base)
-                 .Sky_X,
-               Sky_Bases(Get_Accepted_Mission(Mission_Index => I).Start_Base)
-                 .Sky_Y)
-              .Mission_Index :=
-              I;
-         else
-            Sky_Map
-              (Get_Accepted_Mission(Mission_Index => I).Target_X,
-               Get_Accepted_Mission(Mission_Index => I).Target_Y)
-              .Mission_Index :=
-              I;
-         end if;
-      end loop Update_Map_Loop;
-      return Value(Item => Message);
-   end Auto_Finish_Missions;
-
-   function Get_Mission_Type(M_Type: Missions_Types) return String is
-      function Get_Ada_Mission_Type(M_T: Integer) return chars_ptr with
-         Import => True,
-         Convention => C,
-         External_Name => "getAdaMissionType";
-   begin
-      return
-        Value(Item => Get_Ada_Mission_Type(M_T => Missions_Types'Pos(M_Type)));
-   end Get_Mission_Type;
 
    --## rule off TYPE_INITIAL_VALUES
    type Nim_Mission_Data is record
@@ -95,46 +37,6 @@ package body Missions is
 
    type Nim_Missions_Array is array(0 .. 49) of Nim_Mission_Data;
    --## rule on TYPE_INITIAL_VALUES
-
-   procedure Get_Missions(Base_Index: Positive) is
-      --## rule off IMPROPER_INITIALIZATION
-      Nim_Missions: Nim_Missions_Array;
-      Missions_List: constant Mission_Container.Vector :=
-        Sky_Bases(Base_Index).Missions;
-      --## rule on IMPROPER_INITIALIZATION
-      procedure Get_Ada_Missions
-        (N_Missions: Nim_Missions_Array; B_Index: Natural) with
-         Import => True,
-         Convention => C,
-         External_Name => "getAdaMissions";
-   begin
-      Convert_Missions_Loop :
-      for I in Nim_Missions'Range loop
-         if I <= Integer(Missions_List.Length - 1) then
-            Nim_Missions(I) :=
-              (Time => Missions_List(I + 1).Time,
-               Target_X => Missions_List(I + 1).Target_X,
-               Target_Y => Missions_List(I + 1).Target_Y,
-               Reward => Missions_List(I + 1).Reward,
-               Start_Base => Missions_List(I + 1).Start_Base,
-               Finished => (if Missions_List(I + 1).Finished then 1 else 0),
-               Multiplier => Missions_List(I + 1).Multiplier,
-               M_Type => Missions_Types'Pos(Missions_List(I + 1).M_Type),
-               Data =>
-                 (case Missions_List(I + 1).M_Type is
-                    when DELIVER => Missions_List(I + 1).Item_Index,
-                    when DESTROY => Missions_List(I + 1).Ship_Index,
-                    when PASSENGER => Missions_List(I + 1).Data,
-                    when others => Missions_List(I + 1).Target));
-         else
-            Nim_Missions(I) :=
-              (Time => 0, Target_X => 0, Target_Y => 0, Reward => 0,
-               Start_Base => 0, Finished => 0, Multiplier => 0.0, M_Type => 0,
-               Data => 0);
-         end if;
-      end loop Convert_Missions_Loop;
-      Get_Ada_Missions(N_Missions => Nim_Missions, B_Index => Base_Index);
-   end Get_Missions;
 
    -- ****if* Missions/Missions.Set_Missions_List
    -- FUNCTION
@@ -230,16 +132,114 @@ package body Missions is
       return Missions_List;
    end Set_Missions_List;
 
+   function Auto_Finish_Missions return String is
+      use Maps;
+      use Ships;
+
+      Base_Index: constant Natural :=
+        Sky_Map(Player_Ship.Sky_X, Player_Ship.Sky_Y).Base_Index;
+      Message: chars_ptr;
+      function Auto_Ada_Finish_Mission return chars_ptr with
+         Import => True,
+         Convention => C,
+         External_Name => "autoAdaFinishMissions";
+      function Get_Accepted_Mission
+        (Mission_Index: Positive) return Mission_Data is
+      begin
+         return Set_Missions_List(Base_Index => 0)(Mission_Index);
+      end Get_Accepted_Mission;
+      function Get_Accepted_Missions_Amount return Natural with
+         Import => True,
+         Convention => C,
+         External_Name => "getAdaAcceptedMissionsAmount";
+   begin
+      if Base_Index = 0 then
+         return "";
+      end if;
+      Set_Ship_In_Nim;
+      Get_Ada_Base_Location
+        (Base_Index => Base_Index, X => Sky_Bases(Base_Index).Sky_X,
+         Y => Sky_Bases(Base_Index).Sky_Y);
+      Message := Auto_Ada_Finish_Mission;
+      Get_Ship_From_Nim(Ship => Player_Ship);
+      Sky_Map(Sky_Bases(Base_Index).Sky_X, Sky_Bases(Base_Index).Sky_Y)
+        .Mission_Index :=
+        0;
+      Update_Map_Loop :
+      for I in 1 .. Get_Accepted_Missions_Amount loop
+         if Get_Accepted_Mission(Mission_Index => I).Finished then
+            Sky_Map
+              (Sky_Bases(Get_Accepted_Mission(Mission_Index => I).Start_Base)
+                 .Sky_X,
+               Sky_Bases(Get_Accepted_Mission(Mission_Index => I).Start_Base)
+                 .Sky_Y)
+              .Mission_Index :=
+              I;
+         else
+            Sky_Map
+              (Get_Accepted_Mission(Mission_Index => I).Target_X,
+               Get_Accepted_Mission(Mission_Index => I).Target_Y)
+              .Mission_Index :=
+              I;
+         end if;
+      end loop Update_Map_Loop;
+      return Value(Item => Message);
+   end Auto_Finish_Missions;
+
+   function Get_Mission_Type(M_Type: Missions_Types) return String is
+      function Get_Ada_Mission_Type(M_T: Integer) return chars_ptr with
+         Import => True,
+         Convention => C,
+         External_Name => "getAdaMissionType";
+   begin
+      return
+        Value(Item => Get_Ada_Mission_Type(M_T => Missions_Types'Pos(M_Type)));
+   end Get_Mission_Type;
+
+   procedure Get_Missions(Base_Index: Positive) is
+      --## rule off IMPROPER_INITIALIZATION
+      Nim_Missions: Nim_Missions_Array;
+      Missions_List: constant Mission_Container.Vector :=
+        Sky_Bases(Base_Index).Missions;
+      --## rule on IMPROPER_INITIALIZATION
+      procedure Get_Ada_Missions
+        (N_Missions: Nim_Missions_Array; B_Index: Natural) with
+         Import => True,
+         Convention => C,
+         External_Name => "getAdaMissions";
+   begin
+      Convert_Missions_Loop :
+      for I in Nim_Missions'Range loop
+         if I <= Integer(Missions_List.Length - 1) then
+            Nim_Missions(I) :=
+              (Time => Missions_List(I + 1).Time,
+               Target_X => Missions_List(I + 1).Target_X,
+               Target_Y => Missions_List(I + 1).Target_Y,
+               Reward => Missions_List(I + 1).Reward,
+               Start_Base => Missions_List(I + 1).Start_Base,
+               Finished => (if Missions_List(I + 1).Finished then 1 else 0),
+               Multiplier => Missions_List(I + 1).Multiplier,
+               M_Type => Missions_Types'Pos(Missions_List(I + 1).M_Type),
+               Data =>
+                 (case Missions_List(I + 1).M_Type is
+                    when DELIVER => Missions_List(I + 1).Item_Index,
+                    when DESTROY => Missions_List(I + 1).Ship_Index,
+                    when PASSENGER => Missions_List(I + 1).Data,
+                    when others => Missions_List(I + 1).Target));
+         else
+            Nim_Missions(I) :=
+              (Time => 0, Target_X => 0, Target_Y => 0, Reward => 0,
+               Start_Base => 0, Finished => 0, Multiplier => 0.0, M_Type => 0,
+               Data => 0);
+         end if;
+      end loop Convert_Missions_Loop;
+      Get_Ada_Missions(N_Missions => Nim_Missions, B_Index => Base_Index);
+   end Get_Missions;
+
    procedure Set_Missions(Base_Index: Positive) is
    begin
       Sky_Bases(Base_Index).Missions :=
         Set_Missions_List(Base_Index => Base_Index);
    end Set_Missions;
-
-   function Get_Accepted_Mission
-     (Mission_Index: Positive) return Mission_Data is
-   begin
-      return Set_Missions_List(Base_Index => 0)(Mission_Index);
-   end Get_Accepted_Mission;
 
 end Missions;
