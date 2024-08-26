f0f848f1a1096e5d0f885ff81d161e193807b7ea
thindil <thindil@laeran.pl>
2024-08-26 03:29:09 +0000

    docs: added code documentation to sortFinishedCraftingCommand procedure
    FossilOrigin-Name: 7b1c81c66de5e12f00bbdba17a15a45ed8f2b1ddccb3979c9c6f16381d46fb23

--- nim/src/ui/statisticsui.nim
+++ nim/src/ui/statisticsui.nim
@@ -314,6 +314,18 @@ type
 
 proc sortFinishedCraftingCommand(clientData: cint; interp: PInterp; argc: cint;
     argv: cstringArray): TclResults {.sideEffect, raises: [], tags: [], exportc.} =
+  ## Sort the list of finished crafting orders
+  ##
+  ## * clientData - the additional data for the Tcl command
+  ## * interp     - the Tcl interpreter on which the command was executed
+  ## * argc       - the amount of arguments entered for the command
+  ## * argv       - the list of the command's arguments
+  ##
+  ## The procedure always return tclOk
+  ##
+  ## Tcl:
+  ## SortFinishedCrafting x
+  ## X is the number of column where the player clicked the mouse button
   let column = try:
         ($argv[1]).parseInt
       except:
