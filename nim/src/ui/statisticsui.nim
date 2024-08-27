2fd370a7fdf432116c1e208f0351a59f6e24b7c4
thindil <thindil@laeran.pl>
2024-08-27 03:09:13 +0000

    docs: added code documentation to sortFinishedMissionsCommand procedure
    FossilOrigin-Name: 716c9064d2647e05a974c5aa31ebbbecbfd5ea504cc91ab321e5a17f699af2c1

--- nim/src/ui/statisticsui.nim
+++ nim/src/ui/statisticsui.nim
@@ -375,6 +375,18 @@ var missionsSortOrder: ListSortOrders = defaultListSortOrder
 
 proc sortFinishedMissionsCommand(clientData: cint; interp: PInterp; argc: cint;
     argv: cstringArray): TclResults {.sideEffect, raises: [], tags: [], exportc.} =
+  ## Sort the list of finished missions
+  ##
+  ## * clientData - the additional data for the Tcl command
+  ## * interp     - the Tcl interpreter on which the command was executed
+  ## * argc       - the amount of arguments entered for the command
+  ## * argv       - the list of the command's arguments
+  ##
+  ## The procedure always return tclOk
+  ##
+  ## Tcl:
+  ## SortFinishedMissions x
+  ## X is the number of column where the player clicked the mouse button
   let column = try:
         ($argv[1]).parseInt
       except:
