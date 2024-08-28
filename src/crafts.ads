005020641cb20f95c0a0d6a2d5878ef5e9a317bb
thindil <thindil@laeran.pl>
2024-08-28 03:09:42 +0000

    refactor: removed unused code
    FossilOrigin-Name: b163a16e724b6f06d92174bc4d70dfb87a36bde546b7bac186bd46108f7f693e

--- src/crafts.ads
+++ src/crafts.ads
@@ -15,47 +15,11 @@
 --    You should have received a copy of the GNU General Public License
 --    along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
-with ShipModules; use ShipModules;
-with Game; use Game;
-
 -- ****h* Crafts/Crafts
 -- FUNCTION
 -- Provide code for crafting
 -- SOURCE
 package Crafts is
 -- ****
-
-   --## rule off TYPE_INITIAL_VALUES
-   -- ****s* Crafts/Crafts.Craft_Data
-   -- FUNCTION
-   -- Data structure for recipes
-   -- PARAMETERS
-   -- Material_Types   - Types of material needed for recipe
-   -- Material_Amounts - Amounts of material needed for recipe
-   -- Result_Index     - Prototype index of crafted item
-   -- Result_Amount    - Amount of products
-   -- Workplace        - Ship module needed for crafting
-   -- Skill            - Skill used in crafting item
-   -- Time             - Minutes needed for finish recipe
-   -- Difficulty       - How difficult is recipe to discover
-   -- Tool             - Type of tool used to craft item
-   -- Reputation       - Minimal reputation in base needed to buy that recipe
-   -- Tool_Quality     - Minimal quality of tool needed to craft that recipe
-   -- SOURCE
-   type Craft_Data is record
-      Material_Types: TinyString_Container.Vector;
-      Material_Amounts: Positive_Container.Vector;
-      Result_Index: Natural;
-      Result_Amount: Natural := 0;
-      Workplace: Module_Type;
-      Skill: SkillsData_Container.Extended_Index;
-      Time: Positive := 1;
-      Difficulty: Positive := 1;
-      Tool: Tiny_String.Bounded_String;
-      Reputation: Reputation_Range;
-      Tool_Quality: Positive := 1;
-   end record;
-   -- ****
-   --## rule on TYPE_INITIAL_VALUES
 
 end Crafts;
