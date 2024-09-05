b46f8a742188f2c51f381cb327184f7a78e18ec2
thindil <thindil@laeran.pl>
2024-09-05 03:33:05 +0000

    docs: added code documentation to debugUpdateBaseCommand procedure
    FossilOrigin-Name: 68da0e0e52248fa1c8f72bb9ff122a3114e2b97806bc4262624a325d6f17f59a

--- nim/src/ui/debugui.nim
+++ nim/src/ui/debugui.nim
@@ -579,6 +579,17 @@ proc debugUpdateItemCommand(clientData: cint; interp: PInterp; argc: cint;
 
 proc debugUpdateBaseCommand(clientData: cint; interp: PInterp; argc: cint;
     argv: cstringArray): TclResults {.sideEffect, raises: [], tags: [], exportc.} =
+  ## Update the selected base
+  ##
+  ## * clientData - the additional data for the Tcl command
+  ## * interp     - the Tcl interpreter on which the command was executed
+  ## * argc       - the amount of arguments entered for the command
+  ## * argv       - the list of the command's arguments
+  ##
+  ## The procedure always return tclOk
+  ##
+  ## Tcl:
+  ## DebugUpdateBase
   let
     frameName = ".debugdialog.main.bases"
     baseEntry = frameName & ".name"
