4ba1a402093dda1414d63d6822abddfe51b813e3
thindil <thindil@laeran.pl>
2024-09-05 03:10:06 +0000

    docs: added code documentation to debugUpdateItemCommand procedure
    FossilOrigin-Name: 92e875447c918451d482acc254208df09e1f604d6ce8c5e23eeaa9eea143abce

--- nim/src/ui/debugui.nim
+++ nim/src/ui/debugui.nim
@@ -551,6 +551,17 @@ proc debugAddItemCommand(clientData: cint; interp: PInterp; argc: cint;
 
 proc debugUpdateItemCommand(clientData: cint; interp: PInterp; argc: cint;
     argv: cstringArray): TclResults {.sideEffect, raises: [], tags: [], exportc.} =
+  ## Update the amount of an item in the player ship cargo
+  ##
+  ## * clientData - the additional data for the Tcl command
+  ## * interp     - the Tcl interpreter on which the command was executed
+  ## * argc       - the amount of arguments entered for the command
+  ## * argv       - the list of the command's arguments
+  ##
+  ## The procedure always return tclOk
+  ##
+  ## Tcl:
+  ## DebugUpdateItem
   let
     frameName = ".debugdialog.main.cargo"
     itemCombo = frameName & ".update"
