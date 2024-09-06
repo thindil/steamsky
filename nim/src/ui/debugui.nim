74a1f6136a4303e35a58959e57b8c3ec56c6f375
thindil <thindil@laeran.pl>
2024-09-06 03:24:06 +0000

    docs: added code documentation to debugAddShipCommand procedure
    FossilOrigin-Name: dece69ad42550b2e32bb711e7aa27b0b714d73855ab96fe927f4880077d6af99

--- nim/src/ui/debugui.nim
+++ nim/src/ui/debugui.nim
@@ -639,6 +639,17 @@ proc debugUpdateBaseCommand(clientData: cint; interp: PInterp; argc: cint;
 
 proc debugAddShipCommand(clientData: cint; interp: PInterp; argc: cint;
     argv: cstringArray): TclResults {.sideEffect, raises: [], tags: [], exportc.} =
+  ## Add a new ship based event to the game
+  ##
+  ## * clientData - the additional data for the Tcl command
+  ## * interp     - the Tcl interpreter on which the command was executed
+  ## * argc       - the amount of arguments entered for the command
+  ## * argv       - the list of the command's arguments
+  ##
+  ## The procedure always return tclOk
+  ##
+  ## Tcl:
+  ## DebugAddShip
   let
     frameName = ".debugdialog.main.world"
     shipEntry = frameName & ".ship"
