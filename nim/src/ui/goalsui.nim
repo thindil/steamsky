5157d6bf4c82d7be50014cd65052615b37a5802f
thindil <thindil@laeran.pl>
2024-09-22 03:29:55 +0000

    refactor: continue work on move showGoalsCommand procedure to Nim
    FossilOrigin-Name: 485e871f3ec97b4ecc4a3296a5fcc5849b22bbfabcbd597cd80b40d1e2cab767

--- nim/src/ui/goalsui.nim
+++ nim/src/ui/goalsui.nim
@@ -16,7 +16,7 @@
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
 import std/[os, tables, strutils]
-import ../[game, goals, tk]
+import ../[config, game, goals, tk]
 import errordialog
 
 proc showGoalsCommand(clientData: cint; interp: PInterp; argc: cint;
@@ -30,6 +30,16 @@ proc showGoalsCommand(clientData: cint; interp: PInterp; argc: cint;
         " end -id {" & goal.index & "} -text {" & goalText(
         index = goal.index.parseInt) & "}")
   let selectButton = goalsDialog & ".selectbutton"
+  tclEval(script = selectButton & " configure -command {SetGoal " & $argv[1] & "}")
+  let dialogHeader = goalsDialog & ".header"
+  tclEval(script = "bind " & dialogHeader & " <ButtonPress-" & (
+      if gameSettings.rightButton: "3" else: "1") & "> {SetMousePosition " &
+      dialogHeader & " %X %Y}")
+  tclEval(script = "bind " & dialogHeader & " <Motion> {MoveDialog " &
+      dialogHeader & " %X %Y}")
+  tclEval(script = "bind " & dialogHeader & " <ButtonRelease-" & (
+      if gameSettings.rightButton: "3" else: "1") & "> {SetMousePosition " &
+      dialogHeader & " 0 0}")
   return tclOk
 
 proc addCommands*() {.sideEffect, raises: [], tags: [WriteIOEffect,
