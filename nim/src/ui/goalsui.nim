c57264c9a600e975d54dea67f630f9350db6db2c
thindil <thindil@laeran.pl>
2024-09-22 03:26:25 +0000

    refactor: started work on move showGoalsCommand procedure to Nim
    FossilOrigin-Name: f06eeec255aa39151e5a6a9fcb1e004fc75c5bd6d658b120324e3f2f06b736d9

--- nim/src/ui/goalsui.nim
+++ nim/src/ui/goalsui.nim
@@ -15,12 +15,28 @@
 # You should have received a copy of the GNU General Public License
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
+import std/[os, tables, strutils]
+import ../[game, goals, tk]
 import errordialog
 
-proc addCommands*() {.sideEffect, raises: [], tags: [WriteIOEffect, TimeEffect].} =
+proc showGoalsCommand(clientData: cint; interp: PInterp; argc: cint;
+    argv: cstringArray): TclResults {.exportc.} =
+  tclEvalFile(dataDirectory & "ui" & DirSep & "goals.tcl")
+  let
+    goalsDialog = ".goalsdialog"
+    goalsView = goalsDialog & ".view"
+  for goal in goalsList.values:
+    tclEval(script = goalsView & " insert " & ($goal.goalType).toUpperAscii &
+        " end -id {" & goal.index & "} -text {" & goalText(
+        index = goal.index.parseInt) & "}")
+  let selectButton = goalsDialog & ".selectbutton"
+  return tclOk
+
+proc addCommands*() {.sideEffect, raises: [], tags: [WriteIOEffect,
+    TimeEffect].} =
   ## Adds Tcl commands related to the goals UI
   try:
     discard
-#    addCommand("ShowLoot", showLootCommand)
+#    addCommand("ShowGoals", showGoalsCommand)
   except:
     showError(message = "Can't add a Tcl command.")
