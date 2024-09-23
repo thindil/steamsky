fac1a79d30a9a822596ce20be7ec04f38190c5bc
thindil <thindil@laeran.pl>
2024-09-23 03:21:19 +0000

    refactor: use Nim code to set the current goal for a player
    FossilOrigin-Name: b3743de0efcc80dd8d500a8ce4961e6ebd0e16ebf91aa37694234ffd086635f3

--- src/ui/goals-ui.adb
+++ src/ui/goals-ui.adb
@@ -13,18 +13,18 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-with Ada.Strings;
-with Ada.Strings.Fixed;
+-- with Ada.Strings;
+-- with Ada.Strings.Fixed;
 with Interfaces.C;
 with CArgv;
 with Tcl; use Tcl;
-with Tcl.Ada;
-with Tcl.Tk.Ada;
-with Tcl.Tk.Ada.Widgets;
-with Tcl.Tk.Ada.Widgets.TtkButton;
-with Tcl.Tk.Ada.Widgets.TtkTreeView;
-with Tcl.Tklib.Ada.Tooltip;
-with Utils;
+-- with Tcl.Ada;
+-- with Tcl.Tk.Ada;
+-- with Tcl.Tk.Ada.Widgets;
+-- with Tcl.Tk.Ada.Widgets.TtkButton;
+-- with Tcl.Tk.Ada.Widgets.TtkTreeView;
+-- with Tcl.Tklib.Ada.Tooltip;
+-- with Utils;
 with Utils.UI;
 
 package body Goals.UI is
@@ -68,57 +68,59 @@ package body Goals.UI is
    function Set_Goal_Command
      (Client_Data: Integer; Interp: Tcl.Tcl_Interp; Argc: Interfaces.C.int;
       Argv: CArgv.Chars_Ptr_Ptr) return Interfaces.C.int with
-      Convention => C;
+      Convention => C,
+      Import => True,
+      External_Name => "setGoalCommand";
       -- ****
 
-   function Set_Goal_Command
-     (Client_Data: Integer; Interp: Tcl.Tcl_Interp; Argc: Interfaces.C.int;
-      Argv: CArgv.Chars_Ptr_Ptr) return Interfaces.C.int is
-      pragma Unreferenced(Client_Data, Argc);
-      use Ada.Strings.Fixed;
-      use Tcl.Ada;
-      use Tcl.Tklib.Ada.Tooltip;
-      use Tcl.Tk.Ada.Widgets;
-      use Tcl.Tk.Ada.Widgets.TtkButton;
-      use Tcl.Tk.Ada.Widgets.TtkTreeView;
-      use Utils;
-
-      Goals_View: constant Ttk_Tree_View :=
-        Get_Widget(pathName => ".goalsdialog.view", Interp => Interp);
-      Selected_Goal: Natural;
-      Button_Name: constant String := CArgv.Arg(Argv => Argv, N => 1);
-      Goal_Button: constant Ttk_Button :=
-        Get_Widget(pathName => Button_Name, Interp => Interp);
-      Button_Text: Unbounded_String := Null_Unbounded_String;
-   begin
-      Selected_Goal := Natural'Value(Selection(TreeViewWidget => Goals_View));
-      Clear_Current_Goal;
-      if Selected_Goal > 0 then
-         Set_Current_Goal(Index => Selected_Goal);
-      elsif Index(Source => Button_Name, Pattern => "newgamemenu") = 0 then
-         Set_Current_Goal
-           (Index => Get_Random(Min => 1, Max => Get_Goals_Amount));
-      end if;
-      if Selected_Goal > 0 then
-         Button_Text :=
-           To_Unbounded_String(Source => Goal_Text(Index => Selected_Goal));
-         Add
-           (Widget => Goal_Button,
-            Message => To_String(Source => Button_Text));
-         if Length(Source => Button_Text) > 16 then
-            Button_Text :=
-              Unbounded_Slice(Source => Button_Text, Low => 1, High => 17) &
-              "...";
-         end if;
-         configure
-           (Widgt => Goal_Button,
-            options => "-text {" & To_String(Source => Button_Text) & "}");
-      else
-         configure(Widgt => Goal_Button, options => "-text {Random}");
-      end if;
-      Tcl_Eval(interp => Interp, strng => ".goalsdialog.closebutton invoke");
-      return TCL_OK;
-   end Set_Goal_Command;
+--   function Set_Goal_Command
+--     (Client_Data: Integer; Interp: Tcl.Tcl_Interp; Argc: Interfaces.C.int;
+--      Argv: CArgv.Chars_Ptr_Ptr) return Interfaces.C.int is
+--      pragma Unreferenced(Client_Data, Argc);
+--      use Ada.Strings.Fixed;
+--      use Tcl.Ada;
+--      use Tcl.Tklib.Ada.Tooltip;
+--      use Tcl.Tk.Ada.Widgets;
+--      use Tcl.Tk.Ada.Widgets.TtkButton;
+--      use Tcl.Tk.Ada.Widgets.TtkTreeView;
+--      use Utils;
+--
+--      Goals_View: constant Ttk_Tree_View :=
+--        Get_Widget(pathName => ".goalsdialog.view", Interp => Interp);
+--      Selected_Goal: Natural;
+--      Button_Name: constant String := CArgv.Arg(Argv => Argv, N => 1);
+--      Goal_Button: constant Ttk_Button :=
+--        Get_Widget(pathName => Button_Name, Interp => Interp);
+--      Button_Text: Unbounded_String := Null_Unbounded_String;
+--   begin
+--      Selected_Goal := Natural'Value(Selection(TreeViewWidget => Goals_View));
+--      Clear_Current_Goal;
+--      if Selected_Goal > 0 then
+--         Set_Current_Goal(Index => Selected_Goal);
+--      elsif Index(Source => Button_Name, Pattern => "newgamemenu") = 0 then
+--         Set_Current_Goal
+--           (Index => Get_Random(Min => 1, Max => Get_Goals_Amount));
+--      end if;
+--      if Selected_Goal > 0 then
+--         Button_Text :=
+--           To_Unbounded_String(Source => Goal_Text(Index => Selected_Goal));
+--         Add
+--           (Widget => Goal_Button,
+--            Message => To_String(Source => Button_Text));
+--         if Length(Source => Button_Text) > 16 then
+--            Button_Text :=
+--              Unbounded_Slice(Source => Button_Text, Low => 1, High => 17) &
+--              "...";
+--         end if;
+--         configure
+--           (Widgt => Goal_Button,
+--            options => "-text {" & To_String(Source => Button_Text) & "}");
+--      else
+--         configure(Widgt => Goal_Button, options => "-text {Random}");
+--      end if;
+--      Tcl_Eval(interp => Interp, strng => ".goalsdialog.closebutton invoke");
+--      return TCL_OK;
+--   end Set_Goal_Command;
 
    procedure Add_Commands is
       use Utils.UI;
