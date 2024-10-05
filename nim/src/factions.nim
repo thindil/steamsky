1c49dd69a452ce7f5c98fcb162a8aeee857d0788
thindil <thindil@laeran.pl>
2024-10-05 03:13:51 +0000

    refactor: moved some code to separated procedure
    FossilOrigin-Name: 38afb60d8b09440713a582298731b9b0e606efbbb51866ba085de07d59927dc0

--- nim/src/factions.nim
+++ nim/src/factions.nim
@@ -23,6 +23,144 @@ import std/[strutils, tables, xmlparser, xmltree]
 import contracts
 import basestypes, careers, game, items, log, types, utils
 
+proc loadFactionData(factionNode: XmlNode; factionAction: DataAction;
+    factionIndex: string; faction: var FactionData) {.raises: [
+    DataLoadingError], tags: [], contractual.} =
+  ## Load various data from a file for the selected faction
+  ##
+  ## * factionNode   - the XML node with data which will be loaded
+  ## * factionAction - the action to take with the faction, like add, remove,
+  ##                   etc.
+  ## * factionIndex  - the index of the faction
+  ## * faction       - the faction to which the data will be added
+  ##
+  ## Returns the modified parameter faction.
+  body:
+    for childNode in factionNode:
+      if childNode.kind != xnElement:
+        continue
+      case childNode.tag
+      of "relation":
+        let relationIndex: string = childNode.attr(name = "faction")
+        var relation: RelationsData = RelationsData()
+        var attribute: string = childNode.attr(name = "reputation")
+        if attribute.len() > 0:
+          try:
+            relation.reputation = ReputationRanges(min: attribute.parseInt(), max: 0)
+          except ValueError:
+            raise newException(exceptn = DataLoadingError,
+                message = "Can't " & $factionAction & " faction '" &
+                factionIndex & "', invalid value for reputation.")
+        else:
+          try:
+            relation.reputation = ReputationRanges(min: childNode.attr(
+                name = "minreputation").parseInt(), max: childNode.attr(
+                name = "maxreputation").parseInt())
+          except ValueError:
+            raise newException(exceptn = DataLoadingError,
+                message = "Can't " & $factionAction & " faction '" &
+                factionIndex & "', invalid value for reputation.")
+          if relation.reputation.min > relation.reputation.max:
+            raise newException(exceptn = DataLoadingError,
+                message = "Can't " & $factionAction & " faction '" &
+                factionIndex &
+                "', invalid range for faction's reputation with '" &
+                relationIndex & "'.")
+        if childNode.attr(name = "friendly") == "Y":
+          relation.friendly = true
+        else:
+          relation.friendly = false
+        faction.relations[relationIndex] = relation
+      of "description":
+        faction.description = childNode.innerText()
+      of "foodtype":
+        let foodType: string = childNode.attr(name = "name")
+        if childNode.attr(name = "action") == "remove":
+          for index, food in faction.foodTypes:
+            if food == foodType:
+              faction.foodTypes.delete(i = index)
+              break
+        else:
+          if findProtoItem(itemType = foodType) == 0:
+            raise newException(exceptn = DataLoadingError,
+                message = "Can't " & $factionAction & " faction '" &
+                factionIndex &
+                "', no items with type '" & foodType & "'.")
+          faction.foodTypes.add(y = foodType)
+      of "drinktype":
+        let drinkType: string = childNode.attr(name = "name")
+        if childNode.attr(name = "action") == "remove":
+          for index, drink in faction.drinksTypes:
+            if drink == drinkType:
+              faction.drinksTypes.delete(i = index)
+              break
+        else:
+          if findProtoItem(itemType = drinkType) == 0:
+            raise newException(exceptn = DataLoadingError,
+                message = "Can't " & $factionAction & " faction '" &
+                factionIndex &
+                "', no items with type '" & drinkType & "'.")
+          faction.drinksTypes.add(y = drinkType)
+      of "career":
+        let careerIndex: string = childNode.attr(name = "index")
+        if childNode.attr(name = "action") == "remove":
+          {.warning[ProveInit]: off.}
+          {.warning[UnsafeDefault]: off.}
+          faction.careers.del(key = careerIndex)
+          {.warning[UnsafeDefault]: on.}
+          {.warning[ProveInit]: on.}
+        else:
+          var career: CareerData = CareerData(shipIndex: 1)
+          var attribute: string = childNode.attr(name = "shipindex")
+          if attribute.len() > 0:
+            try:
+              career.shipIndex = attribute.parseInt()
+            except ValueError:
+              raise newException(exceptn = DataLoadingError,
+                  message = "Can't " & $factionAction & " faction '" &
+                  factionIndex & "', invalid value for ship index.")
+          attribute = childNode.attr(name = "playerindex")
+          if attribute.len() > 0:
+            career.playerIndex = attribute
+          attribute = childNode.attr(name = "name")
+          if attribute.len() > 0:
+            career.name = attribute
+          else:
+            try:
+              career.name = careersList[careerIndex].name
+            except KeyError:
+              raise newException(exceptn = DataLoadingError,
+                  message = "Can't " & $factionAction & " faction '" &
+                  factionIndex &
+                  "', no career with index '" & careerIndex & "'.")
+          career.description = childNode.innerText
+          faction.careers[careerIndex] = career
+      of "basetype":
+        let baseIndex: string = childNode.attr(name = "index")
+        if childNode.attr(name = "action") == "remove":
+          {.warning[ProveInit]: off.}
+          {.warning[UnsafeDefault]: off.}
+          faction.basesTypes.del(key = baseIndex)
+          {.warning[UnsafeDefault]: on.}
+          {.warning[ProveInit]: on.}
+        elif basesTypesList.hasKey(key = baseIndex):
+          try:
+            faction.basesTypes[baseIndex] = childNode.attr(
+                name = "chance").parseInt()
+          except ValueError:
+            raise newException(exceptn = DataLoadingError,
+                message = "Can't " & $factionAction & " faction '" &
+                factionIndex & "', invalid value for base spawn chance.")
+      of "flag":
+        let factionFlag: string = childNode.attr(name = "name")
+        if childNode.attr(name = "action") == "remove":
+          for index, flag in faction.foodTypes:
+            if flag == factionFlag:
+              faction.flags.delete(i = index)
+              break
+        else:
+          faction.flags.add(y = factionFlag)
+
 proc loadFactions*(fileName: string) {.sideEffect, raises: [DataLoadingError],
     tags: [WriteIOEffect, ReadIOEffect, RootEffect], contractual.} =
   ## Load available factions from the data file
@@ -157,130 +295,8 @@ proc loadFactions*(fileName: string) {.sideEffect, raises: [DataLoadingError],
               factionIndex &
               "', no skill named '" & attribute & "'.")
         faction.weaponSkill = skillIndex
-      for childNode in factionNode:
-        if childNode.kind != xnElement:
-          continue
-        case childNode.tag
-        of "relation":
-          let relationIndex: string = childNode.attr(name = "faction")
-          var relation: RelationsData = RelationsData()
-          attribute = childNode.attr(name = "reputation")
-          if attribute.len() > 0:
-            try:
-              relation.reputation = ReputationRanges(min: attribute.parseInt(), max: 0)
-            except ValueError:
-              raise newException(exceptn = DataLoadingError,
-                  message = "Can't " & $factionAction & " faction '" &
-                  factionIndex & "', invalid value for reputation.")
-          else:
-            try:
-              relation.reputation = ReputationRanges(min: childNode.attr(
-                  name = "minreputation").parseInt(), max: childNode.attr(
-                  name = "maxreputation").parseInt())
-            except ValueError:
-              raise newException(exceptn = DataLoadingError,
-                  message = "Can't " & $factionAction & " faction '" &
-                  factionIndex & "', invalid value for reputation.")
-            if relation.reputation.min > relation.reputation.max:
-              raise newException(exceptn = DataLoadingError,
-                  message = "Can't " & $factionAction & " faction '" &
-                  factionIndex &
-                  "', invalid range for faction's reputation with '" &
-                  relationIndex & "'.")
-          if childNode.attr(name = "friendly") == "Y":
-            relation.friendly = true
-          else:
-            relation.friendly = false
-          faction.relations[relationIndex] = relation
-        of "description":
-          faction.description = childNode.innerText()
-        of "foodtype":
-          let foodType: string = childNode.attr(name = "name")
-          if childNode.attr(name = "action") == "remove":
-            for index, food in faction.foodTypes:
-              if food == foodType:
-                faction.foodTypes.delete(i = index)
-                break
-          else:
-            if findProtoItem(itemType = foodType) == 0:
-              raise newException(exceptn = DataLoadingError,
-                  message = "Can't " & $factionAction & " faction '" &
-                  factionIndex &
-                  "', no items with type '" & foodType & "'.")
-            faction.foodTypes.add(y = foodType)
-        of "drinktype":
-          let drinkType: string = childNode.attr(name = "name")
-          if childNode.attr(name = "action") == "remove":
-            for index, drink in faction.drinksTypes:
-              if drink == drinkType:
-                faction.drinksTypes.delete(i = index)
-                break
-          else:
-            if findProtoItem(itemType = drinkType) == 0:
-              raise newException(exceptn = DataLoadingError,
-                  message = "Can't " & $factionAction & " faction '" &
-                  factionIndex &
-                  "', no items with type '" & drinkType & "'.")
-            faction.drinksTypes.add(y = drinkType)
-        of "career":
-          let careerIndex: string = childNode.attr(name = "index")
-          if childNode.attr(name = "action") == "remove":
-            {.warning[ProveInit]: off.}
-            {.warning[UnsafeDefault]: off.}
-            faction.careers.del(key = careerIndex)
-            {.warning[UnsafeDefault]: on.}
-            {.warning[ProveInit]: on.}
-          else:
-            var career: CareerData = CareerData(shipIndex: 1)
-            attribute = childNode.attr(name = "shipindex")
-            if attribute.len() > 0:
-              try:
-                career.shipIndex = attribute.parseInt()
-              except ValueError:
-                raise newException(exceptn = DataLoadingError,
-                    message = "Can't " & $factionAction & " faction '" &
-                    factionIndex & "', invalid value for ship index.")
-            attribute = childNode.attr(name = "playerindex")
-            if attribute.len() > 0:
-              career.playerIndex = attribute
-            attribute = childNode.attr(name = "name")
-            if attribute.len() > 0:
-              career.name = attribute
-            else:
-              try:
-                career.name = careersList[careerIndex].name
-              except KeyError:
-                raise newException(exceptn = DataLoadingError,
-                    message = "Can't " & $factionAction & " faction '" &
-                    factionIndex &
-                    "', no career with index '" & careerIndex & "'.")
-            career.description = childNode.innerText
-            faction.careers[careerIndex] = career
-        of "basetype":
-          let baseIndex: string = childNode.attr(name = "index")
-          if childNode.attr(name = "action") == "remove":
-            {.warning[ProveInit]: off.}
-            {.warning[UnsafeDefault]: off.}
-            faction.basesTypes.del(key = baseIndex)
-            {.warning[UnsafeDefault]: on.}
-            {.warning[ProveInit]: on.}
-          elif basesTypesList.hasKey(key = baseIndex):
-            try:
-              faction.basesTypes[baseIndex] = childNode.attr(
-                  name = "chance").parseInt()
-            except ValueError:
-              raise newException(exceptn = DataLoadingError,
-                  message = "Can't " & $factionAction & " faction '" &
-                  factionIndex & "', invalid value for base spawn chance.")
-        of "flag":
-          let factionFlag: string = childNode.attr(name = "name")
-          if childNode.attr(name = "action") == "remove":
-            for index, flag in faction.foodTypes:
-              if flag == factionFlag:
-                faction.flags.delete(i = index)
-                break
-          else:
-            faction.flags.add(y = factionFlag)
+      loadFactionData(factionNode = factionNode, factionAction = factionAction,
+          factionIndex = factionIndex, faction = faction)
       if factionAction == DataAction.add:
         if faction.basesTypes.len() == 0:
           for key in basesTypesList.keys:
