b1d82112c53f76ab4a0e63b2c7734fb75870b667
thindil <thindil@laeran.pl>
2024-10-18 03:10:27 +0000

    refactor: moved Tcl code to Nim
    FossilOrigin-Name: 0fcb83efa54e318e77cb453b69f4b4140f69e5a48a7680bd68d5e2e780b9fe86

--- src/ui/goalsui.nim
+++ src/ui/goalsui.nim
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
-import std/[os, tables, strutils]
+import std/[tables, strutils]
 import ../[config, game, goals, tk, utils]
 import errordialog
 
@@ -34,7 +34,61 @@ proc showGoalsCommand(clientData: cint; interp: PInterp; argc: cint;
   ## Tcl:
   ## ShowGoals buttonpath
   ## Buttonpath is path to the button which is used to set the goal
-  tclEvalFile(dataDirectory & "ui" & DirSep & "goals.tcl")
+  tclEval(script = """
+      ttk::frame .goalsdialog -style Dialog.TFrame
+      set view [ttk::treeview .goalsdialog.view -show tree \
+         -yscrollcommand [list .goalsdialog.yscroll set] -height 9 -cursor hand1]
+      set selectbutton [ttk::button .goalsdialog.selectbutton -text {Select goal}]
+      grid [ttk::label .goalsdialog.header -text {Select a new goal} -wraplength 275 \
+         -style Header.TLabel -cursor hand1] -sticky we -columnspan 2
+      grid $view -padx 2 -pady {2 0}
+      $view column #0 -width 450 -stretch 1
+      $view insert {} end -id 0 -text Random
+      $view insert {} end -id REPUTATION -text {Gain max reputation in bases}
+      $view insert {} end -id DESTROY -text {Destroy enemy ships}
+      $view insert {} end -id DISCOVER -text {Discover map}
+      $view insert {} end -id VISIT -text {Visit bases}
+      $view insert {} end -id CRAFT -text {Craft items}
+      $view insert {} end -id MISSION -text {Finish missions}
+      $view insert {} end -id KILL -text {Kill enemies in melee combat}
+      $view selection set 0
+      bind $view <<TreeviewSelect>> {
+         set selected [lindex [$view selection] 0]
+         if {[$view parent $selected] == {} && \
+            [$view item $selected -text] != {Random}} {
+            $selectbutton state disabled
+         } else {
+            $selectbutton state !disabled
+         }
+      }
+      bind $view <Double-1> {$selectbutton invoke}
+      bind $view <Return> {$selectbutton invoke}
+      grid [ttk::scrollbar .goalsdialog.yscroll -orient vertical \
+         -command [list $view yview]] -column 1 -row 1 -sticky ns -padx {0 3} \
+         -pady {2 0}
+      grid $selectbutton -row 3 -columnspan 2 -sticky we -padx 5 -pady {2 0}
+      tooltip::tooltip $selectbutton \
+         "Select the goal for your character from the list.\nIf you choose Random option, a random goal will\nbe assigned. You can always change it later during\nthe game, but you will lose all progress then."
+      if {[winfo exists .gameframe] && [winfo ismapped .gameframe]} {
+         set parent .gameframe
+      } else {
+         set parent .
+      }
+      grid [ttk::button .goalsdialog.closebutton -text {Close (Escape)} \
+         -command {CloseDialog .goalsdialog $parent}] -row 4 -columnspan 2 \
+         -sticky we -padx 5 -pady 2
+      tooltip::tooltip .goalsdialog.closebutton \
+         {Close the goals list without any changes}
+      bind .goalsdialog.closebutton <Escape> {.goalsdialog.closebutton invoke;break}
+      bind .goalsdialog.closebutton <Tab> {focus $view;break}
+      bind $selectbutton <Escape> {.goalsdialog.closebutton invoke;break}
+      bind $view <Escape> {.goalsdialog.closebutton invoke;break}
+      ::autoscroll::autoscroll .goalsdialog.yscroll
+      place .goalsdialog -in $parent -relx 0.1 -rely 0.075
+      focus .goalsdialog.closebutton
+      tk busy $parent
+      raise .goalsdialog
+  """)
   let
     goalsDialog = ".goalsdialog"
     goalsView = goalsDialog & ".view"
