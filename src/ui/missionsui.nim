b773a6cf039e1242f478f926362d2214653c3292
thindil <thindil@laeran.pl>
2024-10-19 03:08:55 +0000

    refactor: moved Tcl code to Nim
    FossilOrigin-Name: a98da5419df1caef9544648d48264fd6d20e0e1dc651099a4949f677d965bba6

--- src/ui/missionsui.nim
+++ src/ui/missionsui.nim
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
-import std/[algorithm, os, strutils, tables]
+import std/[algorithm, strutils, tables]
 import ../[config, events, game, items, maps, missions, missions2, ships, tk,
     types, utils]
 import coreui, dialogs, errordialog, mapsui, table, utilsui2
@@ -251,7 +251,39 @@ proc showBaseMissionsCommand(clientData: cint; interp: PInterp; argc: cint;
     missionsCanvas = missionsFrame & ".canvas"
     label = missionsCanvas & ".missions.missionslabel"
   if tclEval2(script = "winfo exists " & label) == "0":
-    tclEvalFile(fileName = dataDirectory & "ui" & DirSep & "missions.tcl")
+    tclEval(script = """
+      ttk::frame .gameframe.paned.missionsframe
+      set missionscanvas [canvas .gameframe.paned.missionsframe.canvas \
+         -yscrollcommand [list .gameframe.paned.missionsframe.scrolly set] \
+         -xscrollcommand [list .gameframe.paned.missionsframe.scrollx set]]
+      pack [ttk::scrollbar .gameframe.paned.missionsframe.scrolly -orient vertical \
+         -command [list $missionscanvas yview]] -side right -fill y
+      pack $missionscanvas -side top -fill both
+      pack [ttk::scrollbar .gameframe.paned.missionsframe.scrollx -orient horizontal \
+         -command [list $missionscanvas xview]] -fill x
+      SetScrollbarBindings $missionscanvas .gameframe.paned.missionsframe.scrolly
+      ::autoscroll::autoscroll .gameframe.paned.missionsframe.scrolly
+      ::autoscroll::autoscroll .gameframe.paned.missionsframe.scrollx
+      set missionsframe [ttk::frame $missionscanvas.missions]
+      grid [ttk::frame $missionsframe.missions] -sticky n -padx 5 -pady 5
+      SetScrollbarBindings $missionsframe .gameframe.paned.missionsframe.scrolly
+      # Label with information how many missions are available in the base
+      grid [ttk::frame $missionsframe.missionslabel] -sticky w -padx 5
+      SetScrollbarBindings $missionsframe.missionslabel \
+         .gameframe.paned.missionsframe.scrolly
+      grid [ttk::label $missionsframe.missionslabel.missionslbl1 -text {You can take }] \
+         -sticky w
+      SetScrollbarBindings $missionsframe.missionslabel.missionslbl1 \
+         .gameframe.paned.missionsframe.scrolly
+      grid [ttk::label $missionsframe.missionslabel.missionslbl2 -style Golden.TLabel] \
+         -sticky w -column 1 -row 0
+      SetScrollbarBindings $missionsframe.missionslabel.missionslbl2 \
+         .gameframe.paned.missionsframe.scrolly
+      grid [ttk::label $missionsframe.missionslabel.missionslbl3 -text { more missions in from base.}] \
+         -sticky w -column 2 -row 0
+      SetScrollbarBindings $missionsframe.missionslabel.missionslbl3 \
+         .gameframe.paned.missionsframe.scrolly
+    """)
     tclEval(script = "bind " & missionsFrame & " <Configure> {ResizeCanvas %W.canvas %w %h}")
     try:
       addCommand("ShowMission", showMissionCommand)
