a7a529205a8c513b3d5ef150465f179edf32ff03
thindil <thindil@laeran.pl>
2024-10-19 03:07:22 +0000

    refactor: moved Tcl code to Nim
    FossilOrigin-Name: d1f914985d8f79fb6c0bea872fef478b350a6ff8018ebeac126ccc40257d64ef

--- src/ui/messagesui.nim
+++ src/ui/messagesui.nim
@@ -15,8 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
-import std/[os, strutils]
-import ../[config, game, messages, tk, types]
+import std/strutils
+import ../[config, messages, tk, types]
 import coreui, dialogs, errordialog, utilsui2
 
 proc showMessage(message: MessageData; messageView: string;
@@ -49,7 +49,58 @@ proc showLastMessagesCommand(clientData: cint; interp: PInterp; argc: cint;
   var messagesFrame = mainPaned & ".messagesframe"
   let messagesCanvas = messagesFrame & ".canvas"
   if tclEval2(script = "winfo exists " & messagesCanvas) == "0":
-    tclEvalFile(fileName = dataDirectory & "ui" & DirSep & "messages.tcl")
+    tclEval(script = """
+      ttk::frame .gameframe.paned.messagesframe
+      set messagescanvas [canvas .gameframe.paned.messagesframe.canvas \
+         -xscrollcommand [list .gameframe.paned.messagesframe.scrollx set]]
+      pack $messagescanvas -side top -fill both
+      pack [ttk::scrollbar .gameframe.paned.messagesframe.scrollx \
+         -orient horizontal -command [list $messagescanvas xview]] -fill x
+      ::autoscroll::autoscroll .gameframe.paned.messagesframe.scrollx
+      set messagesframe [ttk::frame $messagescanvas.messages]
+      # Messages options
+      grid [ttk::frame $messagesframe.options] -sticky w
+      grid [ttk::combobox $messagesframe.options.types \
+         -values [list All Combat Trade Orders Craft Others Missions] \
+         -state readonly -width 10]
+      tooltip::tooltip $messagesframe.options.types \
+         {Select the type of messages to show}
+      bind $messagesframe.options.types <<ComboboxSelected>> SelectMessages
+      $messagesframe.options.types current 0
+      grid [ttk::entry $messagesframe.options.search -validate key \
+         -validatecommand {SearchMessages %P} -width 30] -row 0 -column 1
+      tooltip::tooltip $messagesframe.options.search \
+         {Search for the selected text in the messages}
+      grid [ttk::button $messagesframe.options.delete -text {Delete all messages} \
+         -command DeleteMessages] -row 0 -column 2
+      tooltip::tooltip $messagesframe.options.delete {Clear all messages}
+      # Messages list
+      grid [ttk::frame $messagesframe.list] -sticky nwes
+      set messagesview2 [text $messagesframe.list.view -width 10 -height 10 \
+         -yscrollcommand [list $messagesframe.list.scrolly set]]
+      $messagesview2 tag configure yellow -foreground \
+         [ttk::style lookup Messages -yellow]
+      $messagesview2 tag configure green -foreground \
+         [ttk::style lookup Messages -green]
+      $messagesview2 tag configure red -foreground \
+         [ttk::style lookup Messages -red]
+      $messagesview2 tag configure cyan -foreground \
+         [ttk::style lookup Messages -cyan]
+      $messagesview2 tag configure blue -foreground \
+         [ttk::style lookup Messages -blue]
+      $messagesview2 tag configure gray -foreground \
+         [ttk::style lookup Messages -gray]
+      pack [ttk::scrollbar $messagesframe.list.scrolly -orient vertical \
+         -command [list $messagesview2 yview]] -side right -fill y
+      pack $messagesview2 -side top -fill both -expand true
+      ::autoscroll::autoscroll $messagesframe.list.scrolly
+      SetScrollbarBindings $messagescanvas $messagesframe.list.scrolly
+      bind $messagescanvas <Configure> {
+         $messagesview2 configure -height [expr [winfo height $messagescanvas] \
+            / [font metrics InterfaceFont -linespace] - 1] -width [expr %w \
+            / [font measure InterfaceFont {  }] + 4]
+      }
+    """)
     tclEval(script = "bind " & messagesFrame & " <Configure> {ResizeCanvas %W.canvas %w %h}")
   elif tclEval2(script = "winfo ismapped " & messagesCanvas) == "1" and argc == 1:
     tclEval(script = "InvokeButton " & closeButton)
