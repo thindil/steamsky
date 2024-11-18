c40f144c3c176573cf5af37592b7709df68e62c5
thindil <thindil@laeran.pl>
2024-11-18 04:02:39 +0000

    feat: continue work on the question dialog
    FossilOrigin-Name: d93695488552a6e8307a72f97e7d42bd2dded8ad5df8ef2883cb64083e711119

--- src/newui/dialogs.nim
+++ src/newui/dialogs.nim
@@ -17,22 +17,51 @@
 
 ## Provides code related to the game's dialogs, like showing questions, etc.
 
-import contracts
-import coreui
+import std/math
+import contracts, nuklear/nuklear_sdl_renderer
+import coreui, errordialog
 
 type
   QuestionData = object
     question, data: string
+    lines: float
 
 var questionData: QuestionData = QuestionData(question: "", data: "")
 
-proc setQuestion*(question: string; data: string = "") {.raises: [], tags: [], contractual.} =
+proc setQuestion*(question: string; data: string = "", dialog: var GameDialog) {.raises: [], tags: [RootEffect],
+    contractual.} =
   ## Set the data related to the current in-game question
   ##
   ## * question - the question which will be asked to the player
   ## * data     - an additional data for the question, like saved game path,
   ##              optional.
-  questionData = QuestionData(question: question, data: data)
+  ## * dialog   - the current in-game dialog displayed on the screen
+  ##
+  ## Returns the parameter dialog. It is modified only when an error occurs.
+  try:
+    var needLines: float = ceil(x = getTextWidth(text = question) / 250)
+    if needLines < 1.0:
+      needLines = 1.0
+    questionData = QuestionData(question: question, data: data, lines: needLines)
+  except:
+    dialog = setError(message = "Can't set the question.")
 
-proc showQuestion*(dialog: var GameDialog) {.raises: [], tags: [], contractual.} =
-  dialog = questionDialog
+proc showQuestion*(dialog: var GameDialog) {.raises: [], tags: [],
+    contractual.} =
+  ## Show the current question to the player
+  ##
+  ## * dialog - the current in-game dialog displayed on the screen
+  ##
+  ## Returns the parameter dialog. It is modified only when the player
+  ## closed the dialog
+  window(name = "Question", x = 150, y = 75, w = 250, h = 150, flags = {
+      windowBorder, windowMoveable, windowTitle, windowNoScrollbar}):
+    setLayoutRowDynamic(height = 30 * questionData.lines, cols = 1)
+    wrapLabel(str = questionData.question)
+    setLayoutRowDynamic(height = 30, cols = 2)
+    labelButton(title = "Yes"):
+      dialog = none
+      questionData = QuestionData(question: "", data: "")
+    labelButton(title = "No"):
+      dialog = none
+      questionData = QuestionData(question: "", data: "")
