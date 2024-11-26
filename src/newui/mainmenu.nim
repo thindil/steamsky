201f3955e0c183837a3be1f436e6a9432c82ccb9
thindil <thindil@laeran.pl>
2024-11-26 04:01:11 +0000

    feat: continue work on new game setting in the new UI
    FossilOrigin-Name: 456ef9e955b77d1f6db8a80ee09dcce81bb947ae22fea7b0663fefaf4fe9af85

--- src/newui/mainmenu.nim
+++ src/newui/mainmenu.nim
@@ -531,17 +531,17 @@ proc loadGame*(state: var GameState; dialog: var GameDialog) {.raises: [],
   dialog = none
 
 const playerTooltips: array[6, string] = [
-  "General player character settings. Select field which you want to set to see more information about.",
     "Enter character name.", "Enter ship name.",
     "Select your faction from a list. Factions have the biggest impact on game.\nThey determine the amount of bases and some playing styles.\nMore information about each faction can be found after selecting it.\nYou can't change this later.",
-    "Select your career from a list. Careers have some impact on gameplay\n(each have bonuses to gaining experience in some fields plus\nthey determine your starting ship and crew). More info about each\ncareer can be found after selecting it. You can't change career later.", "Select type of base in which you will start the game.\nThis may have some impact on game difficulty."]
+    "Select your career from a list. Careers have some impact on gameplay\n(each have bonuses to gaining experience in some fields plus\nthey determine your starting ship and crew). More info about each\ncareer can be found after selecting it. You can't change career later.", "Select type of base in which you will start the game.\nThis may have some impact on game difficulty.",
+  "General player character settings. Select field which you want to set to see more information about."]
 
 var
   playerName: string = newGameSettings.playerName
   shipName: string = newGameSettings.shipName
   currentTab: cint = 0
   playerGender: cint = 2
-  infoText: string = playerTooltips[0]
+  infoText: string = playerTooltips[5]
   currentFaction, newFaction, currentCareer, currentBase: Natural = 0
 
 proc newGame*(state: var GameState; dialog: var GameDialog) {.raises: [],
@@ -595,9 +595,9 @@ proc newGame*(state: var GameState; dialog: var GameDialog) {.raises: [],
           label(str = "Character name:")
           if gameSettings.showTooltips:
             addTooltip(bounds = getWidgetBounds(),
-                text = playerTooltips[1])
+                text = playerTooltips[0])
           if isMouseClicked(btn = left):
-            infoText = playerTooltips[1]
+            infoText = playerTooltips[0]
           editString(text = playerName, maxLen = 64)
           if gameSettings.showTooltips:
             addTooltip(bounds = getWidgetBounds(),
@@ -631,9 +631,9 @@ proc newGame*(state: var GameState; dialog: var GameDialog) {.raises: [],
           label(str = "Ship name:")
           if gameSettings.showTooltips:
             addTooltip(bounds = getWidgetBounds(),
-                text = playerTooltips[2])
+                text = playerTooltips[1])
           if isMouseClicked(btn = left):
-            infoText = playerTooltips[2]
+            infoText = playerTooltips[1]
           editString(text = shipName, maxLen = 64)
           if gameSettings.showTooltips:
             addTooltip(bounds = getWidgetBounds(),
@@ -656,12 +656,12 @@ proc newGame*(state: var GameState; dialog: var GameDialog) {.raises: [],
           label(str = "Character faction:")
           if gameSettings.showTooltips:
             addTooltip(bounds = getWidgetBounds(),
-                text = playerTooltips[3])
+                text = playerTooltips[2])
           if isMouseClicked(btn = left):
             var i: Natural = 0
             for faction in factionsList.values:
               if i == newFaction:
-                infoText = playerTooltips[3] & "\n" & faction.description
+                infoText = playerTooltips[2] & "\n" & faction.description
                 break
               i.inc
           newFaction = comboList(items = playerFactions,
@@ -670,18 +670,18 @@ proc newGame*(state: var GameState; dialog: var GameDialog) {.raises: [],
           label(str = "Character career:")
           if gameSettings.showTooltips:
             addTooltip(bounds = getWidgetBounds(),
-                text = playerTooltips[4])
+                text = playerTooltips[3])
           if isMouseClicked(btn = left):
-            infoText = playerTooltips[4]
+            infoText = playerTooltips[3]
           currentCareer = comboList(items = playerCareers,
               selected = currentCareer, itemHeight = 25, x = 200, y = 150)
           # Starting base
           label(str = "Starting base type:")
           if gameSettings.showTooltips:
             addTooltip(bounds = getWidgetBounds(),
-                text = playerTooltips[5])
+                text = playerTooltips[4])
           if isMouseClicked(btn = left):
-            infoText = playerTooltips[5]
+            infoText = playerTooltips[4]
           currentBase = comboList(items = playerBases, selected = currentBase,
               itemHeight = 25, x = 200, y = 90)
     row(x = (menuWidth.float * 0.65), y = 0, w = (menuWidth.float * 0.35), h = (
