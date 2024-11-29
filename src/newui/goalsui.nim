8df04564d77d4040517456816cae6ddee94ea2a1
thindil <thindil@laeran.pl>
2024-11-29 04:25:13 +0000

    feat: continue work on setting in-game goals in the new UI
    FossilOrigin-Name: 699cc4fbc26fd71865d0023c844213e88ae51d3ad350713b681fae1fb602741c

--- src/newui/goalsui.nim
+++ src/newui/goalsui.nim
@@ -18,7 +18,9 @@
 ## Provides code related to the selecting the player's goal dialog, like
 ## showing the dialog, and selecting it.
 
+import std/tables
 import contracts, nuklear/nuklear_sdl_renderer
+import ../[config, game]
 import coreui
 
 var selectedGoal*: string = "Random"
@@ -32,11 +34,19 @@ proc showGoals*(dialog: var GameDialog) {.raises: [], tags: [], contractual.} =
   window(name = "Select a new goal", x = 40, y = 20, w = 540, h = 360, flags = {
       windowBorder, windowMoveable, windowTitle, windowMinimizable,
       windowNoScrollbar}):
-    setLayoutRowDynamic(height = 240, cols = 1)
+    setLayoutRowDynamic(height = 230, cols = 1)
     group(title = "GoalsGroup", flags = {windowNoFlags}):
       setLayoutRowDynamic(height = 25, cols = 1)
+      for goal in goalsList.values:
+        discard
     setLayoutRowDynamic(height = 35, cols = 1)
+    if gameSettings.showTooltips:
+      addTooltip(bounds = getWidgetBounds(),
+          text = "Select the goal for your character from the list. If you choose Random option, a random goal will be assigned. You can always change it later during the game, but you will lose all progress then.")
     labelButton(title = "Select goal"):
       dialog = none
+    if gameSettings.showTooltips:
+      addTooltip(bounds = getWidgetBounds(),
+          text = "Close the goals list without any changes")
     labelButton(title = "Close"):
       dialog = none
