7bf1435c9cd6c097aafae8a04596bf8f3b2eda46
thindil <thindil@laeran.pl>
2024-12-18 04:39:04 +0000

    feat: continue work on better tooltips style in nuklear
    FossilOrigin-Name: 2a4ae2057ac1dec48412945e6def6bb197ef5f41bdd558225db1b8fb7798bbe5

--- src/newui/nuklear/nuklear.h
+++ src/newui/nuklear/nuklear.h
@@ -20127,22 +20127,19 @@ nk_panel_end(struct nk_context *ctx)
         if (layout->at_y < (layout->bounds.y + layout->bounds.h))
             layout->bounds.h = layout->at_y - layout->bounds.y;
 
-        struct nk_color color = style->window.background;
-        printf("here");
-
         /* fill top empty space */
         empty_space.x = window->bounds.x;
         empty_space.y = layout->bounds.y;
         empty_space.h = panel_padding.y;
         empty_space.w = window->bounds.w;
-        nk_fill_rect(out, empty_space, 0, color);
+        nk_fill_rect(out, empty_space, 0, style->window.background);
 
         /* fill left empty space */
         empty_space.x = window->bounds.x;
         empty_space.y = layout->bounds.y;
         empty_space.w = panel_padding.x + layout->border;
         empty_space.h = layout->bounds.h;
-        nk_fill_rect(out, empty_space, 0, color);
+        nk_fill_rect(out, empty_space, 0, style->window.background);
 
         /* fill right empty space */
         empty_space.x = layout->bounds.x + layout->bounds.w;
@@ -20151,7 +20148,7 @@ nk_panel_end(struct nk_context *ctx)
         empty_space.h = layout->bounds.h;
         if (*layout->offset_y == 0 && !(layout->flags & NK_WINDOW_NO_SCROLLBAR))
             empty_space.w += scrollbar_size.x;
-        nk_fill_rect(out, empty_space, 0, color);
+        nk_fill_rect(out, empty_space, 0, style->window.background);
 
         /* fill bottom empty space */
         if (layout->footer_height > 0) {
@@ -20159,7 +20156,7 @@ nk_panel_end(struct nk_context *ctx)
             empty_space.y = layout->bounds.y + layout->bounds.h;
             empty_space.w = window->bounds.w;
             empty_space.h = layout->footer_height;
-            nk_fill_rect(out, empty_space, 0, color);
+            nk_fill_rect(out, empty_space, 0, style->window.background);
         }
     }
 
