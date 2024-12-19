997d143bd133dc8e59e174e837b8845c70dc5067
thindil <thindil@laeran.pl>
2024-12-19 04:11:26 +0000

    feat: continue work on themes support in the new UI
    FossilOrigin-Name: 37238fd4779eff7b8f97f53a59e429220e2977ba40f79d9948069a17e45536ed

--- src/newui/themes.nim
+++ src/newui/themes.nim
@@ -28,7 +28,7 @@ type
     name: string
     fileName: string
     icons*: array[8, string]
-    colors*: array[20, Color]
+    colors*: array[21, Color]
 
 let
   defaultThemeIconPath: string = dataDirectory & "ui" & DirSep & "images" &
@@ -49,7 +49,8 @@ let
       name = "#120d0d"), parseColor(name = "#ffdf00"), parseColor(
       name = "#372412"), parseColor(name = "#ffdf00"), parseColor(
       name = "#ffdf00"), parseColor(name = "#7f8c8d"), parseColor(
-      name = "#000000"), parseColor(name = "#006400")])
+      name = "#000000"), parseColor(name = "#006400"), parseColor(
+      name = "#458588")])
 
 var themesList*: Table[string, ThemeData] = initTable[string, ThemeData]() ## The list of all available themes
 
@@ -61,12 +62,13 @@ proc loadThemes*() {.raises: [], tags: [WriteIOEffect, TimeEffect, RootEffect,
   try:
     const iconsNames: array[8, string] = ["LogoImage", "RandomIcon", "MaleIcon",
         "FemaleIcon", "MenuIcon", "FuelIcon", "NofuelIcon", "LowfuelIcon"]
-    const colorsNames: array[20, string] = ["BackgroundColor",
+    const colorsNames: array[21, string] = ["BackgroundColor",
         "ForegroundColor", "GreenColor", "BorderColor", "ButtonColor",
         "ButtonHoverColor", "EditColor", "EditCursorColor", "ButtonActiveColor",
         "HeaderColor", "ComboColor", "PropertyColor", "ScrollbarColor",
         "ButtonTextColor", "ScrollbarCursorColor", "EditTextColor",
-        "ComboTextColor", "TooltipBorderColor", "TooltipColor", "GroupBorderColor"]
+        "ComboTextColor", "TooltipBorderColor", "TooltipColor",
+        "GroupBorderColor", "HeaderTextColor"]
     for themeDir in walkDirs(pattern = themesDirectory):
       for configName in walkPattern(pattern = themeDir & DirSep & "*.cfg"):
         var configFile: FileStream = newFileStream(filename = configName, mode = fmRead)
@@ -159,6 +161,7 @@ proc loadThemes*() {.raises: [], tags: [WriteIOEffect, TimeEffect, RootEffect,
   setColor(colorName = tooltipBorderColor, index = 17)
   setColor(colorName = tooltipColor, index = 18)
   setColor(colorName = groupBorderColor, index = 19)
+  setColor(colorName = headerTextColor, index = 20)
   table[toggleColor] = NimColor(r: 50, g: 58, b: 61, a: 255)
   table[toggleHoverColor] = NimColor(r: 45, g: 53, b: 56, a: 255)
   table[toggleCursorColor] = NimColor(r: 48, g: 83, b: 111, a: 255)
