ae432d7764ed81a83abb173a39ac30c0a5bd56e7
thindil <thindil@laeran.pl>
2025-01-01 04:04:19 +0000

    refactor: updated date in the legal info
    FossilOrigin-Name: 6b762405f320dab28b03503eae62c2e5175763527adb935d66ccf62a9cc44694

--- src/ui/mainmenu.nim
+++ src/ui/mainmenu.nim
@@ -1,4 +1,4 @@
-# Copyright 2024 Bartek thindil Jasicki
+# Copyright 2024-2025 Bartek thindil Jasicki
 #
 # This file is part of Steam Sky.
 #
@@ -128,7 +128,7 @@ proc createMainMenu*() {.raises: [], tags: [ReadDirEffect,
          -command {OpenLink https://thindil.itch.io/steam-sky}] -row 1 -columnspan 3
       tooltip::tooltip .aboutmenu.website \
          {Visit the game website: https://thindil.itch.io/steam-sky}
-      grid [ttk::button .aboutmenu.mail -text {(c)2016-2024 Bartek thindil Jasicki} \
+      grid [ttk::button .aboutmenu.mail -text {(c)2016-2025 Bartek thindil Jasicki} \
          -style Link.Toolbutton -command {OpenLink mailto:thindil@laeran.pl.eu.org}] \
          -row 2 -columnspan 3
       tooltip::tooltip .aboutmenu.mail {Send a mail to the game creator}
