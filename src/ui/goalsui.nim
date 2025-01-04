5247577e645ae6b04aa9f18fb24d8cb630381fbd
thindil <thindil@laeran.pl>
2025-01-04 04:10:42 +0000

    docs: added code documentation to module
    FossilOrigin-Name: d3bdaf226eee30d4c8e28aed6911a6a13ca5d5d1ae06636e459c4c7c443d02b7

--- src/ui/goalsui.nim
+++ src/ui/goalsui.nim
@@ -15,6 +15,9 @@
 # You should have received a copy of the GNU General Public License
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
+## Provides code related to the list of available goals, like showing them and
+## setting them.
+
 import std/[tables, strutils]
 import contracts, nimalyzer
 import ../[config, game, goals, tk, utils]
