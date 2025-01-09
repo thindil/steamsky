f041e4fc63336243e37c717456a6ba22b48d57f7
thindil <thindil@laeran.pl>
2025-01-09 03:58:26 +0000

    refactor: removed unused code
    FossilOrigin-Name: 7dddb86165e30ce3fb2ff7cadea75264ec4ad2dc506308555dc9ec05d9cba0cc

--- src/newui/nuklear/nuklear.nim
+++ src/newui/nuklear/nuklear.nim
@@ -1900,17 +1900,6 @@ template group*(title: string; flags: set[WindowFlags]; content: untyped) =
 # -----
 # Input
 # -----
-proc nkInBox(px, py, x, y, w, h: float): bool {.raises: [], tags: [], contractual.} =
-  ## Check if the selected coordinates are in the selected box
-  ##
-  ## * px - the X coordinate to check
-  ## * py - the Y coordinate to check
-  ## * x  - the starting X value
-  ## * y  - the starting Y value
-  ## * w  - the width of the box
-  ## * h  - the height of the box
-  return px in x..x+w and py in y..y+h
-
 proc isMouseHovering*(rect: NimRect): bool {.raises: [], tags: [],
     contractual.} =
   ## Check if mouse is hovering over the selected rectangle
