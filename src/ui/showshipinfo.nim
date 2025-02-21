91ebb0eac166b8caa775dd92161ee5e29c049abe
thindil <thindil@laeran.pl>
2025-02-21 03:59:28 +0000

    docs: added code documentation to the module
    FossilOrigin-Name: 332843d0108009d8e9476b0c050448bceb7f854fa7df4f9d9b2436f601e8b19b

--- src/ui/showshipinfo.nim
+++ src/ui/showshipinfo.nim
@@ -15,6 +15,10 @@
 # You should have received a copy of the GNU General Public License
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
+## Provides code related to the player's ship's info screen, like show the
+## general information about the ship. Mmoved to separated module to
+## avoid circular dependencies.
+
 import std/tables
 import contracts, nimalyzer
 import ../[config, game, maps, ships, tk, types]
