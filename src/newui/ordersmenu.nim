e7aa7415f2c0e0e409987e4cc540d04c89c83244
thindil <thindil@laeran.pl>
2025-02-23 04:09:29 +0000

    feat: continue work on orders menu in the new UI
    FossilOrigin-Name: 00e8714f95926476eaa3e68cdb7004ce979c857fba1bcce551ffd6742107b997

--- src/newui/ordersmenu.nim
+++ src/newui/ordersmenu.nim
@@ -19,16 +19,134 @@
 
 import std/[tables, strutils]
 import contracts, nuklear/nuklear_sdl_renderer
-import ../[game, maps, shipscrew, stories, types]
+import ../[basestypes, game, maps, shipscrew, stories, types, utils]
 import coreui, errordialog
 
 proc showDockedCommands(baseIndex: ExtendedBasesRange;
-    haveTrader: bool) {.raises: [], tags: [], contractual.} =
+    haveTrader: bool; dialog: var GameDialog) {.raises: [], tags: [RootEffect],
+        contractual.} =
   ## Show the available orders when the player's ship is docked to a base
   ##
   ## * baseIndex  - the index of the base to which the player's ship is docked
   ## * haveTrader - if true, someone in the crew is assigned to trader position
-  discard
+  ## * dialog - the current in-game dialog displayed on the screen
+  ##
+  ## Returns the modified parameters dialog if error happened.
+  labelButton(title = "Undock"):
+    discard
+  if skyBases[baseIndex].population > 0:
+    labelButton(title = "Escape"):
+      discard
+    if haveTrader and skyBases[baseIndex].population > 0:
+      labelButton(title = "Trade"):
+        discard
+      labelButton(title = "School"):
+        discard
+      if skyBases[baseIndex].recruits.len > 0:
+        labelButton(title = "Recruit"):
+          discard
+    if daysDifference(dateToCompare = skyBases[baseIndex].askedForEvents) > 6:
+      labelButton(title = "Ask for events"):
+        discard
+    if not skyBases[baseIndex].askedForBases:
+      labelButton(title = "Ask for bases"):
+        discard
+    try:
+      if "temple" in basesTypesList[skyBases[baseIndex].baseType].flags:
+        labelButton(title = "Pray"):
+          discard
+    except:
+      dialog = setError(message = "Can't check if base has temple flag.")
+      return
+#    for member in playerShip.crew:
+#      if member.health < 100:
+#        addButton(name = ".heal", label = "Heal wounded",
+#            command = "ShowBaseUI heal", shortcut = "w", underline = 5)
+#        break
+#    for module in playerShip.modules:
+#      if module.durability < module.maxDurability:
+#        addButton(name = ".repair", label = "Repair ship",
+#            command = "ShowBaseUI repair", shortcut = "p", underline = 2)
+#        break
+#    try:
+#      if "shipyard" in basesTypesList[skyBases[baseIndex].baseType].flags:
+#        addButton(name = ".shipyard", label = "Shipyard",
+#            command = "ShowShipyard", shortcut = "i", underline = 2)
+#    except:
+#      showError(message = "Can't check if the base has shipyard flag.")
+#      return true
+#    for index, recipe in recipesList:
+#      try:
+#        if index notin knownRecipes and index in basesTypesList[skyBases[
+#            baseIndex].baseType].recipes and recipe.reputation <= skyBases[
+#            baseIndex].reputation.level:
+#          addButton(name = ".recipes", label = "Buy recipes",
+#              command = "ShowBaseUI recipes", shortcut = "y", underline = 2)
+#          break
+#      except:
+#        showError(message = "Can't check if base has recipes for sale.")
+#        return true
+#    if skyBases[baseIndex].missions.len > 0:
+#      var missionsLimit: int = case skyBases[baseIndex].reputation.level
+#        of 0..25:
+#          1
+#        of 26..50:
+#          3
+#        of 51..75:
+#          5
+#        of 76..100:
+#          10
+#        else:
+#          0
+#      for mission in acceptedMissions:
+#        if (mission.finished and mission.startBase == baseIndex) or (
+#            mission.targetX == playerShip.skyX and mission.targetY ==
+#            playerShip.skyY):
+#          case mission.mType
+#          of deliver:
+#            try:
+#              addButton(name = ".mission", label = "Complete delivery of " &
+#                  itemsList[mission.itemIndex].name,
+#                  command = "CompleteMission", shortcut = "c", underline = 0, row = 0)
+#            except:
+#              showError(message = "Can't add mission button.")
+#              return true
+#          of destroy:
+#            if mission.finished:
+#              try:
+#                addButton(name = ".mission", label = "Complete destroy " &
+#                    protoShipsList[mission.shipIndex].name,
+#                    command = "CompleteMission", shortcut = "c",
+#                        underline = 0, row = 0)
+#              except:
+#                showError(message = "Can't add mission button.")
+#                return true
+#          of patrol:
+#            if mission.finished:
+#              addButton(name = ".mission",
+#                  label = "Complete Patrol area mission",
+#                  command = "CompleteMission", shortcut = "c", underline = 0, row = 0)
+#          of explore:
+#            if mission.finished:
+#              addButton(name = ".mission",
+#                  label = "Complete Explore area mission",
+#                  command = "CompleteMission", shortcut = "c", underline = 0, row = 0)
+#          of passenger:
+#            if mission.finished:
+#              addButton(name = ".mission",
+#                  label = "Complete Transport passenger mission",
+#                  command = "CompleteMission", shortcut = "c", underline = 0, row = 0)
+#        if mission.startBase == baseIndex:
+#          missionsLimit.dec
+#      if missionsLimit > 0:
+#        addButton(name = ".missions", label = "Missions",
+#            command = "ShowBaseMissions", shortcut = "m", underline = 0)
+#    if playerShip.homeBase != baseIndex:
+#      addButton(name = ".home", label = "Set as home", command = "SetAsHome",
+#          shortcut = "h", underline = 7)
+#  if skyBases[baseIndex].population == 0:
+#    addButton(name = ".loot", label = "Loot", command = "ShowLoot",
+#        shortcut = "l", underline = 0)
 
 proc showShipOrders*(dialog: var GameDialog) {.raises: [], tags: [RootEffect],
     contractual.} =
@@ -103,7 +221,8 @@ proc showShipOrders*(dialog: var GameDialog) {.raises: [], tags: [RootEffect],
           discard
       let haveTrader: bool = findMember(order = talk) > -1
       if playerShip.speed == docked:
-        showDockedCommands(baseIndex = baseIndex, haveTrader = haveTrader)
+        showDockedCommands(baseIndex = baseIndex, haveTrader = haveTrader,
+            dialog = dialog)
       labelButton(title = "Close"):
         closePopup()
         dialog = none
