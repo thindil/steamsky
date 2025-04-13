a9f0ee5a74b8d3abeb30f665ae97e25597cb2b17
thindil <thindil@laeran.pl>
2025-04-13 03:14:42 +0000

    feat: started work on the trade screen in the new UI
    FossilOrigin-Name: d9c5c4575699ab9145e850cdd1c6e2d9f62fd0337234024ef05c420e4d40ba29

---
+++ src/newui/tradesui.nim
@@ -0,0 +1,34 @@
+# Copyright 2025 Bartek thindil Jasicki
+#
+# This file is part of Steam Sky.
+#
+# Steam Sky is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Steam Sky is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Steam Sky.  if, see <http://www.gnu.org/licenses/>.
+
+## Provides code related to trading with bases and ships UI, like showing the
+## list of items to trade, info about items, trading itself, etc.
+
+import contracts
+import coreui
+
+proc showTrade*(state: var GameState; dialog: var GameDialog) {.raises: [],
+    tags: [RootEffect], contractual.} =
+  ## Show the trading UI
+  ##
+  ## * state - the current game's state
+  ## * dialog - the current in-game dialog displayed on the screen
+  ##
+  ## Returns the modified parameters state and dialog. The latter is modified if
+  ## any error happened.
+  state = trade
+  dialog = none
