9715043b309185e58be8bbb41fa869caa8ed86e1
thindil <thindil@laeran.pl>
2025-05-07 03:10:35 +0000

    refactor: continue work on move Nuklear to Nim
    FossilOrigin-Name: 029c04e6ca200d2ee5f599a864833f4eb10f545a909ad580f11255d17c18d7b2

--- src/newui/nuklear/nk_types.nim
+++ src/newui/nuklear/nk_types.nim
@@ -194,11 +194,18 @@ type
     background*, group_text_color*: nk_color
     border*, combo_border*, contextual_border*, menu_border*, group_border*,
       tooltip_border*, popup_border*: cfloat
-  # FIXME: check nuklear.h needs return type
-  nk_draw_begin* = proc(b: ptr nk_command_buffer; userData: nk_handle) {.cdecl.}
+  nk_draw_command* {.importc: "struct nk_draw_command", nodecl.} = object
     ## Internal Nuklear type
-  # FIXME: check nuklear.h needs return type
-  nk_draw_end* = proc(b: ptr nk_command_buffer; userData: nk_handle) {.cdecl.}
+    elem_count*: cuint
+    clip_rect*: nk_rect
+    texture*: nk_handle
+    when defined(nkIncludeCommandUserData):
+      userdata*: nk_handle
+  nk_draw_begin* = proc(b: ptr nk_command_buffer;
+      userData: nk_handle): nk_draw_command {.cdecl.}
+    ## Internal Nuklear type
+  nk_draw_end* = proc(b: ptr nk_command_buffer;
+      userData: nk_handle): nk_draw_command {.cdecl.}
     ## Internal Nuklear type
   nk_style_button* {.importc: "struct nk_style_button", nodecl.} = object
     ## Internal Nuklear type
@@ -210,9 +217,10 @@ type
     image_padding*: nk_vec2
     touch_padding*: nk_vec2
     # TODO
-    # draw_begin*: nk_draw_begin
-    # draw_end*: nk_draw_end
-    # userdata*: nk_handle
+    #draw_begin*: nk_draw_begin
+    #draw_end*: nk_draw_end
+    when defined(nkIncludeCommandUserData):
+      userdata*: nk_handle
   nk_style_progress* {.importc: "struct nk_style_progress", nodecl.} = object
     cursor_normal*: nk_style_item
     ## Internal Nuklear type
