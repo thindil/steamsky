bbd1083faf2a3c5646893dea1cacb6620291a6e9
thindil <thindil@laeran.pl>
2025-05-25 03:32:55 +0000

    feat: started work on the recruit screen in the new UI
    FossilOrigin-Name: f83423846758d1d5a71f6d587083bee41d4f89ed4b297b8b7128feb1ac18619d

---
+++ src/newui/basesrecruitui.nim
@@ -0,0 +1,46 @@
+# Copyright 2025 Bartek thindil Jasicki
+#
+# This file is part of Steam Sky.
+#
+# Steam Sky is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Steam Sky is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Steam Sky.  if, see <http://www.gnu.org/licenses/>.
+
+## Provides code related to recruit new crew members in bases, like show the
+## UI, start negotiating, show information about a recruit, etc.
+
+import contracts, nuklear/nuklear_sdl_renderer
+import ../config
+import coreui, dialogs, header
+
+proc showRecruits*(state: var GameState; dialog: var GameDialog) {.raises: [],
+    tags: [RootEffect], contractual.} =
+  ## Show the school UI
+  ##
+  ## * state - the current game's state
+  ## * dialog - the current in-game dialog displayed on the screen
+  ##
+  ## Returns the modified parameters state and dialog. The latter is modified if
+  ## any error happened.
+  showHeader(dialog = dialog, close = CloseDestination.map, state = state)
+  if state != GameState.school:
+    return
+  # Draw dialogs
+  showQuestion(dialog = dialog, state = state)
+  if state != school:
+    return
+  showMessage(dialog = dialog)
+  showInfo(dialog = dialog)
+  let tableHeight: float = windowHeight - gameSettings.messagesPosition.float - 20
+  setLayoutRowDynamic(height = tableHeight, cols = 1)
+  group(title = "SchoolGroup", flags = {windowNoFlags}):
+    discard
