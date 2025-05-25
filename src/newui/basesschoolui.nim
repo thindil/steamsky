b417e54b144000ea554891d859d9428e50acf57b
thindil <thindil@laeran.pl>
2025-05-25 03:28:47 +0000

    feat: continue work on the school screen in the new UI
    FossilOrigin-Name: 0429e16e6b13840509d0a31fd740f7d1586866619699f1233b7d9c145300dc07

--- src/newui/basesschoolui.nim
+++ src/newui/basesschoolui.nim
@@ -125,6 +125,9 @@ proc showSchool*(state: var GameState; dialog: var GameDialog) {.raises: [],
       else:
         colorLabel(str = text, color = theme.colors[goldenColor])
     setLayoutRowStatic(height = 30, cols = 4, ratio = [200.cfloat, 200, 50, 250])
+    if gameSettings.showTooltips:
+      addTooltip(bounds = getWidgetBounds(),
+          text = "Train the selected skill of the selected crew member")
     labelButton(title = "Train"):
       try:
         trainSkill(memberIndex = crewIndex, skillIndex = skillsIndexes[
@@ -138,6 +141,9 @@ proc showSchool*(state: var GameState; dialog: var GameDialog) {.raises: [],
       except:
         dialog = setError(message = "Can't train the skill.")
       setSchool(dialog = dialog)
+    if gameSettings.showTooltips:
+      addTooltip(bounds = getWidgetBounds(),
+          text = "Select the crew member which skills will be trained")
     let newMember = comboList(items = crewList,
         selected = crewIndex, itemHeight = 25, x = 200, y = 150)
     if newMember != crewIndex:
@@ -145,28 +151,52 @@ proc showSchool*(state: var GameState; dialog: var GameDialog) {.raises: [],
       setSchoolSkills()
       setTrainingCost(dialog = dialog)
     label(str = "in", alignment = centered)
+    if gameSettings.showTooltips:
+      addTooltip(bounds = getWidgetBounds(),
+          text = "Select the skill which will be trained")
     let newSkill = comboList(items = schoolSkillsList,
         selected = skillIndex, itemHeight = 25, x = 300, y = 150)
     if newSkill != skillIndex:
       skillIndex = newSkill
       setTrainingCost(dialog = dialog)
     setLayoutRowDynamic(height = 30, cols = 1)
+    if gameSettings.showTooltips:
+      addTooltip(bounds = getWidgetBounds(),
+          text = "Train the selected skill the selected amount of times")
     if option(label = "Selected amount of times", selected = tType == times):
       tType = times
     setLayoutRowDynamic(height = 30, cols = 2)
+    if gameSettings.showTooltips:
+      addTooltip(bounds = getWidgetBounds(),
+          text = "Enter amount of training sessions between 1 and 100")
     label(str = "Amount:")
+    if gameSettings.showTooltips:
+      addTooltip(bounds = getWidgetBounds(),
+          text = "Enter amount of training sessions between 1 and 100")
     let newAmount: int = property2(name = "#", min = 1, val = amount, max = 100,
         step = 1, incPerPixel = 1)
     if newAmount != amount:
       amount = newAmount
       timesCost = oneTrainCost * amount
+    if gameSettings.showTooltips:
+      addTooltip(bounds = getWidgetBounds(),
+          text = "Minimal cost of training. The real cost can be higher that this.")
     label(str = "Minimal cost:")
+    if gameSettings.showTooltips:
+      addTooltip(bounds = getWidgetBounds(),
+          text = "Minimal cost of training. The real cost can be higher that this.")
     colorLabel(str = $timesCost & " " & moneyName, color = theme.colors[goldenColor])
     setLayoutRowDynamic(height = 30, cols = 1)
+    if gameSettings.showTooltips:
+      addTooltip(bounds = getWidgetBounds(),
+          text = "Train the selected skill as long as you don't spend the selected amount of money")
     if option(label = "Selected maximum cost of training", selected = tType == cost):
       tType = cost
     setLayoutRowDynamic(height = 30, cols = 2)
     label(str = "Cost:")
+    if gameSettings.showTooltips:
+      addTooltip(bounds = getWidgetBounds(),
+          text = "Enter amount of money which you want to spend")
     let newCost: int = property2(name = "#", min = oneTrainCost, val = minCost,
         max = oneTrainCost * 10_000, step = oneTrainCost, incPerPixel = 1)
     if newCost != minCost:
