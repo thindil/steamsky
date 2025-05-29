630cb77fd95bf461557d4e30b69089a883b653f7
thindil <thindil@laeran.pl>
2025-05-29 03:13:32 +0000

    feat: continue work on the recruit screen in the new UI
    FossilOrigin-Name: f3931f8a9dfa33458427cfec7e5a25a329a6b4f95cf76c2e570e539e562d1f0f

--- src/newui/basesrecruitui.nim
+++ src/newui/basesrecruitui.nim
@@ -22,9 +22,18 @@ import contracts, nuklear/nuklear_sdl_renderer
 import ../config
 import coreui, header, messagesui, table
 
-type RecruitsSortOrders = enum
-  none, nameAsc, nameDesc, genderAsc, genderDesc, factionAsc, factionDesc,
-    priceAsc, priceDesc, attributeAsc, attributeDesc, skillAsc, skillDesc
+type
+  RecruitsSortOrders = enum
+    none, nameAsc, nameDesc, genderAsc, genderDesc, factionAsc, factionDesc,
+      priceAsc, priceDesc, attributeAsc, attributeDesc, skillAsc, skillDesc
+  LocalRecruitData = object
+    name: string
+    gender: char
+    faction: string
+    price: Positive = 1
+    attribute: string
+    skill: string
+    id: Natural = 0
 
 const
   defaultRecruitsSortOrder: RecruitsSortOrders = none
@@ -41,6 +50,92 @@ const
         sortDesc: attributeDesc),
     HeaderData[RecruitsSortOrders](label: "Highest skill", sortAsc: skillAsc,
         sortDesc: skillDesc)]
+  ratio: array[6, cfloat] = [300.cfloat, 200, 200, 200, 200, 200]
+
+var recruitsSortOrder: RecruitsSortOrders = defaultRecruitsSortOrder
+
+proc sortRecruits(x, y: LocalRecruitData): int =
+  ## Check how to sort the selected items on the list
+  ##
+  ## * x - the first item to sort
+  ## * y - the second item to sort
+  ##
+  ## Returns 1 if the x item should go first, otherwise -1
+  case recruitsSortOrder
+  of nameAsc:
+    if x.name < y.name:
+      return 1
+    else:
+      return -1
+  of nameDesc:
+    if x.name > y.name:
+      return 1
+    else:
+      return -1
+  of genderAsc:
+    if x.gender < y.gender:
+      return 1
+    else:
+      return -1
+  of genderDesc:
+    if x.gender > y.gender:
+      return 1
+    else:
+      return -1
+  of factionAsc:
+    if x.faction < y.faction:
+      return 1
+    else:
+      return -1
+  of factionDesc:
+    if x.faction > y.faction:
+      return 1
+    else:
+      return -1
+  of priceAsc:
+    if x.price < y.price:
+      return 1
+    else:
+      return -1
+  of priceDesc:
+    if x.price > y.price:
+      return 1
+    else:
+      return -1
+  of attributeAsc:
+    if x.attribute < y.attribute:
+      return 1
+    else:
+      return -1
+  of attributeDesc:
+    if x.attribute > y.attribute:
+      return 1
+    else:
+      return -1
+  of skillAsc:
+    if x.skill < y.skill:
+      return 1
+    else:
+      return -1
+  of skillDesc:
+    if x.skill > y.skill:
+      return 1
+    else:
+      return -1
+  of none:
+    return -1
+
+proc sortRecruits(sortAsc, sortDesc: RecruitsSortOrders;
+    dialog: var GameDialog) {.raises: [], tags: [RootEffect], contractual.} =
+  ## Sort items on the trades list
+  ##
+  ## * sortAsc  - the sorting value for ascending sort
+  ## * sortDesc - the sorting value for descending sort
+  ## * dialog   - the current in-game dialog displayed on the screen
+  ##
+  ## Returns the modified parameter dialog. It is modified if any error
+  ## happened.
+  discard
 
 proc showRecruits*(state: var GameState; dialog: var GameDialog) {.raises: [],
     tags: [RootEffect], contractual.} =
@@ -56,6 +151,7 @@ proc showRecruits*(state: var GameState; dialog: var GameDialog) {.raises: [],
   let tableHeight: float = windowHeight - gameSettings.messagesPosition.float - 20
   setLayoutRowDynamic(height = tableHeight, cols = 1)
   group(title = "RecruitsGroup", flags = {windowNoFlags}):
-    discard
+    addHeader(headers = headers, ratio = ratio, tooltip = "recruits",
+      code = sortRecruits, dialog = dialog)
   showLastMessages(theme = theme, dialog = dialog, height = windowHeight - tableHeight)
   showGameMenu(dialog = dialog)
