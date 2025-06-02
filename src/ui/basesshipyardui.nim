57f86d2a0b1f3108105c5aa53694b6cdba9f5ef9
thindil <thindil@laeran.pl>
2025-06-02 03:09:47 +0000

    chore: fixed nimalyzer errors
    FossilOrigin-Name: 87e059b4bc067e2ba6d6d2e0ab1b271428e9fe109cc0755968a73423f305884c

--- src/ui/basesshipyardui.nim
+++ src/ui/basesshipyardui.nim
@@ -16,7 +16,7 @@
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
 import std/[algorithm, strutils, tables]
-import contracts
+import contracts, nimalyzer
 import ../[bases, basesship2, config, crewinventory, game, maps, shipscrew,
     shipmodules, tk, types]
 import coreui, dialogs, errordialog, mapsui, table, utilsui2
@@ -27,7 +27,7 @@ var
 
 proc showShipyardCommand(clientData: cint; interp: PInterp; argc: cint;
     argv: cstringArray): TclResults {.raises: [], tags: [
-    RootEffect], cdecl, contractual.} =
+    RootEffect], cdecl, contractual, ruleOff: "params".} =
   ## Show the selected base shipyard
   ##
   ## * clientData - the additional data for the Tcl command
