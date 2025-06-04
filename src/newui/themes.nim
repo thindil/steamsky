051f0abb4abf7fe08efb47ac9bc92b7935ccc9fa
thindil <thindil@laeran.pl>
2025-06-04 03:06:50 +0000

    feat: added new icon to themes in the new UI
    FossilOrigin-Name: c70724e80a14b9aa2969729b0a554584bead7323df77563037a1f12f4e72306c

--- src/newui/themes.nim
+++ src/newui/themes.nim
@@ -53,7 +53,8 @@ type
       arrowDownLeft, arrowDown, arrowDownRight, waitIcon, moveToIcon,
       moveStepIcon, expandIcon, contractIcon, selectAllIcon, unselectAllIcon,
       cancelIcon, assignCrewIcon, exitIcon, moreOptionsIcon, contract2Icon,
-      expand2Icon, buyDefaultIcon, sellDefaultIcon, buyIcon, sellIcon
+      expand2Icon, buyDefaultIcon, sellDefaultIcon, buyIcon, sellIcon,
+      negotiateIcon
   MapIconsNames* = enum
     ## Names of icons used in the game's map
     playerShipIcon, emptyMapIcon, targetIcon, storyIcon, deliverIcon,
@@ -110,7 +111,8 @@ let
       defaultThemeIconPath & "moreoptions.svg", defaultThemeIconPath &
       "vertical-flip.svg", defaultThemeIconPath & "contract.svg",
       defaultThemeIconPath & "buy2.svg", defaultThemeIconPath & "sell2.svg",
-      defaultThemeIconPath & "buy.svg", defaultThemeIconPath & "sell.svg"],
+      defaultThemeIconPath & "buy.svg", defaultThemeIconPath & "sell.svg",
+      defaultThemeFontPath & "negotiate.svg"],
       colors: ["#1a130c".parseColor, "#eee8aa".parseColor,
       "#4e9a06".parseColor, "#372412".parseColor, "#291913".parseColor,
       "#500000".parseColor, "#120d0d".parseColor, "#ffdf00".parseColor,
