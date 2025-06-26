8407d998b228faade7c90ef6fa111afd5ca96031
thindil <thindil@laeran.pl>
2025-06-26 03:11:30 +0000

    feat: added a new icon to themes in the new UI
    FossilOrigin-Name: 8aff2dfb407d0bb149e608f3ba271ce8e67a2715f9581a9bbeb08aa29ca37db3

--- src/newui/themes.nim
+++ src/newui/themes.nim
@@ -54,7 +54,7 @@ type
       moveStepIcon, expandIcon, contractIcon, selectAllIcon, unselectAllIcon,
       cancelIcon, assignCrewIcon, exitIcon, moreOptionsIcon, contract2Icon,
       expand2Icon, buyDefaultIcon, sellDefaultIcon, buyIcon, sellIcon,
-      negotiateIcon, helpIcon
+      negotiateIcon, helpIcon, negotiateColoredIcon
   MapIconsNames* = enum
     ## Names of icons used in the game's map
     playerShipIcon, emptyMapIcon, targetIcon, storyIcon, deliverIcon,
@@ -113,7 +113,7 @@ let
       defaultThemeIconPath & "buy2.svg", defaultThemeIconPath & "sell2.svg",
       defaultThemeIconPath & "buy.svg", defaultThemeIconPath & "sell.svg",
       defaultThemeIconPath & "negotiate.svg", defaultThemeIconPath &
-      "help.svg"],
+      "help.svg", defaultThemeIconPath & "negotiate2.svg"],
       colors: ["#1a130c".parseColor, "#eee8aa".parseColor,
       "#4e9a06".parseColor, "#372412".parseColor, "#291913".parseColor,
       "#500000".parseColor, "#120d0d".parseColor, "#ffdf00".parseColor,
