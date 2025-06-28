bbaf3b727e20548f840544f7d6b7eccb9f327718
thindil <thindil@laeran.pl>
2025-06-28 03:08:23 +0000

    faet: added a new color to themes in the new UI
    FossilOrigin-Name: 641cb98cc82c089cb69a25b2f4cbd55d9c1f28a9deb0464f252e5cb9ac7bc684

--- src/newui/themes.nim
+++ src/newui/themes.nim
@@ -33,7 +33,8 @@ type
       selectActiveTextColor, propertyTextColor, toggleColor, toggleHoverColor,
       toggleCursorColor, goldenColor, redColor, mapInfoBorderColor,
       mapInfoColor, pinkColor, yellowColor, blueColor, cyanColor, grayColor,
-      tableRowColor, tableTextColor, sliderEmptyColor, sliderFillColor
+      tableRowColor, tableTextColor, sliderEmptyColor, sliderFillColor,
+      sliderCursorColor
   MapColorsNames* = enum
     ## Names of colors used in the game's map
     mapVisitedColor, mapUnvisitedColor, mapDefaultColor, mapGreenColor,
@@ -127,7 +128,7 @@ let
       "#7f8c8d".parseColor, "#000000".parseColor, "#b16286".parseColor,
       "#d79921".parseColor, "#458588".parseColor, "#00ffff".parseColor,
       "#7f8c8d".parseColor, "#120d0d".parseColor, "#eee8aa".parseColor,
-      "#120d0d".parseColor, "#ffdf00".parseColor],
+      "#120d0d".parseColor, "#ffdf00".parseColor, "#500000".parseColor],
       fonts: [defaultThemeFontPath & "Amarante-Regular.ttf",
       defaultThemeFontPath & "Hack Bold Nerd Font Complete Mono Windows Compatible.ttf"],
       mapIcons: ["\uf135", " ", "\uf05b", "\uf059", "\uf53b", "\ufc6a",
@@ -295,8 +296,8 @@ proc loadThemes*() {.raises: [], tags: [WriteIOEffect, TimeEffect, RootEffect,
   setColor(colorName = popupColor, index = mapInfoColor)
   setColor(colorName = sliderColor, index = sliderEmptyColor)
   setColor(colorName = sliderCursorColor, index = sliderFillColor)
-  table[sliderCursorHoverColor] = NimColor(r: 53, g: 88, b: 116, a: 255)
-  table[sliderCursorActiveColor] = NimColor(r: 58, g: 93, b: 121, a: 255)
+  setColor(colorName = sliderCursorHoverColor, index = sliderCursorColor)
+  setColor(colorName = sliderCursorActiveColor, index = sliderCursorColor)
   table[chartColor] = NimColor(r: 50, g: 58, b: 61, a: 255)
   table[colorChartColor] = NimColor(r: 48, g: 83, b: 111, a: 255)
   table[colorChartHighlightColor] = NimColor(r: 255, g: 0, b: 0, a: 255)
