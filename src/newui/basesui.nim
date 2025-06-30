301e4cf971f4ef6068d9b6029c3a27988c0436d5
thindil <thindil@laeran.pl>
2025-06-30 03:31:37 +0000

    feat: started work on healing wounded crew members in the new UI
    FossilOrigin-Name: 91cd200c6d1070c5560dcc84ff2a8d8bc1928da680c3a42025c96b8a6b0fe148

---
+++ src/newui/basesui.nim
@@ -0,0 +1,34 @@
+# Copyright 2025 Bartek thindil Jasicki
+#
+# This file is part of Steam Sky.
+#
+# Steam Sky is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Steam Sky is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Steam Sky.  if, see <http://www.gnu.org/licenses/>.
+
+## Provides code related to various interactions in bases, like buying recipes,
+## repair ship, healing wounded crew memebrs, etc.
+
+import contracts
+import coreui, header
+
+proc showWounded*(state: var GameState; dialog: var GameDialog) {.raises: [],
+    tags: [RootEffect], contractual.} =
+  ## Show the UI with the list of wounded the player's ship's crew members
+  ##
+  ## * state - the current game's state
+  ## * dialog - the current in-game dialog displayed on the screen
+  ##
+  ## Returns the modified parameters state and dialog. The latter is modified if
+  ## any error happened.
+  if showHeader(dialog = dialog, close = CloseDestination.map, state = state):
+    return
