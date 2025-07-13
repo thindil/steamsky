1774e7a450a4270a0ae718fe90735afd9901e055
thindil <thindil@laeran.pl>
2025-07-13 02:58:50 +0000

    docs: added code documentation to module
    FossilOrigin-Name: 5320300a88f74504c03bb6d91b5002b3b4cdb4af404ac6d7c4cbef19ad25c883

--- src/ui/messagesui.nim
+++ src/ui/messagesui.nim
@@ -15,6 +15,9 @@
 # You should have received a copy of the GNU General Public License
 # along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
 
+## Provides code related to showing last in-game messages like showing their
+## list, selecting and deleting messages, etc.
+
 import std/strutils
 import contracts, nimalyzer
 import ../[config, messages, tk, types]
