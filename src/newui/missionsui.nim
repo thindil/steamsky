5a696119a3d0c54c11c2ead0fc2c8ebf9fdccd61
thindil <thindil@laeran.pl>
2025-08-04 03:14:45 +0000

    feat: continue work on list of available missions in the new UI
    FossilOrigin-Name: e87088683a99d68d99304306f2c2bbabcf88c72a0c159d2f8dcbd1b266cd67b9

--- src/newui/missionsui.nim
+++ src/newui/missionsui.nim
@@ -19,16 +19,102 @@
 ## the list, accept a mission, show a mission on the map, etc.
 
 import contracts, nuklear/nuklear_sdl_renderer
-import ../config
+import ../[config, types]
 import coreui, header, messagesui, setui, themes
 
-type MissionsSortOrders = enum
-  none, typeAsc, typeDesc, distanceAsc, distanceDesc, detailsAsc, detailsDesc,
-    timeAsc, timeDesc, rewardAsc, rewardDesc, coordAsc, coordDesc
+type
+  MissionsSortOrders = enum
+    none, typeAsc, typeDesc, distanceAsc, distanceDesc, detailsAsc, detailsDesc,
+      timeAsc, timeDesc, rewardAsc, rewardDesc, coordAsc, coordDesc
+  LocalMissionData = object
+    mType: MissionsTypes
+    distance: Natural
+    coords: string
+    details: string
+    time: Natural
+    reward: Natural
+    id: Natural
 
 const defaultMissionsSortOrder: MissionsSortOrders = none
 
 var missionsSortOrder: MissionsSortOrders = defaultMissionsSortOrder
+
+proc sortMissions(x, y: LocalMissionData): int {.raises: [], tags: [],
+    contractual.} =
+  ## Compare two missions and return which should go first, based on the sort
+  ## order of the missions
+  ##
+  ## * x - the first mission to compare
+  ## * y - the second mission to compare
+  ##
+  ## Returns 1 if the first mission should go first, -1 if the second mission
+  ## should go first.
+  case missionsSortOrder
+  of typeAsc:
+    if x.mType < y.mType:
+      return 1
+    return -1
+  of typeDesc:
+    if x.mType > y.mType:
+      return 1
+    return -1
+  of distanceAsc:
+    if x.distance < y.distance:
+      return 1
+    return -1
+  of distanceDesc:
+    if x.distance > y.distance:
+      return 1
+    return -1
+  of detailsAsc:
+    if x.details < y.details:
+      return 1
+    return -1
+  of detailsDesc:
+    if x.details > y.details:
+      return 1
+    return -1
+  of timeAsc:
+    if x.time < y.time:
+      return 1
+    return -1
+  of timeDesc:
+    if x.time > y.time:
+      return 1
+    return -1
+  of rewardAsc:
+    if x.reward < y.reward:
+      return 1
+    return -1
+  of rewardDesc:
+    if x.reward > y.reward:
+      return 1
+    return -1
+  of coordAsc:
+    if x.coords < y.coords:
+      return 1
+    return -1
+  of coordDesc:
+    if x.coords > y.coords:
+      return 1
+    return -1
+  of none:
+    return -1
+
+proc sortMissions(sortAsc, sortDesc: MissionsSortOrders;
+    dialog: var GameDialog) {.raises: [], tags: [RootEffect], contractual.} =
+  ## Sort missions on the available missions list
+  ##
+  ## * sortAsc  - the sorting value for ascending sort
+  ## * sortDesc - the sorting value for descending sort
+  ## * dialog   - the current in-game dialog displayed on the screen
+  ##
+  ## Returns the modified parameter dialog. It is modified if any error
+  ## happened.
+  if missionsSortOrder == sortAsc:
+    missionsSortOrder = sortDesc
+  else:
+    missionsSortOrder = sortAsc
 
 proc showMissions*(state: var GameState; dialog: var GameDialog) {.raises: [],
     tags: [RootEffect], contractual.} =
