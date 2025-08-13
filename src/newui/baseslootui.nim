4baf0d1899428c013ecd3d460cf92d597161214c
thindil <thindil@laeran.pl>
2025-08-13 03:08:23 +0000

    feat: started work on looting screen in the new UI
    FossilOrigin-Name: edf1fa584834d9e225c8c859dfe742a22715e595e68407f1b079bd7e70afc0cd

---
+++ src/newui/baseslootui.nim
@@ -0,0 +1,43 @@
+# Copyright 2025 Bartek thindil Jasicki
+#
+# This file is part of Steam Sky.
+#
+# Steam Sky is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Steam Sky is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Steam Sky.  if, see <http://www.gnu.org/licenses/>.
+
+## Provides code related to recruit new crew members in bases, like show the
+## UI, start negotiating, show information about a recruit, etc.
+
+import contracts, nuklear/nuklear_sdl_renderer
+import ../config
+import coreui, header, messagesui
+
+proc showLoot*(state: var GameState; dialog: var GameDialog) {.raises: [],
+    tags: [RootEffect], contractual.} =
+  ## Show the loot UI
+  ##
+  ## * state - the current game's state
+  ## * dialog - the current in-game dialog displayed on the screen
+  ##
+  ## Returns the modified parameters state and dialog. The latter is modified if
+  ## any error happened.
+  if showHeader(dialog = dialog, close = CloseDestination.map, state = state):
+    return
+  # Show the list of items to loot
+  let tableHeight: float = windowHeight - gameSettings.messagesPosition.float - 20
+  setLayoutRowDynamic(height = tableHeight, cols = 1)
+  group(title = "LootGroup", flags = {windowNoFlags}):
+    if dialog != none:
+      windowDisable()
+  # Show the last in-game messages
+  showLastMessages(theme = theme, dialog = dialog, height = windowHeight - tableHeight)
