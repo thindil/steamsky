0edc9370ff84ef21dca71c04ef4bc40b37f5620b
thindil <thindil@laeran.pl>
2025-08-29 03:15:30 +0000

    fix: position of the move items from inventory dialog
    FossilOrigin-Name: 75d0fa9af278658bb17a517309163cfcf95a9d2f242e3a0b4d80293b3cb60605

--- src/ui/shipsuicrewinventory.nim
+++ src/ui/shipsuicrewinventory.nim
@@ -532,7 +532,7 @@ proc showMoveItemCommand(clientData: cint; interp: PInterp; argc: cint;
   tclEval(script = "focus " & button)
   tclEval(script = "bind " & button & " <Tab> {focus " & itemDialog & ".movebutton;break}")
   tclEval(script = "bind " & button & " <Escape> {" & button & " invoke;break}")
-  showDialog(dialog = itemDialog)
+  showDialog(dialog = itemDialog, parentFrame = ".memberdialog")
   return tclOk
 
 proc toggleAllInventoryCommand(clientData: cint; interp: PInterp; argc: cint;
