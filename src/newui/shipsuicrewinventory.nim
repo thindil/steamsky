431b7364bf688e7bfc25dd613fb44a882d4ab542
thindil <thindil@laeran.pl>
2025-09-21 02:59:19 +0000

    feat: started work on crew inventory info in the ship screen in the new UI
    FossilOrigin-Name: 8d23dad2ec19b9745af3c586a5e3be533545cc37a6865b8740f62310ad05c1a1

---
+++ src/newui/shipsuicrewinventory.nim
@@ -0,0 +1,45 @@
+# Copyright 2025 Bartek thindil Jasicki
+#
+# This file is part of Steam Sky.
+#
+# Steam Sky is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Steam Sky is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
+
+## Provides code related to the information about the player's ship's crew
+## members's inventory, like listing them, showing information, move items, etc.
+
+import contracts, nuklear/nuklear_sdl_renderer
+import ../[game, types]
+import coreui, dialogs, setui
+
+proc showMemberInventory*(dialog: var GameDialog) {.raises: [], tags: [
+    RootEffect], contractual.} =
+  ## Show the dialog with information about inventory of the selected crew member
+  ##
+  ## * dialog - the current in-game dialog displayed on the screen
+  ##
+  ## Returns the modified parameter dialog.
+  const
+    width: float = 400
+    height: float = 500
+
+  let
+    member: MemberData = playerShip.crew[crewIndex]
+    windowName: string = "Inventory of " & member.name
+  updateDialog(width = width, height = height)
+  window(name = windowName, x = dialogX, y = dialogY, w = width, h = height,
+      flags = {windowBorder, windowTitle, windowNoScrollbar, windowMovable}):
+    setLayoutRowDynamic(height = 30, cols = 1)
+    addCloseButton(dialog = dialog, isPopup = false)
+
+  windowSetFocus(name = windowName)
