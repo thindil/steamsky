593ed53a3845eced93f8e3e8d412a40d52b7332d
thindil <thindil@laeran.pl>
2025-10-07 03:14:40 +0000

    feat: continue work on modules info in the ship screen in the new UI
    FossilOrigin-Name: 7e012f1aa113f026940a771286c73b09718c028f605e58333d2bba9a90845959

--- src/newui/shipsuimodules.nim
+++ src/newui/shipsuimodules.nim
@@ -19,8 +19,9 @@
 ## members, like listing them, showing information, changing their settings,
 ## etc.
 
+import std/[algorithm, tables]
 import contracts, nuklear/nuklear_sdl_renderer
-import ../config
+import ../[config, crafts, game, types]
 import coreui, errordialog, setui, table, themes
 
 type ModulesSortOrders = enum
@@ -29,6 +30,76 @@ type ModulesSortOrders = enum
 const defaultModulesSortOrder: ModulesSortOrders = none
 
 var modulesSortOrder: ModulesSortOrders = defaultModulesSortOrder
+
+proc getModuleInfo(moduleIndex: Natural): string {.raises: [],
+    tags: [], contractual.} =
+  ## Get the additional information about the module
+  ##
+  ## * moduleIndex - the index of the module in the player's ship to show info
+  ##
+  ## Returns the string with the additional information about the module or the
+  ## empty string if no info is available
+  let module: ModuleData = playerShip.modules[moduleIndex]
+  case module.mType
+  of gun:
+    try:
+      if module.ammoIndex in 0..playerShip.cargo.high and itemsList[
+          playerShip.cargo[module.ammoIndex].protoIndex].itemType ==
+          itemsTypesList[modulesList[module.protoIndex].value - 1]:
+        result = "Uses " & itemsList[playerShip.cargo[
+            module.ammoIndex].protoIndex].name & ", "
+      else:
+        result = "No ammunition assigned, "
+    except:
+      result = "No ammunition assigned, "
+    if module.owner[0] == -1:
+      result.add(y = " no gunner.")
+    else:
+      result.add(y = " " & playerShip.crew[module.owner[0]].name & " is gunner.")
+  of workshop:
+    let recipeName: string = try:
+        getWorkshopRecipeName(workshop = moduleIndex)
+      except:
+        ""
+    if recipeName.len > 0:
+      result = recipeName
+      var hasWorkers: bool = false
+      for owner in module.owner:
+        if owner > -1:
+          if hasWorkers:
+            result.add(y = ", " & playerShip.crew[owner].name)
+          else:
+            result.add(y = ", workers: " & playerShip.crew[owner].name)
+          hasWorkers = true
+      if not hasWorkers:
+        result.add(y = ", no workers assigned")
+      result.add(y = ".")
+    else:
+      result = "No crafting order."
+  of engine:
+    if module.disabled:
+      result = "Engine disabled"
+  of trainingRoom:
+    try:
+      if module.trainedSkill > 0:
+        result = "Set for training " & skillsList[module.trainedSkill].name
+        var hasTrainees: bool = false
+        for owner in module.owner:
+          if owner > -1:
+            if hasTrainees:
+              result.add(y = ", " & playerShip.crew[owner].name)
+            else:
+              result.add(y = ", trainees: " & playerShip.crew[owner].name)
+            hasTrainees = true
+        if not hasTrainees:
+          result.add(y = ", no trainees assigned")
+        result.add(y = ".")
+      else:
+        result = "Not set for training."
+    except:
+      result = "Not set for training."
+  else:
+    discard
 
 proc sortModules(sortAsc, sortDesc: ModulesSortOrders;
     dialog: var GameDialog) {.raises: [], tags: [RootEffect], contractual.} =
@@ -44,6 +115,61 @@ proc sortModules(sortAsc, sortDesc: ModulesSortOrders;
     modulesSortOrder = sortDesc
   else:
     modulesSortOrder = sortAsc
+  type LocalModuleData = object
+    name: string
+    damage: float
+    id: Natural
+    info: string
+  var localModules: seq[LocalModuleData] = @[]
+  for index, module in playerShip.modules:
+    localModules.add(y = LocalModuleData(name: module.name, damage: (
+        module.durability / module.maxDurability).float, id: index,
+        info: getModuleInfo(moduleIndex = index)))
+
+  proc sortModules(x, y: LocalModuleData): int {.raises: [], tags: [], contractual.} =
+    case modulesSortOrder
+    of nameAsc:
+      if x.name < y.name:
+        return 1
+      return -1
+    of nameDesc:
+      if x.name > y.name:
+        return 1
+      return -1
+    of damageAsc:
+      if x.damage < y.damage:
+        return 1
+      return -1
+    of damageDesc:
+      if x.damage > y.damage:
+        return 1
+      return -1
+    of infoAsc:
+      if x.info < y.info:
+        return 1
+      return -1
+    of infoDesc:
+      if x.info > y.info:
+        return 1
+      return -1
+    of none:
+      return -1
+
+  localModules.sort(cmp = sortModules)
+  modulesIndexes = @[]
+  for module in localModules:
+    modulesIndexes.add(y = module.id)
+
+proc setModuleInfo(data: int; dialog: var GameDialog) {.raises: [], tags: [
+    RootEffect], contractual.} =
+  ## Set the dialog with information about the selected module
+  ##
+  ## * data   - the index of the selected item
+  ## * dialog - the current in-game dialog displayed on the screen
+  ##
+  ## Returns the modified parameter dialog. It is modified if any error
+  ## happened.
+  discard
 
 const
   headers: array[3, HeaderData[ModulesSortOrders]] = [
@@ -79,5 +205,15 @@ proc showModulesInfo*(dialog: var GameDialog) {.raises: [], tags: [RootEffect],
   setButtonStyle(field = border, value = 0)
   let startRow: Positive = ((currentPage - 1) * gameSettings.listsLimit) + 1
   var row: Positive = 1
+  for index in modulesIndexes:
+    if currentRow < startRow:
+      currentRow.inc
+      continue
+    addButton(label = playerShip.modules[index].name,
+        tooltip = "Show the module's info", data = index, code = setModuleInfo,
+        dialog = dialog)
+    row.inc
+    if row == gameSettings.listsLimit + 1:
+      break
   restoreButtonStyle()
   addPagination(page = currentPage, row = row)
