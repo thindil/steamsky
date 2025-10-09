9383a175d93d579e55b939cf4fe5b95314fe027b
thindil <thindil@laeran.pl>
2025-10-09 03:04:45 +0000

    feat: added a new dialog in the new UI
    FossilOrigin-Name: f23a995a96b6fc6c7cd26871cba49549453990478c80ba5a622c5f8804ec1409

--- src/newui/coreui.nim
+++ src/newui/coreui.nim
@@ -35,9 +35,9 @@ type
       newGoalDialog, boardingDialog, defendingDialog, recruitDialog,
       negotiateDialog, moduleDialog, missionDialog, acceptMissionDialog,
       renameDialog, giveOrderDialog, memberDialog, renameMemberDialog,
-      inventoryDialog, moduleInfoDialog, baseActionDialog, ordersDialog,
-      destinationDialog, messageDialog, infoDialog, buyDialog, sellDialog,
-      takeDialog, dropDialog, moveDialog
+      inventoryDialog, moduleInfoDialog, renameModuleDialog, baseActionDialog,
+      ordersDialog, destinationDialog, messageDialog, infoDialog, buyDialog,
+      sellDialog, takeDialog, dropDialog, moveDialog
 
 const
   dtime*: float = 20.0        ## The length in miliseconds of one game's frame
