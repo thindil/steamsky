0c5f84c336adae053401ecb5ab90e65cbda1b504
thindil <thindil@laeran.pl>
2025-10-15 02:58:31 +0000

    docs: added code documentation to module
    FossilOrigin-Name: 064bc6f65040ccb34185fe2632ff4b81ef028c9c8cde4cd11db40bb132facbc6

--- src/newui/nuklear/nk_page.nim
+++ src/newui/nuklear/nk_page.nim
@@ -23,6 +23,8 @@
 # OR TORT *(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+## Provides code for nuklear page type
+
 import contracts
 import nk_types, nk_context
 
