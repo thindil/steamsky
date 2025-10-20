fa3a3c33f2aaf7bda7fb97f919607e4d854e608a
thindil <thindil@laeran.pl>
2025-10-20 03:03:38 +0000

    feat: added a new icon to themes in the new UI
    FossilOrigin-Name: cc6e4a5a7f81639fa5758dfb7a9ddfaa343c9560413c93a87d979afcceb25917

--- src/newui/themes.nim
+++ src/newui/themes.nim
@@ -59,7 +59,7 @@ type
       dropIcon, editIcon, editColoredIcon, showIcon, goRestIcon, cleanOrderIcon,
       repairOrderIcon, giveOrderColoredIcon, inventoryIcon, dismissIcon,
       giveOrderIcon, cargoIcon, moveIcon, repairPriorityIcon, upgradeButtonIcon,
-      powerIcon
+      powerIcon, assignAmmoIcon
   MapIconsNames* = enum
     ## Names of icons used in the game's map
     playerShipIcon, emptyMapIcon, targetIcon, storyIcon, deliverIcon,
@@ -129,7 +129,8 @@ let
       defaultThemeIconPath & "giveorder.svg", defaultThemeIconPath &
       "cargo.svg", defaultThemeIconPath & "cargo2.svg",
       defaultThemeIconPath & "repair-priority.svg", defaultThemeIconPath &
-      "upgrade-button.svg", defaultThemeIconPath & "power.svg"],
+      "upgrade-button.svg", defaultThemeIconPath & "power.svg",
+      defaultThemeIconPath & "assignammo.svg"],
       colors: ["#1a130c".parseColor, "#eee8aa".parseColor,
       "#4e9a06".parseColor, "#372412".parseColor, "#291913".parseColor,
       "#500000".parseColor, "#120d0d".parseColor, "#ffdf00".parseColor,
