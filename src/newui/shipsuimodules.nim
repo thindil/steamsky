32ab1067e67e280d46a0675865b0034f74b9edf6
thindil <thindil@laeran.pl>
2025-10-21 03:11:22 +0000

    feat: continue work on modules info in the ship screen in the new UI
    FossilOrigin-Name: b652f882b0d573d7ec3ffb662c6692ca2c30845d942c59d3ec7b9aaf66931ca6

--- src/newui/shipsuimodules.nim
+++ src/newui/shipsuimodules.nim
@@ -642,11 +642,27 @@ proc showAssignAmmoDialog*(dialog: var GameDialog) {.raises: [], tags: [
     width: float = 300
     height: float = 400
 
-  let windowName: string = "Available ammo"
+  let
+    windowName: string = "Available ammo"
+    ammoIndex: int = (if playerShip.modules[moduleIndex].mType ==
+        ModuleType2.gun: playerShip.modules[
+        moduleIndex].ammoIndex else: playerShip.modules[
+        moduleIndex].harpoonIndex)
   updateDialog(width = width, height = height)
   window(name = windowName, x = dialogX, y = dialogY, w = width, h = height,
       flags = {windowBorder, windowTitle, windowMovable}):
     setLayoutRowDynamic(height = 35, cols = 1)
+    for index, item in playerShip.cargo:
+      try:
+        if itemsList[item.protoIndex].itemType == itemsTypesList[modulesList[
+            playerShip.modules[moduleIndex].protoIndex].value - 1] and index != ammoIndex:
+          labelButton(title = itemsList[item.protoIndex].name):
+            dialog = moduleInfoDialog
+            assignModule(assignAction = ammo, assignIndex = index,
+                dialog = dialog)
+      except:
+        dialog = setError(message = "Can't add button.")
+        return
     addCloseButton(dialog = dialog, isPopup = false)
 
   windowSetFocus(name = windowName)
