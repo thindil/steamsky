d37a51d53977f08c64a62aad9ce309abe24f047a
thindil <thindil@laeran.pl>
2025-10-25 03:17:46 +0000

    feat: started work on cargo info in the ship screen in the new UI
    FossilOrigin-Name: 0a5143dd0c926e22f604b2ebc0c1a8a5467382d52cd7c105136684c7193d0604

---
+++ src/newui/shipsuicargo.nim
@@ -0,0 +1,33 @@
+# Copyright 2025 Bartek thindil Jasicki
+#
+# This file is part of Steam Sky.
+#
+# Steam Sky is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Steam Sky is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
+
+## Provides code related to the information about the player's ship's cargo,
+## like shoing its list, moving to crew members' inventory, dropping items
+## from it, etc.
+
+import contracts
+import coreui
+
+proc showCargoInfo*(dialog: var GameDialog) {.raises: [], tags: [RootEffect],
+    contractual.} =
+  ## Show the list of the player's ship's cargo
+  ##
+  ## * dialog - the current in-game dialog displayed on the screen
+  ##
+  ## Returns the modified parameter dialog. It is modified if any error
+  ## happened.
+  discard
