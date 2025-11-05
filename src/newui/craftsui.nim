99e707ddfed3efb47672c2f3e791977324bef498
thindil <thindil@laeran.pl>
2025-11-05 04:05:19 +0000

    feat: started work on the crafting screen in the new UI
    FossilOrigin-Name: 64d86dd21390fb10529a4a00a1c76c575d7070ed132da1eb5fe3efc63974126f

---
+++ src/newui/craftsui.nim
@@ -0,0 +1,35 @@
+# Copyright 2025 Bartek thindil Jasicki
+#
+# This file is part of Steam Sky.
+#
+# Steam Sky is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Steam Sky is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
+
+## Provides code related to crafting items, like showing the list of available
+## recipes, starting crafting, etc.
+
+import contracts
+import coreui, header
+
+proc showCrafting*(state: var GameState; dialog: var GameDialog) {.raises: [],
+    tags: [RootEffect], contractual.} =
+  ## Show information about available crafting recipes
+  ##
+  ## * state - the current game's state
+  ## * dialog - the current in-game dialog displayed on the screen
+  ##
+  ## Returns the modified parameters state and dialog. The latter is modified if
+  ## any error happened.
+  if showHeader(dialog = dialog, close = CloseDestination.map, state = state,
+    options = true):
+    return
