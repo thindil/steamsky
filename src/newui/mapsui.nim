26fb9ddfcd80c2596d1826b08c4fa9c67ec57887
thindil <thindil@laeran.pl>
2025-11-22 04:06:52 +0000

    refactor: resized size of speed combo box in the new UI
    FossilOrigin-Name: 066330648bf236ad2bab8458ea1ca7accfb7829ffd36e95cc12de98bc46f38e8

--- src/newui/mapsui.nim
+++ src/newui/mapsui.nim
@@ -497,7 +497,7 @@ proc showButtons(dialog: var GameDialog) {.raises: [], tags: [RootEffect],
             text = "Set speed for your ship. The faster you move, the more fuel used. But faster movement has bigger chance to evade enemies.")
       playerShip.speed = (comboList(items = shipSpeeds,
           selected = playerShip.speed.ord - 1, itemHeight = 25, x = 200,
-          y = 150) + 1).ShipSpeed
+          y = 50) + 1).ShipSpeed
       setLayoutRowStatic(height = 30, cols = 3, ratio = [40.cfloat, 40, 40])
       if gameSettings.showTooltips:
         addTooltip(bounds = getWidgetBounds(),
