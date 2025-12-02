57cec3f28a18946836508794281938db7c4cf7c0
thindil <thindil@laeran.pl>
2025-12-02 04:15:35 +0000

    feat: started work on list of known bases
    FossilOrigin-Name: 83865b8b9e62476e6ae4f4c670955e1c2436bf4f733a77ef57e3a43824a96949

---
+++ src/newui/knowledgebases.nim
@@ -0,0 +1,38 @@
+# Copyright 2025 Bartek thindil Jasicki
+#
+# This file is part of Steam Sky.
+#
+# Steam Sky is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Steam Sky is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
+
+## Provides code related to the information about the player's knowledge, like
+## lists of known bases, events missions, finished stories, etc.
+
+import contracts
+import coreui
+
+var
+  showBasesOptions*: bool = false
+    ## Show additonal options for managing the list of known bases
+
+proc showBasesInfo*(dialog: var GameDialog) {.raises: [], tags: [RootEffect],
+    contractual.} =
+  ## Show the list of the known bases
+  ##
+  ## * dialog - the current in-game dialog displayed on the screen
+  ##
+  ## Returns the modified parameter dialog. It is modified if any error
+  ## happened.
+  # Show options related to managing the list
+  if showBasesOptions:
+    discard
