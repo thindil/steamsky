b26121a66b2558259d8ef8c72165f6928047b008
thindil <thindil@laeran.pl>
2025-12-24 04:04:51 +0000

    feat: added new fields to ModuleData type
    FossilOrigin-Name: 683b0664ed0f491f6a8fc7842f28f4034ff085940dcbbef2ff7cc1e853650edf

--- src/types.nim
+++ src/types.nim
@@ -179,6 +179,8 @@ type
     ## * craftingTime     - The amount of time needed to finish the order
     ## * craftingAmount   - How many times repeat the crafting order
     ## * craftingQuality  - The desired quality of crafted item
+    ## * craftingBonus    - The special bonus for crafted item
+    ## * craftingMalus    - The special malus for crafted item
     ## * trainedSkill     - The index of trained skill
     ## * damage2          - The damage of the battering ram
     ## * coolingDown      - If true, the battering ram can't attack now
@@ -214,6 +216,8 @@ type
       craftingTime*: Natural
       craftingAmount*: Natural
       craftingQuality*: ObjectQuality
+      craftingBonus*: CraftBonuses
+      craftingMalus*: CraftMaluses
     of ModuleType2.trainingRoom:
       trainedSkill*: Natural
     of ModuleType2.batteringRam:
