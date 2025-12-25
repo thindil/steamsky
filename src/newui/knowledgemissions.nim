c9dcca84a7dd0624703615086e9230d95aa512ad
thindil <thindil@laeran.pl>
2025-12-25 03:59:48 +0000

    feat: continue work on list of accepted missions
    FossilOrigin-Name: ff3e69323f443fb4d323ef995b8ce8f758910b6dd8ca6d26005fd48cbed5702c

--- src/newui/knowledgemissions.nim
+++ src/newui/knowledgemissions.nim
@@ -18,8 +18,32 @@
 ## Provides code related to the information about the list of accepted
 ## missions, like sorting them, showing information about them, etc.
 
-import contracts
-import coreui
+import contracts, nuklear/nuklear_sdl_renderer
+import ../config
+import coreui, errordialog, setui, table, themes
+
+type MissionsSortOrders = enum
+  none, typeAsc, typeDesc, distanceAsc, distanceDesc, timeAsc, timeDesc,
+    rewardAsc, rewardDesc, coordAsc, coordDesc
+
+const defaultMissionsSortOrder: MissionsSortOrders = none
+
+var missionsSortOrder: MissionsSortOrders = defaultMissionsSortOrder
+
+proc sortMissions(sortAsc, sortDesc: MissionsSortOrders;
+    dialog: var GameDialog) {.raises: [], tags: [RootEffect], contractual.} =
+  ## Sort missions on the list
+  ##
+  ## * sortAsc  - the sorting value for ascending sort
+  ## * sortDesc - the sorting value for descending sort
+  ## * dialog   - the current in-game dialog displayed on the screen
+  ##
+  ## Returns the modified parameter dialog. It is modified if any error
+  ## happened.
+  if missionsSortOrder == sortAsc:
+    missionsSortOrder = sortDesc
+  else:
+    missionsSortOrder = sortAsc
 
 proc showMissionsInfo*(dialog: var GameDialog) {.raises: [], tags: [RootEffect],
     contractual.} =
@@ -30,4 +54,39 @@ proc showMissionsInfo*(dialog: var GameDialog) {.raises: [], tags: [RootEffect],
   ## Returns the modified parameter dialog. It is modified if any error
   ## happened.
   # No missions
-  discard
+  if missionsUIList.len == 0:
+    setLayoutRowDynamic(height = 100, cols = 1)
+    wrapLabel(str = "You didn't accept any mission yet. You may ask for missions in bases. When your ship is docked to base, check Missions from ship orders menu.")
+  else:
+    const
+      headers: array[5, HeaderData[MissionsSortOrders]] = [
+        HeaderData[MissionsSortOrders](label: "Name", sortAsc: typeAsc,
+            sortDesc: typeDesc),
+        HeaderData[MissionsSortOrders](label: "Distance",
+            sortAsc: distanceAsc, sortDesc: distanceDesc),
+        HeaderData[MissionsSortOrders](label: "Coordinates", sortAsc: coordAsc,
+            sortDesc: coordDesc),
+        HeaderData[MissionsSortOrders](label: "Time limit",
+            sortAsc: timeAsc, sortDesc: timeDesc),
+        HeaderData[MissionsSortOrders](label: "Base reward",
+            sortAsc: rewardAsc, sortDesc: rewardDesc)]
+      ratio: array[5, cfloat] = [200.cfloat, 100, 150, 250, 150]
+
+    addHeader(headers = headers, ratio = ratio, tooltip = "missions",
+        code = sortMissions, dialog = dialog)
+    let startRow: Positive = ((currentPage - 1) * gameSettings.listsLimit) + 1
+    saveButtonStyle()
+    setButtonStyle(field = borderColor, a = 0)
+    try:
+      setButtonStyle(field = normal, color = theme.colors[tableRowColor])
+      setButtonStyle(field = textNormal, color = theme.colors[tableTextColor])
+    except:
+      dialog = setError(message = "Can't set table color")
+      return
+    setButtonStyle(field = rounding, value = 0)
+    setButtonStyle(field = border, value = 0)
+    var
+      row, currentRow: Positive = 1
+    # Show the list of accepted missions
+    restoreButtonStyle()
+    addPagination(page = currentPage, row = row)
