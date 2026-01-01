f9828a27e124bdc3988da5279ac470da1f0509c9
thindil <thindil@laeran.pl>
2026-01-01 04:07:43 +0000

    feat: added field weight to InventoryData and BaseCargo types
    FossilOrigin-Name: 61d268240ab5702cdf80f772799dfca7b5f8bb00e162ae80038c3e54c8cb3ac2

--- src/types.nim
+++ src/types.nim
@@ -244,12 +244,14 @@ type
     ## * maxDurability - The maximum durability of the item
     ## * price         - The price for which the item was bought
     ## * quality       - The quality of the item
+    ## * weight        - The weight of the item
     protoIndex*: Natural = 0
     amount*: Positive = 1
     name*: string
     durability*, maxDurability*: ItemsDurability = 100
     price*: Natural = 0
     quality*: ObjectQuality = normal
+    weight*: Positive = 1
 
   MobAttributeRecord* = object
     ## Used to store information about the crew member's attributes
@@ -470,11 +472,13 @@ type
     ## * maxDurability - The maximum durability of the item
     ## * price         - The price for which the item was bought
     ## * quality       - The quality of the item
+    ## * weight        - The weight of the item
     protoIndex*: Natural
     amount*: Natural
     durability*, maxDurability*: ItemsDurability = 100
     price*: Natural
     quality*: ObjectQuality = normal
+    weight*: Positive = 1
 
   DateRecord* = object
     ## Used to store the game's time
