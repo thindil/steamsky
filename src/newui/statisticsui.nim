3af5d1b02b3de1b682e3bc45b94d6f85b8aecfc9
thindil <thindil@laeran.pl>
2026-01-02 04:12:25 +0000

    feat: started work on the game's statistics screen in the new UI
    FossilOrigin-Name: da2b2f13bfbe09281b8b765c017fe37bbb7b3e5674e5612f6e288d673e57c091

---
+++ src/newui/statisticsui.nim
@@ -0,0 +1,32 @@
+# Copyright 2026 Bartek thindil Jasicki
+#
+# This file is part of Steam Sky.
+#
+# Steam Sky is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Steam Sky is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
+
+## Provides code related to the game's statistics ui, like showing them, etc.
+
+import contracts
+import coreui
+
+proc showStatistics*(state: var GameState; dialog: var GameDialog) {.raises: [],
+    tags: [RootEffect], contractual.} =
+  ## Show the screen with information about the current game's statistics
+  ##
+  ## * state - the current game's state
+  ## * dialog - the current in-game dialog displayed on the screen
+  ##
+  ## Returns the modified parameters state and dialog. The latter is modified if
+  ## any error happened.
+  discard
