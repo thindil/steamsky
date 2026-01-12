38e14f3bbf82556b839fb5b4251bb79012c8b4c6
thindil <thindil@laeran.pl>
2026-01-12 04:12:05 +0000

    feat: continue work on the in-game help screen in the new UI
    FossilOrigin-Name: 1ebf1d3463b0a242eb5d1d62c34e8c16317314ae0765d75bd98088ca5f97862b

--- src/newui/helpui.nim
+++ src/newui/helpui.nim
@@ -17,9 +17,12 @@
 
 ## Provides code related to the game's help, like showing it, etc.
 
+import std/tables
 import contracts, nuklear/nuklear_sdl_renderer
-import ../config
+import ../[config, help, types]
 import coreui, header, themes
+
+var selected: ExtendedNatural = -1
 
 proc showHelp*(state: var GameState; dialog: var GameDialog) {.raises: [],
     tags: [RootEffect], contractual.} =
@@ -35,14 +38,23 @@ proc showHelp*(state: var GameState; dialog: var GameDialog) {.raises: [],
   setLayoutRowDynamic(height = gameSettings.topicsPosition.float, cols = 1)
   group(title = "TopicsGroup", flags = {windowNoFlags}):
     setLayoutRowDynamic(height = 25, cols = 1)
+    var index = 0
+    for title, help in helpList:
+      index.inc
+      var sel: bool = selected == index
+      if selectableLabel(str = title, value = sel):
+        if sel:
+          selected = index
+        else:
+          selected = -1
   setLayoutRowDynamic(height = 20, cols = 2)
   if gameSettings.showTooltips:
     addTooltip(bounds = getWidgetBounds(),
         text = "Make the list of topics smaller.")
   imageButtonCentered(image = images[contract2Icon]):
-    gameSettings.topicsPosition += gameSettings.interfaceFontSize + 10
+    gameSettings.topicsPosition -= gameSettings.interfaceFontSize + 10
   if gameSettings.showTooltips:
     addTooltip(bounds = getWidgetBounds(),
         text = "Make the list of topics bigger.")
   imageButtonCentered(image = images[expand2Icon]):
-    gameSettings.topicsPosition -= gameSettings.interfaceFontSize + 10
+    gameSettings.topicsPosition += gameSettings.interfaceFontSize + 10
