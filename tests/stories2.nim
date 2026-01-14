8a7e80e9b8191bb84dc78c65895682a2261466c0
thindil <thindil@laeran.pl>
2026-01-14 04:08:06 +0000

    tests: fixed tests for stories2 module
    FossilOrigin-Name: 98036921c2815b6394a11d34555b1b1eb362a523a5b959a9884e2cf8886868d4

--- tests/stories2.nim
+++ tests/stories2.nim
@@ -1,5 +1,5 @@
 import std/paths
-import ../src/[careers, factions, items]
+import ../src/[careers, crafts, factions, items, mobs, shipmodules, ships]
 import unittest2
 include ../src/stories2
 
@@ -10,19 +10,28 @@ suite "Unit tests for stories2 module":
   loadItems("bin/data/items.dat".Path)
   loadCareers("bin/data/careers.dat".Path)
   loadFactions("bin/data/factions.dat".Path)
+  loadModules("bin/data/shipmodules.dat".Path)
+  loadRecipes("bin/data/recipes.dat".Path)
+  loadMobs("bin/data/mobs.dat".Path)
+  loadShips("bin/data/ships.dat".Path)
   loadStories("bin/data/stories.dat".Path)
 
   playerShip.crew = @[]
   playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural],
-      homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 2, 1, 1,
-      1, 0, 0], order: talk, loyalty: 100, skills: @[SkillInfo(index: 4, level: 4,
-      experience: 0)], attributes: @[MobAttributeRecord(level: 3, experience: 0),
+      homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 2, 1,
+      1, 1, 0, 0], order: talk, loyalty: 100, skills: @[SkillInfo(index: 4,
+      level: 4, experience: 0)], attributes: @[MobAttributeRecord(level: 3,
+      experience: 0), MobAttributeRecord(level: 3, experience: 0),
       MobAttributeRecord(level: 3, experience: 0), MobAttributeRecord(level: 3,
-      experience: 0), MobAttributeRecord(level: 3, experience: 0)]))
+      experience: 0)]))
   while currentStory.index.len == 0:
     startStory("Undead", dropItem)
     if currentStory.index.len > 0:
       break
+  var amount: Natural = getRandom(min = 1, max = 20)
+  while amount > 0:
+    skyBases[getRandom(min = skyBases.low, max = skyBases.high)].known = true
+    amount.dec
 
   test "Progress the current story.":
     discard progressStory()
