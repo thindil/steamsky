082e253d5c43fe71a30293918d312da53517d340
thindil <thindil@laeran.pl>
2026-01-22 04:03:21 +0000

    feat: continue work on the in-game help screen in the new UI
    FossilOrigin-Name: de9e6c84453ee4bf42e2cee0f7c6f95ecdcc3442050a5dfa107d321e72bba911

--- src/newui/helpui.nim
+++ src/newui/helpui.nim
@@ -60,4 +60,6 @@ proc showHelp*(state: var GameState; dialog: var GameDialog) {.raises: [],
   group(title = "ContentGroup", flags = {windowNoFlags}):
     setLayoutRowDynamic(height = windowHeight -
         gameSettings.topicsPosition.float - 20, cols = 1)
-    wrapLabel(str = helpContent[0][0].text[0..10])
+    for row in helpContent:
+      for lbl in row:
+        label(str = lbl.text)
