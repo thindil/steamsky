3284c06e8a03b114422e4a0fe405e2c8b715b69f
thindil <thindil@laeran.pl>
2026-02-03 04:05:46 +0000

    feat: started work on the game options screen in the new UI
    FossilOrigin-Name: 4e164fe57c03e329d2c695bd176635f6415afa50b07eaae2c3c42c72163d2c52

---
+++ src/newui/gameoptions.nim
@@ -0,0 +1,33 @@
+# Copyright 2026 Bartek thindil Jasicki
+#
+# This file is part of Steam Sky.
+#
+# Steam Sky is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Steam Sky is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
+
+## Provides code related to the game's options, like showing it, etc.
+
+import contracts
+import coreui, header
+
+proc showOptions*(state: var GameState; dialog: var GameDialog) {.raises: [],
+    tags: [RootEffect], contractual.} =
+  ## Show the screen with game options
+  ##
+  ## * state - the current game's state
+  ## * dialog - the current in-game dialog displayed on the screen
+  ##
+  ## Returns the modified parameters state and dialog. The latter is modified if
+  ## any error happened.
+  if showHeader(dialog = dialog, close = previous, state = state):
+    return
