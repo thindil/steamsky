b464157bc6535a48ebf4408c38c11a3dd7b91431
thindil <thindil@laeran.pl>
2026-03-05 04:02:40 +0000

    refactor: continue work on move Nuklear to Nim
    FossilOrigin-Name: 796e15d5a8df195cc048f4bd4f4f8673a6e5f16161c964575129ad130d58b535

--- src/newui/nuklear/nk_style.nim
+++ src/newui/nuklear/nk_style.nim
@@ -38,7 +38,10 @@ using
 # High level bindings
 # ------------------
 {.push ruleOff: "varDeclared".}
-var defaultColorStyle: array[countColors, NkColor]
+let defaultColorStyle {.used.}: array[4, NkColor] = [textColor: NkColor(r: 175, g: 175,
+    b: 175, a: 255), windowColor: NkColor(r: 45, g: 45, b: 45, a: 255),
+    headerColor: NkColor(r: 40, g: 40, b: 40, a: 255), borderColor: NkColor(
+    r: 65, g: 65, b: 65, a: 255)]
 {.push ruleOn: "varDeclared".}
 
 proc defaultStyle*() {.raises: [], tags: [], contractual.} =
