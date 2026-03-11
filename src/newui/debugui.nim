942e5ca8c59a633ef47f9e76209a83dd36d4ce8c
thindil <thindil@laeran.pl>
2026-03-11 03:58:57 +0000

    feat: started work on debug UI in the new UI
    FossilOrigin-Name: 7b07782dab18cc74b5d02a667d834b95d9b10c187f8bb8773b4800d1f9958708

---
+++ src/newui/debugui.nim
@@ -0,0 +1,43 @@
+# Copyright 2026 Bartek thindil Jasicki
+#
+# This file is part of Steam Sky.
+#
+# Steam Sky is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Steam Sky is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Steam Sky.  If not, see <http://www.gnu.org/licenses/>.
+
+## Provides code related to showing debug interface.
+
+import contracts, nuklear/nuklear_sdl_renderer
+import coreui
+
+proc showDebugUI*(dialog: var GameDialog) {.raises: [], tags: [ReadIOEffect,
+    RootEffect], contractual.} =
+  ## Show the debug dialog with various development options
+  ##
+  ## * dialog - the current in-game dialog to show
+  ##
+  ## Returns parameter dialog.
+  const
+    width: float = 700
+    height: float = 500
+  updateDialog(width = width, height = height)
+  window(name = "Debug options", x = 40, y = 20, w = width, h = height,
+      flags = {windowBorder, windowTitle, windowMinimizable, windowMovable}):
+    setLayoutRowStatic(height = height, cols = 2, ratio = [140.cfloat, 530])
+    group(title = "debugButtons", flags = {windowNoScrollbar}):
+      setLayoutRowDynamic(height = 30, cols = 1)
+      labelButton(title = "Ship"):
+        discard
+    group(title = "debugMenus", flags = {windowNoFlags}):
+      discard
+  windowSetFocus(name = "Debug options")
