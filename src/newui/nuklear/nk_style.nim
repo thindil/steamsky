8087dc7f7d00575ed92ebbd67b3f8fb2cad0d5c9
thindil <thindil@laeran.pl>
2026-03-13 04:03:02 +0000

    refactor: continue work on move Nuklear to Nim
    FossilOrigin-Name: 0544fa8f55381473948f19ef9cf004e25a16acb2372cb2abb2962c06df728087

--- src/newui/nuklear/nk_style.nim
+++ src/newui/nuklear/nk_style.nim
@@ -38,7 +38,7 @@ using
 # High level bindings
 # ------------------
 {.push ruleOff: "varDeclared".}
-let defaultColorStyle {.used.}: array[32, NkColor] = [textColor: NkColor(r: 175,
+let defaultColorStyle {.used.}: array[36, NkColor] = [textColor: NkColor(r: 175,
     g: 175, b: 175, a: 255), windowColor: NkColor(r: 45, g: 45, b: 45, a: 255),
     headerColor: NkColor(r: 40, g: 40, b: 40, a: 255), borderColor: NkColor(
     r: 65, g: 65, b: 65, a: 255), buttonColor: NkColor(r: 65, g: 65, b: 65,
@@ -66,7 +66,11 @@ let defaultColorStyle {.used.}: array[32, NkColor] = [textColor: NkColor(r: 175,
     tabHeaderColor: NkColor(r: 40, g: 40, b: 40, a: 255), knobColor: NkColor(
     r: 38, g: 38, b: 38, a: 255), knobCursorColor: NkColor(r: 100, g: 100,
     b: 100, a: 255), knobCursorHoverColor: NkColor(r: 120, g: 120, b: 120,
-    a: 255), knobCursorActiveColor: NkColor(r: 150, g: 150, b: 150, a: 255)]
+    a: 255), knobCursorActiveColor: NkColor(r: 150, g: 150, b: 150, a: 255),
+    buttonTextColor: NkColor(r: 175, g: 175, b: 175, a: 255),
+    buttonHoverTextColor: NkColor(r: 175, g: 175, b: 175, a: 255),
+    buttonActiveTextColor: NkColor(r: 175, g: 175, b: 175, a: 255),
+    editTextColor: NkColor(r: 175, g: 175, b: 175, a: 255)]
 {.push ruleOn: "varDeclared".}
 
 proc defaultStyle*() {.raises: [], tags: [], contractual.} =
