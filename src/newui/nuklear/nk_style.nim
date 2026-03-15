6c19d3216e9f705a1fb4b89a22cfdcfd3200cf2c
thindil <thindil@laeran.pl>
2026-03-15 04:03:29 +0000

    refactor: continue work on move Nuklear to Nim
    FossilOrigin-Name: 50e6552400b0c98360fb23f254856db55a4b0a0b344bd982b4686dd7df9f8141

--- src/newui/nuklear/nk_style.nim
+++ src/newui/nuklear/nk_style.nim
@@ -38,7 +38,7 @@ using
 # High level bindings
 # ------------------
 {.push ruleOff: "varDeclared".}
-let defaultColorStyle {.used.}: array[40, NkColor] = [textColor: NkColor(r: 175,
+let defaultColorStyle {.used.}: array[44, NkColor] = [textColor: NkColor(r: 175,
     g: 175, b: 175, a: 255), windowColor: NkColor(r: 45, g: 45, b: 45, a: 255),
     headerColor: NkColor(r: 40, g: 40, b: 40, a: 255), borderColor: NkColor(
     r: 65, g: 65, b: 65, a: 255), buttonColor: NkColor(r: 65, g: 65, b: 65,
@@ -74,7 +74,11 @@ let defaultColorStyle {.used.}: array[40, NkColor] = [textColor: NkColor(r: 175,
     comboTextColor: NkColor(r: 175, g: 175, b: 175, a: 255),
     tooltipColor: NkColor(r: 45, g: 45, b: 45, a: 255),
     tooltipBorderColor: NkColor(r: 65, g: 65, b: 65, a: 255),
-    groupBorderColor: NkColor(r: 65, g: 65, b: 65, a: 255)]
+    groupBorderColor: NkColor(r: 65, g: 65, b: 65, a: 255),
+    headerTextColor: NkColor(r: 175, g: 175, b: 175, a: 255),
+    groupTextColor: NkColor(r: 175, g: 175, b: 175, a: 255),
+    selectActiveTextColor: NkColor(r: 175, g: 175, b: 175, a: 255),
+    propertyTextColor: NkColor(r: 175, g: 175, b: 175, a: 255)]
 {.push ruleOn: "varDeclared".}
 
 proc defaultStyle*() {.raises: [], tags: [], contractual.} =
