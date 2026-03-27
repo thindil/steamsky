6da10cf7921630db0d78518eceb62dff83536231
thindil <thindil@laeran.pl>
2026-03-27 03:59:37 +0000

    refactor\!: redesigned updateInventory procedure
    FossilOrigin-Name: 02cbd68d2f2310924528e920f13c4391aa2b67fedd8047c80eeebff9bcc1be94

--- src/crewinventory.nim
+++ src/crewinventory.nim
@@ -134,10 +134,9 @@ proc takeOffItem*(memberIndex, itemIndex: Natural) {.raises: [],
 proc updateInventory*(memberIndex: Natural; amount: int;
     protoIndex: Natural = 0; durability: ItemsDurability = 0;
     inventoryIndex: int = -1; price: Natural = 0; ship: var ShipRecord;
-    quality: ObjectQuality;
-    maxDurability: ItemsDurability = defaultItemDurability; weight: Natural = 0;
-    breakChance: ExtendedNatural) {.raises: [CrewNoSpaceError, KeyError],
-    tags: [], contractual.} =
+    quality: ObjectQuality; craftBonus: CraftBonuses;
+    craftMalus: CraftMaluses) {.raises: [CrewNoSpaceError, KeyError], tags: [],
+    contractual.} =
   ## Update the inventory of the selected crew member.
   ##
   ## * memberIndex    - the index of the crew member which inventory will be updated
@@ -152,9 +151,8 @@ proc updateInventory*(memberIndex: Natural; amount: int;
   ## * price          - the price of the item to update. Default value is 0.
   ## * ship           - the ship in which the crew member inventory will be updated
   ## * quality        - the quality of the item to update (good, normal, poor, etc).
-  ## * maxDurability  - The maximum durability of the item to modify. Can be empty
-  ## * weight         - The weight of the item. Can be empty
-  ## * breakChance    - The chance for item to break during usage
+  ## * craftBonus     - The crafting bonus for the item
+  ## * craftMalus     - The crafting malus for the item
   ##
   ## Returns the updated ship argument
   require:
@@ -165,13 +163,11 @@ proc updateInventory*(memberIndex: Natural; amount: int;
       if durability > 0:
         itemIndex = findItem(inventory = ship.crew[memberIndex].inventory,
             protoIndex = protoIndex, durability = durability,
-            itemQuality = quality, maxDurability = maxDurability,
-            weight = weight, breakChance = breakChance)
+            itemQuality = quality, breakChance = -1)
       else:
         itemIndex = findItem(inventory = ship.crew[memberIndex].inventory,
             protoIndex = protoIndex, itemQuality = quality,
-            maxDurability = maxDurability, weight = weight,
-                breakChance = breakChance)
+                breakChance = -1)
     else:
       itemIndex = inventoryIndex
     if amount > 0:
@@ -258,7 +254,7 @@ proc damageItem*(inventory: var seq[InventoryData]; itemIndex: Natural;
     else:
       updateInventory(memberIndex = memberIndex, amount = -1,
           inventoryIndex = itemIndex, ship = ship, quality = item.quality,
-          breakChance = item.breakChance)
+          craftBonus = item.craftBonus, craftMalus = item.craftMalus)
     return
   inventory[itemIndex] = item
   var i: int = 0
@@ -276,10 +272,10 @@ proc damageItem*(inventory: var seq[InventoryData]; itemIndex: Natural;
         else:
           updateInventory(memberIndex = memberIndex, amount = 0 - inventory[
               j].amount, inventoryIndex = j, ship = ship,
-              quality = item.quality, breakChance = item.breakChance)
+              quality = item.quality, craftBonus = item.craftBonus, craftMalus = item.craftMalus)
           updateInventory(memberIndex = memberIndex, amount = inventory[
               j].amount, inventoryIndex = i, ship = ship,
-              quality = item.quality, breakChance = item.breakChance)
+              quality = item.quality, craftBonus = item.craftBonus, craftMalus = item.craftMalus)
         i.dec
         break
     i.inc
