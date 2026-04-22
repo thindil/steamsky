d11c59ef9df63054abfc35162bc4698e8e2b4d15
thindil <thindil@laeran.pl>
2026-04-22 03:16:06 +0000

    tests: small code cleanup
    FossilOrigin-Name: 2953c8eb5226e85529e738ff7464ef3d9fe5f160c5619a08d7b0b6ff604c97f5

--- tests/events.nim
+++ tests/events.nim
@@ -16,13 +16,12 @@ suite "Unit tests for events module":
 
   playerShip.skyX = 1
   playerShip.skyY = 1
+  const attribute = initMobAttributeRecord(level = 3, experience = 0)
   playerShip.crew = @[]
   playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural],
       homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 2, 1, 1,
       1, 0, 0], order: talk, loyalty: 100, skills: @[initSkillInfo(index = 4, level = 4,
-      experience = 0)], attributes: @[initMobAttributeRecord(level = 3, experience = 0),
-      initMobAttributeRecord(level = 3, experience = 0), initMobAttributeRecord(level = 3,
-      experience = 0), initMobAttributeRecord(level = 3, experience = 0)]))
+      experience = 0)], attributes: @[attribute, attribute, attribute, attribute]))
   playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural],
       homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 0, 1, 1,
       1, 0, 0], order: gunner, loyalty: 100))
--- tests/stories2.nim
+++ tests/stories2.nim
@@ -16,14 +16,12 @@ suite "Unit tests for stories2 module":
   loadShips("bin/data/ships.dat".Path)
   loadStories("bin/data/stories.dat".Path)
 
+  const attribute = initMobAttributeRecord(level = 3, experience = 0)
   playerShip.crew = @[]
   playerShip.crew.add(MemberData(morale: [1: 50.Natural, 2: 0.Natural],
       homeBase: 1, faction: "POLEIS", orders: [0.Natural, 0, 0, 1, 1, 1, 2, 1,
       1, 1, 0, 0], order: talk, loyalty: 100, skills: @[initSkillInfo(index = 4,
-      level = 4, experience = 0)], attributes: @[initMobAttributeRecord(level = 3,
-      experience = 0), initMobAttributeRecord(level = 3, experience = 0),
-      initMobAttributeRecord(level = 3, experience = 0), initMobAttributeRecord(level = 3,
-      experience = 0)]))
+      level = 4, experience = 0)], attributes: @[attribute, attribute, attribute, attribute]))
   while currentStory.index.len == 0:
     startStory("Undead", dropItem)
     if currentStory.index.len > 0:
