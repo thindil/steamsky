835252b28d0530956452a578fbd5cb856d0b2bb3
thindil <thindil@laeran.pl>
2026-07-25 03:22:45 +0000

    refactor: continue work on move Nuklear to Nim
    FossilOrigin-Name: 8ecdf2ad9b75e663adf2bd84b2e21a185f62a7eb663b0dc055af06266c0d545f

--- src/ui/nuklear/nuklear.nim
+++ src/ui/nuklear/nuklear.nim
@@ -3010,19 +3010,19 @@ template group*(title, tooltip: string; flags: set[PanelFlags];
   if showTips:
     showTooltip2(text = tooltip)
 
-template groupScrolled*(xOffset, yOffset: Natural; title, tooltip: string;
+template groupScrolled*(x, y: Natural; title, tooltip: string;
     flags: set[PanelFlags]; content: untyped) =
   ## Set a group of widgets inside the parent
   ##
-  ## * xOffset - the starting x offset of the scrollbar
-  ## * yOffset - the starting y offset of the
+  ## * x       - the starting x offset of the scrollbar
+  ## * y       - the starting y offset of the
   ## * title   - the title of the group
   ## * tooltip - the tooltip to show on the group.
   ## * flags   - the set of PanelFlags for the group
   ## * content - the content of the group
   let showTips: bool = widgetIsHovered()
-  if nk_group_scrolled_offset_begin(ctx = ctx, x_offset = xOffset.nk_uint,
-      y_Offset = yOffset.nk_unit, ctitle = title.cstring, cflags = winSetToInt(
+  if nk_group_scrolled_offset_begin(ctx = ctx, x_offset = x.nk_uint,
+      y_Offset = y.nk_uint, ctitle = title.cstring, cflags = winSetToInt(
       nimFlags = flags)):
     content
     nk_group_scrolled_end(ctx = ctx)
