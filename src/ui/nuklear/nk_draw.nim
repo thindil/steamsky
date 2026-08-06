bbb72b2bf0dc2e7ff6ee44893c4c8cb3451d2f8d
thindil <thindil@laeran.pl>
2026-08-06 03:03:07 +0000

    refactor: continue work on move Nuklear to Nim
    FossilOrigin-Name: a216090d0ba5aee13a8510d7e5150cc68fbc14b6fe6bb0334ed2bb1ea5360ced

--- src/ui/nuklear/nk_draw.nim
+++ src/ui/nuklear/nk_draw.nim
@@ -43,6 +43,16 @@ proc nk_draw_next*(cmd: ptr nk_draw_command; buffer: ptr nk_buffer;
     ctx): ptr nk_draw_command {.importc: "nk__draw_next", nodecl, raises: [],
     tags: [], contractual.}
   ## A binding to Nuklear's function. Internal use only
+template nkDrawForeach*(ctx; b: ptr nk_buffer; code: untyped) =
+  ## Draw the UI with commands from the buffer
+  ##
+  ## * ctx  - the Nuklear context
+  ## * b    - the buffer with Nuklear commands
+  ## * code - the code to exexute
+  var cmd: ptr nk_draw_command = nk_draw_begin(ctx = ctx, buffer = b)
+  while cmd != nil:
+    code
+    cmd = nk_draw_next(cmd = cmd, buffer = b, ctx = ctx)
 
 
 # -------------------
