148172522ef4217e33b62b9d31dc00cbd1a7d0d2
thindil <thindil@laeran.pl>
2026-08-22 03:08:49 +0000

    feat: started work on constructor for ProtoMobRecord
    FossilOrigin-Name: ce5acf55b8db74dae654deac528dc8f32c03f60b553d83a219af18124529e9fc

--- src/types.nim
+++ src/types.nim
@@ -1983,6 +1983,22 @@ type
     inventory*: seq[MobInventoryRecord]
     equipment*: EquipmentArray
 
+proc initProtoMobRecord*(attributes: seq[MobAttributeRecord] = @[]; skills: seq[
+    SkillInfo] = @[]; order: CrewOrders = rest; priorities: array[1..12,
+    Natural] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]): ProtoMobRecord {.raises: [],
+    tags: [], contractual.} =
+  ## Create a new data structure for a mob's prototype
+  ##
+  ## * attributes - The mob's attributes
+  ## * skills     - The mob's skills
+  ## * order      - The current order of the mob
+  ## * priorities - The orders priorities of the mob
+  ##
+  ## Returns the new structure with information about the selected prototype mob
+  return ProtoMobRecord(attributes: attributes, skills: skills, order: order,
+      priorities: priorities)
+
+type
   EnemyRecord* = object
     ## Used to store information about the enemy
     ##
