0f8c6e3fa9a76ee59ecb9a1c945296c1e10636f3
thindil <thindil@laeran.pl>
2026-09-02 03:09:11 +0000

    refactor: continue work on move Nuklear to Nim
    FossilOrigin-Name: c04b18772e2d00ca4d8c699dd636f709569d8030e0823dd328bdec772dc32248

--- src/ui/nuklear/nuklear_sdl_renderer.nim
+++ src/ui/nuklear/nuklear_sdl_renderer.nim
@@ -273,8 +273,6 @@ proc nk_sdl_init(win: WindowPtr; renderer: RendererPtr): PContext {.importc,
 proc nk_sdl_font_stash_begin(atlas: ptr ptr nk_font_atlas) {.importc, nodecl,
     raises: [], tags: [], contractual.}
   ## Internal Nuklear binding
-proc nk_sdl_font_stash_end() {.importc, nodecl, raises: [], tags: [], contractual.}
-  ## Internal Nuklear binding
 
 # High level bindings
 
@@ -760,21 +758,14 @@ proc nuklearLoadFont*(font: FontData; glyphsRanges: openArray[nk_rune] = [
   result = nk_font_atlas_add_from_file(atlas = sdl.atlas,
       filePath = font.path.cstring, height = font.size.cfloat * fontScale, config.addr)
   {.ruleOn: "namedParams".}
-  nk_sdl_font_stash_end()
-#  var w, h: cint = 0
-#  var image: pointer = nk_font_atlas_bake(atlas = sdl.atlas, width = w,
-#      height = h, fmt = atlasRGBA32)
-#  let sdlFontTexture: TexturePtr = SDL_CreateTexture(renderer = sdl.renderer,
-#      format = SDL_PIXELFORMAT_ARGB8888, access = SDL_TEXTUREACCESS_STATIC,
-#      w = w, h = h)
-#  if sdlFontTexture == nil:
-#    SDL_Log(fmt = "error creating texture")
-#    return
-#  discard SDL_UpdateTexture(texture = sdlFontTexture, rect = nil,
-#      pixels = image, pitch = 4 * w)
-#  SDL_SetTextureBlendMode(texture = sdlFontTexture,
-#      blendMode = SDL_BLENDMODE_BLEND)
-#  sdl.dev.fontTexture = sdlFontTexture
+  {.emit: """
+    const void *image; int w, h;
+    image = nk_font_atlas_bake(&sdl.atlas, &w, &h, NK_FONT_ATLAS_RGBA32);
+    nk_sdl_device_upload_atlas(image, w, h);
+    nk_font_atlas_end(&sdl.atlas, nk_handle_ptr(sdl.ogl.font_tex), &sdl.ogl.tex_null);
+    if (sdl.atlas.default_font)
+        nk_style_set_font(&sdl.ctx, &sdl.atlas.default_font->handle);
+  """.}
 
 proc nuklearSetDefaultFont*(defaultFont: ptr nk_font = nil;
     fontSize: int = 14) {.raises: [], tags: [], contractual.} =
@@ -787,13 +778,24 @@ proc nuklearSetDefaultFont*(defaultFont: ptr nk_font = nil;
     atlas: AtlasPtr = nil
     config: nk_font_config = new_nk_font_config(pixelHeight = 0)
     font: ptr nk_font = nil
+  {.emit: """
+    nk_font_atlas_init_default(&sdl.atlas);
+    nk_font_atlas_begin(&sdl.atlas);
+  """.}
   nk_sdl_font_stash_begin(atlas = atlas.addr)
   if defaultFont == nil:
     font = nk_font_atlas_add_default(atlas = atlas, height = fontSize.cfloat *
         fontScale, config = config.addr)
   else:
     font = defaultFont
-  nk_sdl_font_stash_end()
+  {.emit: """
+    const void *image; int w, h;
+    image = nk_font_atlas_bake(&sdl.atlas, &w, &h, NK_FONT_ATLAS_RGBA32);
+    nk_sdl_device_upload_atlas(image, w, h);
+    nk_font_atlas_end(&sdl.atlas, nk_handle_ptr(sdl.ogl.font_tex), &sdl.ogl.tex_null);
+    if (sdl.atlas.default_font)
+        nk_style_set_font(&sdl.ctx, &sdl.atlas.default_font->handle);
+  """.}
   nk_style_set_font(ctx = getContext(), font = font.handle.unsafeAddr)
 
 proc nuklearResizeWin*(width, height: int) {.raises: [], tags: [],
