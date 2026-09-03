eabec4f22b6c00bb3494b202874d59564190c7ae
thindil <thindil@laeran.pl>
2026-09-03 03:06:40 +0000

    refactor: continue work on better looking info about player's ship's crew
    FossilOrigin-Name: 067e6f73ed49e7d2043e147a868fb887733ed170b38f11c0f65784b6554ce498

--- src/ui/shipsui.nim
+++ src/ui/shipsui.nim
@@ -329,17 +329,14 @@ proc showShipInfo*(state: var GameState; dialog: var GameDialog) {.raises: [],
       showGeneralInfo(dialog = dialog, state = state)
   # The player's ship's crew info
   of 1:
-    setLayoutRowDynamic(height = height, cols = 1)
-    group(title = "ShipInfo", flags = {windowNoFlags}):
-      if dialog != none:
-        windowDisable()
-      setLayoutRowStatic(height = buttonHeight, cols = 1, width = 200)
-      imageLabelButton(image = images[moreOptionsIcon],
-          tooltip = "Show/Hide additional options related to managing the crew",
-          label = (if showCargoOptions: "Hide options" else: "Show options"),
-          alignment = right):
-        showCrewOptions = not showCrewOptions
-      showCrewInfo(dialog = dialog, height = height)
+    setLayoutRowStatic(height = buttonHeight, cols = 1, width = 200)
+    imageLabelButton(image = images[moreOptionsIcon],
+        tooltip = "Show/Hide additional options related to managing the crew",
+        label = (if showCargoOptions: "Hide options" else: "Show options"),
+        alignment = right):
+      showCrewOptions = not showCrewOptions
+    height -= buttonHeight
+    showCrewInfo(dialog = dialog, height = height)
   # The player's ship's modules info
   of 2:
     showModulesInfo(dialog = dialog, height = height)
