0d21b2ddcf4bf2b2fc2c58d306a6bb5df0762a8b
thindil <thindil@laeran.pl>
2026-09-04 03:06:28 +0000

    refactor: updated code to the new version of table widget
    FossilOrigin-Name: 0b8aaa8419e735da7af3fb024d6724ce959a39fc156e17993c7dcd72ecd41b59

--- src/ui/shipsuimodules.nim
+++ src/ui/shipsuimodules.nim
@@ -1219,7 +1219,7 @@ proc showModulesInfo*(dialog: var GameDialog; height: float) {.raises: [],
     ratio: array[3, cfloat] = [300.cfloat, 200, 500]
 
   table(name = "ModulesTable", xScroll = xOffset, headers = headers,
-      ratio = ratio, tableTooltip = "modules", height = height,
+      ratio = ratio, tableTooltip = "modules", tableHeight = height,
       headerCode = sortModules):
     for index in modulesIndexes:
       if not isStartingRow():
