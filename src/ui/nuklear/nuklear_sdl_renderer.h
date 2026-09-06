1bca1e1ad7cd1ebc268d9ee924b9de7b16cf17b3
thindil <thindil@laeran.pl>
2026-09-06 03:12:59 +0000

    refactor: removed unused code
    FossilOrigin-Name: f297224afc81791b8c9a5f366878578e164c354b9194dadc489ab28259769493

--- src/ui/nuklear/nuklear_sdl_renderer.h
+++ src/ui/nuklear/nuklear_sdl_renderer.h
@@ -16,16 +16,6 @@
 NK_API struct nk_context*   nk_sdl_init(SDL_Window *win, SDL_Renderer *renderer);
 NK_API void                 nk_sdl_font_stash_begin(struct nk_font_atlas **atlas);
 
-#if SDL_COMPILEDVERSION < SDL_VERSIONNUM(2, 0, 22)
-/* Metal API does not support cliprects with negative coordinates or large
- * dimensions. The issue is fixed in SDL2 with version 2.0.22 but until
- * that version is released, the NK_SDL_CLAMP_CLIP_RECT flag can be used to
- * ensure the cliprect is itself clipped to the viewport.
- * See discussion at https://discourse.libsdl.org/t/rendergeometryraw-producing-different-results-in-metal-vs-opengl/34953
- */
-#define NK_SDL_CLAMP_CLIP_RECT
-#endif
-
 #endif /* NK_SDL_RENDERER_H_ */
 
 /*
@@ -62,25 +52,6 @@ static struct nk_sdl {
 NK_API struct nk_context*
 nk_sdl_init(SDL_Window *win, SDL_Renderer *renderer)
 {
-#ifndef NK_SDL_CLAMP_CLIP_RECT
-    SDL_RendererInfo info;
-    SDL_version runtimeVer;
-
-    /* warn for cases where NK_SDL_CLAMP_CLIP_RECT should have been set but isn't */
-    SDL_GetRendererInfo(renderer, &info);
-    SDL_GetVersion(&runtimeVer);
-    if (strncmp("metal", info.name, 5) == 0 &&
-        SDL_VERSIONNUM(runtimeVer.major, runtimeVer.minor, runtimeVer.patch) < SDL_VERSIONNUM(2, 0, 22))
-    {
-        SDL_LogWarn(
-            SDL_LOG_CATEGORY_APPLICATION,
-            "renderer is using Metal API but runtime SDL version %d.%d.%d is older than compiled version %d.%d.%d, "
-            "which may cause issues with rendering",
-            runtimeVer.major, runtimeVer.minor, runtimeVer.patch,
-            SDL_MAJOR_VERSION, SDL_MINOR_VERSION, SDL_PATCHLEVEL
-        );
-    }
-#endif
     sdl.win = win;
     sdl.renderer = renderer;
     nk_buffer_init_default(&sdl.ogl.cmds);
