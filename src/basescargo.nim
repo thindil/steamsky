2d5b3b869237099885f45e468a1c47e8aa2c7ca2
thindil <thindil@laeran.pl>
2026-09-07 03:03:20 +0000

    refactor: changed returned type of findBaseCargo procedure
    FossilOrigin-Name: cb12d66023f9bde7ea652407dea1a44a47bdfb9f40cd6d17799656c60d7ad951

--- src/basescargo.nim
+++ src/basescargo.nim
@@ -115,7 +115,7 @@ proc generateCargo*() {.raises: [KeyError], tags: [],
 
 proc findBaseCargo*(protoIndex: Natural;
     durability: ItemsDurability = defaultItemDurability; quality: ObjectQuality;
-    craftBonus: CraftBonuses; craftMalus: CraftMaluses): int {.raises: [],
+    craftBonus: CraftBonuses; craftMalus: CraftMaluses): ExtendedNatural {.raises: [],
         tags: [],
     contractual.} =
   ## Find the selected item in the currently visited base's cargo
