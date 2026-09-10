846b93f0be0127b9bd3471ada9aa2cb02a1045b1
thindil <thindil@laeran.pl>
2026-09-10 03:07:39 +0000

    refactor: changed starting position of crew member info dialog
    FossilOrigin-Name: 0b2104ed276e28074ff7ef611ea931427a597c63b73f9ef9424cdf5b5e580ac3

--- src/ui/shipsuicrew.nim
+++ src/ui/shipsuicrew.nim
@@ -417,7 +417,7 @@ proc setMemberInfo(data: int; dialog: var GameDialog) {.raises: [], tags: [
     tiredPoints = 0
   setPriorites = member.orders
   setInventoryInfo(dialog = dialog)
-  setDialog(x = windowWidth / 5, y = windowHeight / 9)
+  setDialog(x = windowWidth / 7, y = windowHeight / 11)
 
 proc showAttributes(member: MemberData; dialog: var GameDialog) {.raises: [],
     tags: [RootEffect], contractual.} =
