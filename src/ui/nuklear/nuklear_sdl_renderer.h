1a22beed86ada7562493bb518e60364ede7e2063
thindil <thindil@laeran.pl>
2026-09-11 03:02:22 +0000

    refactor: continue work on move Nuklear to Nim
    FossilOrigin-Name: 16d9e8b6f4d1af856109f954c8f423844c1508990d23b2b9f1f14f5fa0af1396

--- src/ui/nuklear/nuklear_sdl_renderer.h
+++ src/ui/nuklear/nuklear_sdl_renderer.h
@@ -11,8 +11,6 @@
 #ifndef NK_SDL_RENDERER_H_
 #define NK_SDL_RENDERER_H_
 
-/*#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>*/
 NK_API struct nk_context*   nk_sdl_init(SDL_Window *win, SDL_Renderer *renderer);
 NK_API void                 nk_sdl_font_stash_begin(struct nk_font_atlas **atlas);
 
@@ -28,12 +26,6 @@ NK_API void                 nk_sdl_font_stash_begin(struct nk_font_atlas **atlas
 #ifdef NK_SDL_RENDERER_IMPLEMENTATION
 
 #include <strings.h>
-
-/*struct nk_sdl_device {
-    struct nk_buffer cmds;
-    struct nk_draw_null_texture tex_null;
-    SDL_Texture *font_tex;
-};*/
 
 static struct nk_sdl {
     SDL_Window *win;
