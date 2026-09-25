16fbac845cf1707ceeb18bb6bdaf53ba4efd216c
thindil <thindil@laeran.pl>
2026-09-25 02:59:01 +0000

    refactor: removed unused code
    FossilOrigin-Name: 184fd1b28c622ffe07fd1a767d9e63509455552581cbc856a868148649ee2ced

--- src/ui/nuklear/nuklear.nim
+++ src/ui/nuklear/nuklear.nim
@@ -3329,33 +3329,3 @@ template tooltip*(x, y, width: float; content: untyped) =
   if createTooltip(width2 = width, x2 = x, y2 = y):
     content
     ctx.nk_tooltip_end
-
-# ------
-# Vertex
-# ------
-when defined(nkIncludeVertexBufferOutput):
-  proc nkDrawListInit(list: var DrawList) {.raises: [], tags: [],
-      contractual.} =
-    ## Initialize the drawing list
-    for i in 0..list.circleVtx.high:
-      let a: float = (i.float / list.circleVtx.len.float) * 2.0 * PI
-      list.circleVtx[i].x = a.cos
-      list.circleVtx[i].y = a.sin
-
-# -------
-# Context
-# -------
-proc nkInit*(ctx: var Context; font: UserFont = UserFont()) {.raises: [],
-    tags: [], contractual.} =
-  ## Init the Nuklear library
-  ##
-  ## * ctx  - the Nuklear context to set
-  ## * font - the font used in the UI. If empty, use the default Nuklear font
-  ##
-  ## Returns the modified parameter ctx
-  defaultStyle()
-  ctx.seq = 1
-  if font.height > 0.0:
-    ctx.style.font = font
-  when defined(nkIncludeVertexBufferOutput):
-    nkDrawListInit(list = ctx.drawList)
